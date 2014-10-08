f39fb2c729559325f6ca888dcbc0984713737f6c
Frederic van der Essen <fva@openerp.com / fvdessen+o@gmail.com>
2014-10-08 05:42:53 +0200

    [IMP] point_of_sale: adding a security pin field on users, and checking it when changing the cashier with very basic GUI

--- addons/point_of_sale/static/src/js/models.js
+++ addons/point_of_sale/static/src/js/models.js
@@ -165,7 +165,7 @@ function openerp_pos_models(instance, module){ //module is instance.point_of_sal
             }
         },{
             model:  'res.users',
-            fields: ['name','ean13'],
+            fields: ['name','pos_security_pin','ean13'],
             domain: null,
             loaded: function(self,users){ self.users = users; },
         },{
