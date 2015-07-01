7ee1261aa1da32005067feceb91053f9883675ae
Xavier Morel <xmo@odoo.com>
2015-07-01 13:48:20 +0200

    [IMP] move module graph to registry
    also get assertions report thing from registry instead of passing it around

--- odoo/modules/loading.py
+++ odoo/modules/loading.py
@@ -11,15 +11,15 @@ from odoo.tools import pycompat
 
 def load_module_graph(cr, graph, status=None, perform_checks=True, skip_modules=None, report=None):
     return odoo.registry(cr.dbname).load_module_graph(
-        cr, graph, perform_checks=perform_checks, skip_modules=skip_modules, report=report)
+        cr, perform_checks=perform_checks, skip_modules=skip_modules)
 
 def _check_module_names(cr, module_names):
     return odoo.registry(cr.dbname)._check_module_names(cr, module_names)
 
 def load_marked_modules(cr, graph, states, force, progressdict, report, loaded_modules, perform_checks):
     return odoo.registry(cr.dbname).load_marked_modules(
-        cr, graph, states, force=force, report=report,
-        loaded_modules=loaded_modules, perform_checks=perform_checks)
+        cr, states, force=force, loaded_modules=loaded_modules,
+        perform_checks=perform_checks)
 
 def load_modules(db, force_demo=False, status=None, update_module=False):
     return odoo.registry(db).load_modules(force_demo=force_demo, update_module=update_module)
