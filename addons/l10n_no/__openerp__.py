fea31e97268012ddbda185172b1d7ea76c1a43d1
cod-odoo <cod@openerp.com>
2015-07-14 12:32:14 +0530

    [REM]Removed account_chart module from depends coz it is now no more.

--- addons/l10n_no/__openerp__.py
+++ addons/l10n_no/__openerp__.py
@@ -7,7 +7,7 @@
     "author" : "Rolv Råen",
     "category" : "Localization/Account Charts",
     "description": "This is the module to manage the accounting chart for Norway in Open ERP.",
-    "depends" : ["account", "base_iban", "base_vat", "account_chart"],
+    "depends" : ["account", "base_iban", "base_vat"],
     "demo_xml" : [],
     "data" : ["account_chart.xml",
                     'account_tax.xml','account_chart_template.yml'],
