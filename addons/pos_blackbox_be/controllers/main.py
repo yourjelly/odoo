121aaf603d50c6974ad86a59bdd306376c28f9ef
Joren Van Onder <jov@odoo.com>
2015-10-16 16:42:22 +0200

    Add hw modules to the controller

--- addons/pos_blackbox_be/controllers/main.py
+++ addons/pos_blackbox_be/controllers/main.py
@@ -11,21 +11,23 @@ class GovCertificationController(http.Controller):
     def handler(self):
         data = {'files': []}
         relative_file_paths_to_show = [
-            "data/pos_blackbox_be_data.xml",
-            "models/pos_blackbox_be.py",
-            "security/ir.model.access.csv",
-            "security/pos_blackbox_be_security.xml",
-            "static/lib/sha1.js",
-            "static/src/css/pos_blackbox_be.css",
-            "static/src/js/pos_blackbox_be.js",
-            "static/src/xml/pos_blackbox_be.xml",
-            "views/pos_blackbox_be_assets.xml",
-            "views/pos_blackbox_be_views.xml",
-            "controllers/main.py",
+            "pos_blackbox_be/data/pos_blackbox_be_data.xml",
+            "pos_blackbox_be/models/pos_blackbox_be.py",
+            "pos_blackbox_be/security/ir.model.access.csv",
+            "pos_blackbox_be/security/pos_blackbox_be_security.xml",
+            "pos_blackbox_be/static/lib/sha1.js",
+            "pos_blackbox_be/static/src/css/pos_blackbox_be.css",
+            "pos_blackbox_be/static/src/js/pos_blackbox_be.js",
+            "pos_blackbox_be/static/src/xml/pos_blackbox_be.xml",
+            "pos_blackbox_be/views/pos_blackbox_be_assets.xml",
+            "pos_blackbox_be/views/pos_blackbox_be_views.xml",
+            "pos_blackbox_be/controllers/main.py",
+            "hw_scanner/controllers/main.py",
+            "hw_scale/controllers/main.py"
         ]
 
         for relative_file_path in relative_file_paths_to_show:
-            absolute_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, relative_file_path))
+            absolute_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, relative_file_path))
             size_in_bytes = os.path.getsize(absolute_file_path)
 
             with open(absolute_file_path, 'r') as f:
