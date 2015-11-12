4b9964372fd3d33f01731620bb2f9329112f0402
Josse Colpart <jco@odoo.com>
2015-11-12 09:44:58 +0100

    [REF] (hr_)maintenance

--- addons/maintenance/__openerp__.py
+++ addons/maintenance/__openerp__.py
@@ -5,15 +5,15 @@
     'version': '1.0',
     'sequence': 125,
     'description': """
-        Track employees' equipment and manage maintenance requests.""",
+        Track equipment and manage maintenance requests.""",
     'depends': ['mail'],
     'summary': 'Equipments, Assets, Internal Hardware, Allocation Tracking',
     'data': [
         'security/maintenance.xml',
         'security/ir.model.access.csv',
         'data/maintenance_data.xml',
-        'views/maintenance_config_settings_views.xml',
         'views/maintenance_views.xml',
+        'views/maintenance_dashboard.xml',
     ],
     'demo': ['data/maintenance_demo.xml'],
     'installable': True,
