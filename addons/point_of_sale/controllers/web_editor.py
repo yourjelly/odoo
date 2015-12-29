5c548eb77b86bb76c21ef4820c200af6d4f2ac7e
Joren Van Onder <jov@odoo.com>
2015-12-29 16:23:28 +0100

    Clean up controller

--- addons/point_of_sale/controllers/web_editor.py
+++ addons/point_of_sale/controllers/web_editor.py
@@ -4,9 +4,9 @@ from openerp.http import request
 from openerp.addons.web_editor.controllers.main import Web_Editor
 
 
-class Web_Editor(Web_Editor):
+class WebEditorPointOfSale(Web_Editor):
     @http.route('/point_of_sale/field/screen_template', type='http', auth="user")
-    def point_of_sale_FieldTextHtmlEmailTemplate(self, model=None, res_id=None, field=None, callback=None, **kwargs):
+    def get_field_text_html(self, model=None, res_id=None, field=None, callback=None, **kwargs):
         kwargs['snippets'] = '/point_of_sale/snippets'
         kwargs['template'] = 'point_of_sale.FieldTextHtmlInline'
         extra_head = request.registry["ir.ui.view"].render(request.cr, request.uid, 'point_of_sale.extra_head', None, context=request.context)
@@ -14,5 +14,5 @@ class Web_Editor(Web_Editor):
         return self.FieldTextHtml(model, res_id, field, callback, head=extra_head, **kwargs)
 
     @http.route(['/point_of_sale/snippets'], type='json', auth="user", website=True)
-    def point_of_sale_snippets(self):
+    def get_snippets(self):
         return request.registry["ir.ui.view"].render(request.cr, request.uid, 'point_of_sale.screen_snippets', None, context=request.context)
