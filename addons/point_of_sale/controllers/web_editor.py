5708d2648755e944735a53c47ddf65aed2146eb5
Joren Van Onder <jov@odoo.com>
2015-12-31 15:57:57 +0100

    Rename FieldTextHtmlInline -> FieldTextHtml

--- addons/point_of_sale/controllers/web_editor.py
+++ addons/point_of_sale/controllers/web_editor.py
@@ -8,7 +8,7 @@ class WebEditorPointOfSale(Web_Editor):
     @http.route('/point_of_sale/field/customer_facing_display_template', type='http', auth="user")
     def get_field_text_html(self, model=None, res_id=None, field=None, callback=None, **kwargs):
         kwargs['snippets'] = '/point_of_sale/snippets'
-        kwargs['template'] = 'point_of_sale.FieldTextHtmlInline'
+        kwargs['template'] = 'point_of_sale.FieldTextHtml'
         extra_head = request.registry["ir.ui.view"].render(request.cr, request.uid, 'point_of_sale.extra_head', None, context=request.context)
 
         return self.FieldTextHtml(model, res_id, field, callback, head=extra_head, **kwargs)
