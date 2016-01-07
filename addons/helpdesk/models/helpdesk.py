54e33776f848266790d000af4650eee96c3082db
Fabien Pinckaers <fp@openerp.com>
2016-01-06 20:05:26 -0800

    [IMP] Helpdesk SLA working

--- addons/helpdesk/models/helpdesk.py
+++ addons/helpdesk/models/helpdesk.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from datetime import datetime, timedelta
+from dateutil import relativedelta
 from openerp import SUPERUSER_ID
 from openerp import api, fields, models, _
 import openerp.addons.decimal_precision as dp
@@ -131,12 +132,40 @@ class HelpdeskTag(models.Model):
     name = fields.Char(string='Helpdesk Tag', required=True)
     color = fields.Integer('Color')
 
+class HelpdeskSLA(models.Model):
+    _name = "helpdesk.sla"
+    _order = "sequence, name"
+    _description = "Helpdesk SLA Policies"
+
+    name = fields.Char('SLA Policy Name', required=True, index=True)
+    sequence = fields.Integer('Sequence', default=1, index=True)
+    note = fields.Text('SLA Policy Description')
+    active = fields.Boolean('Active', default=True)
+    condition_team_id = fields.Many2one('helpdesk.team', string='Team', required=True)
+    condition_type_id = fields.Many2one('helpdesk.type', string='Type')
+    condition_stage_id = fields.Many2one('helpdesk.stage', string='Stage', required=True)
+    condition_priority = fields.Selection([
+        ('0', 'All'),
+        ('1', 'All but low priorities'),
+        ('2', 'High & Urgent'),
+        ('3', 'Urgent Only'),
+    ], string='Priority', required=True, default='0')
+
+    time_days = fields.Integer('Time to Assign')
+    time_hours = fields.Integer('Time since Creation')
+    time_minutes = fields.Integer('Time to First Answer')
+
 class HelpdeskTicket(models.Model):
     _name = 'helpdesk.ticket'
     _description = 'Ticket'
     _order = 'sequence, id desc'
     _inherit = ['mail.thread', 'ir.needaction_mixin', 'utm.mixin']
     _mail_mass_mailing = _('Tickets')
+
+    @api.model
+    def _default_stage_id(self):
+        stages = self.env['helpdesk.stage'].search([], limit=1)
+        return stages and stages.id or False
 
     @api.multi
     def _read_group_stage_ids(self, domain, read_group_order=None, access_rights_uid=None):
@@ -154,9 +183,8 @@ class HelpdeskTicket(models.Model):
         'stage_id': _read_group_stage_ids
     }
 
-
-    name = fields.Char(string='Subject', required=True)
-    sequence = fields.Integer(string='Sequence', default=1)
+    name = fields.Char(string='Subject', required=True, index=True)
+    sequence = fields.Integer(string='Sequence', default=1, index=True)
 
     team_id = fields.Many2one('helpdesk.team', string='Helpdesk Team', required=True, ondelete='cascade', index=True)
     note = fields.Text(string='Description')
@@ -164,6 +192,7 @@ class HelpdeskTicket(models.Model):
 
     type_id = fields.Many2one('helpdesk.type', string='Type')
     tag_ids = fields.Many2many('helpdesk.tag', string='Tags')
+    sla_ids = fields.Many2many('helpdesk.sla', string='SLA Policies')
     company_id = fields.Many2one('res.company', string='Company')
     color = fields.Integer('Color Index')
 
@@ -174,7 +203,8 @@ class HelpdeskTicket(models.Model):
     partner_name = fields.Char(string='Requester Name', related="partner_id.name") #inverse="_set_partner_id") ???
     partner_email = fields.Char(string='Requester Email', related="partner_id.email") #inverse="_set_partner_id") ???
 
-    stage_id = fields.Many2one('helpdesk.stage', 'Stage')
+    stage_id = fields.Many2one('helpdesk.stage', 'Stage', default=_default_stage_id)
+    last_stage_update = fields.Datetime('Last Stage update')
     priority = fields.Selection([
             ('0', 'Low'),
             ('1', 'Normal'),
@@ -182,10 +212,31 @@ class HelpdeskTicket(models.Model):
             ('3', 'Urgent')
         ], string='Priority', default='1')
 
+    sla_id = fields.Many2one('helpdesk.sla', string='Failed SLA Policy ID', compute='_get_sla_id')
+    sla_name = fields.Char(string='Failed SLA Policy', compute='_get_sla_id')
+
     @api.multi
     def name_get(self):
         result = []
         for ticket in self:
             result.append((ticket.id, "%s (#%d)" % (ticket.name, ticket.id)))
         return result
+
+    @api.one
+    @api.depends('team_id','stage_id','priority','type_id')
+    def _get_sla_id(self):
+        dom = [('condition_team_id','=',self.team_id.id), ('condition_priority','<=', self.priority)]
+        if self.type_id: 
+            dom.append('|', ('condition_type_id','=',False), ('condition_type_id','=',self.type_id.id))
+        if self.stage_id: 
+            dom.append(('condition_stage_id.sequence','>=',self.stage_id.sequence))
+        for sla in self.env['helpdesk.sla'].search(dom):
+            dt = self.last_stage_update or datetime.today()
+            dt = dt - relativedelta.relativedelta(days=sla.time_days, hours=sla.time_hours, minutes=sla.time_minutes)
+            if self.active and (self.create_date < dt.strftime('%Y-%m-%d %H:%M:%S')):
+                self.sla_id = sla
+                self.sla_name = sla.name
+                return
+        self.sla_id = False
+        self.sla_name = False
 
