e9e678994b3073b3c2f2b4b6d1b71200b6757625
Martin Trigaux <mat@odoo.com>
2016-01-26 19:57:57 +0530

    l10n_be_invoice_bba: prepare code for better diff

--- addons/l10n_be_invoice_bba/partner.py
+++ addons/l10n_be_invoice_bba/partner.py
@@ -14,22 +14,15 @@ class res_partner(osv.osv):
     def _get_comm_type(self, cr, uid, context=None):
         res = self.pool.get('account.invoice')._get_reference_type(cr, uid,context=context)
         return res
-    
-    _columns = {
-        'out_inv_comm_type': fields.selection(_get_comm_type, 'Communication Type', change_default=True,
-            help='Select Default Communication Type for Outgoing Invoices.' ),
-        'out_inv_comm_algorithm': fields.selection([
-            ('random','Random'),
-            ('date','Date'),
-            ('partner_ref','Customer Reference'),
-            ], 'Communication Algorithm',
-            help='Select Algorithm to generate the Structured Communication on Outgoing Invoices.' ),
-    }
 
     def _commercial_fields(self, cr, uid, context=None):
         return super(res_partner, self)._commercial_fields(cr, uid, context=context) + \
             ['out_inv_comm_type', 'out_inv_comm_algorithm']
-
+    
+    _columns = {
+        'out_inv_comm_type': fields.selection(_get_comm_type, 'Communication Type', change_default=True,
+            help='Select Default Communication Type for Outgoing Invoices.' ),
+    }
 
     _default = {
         'out_inv_comm_type': 'none',
