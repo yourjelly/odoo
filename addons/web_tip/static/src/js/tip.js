23ceee177abd91c33ae83b53aca06756aef27ade
Csaba Fazekas <csabafazekasster@gmail.com>
2016-05-25 09:25:46 +0200

    [IMP] Web tip: fix js issues, style popover

--- addons/web_tip/static/src/js/tip.js
+++ addons/web_tip/static/src/js/tip.js
@@ -1,283 +1,295 @@
 odoo.define('web_tip.web_tip', function (require) {
-"use strict";
+    "use strict";
 
-var core = require('web.core');
-var Model = require('web.DataModel');
-var WebClient = require('web.WebClient');
-var formView = require('web.FormView');
-var utils = require('web.utils');
+    var core = require('web.core');
+    var Model = require('web.DataModel');
+    var WebClient = require('web.WebClient');
+    var formView = require('web.FormView');
+    var utils = require('web.utils');
 
-var bus = core.bus;
-var Class = core.Class;
+    var bus = core.bus;
+    var Class = core.Class;
 
-var Tips = Class.extend({
-    init: function() {
-        var self = this;
+    var Tips = Class.extend({
+        init: function() {
+            var self = this;
 
-        this.tips = [];
-        this.view = null;
+            this.tips = [];
+            this.view = null;
 
-        var Tips = new Model('web.tip');
-        Tips.query(['title', 'description', 'action_id', 'model', 'type', 'mode', 'trigger_selector',
-            'highlight_selector', 'end_selector', 'end_event', 'placement', 'is_consumed'])
-            .all().then(function(tips) {
-                self.tips = tips;
-            });
+            var Tips = new Model('web.tip');
+            Tips.query(['title', 'description', 'action_id', 'model', 'type', 'mode', 'trigger_selector',
+                'highlight_selector', 'end_selector', 'end_event', 'placement', 'is_consumed'])
+                .all().then(function(tips) {
+                    self.tips = tips;
+                });
 
-        bus.on('view_shown', this, function(view) {
-            if (_.keys(view.fields_view).length === 0) {
-                view.on('view_loaded', this, function(fields_view) {
+            bus.on('view_shown', this, function(view) {
+                if (_.keys(view.fields_view).length === 0) {
+                    view.on('view_loaded', this, function(fields_view) {
+                        self.view = view;
+                        self.on_view();
+                    });
+                } else {
                     self.view = view;
                     self.on_view();
+                }
+            });
+        },
+
+        on_view: function() {
+            var self = this;
+
+            bus.on('DOM_updated', this, function() {
+                var action_id = self.view.ViewManager.action ? self.view.ViewManager.action.id : null;
+                var model = self.view.fields_view.model;
+                var mode = self.view.fields_view.type;
+
+                if (mode == 'form') {
+                    self.on_form_view(action_id, model);
+                }
+                else if (self.view.hasOwnProperty('editor')) {
+                    self.view.on('view_list_rendered', self, function() {
+                        self.eval_tips(action_id, model, mode);
+                    });
+                }
+                else {
+                    self.eval_tips(action_id, model, mode);
+                }
+            });
+        },
+
+        on_form_view: function(action_id, model) {
+            var self = this;
+            var type = this.view.datarecord ? this.view.datarecord.type : null
+
+            if ($('.oe_chatter').length > 0) {
+                this.view.on('chatter_messages_displayed', this, function() {
+                    self.eval_tips(action_id, model, 'form', type);
                 });
             } else {
-                self.view = view;
-                self.on_view();
+                this.eval_tips(action_id, model, 'form', type);
             }
-        });
-    },
 
-    on_view: function() {
-        var self = this;
-
-        bus.on('DOM_updated', this, function() {
-            var action_id = self.view.ViewManager.action ? self.view.ViewManager.action.id : null;
-            var model = self.view.fields_view.model;
-            var mode = self.view.fields_view.type;
-
-            if (mode == 'form') {
-                self.on_form_view(action_id, model);
-            }
-            else if (self.view.hasOwnProperty('editor')) {
-                self.view.on('view_list_rendered', self, function() {
-                    self.eval_tips(action_id, model, mode);
-                });
-            }
-            else {
-                self.eval_tips(action_id, model, mode);
-            }
-        });
-    },
-
-    on_form_view: function(action_id, model) {
-        var self = this;
-        var type = this.view.datarecord ? this.view.datarecord.type : null
-
-        if ($('.oe_chatter').length > 0) {
-            this.view.on('chatter_messages_displayed', this, function() {
+            this.view.on('to_edit_mode', this, function() {
+                self.remove_tips();
+            });
+            this.view.on('to_view_mode', this, function() {
                 self.eval_tips(action_id, model, 'form', type);
             });
-        } else {
-            this.eval_tips(action_id, model, 'form', type);
-        }
+        },
 
-        this.view.on('to_edit_mode', this, function() {
-            self.remove_tips();
-        });
-        this.view.on('to_view_mode', this, function() {
-            self.eval_tips(action_id, model, 'form', type);
-        });
-    },
+        eval_tips: function(action_id, model, mode, type) {
+            var self = this;
+            var filter = {};
+            var valid_tips = [];
+            var tips = [];
 
-    eval_tips: function(action_id, model, mode, type) {
-        var self = this;
-        var filter = {};
-        var valid_tips = [];
-        var tips = [];
-
-        if (action_id) {
-            valid_tips = _.filter(self.tips, function (tip) {
-                return tip.action_id[0] === action_id;
-            });
-        }
-
-        filter.model = model;
-        filter.mode = mode;
-        filter.is_consumed = false;
-        tips = _.where(self.tips, filter);
-        // To take into account a tip without fixed model : e.g. a generic tip on the breadcrumb
-        tips = tips.concat(_.where(self.tips, {mode: mode, is_consumed: false, model:false}))
-
-        if (type) {
-            tips = _.filter(tips, function(tip) {
-                if (!tip.type) {
-                    return true;
-                }
-                return tip.type === type;
-            });
-        }
-
-        valid_tips = _.uniq(valid_tips.concat(tips));
-        _.each(valid_tips, function(tip) {
-            if (!tip.is_consumed) {
-                var t = new Tip(tip);
-                t.do_tip();
+            if (action_id) {
+                valid_tips = _.filter(self.tips, function (tip) {
+                    return tip.action_id[0] === action_id;
+                });
             }
-        });
-    },
 
-    remove_tips: function() {
-        $('.oe_breathing').remove();
-    },
-});
+            filter.model = model;
+            filter.mode = mode;
+            filter.is_consumed = false;
+            tips = _.where(self.tips, filter);
+            // To take into account a tip without fixed model : e.g. a generic tip on the breadcrumb
+            tips = tips.concat(_.where(self.tips, {mode: mode, is_consumed: false, model:false}))
 
-var Tip = Class.extend({
-    init: function(tip) {
-        this.tip = tip;
+            if (type) {
+                tips = _.filter(tips, function(tip) {
+                    if (!tip.type) {
+                        return true;
+                    }
+                    return tip.type === type;
+                });
+            }
 
-        this.highlight_selector = tip.highlight_selector;
-        this.end_selector = tip.end_selector ? tip.end_selector : tip.highlight_selector;
-        this.triggers = tip.trigger_selector ? tip.trigger_selector.split(',') : [];
-    },
+            valid_tips = _.uniq(valid_tips.concat(tips));
+            _.each(valid_tips, function(tip) {
+                if (!tip.is_consumed) {
+                    var t = new Tip(tip);
+                    t.do_tip();
+                }
+            });
+        },
 
-    do_tip: function() {
-        var self = this;
+        remove_tips: function() {
+            $('.oe_breathing').remove();
+        },
+    });
 
-        if(!$(this.highlight_selector).length > 0 || !$(this.highlight_selector).is(":visible")) {
-            return false;
-        }
+    var Tip = Class.extend({
+        init: function(tip) {
+            this.tip = tip;
 
-        for (var i = 0; i < this.triggers.length; i++) {
-            if(!$(this.triggers[i]).length > 0) {
+            this.highlight_selector = tip.highlight_selector;
+            this.end_selector = tip.end_selector ? tip.end_selector : tip.highlight_selector;
+            this.triggers = tip.trigger_selector ? tip.trigger_selector.split(',') : [];
+        },
+
+        do_tip: function() {
+            var self = this;
+            if(!$(this.highlight_selector).length > 0 || !$(this.highlight_selector).is(":visible")) {
                 return false;
             }
-        }
 
-        this.$element = $(this.highlight_selector).first();
-
-        if (!this.$element.next().hasClass('oe_breathing')) {
-            this.$breathing = $("<div>", { class: 'oe_breathing' });
-            this.$element.after(this.$breathing);
-        } else {
-            this.$breathing = this.$element.next();
-        }
-        this._set_breathing_position();
-
-        this.$breathing.on('mouseenter', function() {
-            self.$breathing.addClass('oe_explode').fadeOut(300);
-            self.trigger_tip();
-        });
-
-        // resize
-        bus.on('resize', this, function() {
-            self._set_breathing_position();
-        });
-
-        bus.on('image_loaded', this, function() {
-            self._set_breathing_position();
-        });
-
-        bus.on('please_reposition_tip', this, function() {
-            self._set_breathing_position();
-        });
-
-        return true;
-    },
-
-    trigger_tip: function() {
-        var self = this;
-
-        this.$element.popover({
-            placement: self.tip.placement,
-            title: self.tip.title,
-            content: self.tip.description,
-            html: true,
-            container: 'body',
-            animation: false,
-        }).popover('show');
-
-        this.$cross = $('<button type="button" class="close oe_tip_close">&times;</button>');
-
-        if (this.tip.title) {
-            $('.popover-title').prepend(this.$cross);
-        } else {
-            $('.popover-content').prepend(this.$cross);
-        }
-
-        // consume tip
-        $(this.end_selector).one(this.tip.end_event, function($ev) {
-            self.end_tip();
-        });
-
-        // dismiss tip
-        this.$cross.on('click', function($ev) {
-            self.end_tip();
-        });
-
-        $(document).on('keyup.web_tip', function($ev) {
-            if ($ev.which === 27) { // esc
-                self.end_tip();
+            for (var i = 0; i < this.triggers.length; i++) {
+                if(!$(this.triggers[i]).length > 0) {
+                    return false;
+                }
             }
-        });
 
-        // dismiss tip on outside click
-        $(document).on("click", function(e) {
-          if ($(e.target).closest(this.tip).length === 0) { // if click outside of searchInput
-               self.end_tip();
+            this.$element = $(this.highlight_selector).first();
+
+            if (!this.$element.next().hasClass('oe_breathing')) {
+                this.$breathing = $("<div>", { class: 'oe_breathing' });
+                this.$element.after(this.$breathing);
+            } else {
+                this.$breathing = this.$element.next();
             }
-         });
+            this._set_breathing_position();
 
-        // resize
-        bus.on('resize', this, function() {
-            self._set_popover_position();
-        });
-    },
+            this.$breathing.on('mouseenter', function() {
+                self.trigger_tip();
+            });
 
-    scroll_to_tip: function(){
-        var scroll = _.find(this.$element.parentsUntil('body'), function(el) {
-            var overflow = $(el).css('overflow-y');
-            return (overflow === 'auto' || overflow === 'scroll');
-        });
-        if (scroll) {
-            $(scroll).scrollTo(this.$element);
-        }
-    },
+            // resize
+            bus.on('resize', this, function() {
+                self._set_breathing_position();
+            });
 
-    end_tip: function() {
-        var Tips = new Model('web.tip');
+            bus.on('image_loaded', this, function() {
+                self._set_breathing_position();
+            });
 
-        this.$element.popover('destroy');
-        this.$breathing.remove();
-        this.$cross.remove();
+            bus.on('please_reposition_tip', this, function() {
+                self._set_breathing_position();
+            });
 
-        $(document).off('keyup.web_tip');
+            return true;
+        },
 
-        Tips.call('consume', [this.tip.id], {});
-        this.tip.is_consumed = true;
-    },
+        trigger_tip: function() {
+            var self = this;
 
-    _set_breathing_position: function() {
-        this.$breathing.position({ my: "left", at: "right", of: this.$element });
-    },
+            this.$element.popover({
+                placement: self.tip.placement,
+                title: self.tip.title,
+                content: self.tip.description,
+                html: true,
+                container: 'body',
+                animation: false,
+            }).popover('show');
 
-    _set_popover_position: function() {
-        if (!this.tip.is_consumed) {
-            this.$element.popover('show');
-            this.scroll_to_tip();
+            this.popover = this.$element.data('bs.popover').$tip;
+
+            this.$cross = $('<button type="button" class="close oe_tip_close">&times;</button>');
 
             if (this.tip.title) {
                 $('.popover-title').prepend(this.$cross);
             } else {
                 $('.popover-content').prepend(this.$cross);
             }
+
+            // hide tip
+            if (this.popover.hasClass('out')) {
+                this.popover.removeClass('out');
+            }
+            this.popover.on('mouseleave', function() {
+                self.hide_tip();
+            });
+
+            // consume tip
+            $(this.end_selector).one(this.tip.end_event, function($ev) {
+                self.end_tip();
+            });
+
+            // dismiss tip
+            this.$cross.on('click', function($ev) {
+                self.end_tip();
+            });
+
+            $(document).on('keyup.web_tip', function($ev) {
+                if ($ev.which === 27) { // esc
+                    self.end_tip();
+                }
+            });
+
+            // dismiss tip on outside click
+            $(document).on("click", function(e) {
+              if ($(e.target).closest(this.tip).length === 0) { // if click outside of searchInput
+                   self.end_tip();
+                }
+             });
+
+            // resize
+            bus.on('resize', this, function() {
+                self._set_popover_position();
+            });
+        },
+
+        scroll_to_tip: function(){
+            var scroll = _.find(this.$element.parentsUntil('body'), function(el) {
+                var overflow = $(el).css('overflow-y');
+                return (overflow === 'auto' || overflow === 'scroll');
+            });
+            if (scroll) {
+                $(scroll).scrollTo(this.$element);
+            }
+        },
+
+        hide_tip: function() {
+            this.popover.removeClass('in').addClass('out');
+        },
+
+        end_tip: function() {
+            var Tips = new Model('web.tip');
+
+            this.$element.popover('destroy');
+            this.$breathing.remove();
+            this.$cross.remove();
+
+            $(document).off('keyup.web_tip');
+
+            Tips.call('consume', [this.tip.id], {});
+            this.tip.is_consumed = true;
+        },
+
+        _set_breathing_position: function() {
+            this.$breathing.position({ my: "left", at: "right", of: this.$element });
+        },
+
+        _set_popover_position: function() {
+            if (!this.tip.is_consumed) {
+                this.$element.popover('show');
+                this.scroll_to_tip();
+
+                if (this.tip.title) {
+                    $('.popover-title').prepend(this.$cross);
+                } else {
+                    $('.popover-content').prepend(this.$cross);
+                }
+            }
+        },
+    });
+
+    WebClient.include({
+        show_application: function() {
+            this._super();
+            this.tips_handler = new Tips();
         }
-    },
-});
+    });
 
-WebClient.include({
-    show_application: function() {
-        this._super();
-        this.tips_handler = new Tips();
-    }
-});
+    var FieldStatus = core.form_widget_registry.get('statusbar');
 
-var FieldStatus = core.form_widget_registry.get('statusbar');
-
-FieldStatus.include({
-    render_value: function() {
-        this._super();
-        this.trigger('please_reposition_tip');
-    }
-});
+    FieldStatus.include({
+        render_value: function() {
+            this._super();
+            this.trigger('please_reposition_tip');
+        }
+    });
 
 });
