b6c53a6870158f9f863cc6735c9f35243440cd37
tdi-odoo <tdi@odoo.com>
2016-09-22 15:40:27 +0200

    [FIX] stock: remove propagation of uom_qty
    This fix (done by order of @jco-odoo) is just to test if the runbot is ok with the removal of the back propagation of the uom_qty in stock.

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -265,10 +265,10 @@ class StockMove(models.Model):
 
         propagated_changes_dict = {}
         #propagation of quantity change
-        if vals.get('product_uom_qty'):
-            propagated_changes_dict['product_uom_qty'] = vals['product_uom_qty']
-        if vals.get('product_uom_id'):
-            propagated_changes_dict['product_uom_id'] = vals['product_uom_id']
+        # if vals.get('product_uom_qty'):
+        #     propagated_changes_dict['product_uom_qty'] = vals['product_uom_qty']
+        # if vals.get('product_uom_id'):
+        #     propagated_changes_dict['product_uom_id'] = vals['product_uom_id']
         #propagation of expected date:
         propagated_date_field = False
         if vals.get('date_expected'):
