2909212dab57f7225befd84f6ea53e05a104c629
tdi-odoo <tdi@odoo.com>
2016-09-26 13:30:06 +0200

    [FIX] account: cleaning code after review
    This fix cleans the code of electronical payment after review of @qdp-odoo.

--- addons/payment/models/account_journal.py
+++ addons/payment/models/account_journal.py
@@ -7,17 +7,6 @@ from odoo.exceptions import ValidationError
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    @api.one
-    def _create_check_sequence(self):
-        """ Create a check sequence for the journal """
-        self.check_sequence_id = self.env['ir.sequence'].sudo().create({
-            'name': self.name + _(" : Check Number Sequence"),
-            'implementation': 'no_gap',
-            'padding': 5,
-            'number_increment': 1,
-            'company_id': self.company_id.id,
-        })
-
     @api.model
     def _enable_electronic_payment_on_bank_journals(self):
         """ Enables electronic payment method and add a check sequence on bank journals.
@@ -25,7 +14,5 @@ class AccountJournal(models.Model):
         """
         electronic = self.env.ref('payment.account_payment_method_electronic')
         bank_journals = self.search([('type', '=', 'bank')])
-        for bank_journal in bank_journals:
-            bank_journal.write({
-                'inbound_payment_method_ids': [(4, electronic.id, None)],
-            })
+        if bank_journals:
+            bank_journals.write({'inbound_payment_method_ids': [(4, electronic.id, None)]})
