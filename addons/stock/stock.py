abd3a77e97cfe7f8b5f39f9555c469e2a212d7ba
Josse Colpaert <jco@odoo.com>
2016-11-15 11:08:05 +0100

    [FIX] a lot simpler

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -1312,9 +1312,9 @@ class stock_picking(osv.osv):
                         if quant.id in quants_in_package_done:
                             continue
 
-                        #check if the quant is matching the operation details (it should be in the package)
+                        #check if the quant is matching the operation details
                         if ops.package_id:
-                            flag = quant.package_id and (quant.package_id.parent_left >= ops.package_id.parent_left) and (quant.package_id.parent_left < ops.package_id.parent_right) or False
+                            flag = quant.package_id == ops.package_id
                         else:
                             flag = not quant.package_id.id
                         flag = flag and ((ops.lot_id and ops.lot_id.id == quant.lot_id.id) or not ops.lot_id)
