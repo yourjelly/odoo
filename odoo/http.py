958cbe5756ec37e67456a2dd1a423ce16b53e8e8
Julien Legros <jle@odoo.com>
2016-12-21 11:21:44 +0100

    [IMP] http: scatter sessions across multiple directories

--- odoo/http.py
+++ odoo/http.py
@@ -48,7 +48,7 @@ import odoo
 from odoo.service.server import memory_info
 from odoo.service import security, model as service_model
 from odoo.tools.func import lazy_property
-from odoo.tools import ustr, consteq, frozendict
+from odoo.tools import ustr, consteq, frozendict, ignore
 
 from odoo.modules.module import module_manifest
 
@@ -1175,6 +1175,35 @@ class OpenERPSession(werkzeug.contrib.sessions.Session):
                 except IOError:
                     pass
 
+class OpenERPFilesystemSessionStore(werkzeug.contrib.sessions.FilesystemSessionStore):
+    def get_session_filename(self, sid):
+        # scatter sessions across 256 directories
+        sha_dir = sid[:2]
+        dirname = os.path.join(self.path, sha_dir)
+        session_path = os.path.join(dirname, sid)
+        return session_path
+
+    def save(self, session):
+        session_path = self.get_session_filename(session.sid)
+        dirname = os.path.dirname(session_path)
+        if not os.path.isdir(dirname):
+            with ignore(OSError):
+                os.mkdir(dirname, 0755)
+        super(OpenERPFilesystemSessionStore, self).save(session)
+
+    def get(self, sid):
+        # retro compatibility
+        old_path = super(OpenERPFilesystemSessionStore, self).get_session_filename(sid)
+        session_path = self.get_session_filename(sid)
+        if os.path.isfile(old_path) and not os.path.isfile(session_path):
+            dirname = os.path.dirname(session_path)
+            if not os.path.isdir(dirname):
+                with ignore(OSError):
+                    os.mkdir(dirname, 0755)
+            with ignore(OSError):
+                os.rename(old_path, session_path)
+        return super(OpenERPFilesystemSessionStore, self).get(sid)
+
 
 def session_gc(session_store):
     if random.random() < 0.001:
@@ -1290,7 +1319,7 @@ class Root(object):
         # Setup http sessions
         path = odoo.tools.config.session_dir
         _logger.debug('HTTP sessions stored in: %s', path)
-        return werkzeug.contrib.sessions.FilesystemSessionStore(path, session_class=OpenERPSession)
+        return OpenERPFilesystemSessionStore(path, session_class=OpenERPSession)
 
     @lazy_property
     def nodb_routing_map(self):
