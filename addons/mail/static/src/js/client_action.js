4a63c322196b71b751eab6d529b4b862d58e87c8
Akash Bhavsar <abh@odoo.com>
2017-01-19 16:28:26 +0530

    [ADD] mail: enable desktop notification on home of enterprise

--- addons/mail/static/src/js/client_action.js
+++ addons/mail/static/src/js/client_action.js
@@ -690,3 +690,42 @@ var ChatAction = Widget.extend(ControlPanelMixin, {
 core.action_registry.add('mail.chat.instant_messaging', ChatAction);
 
 });
+
+odoo.define('mail.NotificationPanel', function (require) {
+"use strict";
+
+var core = require('web.core');
+var utils = require('mail.utils');
+var Widget = require('web.Widget');
+
+var AppSwitcher = require('web_enterprise.AppSwitcher');
+var ChatAction = require('mail.chat_client_action');
+
+var QWeb = core.qweb;
+
+AppSwitcher.include({
+        init: function() {
+            var self = this;
+            this._super.apply(this, arguments);
+            this.notification_bar = (window.Notification && window.Notification.permission === "default");
+        },
+        start: function() {
+            var self = this;
+            this._super.apply(this, arguments);
+            if (_.last(odoo.session_info.server_version_info) == 'e') {
+                var notification_perm = $(QWeb.render("mail.permission_notification", { widget: this }));
+                notification_perm.insertBefore(this.$menu_search);
+                notification_perm.find('.o_mail_request_permission').on('click', function() {
+                    event.preventDefault();
+                    self.$(".o_mail_annoying_notification_bar").slideUp();
+                    var def = window.Notification.requestPermission();
+                    if (def) {
+                        def.then(function () {
+                            utils.send_notification('Permission granted', 'Odoo has now the permission to send you native notifications on this device.');
+                        });
+                    }
+                });
+            }
+        }
+});
+});
