9126206d0fe6825165f19b99259d653f4915a738
Mohammed Shekha <msh@openerp.com>
2017-02-02 15:05:32 +0530

    [REM]Remove unnecessary files added during rebase by mistake

--- training/open_academy/models/__init__.py
+++
@@ -1,2 +0,0 @@
-import course
-import session
--- training/open_academy/models/session.py
+++
@@ -1,9 +0,0 @@
-from openerp import _, api, fields, models
-
-class Session(models.Model):
-	_name = "session.session"
-
-	name = fields.Char()
-	course_id = fields.Many2one("course.course")
-	instructor_id = fields.Many2one("res.partner")
-	
