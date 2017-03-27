6c1ab751b71a8f51db3dd5a2b7029486c8896535
jpr-odoo <jpr@openerp.com>
2017-03-27 15:22:35 +0530

    [ADD] website_account: added a new module for online viewing invoices and payment [MOV] moved invoice-related code from website_portal_sale to website_account

---
+++ addons/website_account/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Website Portal for Invoices',
+    'category': 'Website',
+    'summary': 'Add your invoices document in the frontend portal',
+    'version': '1.0',
+    'description': """
+Add your invoices document in the frontend portal.
+Your customers will be able to connect to their portal to see the list (and the state) of their invoices (pdf report).
+    """,
+    'depends': [
+        'account',
+        'website_payment'
+    ],
+    'data': [
+        'views/website_account_templates.xml',
+        'views/account_invoice_views.xml',
+        'security/ir.model.access.csv',
+        'security/account_invoice_security.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
