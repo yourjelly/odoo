6fc6f4f95c5c84a8b98cf1d5e48035ef8081b4a0
Josse Colpaert <jco@odoo.com>
2017-04-07 10:25:19 +0200

    [WIP] MTO purchases don't necessarily generate move for every element

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -644,23 +644,28 @@ class PurchaseOrderLine(models.Model):
         }
         # Fullfill all related procurements with this po line
         diff_quantity = self.product_qty - qty
-        for procurement in self.procurement_ids:
-            # If the procurement has some moves already, we should deduct their quantity
-            sum_existing_moves = sum(x.product_qty for x in procurement.move_ids if x.state != 'cancel')
-            existing_proc_qty = procurement.product_id.uom_id._compute_quantity(sum_existing_moves, procurement.product_uom)
-            procurement_qty = procurement.product_uom._compute_quantity(procurement.product_qty, self.product_uom) - existing_proc_qty
-            if float_compare(procurement_qty, 0.0, precision_rounding=procurement.product_uom.rounding) > 0 and float_compare(diff_quantity, 0.0, precision_rounding=self.product_uom.rounding) > 0:
-                tmp = template.copy()
-                tmp.update({
-                    'product_uom_qty': min(procurement_qty, diff_quantity),
-                    'move_dest_ids': procurement.move_dest_ids.ids,  # move destination is same as procurement destination
-                    'procurement_id': procurement.id,
-                    'propagate': procurement.rule_id.propagate,
-                })
-                res.append(tmp)
-                diff_quantity -= min(procurement_qty, diff_quantity)
+        procurements = self.procurement_ids
+        if not self.order_id.picking_type_id.merge_moves:
+            for procurement in self.procurement_ids:
+                # If the procurement has some moves already, we should deduct their quantity
+                sum_existing_moves = sum(x.product_qty for x in procurement.move_ids if x.state != 'cancel')
+                existing_proc_qty = procurement.product_id.uom_id._compute_quantity(sum_existing_moves, procurement.product_uom)
+                procurement_qty = procurement.product_uom._compute_quantity(procurement.product_qty, self.product_uom) - existing_proc_qty
+                if float_compare(procurement_qty, 0.0, precision_rounding=procurement.product_uom.rounding) > 0 and float_compare(diff_quantity, 0.0, precision_rounding=self.product_uom.rounding) > 0:
+                    tmp = template.copy()
+                    tmp.update({
+                        'product_uom_qty': min(procurement_qty, diff_quantity),
+                        'move_dest_ids': [(4, x) for x in procurement.move_dest_ids.ids],  # move destination is same as procurement destination
+                        'procurement_id': procurement.id,
+                        'propagate': procurement.rule_id.propagate,
+                    })
+                    res.append(tmp)
+                    if procurement_qty > diff_quantity: #Not even necessary
+                        procurements -= procurement
+                    diff_quantity -= min(procurement_qty, diff_quantity)
         if float_compare(diff_quantity, 0.0,  precision_rounding=self.product_uom.rounding) > 0:
             template['product_uom_qty'] = diff_quantity
+            template['move_dest_ids'] = [(4, x) for x in procurements.mapped('move_dest_ids').ids]
             res.append(template)
         return res
 
