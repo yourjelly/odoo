24bd603da29dcf17b0398219d2f66b82334e430d
Josse Colpaert <jco@odoo.com>
2017-04-10 15:35:36 +0200

    [IMP] Rework do_new_transfer

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -654,68 +654,79 @@ class Picking(models.Model):
 
     @api.multi
     def do_new_transfer(self):
-        for pick in self:
-            pack_operations_delete = self.env['stock.pack.operation']
-            if not pick.move_lines and not pick.pack_operation_ids:
-                raise UserError(_('Please create some Initial Demand or Mark as Todo and create some Operations. '))
-            # In draft or with no pack operations edited yet, ask if we can just do everything
-            if pick.state == 'draft' or all([x.qty_done == 0.0 for x in pick.pack_operation_ids]):
-                # If no lots when needed, raise error
-                picking_type = pick.picking_type_id
-                if (picking_type.use_create_lots or picking_type.use_existing_lots):
-                    for pack in pick.pack_operation_ids:
-                        if pack.product_id and pack.product_id.tracking != 'none':
-                            raise UserError(_('Some products require lots/serial numbers, so you need to specify those first!'))
-                view = self.env.ref('stock.view_immediate_transfer')
-                wiz = self.env['stock.immediate.transfer'].create({'pick_id': pick.id})
-                # TDE FIXME: a return in a loop, what a good idea. Really.
-                return {
-                    'name': _('Immediate Transfer?'),
-                    'type': 'ir.actions.act_window',
-                    'view_type': 'form',
-                    'view_mode': 'form',
-                    'res_model': 'stock.immediate.transfer',
-                    'views': [(view.id, 'form')],
-                    'view_id': view.id,
-                    'target': 'new',
-                    'res_id': wiz.id,
-                    'context': self.env.context,
-                }
+        self.ensure_one()
+        pack_operations_delete = self.env['stock.pack.operation']
+        if not self.move_lines and not self.pack_operation_ids:
+            raise UserError(_('Please add some lines to move'))
+        # In draft or with no pack operations edited yet, ask if we can just do everything
+        if self.state == 'draft' or all([x.qty_done == 0.0 for x in self.pack_operation_ids]):
+            # If no lots when needed, raise error
+            picking_type = self.picking_type_id
+            if (picking_type.use_create_lots or picking_type.use_existing_lots):
+                for pack in self.pack_operation_ids:
+                    if pack.product_id and pack.product_id.tracking != 'none':
+                        raise UserError(_('Some products require lots/serial numbers, so you need to specify those first!'))
+            view = self.env.ref('stock.view_immediate_transfer')
+            wiz = self.env['stock.immediate.transfer'].create({'pick_id': self.id})
+            return {
+                'name': _('Immediate Transfer?'),
+                'type': 'ir.actions.act_window',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_model': 'stock.immediate.transfer',
+                'views': [(view.id, 'form')],
+                'view_id': view.id,
+                'target': 'new',
+                'res_id': wiz.id,
+                'context': self.env.context,
+            }
 
-            # Check backorder should check for other barcodes
-            if pick.check_backorder():
-                view = self.env.ref('stock.view_backorder_confirmation')
-                wiz = self.env['stock.backorder.confirmation'].create({'pick_id': pick.id})
-                # TDE FIXME: same reamrk as above actually
-                return {
-                    'name': _('Create Backorder?'),
-                    'type': 'ir.actions.act_window',
-                    'view_type': 'form',
-                    'view_mode': 'form',
-                    'res_model': 'stock.backorder.confirmation',
-                    'views': [(view.id, 'form')],
-                    'view_id': view.id,
-                    'target': 'new',
-                    'res_id': wiz.id,
-                    'context': self.env.context,
-                }
-            for operation in pick.pack_operation_ids:
-                if operation.qty_done < 0:
-                    raise UserError(_('No negative quantities allowed'))
-                if operation.qty_done > 0:
-                    operation.write({'product_qty': operation.qty_done})
-                else:
-                    pack_operations_delete |= operation
-            if pack_operations_delete:
-                pack_operations_delete.unlink()
+        # Check backorder should check for other barcodes
+        if self.check_backorder():
+            view = self.env.ref('stock.view_backorder_confirmation')
+            wiz = self.env['stock.backorder.confirmation'].create({'pick_id': self.id})
+            return {
+                'name': _('Create Backorder?'),
+                'type': 'ir.actions.act_window',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_model': 'stock.backorder.confirmation',
+                'views': [(view.id, 'form')],
+                'view_id': view.id,
+                'target': 'new',
+                'res_id': wiz.id,
+                'context': self.env.context,
+            }
+        for operation in self.pack_operation_ids:
+            if operation.qty_done < 0:
+                raise UserError(_('No negative quantities allowed'))
+            if operation.qty_done > 0:
+                pass
+                #operation.write({'product_qty': operation.qty_done})
+            else:
+                pack_operations_delete |= operation
+        if pack_operations_delete:
+            pack_operations_delete.unlink()
         self.do_transfer()
         return
 
     def check_backorder(self):
+        self.ensure_one()
+        quantity_todo = {}
+        quantity_done = {}
         for move in self.move_lines:
-            if float_compare(move.remaining_qty, 0, precision_rounding=move.product_id.uom_id.rounding) != 0:
-                return True
-        return False
+            quantity_todo.setdefault(move.product_id.id, 0)
+            quantity_done.setdefault(move.product_id.id, 0)
+            quantity_todo[move.product_id.id] += move.product_qty
+            quantity_done[move.product_id.id] += move.quantity_done #TODO: convert to base units
+        for ops in self.pack_operation_ids.filtered(lambda x: x.package_id and not x.product_id and not x.move_id):
+            for quant in ops.package_id.quant_ids:
+                quantity_done.setdefault(quant.product_id.id, 0)
+                quantity_done[quant.product_id.id] += quant.qty
+        for pack in self.pack_operation_ids.filtered(lambda x: x.product_id and not x.move_id):
+            quantity_done.setdefault(pack.product_id.id, 0)
+            quantity_done[pack.product_id.id] += pack.qty_done
+        return any(quantity_done[x] < quantity_todo.get(x, 0) for x in quantity_done)
 
     @api.multi
     def do_transfer(self):
