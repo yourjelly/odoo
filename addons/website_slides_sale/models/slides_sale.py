87c6d342f4184ce801090de14a77555147167285
Mohammed Shekha <msh@openerp.com>
2017-04-17 18:58:48 +0530

    [IMP]website_slides_sale: Added backend views for the models and inherit some of the views for slides and channels

--- addons/website_slides_sale/models/slides_sale.py
+++ addons/website_slides_sale/models/slides_sale.py
@@ -8,7 +8,7 @@ class Course(models.Model):
 	_inherit = ['slide.channel', 'rating.mixin']
 
 	is_course = fields.Boolean("Is Course") # To identify whether it is normal channel or used as a course
-	course_html = fields.Html("Course Details")
+	course_html = fields.Html("Course Details", help="This will be displayed on Course Details page, user can design it.")
 	language = fields.Many2one("res.lang", "Language")
 	instructor_ids = fields.Many2many("res.partner", string="Instructors")
 	product_id = fields.Many2one("product.template", "Product")
@@ -17,8 +17,9 @@ class Course(models.Model):
 class Lecture(models.Model):
 	_inherit = "slide.slide"
 
-	attened_users = fields.Many2many("res.partner", "Attendees")
-	slide_type = fields.Selection([('preview', 'Preview'), ('free', 'Free')], string="Slide Type") # TODO: we can use Download Security field for this
+	attendee_ids = fields.Many2many("res.partner", "Attendees")
+	# TODO: we can use Download Security field for this OR download security is used for security and this gonna be used for showing slide type(icons)
+	slide_type = fields.Selection([('preview', 'Preview'), ('free', 'Free')], string="Slide Type")
 
 
 class Instructor(models.Model):
@@ -35,17 +36,21 @@ class Instructor(models.Model):
 class ProductTemplate(models.Model):
 	_inherit = "product.template"
 
-	is_course = fields.Boolean(string="Course", help='Determine if a product is '
+	is_course = fields.Boolean(string="Course Product", help='Determine if a product is '
       'course product or not.')
 
 
 class CourseRegistration(models.Model):
 	_name = "course.registration"
 
+	def _get_total_viewed_slides(self):
+		return 0
+
 	name = fields.Char()
 	partner_id = fields.Many2one("res.partner", string="Customer")
 	course_id = fields.Many2one("slide.channel", string="Course")
 	course_start_date = fields.Date("Course Start Date")
 	course_end_date = fields.Date("Course End Date")
-	last_view = fields.Many2one("slide.slide", string="Last View")
-	viewed_slides = fields.Integer("Total Viewed Lectures")
+	last_view = fields.Many2one("slide.slide", string="Last View", readonly=True)
+	viewed_slide_ids = fields.Many2many("slide.slide", string="Viewed Lectures")
+	viewed_slides = fields.Integer(compute="_get_total_viewed_slides", string="Total Viewed Lectures")
