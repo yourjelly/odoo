7b3a74bb3ed0b40e76d928fac1152b6a11485d89
jpr-odoo <jpr@openerp.com>
2017-04-27 15:01:09 +0530

    [IMP][REF] payment: add generic payment method template for all acquirers show in front-end       payment: added a new widget for code readability and easy to expand the behaviour of payment method       tokenization: allow to use payment.token to pay eCommerce       allow saving card data from checkout Instead of having to register a card then do a payment, a little checkbox during the checkout process is usually simpler       add a controller for payment method       refactor the code for payment method and save token

--- addons/payment/static/src/js/payment_method.js
+++ addons/payment/static/src/js/payment_method.js
@@ -64,17 +64,7 @@ odoo.define('payment.payment_method', function (require) {
 
         payment_transaction_action: function(acquirer_id, params){
             // override this function as per controllers(route) of module wise
-            if($("#online_invoice_payment").length){
-                var href = $(location).attr("href"),
-                    payment_request_id = href.match(/payment\/([0-9]+)/),
-                    access_token = href.match(/payment\/([^\/?]*)/);
-
-                params.access_token = access_token ? access_token[1] : '';
-                params.payment_request_id = payment_request_id ? payment_request_id[1] : '';
-                ajax.jsonRpc('/payment/transaction/' + acquirer_id, 'call', params).then(function (data) {
-                    $(data).appendTo('body').submit();
-                });
-            }
+            return false;
         },
     });
 
