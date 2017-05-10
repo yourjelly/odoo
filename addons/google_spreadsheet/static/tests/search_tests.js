1672c43e6a1c488d468e30d2328ca1cc931197ff
Pragnesh Mistry <pmi@odoo.com>
2017-05-10 18:11:07 +0530

    [ADD] google_spreadsheet Qunit Test Case

--- addons/google_spreadsheet/static/tests/search_tests.js
+++ addons/google_spreadsheet/static/tests/search_tests.js
@@ -1,7 +1,7 @@
 odoo.define('google_spreadsheet.spreadsheet_test', function (require) {
 "use strict";
 
-var FormView = require('web.ListView');
+var ListView = require('web.ListView');
 var testUtils = require('web.test_utils');
 
 var createAsyncView = testUtils.createAsyncView;
@@ -17,8 +17,14 @@ QUnit.module('google_spreadsheet', {
                 },
                 records: [{
                         id: 1, 
-                        name: "spread sheet 1", 
+                        name: "spread sheet 1",
                         url: "https://docs.google.com/spreadsheets/d/1WwC02z1JgngU8J6M08kZQkMZ3DI_XR7SwF5QSfjxFsE/edit#gid=0", 
+                        description: "Demo spread sheet"
+                    },
+                    {
+                        id: 2,
+                        name: "spread sheet 2",
+                        url: "www.google.com",
                         description: "Demo spread sheet"
                     }]
             },
@@ -26,13 +32,11 @@ QUnit.module('google_spreadsheet', {
     }
 },function () {
     QUnit.module('TreeView');
-
     QUnit.test('google spreadsheets form view test', function (assert) {
         var done = assert.async();
-        assert.expect(1);
-
-        createAsyncView({
-            View:FormView,
+        assert.expect(2);
+        var form_view = createAsyncView({
+            View: ListView,
             model:'google_sheet',
             data:this.data,
             arch:'<tree string="Google Spreadsheets">'+
@@ -40,7 +44,11 @@ QUnit.module('google_spreadsheet', {
                     '<field name="url"/>'+
                  '</tree>',
         }).then(function (form) {
-            assert.strictEqual(form.$('.o_data_row').length, 1,"should contain one url");
+            var re = /google\.com\/spreadsheets*/;
+            var valid_url =  form.$el.find('.o_data_cell')[1].textContent;
+            var invalid_url = form.$el.find('.o_data_cell')[3].textContent;
+            assert.ok(re.test(valid_url),"Should display the correct spreadsheet url");
+            assert.notOk(re.test(invalid_url), "Should not display the correct spreadsheet url");
             form.destroy();
             done();
         });
