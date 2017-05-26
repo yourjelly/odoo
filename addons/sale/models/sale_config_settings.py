eded58c0003540751cdcefad035486224933df79
jpr-odoo <jpr@openerp.com>
2017-05-26 15:54:24 +0530

    [IMP] sale: split sale setting for online quotation and templates

--- addons/sale/models/sale_config_settings.py
+++ addons/sale/models/sale_config_settings.py
@@ -28,7 +28,12 @@ class SaleConfiguration(models.TransientModel):
     module_sale_margin = fields.Boolean("Margins")
     group_sale_layout = fields.Boolean("Sections on Sales Orders", implied_group='sale.group_sale_layout')
     group_warning_sale = fields.Boolean("Warnings", implied_group='sale.group_warning_sale')
-    module_website_quote = fields.Boolean("Online Quotations & Templates")
+    module_website_quote = fields.Boolean("Quotation Templates")
+    sale_quote_pay = fields.Boolean("Online Signature & Payment")
+    quote_mode_type = fields.Selection([
+        ('signature', 'Signature'),
+        ('payment', 'Payment'),
+    ], string="Quotation Signature & Payment", default='signature')
     group_sale_delivery_address = fields.Boolean("Customer Addresses", implied_group='sale.group_delivery_invoice_address')
     multi_sales_price = fields.Boolean("Multiple sales price per product", default_model='sale.config.settings')
     multi_sales_price_method = fields.Selection([
@@ -129,6 +134,11 @@ class SaleConfiguration(models.TransientModel):
                 'group_sale_pricelist': False,
                 'group_pricelist_item': False,
             })
+
+    @api.onchange("module_website_quote")
+    def _onchange_module_website_quote(self):
+        if self.module_website_quote:
+            self.sale_quote_pay = True
 
     def set_default_sale_pricelist_setting(self):
         return self.env['ir.values'].sudo().set_default('sale.config.settings', 'sale_pricelist_setting', self.sale_pricelist_setting)
