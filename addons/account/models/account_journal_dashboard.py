4db98ddefb04d8f779b5a2a4b5c9a2b89f4d6db0
Hetashree Chauhan <hch@odoo.com>
2017-05-26 17:10:35 +0530

    [IMP] account: add bank statements for account dashboard graph view

--- addons/account/models/account_journal_dashboard.py
+++ addons/account/models/account_journal_dashboard.py
@@ -46,58 +46,57 @@ class account_journal(models.Model):
         data = []
         today = datetime.today()
         last_month = today + timedelta(days=-30)
-        bank_stmt = []
         # Query to optimize loading of data for bank statement graphs
-        # Return a list containing the latest bank statement balance per day for the
+        # Return a list containing the latest bank statement lines balance per day for the
         # last 30 days for current journal
-        query = """SELECT a.date, a.balance_end 
-                        FROM account_bank_statement AS a, 
-                            (SELECT c.date, max(c.id) AS stmt_id 
-                                FROM account_bank_statement AS c 
-                                WHERE c.journal_id = %s 
-                                    AND c.date > %s 
-                                    AND c.date <= %s 
-                                    GROUP BY date, id 
-                                    ORDER BY date, id) AS b 
-                        WHERE a.id = b.stmt_id;"""
-
-        self.env.cr.execute(query, (self.id, last_month, today))
-        bank_stmt = self.env.cr.dictfetchall()
-
-        last_bank_stmt = self.env['account.bank.statement'].search([('journal_id', 'in', self.ids),('date', '<=', last_month.strftime(DF))], order="date desc, id desc", limit=1)
-        start_balance = last_bank_stmt and last_bank_stmt[0].balance_end or 0
-
-        locale = self._context.get('lang') or 'en_US'
+        query = """SELECT date, sum(amount) as amount_total
+                        FROM  account_bank_statement_line
+                        WHERE journal_id in (%s)
+                            AND date > %s
+                            AND date <= %s
+                        GROUP BY date
+                        ORDER BY date;"""
+        self.env.cr.execute(query, (','.join(map(str, self.ids)), last_month, today))
+        bank_stmt_lines = self.env.cr.dictfetchall()
+        last_bank_stmt = self.env['account.bank.statement'].search([('journal_id', 'in', self.ids),('date', '<', last_month.strftime(DF))], order="date desc, id desc", limit=1)
+        last_bank_stmt_line = self.env['account.bank.statement.line'].search([('journal_id', 'in', self.ids),('date', '<', last_month.strftime(DF))], order="date desc, id desc")
+        if last_bank_stmt:
+            start_balance = last_bank_stmt.balance_end_real
+        else:
+            start_balance = sum(last_bank_stmt_line.mapped('amount'))
+        locale = self._context.get('lang', 'en_US')
         show_date = last_month
         #get date in locale format
         name = format_date(show_date, 'd LLLL Y', locale=locale)
         short_name = format_date(show_date, 'd MMM', locale=locale)
         data.append({'x':short_name,'y':start_balance, 'name':name})
-
-        for stmt in bank_stmt:
+        for stmt in bank_stmt_lines:
             #fill the gap between last data and the new one
             number_day_to_add = (datetime.strptime(stmt.get('date'), DF) - show_date).days
             last_balance = data[len(data) - 1]['y']
             for day in range(0,number_day_to_add + 1):
-                show_date = show_date + timedelta(days=1)
+                show_date = show_date + timedelta(days=5)
+                stmt_date = datetime.strptime(stmt.get('date'), DF)
+                if stmt_date < show_date:
+                    last_balance = last_balance
+                else:
+                    last_balance = last_balance + stmt.get('amount_total')
                 #get date in locale format
                 name = format_date(show_date, 'd LLLL Y', locale=locale)
                 short_name = format_date(show_date, 'd MMM', locale=locale)
                 data.append({'x': short_name, 'y':last_balance, 'name': name})
             #add new stmt value
-            data[len(data) - 1]['y'] = stmt.get('balance_end')
-
+            data[len(data) - 1]['y'] = last_balance + stmt.get('amount_total')
         #continue the graph if the last statement isn't today
         if show_date != today:
             number_day_to_add = (today - show_date).days
             last_balance = data[len(data) - 1]['y']
             for day in range(0,number_day_to_add):
-                show_date = show_date + timedelta(days=1)
+                show_date = show_date + timedelta(days=5)
                 #get date in locale format
                 name = format_date(show_date, 'd LLLL Y', locale=locale)
                 short_name = format_date(show_date, 'd MMM', locale=locale)
                 data.append({'x': short_name, 'y':last_balance, 'name': name})
-
         [graph_title, graph_key] = self._graph_title_and_key()
         color = '#875A7B' if '+e' in version else '#7c7bad'
         return [{'values': data, 'title': graph_title, 'key': graph_key, 'area': True, 'color': color}]
