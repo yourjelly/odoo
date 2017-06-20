3b60ec7116f557af2a0f45b813edd0b22ea8cf46
Simon Lejeune <sle@openerp.com>
2017-06-20 14:16:52 +0200

    [REF] stock: basics of new model
    We use this commit to reimplement `action_assign` (wiped by the new
    quant implentation). A move is reserved if its move lines exist, so we
    introduce a one2many between stock.move and stock.pack.operation.
    We remove the `pack_operation_pack_ids` and `pack_operation_product_ids`
    as they will be represented by the same model, the `pack_operation_ids`
    with the necessary tweaks in the views (once again, in a further
    commit).
    
    [FIX] fixup result_package_id in in of reservation
    
    [IMP] stock: remove fresh_record
    
    fixup! [ADD] stock: tests for the new stock quant implementation
    
    [REF] stock: stock move validation
    
    We use this commit to implement `action_done` (wiped by the removal of the
    models making the link between stock.move and stock.pack.operations). We
    can use the one2many to the pack operations.
    
    blabla manymany au lieu de one2many
    blabla mto mts
    
    Remove get_ancestors, as orig_ids will be used instead
    
    validation
    
    when we are creating some packe operation with lot name, the
    pack_operation are unlink and a new picking is craeted.
    
    This is caused the function _create_lot_for_picking that unlink pack
    operation having no quantity reserved instead of ones having no quantity
    done.
    
    To fix this we changed product_qty to qty_done and move the code from
    action_done of picking to action_done of move
    
    fixup! [IMP] Merge_moves functionality + many2many procurement and original moves instead of one2many
    
    fixup! [REF] stock: basics of new model and stock.move reservation
    
    edit reserved move line
    
    fixup model
    
    fixup model
    
    [REF] remove dead code in pack operation

--- addons/stock/__manifest__.py
+++ addons/stock/__manifest__.py
@@ -55,7 +55,6 @@
         'views/procurement_views.xml',
         'views/stock_warehouse_views.xml',
         'views/stock_move_views.xml',
-        'views/stock_pack_operation_views.xml',
         'views/product_views.xml',
         'views/stock_config_settings_views.xml',
 
