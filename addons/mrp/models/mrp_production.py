be5e19896e67f314bcff08d9f0ed27579763d5f3
Josse Colpaert <jco@odoo.com>
2017-06-23 16:03:27 +0200

    [IMP] mrp: no action_assign needed anymore after posting inventory

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -507,7 +507,7 @@ class MrpProduction(models.Model):
             order._cal_price(moves_to_do)
             moves_to_finish = order.move_finished_ids.filtered(lambda x: x.state not in ('done','cancel'))
             moves_to_finish.action_done()
-            order.action_assign()
+            #order.action_assign()
         return True
 
     @api.multi
