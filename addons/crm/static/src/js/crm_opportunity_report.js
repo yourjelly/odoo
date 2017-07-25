b455fd2b99b2d1e7c65f0b9d2caec75b66b255f6
dbh <dbh@odoo.com>
2017-07-25 19:10:29 +0530

    [IMP] crm: improvement

--- addons/crm/static/src/js/crm_opportunity_report.js
+++ addons/crm/static/src/js/crm_opportunity_report.js
@@ -46,7 +46,35 @@ var OpportunityReport = Widget.extend(ControlPanelMixin, {
             args: [null, this.start_date, this.end_date, this.stages, this.user_id, this.team_id],
         }).then(function (result) {
             self.data = result;
+            self.render_graph();
             self.renderElement();
+        });
+    },
+
+    render_graph: function () {
+        var total_deals = this.data.lost_deals + this.data.won_deals;
+        var won_percent = this.data.won_deals * 100 / total_deals;
+        var lost_percent = 100 - won_percent
+        var graphData = [won_percent, lost_percent];
+        nv.addGraph(function() {
+            var pieChart = nv.models.pieChart()
+                .x(function(d) { return d; })
+                .y(function(d) { return d; })
+                .showLabels(true)
+                .labelThreshold(0.2)
+                .labelType("percent")
+                .showLegend(false)
+                .margin({ "left": 0, "right": 0, "top": 0, "bottom": 0 })
+                .color(['#00ff00', '#ff0000']);
+        var svg = d3.select(".oe_piechart").append("svg");
+
+        svg
+            .attr("height", "15em")
+            .datum(graphData)
+            .call(pieChart);
+
+        nv.utils.windowResize(pieChart.update);
+        return pieChart;
         });
     },
 })
