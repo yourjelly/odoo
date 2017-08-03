3bf3de6c32db99cd1098dfd43bd288158b5e4c2a
Rohan Patel <rop@odoo.com>
2017-08-03 16:08:53 +0530

    [IMP] web_tour: in 'tip.js' check tip direction right or left while langauge direction is RTL then it will change the position for render tip

--- addons/web_tour/static/src/js/tip.js
+++ addons/web_tour/static/src/js/tip.js
@@ -5,6 +5,8 @@ var config = require('web.config');
 var core = require('web.core');
 var Widget = require('web.Widget');
 
+var _t = core._t;
+
 var Tip = Widget.extend({
     template: "Tip",
     events: {
@@ -41,6 +43,19 @@ var Tip = Widget.extend({
             top: "50%",
             left: "50%",
         };
+
+        // set tip position for langauge direction RTL
+        var isRTL = _t.database.parameters.direction == 'rtl';
+        var position = this.info.position;
+        if (isRTL) {
+            if (this.info.position == "right" ) {
+                position= "left";
+            }
+            if (this.info.position == "left" ) {
+                position = "right";
+            }
+        }
+        this.info.position = position;
     },
     /**
      * @param {jQuery} $anchor the node on which the tip should be placed
