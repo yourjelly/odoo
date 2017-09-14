5840bbbbce166cd7f7c82ad1366657cf31d18754
Priyanka Kakadiya <pka@odoo.com>
2017-09-14 15:46:53 +0530

    [FIX] stock: traceback resolved while click on close button.

--- addons/stock/models/product.py
+++ addons/stock/models/product.py
@@ -370,7 +370,7 @@ class Product(models.Model):
     @api.multi
     def write(self, values):
         res = super(Product, self).write(values)
-        if 'active' in values and not values['active'] and self.mapped('orderpoint_ids').filtered(lambda r: r.active):
+        if 'active' in values and not values['active'] == False and self.mapped('orderpoint_ids').filtered(lambda r: r.active):
             raise UserError(_('You still have some active reordering rules on this product. Please archive or delete them first.'))
         return res
 
