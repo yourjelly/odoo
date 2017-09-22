7544bc14d9fd6cf65f93fb0c74befaa017523274
Pratima Gupta <pgu@odoo.com>
2017-09-22 16:38:44 +0530

    [IMP]stock, mrp: inform the user if he is performing an operation on a product that is not in stock

--- addons/mrp/wizard/stock_scrap_wizard.py
+++ addons/mrp/wizard/stock_scrap_wizard.py
@@ -4,15 +4,12 @@
 from odoo import api, fields, models
 
 
-class StockScrap(models.Model):
-    _inherit = 'stock.scrap'
+class StockScrapWizard(models.TransientModel):
+    _name = 'stock.scrap.wizard'
+    _inherit = 'stock.scrap.wizard'
 
-    production_id = fields.Many2one(
-        'mrp.production', 'Manufacturing Order',
-        states={'done': [('readonly', True)]})
-    workorder_id = fields.Many2one(
-        'mrp.workorder', 'Work Order',
-        states={'done': [('readonly', True)]},
+    production_id = fields.Many2one('mrp.production', 'Manufacturing Order')
+    workorder_id = fields.Many2one('mrp.workorder', 'Work Order',
         help='Not to restrict or prefer quants, but informative.')
 
     @api.onchange('workorder_id')
@@ -25,12 +22,9 @@ class StockScrap(models.Model):
         if self.production_id:
             self.location_id = self.production_id.move_raw_ids.filtered(lambda x: x.state not in ('done', 'cancel')) and self.production_id.location_src_id.id or self.production_id.location_dest_id.id
 
-    def _prepare_move_values(self):
-        vals = super(StockScrap, self)._prepare_move_values()
+    def _prepare_scrap_vals(self):
+        vals = super(StockScrapWizard, self)._prepare_scrap_vals()
         if self.production_id:
             vals['origin'] = vals['origin'] or self.production_id.name
-            if self.product_id in self.production_id.move_finished_ids.mapped('product_id'):
-                vals.update({'production_id': self.production_id.id})
-            else:
-                vals.update({'raw_material_production_id': self.production_id.id})
+            vals.update({'production_id': self.production_id.id})
         return vals
