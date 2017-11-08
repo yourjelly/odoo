bcb56191a56e58dfca7a181cdb6bab8168f3b0ed
Darshan Kalola <dka@odoo.com>
2017-11-08 12:16:36 +0530

    [IMP]point_of_sale: posbox testing.

--- addons/point_of_sale/tools/posbox/posbox_create_image.sh
+++ addons/point_of_sale/tools/posbox/posbox_create_image.sh
@@ -42,14 +42,14 @@ rm -rf "${CLONE_DIR}"
 if [ ! -d $CLONE_DIR ]; then
     echo "Clone Github repo"
     mkdir -p "${CLONE_DIR}"
-    git clone -b 8.0 --no-local --no-checkout --depth 1 https://github.com/odoo/odoo.git "${CLONE_DIR}"
+    git clone -b 11.0-posbox-testing --no-local --no-checkout --depth 1 https://github.com/odoo-dev/odoo.git "${CLONE_DIR}"
     cd "${CLONE_DIR}"
     git config core.sparsecheckout true
     echo "addons/web
 addons/hw_*
 addons/point_of_sale/tools/posbox/configuration
-openerp/
-odoo.py" | tee --append .git/info/sparse-checkout > /dev/null
+odoo/
+odoo-bin" | tee --append .git/info/sparse-checkout > /dev/null
     git read-tree -mu HEAD
 fi
 
