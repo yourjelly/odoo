1e9cd323f45f8eca3b62bdbb69c884dbe4a962c1
Siddharth Gajjar <sga@odoo.com>
2017-11-08 18:06:02 +0530

    [IMP] l10n_uk: migrate code accoding to new api.

--- addons/l10n_uk/migrations/2.0/post-migrate_tags_on_taxes.py
+++ addons/l10n_uk/migrations/2.0/post-migrate_tags_on_taxes.py
@@ -1,6 +1,6 @@
-from openerp.modules.registry import RegistryManager
+import odoo
 
 def migrate(cr, version):
-    registry = RegistryManager.get(cr.dbname)
-    from openerp.addons.account.models.chart_template import migrate_tags_on_taxes
+    registry = odoo.registry(cr.dbname)
+    from odoo.addons.account.models.chart_template import migrate_tags_on_taxes
     migrate_tags_on_taxes(cr, registry)
