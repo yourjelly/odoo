be959254c02b04ff092228935663e96f167ed2f1
Ankit Sathvara <asa@odoo.com>
2017-11-09 18:03:58 +0530

    [REM] project: remove merge selected task from project.task -> action

--- addons/project/__manifest__.py
+++ addons/project/__manifest__.py
@@ -30,7 +30,6 @@
         'views/project_templates.xml',
         'views/project_portal_templates.xml',
         'data/project_mail_template_data.xml',
-        'wizard/project_task_merge_wizard_views.xml',
     ],
     'qweb': ['static/src/xml/project.xml'],
     'demo': ['data/project_demo.xml'],
