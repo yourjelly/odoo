1140768ab1c62de1963b6f701c5e4d3e5f388764
Pankaj Kamani <pam@odoo.com>
2017-11-24 18:14:07 +0530

    [ADD] Mail:Layout Template Configs Added in General Settings

--- addons/mail/models/res_config_settings.py
+++ addons/mail/models/res_config_settings.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import datetime
+from ast import literal_eval
 
 from werkzeug import urls
 
@@ -16,30 +17,46 @@ class ResConfigSettings(models.TransientModel):
     fail_counter = fields.Integer('Fail Mail', readonly=True)
     alias_domain = fields.Char('Alias Domain', help="If you have setup a catch-all email domain redirected to "
                                "the Odoo server, enter the domain name here.")
-
+    email_template_layout = fields.Many2one('ir.ui.view', string='Email Template Layout', domain=[('key', 'ilike', '_email_layout')])
+    
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
 
         previous_date = datetime.datetime.now() - datetime.timedelta(days=30)
+        IrConfigParameter = self.env['ir.config_parameter']
 
-        alias_domain = self.env["ir.config_parameter"].get_param("mail.catchall.domain", default=None)
+        alias_domain = IrConfigParameter.get_param("mail.catchall.domain", default=None)
         if alias_domain is None:
-            domain = self.env["ir.config_parameter"].get_param("web.base.url")
+            domain = IrConfigParameter.get_param("web.base.url")
             try:
                 alias_domain = urls.url_parse(domain).host
             except Exception:
                 pass
-
+        email_template_layout = literal_eval(IrConfigParameter.sudo().get_param('email_template_layout', 'False'))
         res.update(
             fail_counter=self.env['mail.mail'].sudo().search_count([
                 ('date', '>=', previous_date.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)),
                 ('state', '=', 'exception')]),
             alias_domain=alias_domain or False,
+            email_template_layout=email_template_layout,
+
         )
 
         return res
 
     def set_values(self):
         super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].set_param("mail.catchall.domain", self.alias_domain or '')
+        IrConfigParameter = self.env['ir.config_parameter']
+        IrConfigParameter.set_param("mail.catchall.domain", self.alias_domain or '')
+        IrConfigParameter.sudo().set_param('email_template_layout', self.email_template_layout.id or False)
+
+    def edit_email_template_layout(self):
+        template_id = self.email_template_layout.id
+        return {
+           'type': 'ir.actions.act_window',
+           'res_model': 'ir.ui.view',
+           'view_type': 'form',
+           'view_mode': 'form',
+           'res_id': template_id,
+        }
