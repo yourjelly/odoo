51cea65f0db82c2c0b76e58dd80a3f3ca1689348
Tejas Shahu <tsh@odoo.com>
2017-11-27 19:18:36 +0530

    [IMP] Accounting: Changes as per Improvement in Reconciliation tab

--- addons/account/models/account_bank_statement.py
+++ addons/account/models/account_bank_statement.py
@@ -575,6 +575,9 @@ class AccountBankStatementLine(models.Model):
             # TODO : find out what use case this permits (match a check payment, registered on a journal whose account type is other instead of liquidity)
             domain_matching = expression.AND([domain_matching, [('account_id.reconcile', '=', True)]])
 
+        if self.company_id.account_opening_date:
+            domain = [('date_maturity', '>=', self.company_id.account_opening_date)]
+
         # Let's add what applies to both
         domain = expression.OR([domain_reconciliation, domain_matching])
         if partner_id and not overlook_partner:
