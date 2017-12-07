b3e77ad2555bd543ec4c45fa163bafb0e0fa1bff
Parth Patel <pat@odoo.com>
2017-12-07 18:59:59 +0530

    [IMP]Commit-3

--- addons/web/static/src/js/views/kanban/kanban_controller.js
+++ addons/web/static/src/js/views/kanban/kanban_controller.js
@@ -297,6 +297,7 @@ var KanbanController = BasicController.extend({
             buttons: false,
             $content: QWeb.render('KanbanColumn.KananGuideDialog', {data: this.data}),
         }).open();
+        debugger;
     },
     /**
      * Loads the record of a given column (used in mobile, as the columns are
