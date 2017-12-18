a0847dc03f02ad3f9181454fcf2b32168ecdde7d
Siddharth Gajjar <sga@odoo.com>
2017-12-18 18:59:15 +0530

    [ADD] account_reports: convert report query in ORM.

--- addons/account/report/account_aged_partner_balance.py
+++ addons/account/report/account_aged_partner_balance.py
@@ -15,6 +15,7 @@ class ReportAgedPartnerBalance(models.AbstractModel):
     def _get_partner_move_lines(self, account_type, date_from, target_move, period_length):
         periods = {}
         start = datetime.strptime(date_from, "%Y-%m-%d")
+        domain = []
         for i in range(5)[::-1]:
             stop = start - relativedelta(days=period_length)
             periods[str(i)] = {
@@ -31,56 +32,30 @@ class ReportAgedPartnerBalance(models.AbstractModel):
         move_state = ['draft', 'posted']
         if target_move == 'posted':
             move_state = ['posted']
-        arg_list = (tuple(move_state), tuple(account_type))
         #build the reconciliation clause to see what partner needs to be printed
-        reconciliation_clause = '(l.reconciled IS FALSE)'
+        domain += [('reconciled', '=', False), ('move_id.state', 'in', move_state), ('account_id.internal_type', 'in', account_type), ('date', '<=', date_from), ('company_id', '=', user_company)]
         cr.execute('SELECT debit_move_id, credit_move_id FROM account_partial_reconcile where create_date > %s', (date_from,))
         reconciled_after_date = []
         for row in cr.fetchall():
             reconciled_after_date += [row[0], row[1]]
         if reconciled_after_date:
-            reconciliation_clause = '(l.reconciled IS FALSE OR l.id IN %s)'
-            arg_list += (tuple(reconciled_after_date),)
-        arg_list += (date_from, user_company)
-        query = '''
-            SELECT DISTINCT l.partner_id, UPPER(res_partner.name)
-            FROM account_move_line AS l left join res_partner on l.partner_id = res_partner.id, account_account, account_move am
-            WHERE (l.account_id = account_account.id)
-                AND (l.move_id = am.id)
-                AND (am.state IN %s)
-                AND (account_account.internal_type IN %s)
-                AND ''' + reconciliation_clause + '''
-                AND (l.date <= %s)
-                AND l.company_id = %s
-            ORDER BY UPPER(res_partner.name)'''
-        cr.execute(query, arg_list)
-
-        partners = cr.dictfetchall()
+            domain += ['|', ('reconciled', '=', False), ('id', 'in', reconciled_after_date)]
+        partner_result = self.env['account.move.line'].read_group(domain, ['partner_id'], ['partner_id'])
+        new_partners = [{'partner_id': partner['partner_id'][0]} for partner in partner_result]
         # put a total of 0
         for i in range(7):
             total.append(0)
 
         # Build a string like (1,2,3) for easy use in SQL query
-        partner_ids = [partner['partner_id'] for partner in partners if partner['partner_id']]
-        lines = dict((partner['partner_id'] or False, []) for partner in partners)
+        partner_ids = [partner['partner_id'] for partner in new_partners if partner['partner_id']]
+        lines = dict((partner['partner_id'] or False, []) for partner in new_partners)
         if not partner_ids:
             return [], [], []
 
         # This dictionary will store the not due amount of all partners
         undue_amounts = {}
-        query = '''SELECT l.id
-                FROM account_move_line AS l, account_account, account_move am
-                WHERE (l.account_id = account_account.id) AND (l.move_id = am.id)
-                    AND (am.state IN %s)
-                    AND (account_account.internal_type IN %s)
-                    AND (COALESCE(l.date_maturity,l.date) > %s)\
-                    AND ((l.partner_id IN %s) OR (l.partner_id IS NULL))
-                AND (l.date <= %s)
-                AND l.company_id = %s'''
-        cr.execute(query, (tuple(move_state), tuple(account_type), date_from, tuple(partner_ids), date_from, user_company))
-        aml_ids = cr.fetchall()
-        aml_ids = aml_ids and [x[0] for x in aml_ids] or []
-        for line in self.env['account.move.line'].browse(aml_ids):
+        am_domain = [('move_id.state', 'in', move_state), ('account_id.internal_type', 'in', account_type), '|', ('date_maturity', '>', date_from), ('date', '>', date_from), '|', ('partner_id', 'in', partner_ids), ('partner_id', '=', False), ('date', '<=', date_from), ('company_id', '=', user_company)]
+        for line in self.env['account.move.line'].search(am_domain):
             partner_id = line.partner_id.id or False
             if partner_id not in undue_amounts:
                 undue_amounts[partner_id] = 0.0
@@ -155,7 +130,7 @@ class ReportAgedPartnerBalance(models.AbstractModel):
                         })
             history.append(partners_amount)
 
-        for partner in partners:
+        for partner in new_partners:
             if partner['partner_id'] is None:
                 partner['partner_id'] = False
             at_least_one_amount = False
