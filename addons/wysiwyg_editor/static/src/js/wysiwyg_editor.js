44b7c82d4ebb4b5894710fdd4aafe1fafefb6531
Sanjay Jamod <sja@odoo.com>
2017-12-22 17:42:20 +0530

    [IMP] wysiwyg_editor: Clear editor on first time edit(focus)

--- addons/wysiwyg_editor/static/src/js/wysiwyg_editor.js
+++ addons/wysiwyg_editor/static/src/js/wysiwyg_editor.js
@@ -12,12 +12,14 @@ var QWeb = core.qweb;
 var MyHtmlEditor = basic_fields.DebouncedField.extend(TranslatableFieldMixin, {
     className: 'oe_form_field oe_form_field_html_text',
     supportedFieldTypes: ['html'],
+    events: {click: '_onFocus'},
     xmlDependencies: ['/wysiwyg_editor/static/src/xml/backend.xml'],
 
     /**
      * @override
      */
     start: function () {
+        $(QWeb.render('MyHtmlEditor')).appendTo(this.$el);
         return this._super.apply(this, arguments);
     },
 
@@ -30,7 +32,9 @@ var MyHtmlEditor = basic_fields.DebouncedField.extend(TranslatableFieldMixin, {
      * @private
      */
     _renderEdit: function () {
-        $(QWeb.render('MyHtmlEditor')).appendTo(this.$el);
+        if (!this.$el.find('#my_editor').html().trim() === 'Write here...' || this.value) {
+            this.$el.find('#my_editor').html(this.value);
+        }
         var colorPalette = ['000000', 'FF9966', '6699FF', '99FF66', 'CC0000', '00CC00', '0000CC', '333333', '0066FF', 'FFFFFF'];
         var forePalette = this.$('.fore-palette');
         var backPalette = this.$('.back-palette');
@@ -45,16 +49,16 @@ var MyHtmlEditor = basic_fields.DebouncedField.extend(TranslatableFieldMixin, {
             e.stopPropagation();
             var command = $(this).data('command');
             if (command == 'h1' || command == 'h2' || command == 'p') {
-              document.execCommand('formatBlock', false, command);
+                document.execCommand('formatBlock', false, command);
             }
             if (command == 'forecolor' || command == 'backcolor') {
-              document.execCommand($(this).data('command'), false, $(this).data('value'));
+                document.execCommand($(this).data('command'), false, $(this).data('value'));
             }
             if (command == 'createlink' || command == 'insertimage') {
-              var url = prompt('Enter the link here: ', 'http:\/\/');
-              document.execCommand($(this).data('command'), false, url);
+                var url = prompt('Enter the link here: ', 'http:\/\/');
+                document.execCommand($(this).data('command'), false, url);
             } else {
-              document.execCommand($(this).data('command'), false, null);
+                document.execCommand($(this).data('command'), false, null);
             }
         });
     },
@@ -72,6 +76,12 @@ var MyHtmlEditor = basic_fields.DebouncedField.extend(TranslatableFieldMixin, {
 
     _getValue: function () {
         return this.$el.find('#my_editor').html();
+    },
+
+    _onFocus: function () {
+        if (this.$el.find('#my_editor').html().trim() === 'Write here...') {
+            this.$el.find('#my_editor').html('')
+        }
     },
 });
 
