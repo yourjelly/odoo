2b811861920051d31f359c8f94afbc41f8bb0c75
jpr-odoo <jpr@openerp.com>
2018-01-12 17:02:45 +0530

    [WIP] portal: update code

--- addons/portal/static/src/js/portal_sidebar.js
+++ addons/portal/static/src/js/portal_sidebar.js
@@ -1,36 +1,35 @@
-odoo.define('portal.o_portal_sidebar', function(require) {
-    'use strict';
+odoo.define('portal.PortalSidebar', function (require) {
+"use strict";
 
-    require('web.dom_ready');
-    var config = require('web.config');
+require('web.dom_ready');
+var config = require('web.config');
 
-    if (!$('.o_portal_sidebar').length) {
-        return $.Deferred().reject("DOM doesn't contain '.o_portal_sidebar'");
-    }
+if (!$('.o_portal_sidebar').length) {
+    return $.Deferred().reject("DOM doesn't contain '.o_portal_sidebar'");
+}
 
-    var $bs_sidebar = $(".o_portal_sidebar .bs-sidebar");
-    $(window).on('resize', _.throttle(adapt_sidebar_position, 200, {
-        leading: false
-    }));
-    adapt_sidebar_position();
+var $bsSidebar = $(".o_portal_sidebar .bs-sidebar");
+$(window).on('resize', _.throttle(adaptSidebarPosition, 200, {leading: false}));
 
-    function adapt_sidebar_position() {
-        $bs_sidebar.css({
-            position: "relative",
-            width: "",
-        });
-        if (config.device.size_class >= config.device.SIZES.MD) {
-            $bs_sidebar.css({
-                position: "fixed",
-                width: $bs_sidebar.outerWidth(),
-            });
-        }
-    }
+adaptSidebarPosition();
 
-    $bs_sidebar.affix({
-        offset: {
-            top: 0,
-            bottom: $('body').height() - $('#wrapwrap').outerHeight() + $("footer").outerHeight(),
-        },
+function adaptSidebarPosition() {
+    $bsSidebar.css({
+        position: "",
+        width: "",
     });
+    if (config.device.size_class >= config.device.SIZES.MD) {
+        $bsSidebar.css({
+            position: "fixed",
+            width: $bsSidebar.outerWidth(),
+        });
+    }
+}
+
+$bsSidebar.affix({
+    offset: {
+        top: 0,
+        bottom: $('body').height() - $('#wrapwrap').outerHeight() + $("footer").outerHeight(),
+    },
+});
 });
