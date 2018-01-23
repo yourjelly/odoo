fcc11fceaf4888e0ce80d6adfc424147f3be5e45
Jaykishan Solanki <jao@odoo.com>
2018-01-23 18:25:41 +0530

    [IMP] l10n_ro : remove account_chart_template_date.yml file

--- addons/l10n_ro/__manifest__.py
+++ addons/l10n_ro/__manifest__.py
@@ -31,7 +31,6 @@ Romanian accounting chart and localization.
              'data/account_tax_template_data.xml',
              'data/account_fiscal_position_data.xml',
              'data/account_reconcile_model_template.xml',
-             'data/account_chart_template_data.yml',
              'data/res.country.state.csv',
              'data/res.bank.csv',
              ],
