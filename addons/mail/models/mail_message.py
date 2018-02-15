yourjelly/odoo

commit 666b59484201004e2cd8890a9c09b6dcf023a725

[FIX] mail: send image with base64 src as attachment
Before this fix, the base64 image are remove and never send. The images
were in the body increasing the size of the database unnecessarily
(cfr some odoo users with the chatter)

NB: According to the prophecy Khoi will come and dominate the world,
ask him anything

addons/mail/models/mail_message.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import logging
+import re
 
 from email.utils import formataddr
 
@@ -11,6 +12,7 @@ from odoo.osv import expression
 
 
 _logger = logging.getLogger(__name__)
+_image_dataurl = re.compile(r'(data:image/[a-z]+?);base64,([a-z0-9+/]{3,}=*)([\'"])', re.I)
 
 
 class Message(models.Model):
@@ -720,6 +722,28 @@ class Message(models.Model):
             values['reply_to'] = self._get_reply_to(values)
         if 'record_name' not in values and 'default_record_name' not in self.env.context:
             values['record_name'] = self._get_record_name(values)
+
+        if 'attachment_ids' not in values:
+            values.setdefault('attachment_ids', [])
+
+        # extract base64 images
+        if 'body' in values:
+            Attachments = self.env['ir.attachment']
+            data_to_url = {}
+            def base64_to_boundary(match):
+                key = match.group(2)
+                if not data_to_url.get(key):
+                    name = 'image%s' % len(data_to_url)
+                    attachment = Attachments.create({
+                        'name': name,
+                        'datas': match.group(2),
+                        'datas_fname': name,
+                        'res_model': 'mail.message',
+                    })
+                    values['attachment_ids'].append((4, attachment.id))
+                    data_to_url[key] = '/web/image/%s' % attachment.id
+                return '%s%s alt="%s"' % (data_to_url[key], match.group(3), name)
+            values['body'] = _image_dataurl.sub(base64_to_boundary, values['body'])
 
         message = super(Message, self).create(values)
         message._invalidate_documents()

commit e2a29b0bbeb85081c7c006e2881f876bc6622591

[FIX] mail: correct errors in b64 images handling
Rev 666b59484201004e2cd8890a9c09b6dcf023a725 introduced an autodetection
mechanism for transforming base64 embedded images inside emails into
attachment with a Content-ID URL.

The patch suffered from several issues:

- the base64 detection regex failed to detect base64 data that is folded
  over multiple lines - which happens to be the default in many Python
  versions when a binary field is read, or when using the base64 codec
  to encode a string.
  (Those cases where left untouched and the image was therefore missing)
- there was a logic error in the code when the same image was present
  multiple times, leading to an exception (`name` variable was
  referenced before being assigned)

This patch addresses both issues.

addons/mail/models/mail_message.py
@@ -12,7 +12,7 @@ from odoo.osv import expression
 
 
 _logger = logging.getLogger(__name__)
-_image_dataurl = re.compile(r'(data:image/[a-z]+?);base64,([a-z0-9+/]{3,}=*)([\'"])', re.I)
+_image_dataurl = re.compile(r'(data:image/[a-z]+?);base64,([a-z0-9+/\n]{3,}=*)\n*([\'"])', re.I)
 
 
 class Message(models.Model):
@@ -757,8 +757,9 @@ class Message(models.Model):
                         'res_model': 'mail.message',
                     })
                     values['attachment_ids'].append((4, attachment.id))
-                    data_to_url[key] = '/web/image/%s' % attachment.id
-                return '%s%s alt="%s"' % (data_to_url[key], match.group(3), name)
+                    data_to_url[key] = (name, '/web/image/%s' % attachment.id)
+                alt, url = data_to_url[key]
+                return '%s%s alt="%s"' % (url, match.group(3), alt)
             values['body'] = _image_dataurl.sub(base64_to_boundary, values['body'])
 
         # delegate creation of tracking after the create as sudo to avoid access rights issues