d8e4188fc09a62032ef94740ee93d1f2d2edf989
Kinjal Mehta <kme@odoo.com>
2018-02-22 18:58:27 +0530

    [ADD] account_analytic_default : In the analytic default, add the possibility to set up an analytic account by default if a specific account is used.

--- addons/account_analytic_default/models/account_payment.py
+++ addons/account_analytic_default/models/account_payment.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class AccountPayment(models.Model):
@@ -9,6 +9,14 @@ class AccountPayment(models.Model):
 
     def _get_counterpart_move_line_vals(self, invoice=False):
         res = super(AccountPayment, self)._get_counterpart_move_line_vals(invoice=False)
+        default_analytic_account = self.env['account.analytic.default'].account_get(res['account_id'], self.partner_id.id, self._uid, fields.Date.today())
+        res.update({
+                    'analytic_account_id': default_analytic_account.analytic_id.id,
+                })
+        return res
+
+    def _get_liquidity_move_line_vals(self, amount):
+        res = super(AccountPayment, self)._get_liquidity_move_line_vals(amount)
         default_analytic_account = self.env['account.analytic.default'].account_get(res['account_id'], self.partner_id.id, self._uid, fields.Date.today())
         res.update({
                     'analytic_account_id': default_analytic_account.analytic_id.id,
