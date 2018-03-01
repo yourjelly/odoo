599c3c7359d983997d0bca516a1549a0386afdec
Kinjal Mehta <kme@odoo.com>
2018-03-01 18:32:08 +0530

    [ADD] account_analytic_default : In the analytic default, add the possibility to set up an analytic account by default if a specific account is used.

--- addons/account_analytic_default/models/pos_order.py
+++ addons/account_analytic_default/models/pos_order.py
@@ -8,7 +8,6 @@ class PosOrder(models.Model):
     _inherit = "pos.order"
 
     def _prepare_analytic_account(self, line):
-        '''This method is designed to be inherited in a custom module'''
         for l in self:
-            default_analytic_account = self.env['account.analytic.default'].account_get(17, line.product_id.id, l.partner_id.id, l.user_id.id, fields.Date.today())
+            default_analytic_account = self.env['account.analytic.default'].account_get(self.sale_journal.default_debit_account_id.id, line.product_id.id, l.partner_id.id, l.user_id.id, fields.Date.today())
         return default_analytic_account.analytic_id.id
