0a200d478c2ab5299693670c300cec47480fe10b
qsm-odoo <qsm@odoo.com>
2018-03-14 14:51:31 +0100

    [IMP] base, web: update bootstrap 3.3.7 to 4.1.1
    Simply updating the lib, the assets bundle content and the assets bundle
    compiler.

--- odoo/addons/base/models/assetsbundle.py
+++ odoo/addons/base/models/assetsbundle.py
@@ -675,9 +675,7 @@ class ScssStylesheetAsset(PreprocessedCSS):
     @property
     def bootstrap_path(self):
         return get_resource_path('web', 'static', 'lib', 'bootstrap', 'scss')
-    @property
-    def bootstrap_components_path(self):
-        return get_resource_path('web', 'static', 'lib', 'bootstrap', 'scss', 'bootstrap')
+
     precision = 8
     output_style = 'expanded'
 
@@ -690,7 +688,6 @@ class ScssStylesheetAsset(PreprocessedCSS):
                 string=source,
                 include_paths=[
                     self.bootstrap_path,
-                    self.bootstrap_components_path,
                 ],
                 output_style=self.output_style,
                 precision=self.precision,
@@ -703,7 +700,7 @@ class ScssStylesheetAsset(PreprocessedCSS):
             sassc = misc.find_in_path('sassc')
         except IOError:
             sassc = 'sassc'
-        return [sassc, '--stdin', '--precision', str(self.precision), '--load-path', self.bootstrap_path, '--load-path', self.bootstrap_components_path, '-t', self.output_style]
+        return [sassc, '--stdin', '--precision', str(self.precision), '--load-path', self.bootstrap_path, '-t', self.output_style]
 
 
 class LessStylesheetAsset(PreprocessedCSS):
