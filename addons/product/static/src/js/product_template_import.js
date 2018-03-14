901438b8347b832020246ea7b3cd52df963a6a23
Parth Patel <pat@odoo.com>
2018-03-14 16:41:58 +0530

    [ADD]product: [ADD-Template] product.vendor pricelist

--- addons/product/static/src/js/product_template_import.js
+++ addons/product/static/src/js/product_template_import.js
@@ -13,6 +13,10 @@ BaseImport.DataImport.include({
             this.$(".import-link").prop({"text": _t(" Import Template for Products"), "href": "/product/static/csv/product.template.csv"});
             this.$(".template-import").removeClass("hidden");
         }
+        if (this.res_model == 'product.supplierinfo') {
+            this.$(".import-link").prop({"text": _t(" Import Template for Vendor Pricelists"), "href": "/product/static/csv/product.vendor.pricelist.template.xlsx"});
+            this.$(".template-import").removeClass("hidden");
+        }
     },   
 });
 
