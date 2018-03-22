0f7696aa9bc7b6c2e4907950423056005481de19
Mohammed Shekha <msh@openerp.com>
2018-03-22 16:16:42 +0530

    [IMP] web: Consider css StylesheetAsset class while check is preprocesed or not if direction is rtl as we store it in ir.attachment

--- odoo/addons/base/models/assetsbundle.py
+++ odoo/addons/base/models/assetsbundle.py
@@ -18,6 +18,7 @@ from subprocess import Popen, PIPE
 from collections import OrderedDict
 from odoo import fields, tools
 from odoo.tools.pycompat import string_types, to_text
+from odoo.exceptions import MissingError
 from odoo.http import request
 from odoo.modules.module import get_resource_path
 from .qweb import escape
@@ -367,7 +368,11 @@ class AssetsBundle(object):
         attachments = None
         # TODO: MSH: If direction is rtl then also unlink css assets as we stored css for rtl direction
 
-        for atype in (SassStylesheetAsset, ScssStylesheetAsset, LessStylesheetAsset):
+        asset_types = [SassStylesheetAsset, ScssStylesheetAsset, LessStylesheetAsset]
+        if self.user_direction == 'rtl':
+            asset_types.append(StylesheetAsset)
+
+        for atype in asset_types:
             outdated = False
             assets = dict((asset.html_url, asset) for asset in self.stylesheets if isinstance(asset, atype))
             if assets:
