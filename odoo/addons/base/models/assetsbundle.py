58d626024c05c504bb6d55cf322183f3ef9623fa
Mohammed Shekha <msh@openerp.com>
2018-03-22 16:16:42 +0530

    [IMP] web: Consider css StylesheetAsset class while check is preprocesed or not if direction is rtl as we store it in ir.attachment

--- odoo/addons/base/models/assetsbundle.py
+++ odoo/addons/base/models/assetsbundle.py
@@ -16,6 +16,7 @@ except ImportError:
 from datetime import datetime
 from subprocess import Popen, PIPE
 from odoo import fields, tools
+from odoo.exceptions import MissingError
 from odoo.http import request
 from odoo.modules.module import get_resource_path
 import psycopg2
@@ -336,7 +337,11 @@ class AssetsBundle(object):
         attachments = None
         # TODO: MSH: If direction is rtl then also unlink css assets as we stored css for rtl direction
 
-        for atype in (SassStylesheetAsset, ScssStylesheetAsset, LessStylesheetAsset):
+        asset_types = [SassStylesheetAsset, ScssStylesheetAsset, LessStylesheetAsset]
+        if self.user_direction == 'rtl':
+            asset_types.append(StylesheetAsset)
+
+        for atype in asset_types:
             outdated = False
             assets = dict((asset.html_url, asset) for asset in self.stylesheets if isinstance(asset, atype))
             if assets:
@@ -418,6 +423,12 @@ class AssetsBundle(object):
             # source = '\n'.join([asset.content for asset in css_assets])
             for asset in css_assets:
                 asset._content = self.run_rtlcss(asset.content)
+                if not self.css_errors and old_attachments:
+                    try: # TODO: MSH: We need to unlink old_attachments in try, except, maybe previous loop unlink all attachments, we should merge this code as it creates many issues
+                        old_attachments.unlink()
+                    except MissingError:
+                        pass
+
                 if self.user_direction == 'rtl' and debug:
                     try:
                         fname = os.path.basename(asset.url)
