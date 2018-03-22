fb3445f33c23d93e956c060244125550e2ed9443
chirag <chv@odoo.com>
2018-03-22 14:45:47 +0530

    [IMP] base: test_inherit and test_inherits has been merged into test_base.
    ORM related tests needed to be merged into a new module 'test_base'.
    
    Hence, test_inherit and test_inherits are merged into test_base.

---
+++ odoo/addons/test_base/__menifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'test-base',
+    'version': '0.1',
+    'category': 'Tests',
+    'description': """A module to verify the inherit and inherits""",
+    'depends': ['base'],
+    'data': [
+        'data/ir.model.access.csv',
+    ],
+    'demo': [
+        'data/inherit_demo.xml',
+        'data/inherits_demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ odoo/addons/test_base/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import inherit
+from . import inherits
