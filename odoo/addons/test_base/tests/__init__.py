674a5f633854d0f129ba93e6ef940b2c062af46c
chirag <chv@odoo.com>
2018-03-28 17:30:11 +0530

    [IMP] base: moving more ORM related test cases

--- odoo/addons/test_base/tests/__init__.py
+++ odoo/addons/test_base/tests/__init__.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
 
+from . import test_acl
+from . import test_api
+from . import test_base
+from . import test_basecase
+from . import test_expression
 from . import test_inherit
 from . import test_inherits
+from . import test_ir_default
+from . import test_menu
+from . import test_misc
+from . import test_orm
+from . import test_ormcache
+from . import test_res_partner_bank
+from . import test_search
+from . import test_uninstall
+from . import test_user_has_group
