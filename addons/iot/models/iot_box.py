d3a6c05ebb4843f7c1e59827a92250d795910a4f
Josse Colpaert <jco@odoo.com>
2018-04-12 15:53:06 +0200

    [ADD] iot: model/views + routes for receiving boxes, devices and measurements

---
+++ addons/iot/models/iot_box.py
@@ -0,0 +1,12 @@
+from odoo import api, fields, models, exceptions
+
+
+# ----------------------------------------------------------
+# Models for client
+# ----------------------------------------------------------
+class IotBox(models.Model):
+    _name = 'iot.box'
+
+    name = fields.Char('Name')
+    identifier = fields.Char(string='Serial Number')
+    device_ids = fields.One2many('iot.device', 'iot_id', string="devices")
