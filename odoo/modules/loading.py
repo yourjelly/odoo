312be2f94c0499812854e79e779f7b4a21625b1f
Xavier Morel <xmo@odoo.com>
2018-04-23 13:56:55 +0200

    [IMP] move selection of modules outside load_marked_modules
    And rename it since it doesn't load *marked* modules anymore.

--- odoo/modules/loading.py
+++ odoo/modules/loading.py
@@ -286,17 +286,13 @@ def _check_module_names(cr, module_names):
             incorrect_names = mod_names.difference([x['name'] for x in cr.dictfetchall()])
             _logger.warning('invalid module names, ignored: %s', ", ".join(incorrect_names))
 
-def load_marked_modules(cr, graph, states, force, report, perform_checks, models_to_check=None):
-    """Loads modules marked with ``states``, adding them to ``graph`` and
-       ``loaded_modules`` and returns a list of installed/upgraded modules."""
+
+def load_listed_modules(cr, graph, module_list, force, report, perform_checks, models_to_check=None):
+    if not module_list:
+        return
 
     if models_to_check is None:
         models_to_check = set()
-
-    cr.execute("SELECT name from ir_module_module WHERE state IN %s" ,(tuple(states),))
-    module_list = [name for (name,) in cr.fetchall() if name not in graph]
-    if not module_list:
-        return
 
     # dict_keys are a live view, if the graph is updated the view also is ->
     # we need to copy the keys in order to get a snapshot of the graph before
@@ -406,16 +402,23 @@ def load_modules(db, force_demo=False, update_module=False):
         processed_modules = []
         loaded = -1
         while loaded < len(graph):
-            cr.execute("select name from ir_module_module where state in ('to install', 'to upgrade')")
-            processed_modules.extend(n for [n] in cr.fetchall())
-
             loaded = len(graph)
-            load_marked_modules(cr, graph,
-                ['installed', 'to upgrade', 'to remove'],
-                force, report, update_module, models_to_check)
+
+            cr.execute("SELECT name, state = 'to upgrade' FROM ir_module_module WHERE state in ('installed', 'to upgrade', 'to remove')")
+            mods = cr.fetchall()
+            processed_modules.extend(name for name, to_upgrade in mods if to_upgrade)
+            load_listed_modules(
+                cr, graph, [name for name, _ in mods], force, report, update_module,
+                models_to_check
+            )
+
             if update_module:
-                load_marked_modules(cr, graph,
-                    ['to install'], force, report, update_module, models_to_check)
+                cr.execute("SELECT name FROM ir_module_module WHERE state = 'to install'")
+                mods = [n for [n] in cr.fetchall()]
+                processed_modules.extend(mods)
+                load_listed_modules(
+                    cr, graph, mods, force, report, update_module, models_to_check
+                )
 
         registry.loaded = True
         registry.setup_models(cr)
