369a83f41b319347eb900a7bb8d50f3ad1bcae7f
Hiral Bhavsar <hbh@odoo.com>
2018-04-25 18:26:39 +0530

    [CLEAN-FIXME] account: Removed ondelete restrict on 'move_line_ids' field
    Removed ondelete restrict on 'move_line_ids' field as odoo doesn't support ondelete on One2many field.
    Not added ondelete on its Many2one because 'account.payment' model has override unlink function to restrict it with a specific message.
    
    Related to Task: 58786

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -340,8 +340,7 @@ class account_payment(models.Model):
     reconciled_invoice_ids = fields.Many2many('account.invoice', string='Reconciled Invoices', compute='_compute_reconciled_invoice_ids', help="Invoices whose journal items have been reconciled with this payment's.")
     has_invoices = fields.Boolean(compute="_compute_reconciled_invoice_ids", help="Technical field used for usability purposes")
 
-    # FIXME: ondelete='restrict' not working (eg. cancel a bank statement reconciliation with a payment)
-    move_line_ids = fields.One2many('account.move.line', 'payment_id', readonly=True, copy=False, ondelete='restrict')
+    move_line_ids = fields.One2many('account.move.line', 'payment_id', readonly=True, copy=False)
     move_reconciled = fields.Boolean(compute="_get_move_reconciled", readonly=True)
 
     def open_payment_matching_screen(self):
