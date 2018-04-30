96d11e2c154a7c99edabec45da9288b75a2e263a
Jigar Vaghela <jva@odoo.com>
2018-04-30 12:51:16 +0530

    [IMP] test_base: Remove business model use from "test_basecase.py".

--- odoo/addons/test_base/tests/__init__.py
+++ odoo/addons/test_base/tests/__init__.py
@@ -4,3 +4,4 @@
 from . import common
 from . import test_acl
 from . import test_api
+from . import test_basecase
--- odoo/addons/test_base/tests/test_basecase.py
+++ odoo/addons/test_base/tests/test_basecase.py
@@ -10,33 +10,33 @@ class TestSingleTransactionCase(common.SingleTransactionCase):
     """
 
     def test_00(self):
-        """ Create a partner. """
-        self.env['res.partner'].create({'name': 'test_per_class_teardown_partner'})
-        partners = self.env['res.partner'].search([('name', '=', 'test_per_class_teardown_partner')])
-        self.assertEqual(1, len(partners), "Test partner not found.")
+        """ Create a test base record. """
+        self.env['test_base.model'].create({'name': 'test_per_class_teardown'})
+        test_base_models = self.env['test_base.model'].search([('name', '=', 'test_per_class_teardown')])
+        self.assertEqual(1, len(test_base_models), "Test base record not found.")
 
     def test_01(self):
-        """ Find the created partner. """
-        partners = self.env['res.partner'].search([('name', '=', 'test_per_class_teardown_partner')])
-        self.assertEqual(1, len(partners), "Test partner not found.")
+        """ Find the created test base record. """
+        test_base_models = self.env['test_base.model'].search([('name', '=', 'test_per_class_teardown')])
+        self.assertEqual(1, len(test_base_models), "Test base record not found.")
 
     def test_20a(self):
-        """ Create a partner with a XML ID """
-        pid, _ = self.env['res.partner'].name_create('Mr Blue')
-        self.env['ir.model.data'].create({'name': 'test_partner_blue',
-                                          'module': 'base',
-                                          'model': 'res.partner',
-                                          'res_id': pid})
+        """ Create a test base record with a XML ID """
+        test_base_id, _ = self.env['test_base.model'].name_create('Mr Blue')
+        self.env['ir.model.data'].create({'name': 'test_base_name_blue',
+                                          'module': 'test_base',
+                                          'model': 'test_base.model',
+                                          'res_id': test_base_id})
 
     def test_20b(self):
         """ Resolve xml id with ref() and browse_ref() """
-        xid = 'base.test_partner_blue'
-        partner = self.env.ref(xid)
+        xid = 'test_base.test_base_name_blue'
+        test_base = self.env.ref(xid)
         pid = self.ref(xid)
         self.assertTrue(pid, "ref() should resolve xid to database ID")
-        self.assertEqual(pid, partner.id, "ref() is not consistent with env.ref()")
-        partner2 = self.browse_ref(xid)
-        self.assertEqual(partner, partner2, "browse_ref() should resolve xid to browse records")
+        self.assertEqual(pid, test_base.id, "ref() is not consistent with env.ref()")
+        test_base2 = self.browse_ref(xid)
+        self.assertEqual(test_base, test_base2, "browse_ref() should resolve xid to browse records")
 
 
 class TestTransactionCase(common.TransactionCase):
@@ -45,28 +45,28 @@ class TestTransactionCase(common.TransactionCase):
     """
 
     def test_00(self):
-        """ Create a partner. """
-        partners = self.env['res.partner'].search([('name', '=', 'test_per_class_teardown_partner')])
-        self.assertEqual(0, len(partners), "Test partner found.")
-        self.env['res.partner'].create({'name': 'test_per_class_teardown_partner'})
-        partners = self.env['res.partner'].search([('name', '=', 'test_per_class_teardown_partner')])
-        self.assertEqual(1, len(partners), "Test partner not found.")
+        """ Create a test base record. """
+        test_base_models = self.env['test_base.model'].search([('name', '=', 'test_per_class_teardown')])
+        self.assertEqual(0, len(test_base_models), "Test base record found.")
+        self.env['test_base.model'].create({'name': 'test_per_class_teardown'})
+        test_base_models = self.env['test_base.model'].search([('name', '=', 'test_per_class_teardown')])
+        self.assertEqual(1, len(test_base_models), "Test base record not found.")
 
     def test_01(self):
-        """ Don't find the created partner. """
-        partners = self.env['res.partner'].search([('name', '=', 'test_per_class_teardown_partner')])
-        self.assertEqual(0, len(partners), "Test partner found.")
+        """ Don't find the created test base record. """
+        test_base_models = self.env['test_base.model'].search([('name', '=', 'test_per_class_teardown')])
+        self.assertEqual(0, len(test_base_models), "Test base record found.")
 
     def test_20a(self):
-        """ Create a partner with a XML ID then resolve xml id with ref() and browse_ref() """
-        pid, _ = self.env['res.partner'].name_create('Mr Yellow')
-        self.env['ir.model.data'].create({'name': 'test_partner_yellow',
-                                          'module': 'base',
-                                          'model': 'res.partner',
-                                          'res_id': pid})
-        xid = 'base.test_partner_yellow'
-        partner = self.env.ref(xid)
+        """ Create a test base record with a XML ID then resolve xml id with ref() and browse_ref() """
+        test_base_id, _ = self.env['test_base.model'].name_create('Mr Yellow')
+        self.env['ir.model.data'].create({'name': 'test_base_name_yellow',
+                                          'module': 'test_base',
+                                          'model': 'test_base.model',
+                                          'res_id': test_base_id})
+        xid = 'test_base.test_base_name_yellow'
+        test_base = self.env.ref(xid)
         pid = self.ref(xid)
-        self.assertEquals(pid, partner.id, "ref() should resolve xid to database ID")
-        partner2 = self.browse_ref(xid)
-        self.assertEqual(partner, partner2, "browse_ref() should resolve xid to browse records")
+        self.assertEquals(pid, test_base.id, "ref() should resolve xid to database ID")
+        test_base2 = self.browse_ref(xid)
+        self.assertEqual(test_base, test_base2, "browse_ref() should resolve xid to browse records")
