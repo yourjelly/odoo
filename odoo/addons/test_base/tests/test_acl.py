50c9c1fc2e0a83f049e779c4c26f44fa23b086e4
Jigar Vaghela <jva@odoo.com>
2018-04-30 10:49:31 +0530

    [IMP] test_base: Remove business model use from "test_acl.py".

--- odoo/addons/test_base/tests/test_acl.py
+++ odoo/addons/test_base/tests/test_acl.py
@@ -6,17 +6,16 @@ from lxml import etree
 from odoo.exceptions import AccessError
 from odoo.tests.common import TransactionCase
 from odoo.tools.misc import mute_logger
+from odoo.addons.test_base.tests.common import TransactionCaseCommon
 
-# test group that demo user should not have
-USER_DEMO = 'base.user_demo'
+
 GROUP_SYSTEM = 'base.group_system'
 
 
-class TestACL(TransactionCase):
+class TestACL(TransactionCaseCommon):
 
     def setUp(self):
         super(TestACL, self).setUp()
-        self.demo_user = self.env.ref(USER_DEMO)
         self.erp_system_group = self.env.ref(GROUP_SYSTEM)
 
     def _set_field_groups(self, model, field_name, groups):
@@ -26,213 +25,212 @@ class TestACL(TransactionCase):
     def test_field_visibility_restriction(self):
         """Check that model-level ``groups`` parameter effectively restricts access to that
            field for users who do not belong to one of the explicitly allowed groups"""
-        currency = self.env['res.currency'].sudo(self.demo_user)
+        TestbaseModel = self.env['test_base.model'].sudo(self.user_test)
 
         # Verify the test environment first
-        original_fields = currency.fields_get([])
-        form_view = currency.fields_view_get(False, 'form')
+        original_fields = TestbaseModel.fields_get([])
+        form_view = TestbaseModel.fields_view_get(False, 'form')
         view_arch = etree.fromstring(form_view.get('arch'))
-        has_group_system = self.demo_user.has_group(GROUP_SYSTEM)
+        has_group_system = self.user_test.has_group(GROUP_SYSTEM)
         self.assertFalse(has_group_system, "`demo` user should not belong to the restricted group before the test")
-        self.assertIn('decimal_places', original_fields, "'decimal_places' field must be properly visible before the test")
-        self.assertNotEquals(view_arch.xpath("//field[@name='decimal_places']"), [],
-                             "Field 'decimal_places' must be found in view definition before the test")
+        self.assertIn('parent_id', original_fields, "'parent_id' field must be properly visible before the test")
+        self.assertNotEquals(view_arch.xpath("//field[@name='parent_id']"), [],
+                             "Field 'parent_id' must be found in view definition before the test")
 
         # restrict access to the field and check it's gone
-        self._set_field_groups(currency, 'decimal_places', GROUP_SYSTEM)
+        self._set_field_groups(TestbaseModel, 'parent_id', GROUP_SYSTEM)
 
-        fields = currency.fields_get([])
-        form_view = currency.fields_view_get(False, 'form')
+        fields = TestbaseModel.fields_get([])
+        form_view = TestbaseModel.fields_view_get(False, 'form')
         view_arch = etree.fromstring(form_view.get('arch'))
-        self.assertNotIn('decimal_places', fields, "'decimal_places' field should be gone")
-        self.assertEquals(view_arch.xpath("//field[@name='decimal_places']"), [],
-                          "Field 'decimal_places' must not be found in view definition")
+        self.assertNotIn('parent_id', fields, "'parent_id' field should be gone")
+        self.assertEquals(view_arch.xpath("//field[@name='parent_id']"), [],
+                          "Field 'parent_id' must not be found in view definition")
 
         # Make demo user a member of the restricted group and check that the field is back
-        self.erp_system_group.users += self.demo_user
-        has_group_system = self.demo_user.has_group(GROUP_SYSTEM)
-        fields = currency.fields_get([])
-        form_view = currency.fields_view_get(False, 'form')
+        self.erp_system_group.users += self.user_test
+        has_group_system = self.user_test.has_group(GROUP_SYSTEM)
+        fields = TestbaseModel.fields_get([])
+        form_view = TestbaseModel.fields_view_get(False, 'form')
         view_arch = etree.fromstring(form_view.get('arch'))
         self.assertTrue(has_group_system, "`demo` user should now belong to the restricted group")
-        self.assertIn('decimal_places', fields, "'decimal_places' field must be properly visible again")
-        self.assertNotEquals(view_arch.xpath("//field[@name='decimal_places']"), [],
-                             "Field 'decimal_places' must be found in view definition again")
+        self.assertIn('parent_id', fields, "'parent_id' field must be properly visible again")
+        self.assertNotEquals(view_arch.xpath("//field[@name='parent_id']"), [],
+                             "Field 'parent_id' must be found in view definition again")
 
     @mute_logger('odoo.models')
     def test_field_crud_restriction(self):
         "Read/Write RPC access to restricted field should be forbidden"
-        partner = self.env['res.partner'].browse(1).sudo(self.demo_user)
+        test_base_model = self.env['test_base.model'].search([], limit=1)[0].sudo(self.user_test)
 
         # Verify the test environment first
-        has_group_system = self.demo_user.has_group(GROUP_SYSTEM)
+        has_group_system = self.user_test.has_group(GROUP_SYSTEM)
         self.assertFalse(has_group_system, "`demo` user should not belong to the restricted group")
-        self.assert_(partner.read(['bank_ids']))
-        self.assert_(partner.write({'bank_ids': []}))
+        self.assert_(test_base_model.read(['color']))
+        self.assert_(test_base_model.write({'color': 1}))
 
         # Now restrict access to the field and check it's forbidden
-        self._set_field_groups(partner, 'bank_ids', GROUP_SYSTEM)
+        self._set_field_groups(test_base_model, 'color', GROUP_SYSTEM)
 
         with self.assertRaises(AccessError):
-            partner.read(['bank_ids'])
+            test_base_model.read(['color'])
         with self.assertRaises(AccessError):
-            partner.write({'bank_ids': []})
+            test_base_model.write({'color': 1})
 
         # Add the restricted group, and check that it works again
-        self.erp_system_group.users += self.demo_user
-        has_group_system = self.demo_user.has_group(GROUP_SYSTEM)
+        self.erp_system_group.users += self.user_test
+        has_group_system = self.user_test.has_group(GROUP_SYSTEM)
         self.assertTrue(has_group_system, "`demo` user should now belong to the restricted group")
-        self.assert_(partner.read(['bank_ids']))
-        self.assert_(partner.write({'bank_ids': []}))
+        self.assert_(test_base_model.read(['color']))
+        self.assert_(test_base_model.write({'color': 1}))
 
     @mute_logger('odoo.models')
     def test_fields_browse_restriction(self):
         """Test access to records having restricted fields"""
-        partner = self.env['res.partner'].sudo(self.demo_user)
-        self._set_field_groups(partner, 'email', GROUP_SYSTEM)
+        TestbaseModel = self.env['test_base.model'].sudo(self.user_test)
+        self._set_field_groups(TestbaseModel, 'parent_id', GROUP_SYSTEM)
 
         # accessing fields must no raise exceptions...
-        partner = partner.search([], limit=1)
-        partner.name
+        test_base_model = TestbaseModel.search([], limit=1)
+        test_base_model.name
         # ... except if they are restricted
         with self.assertRaises(AccessError):
             with mute_logger('odoo.models'):
-                partner.email
+                test_base_model.parent_id
 
     def test_view_create_edit_button_invisibility(self):
         """ Test form view Create, Edit, Delete button visibility based on access right of model"""
         methods = ['create', 'edit', 'delete']
-        company = self.env['res.company'].sudo(self.demo_user)
-        company_view = company.fields_view_get(False, 'form')
-        view_arch = etree.fromstring(company_view['arch'])
+        o2m_group_system = self.env['test_base.m2o.group.system'].sudo(self.user_test)
+        base_restricted_view = o2m_group_system.fields_view_get(False, 'form')
+        view_arch = etree.fromstring(base_restricted_view['arch'])
         for method in methods:
             self.assertEqual(view_arch.get(method), 'false')
 
     def test_view_create_edit_button_visibility(self):
         """ Test form view Create, Edit, Delete button visibility based on access right of model"""
-        self.erp_system_group.users += self.demo_user
+        self.erp_system_group.users += self.user_test
         methods = ['create', 'edit', 'delete']
-        company = self.env['res.company'].sudo(self.demo_user)
-        company_view = company.fields_view_get(False, 'form')
-        view_arch = etree.fromstring(company_view['arch'])
+        o2m_group_system = self.env['test_base.m2o.group.system'].sudo(self.user_test)
+        base_restricted_view = o2m_group_system.fields_view_get(False, 'form')
+        view_arch = etree.fromstring(base_restricted_view['arch'])
         for method in methods:
             self.assertIsNone(view_arch.get(method))
 
     def test_m2o_field_create_edit_invisibility(self):
-        """ Test many2one field Create and Edit option visibility based on access rights of relation field""" 
+        """ Test many2one field Create and Edit option visibility based on access rights of relation field"""
         methods = ['create', 'write']
-        company = self.env['res.company'].sudo(self.demo_user)
-        company_view = company.fields_view_get(False, 'form')
-        view_arch = etree.fromstring(company_view['arch'])
-        field_node = view_arch.xpath("//field[@name='currency_id']")
-        self.assertTrue(len(field_node), "currency_id field should be in company from view")
+        test_base_model = self.env['test_base.model'].sudo(self.user_test)
+        test_base_view = test_base_model.fields_view_get(False, 'form')
+        view_arch = etree.fromstring(test_base_view['arch'])
+        field_node = view_arch.xpath("//field[@name='m2o_group_system_id']")
+        self.assertTrue(len(field_node), "m2o_group_system_id field should be in test_base_model from view")
         for method in methods:
             self.assertEqual(field_node[0].get('can_' + method), 'false')
 
     def test_m2o_field_create_edit_visibility(self):
-        """ Test many2one field Create and Edit option visibility based on access rights of relation field""" 
-        self.erp_system_group.users += self.demo_user
+        """ Test many2one field Create and Edit option visibility based on access rights of relation field"""
+        self.erp_system_group.users += self.user_test
         methods = ['create', 'write']
-        company = self.env['res.company'].sudo(self.demo_user)
-        company_view = company.fields_view_get(False, 'form')
-        view_arch = etree.fromstring(company_view['arch'])
-        field_node = view_arch.xpath("//field[@name='currency_id']")
-        self.assertTrue(len(field_node), "currency_id field should be in company from view")
+        test_base_model = self.env['test_base.model'].sudo(self.user_test)
+        test_base_view = test_base_model.fields_view_get(False, 'form')
+        view_arch = etree.fromstring(test_base_view['arch'])
+        field_node = view_arch.xpath("//field[@name='m2o_group_system_id']")
+        self.assertTrue(len(field_node), "m2o_group_system_id field should be in test_base_model from view")
         for method in methods:
             self.assertEqual(field_node[0].get('can_' + method), 'true')
 
 
-class TestIrRule(TransactionCase):
+class TestIrRule(TransactionCaseCommon):
 
     def test_ir_rule(self):
-        model_res_partner = self.env.ref('base.model_res_partner')
+        model_test_base_model = self.env.ref('test_base.model_test_base_model')
         group_user = self.env.ref('base.group_user')
-        user_demo = self.env.ref('base.user_demo')
 
         # create an ir_rule for the Employee group with an blank domain
         rule1 = self.env['ir.rule'].create({
             'name': 'test_rule1',
-            'model_id': model_res_partner.id,
+            'model_id': model_test_base_model.id,
             'domain_force': False,
             'groups': [(6, 0, group_user.ids)],
         })
 
-        # read as demo user the partners (one blank domain)
-        partners_demo = self.env['res.partner'].sudo(user_demo)
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see some partner.")
+        # read as test user the test_base.model (one blank domain)
+        TestbaseModel = self.env['test_base.model'].sudo(self.user_test)
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see some records.")
 
         # same with domain 1=1
         rule1.domain_force = "[(1,'=',1)]"
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see some partner.")
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see some records.")
 
         # same with domain []
         rule1.domain_force = "[]"
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see some partner.")
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see some records.")
 
         # create another ir_rule for the Employee group (to test multiple rules)
         rule2 = self.env['ir.rule'].create({
             'name': 'test_rule2',
-            'model_id': model_res_partner.id,
+            'model_id': model_test_base_model.id,
             'domain_force': False,
             'groups': [(6, 0, group_user.ids)],
         })
 
-        # read as demo user with domains [] and blank
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see some partner.")
+        # read as test user with domains [] and blank
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see some records.")
 
         # same with domains 1=1 and blank
         rule1.domain_force = "[(1,'=',1)]"
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see some partner.")
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see some records.")
 
         # same with domains 1=1 and 1=1
         rule2.domain_force = "[(1,'=',1)]"
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see some partner.")
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see some records.")
 
         # create another ir_rule for the Employee group (to test multiple rules)
         rule3 = self.env['ir.rule'].create({
             'name': 'test_rule3',
-            'model_id': model_res_partner.id,
+            'model_id': model_test_base_model.id,
             'domain_force': False,
             'groups': [(6, 0, group_user.ids)],
         })
 
-        # read the partners as demo user
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see some partner.")
+        # read the test_base as test user
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see some records.")
 
         # same with domains 1=1, 1=1 and 1=1
         rule3.domain_force = "[(1,'=',1)]"
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see some partner.")
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see some records.")
 
         # modify the global rule on res_company which triggers a recursive check
         # of the rules on company
         global_rule = self.env.ref('base.res_company_rule_employee')
         global_rule.domain_force = "[('id','child_of',[user.company_id.id])]"
 
-        # read as demo user (exercising the global company rule)
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see some partner.")
+        # read as test user (exercising the global company rule)
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see some records.")
 
         # Modify the ir_rule for employee to have a rule that fordids seeing any
         # record. We use a domain with implicit AND operator for later tests on
         # normalization.
         rule2.domain_force = "[('id','=',False),('name','=',False)]"
 
-        # check that demo user still sees partners, because group-rules are OR'ed
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see some partner.")
+        # check that test user still sees test_base, because group-rules are OR'ed
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see some records.")
 
-        # create a new group with demo user in it, and a complex rule
+        # create a new group with test user in it, and a complex rule
         group_test = self.env['res.groups'].create({
             'name': 'Test Group',
-            'users': [(6, 0, user_demo.ids)],
+            'users': [(6, 0, self.user_test.ids)],
         })
 
         # add the rule to the new group, with a domain containing an implicit
@@ -243,13 +241,13 @@ class TestIrRule(TransactionCase):
             'groups': [(6, 0, group_test.ids)],
         })
 
-        # read the partners again as demo user, which should give results
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see partners even with the combined rules.")
+        # read the test_base again as test user, which should give results
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see records even with the combined rules.")
 
         # delete global domains (to combine only group domains)
         self.env['ir.rule'].search([('groups', '=', False)]).unlink()
 
-        # read the partners as demo user (several group domains, no global domain)
-        partners = partners_demo.search([])
-        self.assertTrue(partners, "Demo user should see some partners.")
+        # read the test_base as test user (several group domains, no global domain)
+        test_base_models = TestbaseModel.search([])
+        self.assertTrue(test_base_models, "Test user should see some records.")
