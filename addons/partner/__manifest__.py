5a2b02a68c9a83db9d271d710214ca6e1d8f514d
Jigar Vaghela <jva@odoo.com>
2018-05-10 16:57:25 +0530

    [ADD] partner: Create new module to move res_partner field and related business code from base.

---
+++ addons/partner/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Partner',
+    'category': 'Tools',
+    'summary': 'Partner, Partner category',
+    'version': '1.0',
+    'description': """
+Extedn Partner.
+""",
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
