311213964cc6d61dc0dcae208a44b39ef74d37f5
Josse Colpaert <jco@odoo.com>
2018-06-20 16:32:42 +0200

    [IMP] continue csrf False

--- addons/hw_posbox_homepage/controllers/main.py
+++ addons/hw_posbox_homepage/controllers/main.py
@@ -173,7 +173,7 @@ class PosboxHomepage(odoo.addons.web.controllers.main.Home):
         subprocess.call(['/home/pi/odoo/addons/point_of_sale/tools/posbox/configuration/connect_to_wifi.sh', essid, password, persistent])
         return "connecting to " + essid
 
-    @http.route('/wifi_clear', type='http', auth='none', cors='*')
+    @http.route('/wifi_clear', type='http', auth='none', cors='*', csrf=False)
     def clear_wifi_configuration(self):
         os.system('/home/pi/odoo/addons/point_of_sale/tools/posbox/configuration/clear_wifi_configuration.sh')
         return "configuration cleared"
@@ -246,7 +246,7 @@ class PosboxHomepage(odoo.addons.web.controllers.main.Home):
 """
         return ngrok_template
 
-    @http.route('/enable_ngrok', type='http', auth='none', cors='*')
+    @http.route('/enable_ngrok', type='http', auth='none', cors='*', csrf=False)
     def enable_ngrok(self, auth_token):
         if subprocess.call(['pgrep', 'ngrok']) == 1:
             subprocess.Popen(['ngrok', 'tcp', '-authtoken', auth_token, '-log', '/tmp/ngrok.log', '22'])
