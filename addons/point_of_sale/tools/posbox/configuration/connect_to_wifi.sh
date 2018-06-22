62641b5df8965519c0f3e611519da62acd37b0dd
Josse Colpaert <jco@odoo.com>
2018-06-22 11:41:02 +0200

    [WIP] wifi: check if this solves the ifconfig changes

--- addons/point_of_sale/tools/posbox/configuration/connect_to_wifi.sh
+++ addons/point_of_sale/tools/posbox/configuration/connect_to_wifi.sh
@@ -55,7 +55,7 @@ function connect () {
 	sudo service dhcpcd restart
 
 	# give dhcp some time
-	timeout 30 sh -c 'until ifconfig wlan0 | grep "inet addr:" ; do sleep 0.1 ; done'
+	timeout 30 sh -c 'until ifconfig wlan0 | grep "inet " ; do sleep 0.1 ; done'
 	TIMEOUT_RETURN=$?
 
 	if [ ${TIMEOUT_RETURN} -eq 124 ] && [ -z "${NO_AP}" ] ; then
