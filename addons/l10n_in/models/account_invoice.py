ee42d2acf6cb7b33063808b968b3008505719765
Jigar Vaghela <jva@odoo.com>
2018-06-22 11:40:33 +0530

    [IMP] l10n_in: Reverse charge under GST. As per GST rules tax will have to be paid directly by the receiver to the Government instead of the supplier. So reverse tax line add when tick l10n_in_reverse_charge in invoice. Also add tax account for cess and set in refund_account_id of cess tax. Because refund_account_id must need for reverse charge. Also set refund_account_id as Payable or Receivable rather then Purchase Expense and Local Sales.

--- addons/l10n_in/models/account_invoice.py
+++ addons/l10n_in/models/account_invoice.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class AccountInvoice(models.Model):
@@ -28,6 +29,13 @@ class AccountInvoice(models.Model):
     l10n_in_shipping_bill_date = fields.Date('Shipping bill date', readonly=True, states={'draft': [('readonly', False)]})
     l10n_in_shipping_port_code_id = fields.Many2one('l10n_in.port.code', 'Shipping port code', states={'draft': [('readonly', False)]})
     l10n_in_reseller_partner_id = fields.Many2one('res.partner', 'Reseller', domain=[('vat', '!=', False)], help="Only Registered Reseller", readonly=True, states={'draft': [('readonly', False)]})
+    l10n_in_reverse_charge = fields.Boolean('Reverse Charge', readonly=True, states={'draft': [('readonly', False)]})
+
+    @api.model
+    def _get_refund_common_fields(self):
+        """This list of fields value pass to refund invoice."""
+        return super(AccountInvoice, self)._get_refund_common_fields() + [
+            'l10n_in_reverse_charge']
 
     def _get_printed_report_name(self):
         self.ensure_one()
@@ -61,6 +69,7 @@ class AccountInvoice(models.Model):
             })
         return vals
 
+    @api.multi
     def action_move_create(self):
         res = super(AccountInvoice, self).action_move_create()
         for inv in self:
@@ -70,9 +79,34 @@ class AccountInvoice(models.Model):
                 'l10n_in_shipping_bill_date': inv.l10n_in_shipping_bill_date,
                 'l10n_in_shipping_port_code_id': inv.l10n_in_shipping_port_code_id.id,
                 'l10n_in_reseller_partner_id': inv.l10n_in_reseller_partner_id.id,
+                'l10n_in_reverse_charge': inv.l10n_in_reverse_charge
                 }
             inv.move_id.write(vals)
         return res
+
+    @api.multi
+    def get_taxes_values(self):
+        """Update tax when Reverse charge under GST.
+        reverse charge rule is GST will have to be paid directly by the receiver to the Government instead of the supplier."""
+        tax_grouped = super(AccountInvoice, self).get_taxes_values()
+        if self.l10n_in_reverse_charge:
+            reverse_charge_tax = {}
+            for val in tax_grouped.values():
+                new_val = val.copy()
+                tax = self.env['account.tax'].browse(val['tax_id'])
+                if not tax.refund_account_id or not tax.account_id:
+                    raise ValidationError(_('Cannot find a accounts for %s tax, You should configure it. \nPlease go to Account Tax under Configuration.') % tax.name)
+                new_val['account_id'] = self.type in ('out_invoice', 'in_invoice') and tax.refund_account_id.id or tax.account_id.id
+                key = tax.get_grouping_key(new_val)
+                reverse_charge_tax[key] = new_val
+                reverse_charge_tax[key]['amount'] = new_val['amount'] * -1
+            tax_grouped.update(reverse_charge_tax)
+        return tax_grouped
+
+    @api.onchange('l10n_in_reverse_charge')
+    def _onchange_l10n_in_reverse_charge(self):
+        """For update tax_lines."""
+        return self._onchange_invoice_line_ids()
 
 
 class AccountInvoiceLine(models.Model):
