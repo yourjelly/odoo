ca6782cb41ea70be3b7d4122d3a3b0a98fa28c71
Jigar Vaghela <jva@odoo.com>
2018-06-22 17:07:16 +0530

    [IMP] l10n_in: Add GSTR Invoice report.In Report We need to group by gst rate but when tax is python code or group of tax then we can not find rate of tax so add new field l10n_in_description in account.tax and account.tax.template. Also need one gst tax from m2m tax_ids in account.invoice.line so we add compute field l10n_in_tax_id that give gst tax from tax_ids so easily use in sql view report. This report cover GSRT-1 section B2B, B2CL, B2CS, EXP, CDNR and CDNUR.

--- addons/l10n_in/models/chart_template.py
+++ addons/l10n_in/models/chart_template.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models, _
+from odoo import api, fields, models, _
 
 
 class AccountChartTemplate(models.Model):
@@ -21,3 +21,16 @@ class AccountChartTemplate(models.Model):
         res += [{'type': 'sale', 'name': _('Retail Invoices'), 'code': 'RETINV', 'company_id': company.id, 'show_on_dashboard': True,},
                 {'type': 'sale', 'name': _('Export Invoices'), 'code': 'EXPINV', 'company_id': company.id, 'show_on_dashboard': True, 'l10n_in_import_export': True},]
         return res
+
+
+class AccountTaxTemplate(models.Model):
+    _inherit = 'account.tax.template'
+
+    #use in GSTR export report as Rate of tax.
+    l10n_in_description = fields.Char(string='Label GST Report')
+
+    def _get_tax_vals(self, company, tax_template_to_tax):
+        val = super(AccountTaxTemplate, self)._get_tax_vals(company, tax_template_to_tax)
+        if self.tax_group_id:
+            val['l10n_in_description'] = self.l10n_in_description
+        return val
