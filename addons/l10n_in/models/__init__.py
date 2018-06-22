510eca9e428836a55ef29861e14ef398cf79eebb
Jigar Vaghela <jva@odoo.com>
2018-06-22 18:03:35 +0530

    [IMP] l10n_in: Add hsn report.it cover GSTIR-1 section HSN. Hsn report is base on product.In HSN report as per gst rules hsn description is required when hsn code not given. As per GST Rules you need to Specify UQC from given list. Given list as shown below: BAG-BAGS BAL-BALE BDL-BUNDLES BKL-BUCKLES BOU-BILLION OF UNITS BOX-BOX BTL-BOTTLES BUN-BUNCHES CAN-CANS CBM-CUBIC METERS CCM-CUBIC CENTIMETERS CMS-CENTIMETERS CTN-CARTONS DOZ-DOZENS DRM-DRUMS GGK-GREAT GROSS GMS-GRAMMES GRS-GROSS GYD-GROSS YARDS KGS-KILOGRAMS KLR-KILOLITRE KME-KILOMETRE MLT-MILILITRE MTR-METERS MTS-METRIC TON NOS-NUMBERS PAC-PACKS PCS-PIECES PRS-PAIRS QTL-QUINTAL ROL-ROLLS SET-SETS SQF-SQUARE FEET SQM-SQUARE METERS SQY-SQUARE YARDS TBS-TABLETS TGM-TEN GROSS THD-THOUSANDS TON-TONNES TUB-TUBES UGS-US GALLONS UNT-UNITS YDS-YARDS OTH-OTHERS.
    So we add new field in uom l10n_in_code.

--- addons/l10n_in/models/__init__.py
+++ addons/l10n_in/models/__init__.py
@@ -12,3 +12,4 @@ from . import res_company
 from . import res_config_settings
 from . import res_country_state
 from . import res_partner
+from . import uom_uom
--- addons/l10n_in/report/__init__.py
+++ addons/l10n_in/report/__init__.py
@@ -3,3 +3,4 @@
 
 from . import account_invoice_report
 from . import account_payment_report
+from . import hsn_gst_report
