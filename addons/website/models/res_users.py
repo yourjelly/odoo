dd19dc944d51d483218185b9dbf1853151985517
Joren Van Onder <jov@odoo.com>
2018-06-26 16:35:25 -0700

    Move registered_on_website_id to res.partner (also rename to website_id)

--- addons/website/models/res_users.py
+++ addons/website/models/res_users.py
@@ -15,11 +15,10 @@ class ResUsers(models.Model):
         selection=lambda self: self._get_group_selection('base.module_category_website'),
         string='Website Editor', compute='_compute_groups_id', inverse='_inverse_groups_id',
         category_xml_id='base.module_category_website')
-    registered_on_website_id = fields.Many2one('website')
 
     _sql_constraints = [
         # this is done in Python because a SQL constraint like UNIQUE
-        # (login, registered_on_website_id) allows ('abc', NULL) and
+        # (login, website_id) allows ('abc', NULL) and
         # ('abc', NULL) to coexist because of how SQL handles NULLs.
         ('login_key', 'CHECK (1=1)', 'You can not have two users with the same login!')
     ]
@@ -31,24 +30,24 @@ class ResUsers(models.Model):
             return True
         return super(ResUsers, self)._has_unsplash_key_rights()
 
-    @api.constrains('login', 'registered_on_website_id')
+    @api.constrains('login', 'website_id')
     def _check_login(self):
         for user in self:
             if self.search([('id', '!=', user.id), ('login', '=', user.login),
-                            '|', ('registered_on_website_id', '=', False),
-                                 ('registered_on_website_id', '=', user.registered_on_website_id.id)]):
+                            '|', ('website_id', '=', False),
+                                 ('website_id', '=', user.website_id.id)]):
                 raise ValidationError(_('You can not have two users with the same login!'))
 
     @api.model
     def _get_login_domain(self, login):
         current_website_id = self.env['website'].get_current_website().id
-        multi_website_domain = ['|', ('registered_on_website_id', '=', False), ('registered_on_website_id', '=', current_website_id)]
+        multi_website_domain = ['|', ('website_id', '=', False), ('website_id', '=', current_website_id)]
         return super(ResUsers, self)._get_login_domain(login) + multi_website_domain
 
     @api.model
     def _signup_create_user(self, values):
         new_user = super(ResUsers, self)._signup_create_user(values)
-        new_user.registered_on_website_id = self.env['website'].get_current_website()
+        new_user.website_id = self.env['website'].get_current_website()
         return new_user
 
     @api.model
