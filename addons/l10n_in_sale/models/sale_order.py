5ae92d6225450be8fe633f507e8adf046f3fc4ff
Jigar Vaghela <jva@odoo.com>
2018-06-27 16:01:06 +0530

    [IMP] l10n_in_sale: Add multi GSTIN features for sale. Add Multi GSTIN partner in sale.order and pass this value to account.invoice when invoice create from sale. Also update invoice qweb report with new fields igst_amount,cgst_amount,sgst_amount and cess_amount. And set price_total and sub_total in last row. And replace external_layout with l10n_in_external_layout if country of company is India, For set report header and footer address and GSTIN(vat) of selected l10n_in_location_partner(GSTIN).

---
+++ addons/l10n_in_sale/models/sale_order.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    l10n_in_gstin_partner_id = fields.Many2one(
+        'res.partner',
+        string="GSTIN Partner",
+        required=True,
+        default=lambda self: self.env['res.company']._company_default_get('sale.order').partner_id,
+        readonly=True, states={'draft': [('readonly', False)]}
+        )
+
+    @api.multi
+    def _prepare_invoice(self):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals['l10n_in_gstin_partner_id'] = self.l10n_in_gstin_partner_id.id
+        return invoice_vals
