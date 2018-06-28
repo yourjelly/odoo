c61625f59d32cf046a2bfbf741c391625fb5dd2b
Jigar Vaghela <jva@odoo.com>
2018-06-28 17:54:26 +0530

    [IMP] l10n_in: Add new futuristic fields for GSTR-2 reports. GSTR-1 is for outward supplies invoices and GSTR-2 is for inward supplies invoices. Compostion scheme for small taxpayers can get rid of tedious GST formalities and pay GST at a fixed rate of turnover. Input Tax Credit(ITC) means When purchase anything then required to pay GST on it. Later, GST registered person can claim input tax credit on the GST paid on purchases base on GST rules. In GSTR-2 report there is 3 selection for itc eligible as shown below: Inputs (means normal goods) Capital goods (means asset goods) Input services. So add new field l10n_in_is_asset in product so differentiate asset product. When import of goods then import type need to specify.

--- addons/l10n_in/models/res_partner.py
+++ addons/l10n_in/models/res_partner.py
@@ -11,3 +11,5 @@ class ResPartner(models.Model):
     l10n_in_gstin_company_id = fields.Many2one('res.company', string="GSTIN Company")
     #Use in view attrs. Need to required state_id if Country is India.
     country_code = fields.Char(related="country_id.code", string="Country code")
+    #In GSTR-2 report We need to specify that vendor is under composition scheme or not.
+    l10n_in_composition = fields.Boolean(string="Is Composition", help="Check this box if this vendor is under composition scheme")
