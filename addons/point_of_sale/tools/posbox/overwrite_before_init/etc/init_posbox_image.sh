8c2480ece16ebfd7ab54970a24e7af8d016436da
Josse Colpaert <jco@odoo.com>
2018-07-06 10:15:44 +0200

    [IMP] packages: try to install as much as possible with apt instead of pip

--- addons/point_of_sale/tools/posbox/overwrite_before_init/etc/init_posbox_image.sh
+++ addons/point_of_sale/tools/posbox/overwrite_before_init/etc/init_posbox_image.sh
@@ -22,7 +22,7 @@ apt-get update && apt-get -y upgrade
 # Firmware 4.44 seems to prevent the LED mechanism from working
 
 #PKGS_TO_INSTALL="adduser postgresql-client python3 python3-dateutil python3-decorator python3-docutils python3-feedparser python3-pil python3-jinja2 python3-ldap3 python3-lxml python3-mako python3-mock python3-openid python3-psutil python3-psycopg2 python3-babel python3-pydot python3-pyparsing python3-pypdf2 python3-reportlab python3-requests python3-simplejson python3-tz python3-vatnumber python3-werkzeug python3-yaml postgresql python3-serial python3-pip python3-dev localepurge vim mc mg screen iw hostapd isc-dhcp-server git rsync console-data lightdm xserver-xorg-video-fbdev xserver-xorg-input-evdev iceweasel xdotool unclutter x11-utils openbox python3-netifaces rpi-update"
-PKGS_TO_INSTALL="adduser postgresql python3 python3-dateutil python3-decorator python3-docutils python3-feedparser python3-pil python3-jinja2 python3-ldap3 python3-lxml python3-mako python3-mock python3-openid python3-psutil python3-psycopg2 python3-babel python3-pydot python3-pyparsing python3-pypdf2 python3-reportlab python3-requests python3-simplejson python3-tz python3-vatnumber python3-werkzeug python3-yaml python3-serial python3-pip python3-dev localepurge vim mc mg screen iw hostapd isc-dhcp-server git rsync console-data lightdm xserver-xorg-video-fbdev xserver-xorg-input-evdev iceweasel xdotool unclutter x11-utils openbox python3-netifaces rpi-update"
+PKGS_TO_INSTALL="adduser postgresql python3 python3-dateutil python3-decorator python3-docutils python3-feedparser python3-pil python3-jinja2 python3-ldap3 python3-lxml python3-mako python3-mock python3-openid python3-psutil python3-psycopg2 python3-babel python3-pydot python3-pyparsing python3-pypdf2 python3-reportlab python3-requests python3-simplejson python3-tz python3-vatnumber python3-werkzeug python3-yaml python3-serial python3-pip python3-dev localepurge vim mc mg screen iw hostapd isc-dhcp-server git rsync console-data lightdm xserver-xorg-video-fbdev xserver-xorg-input-evdev iceweasel xdotool unclutter x11-utils openbox python3-netifaces rpi-update python3-passlib python3-libsass python3-qrcode python3-html2text python3-unittest2 python3-simplejson"
 # KEEP OWN CONFIG FILES DURING PACKAGE CONFIGURATION
 # http://serverfault.com/questions/259226/automatically-keep-current-version-of-config-files-when-apt-get-install
 apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install ${PKGS_TO_INSTALL}
@@ -41,18 +41,12 @@ rm -rf /usr/share/doc
 # this may be fixed with libusb>2:1.0.11-1, but that's the most recent one in raspbian
 # so we install the latest pyusb that works with this libusb
 pip3 install pyusb==1.0.0b1
-pip3 install qrcode
-pip3 install html2text
 pip3 install evdev
-pip3 install simplejson
-pip3 install unittest2
-pip3 install passlib
-pip3 install libsass
 pip3 install gatt
 
 # --upgrade because websocket_client in wheezy is bad:
 # https://github.com/docker/compose/issues/1288
-pip3 install --upgrade websocket_client
+#pip3 install --upgrade websocket_client
 
 groupadd usbusers
 usermod -a -G usbusers pi
