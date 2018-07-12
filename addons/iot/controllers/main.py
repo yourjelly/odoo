cc39a41d179a8c53e0bef703d19bec550d06babc
Josse Colpaert <jco@odoo.com>
2018-07-12 12:02:10 +0200

    [IMP] iot: send box/ip/device to server and start driverdetails

---
+++ addons/iot/controllers/main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#import logging
+#import os
+#import re
+#import time
+#from collections import namedtuple
+#from os import listdir
+#from threading import Thread, Lock
+from odoo import http
+from odoo.http import request
+
+class IoTController(http.Controller):
+
+    @http.route('/iot2', type='http', auth='public', csrf=False)
+    def update_device(self, iot_identifier, name, identifier):
+        # Search id of iotbox that corresponds to this identifier
+        existing_devices = request.env['iot.device'].sudo().search([('iot_id.identifier', '=', iot_identifier),
+                                                               ('identifier', '=', identifier)])
+        if not existing_devices:
+            box = request.env['iot.box'].sudo().search([('identifier', '=', iot_identifier)], limit=1)
+            request.env['iot.device'].sudo().create({
+                'iot_id': box.id, #Might return error code when not successful
+                'name': name,
+                'identifier': identifier,
+            })
+        return 'ok'
+
+    @http.route('/iot3', type='http', auth='public', csrf=False)
+    def update_box(self, name, identifier, ip):
+        existing_box = request.env['iot.box'].sudo().search([('identifier', '=', identifier)])
+        if not existing_box:
+            request.env['iot.box'].sudo().create({
+                'name': name,
+                'identifier': identifier,
+                'ip': ip,
+            })
+        else:
+            existing_box[0].ip = ip #Might set name to
+
+        return 'ok'
