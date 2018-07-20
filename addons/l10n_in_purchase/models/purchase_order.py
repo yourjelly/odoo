8421e2724e148db89e57c05cfb66748e37557357
Jigar Vaghela <jva@odoo.com>
2018-07-20 13:00:16 +0530

    [IMP] l10n_in_purchase: Fiscal position set by GSTIN. Pass GSTIN value using context in onchange_partner_id so get this context in get_fiscal_position.

--- addons/l10n_in_purchase/models/purchase_order.py
+++ addons/l10n_in_purchase/models/purchase_order.py
@@ -14,3 +14,7 @@ class PurchaseOrder(models.Model):
         default=lambda self: self.env['res.company']._company_default_get('purchase.order').partner_id,
         readonly=True, states={'draft': [('readonly', False)]}
         )
+
+    @api.onchange('partner_id', 'company_id', 'l10n_in_gstin_partner_id')
+    def onchange_partner_id(self):
+        return super(PurchaseOrder, self.with_context(l10n_in_gstin_partner_id=self.l10n_in_gstin_partner_id.id)).onchange_partner_id()
