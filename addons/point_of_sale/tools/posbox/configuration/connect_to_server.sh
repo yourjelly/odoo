a412a243dc64097ef1c137f0ec6fc71e09921f84
Josse Colpaert <jco@odoo.com>
2018-07-23 10:51:58 +0200

    [IMP] iot: need to update hostname in other branch

--- addons/point_of_sale/tools/posbox/configuration/connect_to_server.sh
+++ addons/point_of_sale/tools/posbox/configuration/connect_to_server.sh
@@ -4,14 +4,19 @@
 function connect () {
 	SERVER="${1}"
 	CURRENT_SERVER_FILE=/home/pi/odoo-remote-server.conf
-	HOSTS=/etc/hosts
+	HOSTS=/root_bypass_ramdisks/etc/hosts
+	HOST_FILE=/root_bypass_ramdisks/etc/hostname
 	HOSTNAME="$(hostname)"
 	IOT_NAME="${2}"
 	sudo mount -o remount,rw /
+	sudo mount -o remount,rw /root_bypass_ramdisks
 	echo "${SERVER}" > ${CURRENT_SERVER_FILE}
 	sudo sed -i "s/${HOSTNAME}/${IOT_NAME}/g" ${HOSTS}
+	echo "${HOSTNAME}" > /tmp/hostname
+	sudo cp /tmp/hostname ${HOST_FILE}
 	sudo hostname "${IOT_NAME}"
 	sudo mount -o remount,ro /
+	sudo mount -o remount,ro /root_bypass_ramdisks
 	sudo service odoo restart
 }
 
