f3986b71fcdfafe400091592a12506ca03372206
Josse Colpaert <jco@odoo.com>
2018-07-24 10:23:46 +0200

    [IMP] iot: add url to box for startup page on box

--- addons/iot/models/iot_box.py
+++ addons/iot/models/iot_box.py
@@ -11,3 +11,4 @@ class IotBox(models.Model):
     identifier = fields.Char(string='Serial Number (Mac Address)')
     device_ids = fields.One2many('iot.device', 'iot_id', string="devices")
     ip = fields.Char('IP Address')
+    url = fields.Char('Start URL', help="When the IoTBox browser loads after boot, it will load it with this url instead of the config page. ")
