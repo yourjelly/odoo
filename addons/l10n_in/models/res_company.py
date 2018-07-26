8b342c95175513ce3723b64fc2ac652bd7884198
Jigar Vaghela <jva@odoo.com>
2018-07-26 17:03:35 +0530

    [IMP] l10n_in: Multi GSTIN feature only show if company country_id is India.

--- addons/l10n_in/models/res_company.py
+++ addons/l10n_in/models/res_company.py
@@ -8,13 +8,5 @@ class ResCompany(models.Model):
     _inherit = "res.company"
 
     #for Multi GSTIN.
+    l10n_in_country_code = fields.Char(string="Country code", related='country_id.code')
     l10n_in_gstin_partner_ids = fields.One2many('res.partner', 'l10n_in_gstin_company_id', string="GST")
-
-    @api.model
-    def create(self, vals):
-        """update company partner in l10n_in_gstin_partner_ids relation.
-        For use in domain of l10n_in_gstin_partner_id.
-        """
-        company = super(ResCompany, self).create(vals)
-        company.partner_id.write({'l10n_in_gstin_company_id': company.id})
-        return company
