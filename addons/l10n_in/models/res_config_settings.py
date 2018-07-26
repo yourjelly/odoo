5e387b759ec16b4c4152023583dc5778f0e5c92b
Jigar Vaghela <jva@odoo.com>
2018-07-26 17:03:35 +0530

    [IMP] l10n_in: Multi GSTIN feature only show if company country_id is India.

--- addons/l10n_in/models/res_config_settings.py
+++ addons/l10n_in/models/res_config_settings.py
@@ -7,7 +7,12 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    group_l10n_in_multi_gstin = fields.Boolean(string="Multi GSTIN", implied_group='l10n_in.group_l10n_in_multi_gstin')
+    group_l10n_in_multi_gstin = fields.Boolean(implied_group='l10n_in.group_l10n_in_multi_gstin', string="Multiple GSTIN registered")
+    show_module_l10n_in = fields.Boolean(compute='_compute_show_module_l10n_in')
+
+    @api.depends('company_id')
+    def _compute_show_module_l10n_in(self):
+        self.show_module_l10n_in = self.company_id.country_id.code == 'IN'
 
     @api.multi
     def set_values(self):
@@ -15,5 +20,5 @@ class ResConfigSettings(models.TransientModel):
         For use in domain of l10n_in_gstin_partner_id"""
         super(ResConfigSettings, self).set_values()
         if self.group_l10n_in_multi_gstin:
-            for company in self.env['res.company'].search([]):
+            for company in self.env['res.company'].search([('country_id.code','=', 'IN')]):
                 company.partner_id.write({'l10n_in_gstin_company_id': company.id})
