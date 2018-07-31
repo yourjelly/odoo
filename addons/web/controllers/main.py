2373ff3524c3073c1da5ae0c093bf75ca30c60bc
William Henrotin <whe@odoo.com>
2018-07-31 13:53:09 +0200

    [IMP] base: Text type report
    The goal of this task is to produce plain text reports. Those reports
    will be downloaded directly from the web client like PDF ones. They
    support qweb syntax.Plain text reports act as 'special cases' outputs. Printing custom text
    with Odoo data for instance. Another example is printing a ZPL script to pass
    to a label Zebra termal printer.
    
    This commit adds a new ir.action.report type ('other'). The  render
    engine call the html render in case of text then create a file and download it.
    
    task ID : 1837175

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -1575,6 +1575,10 @@ class ReportController(http.Controller):
             pdf = report.with_context(context).render_qweb_pdf(docids, data=data)[0]
             pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', len(pdf))]
             return request.make_response(pdf, headers=pdfhttpheaders)
+        elif converter == 'other':
+            text = report.with_context(context).render_qweb_other(docids, data=data)[0]
+            texthttpheaders = [('Content-Type', 'text/plain'), ('Content-Length', len(text))]
+            return request.make_response(text, headers=texthttpheaders)
         else:
             raise werkzeug.exceptions.HTTPException(description='Converter %s not implemented.' % converter)
 
@@ -1615,33 +1619,43 @@ class ReportController(http.Controller):
         url, type = requestcontent[0], requestcontent[1]
         try:
             if type == 'qweb-pdf':
-                reportname = url.split('/report/pdf/')[1].split('?')[0]
-
-                docids = None
-                if '/' in reportname:
-                    reportname, docids = reportname.split('/')
-
-                if docids:
-                    # Generic report:
-                    response = self.report_routes(reportname, docids=docids, converter='pdf')
-                else:
-                    # Particular report:
-                    data = url_decode(url.split('?')[1]).items()  # decoding the args represented in JSON
-                    response = self.report_routes(reportname, converter='pdf', **dict(data))
-
-                report = request.env['ir.actions.report']._get_report_from_name(reportname)
-                filename = "%s.%s" % (report.name, "pdf")
-                if docids:
-                    ids = [int(x) for x in docids.split(",")]
-                    obj = request.env[report.model].browse(ids)
-                    if report.print_report_name and not len(obj) > 1:
-                        report_name = safe_eval(report.print_report_name, {'object': obj, 'time': time})
-                        filename = "%s.%s" % (report_name, "pdf")
-                response.headers.add('Content-Disposition', content_disposition(filename))
-                response.set_cookie('fileToken', token)
-                return response
+                ext = 'pdf'
+            elif type == 'qweb-other':
+                ext = 'other'
             else:
                 return
+            reportname = url.split('/report/'+ext+'/')[1].split('?')[0]
+
+            docids = None
+            if '/' in reportname:
+                reportname, docids = reportname.split('/')
+
+            if docids:
+                # Generic report:
+                response = self.report_routes(reportname, docids=docids, converter=ext)
+            else:
+                # Particular report:
+                data = url_decode(url.split('?')[1]).items()  # decoding the args represented in JSON
+                response = self.report_routes(reportname, converter=ext, **dict(data))
+
+            report = request.env['ir.actions.report']._get_report_from_name(reportname)
+            if type == 'qweb-other':
+                filename = "%s" % (report.name,)
+            else:
+                filename = "%s.%s" % (report.name,ext)
+
+            if docids:
+                ids = [int(x) for x in docids.split(",")]
+                obj = request.env[report.model].browse(ids)
+                if report.print_report_name and not len(obj) > 1:
+                    report_name = safe_eval(report.print_report_name, {'object': obj, 'time': time})
+                    if type == 'qweb-other':
+                        filename = "%s" % (report.name,)
+                    else:
+                        filename = "%s.%s" % (report.name,ext)
+            response.headers.add('Content-Disposition', content_disposition(filename))
+            response.set_cookie('fileToken', token)
+            return response
         except Exception as e:
             se = _serialize_exception(e)
             error = {
