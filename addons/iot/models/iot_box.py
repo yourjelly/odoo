30c6b300a92d2895fbd08f7c8fb78ddb8c89c0ab
lejeune quentin <qle@odoo.com>
2018-08-06 16:56:45 +0200

    [IMP] iot : modify access of iotbox fields

--- addons/iot/models/iot_box.py
+++ addons/iot/models/iot_box.py
@@ -7,8 +7,8 @@ from odoo import api, fields, models, exceptions
 class IotBox(models.Model):
     _name = 'iot.box'
 
-    name = fields.Char('Name')
-    identifier = fields.Char(string='Serial Number (Mac Address)')
+    name = fields.Char('Name', readonly=True)
+    identifier = fields.Char(string='Serial Number (Mac Address)', readonly=True)
     device_ids = fields.One2many('iot.device', 'iot_id', string="devices")
-    ip = fields.Char('IP Address')
+    ip = fields.Char('IP Address', readonly=True)
     url = fields.Char('Start URL', help="When the IoTBox browser loads after boot, it will load it with this url instead of the config page. ")
