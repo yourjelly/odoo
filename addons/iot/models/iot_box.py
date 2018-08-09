408e4653379d04b21d989634fa40799f166728eb
Josse Colpaert <jco@odoo.com>
2018-08-09 16:16:21 +0200

    [IMP] iot: foresee authentication code field

--- addons/iot/models/iot_box.py
+++ addons/iot/models/iot_box.py
@@ -12,3 +12,4 @@ class IotBox(models.Model):
     device_ids = fields.One2many('iot.device', 'iot_id', string="devices", readonly=True)
     ip = fields.Char('IP Address', readonly=True)
     url = fields.Char('Start URL', help="When the IoTBox browser loads after boot, it will load it with this url instead of the config page. ")
+    authentication_code = fields.Char('Authentication code', readonly=True)
