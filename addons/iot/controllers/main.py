77682239451165369f63722de3244bd56f2879b8
Josse Colpaert <jco@odoo.com>
2018-08-21 14:41:40 +0200

    [IMP] iot: on par with freeze branch

--- addons/iot/controllers/main.py
+++ addons/iot/controllers/main.py
@@ -21,21 +21,22 @@ class IoTController(http.Controller):
         box = request.env['iot.box'].sudo().search([('identifier', '=', data['identifier'])])
         if box:
             box = box[0]
-            box.ip = data['iotbox']['ip']
-            box.name = data['iotbox']['name']
+            box.ip = data['ip']
+            box.name = data['name']
         else:
-            box = request.env['iot.box'].sudo().create({'name': box['name'], 'identifier': box['identifier'], 'ip': box['ip'], })
+            box = request.env['iot.box'].sudo().create({'name': data['name'], 'identifier': data['identifier'], 'ip': data['ip'], })
 
         # Update or create devices
-        for data_device in data['devices']:
-            device = request.env['iot.device'].sudo().search([('iot_id', '=', box.id), ('identifier', '=', data_device['identifier'])])
+        for device_identifier in data['devices']:
+            data_device = data['devices'][device_identifier]
+            device = request.env['iot.device'].sudo().search([('iot_id', '=', box.id), ('identifier', '=', device_identifier)])
             if device:
                 device.name = data_device['name']
             else:
                 device = request.env['iot.device'].sudo().create({
                     'iot_id': box.id,
                     'name': data_device['name'],
-                    'identifier': data_device['identifier'],
+                    'identifier': device_identifier,
                     'type': data_device['type'],
                     'connection': data_device['connection'],
                 })
--- addons/iot/static/src/js/iot.js
+++ addons/iot/static/src/js/iot.js
@@ -68,7 +68,7 @@ ActionManager.include({
             self.action=action;
             this._rpc({model: 'ir.actions.report',
                        method: 'iot_render',
-                       args: [action.id, action.context.active_ids, action]
+                       args: [action.id, action.context.active_ids, {'device_id': action.device_id}]
                       }).then(function (result) {
                         var data = {action: 'print',
                                     type: result[1],
