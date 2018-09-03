98c2c67ad57fb5a259c04acf2e31a1a2b2a92d10
lejeune quentin <qle@odoo.com>
2018-09-03 15:41:25 +0200

    [IMP] iot : Create dedicated folder fo drivers 3

--- addons/hw_drivers/controllers/driver.py
+++ addons/hw_drivers/controllers/driver.py
@@ -10,6 +10,7 @@ import subprocess
 import netifaces as ni
 import json
 import re
+import os
 from odoo import http
 import urllib3
 from odoo.http import request as httprequest
@@ -139,36 +140,10 @@ class USBDriver(Driver,metaclass=UsbMetaClass):
     def value(self):
         return self.value
 
+driversList = os.listdir("addons/hw_drivers/drivers")
+for driver in driverList:
+    from ..drivers import driver
 
-class SylvacUSBDriver(USBDriver):
-
-    def supported(self):
-        return getattr(self.dev, 'idVendor') == 0x0403 and getattr(self.dev, 'idProduct') == 0x6001
-
-    def run(self):
-        connection = serial.Serial('/dev/serial/by-id/usb-Sylvac_Power_USB_A32DV5VM-if00-port0',
-                                   baudrate=4800,
-                                   bytesize=7,
-                                   stopbits=2,
-                                   parity=serial.PARITY_EVEN)
-        measure = b''
-        no_except = True
-        while no_except:
-            try:
-                char = connection.read(1)
-                if ord(char) == 13:
-                    # Let's send measure
-                    self.value = measure.decode("utf-8")
-                    measure = b''
-                else:
-                    measure += char
-            except:
-                no_except = False
-
-    def action(self, action):
-        pass
-
-from ..drivers import KeyboardUSBDriver
 
 class BarcodeScannerdUSBDriver(USBDriver):
 
