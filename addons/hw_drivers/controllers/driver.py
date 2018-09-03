fee02dc43c374253a1565ce62dc728f3f50fe461
lejeune quentin <qle@odoo.com>
2018-09-03 14:58:49 +0200

    [IMP] iot : Create dedicated folder fo drivers

--- addons/hw_drivers/controllers/driver.py
+++ addons/hw_drivers/controllers/driver.py
@@ -168,36 +168,7 @@ class SylvacUSBDriver(USBDriver):
     def action(self, action):
         pass
 
-class KeyboardUSBDriver(USBDriver):
-
-    def supported(self):
-        return getattr(self.dev, 'idVendor') == 0x046d and getattr(self.dev, 'idProduct') == 0xc31c
-
-    def value(self):
-        return self.value
-
-    def run(self):
-        devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-        for device in devices:
-            if ('Keyboard' in device.name) & ('input0' in device.phys):
-                path = device.path
-
-        device = evdev.InputDevice(path)
-
-        for event in device.read_loop():
-            if event.type == evdev.ecodes.EV_KEY:
-                data = evdev.categorize(event)
-                if data.scancode == 96:
-                    return {}
-                elif data.scancode == 28:
-                    self.value = ''
-                elif data.keystate:
-                    self.value += data.keycode.replace('KEY_','')
-
-    def action(self, action):
-        pass
-
-from ..drivers import hello
+from ..drivers import KeyboardUSBDriver
 
 class BarcodeScannerdUSBDriver(USBDriver):
 
@@ -230,8 +201,6 @@ class BarcodeScannerdUSBDriver(USBDriver):
 
     def action(self, action):
         pass
-
-import /drivers/hello.py
 
 class USBDeviceManager(Thread):
     devices = {}
---
+++ addons/hw_drivers/drivers/KeyboardUSBDriver.py
@@ -0,0 +1,28 @@
+class KeyboardUSBDriver(USBDriver):
+
+    def supported(self):
+        return getattr(self.dev, 'idVendor') == 0x046d and getattr(self.dev, 'idProduct') == 0xc31c
+
+    def value(self):
+        return self.value
+
+    def run(self):
+        devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+        for device in devices:
+            if ('Keyboard' in device.name) & ('input0' in device.phys):
+                path = device.path
+
+        device = evdev.InputDevice(path)
+
+        for event in device.read_loop():
+            if event.type == evdev.ecodes.EV_KEY:
+                data = evdev.categorize(event)
+                if data.scancode == 96:
+                    return {}
+                elif data.scancode == 28:
+                    self.value = ''
+                elif data.keystate:
+                    self.value += data.keycode.replace('KEY_','')
+
+    def action(self, action):
+        pass
