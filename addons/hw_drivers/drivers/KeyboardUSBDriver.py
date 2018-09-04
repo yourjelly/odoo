02ac3a033241c349a6ad5caaaa197b9356baf28f
lejeune quentin <qle@odoo.com>
2018-09-04 11:58:50 +0200

    [IMP] iot : Create dedicated folder fo drivers 10

--- addons/hw_drivers/drivers/KeyboardUSBDriver.py
+++ addons/hw_drivers/drivers/KeyboardUSBDriver.py
@@ -1,58 +1,5 @@
 import evdev
-from threading import Thread
-
-#from ..controllers.driver import USBDriver
-
-class Driver(Thread):
-#    def __init__(self, path):
-#        pass
-
-    def supported(self):
-        pass
-
-    def value(self):
-        pass
-
-    def get_name(self):
-        pass
-
-    def get_connection(self):
-        pass
-
-    def action(self, action):
-        pass
-
-#----------------------------------------------------------
-# Usb drivers
-#----------------------------------------------------------
-usbdrivers = []
-
-class UsbMetaClass(type):
-    def __new__(cls, clsname, bases, attrs):
-        newclass = super(UsbMetaClass, cls).__new__(cls, clsname, bases, attrs)
-        usbdrivers.append(newclass)
-        return newclass
-
-
-class USBDriver(Driver,metaclass=UsbMetaClass):
-    def __init__(self, dev):
-        super(USBDriver, self).__init__()
-        self.dev = dev
-        self.value = ""
-
-    def get_name(self):
-        lsusb = str(subprocess.check_output('lsusb')).split("\\n")
-        for usbpath in lsusb:  # Should filter on usb devices or inverse loops?
-            device = self.dev
-            if "%04x:%04x" % (device.idVendor, device.idProduct) in usbpath:
-                return usbpath.split("%04x:%04x" % (device.idVendor, device.idProduct))[1]
-        return str(device.idVendor) + ":" + str(device.idProduct)
-
-    def get_connection(self):
-        return 'direct'
-
-    def value(self):
-        return self.value
+from ..controllers.driver import USBDriver
 
 class KeyboardUSBDriver(USBDriver):
 
