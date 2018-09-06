ac070831aad7283e9e3baeee8270fe7e3355ab1a
lejeune quentin <qle@odoo.com>
2018-09-06 11:11:25 +0200

    [IMP] iot : Load distant drivers 4

--- addons/hw_drivers/controllers/load_drivers.py
+++ addons/hw_drivers/controllers/load_drivers.py
@@ -14,17 +14,17 @@ class load_uuid():
     except: #In case the file does not exist
         server=''
     if server:
-	    server = server.split('\n')[0]
-		url = server + '/iot/get_db_uuid'
-		data['mac_address'] = maciotbox
-		data['token']='token'
-		data_json = json.dumps(data).encode('utf8')
-	    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-	    http = urllib3.PoolManager()
-	    try:
-	        req = http.request('POST',
-	                            url,
-	                            body=data_json,
-	                            headers=headers)
-	    except:
-	        logger.warning('Could not reach configured server')
+        server = server.split('\n')[0]
+        url = server + '/iot/get_db_uuid'
+        data['mac_address'] = maciotbox
+        data['token']='token'
+        data_json = json.dumps(data).encode('utf8')
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        http = urllib3.PoolManager()
+        try:
+            req = http.request('POST',
+                                url,
+                                body=data_json,
+                                headers=headers)
+        except:
+            logger.warning('Could not reach configured server')
