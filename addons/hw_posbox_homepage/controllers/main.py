608b83249f7420e4a1e621b79075e4e7f62fa277
Deep Patel <dep@odoo.com>
2018-09-07 13:01:42 +0530

    improve load driver button

--- addons/hw_posbox_homepage/controllers/main.py
+++ addons/hw_posbox_homepage/controllers/main.py
@@ -273,7 +273,7 @@ def get_homepage_html(data):
                     </tr>
                     <tr>
                         <td class="heading">IOT Device</td>
-                        <td>""" + get_iot_device_status_html() + """ <a class="float-right" href='/list_drivers'>drivers list</a></td>
+                        <td>""" + get_iot_device_status_html() + """ <a class="btn btn-sm float-right" href='/list_drivers'>drivers list</a></td>
                     </tr>
                 </table>
                 <div style="margin: 20px auto 10px auto;" class="text-center">
