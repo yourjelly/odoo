f31a9630c23ade9a767e177b0af0a95f564d782c
lejeune quentin <qle@odoo.com>
2018-09-07 23:27:06 +0200

    [IMP] iot : Step form redirect 3

--- addons/hw_posbox_homepage/controllers/main.py
+++ addons/hw_posbox_homepage/controllers/main.py
@@ -124,14 +124,14 @@ class IoTboxHomepage(odoo.addons.web.controllers.main.Home):
     @http.route('/', type='http', auth='none')
     def index(self):
         if os.path.isfile('/home/pi/wifi_network.txt') and os.path.isfile('/home/pi/odoo-remote-server.conf'):
+            return homepage_template.render(self.get_homepage_data())
+        else:
             return configure_wizard_template.render({
                 'title': 'Configure IoT Box',
                 'breadcrumb': 'Configure IoTBox',
                 'loading_message': 'Configuring your IoT Box',
                 'ssid': self.get_wifi_essid(),
                 })
-        else:
-            return homepage_template.render(self.get_homepage_data())
 
     @http.route('/list_drivers', type='http', auth='none', website=True)
     def list_drivers(self):
