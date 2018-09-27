35f39cbeb61a2ba65e2a2b1a47cd063dc5802659
Tejas Shahu <tsh@odoo.com>
2018-09-27 19:09:21 +0530

    [IMP]web: Now user can navigate on formview's save button using SHIFT+TAB while formview is opened in edit mode

--- addons/web/static/src/js/views/form/form_renderer.js
+++ addons/web/static/src/js/views/form/form_renderer.js
@@ -278,6 +278,32 @@ var FormRenderer = BasicRenderer.extend({
         }
         return this.lastActivatedFieldIndex;
     },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+    /**
+     * @override
+     */
+    _activatePreviousFieldWidget: function (record, currentIndex) {
+        //if we are at the first widget, we should give the focus to the first Primary Button in the form
+        //else do the default behavior
+        if ( (currentIndex - 1) < 0 ) {
+            this.trigger_up('focus_control_button');
+            this.lastActivatedFieldIndex = -1;
+        }
+        else {
+            var activatedIndex =  this._super.apply(this, arguments);
+            if (activatedIndex === -1 ) { // no widget have been activated, we should go to the edit/save buttons
+                this.trigger_up('focus_control_button');
+                this.lastActivatedFieldIndex = -1;
+            }
+            else {
+                this.lastActivatedFieldIndex = activatedIndex;
+            }
+        }
+        return this.lastActivatedFieldIndex;
+    },
     /**
      * Add a tooltip on a button
      *
