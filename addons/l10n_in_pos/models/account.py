55f930d26a54cdf7274519acccd4985c61d3d6d8
Jigar Vaghela <jva@odoo.com>
2018-09-28 19:48:36 +0530

    [IMP] l10n_in_pos: Improve pos method.

--- addons/l10n_in_pos/models/account.py
+++ addons/l10n_in_pos/models/account.py
@@ -8,3 +8,25 @@ class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
     l10n_in_pos_order_id = fields.Many2one('pos.order', string="POS order")
+
+
+class AccountBankStatementLine(models.Model):
+    _inherit = "account.bank.statement.line"
+
+    def _prepare_reconciliation_move(self, move_ref):
+        pos_config = self.statement_id.pos_session_id.config_id
+        res = super(AccountBankStatementLine, self)._prepare_reconciliation_move(move_ref)
+        res.update({
+            'l10n_in_gstin_partner_id': pos_config.l10n_in_gstin_partner_id.id,
+            'l10n_in_place_of_supply': pos_config.l10n_in_place_of_supply.id
+        })
+        return res
+
+    def _prepare_payment_vals(self, total):
+        vals = super(AccountBankStatementLine, self)._prepare_payment_vals(total)
+        pos_config = self.statement_id.pos_session_id.config_id
+        vals.update({
+            'l10n_in_gstin_partner_id': pos_config.l10n_in_gstin_partner_id.id,
+            'l10n_in_place_of_supply': pos_config.l10n_in_place_of_supply.id
+        })
+        return vals
