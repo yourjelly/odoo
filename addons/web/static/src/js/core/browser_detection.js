d050193c5d799725773186c2edc7af21b574db1d
TWA <twa@odoo.com>
2018-10-12 11:17:53 +0200

    [IMP] web: improve browser detection

--- addons/web/static/src/js/core/browser_detection.js
+++ addons/web/static/src/js/core/browser_detection.js
@@ -12,9 +12,21 @@ odoo.define('web.BrowserDetection', function (require) {
         isBrowserChrome: function () {
             return $.browser.chrome && // depends on jquery 1.x, removed in jquery 2 and above
                 navigator.userAgent.toLocaleLowerCase().indexOf('edge') === -1; // as far as jquery is concerned, Edge is chrome
+        },
+        getBrowser: function (){
+            var agent = window.navigator.userAgent.toLowerCase();
+            switch (true){
+                case agent.indexOf("edge") > -1: return "edge";
+                case agent.indexOf("opr") > -1 && !!window.opr: return "opera";
+                case agent.indexOf("chrome") > -1 && !!window.chrome: return "chrome";
+                case agent.indexOf("trident") > -1: return "ie";
+                case agent.indexOf("firefox") > -1: return "firefox";
+                case agent.indexOf("safari") > -1: return "safari";
+                default: return "other";
             }
-
+        },
     });
+
     return BrowserDetection;
 });
 
