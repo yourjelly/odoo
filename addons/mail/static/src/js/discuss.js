1ae156bea189ea161a31425256aca683271448a8
Tejas Shahu <tsh@odoo.com>
2018-11-21 15:32:03 +0530

    [ADD]Discuss: Default focus has been set on searchview

--- addons/mail/static/src/js/discuss.js
+++ addons/mail/static/src/js/discuss.js
@@ -312,6 +312,9 @@ var Discuss = AbstractAction.extend(ControlPanelMixin, {
             this._threadWidget.scrollToPosition(this._threadsScrolltop[this._thread.getID()]);
         }
         this._loadEnoughMessages();
+        if(this.searchview){
+            this.searchview.$('input').focus();
+        }
     },
     /**
      * @override
