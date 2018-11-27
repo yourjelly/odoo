a40e8eb4f8068133c2fe6a96f209e88089bb342c
jpr-odoo <jpr@openerp.com>
2018-11-27 10:32:45 +0530

    WIP

--- addons/mrp/controllers/main.py
+++ addons/mrp/controllers/main.py
@@ -26,59 +26,6 @@ class GoogleSlides(http.Controller):
         }
         return request.render('mrp.slide_view', values)
 
-
-
-    # @http.route(['/slides/add_slide'], type='json', auth='user', methods=['POST'])
-    # def create_slide(self, *args, **post):
-    #     # check the size only when we upload a file.
-    #     if post.get('datas'):
-    #         file_size = len(post['datas']) * 3 / 4 # base64
-    #         if (file_size / 1024.0 / 1024.0) > 25:
-    #             return {'error': _('File is too big. File size cannot exceed 25MB')}
-    #
-    #     values = dict((fname, post[fname]) for fname in [
-    #         'name', 'url', 'mime_type', 'datas', 'index_content'] if post.get(fname))
-    #     if post.get('category_id'):
-    #         if post['category_id'][0] == 0:
-    #             values['category_id'] = request.env['slide.category'].create({
-    #                 'name': post['category_id'][1]['name'],
-    #                 'channel_id': values.get('channel_id')}).id
-    #         else:
-    #             values['category_id'] = post['category_id'][0]
-    #
-    #     # handle exception during creation of slide and sent error notification to the client
-    #     # otherwise client slide create dialog box continue processing even server fail to create a slide.
-    #     try:
-    #         slide_id = request.env['slide.slide'].create(values)
-    #     except (UserError, AccessError) as e:
-    #         _logger.error(e)
-    #         return {'error': e.name}
-    #     except Exception as e:
-    #         _logger.error(e)
-    #         return {'error': _('Internal server error, please try again later or contact administrator.\nHere is the error message: %s') % e}
-    #     return {'url': "/slides/slide/%s" % (slide_id.id)}
-
-    # @http.route(['/slides/validate/url'], type='json', auth='user', methods=['POST'])
-    # def validate_url(self, **data):
-    #     MrpSlide = request.env['mrp.slide.show']
-    #     document_type, document_id = MrpSlide._find_document_data_from_url(data['url'])
-    #     preview = {}
-    #     if not document_id:
-    #         preview['error'] = _('Please enter valid youtube or google doc url')
-    #         return preview
-    #     existing_slide = MrpSlide.search([('res_id', '=', int(data['id'])), ('document_id', '=', document_id)], limit=1)
-    #     if existing_slide:
-    #         preview['slide'] = existing_slide
-    #         return preview
-    #     values = MrpSlide._parse_document_url(data['url'])
-    #     if values.get('error'):
-    #         preview['error'] = _('Could not fetch data from url. Document or access right not available.\nHere is the received response: %s') % values['error']
-    #         return preview
-    #     return values
-
-    # --------------------------------------------------
-    # EMBED IN THIRD PARTY WEBSITES
-    # --------------------------------------------------
     @http.route('''/slides/slide/pdf_content''', type='http', auth="user")
     def slide_get_pdf_content(self, **kw):
         response = werkzeug.wrappers.Response()
@@ -93,11 +40,6 @@ class GoogleSlides(http.Controller):
 
     @http.route('/slides/embed', type='http', auth='user')
     def slides_embed(self, page="1", **kw):
-        # Note : don't use the 'model' in the route (use 'slide_id'), otherwise if public cannot access the embedded
-        # slide, the error will be the website.403 page instead of the one of the website_slides.embed_slide.
-        # Do not forget the rendering here will be displayed in the embedded iframe
-
-        # determine if it is embedded from external web page
         referrer_url = request.httprequest.headers.get('Referer', '')
         base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
         is_embedded = referrer_url and not bool(base_url in referrer_url) or False
@@ -107,7 +49,6 @@ class GoogleSlides(http.Controller):
             slide = request.env[model_info.get('model')].browse(int(model_info.get('id')))
             if is_embedded:
                 request.env['slide.embed'].sudo().add_embed_url(slide.id, referrer_url)
-            # values = self._get_slide_detail(slide)
             values = {
                 'slide': slide,
                 'page': page,
@@ -115,7 +56,6 @@ class GoogleSlides(http.Controller):
                 'model_info': model_info,
             }
             return request.render('mrp.embed_slide', values)
-        except AccessError: # TODO : please, make it clean one day, or find another secure way to detect
-                            # if the slide can be embedded, and properly display the error message.
+        except AccessError:
             slide = request.env[model_info.get('model')].sudo().browse(int(model_info.get('id')))
             return request.render('google_slide.embed_slide_forbidden', {'slide': slide})
--- addons/mrp/models/mrp_routing.py
+++ addons/mrp/models/mrp_routing.py
@@ -122,28 +122,17 @@ class MrpRoutingWorkcenter(models.Model):
             if record.worksheet:
                 slide_url = base_url + url_for('/slides/embed/%s?page=1' % record.id)
                 record.embed_code = '<iframe src="%s" class="o_wslides_iframe_viewer" allowFullScreen="true" height="%s" width="%s" frameborder="0"></iframe>' % (slide_url, 315, 420)
-            # elif record.slide_type == 'video' and record.document_id:
-            #     if not record.mime_type:
-            #         # embed youtube video
-            #         record.embed_code = '<iframe src="//www.youtube.com/embed/%s?theme=light" allowFullScreen="true" frameborder="0"></iframe>' % (record.document_id)
-            #     else:
-            #         # embed google doc video
-            #         record.embed_code = '<iframe src="//drive.google.com/file/d/%s/preview" allowFullScreen="true" frameborder="0"></iframe>' % (record.document_id)
+            elif record.slide_type == 'video' and record.document_id:
+                #  TODO: fix me
+                if not record.mime_type:
+                    # embed youtube video
+                    record.embed_code = '<iframe src="//www.youtube.com/embed/%s?theme=light" allowFullScreen="true" frameborder="0"></iframe>' % (record.document_id)
+                else:
+                    # embed google doc video
+                    record.embed_code = '<iframe src="//drive.google.com/file/d/%s/preview" allowFullScreen="true" frameborder="0"></iframe>' % (record.document_id)
             else:
                 record.embed_code = False
 
-    # @api.onchange('url')
-    # def on_change_url(self):
-    #     self.ensure_one()
-    #     if self.url:
-    #         res = self._parse_document_url(self.url)
-    #         if res.get('error'):
-    #             raise Warning(_('Could not fetch data from url. Document or access right not available:\n%s') % res['error'])
-    #         values = res['values']
-    #         if not values.get('document_id'):
-    #             raise Warning(_('Please enter valid Youtube or Google Doc URL'))
-    #         for key, value in values.items():
-    #             self[key] = value
 
     @api.model
     def _fetch_data(self, base_url, data, content_type=False, extra_params=False):
@@ -164,11 +153,6 @@ class MrpRoutingWorkcenter(models.Model):
         return result
 
     def _find_document_data_from_url(self, url):
-        # expr = re.compile(r'^.*((youtu.be/)|(v/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*')
-        # arg = expr.match(url)
-        # document_id = arg and arg.group(7) or False
-        # if document_id:
-        #     return ('youtube', document_id)
 
         expr = re.compile(r'(^https:\/\/docs.google.com|^https:\/\/drive.google.com).*\/d\/([^\/]*)')
         arg = expr.match(url)
@@ -186,61 +170,30 @@ class MrpRoutingWorkcenter(models.Model):
 
     @api.model
     def _parse_google_document(self, document_id, only_preview_fields):
-        # def get_slide_type(vals):
-        #     # TDE FIXME: WTF ??
-        #     slide_type = 'presentation'
-        #     if vals.get('image'):
-        #         image = Image.open(io.BytesIO(base64.b64decode(vals['image'])))
-        #         width, height = image.size
-        #         if height > width:
-        #             return 'document'
-        #     return slide_type
-
-        # Google drive doesn't use a simple API key to access the data, but requires an access
-        # token. However, this token is generated in module google_drive, which is not in the
-        # dependencies of website_slides. We still keep the 'key' parameter just in case, but that
-        # is probably useless.
         params = {}
         params['projection'] = 'BASIC'
         if 'google.drive.config' in self.env:
             access_token = self.env['google.drive.config'].get_access_token()
             if access_token:
                 params['access_token'] = access_token
-        # if not params.get('access_token'):
-        #     params['key'] = self.env['website'].get_current_website().website_slide_google_app_key
 
         fetch_res = self._fetch_data('https://www.googleapis.com/drive/v2/files/%s' % document_id, params, "json")
         if fetch_res.get('error'):
             return fetch_res
 
         google_values = fetch_res['values']
-        # if only_preview_fields:
-        #     return {
-        #         'url_src': google_values['thumbnailLink'],
-        #         'title': google_values['title'],
-        #     }
-
         values = {
             'mime_type': google_values['mimeType'],
             'document_id': document_id,
         }
-        # if google_values['mimeType'].startswith('video/'):
-        #     values['slide_type'] = 'video'
         if google_values['mimeType'].startswith('image/'):
             values['worksheet'] = values['image']
-            # values['slide_type'] = 'infographic'
         elif google_values['mimeType'].startswith('application/vnd.google-apps'):
             if 'exportLinks' in google_values:
                 values['worksheet'] = self._fetch_data(google_values['exportLinks']['application/pdf'], params, 'pdf', extra_params=True)['values']
-                # Content indexing
-                # if google_values['exportLinks'].get('text/plain'):
-                #     values['index_content'] = self._fetch_data(google_values['exportLinks']['text/plain'], params, extra_params=True)['values']
-                # elif google_values['exportLinks'].get('text/csv'):
-                #     values['index_content'] = self._fetch_data(google_values['exportLinks']['text/csv'], params, extra_params=True)['values']
         elif google_values['mimeType'] == 'application/pdf':
             # TODO: Google Drive PDF document doesn't provide plain text transcript
             values['worksheet'] = self._fetch_data(google_values['webContentLink'], {}, 'pdf')['values']
-            # values['slide_type'] = get_slide_type(values)
 
         return {'values': values}
 
@@ -252,11 +205,7 @@ class MrpRoutingWorkcenter(models.Model):
                 raise Warning(_('Could not fetch data from url. Document or access right not available:\n%s') % doc_data['error'])
             if not doc_data.get('document_id'):
                 raise Warning(_('Please enter valid Google Slide URL'))
-            # for key, value in doc_data.items():
-            if(doc_data.get('worksheet')):
-                values['worksheet'] = doc_data.get('worksheet')
-                # values.setdefault(key, value)
-        print('sdsd', values)
+            values['worksheet'] = doc_data.get('worksheet')
         return super(MrpRoutingWorkcenter, self).create(values)
 
     @api.multi
