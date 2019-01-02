d39a5a9bad389bed29d6b1c13185390a1e9e0158
Martin Trigaux <mat@odoo.com>
2019-01-02 14:55:07 +0100

    [FIX] tools: properly verify the size of HTML nodes
    Instead of checking the size of text only node, let the xml_translate verify it.
    
    The size verification using etree was introduced at 40efdccc3d1e1f070c9 but was
    probably too simple.
    Since 45352512a842, this becomes redundant with the nonspace method which can
    be extended.

--- odoo/tools/translate.py
+++ odoo/tools/translate.py
@@ -162,7 +162,10 @@ def translate_xml_node(node, callback, parse, serialize):
     """
 
     def nonspace(text):
-        return bool(text) and not text.isspace()
+        sanitized_text = text and re.sub(r'\W+', '', text)
+        res = bool(sanitized_text) and not sanitized_text.isspace() and len(sanitized_text) > 1
+        return res
+
 
     def concat(text1, text2):
         return text2 if text1 is None else text1 + (text2 or "")
@@ -814,16 +817,6 @@ def trans_generate(lang, modules, cr):
         # empty and one-letter terms are ignored, they probably are not meant to be
         # translated, and would be very hard to translate anyway.
         sanitized_term = (source or '').strip()
-        try:
-            # verify the minimal size without eventual xml tags
-            # wrap to make sure html content like '<a>b</a><c>d</c>' is accepted by lxml
-            wrapped = u"<div>%s</div>" % sanitized_term
-            node = etree.fromstring(wrapped)
-            sanitized_term = etree.tostring(node, encoding='unicode', method='text')
-        except etree.ParseError:
-            pass
-        # remove non-alphanumeric chars
-        sanitized_term = re.sub(r'\W+', '', sanitized_term)
         if not sanitized_term or len(sanitized_term) <= 1:
             return
 
