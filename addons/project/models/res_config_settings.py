88cc77b07c5a2af7d22e7f9edec878d193452480
Hetal Dhanak <hdh@odoo.com>
2019-01-17 15:17:37 +0530

    [IMP] project: move fields/buttons 	-Moved the share Button to the dropdown Action menu as it was better suitable there. 	-moved sale order item field to extra info as it was better suitable there. 	-changed display of settings for project to give it organized look. This commit is related to task id: 1928422

--- addons/project/models/res_config_settings.py
+++ addons/project/models/res_config_settings.py
@@ -8,6 +8,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     module_project_forecast = fields.Boolean(string="Forecasts")
-    module_hr_timesheet = fields.Boolean(string="Task Logs")
+    module_hr_timesheet = fields.Boolean(string="Timesheet")
     group_subtask_project = fields.Boolean("Sub-tasks", implied_group="project.group_subtask_project")
     group_project_rating = fields.Boolean("Use Rating on Project", implied_group='project.group_project_rating')
