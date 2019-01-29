ce3b3ad561b81e0f34fbf902dfc5648bdbacae80
Ravi Gadhia <rga@odoo.com>
2019-01-29 17:38:00 +0530

    [IMP] make imported file editable

--- addons/base_import/controllers/main.py
+++ addons/base_import/controllers/main.py
@@ -13,11 +13,5 @@ class ImportController(http.Controller):
     @http.route('/base_import/set_file', methods=['POST'])
     def set_file(self, file, import_id, jsonp='callback'):
         import_id = int(import_id)
-
-        written = request.env['base_import.import'].browse(import_id).write({
-            'file': file.read(),
-            'file_name': file.filename,
-            'file_type': file.content_type,
-        })
-
+        written = request.env['base_import.import'].browse(import_id).set_file(file)
         return 'window.top.%s(%s)' % (misc.html_escape(jsonp), json.dumps({'result': written}))
