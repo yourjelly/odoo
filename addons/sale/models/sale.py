f183e77861f3774f665a065924ab2439b716f93c
Jinal Patel <jip@odoo.com>
2019-02-08 15:24:04 +0530

    [IMP] sale: Do not reset unit price

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -1454,17 +1454,19 @@ class SaleOrderLine(models.Model):
         if not self.product_uom or not self.product_id:
             self.price_unit = 0.0
             return
-        if self.order_id.pricelist_id and self.order_id.partner_id:
-            product = self.product_id.with_context(
-                lang=self.order_id.partner_id.lang,
-                partner=self.order_id.partner_id,
-                quantity=self.product_uom_qty,
-                date=self.order_id.date_order,
-                pricelist=self.order_id.pricelist_id.id,
-                uom=self.product_uom.id,
-                fiscal_position=self.env.context.get('fiscal_position')
-            )
-            self.price_unit = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
+        for item in self.order_id.pricelist_id.item_ids:
+            if item.min_quantity <= self.product_uom_qty and item.min_quantity != 0:
+                if self.order_id.pricelist_id and self.order_id.partner_id:
+                    product = self.product_id.with_context(
+                        lang=self.order_id.partner_id.lang,
+                        partner=self.order_id.partner_id,
+                        quantity=self.product_uom_qty,
+                        date=self.order_id.date_order,
+                        pricelist=self.order_id.pricelist_id.id,
+                        uom=self.product_uom.id,
+                        fiscal_position=self.env.context.get('fiscal_position')
+                    )
+                    self.price_unit = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
 
     @api.multi
     def name_get(self):
