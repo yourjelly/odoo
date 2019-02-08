235070876ff05590a0aeb7edf0a1d47390728c8d
Arnaud Baes <arb@odoo.com>
2019-02-08 16:42:38 +0100

    [ADD] mrp: Subcontracting locations

--- addons/mrp/models/res_partner.py
+++ addons/mrp/models/res_partner.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models, _
+from odoo import fields, models, _, api
 
 
 class Partner(models.Model):
@@ -16,3 +16,18 @@ class Partner(models.Model):
             if not contact_name.strip():
                 name += _('Subcontractor')
         return name
+
+    @api.onchange('type')
+    def _onchange_partner_type(self):
+        company = self.company_id or self.parent_id.company_id
+        subcontracting_location = company.subcontracting_location_id
+        if self.type == 'subcontractor':
+            self.update({
+                'property_stock_customer': subcontracting_location,
+                'property_stock_supplier': subcontracting_location
+            })
+        else:
+            self.update({
+                'property_stock_customer': self.env.ref('stock.stock_location_customers'),
+                'property_stock_supplier': self.env.ref('stock.stock_location_suppliers')
+            })
--- addons/stock/models/res_company.py
+++ addons/stock/models/res_company.py
@@ -11,6 +11,7 @@ class Company(models.Model):
     internal_transit_location_id = fields.Many2one(
         'stock.location', 'Internal Transit Location', on_delete="restrict",
         help="Technical field used for resupply routes between warehouses that belong to this company")
+    subcontracting_location_id = fields.Many2one('stock.location')
 
     def _create_transit_location(self):
         '''Create a transit location with company_id being the given company_id. This is needed
@@ -78,6 +79,19 @@ class Company(models.Model):
                 'scrap_location': True,
             })
 
+    def _create_subcontracting_location(self):
+        for company in self:
+            parent_location = self.env.ref('stock.stock_location_locations_partner', raise_if_not_found=False)
+            subcontracting_location = self.env['stock.location'].create({
+                'name': _('%s: Subcontracting Location') % company.name,
+                'usage': 'internal',
+                'location_id': parent_location.id,
+                'company_id': company.id,
+                'active': False
+            })
+
+            company.write({'subcontracting_location_id': subcontracting_location.id})
+
     @api.model
     def create_missing_warehouse(self):
         """ This hook is used to add a warehouse on existing companies
@@ -120,19 +134,26 @@ class Company(models.Model):
 
     @api.model
     def create_missing_scrap_location(self):
-        company_ids  = self.env['res.company'].search([])
+        company_ids = self.env['res.company'].search([])
         companies_having_scrap_loc = self.env['stock.location'].search([('scrap_location', '=', True)]).mapped('company_id')
         company_without_property = company_ids - companies_having_scrap_loc
         for company in company_without_property:
             company._create_scrap_location()
 
     @api.model
+    def create_missing_subcontracting_location(self):
+        company_without_subcontracting_loc = self.env['res.company'].search(
+            [('subcontracting_location_id', '=', False)])
+        for company in company_without_subcontracting_loc:
+            company._create_subcontracting_location()
+
+    @api.model
     def create(self, vals):
         company = super(Company, self).create(vals)
-
         company.sudo()._create_transit_location()
         company.sudo()._create_inventory_loss_location()
         company.sudo()._create_production_location()
         company.sudo()._create_scrap_location()
+        company.sudo()._create_subcontracting_location()
         self.env['stock.warehouse'].sudo().create({'name': company.name, 'code': company.name[:5], 'company_id': company.id, 'partner_id': company.partner_id.id})
         return company
