e3f06a31e8b267e8e641e40a2cdfef49ac7676f6
Sébastien Theys <seb@odoo.com>
2019-02-13 16:25:56 +0000

    -- waiting forward-port of this --
    [FIX] sale,website_sale: fix list view of variants price compute
    
    Before this commit, the list view of variant didn't use `_get_combination_info`
    so the displayed price was not taking into account important settings such as
    B2B/B2C and currency convert.

--- addons/website_sale/static/src/js/website_sale_tour_shop_deleted_archived_variants.js
+++ addons/website_sale/static/src/js/website_sale_tour_shop_deleted_archived_variants.js
@@ -6,12 +6,16 @@ var tour = require('web_tour.tour');
 // This tour relies on a data created from the python test.
 tour.register('tour_shop_deleted_archived_variants', {
     test: true,
-    url: '/shop?search=Test Product',
+    url: '/shop?search=Test Product 2',
 },
 [
     {
-        content: "select Test Product",
-        trigger: '.oe_product_cart a:containsExact("Test Product")',
+        content: "check price on /shop (3rd variant)",
+        trigger: '.oe_product_cart .oe_currency_value:contains("31.00")',
+    },
+    {
+        content: "select Test Product 2",
+        trigger: '.oe_product_cart a:containsExact("Test Product 2")',
     },
     {
         content: "check price (3rd variant)",
