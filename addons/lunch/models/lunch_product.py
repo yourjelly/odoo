b59bb483004de8be3edc495abc615be2d6da06cc
Raphael Collet <rco@odoo.com>
2019-02-20 11:49:52 +0100

    [FIX] *: wrong parameter 'ondelete' on one2many field (instead of many2one)

--- addons/lunch/models/lunch_product.py
+++ addons/lunch/models/lunch_product.py
@@ -18,9 +18,9 @@ class LunchProductCategory(models.Model):
     topping_label_1 = fields.Char('Extra 1 Label', required=True, default='Extras')
     topping_label_2 = fields.Char('Extra 2 Label', required=True, default='Beverages')
     topping_label_3 = fields.Char('Extra 3 Label', required=True, default='Extra Label 3')
-    topping_ids_1 = fields.One2many('lunch.topping', 'category_id', domain=[('topping_category', '=', 1)], ondelete='cascade')
-    topping_ids_2 = fields.One2many('lunch.topping', 'category_id', domain=[('topping_category', '=', 2)], ondelete='cascade')
-    topping_ids_3 = fields.One2many('lunch.topping', 'category_id', domain=[('topping_category', '=', 3)], ondelete='cascade')
+    topping_ids_1 = fields.One2many('lunch.topping', 'category_id', domain=[('topping_category', '=', 1)])
+    topping_ids_2 = fields.One2many('lunch.topping', 'category_id', domain=[('topping_category', '=', 2)])
+    topping_ids_3 = fields.One2many('lunch.topping', 'category_id', domain=[('topping_category', '=', 3)])
     topping_quantity_1 = fields.Selection([
         ('0_more', 'None or More'),
         ('1_more', 'One or More'),
@@ -69,7 +69,7 @@ class LunchTopping(models.Model):
     name = fields.Char('Name', required=True)
     company_id = fields.Many2one('res.company', default=lambda self: self.env.company)
     currency_id = fields.Many2one('res.currency', related='company_id.currency_id')
-    price = fields.Float('Price', digits='Account', required=True)
+    price = fields.Float('Price', digits=dp.get_precision('Account'), required=True)
     category_id = fields.Many2one('lunch.product.category')
     topping_category = fields.Integer('Topping Category', help="This field is a technical field", required=True, default=1)
 
