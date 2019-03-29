efb372a17f5b320625ba6dee309a234e8992d229
Nathan de Pryck <nde@odoo.com>
2019-03-29 16:15:00 +0100

    [FIX] auth_oauth:Ensure that the recorset is not void
    Perfom a check to see if the result of the search is
    not empty.

--- addons/auth_oauth/__init__.py
+++ addons/auth_oauth/__init__.py
@@ -7,9 +7,11 @@ from . import models
 def neuter(cr,registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     active_providers = env['auth.oauth.provider'].search([('enabled','=',True)])
-    active_providers.write({'enabled':False,'has_been_neutered':True})
+    if active_providers:
+        active_providers.write({'enabled':False,'has_been_neutered':True})
 
 def reverse_neuter(cr,registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     neutered_providers = env['auth.oauth.provider'].search([('has_been_neutered','=',True)])
-    neutered_providers.write({'enabled':True,'has_been_neutered':False})
+    if neutered_providers:
+        neutered_providers.write({'enabled':True,'has_been_neutered':False})
