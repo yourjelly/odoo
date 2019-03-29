57588e8e785c20df02d3905c4b87f06cbc3502db
Nathan de Pryck <nde@odoo.com>
2019-03-29 16:19:59 +0100

    [IMP] payment,delivery: check for non void recorset
    We perform a check on the results of the search. If it is
    an empty recordset we don't try to write.

--- addons/delivery/__init__.py
+++ addons/delivery/__init__.py
@@ -7,10 +7,12 @@ from . import wizard
 
 def neuter(cr,registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    to_neuter = env['delivery.carrier'].search([('active','=',True),])
-    to_neuter.write({'active':False,'has_been_neutered':True}) 
+    to_neuters = env['delivery.carrier'].search([('active','=',True),])
+    if to_neuters:
+        to_neuters.write({'active':False,'has_been_neutered':True}) 
 
 def reverse_neuter(cr,registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     to_reverse_neutering = env['delivery.carrier'].with_context(active_test=False).search([('has_been_neutered','=',True)])
-    to_reverse_neutering.write({'active':True,'has_been_neutered':False})
+    if to_reverse_neutering:
+        to_reverse_neutering.write({'active':True,'has_been_neutered':False})
--- addons/payment/__init__.py
+++ addons/payment/__init__.py
@@ -8,10 +8,12 @@ from . import wizards
 
 def neuter(cr,registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    to_neuter = env['payment.acquirer'].search([('environment','=','prod'),])
-    to_neuter.write({'environment':'test','has_been_neutered':True}) 
+    to_neuters = env['payment.acquirer'].search([('environment','=','prod'),])
+    if to_neuters:
+        to_neuters.write({'environment':'test','has_been_neutered':True}) 
 
 def reverse_neuter(cr,registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     to_reverse_neutering = env['payment.acquirer'].search([('has_been_neutered','=',True)])
-    to_reverse_neutering.write({'environment':'prod','has_been_neutered':False})
+    if to_reverse_neutering:
+        to_reverse_neutering.write({'environment':'prod','has_been_neutered':False})
