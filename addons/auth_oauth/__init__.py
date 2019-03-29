67d25e05a6c7da6ebc5cb11029ba18793b217a61
Nathan de Pryck <nde@odoo.com>
2019-03-29 09:58:55 +0100

    [IMP] auth_oauth: Neutralize auth_oauth
    Add the neutralize method and the reverse method
    for the authentification service provider.

--- addons/auth_oauth/__init__.py
+++ addons/auth_oauth/__init__.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+from odoo import api, SUPERUSER_ID
 from . import controllers
 from . import models
+
+def neuter(cr,registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    active_providers = env['auth.oauth.provider'].search([('enabled','=',True)])
+    active_providers.write({'enabled':False,'has_been_neutered':True})
+
+def reverse_neuter(cr,registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    neutered_providers = env['auth.oauth.provider'].search([('has_been_neutered','=',True)])
+    neutered_providers.write({'enabled':True,'has_been_neutered':False})
