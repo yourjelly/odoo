5c29c7f42347ce830edf4380d6f9334ffba96d79
mar <arthur@oos.be>
2019-04-15 13:47:14 +0200

    add server action

---
+++ addons/website_forum_best_contributor/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Find Best Contributor',
+    'category': 'Website',
+    'sequence': 150,
+    'summary': 'Find Best Contributor',
+    'version': '1.0',
+    'description': """
+        """,
+    'website': 'https://www.odoo.com/page/community-builder',
+    'depends': [
+        'website_forum',
+    ],
+    'data': [
+        'data/action.xml',
+    ],
+    'qweb': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': False,
+}
