c54001875fe51cc7c2cf53178bcd81a655129364
Mathieu Duckerts-Antoine <dam@odoo.com>
2019-04-23 17:37:06 +0200

    more tests pass

--- addons/web/static/tests/views/search_view_tests.js
+++ addons/web/static/tests/views/search_view_tests.js
@@ -21,7 +21,7 @@ QUnit.module('Search View', {
                     birthday: {string: "Birthday", type: "date", store: true, sortable: true},
                     foo: {string: "Foo", type: "char", store: true, sortable: true},
                     bar: {string: "Bar", type: "many2one", relation: 'partner'},
-                    float_field: {string: "Float", type: "float"},
+                    float_field: {string: "Float", type: "float", group_operator: 'sum'},
                 },
                 records: [
                     {id: 1, display_name: "First record", foo: "yop", bar: 2, date_field: "2017-01-25", birthday: "1983-07-15", float_field: 1},
