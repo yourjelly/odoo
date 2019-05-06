90da6dde188d7c14120a138266eb680c510c9561
Rishabh Jadia <rja@odoo.com>
2019-05-06 15:19:39 +0530

    [IMP] mrp: hide work order that are in done and cancel state

--- addons/mrp/static/src/js/mrp_gantt_progressbar.js
+++ addons/mrp/static/src/js/mrp_gantt_progressbar.js
@@ -1,31 +1,47 @@
 odoo.define('mrp.mrp_gantt_progressbar', function (require) {
 'use strict';
 
-var GanttRenderer = require('web_gantt.GanttRenderer');
+    var GanttView = require('web_gantt.GanttView');
+    var GanttRenderer = require('web_gantt.GanttRenderer');
+    var viewRegistry = require('web.view_registry');
 
-var MrpGanttProgressbar = GanttRenderer.extend({ 
-    /**
-     * Render work order on gantt view and its rows expect which are in done and cancel state.
-     *
-     * @override
-     */
-    _renderRows: function(rows, groupedBy) {
-        var self = this;
-        rows.forEach(function (row) {
-            row.records = _.filter(row.records, function(rec) {
-                if (rec.state == 'done' || rec.state == 'cancel'){
-                    return false;
-                } else {
-                    return true;
-                }
+    var MrpGanttProgressbar = GanttRenderer.extend({
+
+        /**
+         * @override
+         */
+        init: function (parent, state, params) {
+            var self = this;
+            this.HasPillDuration = true;
+            this._super.apply(this, arguments);
+        },
+
+        /**
+         * Render work order on gantt view and its rows expect which are in done and cancel state.
+         *
+         * @override
+         */
+        _renderRows: function(rows, groupedBy) {
+            var self = this;
+            rows.forEach(function (row) {
+                row.records = _.filter(row.records, function (rec) {
+                    rec.DurationPopover = true;
+                    return ['done', 'cancel'].indexOf(rec.state) == -1
+                });
             });
-        });
-        var rows = _.filter(rows, function (row) {
-            if (row.records.length){
-                return true;
-            }
-        });
-        this._super.apply(this, arguments);;
-    }
-});
+            var rows = this._super.apply(this, arguments)
+            return _.filter(rows, function (row) {
+                return row.pills.length;
+            });
+        }
+    });
+
+    var MrpGanttView = GanttView.extend({
+        config: _.extend({}, GanttView.prototype.config, {
+            Renderer: MrpGanttProgressbar
+        }),
+    });
+
+    viewRegistry.add('mrp_gantt_progressbar', MrpGanttView);
+    return MrpGanttView;
 });
