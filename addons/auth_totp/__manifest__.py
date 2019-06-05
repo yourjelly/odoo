7ebb764cb8fdf39ba93db7344d70d09b76e3bda1
Xavier Morel <xmo@odoo.com>
2019-06-05 14:45:16 +0200

    [ADD] totp

---
+++ addons/auth_totp/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Time-based One-Time-Password support',
+    'depends': ['web'],
+    'category': 'Extra Tools',
+    'auto_install': True,
+    'data': [
+        'security/security.xml',
+        'views/user_preferences.xml',
+        'views/templates.xml',
+    ],
+}
