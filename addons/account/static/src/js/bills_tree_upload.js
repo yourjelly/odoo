aeb4df9f26fa75fd32712af38b5b9e4cd99118b6
Cedric Snauwaert <csn@openerp.com>
2019-06-17 14:50:53 +0200

    [FIX] refactor js

--- addons/account/static/src/js/bills_tree_upload.js
+++ addons/account/static/src/js/bills_tree_upload.js
@@ -1,64 +1,43 @@
-odoo.define('web.UploadBillMixin', function (require) {
+odoo.define('account.upload.bill.mixin', function (require) {
 "use strict";
 
     var core = require('web.core');
     var _t = core._t;
 
+    var qweb = core.qweb;
+
     var UploadBillMixin = {
-        _onUpload: function (event) {
-            var self = this;
-            if (!this.initialState.context.journal_type) {
-                this.initialState.context.journal_type = event.currentTarget.attributes['journal_type'].value;
+
+        start: function () {
+            // define a unique uploadId and a callback method
+            this.fileUploadID = _.uniqueId('account_bill_file_upload');
+            $(window).on(this.fileUploadID, this._onFileUploaded.bind(this));
+            return this._super.apply(this, arguments);
+        },
+
+        _onAddAttachment: function (ev) {
+            // Auto submit form once we've selected an attachment
+            var $input = $(ev.currentTarget).find('input.o_input_file');
+            if ($input.val() !== '') {
+                var $binaryForm = this.$('.o_vendor_bill_upload form.o_form_binary_form');
+                $binaryForm.submit();
             }
-            var $uploadInput = $('<input>', {type: 'file', name: 'files[]', multiple: 'multiple'});
-            var always = function () {
-                $uploadInput.remove();
-            };
-            $uploadInput.on('change', function (ev) {
-                self._processFiles(ev.target.files).then(always).guardedCatch(always);
-            });
-            $uploadInput.click();
         },
 
-        _processFiles: function (files) {
+        _onFileUploaded: function () {
+            // Callback once attachment have been created, create a bill with attachment ids
             var self = this;
-            var postData = new FormData();
-            postData.append('model', 'account.invoice');
-            postData.append('id', 0);
-            postData.append('csrf_token', core.csrf_token);
-            postData.append('callback', "");
-            postData.append('ufile', files);
-            _.each(files, function (file) {
-                postData.append('ufile', file);
-            });
-            var prom = new Promise(function (resolve) {
-                $.ajax({
-                    url: '/web/binary/upload_attachment',
-                    processData: false,
-                    contentType: false,
-                    type: "POST",
-                    enctype: 'multipart/form-data',
-                    data: postData,
-                    success: function (result) {
-                        var matches = []
-                        var regex = /(?:id\": )(\d+)/g;
-                        var match;
-                        while (match = regex.exec(result)) {
-                            matches.push(parseInt(match[1]));
-                        }
-                        return self._createInvoices(matches);
-                    },
-                    error: function (error) {
-                        self.do_notify(_t("Error"), _t("An error occurred during the upload"));
-                        resolve();
-                    },
-                });
-            });
-            return prom;
-        },
-
-        _createInvoices: function(attachent_ids) {
-            var self = this;
+            var attachments = Array.prototype.slice.call(arguments, 1);
+            // Get id from result
+            var attachent_ids = attachments.reduce(function(filtered, record) {
+                if (record.id) {
+                    filtered.push(record.id);
+                } 
+                return filtered;
+            }, []);
+            if (!attachent_ids.length) {
+                return self.do_notify(_t("Error"), _t("An error occurred during the upload"));
+            }
             return this._rpc({
                 model: 'account.invoice.import.wizard',
                 method: 'create_invoices',
@@ -66,7 +45,22 @@ odoo.define('web.UploadBillMixin', function (require) {
                 context: this.initialState.context,
             }).then(function(result) {
                 self.do_action(result);
-            })
+            });
+        },
+
+        _onUpload: function (event) {
+            var self = this;
+            if (!this.initialState.context.journal_type) {
+                this.initialState.context.journal_type = event.currentTarget.attributes['journal_type'].value;
+            }
+            // If hidden upload form don't exists, create it
+            var $formContainer = this.$('.o_content').find('.o_vendor_bill_upload');
+            if (!$formContainer.length) {
+                $formContainer = $(qweb.render('account.BillsHiddenUploadForm', {widget: this}));
+                $formContainer.appendTo(this.$('.o_content'));
+            }
+            // Trigger the input to select a file
+            this.$('.o_vendor_bill_upload .o_input_file').click();
         },
     }
     return UploadBillMixin;
@@ -78,15 +72,14 @@ odoo.define('account.bills.tree', function (require) {
     var core = require('web.core');
     var ListController = require('web.ListController');
     var ListView = require('web.ListView');
-    var UploadBillMixin = require('web.UploadBillMixin');
+    var UploadBillMixin = require('account.upload.bill.mixin');
     var viewRegistry = require('web.view_registry');
-
-    var qweb = core.qweb;
 
     var BillsListController = ListController.extend(UploadBillMixin, {
         buttons_template: 'BillsListView.buttons',
         events: _.extend({}, ListController.prototype.events, {
             'click .o_button_upload_bill': '_onUpload',
+            'change .o_vendor_bill_upload .o_form_binary_form': '_onAddAttachment',
         }),
     });
 
@@ -104,15 +97,13 @@ odoo.define('account.dashboard.kanban', function (require) {
     var core = require('web.core');
     var KanbanController = require('web.KanbanController');
     var KanbanView = require('web.KanbanView');
-    var UploadBillMixin = require('web.UploadBillMixin');
+    var UploadBillMixin = require('account.upload.bill.mixin');
     var viewRegistry = require('web.view_registry');
 
-    var qweb = core.qweb;
-
     var DashboardKanbanController = KanbanController.extend(UploadBillMixin, {
-        buttons_template: 'BillsListView.buttons',
         events: _.extend({}, KanbanController.prototype.events, {
             'click .o_button_upload_bill': '_onUpload',
+            'change .o_vendor_bill_upload .o_form_binary_form': '_onAddAttachment',
         }),
     });
 
