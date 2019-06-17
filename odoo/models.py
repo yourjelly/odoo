8d722684f9c71b123e9dc1aeb4bd4691d9f70e88
Denis Ledoux <dle@odoo.com>
2019-06-17 17:25:07 +0200

    fix

--- odoo/models.py
+++ odoo/models.py
@@ -4508,6 +4508,8 @@ Fields:
 
         # must ignore 'active' flag, ir.rules, etc. => direct SQL query
         cr = self._cr
+        # DLE P64: `test_no_recursion`
+        self.flush([parent])
         query = 'SELECT "%s" FROM "%s" WHERE id = %%s' % (parent, self._table)
         for id in self.ids:
             current_id = id
