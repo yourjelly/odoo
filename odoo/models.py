48870b04a8fd2bf0584f9be2c3fc94eff7351f76
Denis Ledoux <dle@odoo.com>
2019-06-19 10:46:26 +0200

    fix referenced variable before assignment of b43b84bc601c7902e7d42402f5135667542ad21c
    UnboundLocalError: local variable 'fetched' referenced before assignment

--- odoo/models.py
+++ odoo/models.py
@@ -2896,7 +2896,7 @@ Fields:
             cr.execute(query_str, params)
             result += cr.fetchall()
 
-        empty = self.browse()
+        empty = fetched = self.browse()
         if result:
             cols = zip(*result)
             ids = next(cols)
