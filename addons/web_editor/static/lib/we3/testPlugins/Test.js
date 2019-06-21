yourjelly/odoo

commit 422f854c8f8984f73f3f775b946dab6e46a31784

add test container to activate qunit test on website

addons/web_editor/static/lib/we3/testPlugins/Test.js
@@ -85,6 +85,48 @@ we3.addArchNode('TEST', TEST);
 
 /////////////////////////////////////////////////////////////////
 
+var TEST_CONTAINER = class extends we3.ArchNode {
+    //--------------------------------------------------------------------------
+    // public
+    //--------------------------------------------------------------------------
+
+    isBlock () {
+        return true;
+    }
+    isContentEditable () {
+        return true;
+    }
+    isEditable () {
+        return true;
+    }
+    isRoot () {
+        return true;
+    }
+    isTestNode () {
+        return true;
+    }
+    isUnbreakable () {
+        return true;
+    }
+    split (offset) {
+        var virtualText = this.params.create();
+        this.childNodes[offset].after(virtualText);
+        return virtualText;
+    }
+    get type () {
+        return 'TEST_CONTAINER';
+    }
+
+    //--------------------------------------------------------------------------
+    // private
+    //--------------------------------------------------------------------------
+
+    _applyRulesArchNode () {}
+};
+we3.addArchNode('TEST_CONTAINER', TEST_CONTAINER);
+
+/////////////////////////////////////////////////////////////////
+
 function deepEqual (v1, v2) {
     if (v1 === v2) {
         return true;
@@ -322,13 +364,18 @@ var TestPlugin = class extends we3.AbstractPlugin {
             });
         });
 
-        var result;
-        if (archNodeId) {
-            result = this.dependencies.Arch.getNode(archNodeId).toString().replace(/^<[^>]+>/, '').replace(/<\/[^>]+>$/, '');
-        } else {
-            result = this.dependencies.Arch.getValue();
+        if (!archNodeId) {
+            var containers = Arch.findAll('isRoot');
+            if (containers.length !== 1) {
+                throw new Error("Multiple test containers found");
+            }
+            archNodeId = containers[0].id;
         }
+
+        var result = this.dependencies.Arch.getNode(archNodeId).toString();
+
         return result
+            .replace(/^<[^>]+>/, '').replace(/<\/[^>]+>$/, '') // remove container
             .replace(regExpRangeToCollapsed, rangeCollapsed)
             .replace(regSpace, '&nbsp;')
             .replace(regInvisible, '&#65279;');
@@ -429,7 +476,32 @@ var TestPlugin = class extends we3.AbstractPlugin {
         }
         var self = this;
         var Arch = this.dependencies.Arch;
-        var container = Arch.getNode(archNodeId || 1);
+        var container;
+
+        if (archNodeId) {
+            container = Arch.getNode(archNodeId);
+        } else {
+            var containers = Arch.findAll('isRoot');
+            Arch.bypassUpdateConstraints(function () {
+                Arch.bypassChangeTrigger(function () {
+                    containers.forEach(function (a) {
+                        Arch.remove(a.id);
+                    });
+                });
+
+                var root = Arch.getNode(1);
+                container = new TEST_CONTAINER(root.params, 'test-container');
+                Arch.bypassUpdateConstraints(function () {
+                    Arch.bypassChangeTrigger(function () {
+                        if (root.childNodes.length) {
+                            Arch.insertBefore(container, root.childNodes[0].id);
+                        } else {
+                            Arch.insert(container, root.id, 0);
+                        }
+                    });
+                });
+            });
+        }
 
         Arch.setValue(value, container.id);
 
@@ -565,14 +637,18 @@ var TestPlugin = class extends we3.AbstractPlugin {
                     break;
             }
 
-            var onerror = window.onerror;
-            window.onerror = function (e) {
-                window.onerror = onerror;
-                console.error(e);
-                self.assert.notOk(e, 'ERROR Event: ' + eventName);
+            if (!self.options.test.assert) {
+                var onerror = window.onerror
+                window.onerror = function (e) {
+                    window.onerror = onerror;
+                    console.error(e);
+                    self.assert.notOk(e, 'ERROR Event: ' + eventName);
+                }
             }
             el.dispatchEvent(event);
-            window.onerror = onerror;
+            if (!self.options.test.assert) {
+                window.onerror = onerror;
+            }
 
             triggeredEvents.push(event);
         });

addons/web_editor/static/lib/we3/base/arch/ArchNode.js
@@ -204,7 +204,7 @@ we3.ArchNode = class {
      * Find the first ancestor (last if `findGreatest` is true)
      * that matches the given predicate function.
      *
-     * @param {function(ArchNode)} fn
+     * @param {string|function(ArchNode)} fn
      * @param {boolean} findGreatest
      * @returns {ArchNode|undefined}
      */
@@ -905,7 +905,9 @@ we3.ArchNode = class {
         parent.before(this);
         parent.remove();
         // Remove generated empty nodes
-        start.removeIfEmpty()
+        if (start) {
+            start.removeIfEmpty();
+        }
         this._cleanAfterUnwrap();
     }
     /**

addons/web_editor/static/lib/we3/base/plugins/Arch.js
@@ -1254,6 +1254,32 @@ var Arch = class extends we3.AbstractPlugin {
     //--------------------------------------------------------------------------
 
     /**
+     * Find the first archNode that matches the given predicate function.
+     *
+     * @param {string|function(ArchNode)} fn
+     * @returns {ArchNode|undefined}
+     */
+    find (fn) {
+        return this.dependencies.BaseArch.getClonedArchNode(1).nextUntil(function (a) {
+            return a.id !== 1 && (typeof fn === 'string' ? a[fn] && a[fn].call(a, a) : fn.call(a, a));
+        });
+    }
+    /**
+     * Find all archNodes that matches the given predicate function.
+     *
+     * @param {string|function(ArchNode)} fn
+     * @returns {ArchNode[]}
+     */
+    findAll (fn) {
+        var archNodes = [];
+        this.dependencies.BaseArch.getClonedArchNode(1).nextUntil(function (a) {
+            if(a.id !== 1 && (typeof fn === 'string' ? a[fn] && a[fn].call(a, a) : fn.call(a, a))) {
+                archNodes.push(a);
+            }
+        });
+        return archNodes;
+    }
+    /**
      * Get a clone of an ArchNode from its ID or its corresponding node in the DOM.
      *
      * @param {Number|Node} idOrElement

addons/web_editor/static/lib/we3/base/plugins/Rules.js
@@ -53,10 +53,18 @@ var parentedRules = [
     },
     {
         nodes: {
+            nodeNames: ['div', 'ul', 'ol', 'table'].concat(tags.style.filter((tag) => tag !== 'td' && tag !== 'th')),
+        },
+        permittedParents: {
+            methods: ['isRoot'],
+        },
+    },
+    {
+        nodes: {
             nodeNames: ['div'],
         },
         permittedParents: {
-            nodeNames: ['EDITABLE', 'div'],
+            nodeNames: ['div'],
         },
     },
     {
@@ -107,13 +115,12 @@ var parentedRules = [
             nodeNames: ['li'],
         },
     },
-    // editable > p
     {
         nodes: {
-            nodeNames: ['ul', 'ol'],
+            nodeNames: ['ul', 'ol', 'table'],
         },
         permittedParents: {
-            nodeNames: ['EDITABLE', 'div', 'td', 'th', 'li'],
+            nodeNames: ['div', 'td', 'th', 'li'],
         },
     },
     {
@@ -121,7 +128,7 @@ var parentedRules = [
             nodeNames: tags.style.filter((tag) => tag !== 'td' && tag !== 'th'),
         },
         permittedParents: {
-            nodeNames: ['EDITABLE', 'div', 'td', 'th', 'li'],
+            nodeNames: ['div', 'td', 'th', 'li'],
         },
     },
     // H1 > i

addons/website/static/src/js/editor/we3_website.js
@@ -154,7 +154,9 @@ var OdooWebsite = class extends we3.AbstractPlugin {
         this.options.xhr.res_id = res_id;
         this.options.xhr.xpath = xpath;
 
-        console.log(res_model, res_id, xpath);
+        if (res_model) {
+            console.log(res_model, res_id, xpath);
+        }
     }
     _overwriteBootstrap () {
         var self = this;

addons/website/static/src/js/editor/we3_website_tests.js
@@ -104,7 +104,7 @@ QUnit.module('website', {
     QUnit.module('home page edition');
 
     QUnit.test('we3', async function (assert) {
-        assert.expect(9);
+        assert.expect(39);
 
         var $website = $(`<div id="wrapwrap" class="homepage">
                 <header id="top" data-anchor="true" class=" o_affix_enabled">
@@ -253,6 +253,8 @@ QUnit.module('website', {
         assert.ok($('we3-editor #wrapwrap:visible'), "Html value should be appear in the editable area");
 
         await wysiwyg.editor._editor._pluginsManager.call('Test', 'loadTest', ['TestOdooWebsite']);
+        await wysiwyg.editor._editor._pluginsManager.call('Test', 'loadTest', ['TestKeyboardChar']);
+        // await wysiwyg.editor._editor._pluginsManager.call('Test', 'loadTest', ['TestToolbarWand']);
 
         var target = $('we3-editable #wrap h2 b:first')[0].firstChild;
         var id = wysiwyg.editor._editor._pluginsManager.call('Renderer', 'getID', [target]);

commit 4002b2200127242d1408ada7c5b79fd6563877e8

FIX Test utils traceback introduced in 422f854c8f8984f73f3f775b946dab6e46a31784

addons/web_editor/static/lib/we3/testPlugins/Test.js
@@ -657,7 +657,7 @@ var TestPlugin = class extends we3.AbstractPlugin {
                     break;
             }
 
-            if (!self.options.test.assert) {
+            if (!self.options.test || !self.options.test.assert) {
                 var onerror = window.onerror
                 window.onerror = function (e) {
                     window.onerror = onerror;
@@ -666,7 +666,7 @@ var TestPlugin = class extends we3.AbstractPlugin {
                 }
             }
             el.dispatchEvent(event);
-            if (!self.options.test.assert) {
+            if (!self.options.test || !self.options.test.assert) {
                 window.onerror = onerror;
             }
 