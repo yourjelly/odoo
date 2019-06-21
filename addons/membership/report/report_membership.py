shaneebkhan/android-app

[ADD] account,*: Merge account.invoice & account.move
Before this commit, an invoice had no journal entries until it was post. As there is no
synchronization between an invoice and its journal entries, it becomes very difficult for
fiduciary companies to deal with Odoo. Furthermore, it implies there is no possible reporting
based on draft invoice journal entries.

==== models ====

- account.invoice has been merged with account.move. So, when creating an invoice, you are
creating a journal entry as well. account.invoice no longer exists.
- account.invoice.line & account.invoice.tax: Both models has been removed. A journal item
(account.move.line) is created instead in both cases.
- account.invoice.confirm: This wizard has been removed has the 'post' action already works
in multi.
- account.invoice.refund: This wizard has been removed and its functionalities are handled
by account.move.reversal used to reverse the entries.

==== fields ====

account.invoice			| account.move
-----------------------------------------------------------
name				| invoice_payment_ref
origin				| invoice_origin
refund_invoice_id		| reverse_entry_id
number				| name
move_name			|
reference			| ref
comment				| narration
sent				| invoice_sent
date_invoice			| invoice_date
date_due			| invoice_date_due
vendor_bill_id			| invoice_vendor_bill_id
payment_term_id			| invoice_payment_term_id
account_id			|
move_id				|
amount_untaxed_signed		|
amount_total_signed		|
amount_total_company_signed	|
reconciled			|
partner_bank_id			| invoice_partner_bank_id
residual_signed			|
residual_company_signed		|
cash_rounding_id		| invoice_cash_rounding_id
sequence_number_next		| invoice_sequence_number_next
sequence_number_next_prefix	| invoice_sequence_number_next_prefix
incoterm_id			| invoice_incoterm_id
source_email			| invoice_source_email
vendor_display_name		| invoice_vendor_display_name
invoice_icon			| invoice_vendor_icon

-task: 1917430

addons/membership/report/report_membership.py
@@ -73,14 +73,14 @@ class ReportMembership(models.Model):
             CASE WHEN ml.state = 'waiting'  THEN ml.id END AS num_waiting,
             CASE WHEN ml.state = 'invoiced' THEN ml.id END AS num_invoiced,
             CASE WHEN ml.state = 'paid'     THEN ml.id END AS num_paid,
-            CASE WHEN ml.state IN ('waiting', 'invoiced') THEN SUM(il.price_subtotal) ELSE 0 END AS tot_pending,
-            CASE WHEN ml.state = 'paid' OR p.membership_state = 'old' THEN SUM(il.price_subtotal) ELSE 0 END AS tot_earned,
+            CASE WHEN ml.state IN ('waiting', 'invoiced') THEN SUM(al.price_subtotal) ELSE 0 END AS tot_pending,
+            CASE WHEN ml.state = 'paid' OR p.membership_state = 'old' THEN SUM(al.price_subtotal) ELSE 0 END AS tot_earned,
             ml.membership_id AS membership_id,
             p.company_id AS company_id
             FROM res_partner p
             LEFT JOIN membership_membership_line ml ON (ml.partner = p.id)
-            LEFT JOIN account_invoice_line il ON (ml.account_invoice_line = il.id)
-            LEFT JOIN account_invoice ai ON (il.invoice_id = ai.id)
+            LEFT JOIN account_move_line al ON (ml.account_invoice_line = al.id)
+            LEFT JOIN account_move am ON (al.move_id = am.id)
             WHERE p.membership_state != 'none' and p.active = 'true'
             GROUP BY
               p.id,

yourjelly/odoo

addons/membership/report/report_membership.py
@@ -73,14 +73,14 @@ class ReportMembership(models.Model):
             CASE WHEN ml.state = 'waiting'  THEN ml.id END AS num_waiting,
             CASE WHEN ml.state = 'invoiced' THEN ml.id END AS num_invoiced,
             CASE WHEN ml.state = 'paid'     THEN ml.id END AS num_paid,
-            CASE WHEN ml.state IN ('waiting', 'invoiced') THEN SUM(il.price_subtotal) ELSE 0 END AS tot_pending,
-            CASE WHEN ml.state = 'paid' OR p.membership_state = 'old' THEN SUM(il.price_subtotal) ELSE 0 END AS tot_earned,
+            CASE WHEN ml.state IN ('waiting', 'invoiced') THEN SUM(aml.price_subtotal) ELSE 0 END AS tot_pending,
+            CASE WHEN ml.state = 'paid' OR p.membership_state = 'old' THEN SUM(aml.price_subtotal) ELSE 0 END AS tot_earned,
             ml.membership_id AS membership_id,
             p.company_id AS company_id
             FROM res_partner p
             LEFT JOIN membership_membership_line ml ON (ml.partner = p.id)
-            LEFT JOIN account_invoice_line il ON (ml.account_invoice_line = il.id)
-            LEFT JOIN account_invoice ai ON (il.invoice_id = ai.id)
+            LEFT JOIN account_move_line aml ON (ml.account_invoice_line = aml.id)
+            LEFT JOIN account_move am ON (aml.move_id = am.id)
             WHERE p.membership_state != 'none' and p.active = 'true'
             GROUP BY
               p.id,