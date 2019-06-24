eea0477a0a733581314e10d481e7d7dbe1658944
Jigar Vaghela <jva@odoo.com>
2019-06-24 18:03:37 +0530

    [IMP] l10n_nl: add tag mapping method

---
+++ addons/l10n_nl/tax_report_line_mapping.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import odoo
+from odoo.exceptions import UserError
+
+
+def _fill_grids_mapping_for_nl(self, dict_to_fill):
+    self.env.cr.execute("""
+        select name, id
+            from financial_report_lines_v12_bckp rlv12
+            where xmlid like 'financial_report_line_nl%';
+    """)
+    nltax_lines_map = {entry['name']: entry for entry in self.env.cr.dictfetchall()}
+    netherlands_id = self.env['ir.model.data'].xmlid_to_res_id('base.nl')
+    v13_tax_report_lines = self.env['account.tax.report.line'].search([('country_id', '=', netherlands_id), ('tag_name', '!=', None)])
+    # For report lines with childrens that are merged in a single line in v13,
+    for v13_line in v13_tax_report_lines:
+        v12_line_data = nltax_lines_map.get(v13_line.name)
+        if v12_line_data:
+            dict_to_fill.update({v12_line_data['id']: v13_line.tag_name})
+        else:
+            raise UserError("No V12 report line found with name " + v13_line.name)
