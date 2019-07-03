8fda255ef71e48137e0952c23d36bf142553c697
David Monjoie <dmo@odoo.com>
2019-07-03 09:52:33 +0200

    move colorpicker tests oustide of lib

--- addons/web_editor/static/lib/we3/testPlugins/toolbarColor.js
+++ addons/web_editor/static/lib/we3/testPlugins/toolbarColor.js
@@ -79,110 +79,6 @@ var TestToolbarColor = class extends we3.AbstractPlugin {
                 test: '<p>dom not to edit</p><p>d▶om t◀<font class="text-black-25">o </font>edit</p>',
             },
             {
-                name: "Click CUSTOM COLORS then CUSTOM COLOR: blue #0000FF & default -> #875A7B",
-                content: '<p>dom not to edit</p><p>d▶o<font style="color: rgb(0, 0, 255);">m t◀o </font>edit</p>',
-                do: async function () {
-                    await self.dependencies.Test.triggerNativeEvents(self.foreColorToggler, ['mousedown', 'click']);
-                    await self.dependencies.Test.triggerNativeEvents(self.foreColorDropdown.querySelector('we3-button[data-method="custom"]'), ['mousedown', 'click']);
-
-                    $('.modal-dialog .o_hex_input').val('#875A7B').change();
-                    await self.dependencies.Test.triggerNativeEvents($('.o_technical_modal .modal-footer .btn-primary')[0], ['mousedown', 'click']);
-                },
-                test: '<p>dom not to edit</p><p>d<font style="color:#875a7b">▶om t◀</font><font style="color:rgb(0, 0, 255)">o </font>edit</p>',
-            },
-            {
-                name: "Click CUSTOM COLORS then CUSTOM COLOR: change blue input",
-                content: '<p>d▶om to◀ edit</p>',
-                do: async function (assert, testName) {
-                    await self.dependencies.Test.triggerNativeEvents(self.foreColorToggler, ['mousedown', 'click']);
-                    await self.dependencies.Test.triggerNativeEvents(self.foreColorDropdown.querySelector('we3-button[data-method="custom"]'), ['mousedown', 'click']);
-
-                    $('.modal-dialog .o_blue_input').val('100').change();
-                    assert.deepEqual($('.modal-dialog .o_hex_input').val(), '#ff0064', testName + ' (hex)');
-                    assert.deepEqual($('.modal-dialog .o_hue_input').val(), '337', testName + ' (hue)');
-                    await self.dependencies.Test.triggerNativeEvents($('.o_technical_modal .modal-footer .btn-primary')[0], ['mousedown', 'click']);
-                },
-                test: '<p>d<font style="color:#ff0064">▶om to◀</font> edit</p>',
-            },
-            {
-                name: "CUSTOM COLOR: change hue, saturation and lightness inputs",
-                content: '<p>d▶om to◀ edit</p>',
-                do: async function (assert, testName) {
-                    await self.dependencies.Test.triggerNativeEvents(self.foreColorToggler, ['mousedown', 'click']);
-                    await self.dependencies.Test.triggerNativeEvents(self.foreColorDropdown.querySelector('we3-button[data-method="custom"]'), ['mousedown', 'click']);
-
-                        $('.modal-dialog .o_hue_input').val('337').change();
-                        $('.modal-dialog .o_saturation_input').val('50').change();
-                        $('.modal-dialog .o_lightness_input').val('40').change();
-                        assert.deepEqual($('.modal-dialog .o_hex_input').val(), '#99335a', testName + ' (hex)');
-                        assert.deepEqual($('.modal-dialog .o_green_input').val(), '51', testName + ' (green)');
-                    await self.dependencies.Test.triggerNativeEvents($('.o_technical_modal .modal-footer .btn-primary')[0], ['mousedown', 'click']);
-                },
-                test: '<p>d<font style="color:#99335a">▶om to◀</font> edit</p>',
-            },
-            {
-                name: "CUSTOM COLOR: mousedown on area",
-                content: '<p>d▶om to◀ edit</p>',
-                do: async function (assert, testName) {
-                    await self.dependencies.Test.triggerNativeEvents(self.foreColorToggler, ['mousedown', 'click']);
-                    await self.dependencies.Test.triggerNativeEvents(self.foreColorDropdown.querySelector('we3-button[data-method="custom"]'), ['mousedown', 'click']);
-
-                        var $area = $('.modal-dialog .o_color_pick_area');
-                        var pos = $area.offset();
-                        $area.trigger($.Event("mousedown", {
-                            which: 1,
-                            pageX: pos.left + 50,
-                            pageY: pos.top + 50
-                        }));
-                        $area.trigger('mouseup');
-
-                        assert.deepEqual($('.modal-dialog .o_hex_input').val(), '#cfafaf', testName + ' (hex)');
-                        assert.deepEqual($('.modal-dialog .o_red_input').val(), '207', testName + ' (red)');
-                        assert.deepEqual($('.modal-dialog .o_green_input').val(), '175', testName + ' (green)');
-                        assert.deepEqual($('.modal-dialog .o_blue_input').val(), '175', testName + ' (blue)');
-                        assert.deepEqual($('.modal-dialog .o_hue_input').val(), '0', testName + ' (hue)');
-                        assert.deepEqual($('.modal-dialog .o_saturation_input').val(), '25', testName + ' (saturation)');
-                        assert.deepEqual($('.modal-dialog .o_lightness_input').val(), '75', testName + ' (lightness)');
-
-                        await self.dependencies.Test.triggerNativeEvents($('.o_technical_modal .modal-footer .btn-primary')[0], ['mousedown', 'click']);
-                },
-                test: '<p>d<font style="color:#cfafaf">▶om to◀</font> edit</p>',
-            },
-            {
-                name: "CUSTOM COLOR: mousedow on sliders",
-                content: '<p>d▶om to◀ edit</p>',
-                do: async function (assert, testName) {
-                    await self.dependencies.Test.triggerNativeEvents(self.foreColorToggler, ['mousedown', 'click']);
-                    await self.dependencies.Test.triggerNativeEvents(self.foreColorDropdown.querySelector('we3-button[data-method="custom"]'), ['mousedown', 'click']);
-
-                        var $slider1 = $('.modal-dialog .o_slider_pointer');
-                        var pos1 = $slider1.offset();
-                        $slider1.trigger($.Event("mousedown", {
-                            which: 1,
-                            pageX: pos1.left,
-                            pageY: pos1.top + 50
-                        }));
-                        $slider1.trigger('mouseup');
-
-                        assert.deepEqual($('.modal-dialog .o_hex_input').val(), '#83ff00', testName + ' (hex)');
-
-                        var $slider2 = $('.modal-dialog .o_opacity_slider');
-                        var pos2 = $slider2.offset();
-                        $slider2.trigger($.Event("mousedown", {
-                            which: 1,
-                            pageX: pos2.left,
-                            pageY: pos2.top + 80
-                        }));
-                        $slider2.trigger('mouseup');
-
-                        assert.deepEqual($('.modal-dialog .o_hue_input').val(), '89', testName + ' (hue)');
-                        assert.deepEqual($('.modal-dialog .o_opacity_input').val(), '60', testName + ' (opacity)');
-
-                        await self.dependencies.Test.triggerNativeEvents($('.o_technical_modal .modal-footer .btn-primary')[0], ['mousedown', 'click']);
-                },
-                test: '<p>d<font style="color:rgba(131, 255, 0, 0.6)">▶om to◀</font> edit</p>',
-            },
-            {
                 name: "Apply a color on a fontawesome",
                 content: '<p>dom <i class="fa fa-glass">◆</i>not to edit</p>',
                 do: async function () {
@@ -285,17 +181,6 @@ var TestToolbarColor = class extends we3.AbstractPlugin {
                 },
                 test: '<p>dom not to edit</p><p>d▶om t◀<font class="bg-black-25">o </font>edit</p>',
             },
-        {
-            name: "Click CUSTOM COLORS then CUSTOM COLOR: blue #0000FF & default -> #875a7b",
-            content: '<p>dom not to edit</p><p>d▶o<font style="background-color: rgb(0, 0, 255);">m t◀o </font>edit</p>',
-            do: async function (assert, testName) {
-                await self.dependencies.Test.triggerNativeEvents(self.bgColorToggler, ['mousedown', 'click']);
-                await self.dependencies.Test.triggerNativeEvents(self.bgColorDropdown.querySelector('we3-button[data-method="custom"]'), ['mousedown', 'click']);
-                $('.modal-dialog .o_hex_input').val('#875a7b').change();
-                await self.dependencies.Test.triggerNativeEvents($('.o_technical_modal .modal-footer .btn-primary')[0], ['mousedown', 'click']);
-            },
-            test: '<p>dom not to edit</p><p>d<font style="background-color:#875a7b">▶om t◀</font><font style="background-color:rgb(0, 0, 255)">o </font>edit</p>',
-        },
     ];
 
         this.toolbarTests = this.foreColorTests
--- addons/web_editor/static/src/js/wysiwyg/plugin/text/ColorPicker.js
+++ addons/web_editor/static/src/js/wysiwyg/plugin/text/ColorPicker.js
@@ -4,10 +4,28 @@ odoo.define('web_editor.plugins.color_picker', function (require) {
 var ColorpickerDialog = require('wysiwyg.widgets.ColorpickerDialog');
 
 /**
+ * Insert custom color button and palette after normal grid
+ *
+ */
+var insertCustomPalette = function (target) {
+    var custom = document.createDocumentFragment();
+    var customButton = document.createElement('we3-button');
+    customButton.setAttribute('data-method', 'custom');
+    customButton.appendChild(document.createTextNode('Custom color'));
+    var customPalette = document.createElement('we3-palette');
+    customPalette.setAttribute('name', 'Custom colors');
+    customPalette.classList.add('wysiwyg-palette-custom');
+    customPalette.appendChild(document.createElement('we3-row'));
+    custom.appendChild(customButton);
+    custom.appendChild(customPalette);
+
+    target.appendChild(custom);
+}
+
+/**
  * Method called on custom color button click :
  * opens the color picker dialog and saves the chosen color on save.
  *
- * @todo
  */
 var custom = function (value, archNode) {
     var self = this;
@@ -41,9 +59,14 @@ we3.addPlugin('ForeColor', class extends (we3.getPlugin('ForeColor')) {
      */
     constructor() {
         super(...arguments);
+        this.templatesDependencies.push('/web_editor/static/src/xml/wysiwyg_colorpicker_custom.xml');
+        this.buttons.template = 'wysiwyg.buttons.forecolor';
         this.custom = custom.bind(this);
     }
-
+    _insertGrid(rows, target, prepend) {
+        super._insertGrid(rows, target, prepend);
+        insertCustomPalette(target);
+    }
 });
 
 we3.addPlugin('BgColor', class extends (we3.getPlugin('BgColor')) {
@@ -52,9 +75,14 @@ we3.addPlugin('BgColor', class extends (we3.getPlugin('BgColor')) {
      */
     constructor() {
         super(...arguments);
+        this.templatesDependencies.push('/web_editor/static/src/xml/wysiwyg_colorpicker_custom.xml');
+        this.buttons.template = 'wysiwyg.buttons.bgcolor';
         this.custom = custom.bind(this);
     }
-
+    _insertGrid(rows, target, prepend) {
+        super._insertGrid(rows, target, prepend);
+        insertCustomPalette(target);
+    }
 });
 
 });
