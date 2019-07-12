8a2d88d48b22d3cd8d2999a454118299a9dcbc65
Olivier Colson <oco@odoo.com>
2019-07-12 17:00:15 +0200

    [FIX] l10n_fr: keep taxes repartition and report line names consistent with 12.2

--- addons/l10n_fr/migrations/2.0/post-migrate_tags_on_taxes.py
+++ addons/l10n_fr/migrations/2.0/post-migrate_tags_on_taxes.py
@@ -2,4 +2,5 @@
 from odoo.addons.account.models.chart_template import migrate_tags_on_taxes
 
 def migrate(cr, version):
-    migrate_tags_on_taxes(cr, None)
+    #migrate_tags_on_taxes(cr, None)
+    pass
