91ca900d5d23a76eb2e31ff9d9756aff9b004ae0
Denis Ledoux <dle@odoo.com>
2019-07-25 17:55:50 +0200

    fixes

--- addons/website_sale/tests/test_website_sale_product_attribute_value_config.py
+++ addons/website_sale/tests/test_website_sale_product_attribute_value_config.py
@@ -34,8 +34,22 @@ class TestWebsiteSaleProductAttributeValueConfig(TestSaleProductAttributeValueSe
         pricelist.discount_policy = 'with_discount'
 
         # CASE: B2B setting
-        self.env.ref('account.group_show_line_subtotals_tax_included').users -= self.env.user
-        self.env.ref('account.group_show_line_subtotals_tax_excluded').users |= self.env.user
+        # DLE P171: Adding an inactive user to a group in order to test things is a bit weird.
+        # When doing
+        # self.env.ref('account.group_show_line_subtotals_tax_excluded').users |= self.env.user
+        # You would expect self.env.user to be in self.env.ref('account.group_show_line_subtotals_tax_excluded').users
+        # Actually in current master this is not the case, as self.env.user (admin) is inactive,
+        # and the active_test=True will remove it automatically.
+        # In the current branch, it will add the user in the many2many cache field even if the user is not active and active_test is enabled,
+        # which in my sense is not entirely False because this is what the developer wanted here.
+        # What is wrong is that when it removes it, it removes it for the cache with the current context (without active_test=False),
+        # and self.env.user is therefore already not in the cache value of group_tax_included.users and there is a shortcut in that case to not change the cache:
+        # if new_relation == old_relation:
+        #     return records.browse()
+        group_tax_included = self.env.ref('account.group_show_line_subtotals_tax_included').with_context(active_test=False)
+        group_tax_excluded = self.env.ref('account.group_show_line_subtotals_tax_excluded').with_context(active_test=False)
+        group_tax_included.users -= self.env.user
+        group_tax_excluded.users |= self.env.user
 
         combination_info = self.computer._get_combination_info()
         self.assertEqual(combination_info['price'], 2222 * discount_rate * currency_ratio)
@@ -43,8 +57,8 @@ class TestWebsiteSaleProductAttributeValueConfig(TestSaleProductAttributeValueSe
         self.assertEqual(combination_info['has_discounted_price'], False)
 
         # CASE: B2C setting
-        self.env.ref('account.group_show_line_subtotals_tax_excluded').users -= self.env.user
-        self.env.ref('account.group_show_line_subtotals_tax_included').users |= self.env.user
+        group_tax_excluded.users -= self.env.user
+        group_tax_included.users |= self.env.user
 
         combination_info = self.computer._get_combination_info()
         self.assertEqual(combination_info['price'], 2222 * discount_rate * currency_ratio * tax_ratio)
