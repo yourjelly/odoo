64c91cbb1f210414f3e24e939e096d43737a0fe9
Toufik Ben Jaa <tbe@odoo.com>
2019-08-07 16:02:51 +0200

    protected fields fix

--- odoo/models.py
+++ odoo/models.py
@@ -3526,7 +3526,7 @@ Fields:
                 # DLE P83: `test_validation_error`, ignore the protection of compute fields which do not have an inverse
                 # Otherwise their computation are not correctly performed, neither the fields which are computed in the same compute method.
                 if not field.compute or field.inverse:
-                    protected.update(self._field_computed.get(field, [field]))
+                    protected.update([field])
 
             data_list.append(data)
 
