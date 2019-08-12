4a4d60c0b0aa83b8a80cd234d9a6c7803ae4665b
Simon Lejeune <sle@openerp.com>
2019-08-12 10:24:32 +0200

    [REF] stock: res.partner: multi company
    Check company consistency of property_stock_supplier and
    property_stock_customer.
    
    Add domains in the views.
    
    Note that company_id of res.partner isn't required but the property
    fields depends on the current user's company.
    
    We also count on the fact the ir.rule will always be active and not via
    an option in the general settings, a task that should be merged before
    v13.
    
    task-1985992

--- addons/stock/models/res_partner.py
+++ addons/stock/models/res_partner.py
@@ -1,19 +1,38 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import UserError
 from odoo.addons.base.models.res_partner import WARNING_MESSAGE, WARNING_HELP
 
 
 class Partner(models.Model):
-    _inherit = 'res.partner'
+    _name = 'res.partner'
+    _inherit = ['res.partner', 'company.consistency.mixin']
 
     property_stock_customer = fields.Many2one(
         'stock.location', string="Customer Location", company_dependent=True,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
         help="The stock location used as destination when sending goods to this contact.")
     property_stock_supplier = fields.Many2one(
         'stock.location', string="Vendor Location", company_dependent=True,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
         help="The stock location used as source when receiving goods from this contact.")
     picking_warn = fields.Selection(WARNING_MESSAGE, 'Stock Picking', help=WARNING_HELP, default='no-message')
-    # TDE FIXME: expand this message / help
     picking_warn_msg = fields.Text('Message for Stock Picking')
+
+    @api.model
+    def create(self, vals):
+        partner = super(Partner, self).create(vals)
+        partner._company_consistency_check()
+        return partner
+
+    def write(self, vals):
+        res = super(Partner, self).write(vals)
+        if any(field in vals for field in self._company_consistency_fields()):
+            self._company_consistency_check()
+        return res
+
+    def _company_consistency_m2o_property_optional_cid_fields(self):
+        res = super(Partner, self)._company_consistency_m2o_property_optional_cid_fields()
+        return res + ['property_stock_customer', 'property_stock_supplier']
