ecb040aa438c281d546322fdecd08346c603144a
Josse Colpaert <jco@odoo.com>
2019-08-14 14:19:06 +0200

    [FIX] the fix

--- addons/l10n_cl/models/account_chart_template.py
+++ addons/l10n_cl/models/account_chart_template.py
@@ -9,7 +9,7 @@ class AccountChartTemplate(models.Model):
 
     def _load(self, sale_tax_rate, purchase_tax_rate, company):
         """ Set tax calculation rounding method required in Chilean localization"""
-        res = super().load_for_current_company(sale_tax_rate, purchase_tax_rate, company)
+        res = super()._load(sale_tax_rate, purchase_tax_rate, company)
         if company.country_id.code == 'CL':
             company.write({'tax_calculation_rounding_method': 'round_globally'})
         return res
