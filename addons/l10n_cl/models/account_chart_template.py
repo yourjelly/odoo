efeb8b33d790a04cf9d99a6f303c7fdd8eef90f9
Josse Colpaert <jco@odoo.com>
2019-08-14 14:11:26 +0200

    [FIX] _load for company change

--- addons/l10n_cl/models/account_chart_template.py
+++ addons/l10n_cl/models/account_chart_template.py
@@ -5,14 +5,11 @@ from odoo.http import request
 
 
 class AccountChartTemplate(models.Model):
-
     _inherit = 'account.chart.template'
 
-    def load_for_current_company(self, sale_tax_rate, purchase_tax_rate):
+    def _load(self, sale_tax_rate, purchase_tax_rate, company):
         """ Set tax calculation rounding method required in Chilean localization"""
-        self.ensure_one()
-        res = super().load_for_current_company(sale_tax_rate, purchase_tax_rate)
-        company = self.env.company
+        res = super().load_for_current_company(sale_tax_rate, purchase_tax_rate, company)
         if company.country_id.code == 'CL':
             company.write({'tax_calculation_rounding_method': 'round_globally'})
         return res
