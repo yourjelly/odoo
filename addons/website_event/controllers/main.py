e361eaa44f04b81f85cfd9b31df1f5755787b171
Cocographique <nmi@odoo.com>
2019-08-27 00:14:21 +0200

    Add cover image

--- addons/website_event/controllers/main.py
+++ addons/website_event/controllers/main.py
@@ -138,6 +138,7 @@ class WebsiteEventController(http.Controller):
             'pager': pager,
             'searches': searches,
             'search_path': "?%s" % werkzeug.url_encode(searches),
+            'events_cover_properties': [json.loads(c.cover_properties) for c in events],
         }
 
         if searches['date'] == 'old':
@@ -192,7 +193,8 @@ class WebsiteEventController(http.Controller):
             'event': event,
             'main_object': event,
             'range': range,
-            'registrable': event.sudo()._is_event_registrable()
+            'registrable': event.sudo()._is_event_registrable(),
+            'event_cover_properties': json.loads(event.cover_properties),
         }
         return request.render("website_event.event_description_full", values)
 
@@ -289,3 +291,9 @@ class WebsiteEventController(http.Controller):
             'google_url': urls.get('google_url'),
             'iCal_url': urls.get('iCal_url')
         })
+
+    @http.route('/event/event_edit_cover', type='json', auth="public", website=True)
+    def change_cover(self, event_id=0, cover_properties={}, **post):
+        if not event_id:
+            return False
+        return request.env['event.event'].browse(int(event_id)).write({'cover_properties': json.dumps(cover_properties)})
