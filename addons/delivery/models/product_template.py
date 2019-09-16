09da143579cf830e64cc85789b85f4c529c4b182
svs-odoo <svs@odoo.com>
2019-09-16 15:48:47 +0200

    [IMP] delivery: show hs code for product variant
    The field `hs_code` is now shown on product.product and product.template
    form views, except for product.template with multiple variants.

--- addons/delivery/models/product_template.py
+++ addons/delivery/models/product_template.py
@@ -11,3 +11,12 @@ class ProductTemplate(models.Model):
         string="HS Code",
         help="Standardized code for international shipping and goods declaration. At the moment, only used for the FedEx shipping provider.",
     )
+    hide_hs_code = fields.Boolean(compute='_compute_hide_hs_code')
+
+    def _compute_hide_hs_code(self):
+        display_product_variant_view = self.env.context.get('display_product_variant_view')
+        for template in self:
+            if display_product_variant_view or template.product_variant_count == 1:
+                template.hide_hs_code = False
+            else:
+                template.hide_hs_code = True
