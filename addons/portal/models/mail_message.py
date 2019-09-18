33e9ecfd55fc968d82fb183a5b57000ed4ae5c9e
Bhavita Bhatt <bbh@odoo.com>
2019-09-18 12:43:01 +0530

    [WIP] sales: added post_init function in init file and new file in portal module to set mandatory field

--- addons/portal/models/mail_message.py
+++ addons/portal/models/mail_message.py
@@ -1,41 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models 
+from odoo import api, models
 
 class MailMessage(models.Model):
     _inherit = 'mail.message'
-
-    def portal_message_format(self):
-        return self._portal_message_format([
-            'id', 'body', 'date', 'author_id', 'email_from',  # base message fields
-            'message_type', 'subtype_id', 'subject',  # message specific
-            'model', 'res_id', 'record_name',  # document related
-        ])
-
-
-    @api.model
-    def default_get(self, fields_list):
-        defaults = super(MailMessage, self).default_get(fields_list)
-        # Note: explicitly implemented in default_get() instead of field default,
-        # to avoid setting to True for all existing messages during upgrades.
-        # TODO: this default should probably be dynamic according to the model
-        # on which the messages are attached, thus moved to create().
-        if 'website_published' in fields_list:
-            defaults.setdefault('website_published', True)
-        return defaults
-
-    website_published = fields.Boolean(string='Published', help="Visible on the website as a comment")
-
-    @api.model
-    def _non_employee_message_domain(self):
-        return [('subtype_id', '!=', False), ('subtype_id.internal', '=', False)]
-
-    def _portal_message_format(self, fields_list):
-        message_values = self.read(fields_list)
-        message_tree = dict((m.id, m) for m in self.sudo())
-        self._message_read_dsage_domain(self)
-        return [('subtype_id', '!=', False), ('subtype_id.internal', '=', False)]
 
     def portal_message_format(self):
         return self._portal_message_format([
@@ -55,4 +24,6 @@ class MailMessage(models.Model):
                     attachment['access_token'] = IrAttachmentSudo.browse(attachment['id']).generate_access_token()[0]
         return message_values
 
-   
+    @api.model
+    def _non_employee_message_domain(self):
+        return [('subtype_id', '!=', False), ('subtype_id.internal', '=', False)]
--- addons/website_mail/__init__.py
+++ addons/website_mail/__init__.py
@@ -3,3 +3,13 @@
 
 from . import controllers
 from . import models
+from odoo.api import Environment, SUPERUSER_ID
+
+def post_init_hook(cr,registry):
+    env = Environment(cr,SUPERUSER_ID,{})
+    models = env['ir.model'].search([('is_portal_mixin','=',True),('is_mail_thread','=',True)]).mapped('model')
+    messages = env['mail.message'].search([
+        ('subtype_id', '!=', False),
+        ('subtype_id.internal', '=', False),
+        ('model', 'in', models)
+    ]).write({'website_published': True})
