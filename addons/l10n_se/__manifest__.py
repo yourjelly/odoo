a6acfbac826f443d1932d0c6b0de73702a7621dc
Shreya Modi <smo@odoo.com>
2019-10-09 14:57:43 +0530

    [IMP] l10n_se: l10n_se Invoice requirements
    -create module l10n_se and added some additional changes
    in invoice and contacts required when the module is installed.
    
    task-1919382

---
+++ addons/l10n_se/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Swedish Localization",
+    'depends': ['account'],
+    'data': [
+        'views/res_partner_views.xml',
+        'views/res_company_views.xml',
+        'views/report_invoice.xml',
+        'views/report_templates.xml',
+    ],
+}
