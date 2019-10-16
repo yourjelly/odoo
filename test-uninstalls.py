c664a90a10772ac25a1a00dc5b6a5477d639e519
Adrian Torres <adt@odoo.com>
2019-10-16 15:52:11 +0200

    ok now it works

--- test-uninstalls.py
+++ test-uninstalls.py
@@ -1,40 +1,60 @@
 import odoo
+import logging
 
 
 from odoo.tools.misc import topological_sort
 
-
-def cycle(mod):
-    print("Installing %s" % mod.name)
-    mod.button_immediate_install()
-    print("Uninstalling %s" % mod.name)
-    mod.button_immediate_uninstall()
-    print("Reinstalling %s" % mod.name)
-    mod.button_immediate_install()
+DB = 'foobarbaz'
+REGISTRY = odoo.registry(DB)
+CALLBACKS = ('button_immediate_install', 'button_immediate_uninstall')
 
 
-db = 'foobarbaz'
-reg = odoo.registry(db)
+def _call_on_mod(mod_id, callback):
+    assert callback in CALLBACKS, f"_call_on_mod only accepts {CALLBACKS} as arguments"
+    REGISTRY = odoo.registry(DB)
+    with odoo.api.Environment.manage():
+        with REGISTRY.cursor() as cr:
+            env = odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
+            mod = env['ir.module.module'].browse(mod_id)
+            name = mod.name
+            getattr(mod, callback)()
+    return name
+
+
+def _install(mod_id):
+    name = _call_on_mod(mod_id, 'button_immediate_install')
+    print(f"{name} installed")
+
+
+def _uninstall(mod_id):
+    name = _call_on_mod(mod_id, 'button_immediate_uninstall')
+    print(f"{name} uninstalled")
+
+
+def cycle(mod_id):
+    _install(mod_id)
+    _uninstall(mod_id)
+    _install(mod_id)
+
 
 with odoo.api.Environment.manage():
-    with reg.cursor() as cr:
+    with REGISTRY.cursor() as cr:
         env = odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
 
         mod_blacklist = {
             'auth_ldap', 'document_ftp', 'base_gengo', 'website_gengo', 'website_instantclick',
             'pad', 'pad_project', 'note_pad', 'pos_cache', 'pos_blackbox_be', 'base',
+            'payment_test',
         }
 
         def filter_mod(mod):
-            return not (mod.name in mod_blacklist or mod.name.startswith(('hw_', 'theme_', 'l10n_', 'test_')))
+            return not (mod.name in mod_blacklist or mod.name.startswith((
+                'hw_', 'theme_', 'l10n_', 'test_')))
 
-        testable_mods = env['ir.module.module'].search([]).filtered(filter_mod)
-        mod_dict = {mod.name: mod for mod in testable_mods}
-        sorted_mods = topological_sort({k: v.dependencies_id.mapped('name') for k, v in mod_dict.items()})
-        errs = []
+        mods = env['ir.module.module'].search([]).filtered(filter_mod)
+        sorted_mods = topological_sort({
+            mod.id: mod.dependencies_id.mapped('depend_id').ids for mod in mods
+        })
 
-        for mod in sorted_mods:
-            try:
-                cycle(mod_dict[mod])
-            except Exception:
-                errs.append(mod)
+for mod in sorted_mods:
+    cycle(mod)
