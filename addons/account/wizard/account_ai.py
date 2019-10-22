6bf2ea0a6bde379a169c2d7f606366100e2271c8
wan <wan@odoo.com>
2019-10-22 16:09:21 +0200

    wip

--- addons/account/wizard/account_ai.py
+++ addons/account/wizard/account_ai.py
@@ -2,158 +2,160 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
-import pandas as pd
-import numpy as np
-import sklearn as sk
-import random
-
-from sklearn.svm import SVC
-from sklearn.neural_network import BernoulliRBM
-from sklearn.neural_network import MLPClassifier
-
-import tensorflow as tf
-
-
+from collections import defaultdict
 from pprint import pprint
+from timeit import default_timer as timer
+import random
+import psycopg2
+import datetime
+
+REFERENCE_DATE = datetime.date(1970, 1, 1)
 
 
-class AccrualAccountingWizard(models.TransientModel):
+def compute_recurence(dates, max_bin_size=5):
+    dates_diff = [dates[k + 1] - dates[k] for k in range(len(dates) - 1)]
+    recurence = [defaultdict(lambda: defaultdict(lambda: 0)) for i in range(max_bin_size + 1)]
+    for i in range(len(dates_diff)):
+        for sequence_len in range(0, max_bin_size + 1):
+            if i + sequence_len < len(dates_diff):
+                sequence = tuple(dates_diff[i:i + sequence_len])
+                recurence[sequence_len][sequence][dates_diff[i + sequence_len]] += 1
+    return recurence
+
+
+def prune(recurence, min_occurence=5):
+    tree = {}
+    for rec in recurence:
+        for k, v in rec.items():
+            sorted = list(v.items())
+            summed = sum(s[1] for s in sorted)
+            sorted.sort(key=lambda i: i[1])
+            max_item = sorted[-1]
+            if not sorted or max_item[1] < min_occurence or max_item[1] < (summed / 2):
+                continue
+            tree[k] = max_item[0]
+    return tree
+
+
+def find(dates, tree, min_bin_size=2):
+    max_bin_size = max((len(i) for i in tree.keys()), default=0)
+    found = set()
+    dates_set = set(dates)
+    dates_diff = [dates[k + 1] - dates[k] for k in range(len(dates) - 1)]
+    for i in range(len(dates_diff) + 1):
+        for sequence_len in range(max_bin_size, min_bin_size, -1):
+            if i - sequence_len >= 0:
+                seq = dates_diff[i - sequence_len:i]
+                res = tree.get(tuple(seq))
+                if res:
+                    add = True
+                    range_res = int(res / 4)
+                    for days in range(-range_res, range_res + 1):
+                        if res + dates[i] + days in dates_set:
+                            add = False
+                            break
+                    if add:
+                        found.add(dates[i] + res)
+                        break
+    return found
+
+
+class AIWizard(models.TransientModel):
     _name = 'account.ai.wizard'
     _description = 'Deduce things'
 
-    def raisethings(self):
-        pass
+    company_id = fields.Many2one('res.company', default=lambda self: self.env.company)
+    line_ids = fields.One2many('account.ai.wizard.line', 'wizard_id', compute="compute_things")
 
-df = pd.read_sql_query("""
-    SELECT account_id, partner_id, date, debit, credit, account_internal_type
-    FROM account_move_line
-""", env.cr._cnx).fillna(0)
-df.partner_id = df.partner_id.astype('int64')
-df['year'] = pd.DatetimeIndex(df.date).year
-df['month'] = pd.DatetimeIndex(df.date).month
-df['day'] = pd.DatetimeIndex(df.date).day
-df = df.drop(['date'], axis=1)
-df['exists'] = 1
+    def _get_dates(self):
+        self.env.cr.execute("""
+            WITH amount_range(amount_range) AS ( VALUES
+                (int4range(NULL, -51200)),
+                (int4range(-102400, -12800)),
+                (int4range(-25600, -3200)),
+                (int4range(-6400, -800)),
+                (int4range(-1600, -200)),
+                (int4range(-400, -50)),
+                (int4range(-100, 0)),
+                (int4range(0, 100)),
+                (int4range(50, 400)),
+                (int4range(200, 1600)),
+                (int4range(800, 6400)),
+                (int4range(3200, 25600)),
+                (int4range(12800, 102400)),
+                (int4range(51200, NULL))
+            ),
+            amount_range_sign(amount_range) AS ( VALUES
+                (int4range(NULL, 0)),
+                (int4range(0, NULL))
+            ),
+            no_amount_range(amount_range) AS ( VALUES
+                (int4range(NULL, NULL))
+            )
+            SELECT account_id, partner_id, amount_range, array_agg(DISTINCT date ORDER BY date ASC)
+            FROM account_move_line
+            LEFT JOIN amount_range ON balance::int <@ amount_range
+            GROUP BY account_id, partner_id, amount_range
+        """)
+        dates_grouped = self.env.cr.fetchall()
+        return {(acc, par, (ran.lower, ran.upper)): [(dat - REFERENCE_DATE).days for dat in dats] for acc, par, ran, dats in dates_grouped}
 
-value_counts = {}
-for serie in list(df.columns):
-    value_counts[serie] = dict(df[serie].value_counts())
+    @api.depends('company_id')
+    def compute_things(self):
+        dates_dict = self._get_dates()
+        propositions = {}
+        for key, dates in dates_dict.items():
+            recurence = compute_recurence(dates, 25)
+            tree = prune(recurence, 2)
+            found = find(dates, tree)
 
-false_df = pd.DataFrame([[random.choice(list(value_counts[serie].keys())) if serie != 'exists' else 0 for serie in list(df.columns)] for i in range(10 * len(df))], columns=list(df.columns))
-
-all_df = pd.get_dummies(df.append(false_df))
-# model = SVC()
-# model.fit(all_df.drop('exists', axis=1), all_df.exists)
-
-# model = BernoulliRBM()
-# model.fit(df)
-
-model = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-model.fit(all_df.drop('exists', axis=1), all_df.exists)
-
-
-
-# class Generator(tf.keras.Model):
-#     def __init__(self):
-#         super(Generator, self).__init__()
-#         self.hidden1 = tf.keras.layers.Dense(128, activation='relu', name='hidden1')
-#         self.hidden2 = tf.keras.layers.Dense(128, activation='relu', name='hidden2')
-#         self.output_layer = tf.keras.layers.Dense(2, activation='tanh', name='output')
-#     def call(self, x):
-#         x = self.hidden1(x)
-#         x = self.hidden2(x)
-#         x = self.output_layer(x)
-#         return x
-#
-#
-# class Discriminator(tf.keras.Model):
-#     def __init__(self):
-#         super(Discriminator, self).__init__()
-#         self.hidden1 = tf.keras.layers.Dense(128, activation='relu', name='hidden1')
-#         self.hidden2 = tf.keras.layers.Dense(128, activation='relu', name='hidden2')
-#         self.logits = tf.keras.layers.Dense(2, activation='relu', name='logits')
-#         self.sigmoid = tf.keras.layers.Dense(2, activation='sigmoid', name='sigmoid')
-#     def call(self, x):
-#         x = self.hidden1(tf.keras.layers.Input(tensor=x))
-#         x = self.hidden2(x)
-#         logits = self.output(x)
-#         output = self.sigmoid(logits)
-#         return output, logits
-#
-#
-# discriminator = Discriminator()
-# generator = Generator()
-#
-# discriminator.compile(optimizer=tf.keras.optimizers.Adam(0.01),
-#               loss='categorical_crossentropy',
-#               metrics=['accuracy'])
-#
-# generator.compile(optimizer=tf.keras.optimizers.Adam(0.01),
-#               loss='categorical_crossentropy',
-#               metrics=['accuracy'])
+            if found:
+                propositions[key] = {REFERENCE_DATE + datetime.timedelta(f) for f in found}
+        self.line_ids = self.env['account.ai.wizard.line']
+        for key, value in propositions.items():
+            account_id, partner_id, range = key
+            for date in value:
+                self.line_ids += self.env['account.ai.wizard.line'].create({
+                    'account_id': account_id,
+                    'partner_id': partner_id,
+                    'range_up': range[1],
+                    'range_down': range[0],
+                    'date': date,
+                })
 
 
-def make_generator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
-    model.add(layers.Dense(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+class AIWizardProposition(models.TransientModel):
+    _name = 'account.ai.wizard.line'
+    _description = 'Deduced things'
 
-    return model
+    wizard_id = fields.Many2one('account.ai.wizard')
+    account_id = fields.Many2one('account.account')
+    partner_id = fields.Many2one('res.partner')
+    range_up = fields.Integer()
+    range_down = fields.Integer()
+    date = fields.Date()
 
-generator = make_generator_model()
+    def action_open(self):
+        self.ensure_one()
+        domain = [('account_id', '=', self.account_id.id), ('partner_id', '=', self.partner_id.id)]
+        if self.range_up:
+            domain += [('balance', '<=', self.range_up)]
+        if self.range_down:
+            domain += [('balance', '>=', self.range_down)]
+        return {
+            'name': "{account}, {partner} between {down} and {up} for {date}".format(
+                account=self.account_id.display_name,
+                partner=self.partner_id.display_name,
+                down=self.range_down,
+                up=self.range_up,
+                date=self.date,
+            ),
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move.line',
+            'view_mode': 'tree,form',
+            'domain': domain,
+        }
 
-def make_discriminator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                                     input_shape=[28, 28, 1]))
-    model.add(layers.Dense(1))
-
-    return model
-
-discriminator = make_discriminator_model()
-
-
-
-
-
-def loss_func(logits_in, labels_in):
-    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_in, labels=labels_in))
-
-
-real_images = tf.placeholder(tf.float32, shape=[None, 784])
-z = tf.placeholder(tf.float32, shape=[None, 100])
-
-G = generator(z)
-D_output_real, D_logits_real = discriminator(real_images)
-D_output_fake, D_logits_fake = discriminator(G)
-
-D_real_loss = loss_func(D_logits_real, tf.ones_like(D_logits_real) * 0.9)
-D_fake_loss = loss_func(D_logits_fake, tf.zeros_like(D_logits_real))
-D_loss = D_real_loss + D_fake_loss
-G_loss = loss_func(D_logits_fake, tf.ones_like(D_logits_fake))
-
-lr = 0.001  # learning rate
-batch_size = 100  # batch size
-epochs = 500  # number of epochs. The higher the better the result
-
-tvars = tf.trainable_variables()
-d_vars = [var for var in tvars if 'dis' in var.name]
-g_vars = [var for var in tvars if 'gen' in var.name]
-
-D_trainer = tf.train.AdamOptimizer(lr).minimize(D_loss, var_list=d_vars)
-G_trainer = tf.train.AdamOptimizer(lr).minimize(G_loss, var_list=g_vars)
-
-init = tf.global_variables_initializer()
-
-mnist = tf.data.Dataset.from_tensor_slices(dict(df))
-
-samples = []
-with tf.Session() as sess:
-    sess.run(init)
-    for epoch in range(epochs):
-        _, dloss = sess.run(D_trainer, feed_dict={real_images: real_images, z: z})
-        _, gloss = sess.run(G_trainer, feed_dict={z: batch_z})
-        print("on epoch{}".format(epoch))
-        sample_z = np.random.uniform(-1, 1, size=(1, 100))
-        gen_sample = sess.run(generator(z, reuse=True), feed_dict={z: sample_z})
-        samples.append(gen_sample)
+    def action_create(self):
+        self.ensure_one()
+        raise UserError("Not implemented yet, dont be greedy")
