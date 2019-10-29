c487dce3dbf2a3d254d4515408122b482f812571
Adrian Torres <adt@odoo.com>
2019-10-29 14:43:53 +0100

    add skip argument to test-uninstall script

--- test-uninstalls.py
+++ test-uninstalls.py
@@ -7,6 +7,11 @@ from odoo.tools.misc import topological_sort
 
 
 CALLBACKS = ('button_immediate_install', 'button_immediate_uninstall')
+BLACKLIST = {
+    'auth_ldap', 'document_ftp', 'base_gengo', 'website_gengo', 'website_instantclick', 'pad',
+    'pad_project', 'note_pad', 'pos_cache', 'pos_blackbox_be', 'base', 'payment_test',
+}
+IGNORE = ('hw_', 'theme_', 'l10n_', 'test_')
 
 
 def _call_on_mod(mod_id, callback, db):
@@ -39,34 +44,33 @@ def cycle(mod_id, db):
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Script for testing the install / uninstall / reinstall cycle of Odoo modules")
-    parser.add_argument("--database", "-d", metavar="db", type=str, nargs=1, required=True,
+    parser.add_argument("--database", "-d", type=str, nargs=1, required=True,
         help="The database to test (note: must have only 'base' installed)")
+    parser.add_argument("--skip", "-s", type=str, nargs=1,
+        help="Comma-separated list of modules to skip (they will only be installed)")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
     with odoo.api.Environment.manage():
-        with odoo.registry(args.db).cursor() as cr:
+        with odoo.registry(args.database[0]).cursor() as cr:
             env = odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
 
-            mod_blacklist = {
-                'auth_ldap', 'document_ftp', 'base_gengo', 'website_gengo', 'website_instantclick',
-                'pad', 'pad_project', 'note_pad', 'pos_cache', 'pos_blackbox_be', 'base',
-                'payment_test',
-            }
-
             def filter_mod(mod):
-                return not (mod.name in mod_blacklist or mod.name.startswith((
-                    'hw_', 'theme_', 'l10n_', 'test_')))
+                return not (mod.name in BLACKLIST or mod.name.startswith(IGNORE))
 
             mods = env['ir.module.module'].search([]).filtered(filter_mod)
             sorted_mods = topological_sort({
                 mod.id: mod.dependencies_id.mapped('depend_id').ids for mod in mods
             })
+            mod_names = {mod.id: mod.name for mod in mods}
 
     for mod in sorted_mods:
-        cycle(mod, args.db)
+        if args.skip and mod_names[mod] in args.skip[0].split(','):
+            _install(mod, args.database[0])
+        else:
+            cycle(mod, args.database[0])
 
 if __name__ == '__main__':
     main()
