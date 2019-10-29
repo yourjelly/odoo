11591619109dc91489f95e4c8acf4f62f3612681
Laurent Smet <las@odoo.com>
2019-10-29 13:21:27 +0100

    [IMP] account,*: Improve account.bank.statement.line / account.payment workflow
    - Create journal entries as soon as bank/cash statement lines are created, temporary booked on a
    suspense account set on the journal,
    - Simplify the management of "blue" lines in the reconciliation widget. A "blue" line is now
    a journal item using a temporary liquidity account (outstanding payment/receipt accounts, set
    on the journal),
    - Adapt and simplify the bank reconciliation report,
    - Remove the bank reconciliation threshold date. The reconciliation report will show the not
    already reconciled journal entries using a liquidity account and the not already reconciled
    journal entries using a temporary liquidity account. Without accounting, an account.payment
    will involve directly the liquidity account and then, will be considered as a statement line
    directly.
    - Remove the post_at bank reconciliation feature. The "paid" state will be set on the
    invoices only if reconciled with a journal entry involving the journal's liquidity account.
    With invoicing, the payment will do that so the "in_payment" state should never be shown up.
    With accounting, only the statement lines have the power to move an invoice to the "paid"
    state.
    - Fix various corner cases about the management of multi-currency in bank statement lines.
    - Fix the conversion dates in multi-currency: Since the bank/cash is always used on the
    statement lines, it will use always the real "bank" date instead of the fictive payment one.
    - Ensure the 'reconcile' method will raise an error if the involved moves are not posted.
    
    --task: 2092096

--- addons/account/tests/test_account_journal.py
+++ addons/account/tests/test_account_journal.py
@@ -1,30 +1,19 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 from odoo.tests import tagged
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 
 @tagged('post_install', '-at_install')
 class TestAccountJournal(AccountTestInvoicingCommon):
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
+    def test_constraint_currency_consistency_with_accounts(self):
+        ''' The accounts linked to a bank/cash journal must share the same foreign currency
+        if specified.
+        '''
+        journal_bank = self.company_data['default_journal_bank']
+        journal_bank.currency_id = self.currency_data['currency']
 
-        # Create another company.
-        cls.company_data_2 = cls.setup_company_data('company_2_data')
-
-        # By default, tests are run with the current user set on the first company.
-        cls.env.user.company_id = cls.company_data['company']
-
-    def test_changing_journal_company(self):
-        ''' Ensure you can't change the company of an account.journal if there are some journal entries '''
-
-        self.env['account.move'].create({
-            'move_type': 'entry',
-            'date': '2019-01-01',
-            'journal_id': self.company_data['default_journal_sale'].id,
-        })
-
-        with self.assertRaises(UserError), self.cr.savepoint():
-            self.company_data['default_journal_sale'].company_id = self.company_data_2['company']
+        # Try to set a different currency on the 'debit' account.
+        with self.assertRaises(ValidationError), self.cr.savepoint():
+            journal_bank.default_debit_account_id.currency_id = self.company_data['currency']
