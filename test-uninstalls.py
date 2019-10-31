6f0ee63a26cf2c2c61b5187a957df3837f7f64c5
Adrian Torres <adt@odoo.com>
2019-10-31 13:01:20 +0100

    foo

--- test-uninstalls.py
+++ test-uninstalls.py
@@ -1,6 +1,7 @@
 import argparse
 import odoo
 import logging
+import sys
 
 
 from odoo.tools.misc import topological_sort
@@ -11,7 +12,7 @@ BLACKLIST = {
     'auth_ldap', 'document_ftp', 'base_gengo', 'website_gengo', 'website_instantclick', 'pad',
     'pad_project', 'note_pad', 'pos_cache', 'pos_blackbox_be', 'base', 'payment_test',
 }
-IGNORE = ('hw_', 'theme_', 'l10n_', 'test_')
+IGNORE = ('hw_', 'theme_', 'l10n_', 'test_', 'payment_')
 
 
 def _call_on_mod(mod_id, callback, db):
@@ -48,11 +49,21 @@ def parse_args():
         help="The database to test (note: must have only 'base' installed)")
     parser.add_argument("--skip", "-s", type=str, nargs=1,
         help="Comma-separated list of modules to skip (they will only be installed)")
+    parser.add_argument("--resume", "-r", type=str, nargs=1,
+        help="Skip the cycle on all modules up to the specified one")
+    parser.add_argument("--paths", "-p", type=str, nargs=1,
+        help="Comma-separated list of paths to directories containing extra Odoo modules")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
+
+    # handle paths option
+    paths = args.paths and args.paths[0].split(',')
+    for path in paths:
+        sys.path.insert(0, path)
+
     with odoo.api.Environment.manage():
         with odoo.registry(args.database[0]).cursor() as cr:
             env = odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
@@ -66,8 +77,22 @@ def main():
             })
             mod_names = {mod.id: mod.name for mod in mods}
 
+    done = False
     for mod in sorted_mods:
+        skip = False
+
+        # handle resume option
+        if args.resume and not done:
+            if mod_names[mod] != args.resume[0]:
+                skip = True
+            else:
+                done = True
+
+        # handle skip option
         if args.skip and mod_names[mod] in args.skip[0].split(','):
+            skip = True
+
+        if skip:
             _install(mod, args.database[0])
         else:
             cycle(mod, args.database[0])
