2991b71c7e90975c26d48f14971cee15e7b7486a
Prajapati Hardik <hpr@odoo.com>
2019-11-19 16:06:52 +0530

    review

--- addons/l10n_se/models/res_partner.py
+++ addons/l10n_se/models/res_partner.py
@@ -2,7 +2,8 @@
 
 from odoo import models, fields
 
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    corporate_id = fields.Char(string='Corporate ID')
+    corporate_identification = fields.Char(string='Corporate ID')
