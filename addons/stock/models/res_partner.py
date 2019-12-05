1b86e4f1f6b71d09ff439476086a89fff9e425a9
Mitali Patel <mpa@odoo.com>
2019-12-05 20:00:37 +0530

    [IMP] sale: suggest the most used addresses first

--- addons/stock/models/res_partner.py
+++ addons/stock/models/res_partner.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 from odoo.addons.base.models.res_partner import WARNING_MESSAGE, WARNING_HELP
 
 
@@ -19,3 +19,33 @@ class Partner(models.Model):
         help="The stock location used as source when receiving goods from this contact.")
     picking_warn = fields.Selection(WARNING_MESSAGE, 'Stock Picking', help=WARNING_HELP, default='no-message')
     picking_warn_msg = fields.Text('Message for Stock Picking')
+    # Computed fields to order the partners as suppliers/customers according to the
+    # amount of their generated incoming/outgoing stock moves
+    delivery_rank = fields.Integer(default=0)
+    reciept_rank = fields.Integer(default=0)
+
+    def _get_name_search_order_by_fields(self):
+        res = super()._get_name_search_order_by_fields()
+        partner_search_mode = self.env.context.get('res_partner_search_mode')
+        if not partner_search_mode in ('delivery', 'reciept'):
+            return res
+        order_by_field = 'COALESCE(res_partner.%s, 0) DESC,'
+        if partner_search_mode == 'delivery':
+            field = 'delivery_rank'
+        else:
+            field = 'reciept_rank'
+        order_by_field = order_by_field % field
+        return '%s, %s' % (res, order_by_field % field) if res else order_by_field
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        search_partner_mode = self.env.context.get('res_partner_search_mode')
+        is_delivery = search_partner_mode == 'delivery'
+        is_reciept = search_partner_mode == 'reciept'
+        if search_partner_mode:
+            for vals in vals_list:
+                if is_delivery and 'delivery_rank' not in vals:
+                    vals['delivery_rank'] = 1
+                elif is_reciept and 'reciept_rank' not in vals:
+                    vals['reciept_rank'] = 1
+        return super().create(vals_list)
