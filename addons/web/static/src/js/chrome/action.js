c5a46c77cef44c168592929d5b5d9f80cb31474b
Lucas Perais (lpe) <lpe@odoo.com>
2020-01-09 15:22:25 +0100

    [IMP] SwitchView makes a reload
    TODO: reload with right params !

--- addons/web/static/src/js/chrome/action.js
+++ addons/web/static/src/js/chrome/action.js
@@ -12,13 +12,24 @@ const AbstractView = require('web.AbstractView');
 const { ComponentAdapter } = require('web.OwlCompatibility');
 
 class Action extends ComponentAdapter {
+    constructor(parent, props) {
+        super(...arguments);
+        if (!(props.Component.prototype instanceof owl.Component)) {
+            this.legacy = true;
+        }
+    }
+
     async willStart() {
         if (this.props.Component.prototype instanceof AbstractView) {
+            this.legacy = 'view';
+            this.starting = true;
             const action = this.props.action;
             const viewDescr = action.views.find(view => view.type === action.controller.viewType);
             const view = new viewDescr.View(viewDescr.fieldsView, action.controller.viewOptions);
             this.widget = await view.getController(this);
             return this.widget._widgetRenderAndInsert(() => {});
+        } else if (this.legacy) {
+            this.legacy = 'action';
         }
         return super.willStart();
     }
@@ -27,8 +38,20 @@ class Action extends ComponentAdapter {
         return [this.props.action, this.props.options];
     }
 
-    shouldUpdate() {
-        return false;
+    shouldUpdate(nextProps) {
+        if (this.legacy) {
+            const starting = this.starting;
+            this.starting = false
+            return nextProps.shouldUpdateWidget && !starting;
+        }
+        return super.shouldUpdate(nextProps);
+    }
+    async willUpdateProps(nextProps) {
+        if (this.legacy === 'view') {
+            const options = nextProps.options;
+            await this.widget.reload(options);
+        }
+        return super.willUpdateProps(...arguments);
     }
 
     // TODO: override destroy to keep actions in action stack alive?
