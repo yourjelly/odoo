da500732d7e7d2b623bd0d0df32f3ca10c94fb06
Lucas Perais (lpe) <lpe@odoo.com>
2020-01-10 17:51:18 +0100

    [IMP] action.js: params are passed for switching controller
    Legacy widget in act_window are kept alive (not destroyed!)

--- addons/web/static/src/js/chrome/action.js
+++ addons/web/static/src/js/chrome/action.js
@@ -25,8 +25,14 @@ class Action extends ComponentAdapter {
             this.starting = true;
             const action = this.props.action;
             const viewDescr = action.views.find(view => view.type === action.controller.viewType);
-            const view = new viewDescr.View(viewDescr.fieldsView, action.controller.viewOptions);
+            const viewParams = Object.assign(
+                {},
+                { action: action, controllerState: action.controllerState },
+                action.controller.viewOptions,
+            );
+            const view = new viewDescr.View(viewDescr.fieldsView, viewParams);
             this.widget = await view.getController(this);
+            this._reHookControllerMethods();
             return this.widget._widgetRenderAndInsert(() => {});
         } else if (this.legacy) {
             this.legacy = 'action';
@@ -48,13 +54,46 @@ class Action extends ComponentAdapter {
     }
     async willUpdateProps(nextProps) {
         if (this.legacy === 'view') {
-            const options = nextProps.options;
-            await this.widget.reload(options);
+            const action = nextProps.action;
+            const controllerState = action.controllerState;
+            await this.widget.reload(
+                {
+                    offset: 0,
+                    domain: action.domain,
+                    groupBy: action.groupBy,
+                    context: action.context,
+                    controllerState
+                }
+            );
         }
         return super.willUpdateProps(...arguments);
     }
 
-    // TODO: override destroy to keep actions in action stack alive?
+    _reHookControllerMethods() {
+        const self = this;
+        const widget = this.widget;
+        const controllerReload = widget.reload;
+        this.widget.reload = async function(params) {
+            const controllerState = widget.exportState();
+            const commonState = {};
+            commonState.domain = params.domain || widget._getSearchDomain();
+            if (params) {
+                if (params.groupBy) {commonState.groupBy = params.groupBy;}
+                if (params.context) {commonState.context = params.context;}
+            }
+
+            self.trigger('reloading-legacy', { commonState , controllerState });
+            return controllerReload.call(widget, ...arguments);
+        }
+    }
+    destroy() {
+        if (this.legacy) {
+            // keep legacy stuff alive because some stuff
+            // are kept by AbstractModel (e.g.: orderedBy)
+            return;
+        }
+        return super.destroy();
+    }
 }
 
 return Action;
