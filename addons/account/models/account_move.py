yf956613/odoo-1

[IMP] *_*: change the field type float by uom field type
Purpose of this commit is to removed decimal accuracy
for UoM(i.e digits=3) and change the field type float
by uom type field to rounding the field value base on
uom rounding by passing the uom field on field level
that will make esier configuration of
rounding + decimal precision

task-1914025

addons/account/models/account_move.py
@@ -624,7 +624,7 @@ class AccountMoveLine(models.Model):
             record.parent_state = record.move_id.state
 
     name = fields.Char(string="Label")
-    quantity = fields.Float(digits=dp.get_precision('Product Unit of Measure'),
+    quantity = fields.Uom(uom_field='product_uom_id',
         help="The optional quantity expressed by this line, eg: number of product sold. The quantity is not a legal requirement but is very useful for some reports.")
     product_uom_id = fields.Many2one('uom.uom', string='Unit of Measure')
     product_id = fields.Many2one('product.product', string='Product')

yourjelly/odoo

addons/account/models/account_move.py
@@ -2437,8 +2437,8 @@ class AccountMoveLine(models.Model):
     account_root_id = fields.Many2one(related='account_id.root_id', string="Account Root", store=True, readonly=True)
     sequence = fields.Integer(default=10)
     name = fields.Text(string='Label', tracking=True)
-    quantity = fields.Float(string='Quantity',
-        default=1.0, digits='Product Unit of Measure',
+    quantity = fields.Uom(string='Quantity',
+        default=1.0, uom_field='product_uom_id',
         help="The optional quantity expressed by this line, eg: number of product sold. "
              "The quantity is not a legal requirement but is very useful for some reports.")
     price_unit = fields.Float(string='Unit Price', digits='Product Price')