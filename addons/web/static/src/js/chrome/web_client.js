61b9f9820f69b0e502c67cbd65e0100256698ee7
Michael Mattiello (mcm) <mcm@odoo.com>
2020-02-14 10:20:09 +0100

    Fix initial load menu + keep cids in hash

--- addons/web/static/src/js/chrome/web_client.js
+++ addons/web/static/src/js/chrome/web_client.js
@@ -53,8 +53,7 @@ class WebClient extends Component {
 
         const state = this._getUrlState();
         this._determineCompanyIds(state);
-        // if (Object.keys(state).length === 1 && Object.keys(state)[0] === "cids") {
-        if (Object.keys(state).length === 0) {
+        if (Object.keys(state).length === 1 && Object.keys(state)[0] === "cids") {
             const menuID = this.menus && this.menus.root.children[0];
             if (!menuID) {
                 return Promise.resolve();
@@ -261,6 +260,8 @@ class WebClient extends Component {
                 if (!('title' in state)) {
                     state.title = mainComponent.title;
                 }
+                // keep cids in hash
+                this._determineCompanyIds(state);
                 const scrollPosition = this.renderingInfo.main.controller.scrollPosition;
                 if (scrollPosition) {
                     this._scrollTo(scrollPosition);
