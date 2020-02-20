ae7725389a95b161729ce8d236db02e1bc84a5e6
qsm-odoo <qsm@odoo.com>
2020-02-20 17:17:10 +0100

    [IMP] web_editor, *: integrate new o_cc${xxx} classes in the colorpicker
    *: website
    
    Part of https://github.com/odoo/odoo/pull/45856
    task-2197038

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -1053,10 +1053,14 @@ const ColorpickerUserValueWidget = SelectUserValueWidget.extend({
      * @returns {Promise}
      */
     _renderColorPalette: function () {
-        const options = {};
-        options.selectedColor = this._value;
+        const options = {
+            selectedColor: this._value,
+        };
         if (this.options.dataAttributes.excluded) {
             options.excluded = this.options.dataAttributes.excluded.replace(/ /g, '').split(',');
+        }
+        if (this.options.dataAttributes.withCombinations) {
+            options.withCombinations = !!this.options.dataAttributes.withCombinations;
         }
         const oldColorPalette = this.colorPalette;
         this.colorPalette = new ColorPaletteWidget(this, options);
@@ -1581,9 +1585,19 @@ const SnippetOptionWidget = Widget.extend({
         // other potential color names (to remove) and if we know about a prefix
         // (otherwise we suppose that we should use the actual related color).
         if (params.colorNames && params.colorPrefix) {
-            const classes = params.colorNames.map(c => params.colorPrefix + c);
+            const classes = params.colorNames.map(c => params.colorPrefix + c)
+                .concat(params.colorNames.map(c => `o_cc${c}`));
             this.$target[0].classList.remove(...classes);
 
+            const colorNumber = parseInt(widgetValue);
+            if (!isNaN(colorNumber) && colorNumber % 100 !== 0) {
+                // We got a color name which is a number (different of 100x
+                // grays), those are the special color combinations classes.
+                // Just have to add it (and adding the potential extra class)
+                // then leave.
+                this.$target[0].classList.add(`o_cc${widgetValue}`, params.extraClass);
+                return;
+            }
             if (params.colorNames.includes(widgetValue)) {
                 const originalCSSValue = window.getComputedStyle(this.$target[0])[cssProps[0]];
                 const className = params.colorPrefix + widgetValue;
@@ -1874,7 +1888,9 @@ const SnippetOptionWidget = Widget.extend({
             case 'selectStyle': {
                 if (params.colorPrefix && params.colorNames) {
                     for (const c of params.colorNames) {
-                        if (this.$target[0].classList.contains(params.colorPrefix + c)) {
+                        const colorNumber = parseInt(c);
+                        const className = (isNaN(colorNumber) || colorNumber % 100 === 0) ? `${params.colorPrefix}${c}` : `o_cc${c}`;
+                        if (this.$target[0].classList.contains(className)) {
                             return c;
                         }
                     }
