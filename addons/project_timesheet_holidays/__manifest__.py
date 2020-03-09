549e11fa86367d135558b66683304b979253fd56
Victor Feyens <vfe@odoo.com>
2020-03-09 17:44:55 +0100

    [IMP] *: remove useless ACLs

--- addons/project_timesheet_holidays/__manifest__.py
+++ addons/project_timesheet_holidays/__manifest__.py
@@ -17,8 +17,6 @@ on leaves. Project and task can be configured company-wide.
     'data': [
         'views/res_config_settings_views.xml',
         'views/hr_holidays_views.xml',
-        'security/ir.model.access.csv',
-
     ],
     'demo': [],
     'installable': True,
