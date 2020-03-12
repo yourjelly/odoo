1a95e9a3264b6efd07fe80adaa015d88689e69fc
Christophe Monniez <moc@odoo.com>
2020-03-12 11:34:34 +0100

    m

--- addons/website/static/tests/tours/rte.js
+++ addons/website/static/tests/tours/rte.js
@@ -29,7 +29,7 @@ tour.register('rte_translator', {
 }, {
     content : "click language dropdown",
     trigger : '.js_language_selector .dropdown-toggle',
-    timeout: 30000,
+    timeout: 19000,
 }, {
     content: "go to english version",
     trigger: '.js_language_selector a[data-url_code="en"]',
