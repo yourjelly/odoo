47466fff22d0401b56162dfa08c388ce8fda501a
Krunal Trivedi <ktr@odoo.com>
2020-04-01 11:16:05 +0530

    [FIX] website_blog: fix last update date
    wip
    
    task-2180236

--- addons/website_blog/models/website_blog.py
+++ addons/website_blog/models/website_blog.py
@@ -163,7 +163,7 @@ class BlogPost(models.Model):
     post_date = fields.Datetime('Publishing date', compute='_compute_post_date', inverse='_set_post_date', store=True,
                                 help="The blog post will be visible for your visitors as of this date on the website if it is set as published.")
     create_uid = fields.Many2one('res.users', 'Created by', index=True, readonly=True)
-    write_date = fields.Datetime('Last Updated on', index=True, readonly=True)
+    last_update_on = fields.Datetime('Last Updated on', index=True, readonly=True)
     write_uid = fields.Many2one('res.users', 'Last Contributor', index=True, readonly=True)
     author_avatar = fields.Binary(related='author_id.image_small', string="Avatar", readonly=False)
     visits = fields.Integer('No of Views', copy=False)
