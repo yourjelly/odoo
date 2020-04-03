b147faa52a670ecbfbf56dd7b907c5c525ad7d7a
qsm-odoo <qsm@odoo.com>
2020-04-03 17:50:33 +0200

    tmp

--- addons/web/static/src/js/public/lazyloader.js
+++ addons/web/static/src/js/public/lazyloader.js
@@ -80,28 +80,24 @@ if (document.readyState === 'complete') {
 }
 
 /**
- * @param {DOMElement[]} scripts
- * @param {integer} index
+ * @param {DOMElement[]} [scripts]
+ * @param {integer} [index]
  */
 function _loadScripts(scripts, index) {
     if (scripts === undefined) {
         scripts = document.querySelectorAll('script[data-src]');
     }
-    if (index === undefined) {
-        index = 0;
+    // TODO remove index in master, kept for stability during a fix
+    for (var i = index || 0, l = scripts.length; i < l; i++) {
+        var script = scripts[i];
+        script.src = script.dataset.src;
+        script.removeAttribute('data-src');
     }
-    if (index >= scripts.length) {
-        if (typeof doResolve === 'function') {
-            doResolve();
-        } else {
-            doResolve = true;
-        }
-        return;
+    if (typeof doResolve === 'function') {
+        doResolve();
+    } else {
+        doResolve = true;
     }
-    var script = scripts[index];
-    script.addEventListener('load', _loadScripts.bind(this, scripts, index + 1));
-    script.src = script.dataset.src;
-    script.removeAttribute('data-src');
 }
 
 return {
