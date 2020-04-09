539df1beec74a69552795378a699cdbb31ca1f05
jerome hanke (jhk) <jhk@odoo.com>
2020-04-09 15:08:43 +0200

    [FIX] website_crm: state_id=false not shown in lead description
    Steps to reproduce:
    - install website, crm
    - go to crm > settings > activate leads
    - add a contact form to your website and submit
    - go to the created lead
    
    Previous behavior:
    state_id is shown as false when geoip data is empty
    
    Current behavior:
    state_id is not shwon if geoip is empty
    
    opw-2218498

--- addons/website_crm/controllers/main.py
+++ addons/website_crm/controllers/main.py
@@ -11,12 +11,13 @@ class WebsiteForm(WebsiteForm):
     @http.route('/website_form/<string:model_name>', type='http', auth="public", methods=['POST'], website=True)
     def website_form(self, model_name, **kwargs):
         if model_name == 'crm.lead' and not request.params.get('state_id'):
-            geoip_country_code = request.session.get('geoip', {}).get('country_code')
-            geoip_state_code = request.session.get('geoip', {}).get('region')
-            if geoip_country_code and geoip_state_code:
-                State = request.env['res.country.state']
-                request.params['state_id'] = State.search([('code', '=', geoip_state_code), ('country_id.code', '=', geoip_country_code)]).id
-
+            geoip =  request.session.get('geoip', {})
+            if geoip:
+                geoip_country_code = geoip.get('country_code')
+                geoip_state_code = geoip.get('region')
+                if geoip_country_code and geoip_state_code:
+                    State = request.env['res.country.state']
+                    request.params['state_id'] = State.search([('code', '=', geoip_state_code), ('country_id.code', '=', geoip_country_code)]).id
         return super(WebsiteForm, self).website_form(model_name, **kwargs)
 
     def insert_record(self, request, model, values, custom, meta=None):
