10b81b82a04e2424fea524ec972a5a3dd2f2f4ad
Mantavya Gajjar <mga@openerp.com>
2020-04-20 20:42:25 +0530

    [WIP]: improve the label on field and view

--- addons/l10n_in/models/res_config_settings.py
+++ addons/l10n_in/models/res_config_settings.py
@@ -7,5 +7,7 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    group_l10n_in_reseller = fields.Boolean(implied_group='l10n_in.group_l10n_in_reseller', string="Manage Reseller(E-Commerce)")
-    module_l10n_in_einvoice = fields.Boolean("E-invoice")
+    group_l10n_in_reseller = fields.Boolean(implied_group='l10n_in.group_l10n_in_reseller', 
+    	string="Manage Reseller(E-Commerce)")
+    module_l10n_in_einvoice = fields.Boolean(string='e-Invoice System', 
+    	help='Upload Invoices to Invoice Registration Portal (IRP), managed by the GST Network(GSTN)')
