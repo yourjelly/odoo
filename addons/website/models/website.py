9d2c8a40e49f83b0a3c37d0cee6c971ff52cabbb
Okan SUMER (osu) <osu@odoo.com>
2020-04-28 18:05:36 +0200

    [IMP] Edit mode redirection after theme selection
    This commit redirect the user to the website in the edit mode
    after he chose a theme
    
    Part of https://github.com/odoo/odoo/pull/44328
    
    task-2172208

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -908,11 +908,14 @@ class Website(models.Model):
             return self.env.ref('website.backend_dashboard').read()[0]
         return self.env.ref('website.action_website').read()[0]
 
-    def button_go_website(self):
+    def button_go_website(self, force_edit):
         self._force()
+        param = ''
+        if force_edit:
+            param += '?enable_editor=1'
         return {
             'type': 'ir.actions.act_url',
-            'url': '/',
+            'url': '/%s' % param,
             'target': 'self',
         }
 
