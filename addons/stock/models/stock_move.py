b5a41afcf153161ab692ca36358837acdcbca690
Simon Lejeune <sle@openerp.com>
2020-04-29 09:01:44 +0200

    [REF] stock: make the compute of reservation work in onchange
    i.e. don't use read_group which will ignore new records.
    
    task-2241471

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -332,10 +332,12 @@ class StockMove(models.Model):
         and is represented by the aggregated `product_qty` on the linked move lines. If the move
         is force assigned, the value will be 0.
         """
-        result = {data['move_id'][0]: data['product_qty'] for data in
-            self.env['stock.move.line'].read_group([('move_id', 'in', self.ids)], ['move_id','product_qty'], ['move_id'])}
+        result = defaultdict(lambda: 0)
+        for res in self.env['stock.move.line'].search_read([('move_id', 'in', self.ids)], ['move_id', 'product_qty']):
+            result[res['move_id'][0]] += res['product_qty']
         for rec in self:
-            rec.reserved_availability = rec.product_id.uom_id._compute_quantity(result.get(rec.id, 0.0), rec.product_uom, rounding_method='HALF-UP')
+            rec.reserved_availability = rec.product_id.uom_id._compute_quantity(result.get(rec._origin.id or rec.id, 0.0), rec.product_uom, rounding_method='HALF-UP')
+
 
     @api.depends('state', 'product_id', 'product_qty', 'location_id')
     def _compute_product_availability(self):
