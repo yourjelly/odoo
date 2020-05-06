2bf8f08829cf5fe8550b8030d3c96c36e73622a9
Rushi Sathawar <rus@odoo.com>
2020-05-06 17:22:51 +0530

    [IMP] sale_team : find another domain

--- addons/sale_crm/models/crm_team.py
+++ addons/sale_crm/models/crm_team.py
@@ -15,7 +15,10 @@ class CrmTeam(models.Model):
 
     def action_primary_channel_button(self):
         if self._context.get('in_sales_app') and self.use_opportunities:
-            return self.env.ref('sale.action_order_report_so_salesteam').read()[0]
+            result = str(self.env.ref('sale.action_order_report_so_salesteam').read()[0])
+            print(result)
+            return result
+            # return self.env.ref('sale.action_order_report_so_salesteam').read()[0]
         return super(CrmTeam,self).action_primary_channel_button()
     
     def _graph_get_model(self):
