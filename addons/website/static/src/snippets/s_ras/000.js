357717c106f4937881b06a637313d9876603688b
Ravi Singh <ras@odoo.com>
2020-06-03 15:32:14 +0530

    wip

--- addons/website/static/src/snippets/s_ras/000.js
+++ addons/website/static/src/snippets/s_ras/000.js
@@ -5,13 +5,24 @@ const publicWidget = require('web.public.widget');
 
 const CountWidget = publicWidget.Widget.extend({
     selector: '.s_count',
+    events: {
+        'click .plus': '_onPlusClick',
+        'click .minus': '_onMinusClick',
+    },
 
     /**
      * @override
      */
     start: function () {
-       debugger;
+       this.resultEL = this.$el.find('.result')[0];
     },
+
+    _onPlusClick: function () {
+        this.resultEL.innerHTML = parseInt(this.resultEL.innerHTML)+1;
+    },
+    _onMinusClick: function () {
+        this.resultEL.innerHTML = parseInt(this.resultEL.innerHTML)-1;
+    }
 });
 
 publicWidget.registry.CountWidget = CountWidget;
