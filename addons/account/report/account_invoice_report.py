128c93b0bd2d534005df493c57f60528ecccbd8b
william <wan@odoo.com>
2020-06-03 14:53:29 +0200

    poc

--- addons/account/report/account_invoice_report.py
+++ addons/account/report/account_invoice_report.py
@@ -12,43 +12,21 @@ class AccountInvoiceReport(models.Model):
     _auto = False
     _rec_name = 'invoice_date'
     _order = 'invoice_date desc'
+    _inherits = {'account.move.line': 'line_id'}
 
     # ==== Invoice fields ====
-    move_id = fields.Many2one('account.move', readonly=True)
-    journal_id = fields.Many2one('account.journal', string='Journal', readonly=True)
-    company_id = fields.Many2one('res.company', string='Company', readonly=True)
-    company_currency_id = fields.Many2one('res.currency', string='Company Currency', readonly=True)
+    line_id = fields.Many2one('account.move.line', required=True, ondelete="cascade")
     partner_id = fields.Many2one('res.partner', string='Partner', readonly=True)
     commercial_partner_id = fields.Many2one('res.partner', string='Partner Company', help="Commercial Entity")
     country_id = fields.Many2one('res.country', string="Country")
-    invoice_user_id = fields.Many2one('res.users', string='Salesperson', readonly=True)
-    move_type = fields.Selection([
-        ('out_invoice', 'Customer Invoice'),
-        ('in_invoice', 'Vendor Bill'),
-        ('out_refund', 'Customer Credit Note'),
-        ('in_refund', 'Vendor Credit Note'),
-        ], readonly=True)
-    state = fields.Selection([
-        ('draft', 'Draft'),
-        ('posted', 'Open'),
-        ('cancel', 'Cancelled')
-        ], string='Invoice Status', readonly=True)
-    payment_state = fields.Selection(selection=[
-        ('not_paid', 'Not Paid'),
-        ('in_payment', 'In Payment'),
-        ('paid', 'paid')
-    ], string='Payment Status', readonly=True)
-    fiscal_position_id = fields.Many2one('account.fiscal.position', string='Fiscal Position', readonly=True)
+    invoice_user_id = fields.Many2one('res.users', related='move_id.invoice_user_id')
     invoice_date = fields.Date(readonly=True, string="Invoice Date")
 
     # ==== Invoice line fields ====
     quantity = fields.Float(string='Product Quantity', readonly=True)
-    product_id = fields.Many2one('product.product', string='Product', readonly=True)
     product_uom_id = fields.Many2one('uom.uom', string='Unit of Measure', readonly=True)
     product_categ_id = fields.Many2one('product.category', string='Product Category', readonly=True)
     invoice_date_due = fields.Date(string='Due Date', readonly=True)
-    account_id = fields.Many2one('account.account', string='Revenue/Expense Account', readonly=True, domain=[('deprecated', '=', False)])
-    analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic Account', groups="analytic.group_analytic_accounting")
     price_subtotal = fields.Float(string='Untaxed Total', readonly=True)
     price_average = fields.Float(string='Average Price', readonly=True, group_operator="avg")
 
@@ -74,20 +52,9 @@ class AccountInvoiceReport(models.Model):
         return '''
             SELECT
                 line.id,
-                line.move_id,
-                line.product_id,
-                line.account_id,
-                line.analytic_account_id,
-                line.journal_id,
-                line.company_id,
-                line.company_currency_id,
+                line.id AS line_id,
                 line.partner_id AS commercial_partner_id,
-                move.state,
-                move.move_type,
                 move.partner_id,
-                move.invoice_user_id,
-                move.fiscal_position_id,
-                move.payment_state,
                 move.invoice_date,
                 move.invoice_date_due,
                 uom_template.id                                             AS product_uom_id,
