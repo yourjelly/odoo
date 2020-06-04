6adcfdf1c9946625c89b08aa61cf8fa099043f86
Ravi Singh <ras@odoo.com>
2020-06-04 14:39:02 +0530

    wip

---
+++ addons/website/static/src/snippets/s_ras/options.js
@@ -0,0 +1,13 @@
+odoo.define('website.s_ras_options', function (reauire) {
+'use strict';
+
+const options = require('web_editor.snippets.options');
+
+   options.registry.SnipetCounter = options.Class.extend({
+    // isTopOption: true,
+    start: function () {
+        debugger;
+    },
+    
+});
+});
