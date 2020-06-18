0c38ba4ef96ad41b13d036cceaa8420aa3590932
Goaman <nby@odoo.com>
2020-06-18 15:34:42 +0200

    [FIX] navbar

--- addons/website/static/src/snippets/s_tabs/options.js
+++ addons/website/static/src/snippets/s_tabs/options.js
@@ -9,21 +9,24 @@ snippetOptions.registry.NavTabs = snippetOptions.SnippetOptionsWidget.extend({
     /**
      * @override
      */
-    start: function () {
+    start: async function () {
         this._findLinksAndPanes();
-        return this._super.apply(this, arguments);
+        await this._super.apply(this, arguments);
+        await this._refreshTarget();
     },
     /**
      * @override
      */
-    onBuilt: function () {
+    onBuilt: async function () {
         this._generateUniqueIDs();
+        await this._refreshTarget();
     },
     /**
      * @override
      */
-    onClone: function () {
+    onClone: async function () {
         this._generateUniqueIDs();
+        this._refreshTarget();
     },
 
     //--------------------------------------------------------------------------
@@ -35,7 +38,8 @@ snippetOptions.registry.NavTabs = snippetOptions.SnippetOptionsWidget.extend({
      *
      * @see this.selectClass for parameters
      */
-    addTab: function (previewMode, widgetValue, params) {
+    addTab: async function (previewMode, widgetValue, params) {
+        this._findLinksAndPanes();
         var $activeItem = this.$navLinks.filter('.active').parent();
         var $activePane = this.$tabPanes.filter('.active');
 
@@ -44,17 +48,21 @@ snippetOptions.registry.NavTabs = snippetOptions.SnippetOptionsWidget.extend({
         var $tabPane = $activePane.clone().removeClass('active show');
         $navItem.insertAfter($activeItem);
         $tabPane.insertAfter($activePane);
-        this._findLinksAndPanes();
+
+        if (previewMode === false) await this._refreshTarget();
+
         this._generateUniqueIDs();
 
         $navLink.tab('show');
+
     },
     /**
      * Removes the current active tab and its content.
      *
      * @see this.selectClass for parameters
      */
-    removeTab: function (previewMode, widgetValue, params) {
+    removeTab: async function (previewMode, widgetValue, params) {
+        this._findLinksAndPanes();
         var self = this;
 
         var $activeLink = this.$navLinks.filter('.active');
@@ -62,7 +70,7 @@ snippetOptions.registry.NavTabs = snippetOptions.SnippetOptionsWidget.extend({
 
         var $next = this.$navLinks.eq((this.$navLinks.index($activeLink) + 1) % this.$navLinks.length);
 
-        return new Promise(resolve => {
+        await new Promise(resolve => {
             $next.one('shown.bs.tab', function () {
                 $activeLink.parent().remove();
                 $activePane.remove();
@@ -71,6 +79,8 @@ snippetOptions.registry.NavTabs = snippetOptions.SnippetOptionsWidget.extend({
             });
             $next.tab('show');
         });
+
+        if (previewMode === false) await this._refreshTarget();
     },
 
     //--------------------------------------------------------------------------
@@ -126,39 +136,30 @@ snippetOptions.registry.NavTabsStyle = snippetOptions.SnippetOptionsWidget.exten
      * @see this.selectClass for parameters
      */
     setStyle: async function (previewMode, widgetValue, params) {
-        await this.wysiwyg.editor.execBatch(async ()=> {
-            const $nav = this.$target.find('.s_tabs_nav:first .nav');
-            const isPills = widgetValue === 'pills';
-            const firstTab = this.$target.find('.s_tabs_nav:first')[0];
-            if (isPills) {
-                await this.editorHelpers.removeClass($nav, 'nav-tabs card-header-tabs');
-                await this.editorHelpers.addClass($nav, 'nav-pills');
-                await this.editorHelpers.removeClass(firstTab, 'card-header');
-                await this.editorHelpers.addClass(firstTab, 'mb-3');
-                await this.editorHelpers.removeClass(this.$target[0], 'card');
-                await this.editorHelpers.removeClass(this.$target.find('.s_tabs_content:first')[0], 'card-body');
-            } else {
-                await this.editorHelpers.addClass($nav, 'nav-tabs card-header-tabs');
-                await this.editorHelpers.removeClass($nav, 'nav-pills');
-                await this.editorHelpers.addClass(firstTab, 'card-header');
-                await this.editorHelpers.removeClass(firstTab, 'mb-3');
-                await this.editorHelpers.addClass(this.$target[0], 'card');
-                await this.editorHelpers.addClass(this.$target.find('.s_tabs_content:first')[0], 'card-body');
-            }
-        });
+        const $nav = this.$target.find('.s_tabs_nav:first .nav');
+        const isPills = widgetValue === 'pills';
+        $nav.toggleClass('nav-tabs card-header-tabs', !isPills);
+        $nav.toggleClass('nav-pills', isPills);
+        this.$target.find('.s_tabs_nav:first').toggleClass('card-header', !isPills).toggleClass('mb-3', isPills);
+        this.$target.toggleClass('card', !isPills);
+        this.$target.find('.s_tabs_content:first').toggleClass('card-body', !isPills);
+
+        if (previewMode === false) await this._refreshTarget();
     },
     /**
      * Horizontal/vertical nav.
      *
      * @see this.selectClass for parameters
      */
-    setDirection: function (previewMode, widgetValue, params) {
+    setDirection: async function (previewMode, widgetValue, params) {
         const isVertical = widgetValue === 'vertical';
         this.$target.toggleClass('row s_col_no_resize s_col_no_bgcolor', isVertical);
         this.$target.find('.s_tabs_nav:first .nav').toggleClass('flex-column', isVertical);
         this.$target.find('.s_tabs_nav:first > .nav-link').toggleClass('py-2', isVertical);
         this.$target.find('.s_tabs_nav:first').toggleClass('col-md-3', isVertical);
         this.$target.find('.s_tabs_content:first').toggleClass('col-md-9', isVertical);
+
+        if (previewMode === false) await this._refreshTarget();
     },
 
     //--------------------------------------------------------------------------
