ac7a8a049e2a1f5a959a6502851f0e2dc64823cc
Thibault Delavallée <tde@odoo.com>
2020-06-24 10:19:26 +0200

    [ADD] website_event_*: add Online Event support base modules
    PURPOSE
    
    SPECIFICATIONS
    
    LINKS
    
    Task ID 2252655 (master Online Event task)

---
+++ addons/website_event_track_live/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Live Event Tracks',
+    'category': 'Marketing/Events',
+    'sequence': 1002,
+    'version': '1.0',
+    'summary': 'Support live tracks: streaming, participation, redesigned tracks',
+    'website': 'https://www.odoo.com/page/events',
+    'description': "",
+    'depends': [
+        'website_event_track'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': True,
+    'installable': True,  # set to False
+}
