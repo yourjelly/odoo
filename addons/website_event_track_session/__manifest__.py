bb14a67904d6b1184bd3f2069b981b1623ba6496
Thibault Delavallée <tde@odoo.com>
2020-06-24 10:19:26 +0200

    [ADD] website_event_*: add Online Event support base modules
    PURPOSE
    
    SPECIFICATIONS
    
    This commit simply adds modules to be completed while working on new Online
    Event feature. This is mainly done to ease future work as Online Event was
    developed in a stable release, leading to some restrictions for pre-merging
    and organizing commits.
    
    LINKS
    
    Task ID 2252655 (master Online Event task)
    Task ID 2283796 (Event B2Basics)

---
+++ addons/website_event_track_session/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Event Lobby',
+    'category': 'Marketing/Events',
+    'sequence': 1005,
+    'version': '1.0',
+    'summary': 'Event: lobby',
+    'website': 'https://www.odoo.com/page/events',
+    'description': "",
+    'depends': [
+        'website_event_track_online',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': True,
+    'installable': True,  # set to False
+}
