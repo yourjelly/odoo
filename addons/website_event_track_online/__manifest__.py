2eb909c571be1d66ab00533af326c36bc7b17591
Thibault Delavallée <tde@odoo.com>
2020-07-07 10:22:48 +0200

    [IMP] website_event_track_online: improve sponsor partner synchronization
    SPECIFICATIONS: PARTNER SYNCHRONIZATION
    
    Right now, information on the sponsor page in frontend is dependent on partner
    through related fields. It is not possible to have contact data specific to
    the sponsor without modifying the related partner.
    
    In this commit we make sponsor a model based on partner while allowing to
    update its contact information. Name, email, mobile and phone are not computed
    stored based on partner and editable.
    
    A new image_512 field is introduced, as base image field is too small for
    a new list view of sponsors we plan to make.
    
    LINKS
    
    Task ID-2252655 (Main Online Event task)
    Task ID-2283796 (Event B2Basics / Registration Flow)
    Task ID-2283738 (Sponsors -> Exhibitors upgrade)

--- addons/website_event_track_online/__manifest__.py
+++ addons/website_event_track_online/__manifest__.py
@@ -19,6 +19,7 @@
         'security/ir.model.access.csv',
         'views/assets.xml',
         'views/event_event_views.xml',
+        'views/event_sponsor_views.xml',
         'views/event_templates.xml',
         'views/event_track_templates_reminder.xml',
         'views/event_track_templates.xml',
@@ -29,6 +30,7 @@
         'views/event_menus.xml',
     ],
     'demo': [
+        'data/event_sponsor_demo.xml',
         'data/event_track_demo.xml',
         'data/event_track_tag_demo.xml',
         'data/event_track_visitor_demo.xml',
