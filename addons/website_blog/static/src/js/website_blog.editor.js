c52c7cd386de60970b021196bd4d0bbe4c8682b2
DramixDw <rve@odoo.com>
2020-07-08 16:23:39 +0200

    [IMP] website_blog: add tag from frontend (editor left panel)
    Previously, you could only add a tag on a blog post from the backend.
    Here we're adding a way to add them from the left-panel so that it's
    easier for the user to add them.
    
    The long term goal will not be to reimplement complex view of the
    backend. This is actually already past the limit: we will need a way
    to instantiate views from the backend in the frontend in the future.
    
    Part of https://github.com/odoo/odoo/pull/52759
    task-2267693
    
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/website_blog/static/src/js/website_blog.editor.js
+++ addons/website_blog/static/src/js/website_blog.editor.js
@@ -62,23 +62,105 @@ odoo.define('website_blog.editor', function (require) {
 'use strict';
 
 require('web.dom_ready');
-var core = require('web.core');
-var options = require('web_editor.snippets.options');
+const {qweb, _t} = require('web.core');
+const options = require('web_editor.snippets.options');
 var WysiwygMultizone = require('web_editor.wysiwyg.multizone');
-
-var _t = core._t;
 
 if (!$('.website_blog').length) {
     return Promise.reject("DOM doesn't contain '.website_blog'");
 }
 
 WysiwygMultizone.include({
+    custom_events: Object.assign({}, WysiwygMultizone.prototype.custom_events, {
+        'set_blog_post_update_data': '_onSetBlogPostUpdateData',
+    }),
+
     /**
      * @override
      */
-    start: function () {
+    init() {
+        this._super(...arguments);
+        this.blogTagsPerBlogPost = {};
+    },
+    /**
+     * @override
+     */
+    async start() {
+        await this._super(...arguments);
         $('.js_tweet, .js_comment').off('mouseup').trigger('mousedown');
-        return this._super.apply(this, arguments);
+    },
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
+    async save() {
+        const _super = this._super.bind(this);
+        await this._saveBlogTags();
+        return _super(...arguments);
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Saves the blog tags in the database.
+     *
+     * @private
+     */
+    async _saveBlogTags() {
+        for (const [blogPostID, data] of Object.entries(this.blogTagsPerBlogPost)) {
+            const additionsProms = data.additions.map(tag => {
+                if (typeof tag.id === 'string') {
+                    return this._rpc({
+                        model: 'blog.tag',
+                        method: 'create',
+                        args: [{
+                            'name': tag.name,
+                            'post_ids': [blogPostID],
+                        }],
+                    });
+                } else {
+                    return this._rpc({
+                        model: 'blog.tag',
+                        method: 'write',
+                        args: [tag.id, {
+                            'post_ids': [[4, blogPostID, 0]],
+                        }],
+                    });
+                }
+            });
+            const removalProms = data.removals.map(tag => {
+                return this._rpc({
+                    model: 'blog.tag',
+                    method: 'write',
+                    args: [tag.id, {
+                        'post_ids': [[3, blogPostID, 0]],
+                    }],
+                });
+            });
+            await Promise.all(additionsProms);
+            await Promise.all(removalProms);
+        }
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     * @param {OdooEvent} ev
+     */
+    _onSetBlogPostUpdateData: function (ev) {
+        this.blogTagsPerBlogPost[ev.data.blogPostID] = {
+            additions: ev.data.blogTagsAdditions,
+            removals: ev.data.blogTagsRemovals,
+        };
     },
 });
 
@@ -123,6 +205,171 @@ options.registry.CoverProperties.include({
         $coverFull.children('div').text(isRegularCover ? _t("Large") : this._coverFullOriginalLabel);
         $coverMid.children('div').text(isRegularCover ? _t("Medium") : this._coverMidOriginalLabel);
         $coverAuto.children('div').text(isRegularCover ? _t("Tiny") : this._coverAutoOriginalLabel);
+    },
+});
+
+const NEW_TAG_PREFIX = 'new-blog-tag-';
+
+options.registry.BlogPostTagSelection = options.Class.extend({
+    xmlDependencies: (options.Class.prototype.xmlDependencies || [])
+        .concat(['/website_blog/static/src/xml/website_blog_tag.xml']),
+
+    /**
+     * @override
+     */
+    async willStart() {
+        await this._super(...arguments);
+        this.blogPostID = parseInt(this.$target[0].dataset.blogId);
+        this.isEditingTags = false;
+        this.allTags = await this._rpc({
+            model: 'blog.tag',
+            method: 'search_read',
+            args: [[], ['id', 'name', 'post_ids']],
+        });
+        this.tagIDs = this.allTags.filter(tag => {
+            return tag['post_ids'].includes(this.blogPostID);
+        });
+    },
+    /**
+     * @override
+     */
+    cleanForSave() {
+        if (this.isEditingTags) {
+            this._notifyUpdatedTags();
+        }
+    },
+
+    //--------------------------------------------------------------------------
+    // Options
+    //--------------------------------------------------------------------------
+
+    /**
+     * @see this.selectClass for params
+     */
+    editTagList(previewMode, widgetValue, params) {
+        this._toggleEditUI(true);
+    },
+    /**
+     * Send changes that will be saved in the database.
+     *
+     * @see this.selectClass for params
+     */
+    saveTagList(previewMode, widgetValue, params) {
+        this._toggleEditUI(false);
+        this._notifyUpdatedTags();
+    },
+    /**
+     * @see this.selectClass for params
+     */
+    setNewTagName(previewMode, widgetValue, params) {
+        this.newTagName = widgetValue;
+    },
+    /**
+     * @see this.selectClass for params
+     */
+    confirmNew(previewMode, widgetValue, params) {
+        if (!this.newTagName) {
+            return;
+        }
+        const existing = this.allTags.some(tag => tag.name.toLowerCase() === this.newTagName.toLowerCase());
+        if (existing) {
+            return this.displayNotification({
+                type: 'warning',
+                message: _t("This tag already exists"),
+            });
+        }
+        const newTag = {
+            'id': _.uniqueId(NEW_TAG_PREFIX),
+            'name': this.newTagName,
+        };
+        this.allTags.push(newTag);
+        this.tagIDs.push(newTag);
+        this.newTagName = '';
+        this.rerender = true;
+    },
+    /**
+     * @see this.selectClass for params
+     */
+    addTag(previewMode, widgetValue, params) {
+        const tagID = parseInt(widgetValue);
+        const tag = this.allTags.find(tag => tag.id === tagID);
+        this.tagIDs.push(tag);
+        this.rerender = true;
+    },
+    /**
+     * @see this.selectClass for params
+     */
+    removeTag(previewMode, widgetValue, params) {
+        this.tagIDs = this.tagIDs.filter(tag => (`${tag.id}` !== widgetValue));
+        if (widgetValue.startsWith(NEW_TAG_PREFIX)) {
+            this.allTags.filter(tag => tag.id !== widgetValue);
+        }
+        this.rerender = true;
+    },
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
+    async updateUI() {
+        if (this.rerender) {
+            this.rerender = false;
+            await this._rerenderXML();
+            return;
+        }
+        return this._super(...arguments);
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     */
+    _notifyUpdatedTags() {
+        this.trigger_up('set_blog_post_update_data', {
+            blogPostID: this.blogPostID,
+            blogTagsAdditions: this.blogTagsAdditions,
+            blogTagsRemovals: this.blogTagsRemovals,
+        });
+    },
+    /**
+     * @override
+     */
+    _renderCustomXML: async function (uiFragment) {
+        const $tagList = $(uiFragment.querySelector('[data-name="tag_list"]'));
+        const $select = $(uiFragment.querySelector('[data-name="add_tag_opt"] we-select'));
+        for (const tag of this.tagIDs) {
+            $tagList.append(qweb.render('website_blog.tagListItem', {
+                tag: tag,
+                isEditingTags: this.isEditingTags,
+            }));
+        }
+        const availableTags = this.allTags.filter(tag => !this.tagIDs.includes(tag));
+        for (const tag of availableTags) {
+            $select.prepend(qweb.render('website_blog.tagSelectItem', {
+                tag: tag,
+                isEditingTags: this.isEditingTags,
+            }));
+        }
+    },
+    /**
+     * Toggles the UI between read/edit mode.
+     *
+     * @private
+     * @param {boolean} edit
+     */
+    _toggleEditUI: function (edit) {
+        this.isEditingTags = edit;
+        const $editMenu = $('[data-name="add_tag_opt"]');
+        $editMenu.toggleClass('d-none', !this.isEditingTags);
+        this.$el.find('.o_we_edit_tag_btn').toggleClass('d-none', this.isEditingTags);
+        this.$el.find('.o_we_save_tag_btn').toggleClass('d-none', !this.isEditingTags);
+        this.rerender = true;
     },
 });
 });
