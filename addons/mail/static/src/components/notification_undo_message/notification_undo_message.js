f33005169befbc4fc5183b0a90f9ee0436d34cfc
Priyanka Kakadiya <pka@odoo.com>
2020-07-08 11:38:38 +0530

    [IMP] mail: remove message by clicking on undo

--- addons/mail/static/src/components/notification_undo_message/notification_undo_message.js
+++ addons/mail/static/src/components/notification_undo_message/notification_undo_message.js
@@ -1,10 +1,21 @@
 odoo.define('mail/static/src/components/notification_undo_message/notification_undo_message.js', function (require) {
     'use strict';
 
+    const useStore = require('mail/static/src/component_hooks/use_store/use_store.js');
     const { Component } = owl;
 
     class NotificationUndoMessage extends Component {
 
+        async _onClickUndo(ev) {
+            const message = this.env.models['mail.message'].get(this.props.messageLocalId);
+            await this.env.services.rpc({
+                model: 'mail.message',
+                method: 'unlink',
+                args: [[message.id]],
+            });
+            message.delete();
+            this.destroy();
+        }
 
     }
 
