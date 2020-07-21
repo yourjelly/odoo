bf1b25c36af57ba8088107cbde4f039b2674427b
Nikunj Ladava <nla@odoo.com>
2020-07-21 12:56:21 +0530

    [WIP] l10n_co_pos: country wise report

--- addons/l10n_co_pos/wizard/report.py
+++ addons/l10n_co_pos/wizard/report.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, _
+from odoo.exceptions import UserError
 
 
 class PosDetails(models.TransientModel):
@@ -13,6 +14,8 @@ class PosDetails(models.TransientModel):
 
     def generate_report(self):
         data = {'date_start': self.start_date, 'date_stop': self.end_date, 'config_ids': self.pos_config_ids.ids, 'include_products': self.include_products}
-        if all(code == 'CO' for code in self.pos_config_ids.mapped('company_id.country_id.code')):
+        if self.country_code == 'CO' and any(code != 'CO' for code in self.pos_config_ids.mapped('company_id.country_id.code')):
+            raise UserError(_("You are trying to Print non Colombian company's report from Colombian company."))
+        elif self.country_code == 'CO':
             return self.env.ref('l10n_co_pos.sale_details_report').report_action([], data=data)
         return self.env.ref('point_of_sale.sale_details_report').report_action([], data=data)
