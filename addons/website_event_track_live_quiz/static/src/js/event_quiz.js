87920d781b98c1c1390ab96218e3c0e4f15ac7a8
Aurélien Warnon <awa@odoo.com>
2020-07-23 16:12:10 +0200

    [ADD] website_event_track_live_quiz: integrate quiz withing live track
    RATIONALE
    
    Events are sometimes held online, gathering a community. In this merge
    we improve Event application to better support full-online events with
    improved tracks, wishlists, chat rooms, ...
    
    PURPOSE
    
    This bridge module enables a better integration of the quiz features within the
    "next track suggestion" at the end of a track video.
    
    The track suggestion mechanism will let the user choose to take the quiz before
    being redirected to the next track.
    If he takes no action, he will be redirected automatically.
    
    LINKS
    
    Task 2283729
    
    Co-Authored-By: Aurélien Warnon <awa@odoo.com>
    Co-Authored-By: David Beguin <dbe@odoo.com>
    Co-Authored-By: Quentin Mourier <qmo@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>
    
    [MERGEUP] website_event_track_live_quiz: Add next track button on quiz
    
    This commit adds a "next track" button in the quiz itself next to the
    submit button. Clicking on it will redirect the visitor to the next suggested track.
    
    MERGEUP website_event_track_live_quiz
    
    Improved "next suggestion" views

---
+++ addons/website_event_track_live_quiz/static/src/js/event_quiz.js
@@ -0,0 +1,39 @@
+odoo.define('website_event_track_live_quiz.event_quiz', function (require) {
+'use strict';
+
+var Quiz = require('website_event_track_quiz.event.quiz').Quiz;
+
+var WebsiteEventTrackSuggestionQuiz = Quiz.include({
+    xmlDependencies: Quiz.prototype.xmlDependencies.concat([
+        '/website_event_track_live_quiz/static/src/xml/website_event_track_quiz_templates.xml',
+    ]),
+
+    /**
+     * @override
+     */
+    willStart: function () {
+        var defs = [this._super.apply(this, arguments)];
+        defs.push(this._getTrackSuggestion());
+        return Promise.all(defs);
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    _getTrackSuggestion: function () {
+        var self = this;
+        return this._rpc({
+            route: '/event_track/get_track_suggestion',
+            params: {
+                track_id: this.track.id,
+            }
+        }).then(function (suggestion) {
+            self.nextSuggestion = suggestion;
+        });
+    },
+});
+
+return WebsiteEventTrackSuggestionQuiz;
+
+});
