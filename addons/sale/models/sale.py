8f6201adfc47d793a7dcb3c27a0d978e95e0576a
Goffin Simon <sig@odoo.com>
2020-08-02 14:06:45 +0200

    [CUSTO] sale: Taxes in multi companies

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -1045,7 +1045,8 @@ class SaleOrderLine(models.Model):
         for line in self:
             fpos = line.order_id.fiscal_position_id or line.order_id.partner_id.property_account_position_id
             # If company_id is set, always filter taxes by the company
-            taxes = line.product_id.taxes_id.filtered(lambda r: not line.company_id or r.company_id == line.company_id)
+            companies = line.company_id.search([('id', 'child_of', line.company_id.id)]).ids if line.company_id else [False]
+            taxes = line.product_id.taxes_id.filtered(lambda r: line.company_id.id in companies)
             line.tax_id = fpos.map_tax(taxes, line.product_id, line.order_id.partner_shipping_id) if fpos else taxes
 
     @api.model
