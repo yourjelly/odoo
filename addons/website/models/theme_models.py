092deb3e4ac65499a08b33cb71e8ad3e55fe6c75
Cocographique <nmi@odoo.com>
2020-08-03 17:03:14 +0200

    [TMP] website: remove Hamburger options

--- addons/website/models/theme_models.py
+++ addons/website/models/theme_models.py
@@ -192,7 +192,6 @@ class Theme(models.AbstractModel):
         self.disable_view('website.template_header_hamburger')
         self.disable_view('website.template_header_vertical')
         self.disable_view('website.template_header_sidebar')
-        self.disable_view('website.option_header_hamburger_center_logo')
 
         # Reinitialize footer templates
         self.enable_view('website.footer_custom')
