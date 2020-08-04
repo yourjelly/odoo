84359b6b93f04c16e58e83c013863d347a2b89bb
Cocographique <nmi@odoo.com>
2020-08-04 11:05:32 +0200

    [ADD] website: Header 6

--- addons/website/models/theme_models.py
+++ addons/website/models/theme_models.py
@@ -196,6 +196,7 @@ class Theme(models.AbstractModel):
         self.disable_view('website.template_header_3')
         self.disable_view('website.template_header_4')
         self.disable_view('website.template_header_5')
+        self.disable_view('website.template_header_6')
 
         # Reinitialize footer templates
         self.enable_view('website.footer_custom')
