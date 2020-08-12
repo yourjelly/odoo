f9b0f7378d0db27ffe1640a19edab5eb8f26446b
Cocographique <nmi@odoo.com>
2020-08-12 10:22:13 +0200

    [ADD] website: 2 Levels

--- addons/website/models/theme_models.py
+++ addons/website/models/theme_models.py
@@ -192,6 +192,7 @@ class Theme(models.AbstractModel):
         self.disable_view('website.template_header_hamburger')
         self.disable_view('website.template_header_vertical')
         self.disable_view('website.template_header_sidebar')
+        self.disable_view('website.template_header_1')
 
         # Reinitialize footer templates
         self.enable_view('website.footer_custom')
