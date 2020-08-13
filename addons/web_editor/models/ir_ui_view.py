c1a599c8bb00eee25cf071c16e9d7d02bbe070ea
jpr-odoo <jpr@openerp.com>
2020-08-13 14:56:13 +0530

    wip

--- addons/web_editor/models/ir_ui_view.py
+++ addons/web_editor/models/ir_ui_view.py
@@ -81,6 +81,7 @@ class IrUiView(models.Model):
             'arch': self._pretty_arch(arch),
             'key': '%s_%s' % (self.key, el.get('id')),
             'type': 'qweb',
+            'mode': 'extension',
         }
         vals.update(self._save_oe_structure_hook())
         self.create(vals)
