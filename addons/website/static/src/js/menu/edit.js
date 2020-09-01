5ac2b0f08f4b5b9f8cf39a883329f35ac3eb2f15
Jeremy Kersten <jke@odoo.com>
2020-09-01 14:43:35 +0200

    [IMP] website: remove welcome tips on homepage
    Now we only support tour in edit mode already.
    We win a click on onboarding., and after the install of a theme
    
    Sbu's spec

--- addons/website/static/src/js/menu/edit.js
+++ addons/website/static/src/js/menu/edit.js
@@ -57,23 +57,6 @@ var EditPageMenu = websiteNavbarData.WebsiteNavbarActionWidget.extend({
             return Promise.all([def, this._startEditMode()]);
         }
 
-        // Check that the page is empty
-        var $wrap = this._targetForEdition().filter('#wrapwrap.homepage').find('#wrap');
-
-        if ($wrap.length && $wrap.html().trim() === '') {
-            // If readonly empty page, show the welcome message
-            this.$welcomeMessage = $(core.qweb.render('website.homepage_editor_welcome_message'));
-            this.$welcomeMessage.addClass('o_homepage_editor_welcome_message');
-            this.$welcomeMessage.css('min-height', $wrap.parent('main').height() - ($wrap.outerHeight(true) - $wrap.height()));
-            $wrap.empty().append(this.$welcomeMessage);
-        }
-
-        setTimeout(function () {
-            if ($('.o_tooltip.o_animated').length) {
-                $('.o_tooltip_container').addClass('show');
-            }
-        }, 1000); // ugly hack to wait that tooltip is loaded
-
         return def;
     },
 
