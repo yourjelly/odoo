307f2d6010bd8722314ce86395f7f9ef5bec4915
Aaron Bohy <aab@odoo.com>
2020-09-24 10:40:54 +0200

    [REF] rename wowl.js into app.js

--- addons/wowl/rollup.config.js
+++ addons/wowl/rollup.config.js
@@ -2,7 +2,7 @@ export default {
   input: "static/dist/js/main.js",
   external: ["@odoo/owl"],
   output: {
-    file: "static/dist/wowl.js",
+    file: "static/dist/app.js",
     format: "iife",
     name: "wowl",
     extend: true,
