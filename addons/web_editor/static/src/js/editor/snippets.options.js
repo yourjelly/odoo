e6baa85e5f5bfcabdf9192b9a87212dcfc287b41
Goaman <nby@odoo.com>
2020-09-28 12:20:47 +0200

    [FIX] show preview overlay

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -4270,6 +4270,8 @@ registry.BackgroundPosition = SnippetOptionWidget.extend({
         this.trigger_up('activate_snippet', {
             $element: this.$target,
             previewMode: true,
+            saveTarget: true,
+            savePerview: true,
         });
         this.trigger_up('block_preview_overlays');
 
