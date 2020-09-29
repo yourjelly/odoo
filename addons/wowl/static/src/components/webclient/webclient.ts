0d1e5eb68495222037978f2a6d3d1b9a678c1f50
Aaron Bohy <aab@odoo.com>
2020-09-29 10:26:38 +0200

    oups

--- addons/wowl/static/src/components/webclient/webclient.ts
+++ addons/wowl/static/src/components/webclient/webclient.ts
@@ -2,11 +2,9 @@ import { Action } from "../action/action";
 import { Component } from "@odoo/owl";
 import { NavBar } from "../navbar/navbar";
 import { OdooEnv } from "../../env";
-import { useService } from "../../services";
 
 export class WebClient extends Component<{}, OdooEnv> {
   static components = { Action, NavBar };
   static template = "wowl.WebClient";
   Components = this.env.registries.Components.getEntries();
-  notificationService = useService("notifications");
 }
