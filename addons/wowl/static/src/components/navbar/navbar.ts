28d07b48a69d19b403b9db7447c99e35bd26b861
Bruno Boi <boi@odoo.com>
2020-10-05 15:41:46 +0200

    [REF] wowl: remove jQuery from navbar
    Taskid: 2349005

--- addons/wowl/static/src/components/navbar/navbar.ts
+++ addons/wowl/static/src/components/navbar/navbar.ts
@@ -1,7 +1,9 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { useService } from "../../services";
 
 export class NavBar extends Component {
   static template = "wowl.NavBar";
   menuRepo = useService("menus");
+  state = useState({ showDropdownMenu: false });
+  toggleDropdownMenu = () => { this.state.showDropdownMenu = !this.state.showDropdownMenu; }
 }
--- addons/wowl/static/tests/components/navbar_tests.ts
+++ addons/wowl/static/tests/components/navbar_tests.ts
@@ -1,6 +1,6 @@
 import { NavBar } from "../../src/components/navbar/navbar";
 import * as QUnit from "qunit";
-import { createMockedFetch, mount, makeTestEnv, OdooEnv, getFixture } from "../helpers";
+import { click, createMockedFetch, mount, makeTestEnv, OdooEnv, getFixture } from "../helpers";
 import { MenuData, menusService } from "./../../src/services/menus";
 import { Registry } from "./../../src/core/registry";
 import { Service } from "./../../src/services";
@@ -37,4 +37,15 @@ QUnit.test("can be rendered", async (assert) => {
   assert.expect(1);
   const navbar = await mount(NavBar, { env, target });
   assert.containsOnce(navbar.el!, '.o_menu_apps a[role="menuitem"]', "1 app present");
+});
+
+QUnit.test("dropdown menu can be toggled", async (assert) => {
+  assert.expect(2);
+  const navbar = await mount(NavBar, { env, target });
+
+  const dropdown = navbar.el!.querySelector<HTMLElement>('.dropdown-menu')!;
+  await click(navbar.el!, 'a[data-toggle="dropdown"]');
+  assert.hasClass(dropdown, 'show');
+  await click(navbar.el!, 'a[data-toggle="dropdown"]');
+  assert.doesNotHaveClass(dropdown, 'show');
 });
