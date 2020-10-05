bf54c5639504bdfffad1d31c8447d0009b2d56f7
Benoit Socias <bso@odoo.com>
2020-10-05 12:05:59 +0000

    [FIX] website: filter filter_opt of dynamic snippet by website_id
    Add a filtering by the contextual website_id when looking up available
    website_snippet_filters by going through a route restricted to logged in
    users and using sudo to fetch the available filters
    
    https://github.com/odoo/odoo/pull/59341
    task-2312878

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -276,13 +276,6 @@ class Website(Home):
         )
         return dynamic_filter and dynamic_filter.render(template_key, limit, search_domain) or ''
 
-    @http.route('/website/snippet/filters', type='json', auth='public', website=True)
-    def get_dynamic_filter(self, filter_id, template_key, limit=None, search_domain=None):
-        dynamic_filter = request.env['website.snippet.filter'].sudo().search(
-            [('id', '=', filter_id)] + request.website.website_domain()
-        )
-        return dynamic_filter and dynamic_filter.render(template_key, limit, search_domain) or ''
-
     @http.route('/website/snippet/options_filters', type='json', auth='user', website=True)
     def get_dynamic_snippet_filters(self):
         dynamic_filter = request.env['website.snippet.filter'].sudo().search_read(
