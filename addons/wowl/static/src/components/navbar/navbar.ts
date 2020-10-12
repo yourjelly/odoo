9d98dc4fd0165c65759d1ff4f342c369c473937a
Bruno Boi <boi@odoo.com>
2020-10-12 17:05:14 +0200

    [IMP] wowl: navbar apps menu
    with usage of new dropdown component
    
    Taskid: 2350248

--- addons/wowl/static/src/components/navbar/navbar.ts
+++ addons/wowl/static/src/components/navbar/navbar.ts
@@ -1,18 +1,25 @@
 import { Component, useState } from "@odoo/owl";
+import { OwlEvent } from "@odoo/owl/dist/types/core/owl_event";
 import { useService } from "../../core/hooks";
+import { MenuTree } from "../../services/menus";
 import { OdooEnv } from "../../types";
+import { Dropdown } from "../dropdown/dropdown";
+import { DropdownItem } from "../dropdown/dropdown_item";
+
+export interface NavBarState {
+  selectedApp: null | MenuTree;
+}
 
 export class NavBar extends Component<{}, OdooEnv> {
   static template = "wowl.NavBar";
+  static components = { Dropdown, DropdownItem };
   actionManager = useService("action_manager");
   menuRepo = useService("menus");
-  state = useState({ showDropdownMenu: false });
+  state = useState<NavBarState>({
+    selectedApp: null,
+  });
 
   systrayItems = this._getSystrayItems();
-
-  toggleDropdownMenu() {
-    this.state.showDropdownMenu = !this.state.showDropdownMenu;
-  }
 
   _getSystrayItems() {
     return this.env.registries.systray.getAll().sort((x, y) => {
@@ -22,8 +29,9 @@ export class NavBar extends Component<{}, OdooEnv> {
     });
   }
 
-  _onMenuClicked(menu: any) {
+  onNavBarDropdownItemSelection(ev: OwlEvent<{ payload: any }>) {
+    const { payload: menu } = ev.detail;
+    this.state.selectedApp = this.menuRepo.getMenuAsTree(menu.appID);
     this.actionManager.doAction(menu.actionID, { clearBreadcrumbs: true });
-    this.state.showDropdownMenu = false;
   }
 }
