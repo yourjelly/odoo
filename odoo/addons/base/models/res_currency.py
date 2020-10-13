f98260a13476d6fbe0e14a36d14c0de4f69ee9f4
Achraf (abz) <abz@odoo.com>
2020-10-13 16:58:21 +0200

    [FIX] base: Traceback raised at new currency creation
    Issue
    
        - Install `Accounting`
        - Go to `Settings`
        - Create new currency
    
        Traceback raised.
    
    Cause
    
        _get_rates function is called with empty self.ids.
    
    Solution
    
        Dont call _get_rates if there is no element in ids
    
    opw-2359632

--- odoo/addons/base/models/res_currency.py
+++ odoo/addons/base/models/res_currency.py
@@ -61,6 +61,10 @@ class Currency(models.Model):
 
     @api.depends('rate_ids.rate')
     def _compute_current_rate(self):
+        if not self.ids:
+            for currency in self:
+                currency.rate = False
+            return
         date = self._context.get('date') or fields.Date.today()
         company = self.env['res.company'].browse(self._context.get('company_id')) or self.env.company
         # the subquery selects the last rate before 'date' for the given currency/company
