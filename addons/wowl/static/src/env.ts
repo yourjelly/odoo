5c709a48826c8460338234622c025db32fdec71a
Mathieu Duckerts-Antoine <dam@odoo.com>
2020-11-16 16:54:19 +0100

    [IMP] wowl: new way to deploy services

--- addons/wowl/static/src/env.ts
+++ addons/wowl/static/src/env.ts
@@ -1,5 +1,7 @@
 import * as owl from "@odoo/owl";
-import { OdooConfig, OdooEnv, Service } from "./types";
+import { deployServices } from "./services/deploy_services";
+import { OdooConfig, OdooEnv } from "./types";
+import { OdooBrowser } from "./types";
 
 export async function makeEnv(config: OdooConfig): Promise<OdooEnv> {
   const {
@@ -28,57 +30,9 @@ export async function makeEnv(config: OdooConfig): Promise<OdooEnv> {
   };
 
   await deployServices(env, config);
+
   return env;
 }
-
-async function deployServices(env: OdooEnv, config: OdooConfig): Promise<void> {
-  const services = env.services;
-  const serviceRegistry = config.services;
-  const toBeDeployed = new Set(serviceRegistry.getAll());
-  // deploy as many services in parallel as possible
-  function deploy(): Promise<any> {
-    let service: Service | null = null;
-    const proms: Promise<any>[] = [];
-
-    while ((service = findNext())) {
-      let name = service.name;
-      toBeDeployed.delete(service);
-      const value = service.deploy(env, config);
-      if (value instanceof Promise) {
-        proms.push(
-          value.then((val) => {
-            services[name] = val || null;
-            return deploy();
-          })
-        );
-      } else {
-        services[service.name] = value || null;
-      }
-    }
-    return Promise.all(proms);
-  }
-
-  await deploy();
-
-  if (toBeDeployed.size) {
-    throw new Error(`Some services could not be deployed: ${[...toBeDeployed].map((s) => s.name)}`);
-  }
-
-  function findNext(): Service | null {
-    for (let s of toBeDeployed) {
-      if (s.dependencies) {
-        if (s.dependencies.every((d) => d in services)) {
-          return s;
-        }
-      } else {
-        return s;
-      }
-    }
-    return null;
-  }
-}
-
-import { OdooBrowser } from "./types";
 
 export function makeRAMLocalStorage(): OdooBrowser["localStorage"] {
   let store: any = {};
