122115ce3fae562e8e1aaf06d7433af050384223
Aaron Bohy <aab@odoo.com>
2020-11-24 16:14:09 +0100

    [ADD] display_notification client action

---
+++ addons/wowl/static/src/services/action_manager/client_actions.ts
@@ -0,0 +1,23 @@
+import { utils } from "@odoo/owl";
+import { OdooEnv } from "../../types";
+import { sprintf } from "../../core/utils";
+
+const { escape } = utils;
+
+export const displayNotificationAction = (env: OdooEnv, action: any) => {
+  const params = action.params || {};
+  const options = {
+    className: params.className || "",
+    icon: params.icon || "",
+    sticky: params.sticky || false,
+    title: params.title ? escape(params.title) : "",
+    type: params.type || "info",
+  };
+
+  let links: string[] = (params.links || []).map((link: any) => {
+    return `<a href="${escape(link.url)}" target="_blank">${escape(link.label)}</a>`;
+  });
+  const message = sprintf(escape(params.message), ...links);
+  env.services.notifications.create(message, options);
+  return params.next;
+};
