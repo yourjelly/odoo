942cdb2d3134e918c59fbb7b1444a8cbba678117
Géry Debongnie <ged@odoo.com>
2020-11-26 16:13:27 +0100

    [REF] unify utils code

--- addons/wowl/static/src/utils/utils.ts
+++ addons/wowl/static/src/utils/utils.ts
@@ -1,6 +1,22 @@
-import { Odoo } from "../../types";
+import { Odoo } from "../types";
 
 declare const odoo: Odoo;
+
+/**
+ * Returns a string formatted using given values.
+ * If the value is an object, its keys will replace `%(key)s` expressions.
+ * If the values are a set of strings, they will replace `%s` expressions.
+ * If no value is given, the string will not be formatted.
+ */
+export function sprintf(s: string, ...values: string[] | [{ [key: string]: string }]): string {
+  if (values.length === 1 && typeof values[0] === "object") {
+    const valuesDict = values[0] as { [key: string]: string };
+    s = s.replace(/\%\(?([^\)]+)\)s/g, (match, value) => valuesDict[value]);
+  } else if (values.length > 0) {
+    s = s.replace(/\%s/g, () => values.shift() as string);
+  }
+  return s;
+}
 
 export function isBrowserChromium(): boolean {
   // true for the browser base on Chromium (Google Chrome, Opera, Edge)
--- addons/wowl/static/tests/core/utils_tests.ts
+++ addons/wowl/static/tests/core/utils_tests.ts
@@ -1,29 +1,6 @@
-import { makeContext, sprintf } from "../../src/core/utils";
+import { sprintf } from "../../src/utils/utils";
 
 QUnit.module("utils", {}, () => {
-  QUnit.module("makeContext");
-
-  QUnit.test("return empty context", (assert) => {
-    assert.deepEqual(makeContext(), {});
-  });
-
-  QUnit.test("duplicate a context", (assert) => {
-    const ctx1 = { a: 1 };
-    const ctx2 = makeContext(ctx1);
-    assert.notStrictEqual(ctx1, ctx2);
-    assert.deepEqual(ctx1, ctx2);
-  });
-
-  QUnit.test("can accept undefined", (assert) => {
-    assert.deepEqual(makeContext(undefined), {});
-    assert.deepEqual(makeContext({ a: 1 }, undefined, { b: 2 }), { a: 1, b: 2 });
-  });
-
-  QUnit.test("evaluate strings", (assert) => {
-    assert.deepEqual(makeContext("{'a': 33}"), { a: 33 });
-    assert.deepEqual(makeContext({ a: 1 }, "{'b': a + 1}"), { a: 1, b: 2 });
-  });
-
   QUnit.module("sprintf");
 
   QUnit.test("sprintf properly formats strings", (assert) => {
