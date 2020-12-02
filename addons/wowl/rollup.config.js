a7a03bc24700e4735349986f62491673a9041d1a
Mathieu Duckerts-Antoine <dam@odoo.com>
2020-12-02 11:17:01 +0100

    [IMP] wowl: import Luxon and Chart.js

--- addons/wowl/rollup.config.js
+++ addons/wowl/rollup.config.js
@@ -1,22 +1,22 @@
 export default [{
   input: "static/dist/js/src/main.js",
-  external: ["@odoo/owl"],
+  external: ["@odoo/owl", "chart.js", "luxon"],
   output: {
     file: "static/dist/app.js",
     format: "iife",
     name: "wowl",
     extend: true,
-    globals: { "@odoo/owl": "owl" },
+    globals: { "@odoo/owl": "owl", "chart.js": "Chart", "luxon": "luxon" },
   },
 }, {
   input: "static/dist/js/tests/main.js",
-  external: ["@odoo/owl", "qunit", "jquery"],
+  external: ["@odoo/owl", "chart.js", "qunit", "jquery", "luxon"],
   output: {
     file: "static/dist/app_tests.js",
     format: "iife",
     name: "wowl",
     extend: true,
-    globals: { "@odoo/owl": "owl", "qunit": "QUnit", "jquery": "$" },
+    globals: { "@odoo/owl": "owl", "chart.js": "Chart", "qunit": "QUnit", "jquery": "$", "luxon": "luxon" },
     interop: false, // to be allowed to add a 'debug' function on the QUnit object
   },
 }];
