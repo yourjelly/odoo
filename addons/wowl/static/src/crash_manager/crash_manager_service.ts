d4c4232cd44eb83b0276d9973c00f215e0d6da1e
Géry Debongnie <ged@odoo.com>
2020-12-09 10:23:15 +0100

    [REF] sligthly improve/simplify crash manager service

--- addons/wowl/static/src/crash_manager/crash_manager_service.ts
+++ addons/wowl/static/src/crash_manager/crash_manager_service.ts
@@ -11,6 +11,70 @@ import { Env } from "@odoo/owl/dist/types/component/component";
 import { RPCError } from "../services/rpc";
 import OdooError from "./odoo_error";
 
+function handleError(error: OdooError, env: OdooEnv) {
+  switch (error.name) {
+    case "UNKNOWN_CORS_ERROR":
+      env.services.dialog_manager.open(NetworkErrorDialog, {
+        traceback: error.traceback || error.stack,
+        message: error.message,
+        name: error.name,
+      });
+      break;
+    case "UNCAUGHT_CLIENT_ERROR":
+      env.services.dialog_manager.open(ClientErrorDialog, {
+        traceback: error.traceback || error.stack,
+        message: error.message,
+        name: error.name,
+      });
+      break;
+    case "UNCAUGHT_EMPTY_REJECTION_ERROR":
+      env.services.dialog_manager.open(ClientErrorDialog, {
+        message: error.message,
+        name: error.name,
+      });
+      break;
+    case "RPC_ERROR":
+      // When an error comes from the server, it can have an exeption name.
+      // (or any string truly). It is used as key in the error dialog from
+      // server registry to know which dialog component to use.
+      // It's how a backend dev can easily map its error to another component.
+      // Note that for a client side exception, we don't use this registry
+      // as we can directly assign a value to `component`.
+
+      // error is here a RPCError
+      const exceptionName = (error as RPCError).exceptionName;
+      let ErrorComponent = error.Component;
+      if (!ErrorComponent && exceptionName && odoo.errorDialogRegistry.contains(exceptionName)) {
+        ErrorComponent = odoo.errorDialogRegistry.get(exceptionName);
+      }
+
+      env.services.dialog_manager.open(ErrorComponent || RPCErrorDialog, {
+        traceback: error.traceback || error.stack,
+        message: error.message,
+        name: error.name,
+        exceptionName: (error as RPCError).exceptionName,
+        data: (error as RPCError).data,
+        subType: (error as RPCError).subType,
+        code: (error as RPCError).code,
+        type: (error as RPCError).type,
+      });
+      break;
+    default:
+      let DialogComponent: Type<Component<any, Env>> = ErrorDialog;
+      // If an error has been defined to have a custom dialog
+      if (error.Component) {
+        DialogComponent = error.Component!;
+      }
+      env.services.dialog_manager.open(DialogComponent, {
+        traceback: error.traceback || error.stack,
+        message: error.message,
+        name: error.name,
+      });
+      break;
+  }
+  env.bus.trigger("ERROR_DISPATCHED", error);
+}
+
 export const crashManagerService: Service<void> = {
   name: "crash_manager",
   dependencies: ["dialog_manager"],
@@ -43,7 +107,7 @@ export const crashManagerService: Service<void> = {
         err = new OdooError("UNCAUGHT_CLIENT_ERROR");
         err.traceback = `${message}\n\n${filename}:${lineno}\n${env._t("Traceback")}:\n${stack}`;
       }
-      env.bus.trigger("ERROR_DISPATCH", err);
+      handleError(err, env);
     });
 
     window.addEventListener("unhandledrejection", (ev) => {
@@ -52,7 +116,7 @@ export const crashManagerService: Service<void> = {
       if (!unhandledError) {
         const error = new OdooError("UNCAUGHT_EMPTY_REJECTION_ERROR");
         error.message = env._t("A Promise reject call with no argument is not getting caught.");
-        env.bus.trigger("ERROR_DISPATCH", error);
+        handleError(error, env);
         return;
       }
 
@@ -61,11 +125,11 @@ export const crashManagerService: Service<void> = {
         const error = new OdooError("DEFAULT_ERROR");
         error.message = ev.reason.message;
         error.traceback = ev.reason.stack;
-        env.bus.trigger("ERROR_DISPATCH", error);
+        handleError(error, env);
       }
       // The thrown error was originally an instance of "OdooError" or subtype.
       else if (OdooError.prototype.isPrototypeOf(unhandledError)) {
-        env.bus.trigger("ERROR_DISPATCH", unhandledError);
+        handleError(unhandledError, env);
       }
       // The thrown value was originally a non-Error instance or a raw js object
       else {
@@ -75,58 +139,7 @@ export const crashManagerService: Service<void> = {
           unhandledError,
           Object.getOwnPropertyNames(unhandledError)
         );
-        env.bus.trigger("ERROR_DISPATCH", error);
-      }
-    });
-
-    env.bus.on("ERROR_DISPATCH", null, (error: OdooError) => {
-      const type = error.name;
-      let dialogComponent: Type<Component<any, Env>> = ErrorDialog;
-      // If an error has been defined to have a custom dialog
-      if (error.component) {
-        dialogComponent = error.component!;
-      }
-
-      switch (type) {
-        case "UNKNOWN_CORS_ERROR":
-          env.services.dialog_manager.open(NetworkErrorDialog, {
-            traceback: error.traceback ?? error.stack,
-            message: error.message,
-            name: error.name,
-          });
-          break;
-        case "UNCAUGHT_CLIENT_ERROR":
-          env.services.dialog_manager.open(ClientErrorDialog, {
-            traceback: error.traceback ?? error.stack,
-            message: error.message,
-            name: error.name,
-          });
-          break;
-        case "UNCAUGHT_EMPTY_REJECTION_ERROR":
-          env.services.dialog_manager.open(ClientErrorDialog, {
-            message: error.message,
-            name: error.name,
-          });
-          break;
-        case "RPC_ERROR":
-          env.services.dialog_manager.open(error.component || RPCErrorDialog, {
-            traceback: error.traceback ?? error.stack,
-            message: error.message,
-            name: error.name,
-            exceptionName: (error as RPCError).exceptionName,
-            data: (error as RPCError).data,
-            subType: (error as RPCError).subType,
-            code: (error as RPCError).code,
-            type: (error as RPCError).type,
-          });
-          break;
-        default:
-          env.services.dialog_manager.open(dialogComponent, {
-            traceback: error.traceback ?? error.stack,
-            message: error.message,
-            name: error.name,
-          });
-          break;
+        handleError(error, env);
       }
     });
   },
