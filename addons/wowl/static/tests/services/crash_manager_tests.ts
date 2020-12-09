143c56ade8993ffa9985d3c55eb18b32e80fd754
Géry Debongnie <ged@odoo.com>
2020-12-09 10:23:15 +0100

    [REF] sligthly improve/simplify crash manager service

--- addons/wowl/static/tests/services/crash_manager_tests.ts
+++ addons/wowl/static/tests/services/crash_manager_tests.ts
@@ -1,12 +1,12 @@
-import * as QUnit from "qunit";
-import { makeTestEnv, OdooEnv } from "../helpers/index";
-import { Registry } from "../../src/core/registry";
-import { Registries, Service, Type } from "../../src/types";
-import { crashManagerService } from "../../src/crash_manager/crash_manager_service";
-import { DialogManagerService } from "../../src/services/dialog_manager";
 import { Component, tags } from "@odoo/owl";
+import * as QUnit from "qunit";
+import { Registry } from "../../src/core/registry";
+import { crashManagerService } from "../../src/crash_manager/crash_manager_service";
 import { RPCErrorDialog } from "../../src/crash_manager/error_dialogs";
+import { DialogManagerService } from "../../src/services/dialog_manager";
 import { RPCError } from "../../src/services/rpc";
+import { Registries, Service, Type } from "../../src/types";
+import { makeTestEnv, OdooEnv } from "../helpers/index";
 
 function makeFakeDialogManagerService(
   open: (dialogClass: Type<Component>, props?: object) => void
@@ -21,24 +21,31 @@ function makeFakeDialogManagerService(
 
 let env: OdooEnv;
 let serviceRegistry: Registries["serviceRegistry"];
+let windowAddEventListener = window.addEventListener;
 
 QUnit.module("CrashManager", {
   async beforeEach() {
     serviceRegistry = new Registry();
     serviceRegistry.add(crashManagerService.name, crashManagerService);
   },
+  afterEach() {
+    window.addEventListener = windowAddEventListener;
+  },
 });
 
 QUnit.test("handle RPC_ERROR of type='server' and no associated dialog class", async (assert) => {
   assert.expect(2);
-
+  let errorCb: any;
+  window.addEventListener = (type: any, cb: any) => {
+    if (type === "unhandledrejection") {
+      errorCb = cb;
+    }
+  };
   const error = new RPCError();
   error.code = 701;
   error.message = "Some strange error occured";
-  (error.data = {
-    debug: "somewhere",
-  }),
-    (error.subType = "strange_error");
+  error.data = { debug: "somewhere" };
+  error.subType = "strange_error";
 
   function open(dialogClass: Type<Component>, props?: object) {
     assert.strictEqual(dialogClass, RPCErrorDialog);
@@ -56,14 +63,23 @@ QUnit.test("handle RPC_ERROR of type='server' and no associated dialog class", a
     });
   }
   serviceRegistry.add("dialog_manager", makeFakeDialogManagerService(open));
+
   env = await makeTestEnv({ serviceRegistry });
-  env.bus.trigger("ERROR_DISPATCH", error);
+  const errorEvent = new PromiseRejectionEvent("error", { reason: error, promise: null as any });
+  errorCb(errorEvent);
 });
 
 QUnit.test(
   "handle RPC_ERROR of type='server' and associated custom dialog class",
   async (assert) => {
     assert.expect(2);
+    let errorCb: any;
+    window.addEventListener = (type: any, cb: any) => {
+      if (type === "unhandledrejection") {
+        errorCb = cb;
+      }
+    };
+
     class CustomDialog extends Component<{}, OdooEnv> {
       static template = tags.xml`<RPCErrorDialog title="'Strange Error'"/>`;
       static components = { RPCErrorDialog };
@@ -72,7 +88,7 @@ QUnit.test(
     const error = new RPCError();
     error.code = 701;
     error.message = "Some strange error occured";
-    error.component = CustomDialog;
+    error.Component = CustomDialog;
 
     function open(dialogClass: Type<Component>, props?: object) {
       assert.strictEqual(dialogClass, CustomDialog);
@@ -90,6 +106,7 @@ QUnit.test(
     serviceRegistry.add("dialog_manager", makeFakeDialogManagerService(open));
     env = await makeTestEnv({ serviceRegistry });
     odoo.errorDialogRegistry.add("strange_error", CustomDialog);
-    env.bus.trigger("ERROR_DISPATCH", error);
+    const errorEvent = new PromiseRejectionEvent("error", { reason: error, promise: null as any });
+    errorCb(errorEvent);
   }
 );
