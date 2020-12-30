ec011e407dd604773fde01db9f665256d5df88e4
lejeune quentin <qle@odoo.com>
2020-12-30 17:30:00 +0100

    build dev branch

--- addons/point_of_sale/tools/posbox/posbox_create_image.sh
+++ addons/point_of_sale/tools/posbox/posbox_create_image.sh
@@ -28,9 +28,9 @@ __base="$(basename ${__file} .sh)"
 MOUNT_POINT="${__dir}/root_mount"
 OVERWRITE_FILES_BEFORE_INIT_DIR="${__dir}/overwrite_before_init"
 OVERWRITE_FILES_AFTER_INIT_DIR="${__dir}/overwrite_after_init"
-VERSION=14.0
-VERSION_IOTBOX=21.04
-REPO=https://github.com/odoo/odoo.git
+VERSION=master-iot-opcua-qle
+VERSION_IOTBOX=20.12
+REPO=https://github.com/odoo-dev/odoo.git
 
 if ! file_exists *raspios*.img ; then
     wget 'http://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-03-25/2021-03-04-raspios-buster-armhf-lite.zip' -O raspios.img.zip
