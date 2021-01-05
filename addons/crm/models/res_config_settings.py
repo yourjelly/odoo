b717c0326d2afe82303a6df105920e179fa59057
dht-odoo <dht@odoo.com>
2021-01-05 12:45:51 +0530

    [IMP] crm: lead scoring start date wii be set in wizard
    To avoid warning to save while changing  predictive_lead_scoring_start_date.
    So, now it will be set in wizard only and it will be readonly in res.config.settings

--- addons/crm/models/res_config_settings.py
+++ addons/crm/models/res_config_settings.py
@@ -24,7 +24,7 @@ class ResConfigSettings(models.TransientModel):
         ('auto', 'Enrich all leads automatically'),
     ], string='Enrich lead automatically', default='manual', config_parameter='crm.iap.lead.enrich.setting')
     lead_mining_in_pipeline = fields.Boolean("Create a lead mining request directly from the opportunity pipeline.", config_parameter='crm.lead_mining_in_pipeline')
-    predictive_lead_scoring_start_date = fields.Date(string='Lead Scoring Starting Date', compute="_compute_pls_start_date", inverse="_inverse_pls_start_date_str")
+    predictive_lead_scoring_start_date = fields.Date(string='Lead Scoring Starting Date', compute="_compute_pls_start_date", inverse="_inverse_pls_start_date_str", readonly=True)
     predictive_lead_scoring_start_date_str = fields.Char(string='Lead Scoring Starting Date in String', config_parameter='crm.pls_start_date')
     predictive_lead_scoring_fields = fields.Many2many('crm.lead.scoring.frequency.field', string='Lead Scoring Frequency Fields', compute="_compute_pls_fields", inverse="_inverse_pls_fields_str")
     predictive_lead_scoring_fields_str = fields.Char(string='Lead Scoring Frequency Fields in String', config_parameter='crm.pls_fields')
--- addons/crm/wizard/crm_update_probabilities.py
+++ addons/crm/wizard/crm_update_probabilities.py
@@ -12,8 +12,9 @@ class CrmUpdateProbabilities(models.TransientModel):
         pls_start_date_config = self.env['ir.config_parameter'].sudo().get_param('crm.pls_start_date')
         return fields.Date.to_date(pls_start_date_config)
 
-    pls_start_date = fields.Date(required=True, default=_get_default_pls_start_date, readonly=True)
+    pls_start_date = fields.Date(required=True, default=_get_default_pls_start_date)
 
     def action_update_crm_lead_probabilities(self):
         if self.env.user._is_admin():
+            self.env['ir.config_parameter'].sudo().set_param('crm.pls_start_date', str(self.pls_start_date))
             self.env['crm.lead'].sudo()._cron_update_automated_probabilities()
