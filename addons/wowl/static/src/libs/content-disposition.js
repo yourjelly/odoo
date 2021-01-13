3399a8e2d32734c6f7ae47208d9171089b96c591
Aaron Bohy <aab@odoo.com>
2021-01-13 15:08:39 +0100

    fixup: restore linebreaks in libs

--- addons/wowl/static/src/libs/content-disposition.js
+++ addons/wowl/static/src/libs/content-disposition.js
@@ -1,5 +1,6 @@
 /** @odoo-module **/
 /* eslint-disable no-control-regex */
+
 /*
 (The MIT License)
 Copyright (c) 2014-2017 Douglas Christopher Wilson
@@ -20,20 +21,24 @@ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 /**
  * Stripped down to only parsing/decoding.
  * Slightly changed for export and lint compliance
  */
+
 /**
  * RegExp to match percent encoding escape.
  * @private
  */
 const HEX_ESCAPE_REPLACE_REGEXP = /%([0-9A-Fa-f]{2})/g;
+
 /**
  * RegExp to match non-latin1 characters.
  * @private
  */
 const NON_LATIN1_REGEXP = /[^\x20-\x7e\xa0-\xff]/g;
+
 /**
  * RegExp to match quoted-pair in RFC 2616
  *
@@ -42,6 +47,7 @@ const NON_LATIN1_REGEXP = /[^\x20-\x7e\xa0-\xff]/g;
  * @private
  */
 const QESC_REGEXP = /\\([\u0000-\u007f])/g;
+
 /**
  * RegExp for various RFC 2616 grammar
  *
@@ -67,6 +73,7 @@ const QESC_REGEXP = /\\([\u0000-\u007f])/g;
  * @private
  */
 const PARAM_REGEXP = /;[\x09\x20]*([!#$%&'*+.0-9A-Z^_`a-z|~-]+)[\x09\x20]*=[\x09\x20]*("(?:[\x20!\x23-\x5b\x5d-\x7e\x80-\xff]|\\[\x20-\x7e])*"|[!#$%&'*+.0-9A-Z^_`a-z|~-]+)[\x09\x20]*/g;
+
 /**
  * RegExp for various RFC 5987 grammar
  *
@@ -89,6 +96,7 @@ const PARAM_REGEXP = /;[\x09\x20]*([!#$%&'*+.0-9A-Z^_`a-z|~-]+)[\x09\x20]*=[\x09
  * @private
  */
 const EXT_VALUE_REGEXP = /^([A-Za-z0-9!#$%&+\-^_`{}~]+)'(?:[A-Za-z]{2,3}(?:-[A-Za-z]{3}){0,3}|[A-Za-z]{4,8}|)'((?:%[0-9A-Fa-f]{2}|[A-Za-z0-9!#$&+.^_`|~-])+)$/;
+
 /**
  * RegExp for various RFC 6266 grammar
  *
@@ -103,6 +111,7 @@ const EXT_VALUE_REGEXP = /^([A-Za-z0-9!#$%&+\-^_`{}~]+)'(?:[A-Za-z]{2,3}(?:-[A-Z
  * @private
  */
 const DISPOSITION_TYPE_REGEXP = /^([!#$%&'*+.0-9A-Z^_`a-z|~-]+)[\x09\x20]*(?:$|;)/;
+
 /**
  * Decode a RFC 6987 field value (gracefully).
  *
@@ -112,11 +121,14 @@ const DISPOSITION_TYPE_REGEXP = /^([!#$%&'*+.0-9A-Z^_`a-z|~-]+)[\x09\x20]*(?:$|;
  */
 function decodefield(str) {
   const match = EXT_VALUE_REGEXP.exec(str);
+
   if (!match) {
     throw new TypeError("invalid extended field value");
   }
+
   const charset = match[1].toLowerCase();
   const encoded = match[2];
+
   switch (charset) {
     case "iso-8859-1":
       return encoded.replace(HEX_ESCAPE_REPLACE_REGEXP, pdecode).replace(NON_LATIN1_REGEXP, "?");
@@ -126,6 +138,7 @@ function decodefield(str) {
       throw new TypeError("unsupported charset in extended field");
   }
 }
+
 /**
  * Parse Content-Disposition header string.
  *
@@ -137,53 +150,70 @@ export default function parse(string) {
   if (!string || typeof string !== "string") {
     throw new TypeError("argument string is required");
   }
+
   let match = DISPOSITION_TYPE_REGEXP.exec(string);
+
   if (!match) {
     throw new TypeError("invalid type format");
   }
+
   // normalize type
   let index = match[0].length;
   const type = match[1].toLowerCase();
+
   let key;
   const names = [];
   const params = {};
   let value;
+
   // calculate index to start at
   index = PARAM_REGEXP.lastIndex = match[0].substr(-1) === ";" ? index - 1 : index;
+
   // match parameters
   while ((match = PARAM_REGEXP.exec(string))) {
     if (match.index !== index) {
       throw new TypeError("invalid parameter format");
     }
+
     index += match[0].length;
     key = match[1].toLowerCase();
     value = match[2];
+
     if (names.indexOf(key) !== -1) {
       throw new TypeError("invalid duplicate parameter");
     }
+
     names.push(key);
+
     if (key.indexOf("*") + 1 === key.length) {
       // decode extended value
       key = key.slice(0, -1);
       value = decodefield(value);
+
       // overwrite existing value
       params[key] = value;
       continue;
     }
+
     if (typeof params[key] === "string") {
       continue;
     }
+
     if (value[0] === '"') {
       // remove quotes and escapes
       value = value.substr(1, value.length - 2).replace(QESC_REGEXP, "$1");
     }
+
     params[key] = value;
   }
+
   if (index !== -1 && index !== string.length) {
     throw new TypeError("invalid parameter format");
   }
+
   return new ContentDisposition(type, params);
 }
+
 /**
  * Percent decode a single character.
  *
@@ -195,6 +225,7 @@ export default function parse(string) {
 function pdecode(str, hex) {
   return String.fromCharCode(parseInt(hex, 16));
 }
+
 /**
  * Class for parsed Content-Disposition header for v8 optimization
  *
--- addons/wowl/static/src/libs/download.js
+++ addons/wowl/static/src/libs/download.js
@@ -1,5 +1,6 @@
 /** @odoo-module **/
 /* eslint-disable no-undef */
+
 /*
 MIT License
 Copyright (c) 2016 dandavis
@@ -19,6 +20,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
+
 /**
  * download.js v4.2, by dandavis; 2008-2018. [MIT] see http://danml.com/download.html for tests/usage
  * v1 landed a FF+Chrome compat way of downloading strings to local un-named files, upgraded to use a hidden frame and optional mime
@@ -49,12 +51,14 @@ export default function download(data, filename, mimetype) {
     blob,
     reader;
   myBlob = myBlob.call ? myBlob.bind(self) : Blob;
+
   if (String(this) === "true") {
     //reverse arguments, allowing download.bind(true, "text/xml", "export.xml") to act as a callback
     payload = [payload, mimeType];
     mimeType = payload[0];
     payload = payload[1];
   }
+
   if (url && url.length < 2048) {
     // if no filename and no mime, assume a url was passed as the only argument
     fileName = url.split("/").pop().split("?")[0];
@@ -73,6 +77,7 @@ export default function download(data, filename, mimetype) {
       return ajax;
     }
   }
+
   //go ahead and download dataURLs right away
   if (/^data:[\w+\-]+\/[\w+\-]+[,;]/.test(payload)) {
     if (payload.length > 1024 * 1024 * 1.999 && myBlob !== toString) {
@@ -84,7 +89,9 @@ export default function download(data, filename, mimetype) {
         : saver(payload); // everyone else can save dataURLs un-processed
     }
   }
+
   blob = payload instanceof myBlob ? payload : new myBlob([payload], { type: mimeType });
+
   function dataUrlToBlob(strUrl) {
     let parts = strUrl.split(/[:;,]/),
       type = parts[1],
@@ -93,11 +100,14 @@ export default function download(data, filename, mimetype) {
       mx = binData.length,
       i = 0,
       uiArr = new Uint8Array(mx);
+
     for (i; i < mx; ++i) {
       uiArr[i] = binData.charCodeAt(i);
     }
+
     return new myBlob([uiArr], { type });
   }
+
   function saver(url, winMode) {
     if ("download" in anchor) {
       //html5 A[download]
@@ -118,6 +128,7 @@ export default function download(data, filename, mimetype) {
       }, 66);
       return true;
     }
+
     // handle non-a[download] safari as best we can:
     if (/(Version)\/(\d+)\.(\d+)(?:\.(\d+))?.*Safari\//.test(navigator.userAgent)) {
       url = url.replace(/^data:([\w\/\-+]+)/, defaultMime);
@@ -133,9 +144,11 @@ export default function download(data, filename, mimetype) {
       }
       return true;
     }
+
     //do iframe dataURL download (old ch+FF):
     let f = document.createElement("iframe");
     document.body.appendChild(f);
+
     if (!winMode) {
       // force a mime that will download:
       url = `data:${url.replace(/^data:([\w\/\-+]+)/, defaultMime)}`;
@@ -145,10 +158,12 @@ export default function download(data, filename, mimetype) {
       document.body.removeChild(f);
     }, 333);
   }
+
   if (navigator.msSaveBlob) {
     // IE10+ : (has Blob, but not a[download] or URL)
     return navigator.msSaveBlob(blob, fileName);
   }
+
   if (self.URL) {
     // simple fast and modern way using Blob and URL:
     saver(self.URL.createObjectURL(blob), true);
@@ -161,6 +176,7 @@ export default function download(data, filename, mimetype) {
         return saver(`data:${mimeType},${encodeURIComponent(blob)}`);
       }
     }
+
     // Blob but not URL support:
     reader = new FileReader();
     reader.onload = function () {
