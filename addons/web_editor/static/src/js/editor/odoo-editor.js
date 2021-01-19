85e713948892e77ca001bb3b92df9d4314a4d6fc
Nicolas Bayet <nby@odoo.com>
2021-01-19 16:30:08 +0100

    integration odoo-editor phase 1

---
+++ addons/web_editor/static/src/js/editor/odoo-editor.js
@@ -0,0 +1,1 @@
+/home/goaman/projects/external/odoo/odoo-editor/build/odoo-editor-bundle.js
--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1,13 +1,16 @@
 odoo.define('web_editor.wysiwyg', function (require) {
 'use strict';
-var Widget = require('web.Widget');
-var SummernoteManager = require('web_editor.rte.summernote');
-var summernoteCustomColors = require('web_editor.rte.summernote_custom_colors');
+const Widget = require('web.Widget');
+const customColors = require('web_editor.custom_colors');
+const concurrency = require('web.concurrency');
+const weContext = require('web_editor.context');
+const OdooEditor = require('web_editor.odoo-editor').OdooEditor;
+const snippetsEditor = require('web_editor.snippet.editor');
+const Toolbar = require('web_editor.toolbar');
+
 var id = 0;
 
-// core.bus
-// media_dialog_demand
-var Wysiwyg = Widget.extend({
+const Wysiwyg = Widget.extend({
     xmlDependencies: [
     ],
     defaultOptions: {
@@ -25,7 +28,7 @@ var Wysiwyg = Widget.extend({
         'styleWithSpan': false,
         'inlinemedia': ['p'],
         'lang': 'odoo',
-        'colors': summernoteCustomColors,
+        'colors': customColors,
         recordInfo: {
             context: {},
         },
@@ -49,34 +52,42 @@ var Wysiwyg = Widget.extend({
         this._super.apply(this, arguments);
         this.id = ++id;
         this.options = options;
-    },
-    /**
-     * Load assets and color picker template then call summernote API
-     * and replace $el by the summernote editable node.
-     *
-     * @override
-     **/
-    willStart: function () {
-        this._summernoteManager = new SummernoteManager(this);
-        this.$target = this.$el;
-        return this._super();
+        this.saving_mutex = new concurrency.Mutex();
     },
     /**
      *
      * @override
      */
-    start: function () {
-        this.$target.wrap('<odoo-wysiwyg-container>');
-        this.$el = this.$target.parent();
+    start: async function () {
+        const _super = this._super;
+
         var options = this._editorOptions();
-        this.$target.summernote(options);
-        this.$editor = this.$('.note-editable:first');
+        this.$target = this.$el;
+        let value = options.value;
+        this.$editor = this.$target;
+        this.$editor.html(value);
         this.$editor.data('wysiwyg', this);
         this.$editor.data('oe-model', options.recordInfo.res_model);
         this.$editor.data('oe-id', options.recordInfo.res_id);
         $(document).on('mousedown', this._blur);
+
+        this.toolbar = new Toolbar();
+        await this.toolbar.appendTo(document.createElement('void'));
+        this.odooEditor = new OdooEditor(this.$editor[0], { toolbar: this.toolbar.$el[0] });
+
         this._value = this.$target.html() || this.$target.val();
-        return this._super.apply(this, arguments);
+
+        if (options.snippets) {
+            $('body').addClass('editor_enable');
+            this.snippetsMenu = new snippetsEditor.SnippetsMenu(this, Object.assign({
+                wysiwyg: this,
+                selectorEditableArea: '.o_editable',
+            }, options));
+            this._insertSnippetMenu();
+            console.log('inserted', this.snippetsMenu.$el);
+        }
+
+        return _super.apply(this, arguments);
     },
     /**
      * @override
@@ -143,6 +154,7 @@ var Wysiwyg = Widget.extend({
     save: function () {
         var isDirty = this.isDirty();
         var html = this.getValue();
+        debugger
         if (this.$target.is('textarea')) {
             this.$target.val(html);
         } else {
@@ -151,13 +163,100 @@ var Wysiwyg = Widget.extend({
         return Promise.resolve({isDirty:isDirty, html:html});
     },
     /**
+     * Save the content for the normal mode or the translation mode.
+     */
+    saveContent: async function (reload = true) {
+        if (this.options.enableTranslation) {
+            await this._onSaveTranslation();
+        } else {
+            await this.saveToServer(reload);
+        }
+    },
+
+    /**
+     * Save the content to the server for the normal mode.
+     */
+    saveToServer: async function (reload = true) {
+        const defs = [];
+        this.trigger_up('edition_will_stopped');
+        this.trigger_up('ready_to_save', {defs: defs});
+        await Promise.all(defs);
+
+        if (this.snippetsMenu) {
+            await this.snippetsMenu.cleanForSave();
+        }
+
+        // await this._saveModifiedImages();
+        await this._saveViewBlocks();
+        // await this._saveCoverPropertiesBlocks();
+        // await this._saveMegaMenuClasses();
+
+        this.trigger_up('edition_was_stopped');
+        window.onbeforeunload = null;
+        if (reload) {
+            window.location.reload();
+        }
+    },
+    /**
+     * Asks the user if he really wants to discard its changes (if there are
+     * some of them), then simply reload the page if he wants to.
+     *
+     * @param {boolean} [reload=true]
+     *        true if the page has to be reloaded when the user answers yes
+     *        (do nothing otherwise but add this to allow class extension)
+     * @returns {Promise}
+     */
+    cancel: function (reload) {
+        var self = this;
+        return new Promise((resolve, reject) => {
+            if (!this.odooEditor.history.length) {
+                resolve();
+            } else {
+                var confirm = Dialog.confirm(this, _t("If you discard the current edits, all unsaved changes will be lost. You can cancel to return to edit mode."), {
+                    confirm_callback: resolve,
+                });
+                confirm.on('closed', self, reject);
+            }
+        }).then(function () {
+            if (reload !== false) {
+                window.onbeforeunload = null;
+                return self._reload();
+            }
+        });
+    },
+    /**
      * Create/Update cropped attachments.
      *
      * @param {jQuery} $editable
      * @returns {Promise}
      */
     saveModifiedImages: function ($editable) {
-        return this._summernoteManager.saveModifiedImages($editable);
+        const defs = _.map($editable, async editableEl => {
+            const {oeModel: resModel, oeId: resId} = editableEl.dataset;
+            const proms = [...editableEl.querySelectorAll('.o_modified_image_to_save')].map(async el => {
+                const isBackground = !el.matches('img');
+                el.classList.remove('o_modified_image_to_save');
+                // Modifying an image always creates a copy of the original, even if
+                // it was modified previously, as the other modified image may be used
+                // elsewhere if the snippet was duplicated or was saved as a custom one.
+                const newAttachmentSrc = await this._rpc({
+                    route: `/web_editor/modify_image/${el.dataset.originalId}`,
+                    params: {
+                        res_model: resModel,
+                        res_id: parseInt(resId),
+                        data: (isBackground ? el.dataset.bgSrc : el.getAttribute('src')).split(',')[1],
+                    },
+                });
+                if (isBackground) {
+                    $(el).css('background-image', `url('${newAttachmentSrc}')`);
+                    delete el.dataset.bgSrc;
+                } else {
+                    el.setAttribute('src', newAttachmentSrc);
+                }
+            });
+            return Promise.all(proms);
+        });
+        return Promise.all(defs);
     },
     /**
      * @param {String} value
@@ -178,7 +277,7 @@ var Wysiwyg = Widget.extend({
     //--------------------------------------------------------------------------
     _editorOptions: function () {
         var self = this;
-        var options = Object.assign({}, $.summernote.options, this.defaultOptions, this.options);
+        var options = Object.assign({},  this.defaultOptions, this.options);
         if (this.options.generateOptions) {
             options = this.options.generateOptions(options);
         }
@@ -198,6 +297,179 @@ var Wysiwyg = Widget.extend({
         };
         return options;
     },
+    _insertSnippetMenu: function() {
+        return this.snippetsMenu.insertAfter(this.$el);
+    },
+
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    // Previously on rte.js
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+
+    /**
+     * Returns the editable areas on the page.
+     *
+     * @returns {jQuery}
+     */
+    editable: function () {
+        return $('#wrapwrap [data-oe-model]')
+            .not('.o_not_editable')
+            .filter(function () {
+                return !$(this).closest('.o_not_editable').length;
+            })
+            .not('link, script')
+            .not('[data-oe-readonly]')
+            .not('img[data-oe-field="arch"], br[data-oe-field="arch"], input[data-oe-field="arch"]')
+            .not('.oe_snippet_editor')
+            .add('.o_editable');
+    },
+
+    /**
+     * Searches all the dirty element on the page and saves them one by one. If
+     * one cannot be saved, this notifies it to the user and restarts rte
+     * edition.
+     *
+     * @param {Object} [context] - the context to use for saving rpc, default to
+     *                           the editor context found on the page
+     * @return {Promise} rejected if the save cannot be done
+     */
+    _saveViewBlocks: function (context) {
+        $('.o_editable')
+            .removeClass('o_editable o_is_inline_editable o_editable_date_field_linked o_editable_date_field_format_changed');
+
+        const $oeStructure = $('.o_dirty.oe_structure[data-oe-xpath][data-oe-id]');
+        const $oeFields = $('.o_dirty[data-oe-field]');
+        const $allBlocks = $oeStructure.add($oeFields);
+
+        const $dirty = $('.o_dirty');
+        $dirty
+            .removeAttr('contentEditable')
+            .removeClass('o_dirty oe_carlos_danger o_is_inline_editable');
+        console.log('$dirty', $dirty);
+
+        const defs = _.map($allBlocks, (el) => {
+            const $el = $(el);
+
+            $el.find('[class]').filter(function () {
+                if (!this.getAttribute('class').match(/\S/)) {
+                    this.removeAttribute('class');
+                }
+            });
+
+            // TODO: Add a queue with concurrency limit in webclient
+            return this.saving_mutex.exec(() => {
+                return this._saveElement($el, context || weContext.get())
+                .then(function () {
+                    $el.removeClass('o_dirty');
+                }).guardedCatch(function (response) {
+                    // because ckeditor regenerates all the dom, we can't just
+                    // setup the popover here as everything will be destroyed by
+                    // the DOM regeneration. Add markings instead, and returns a
+                    // new rejection with all relevant info
+                    var id = _.uniqueId('carlos_danger_');
+                    $el.addClass('o_dirty oe_carlos_danger ' + id);
+                    $('.o_editable.' + id)
+                        .removeClass(id)
+                        .popover({
+                            trigger: 'hover',
+                            content: response.message.data.message || '',
+                            placement: 'auto top',
+                        })
+                        .popover('show');
+                });
+            });
+        });
+        return Promise.all(defs).then(function () {
+            window.onbeforeunload = null;
+        }).guardedCatch((failed) => {
+            console.log(failed)
+            // If there were errors, re-enable edition
+            this.cancel();
+            this.start();
+        });
+    },
+
+    _attachTooltips: function () {
+        $(document.body)
+            .tooltip({
+                selector: '[data-oe-readonly]',
+                container: 'body',
+                trigger: 'hover',
+                delay: { 'show': 1000, 'hide': 100 },
+                placement: 'bottom',
+                title: _t("Readonly field")
+            })
+            .on('click', function () {
+                $(this).tooltip('hide');
+            });
+    },
+    /**
+     * Gets jQuery cloned element with internal text nodes escaped for XML
+     * storage.
+     *
+     * @private
+     * @param {jQuery} $el
+     * @return {jQuery}
+     */
+    _getEscapedElement: function ($el) {
+        var escaped_el = $el.clone();
+        var to_escape = escaped_el.find('*').addBack();
+        to_escape = to_escape.not(to_escape.filter('object,iframe,script,style,[data-oe-model][data-oe-model!="ir.ui.view"]').find('*').addBack());
+        to_escape.contents().each(function () {
+            if (this.nodeType === 3) {
+                this.nodeValue = $('<div />').text(this.nodeValue).html();
+            }
+        });
+        return escaped_el;
+    },
+    /**
+     * Saves one (dirty) element of the page.
+     *
+     * @private
+     * @param {jQuery} $el - the element to save
+     * @param {Object} context - the context to use for the saving rpc
+     * @param {boolean} [withLang=false]
+     *        false if the lang must be omitted in the context (saving "master"
+     *        page element)
+     */
+    _saveElement: function ($el, context, withLang) {
+        var viewID = $el.data('oe-id');
+        if (!viewID) {
+            return Promise.resolve();
+        }
+
+        return this._rpc({
+            model: 'ir.ui.view',
+            method: 'save',
+            args: [
+                viewID,
+                this._getEscapedElement($el).prop('outerHTML'),
+                !$el.data('oe-expression') && $el.data('oe-xpath') || null, // Note: hacky way to get the oe-xpath only if not a t-field
+            ],
+            context: context,
+        }, withLang ? undefined : {
+            noContextKeys: 'lang',
+        });
+    },
+
+    /**
+     * Reloads the page in non-editable mode, with the right scrolling.
+     *
+     * @private
+     * @returns {Promise} (never resolved, the page is reloading anyway)
+     */
+    _reload: function () {
+        window.location.hash = 'scrollTop=' + window.document.body.scrollTop;
+        if (window.location.search.indexOf('enable_editor') >= 0) {
+            window.location.href = window.location.href.replace(/&?enable_editor(=[^&]*)?/g, '');
+        } else {
+            window.location.reload(true);
+        }
+        return new Promise(function(){});
+    },
 });
 //--------------------------------------------------------------------------
 // Public helper
@@ -211,12 +483,14 @@ var Wysiwyg = Widget.extend({
  * @returns {Number} eo - end offset
  */
 Wysiwyg.getRange = function (node) {
-    var range = $.summernote.core.range.create();
-    return range && {
-        sc: range.sc,
-        so: range.so,
-        ec: range.ec,
-        eo: range.eo,
+    const selection = document.getSelection();
+    const range = selection.getRangeAt(0);
+
+    return {
+        sc: range.startContainer,
+        so: range.startOffset,
+        ec: range.endContainer,
+        eo: range.endOffset,
     };
 };
 /**
@@ -226,37 +500,13 @@ Wysiwyg.getRange = function (node) {
  * @param {Number} endOffset
  */
 Wysiwyg.setRange = function (startNode, startOffset, endNode, endOffset) {
-    $(startNode).focus();
-    if (endNode) {
-        $.summernote.core.range.create(startNode, startOffset, endNode, endOffset).select();
-    } else {
-        $.summernote.core.range.create(startNode, startOffset).select();
-    }
-    // trigger for Unbreakable
-    $(startNode.tagName ? startNode : startNode.parentNode).trigger('wysiwyg.range');
-};
-/**
- * @param {Node} node - dom node
- * @param {Object} [options]
- * @param {Boolean} options.begin move the range to the beginning of the first node.
- * @param {Boolean} options.end move the range to the end of the last node.
- */
-Wysiwyg.setRangeFromNode = function (node, options) {
-    var last = node;
-    while (last.lastChild) {
-        last = last.lastChild;
-    }
-    var first = node;
-    while (first.firstChild) {
-        first = first.firstChild;
-    }
-    if (options && options.begin && !options.end) {
-        Wysiwyg.setRange(first, 0);
-    } else if (options && !options.begin && options.end) {
-        Wysiwyg.setRange(last, last.textContent.length);
-    } else {
-        Wysiwyg.setRange(first, 0, last, last.tagName ? last.childNodes.length : last.textContent.length);
-    }
+    const selection = document.getSelection();
+    selection.removeAllRanges();
+
+    const range = new Range();
+    range.setStart(startNode, startOffset);
+    range.setEnd(endNode, endOffset);
+    selection.addRange(range);
 };
 return Wysiwyg;
 });
