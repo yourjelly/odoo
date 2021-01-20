490d34738ffd3d28d28f952996c96d9bd14e6d97
Nicolas Bayet <nby@odoo.com>
2021-01-20 14:25:43 +0100

    bind snippet resizer to the proper document

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -3248,7 +3248,7 @@ registry.sizing = SnippetOptionWidget.extend({
             };
             var bodyMouseUp = function () {
                 $body.off('mousemove', bodyMouseMove);
-                $(window).off('mouseup', bodyMouseUp);
+                $body.off('mouseup', bodyMouseUp);
                 $body.removeClass(cursor);
                 $handle.removeClass('o_active');
 
@@ -3269,7 +3269,7 @@ registry.sizing = SnippetOptionWidget.extend({
                 }, 0);
             };
             $body.on('mousemove', bodyMouseMove);
-            $(window).on('mouseup', bodyMouseUp);
+            $body.on('mouseup', bodyMouseUp);
         });
 
         return def;
