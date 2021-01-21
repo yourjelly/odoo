aacc68ea05332281611aa1c240b02d4b2d363c12
Nicolas Bayet <nby@odoo.com>
2021-01-21 11:01:13 +0100

    history step when dropping a snippet

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2119,6 +2119,7 @@ var SnippetsMenu = Widget.extend({
 
                         var $target = $toInsert;
                         await self._scrollToSnippet($target);
+                        self.options.wysiwyg.odooEditor.historyStep();
 
                         _.defer(async function () {
                             self.trigger_up('snippet_dropped', {$target: $target});
