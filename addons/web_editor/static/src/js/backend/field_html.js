3c39177fd3e5eb673d241ee3cd7b4adbdb90c002
Nicolas Bayet <nby@odoo.com>
2021-01-26 10:47:29 +0100

    [FIX] syntax error

--- addons/web_editor/static/src/js/backend/field_html.js
+++ addons/web_editor/static/src/js/backend/field_html.js
@@ -222,7 +222,7 @@ var FieldHtml = basic_fields.DebouncedField.extend(TranslatableFieldMixin, {
         var value = this._textToHtml(this.value);
         if (this.nodeOptions.wrapper) {
             value = this._wrap(value);
-
+        }
         var fieldNameAttachment = _.chain(this.recordData)
             .pairs()
             .find(function (value) {
