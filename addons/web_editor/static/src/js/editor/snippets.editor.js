35961de8e600119f98b9d8e3dc3c9b45b36033ea
Nicolas Bayet <nby@odoo.com>
2021-01-27 11:13:46 +0100

    fix typo

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2796,7 +2796,7 @@ var SnippetsMenu = Widget.extend({
         this.trigger_up('request_cancel');
     },
     /**
-     * Preview on mobile..
+     * Preview on mobile.
      */
     _onMobilePreviewClick: async function() {
         throw new Error('implement me');
