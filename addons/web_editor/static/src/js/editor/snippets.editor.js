750be4178b04d47c90c1c26ce09d4496f1b3c7f8
Antoine Guenet <age@odoo.com>
2021-01-29 11:27:55 +0100

    parametrize toolbar

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2849,7 +2849,7 @@ var SnippetsMenu = Widget.extend({
         this._filterSnippets('');
     },
     _addToolbar(toolbarMode = "text") {
-        let titleText = _t("Text Formatting");
+        let titleText = _t("Inline Text");
         switch (toolbarMode) {
             case "image":
                 titleText = _t("Image Formatting");
