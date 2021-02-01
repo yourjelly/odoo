987014c344096d89bd73ad7ff1ba129ac5651ed4
Nicolas Bayet <nby@odoo.com>
2021-02-01 12:21:51 +0100

    [FIX] put back focusIn and focusInEnd

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -31,7 +31,52 @@ $.extend($.expr[':'], {
     },
 });
 
+function firstChild (node) {
+    while (node.firstChild) { node = node.firstChild; }
+    return node;
+};
+function lastChild (node) {
+    while (node.lastChild) { node = node.lastChild; }
+    return node;
+};
+function nodeLength (node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+        return node.nodeValue.length;
+    } else {
+        return node.childNodes.length;
+    }
+};
+
+
 $.fn.extend({
+    focusIn: function () {
+        if (this.length) {
+            const selection = document.getSelection();
+            selection.removeAllRanges();
+
+            const range = new Range();
+            const node = firstChild(this[0]);
+            range.setStart(node, 0);
+            range.setEnd(node, 0);
+            selection.addRange(range);
+        }
+        return this;
+    },
+    focusInEnd: function () {
+        if (this.length) {
+            const selection = document.getSelection();
+            selection.removeAllRanges();
+
+            const range = new Range();
+            const node = lastChild(this[0]);
+            const length = nodeLength(node);
+
+            range.setStart(node, length);
+            range.setEnd(node, length);
+            selection.addRange(range);
+        }
+        return this;
+    },
     selectContent: function () {
         if (this.length) {
             const selection = document.getSelection();
