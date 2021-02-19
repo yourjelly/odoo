1a289cf063bf116961228edbecf55857c6194870
Antoine Guenet <age@odoo.com>
2021-02-19 10:50:34 +0100

    [FIX] update history buttons on history step

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -1304,7 +1304,7 @@ var SnippetsMenu = Widget.extend({
                     $undoButton.attr('disabled', !this.options.wysiwyg.odooEditor.historyCanUndo());
                     $redoButton.attr('disabled', !this.options.wysiwyg.odooEditor.historyCanRedo());
                 };
-                this.options.wysiwyg.odooEditor.addEventListener('historyApply', updateHistoryButtons);
+                this.options.wysiwyg.odooEditor.addEventListener('historyStep', updateHistoryButtons);
             }
 
             this.$('[data-title]').tooltip({
