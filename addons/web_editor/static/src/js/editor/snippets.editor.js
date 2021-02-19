f60015048153ffeaad1efcbfe9f70515f7d73c8f
Sébastien Geelen (sge) <sge@odoo.com>
2021-02-19 09:54:47 +0100

    [FIX] web_editor: remove wrong offset in snippet drag when scrolled

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2110,11 +2110,14 @@ var SnippetsMenu = Widget.extend({
                     );
                     return dragSnip;
                 },
-                start: function () {
+                start: function (ev, ui) {
                     self.$el.find('.oe_snippet_thumbnail').addClass('o_we_already_dragging');
 
                     dropped = false;
                     $snippet = $(this);
+                    const $wrapwrap = $("#wrapwrap");
+                    const initalScrollTop = $wrapwrap[0].scrollTop
+                    $snippet.css("margin-top",  (ui.offset.top - ui.position.top) + "px");
                     var $baseBody = $snippet.find('.oe_snippet_body');
                     var $selectorSiblings = $();
                     var $selectorChildren = $();
@@ -2179,7 +2182,8 @@ var SnippetsMenu = Widget.extend({
 
                     // Trigger a scroll on the draggable element so that jQuery updates
                     // the position of the drop zones.
-                    self.draggableComponent.$scrollTarget.on('scroll.scrolling_element', function () {
+                    self.draggableComponent.$scrollTarget.on('scroll.scrolling_element', function (e) {
+                        $snippet.css("margin-top",  (ui.offset.top - ui.position.top + $wrapwrap[0].scrollTop - initalScrollTop) + "px");
                         self.$el.trigger('scroll');
                     });
 
@@ -2199,7 +2203,6 @@ var SnippetsMenu = Widget.extend({
                     }
 
                     self.getEditableArea().find('.oe_drop_zone').droppable('destroy').remove();
-
                     if (dropped) {
                         var prev = $toInsert.first()[0].previousSibling;
                         var next = $toInsert.last()[0].nextSibling;
