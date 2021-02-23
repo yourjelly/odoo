ad139867f58d56186ac685ece298a62f295adbc9
Nicolas Bayet <nby@odoo.com>
2021-02-23 11:31:34 +0100

    [FIX] position of snippets upon drag and drop

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -1056,6 +1056,7 @@ var SnippetsMenu = Widget.extend({
     init: function (parent, options) {
         this._super.apply(this, arguments);
         options = options || {};
+        this.$body = $((options.document || document).body);
 
         this.options = options;
         if (!this.options.snippets) {
