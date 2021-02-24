98a67e12590f36d2ee38cf31934bb92de71b4432
Arnold Moyaux <arm@odoo.com>
2021-02-24 11:12:38 +0100

    [REF] stock_inventory set setter as action

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -500,7 +500,7 @@ class StockMoveLine(models.Model):
                             # checkboxes on the picking type, he's allowed to enter tracked
                             # products without a `lot_id`.
                             continue
-                    elif ml.move_id.inventory_id:
+                    elif ml.move_id.is_inventory:
                         # If an inventory adjustment is linked, the user is allowed to enter
                         # tracked products without a `lot_id`.
                         continue
