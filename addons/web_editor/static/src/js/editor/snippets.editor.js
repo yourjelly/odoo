f8d10a8158c340afefe6abe69378935f3edf9f3b
Nicolas Bayet <nby@odoo.com>
2021-02-24 11:05:47 +0100

    [FIX] deactivate unbreakable upon snippet update

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -2260,7 +2260,6 @@ var SnippetsMenu = Widget.extend({
 
                         var $target = $toInsert;
                         await self._scrollToSnippet($target);
-                        self.options.wysiwyg.odooEditor.historyStep();
 
                         _.defer(async function () {
                             self.trigger_up('snippet_dropped', {$target: $target});
@@ -2273,6 +2272,9 @@ var SnippetsMenu = Widget.extend({
                             });
                             $target.trigger('content_changed');
                             await self._updateInvisibleDOM();
+
+                            self.options.wysiwyg.odooEditor.unbreakableStepUnactive();
+                            self.options.wysiwyg.odooEditor.historyStep();
 
                             self.$el.find('.oe_snippet_thumbnail').removeClass('o_we_already_dragging');
                         });
