7a1848c336bd07f0949d17ac6a19c3514a81aac8
Antoine Guenet <age@odoo.com>
2021-03-11 12:10:01 +0100

    [FIX] only add floating toolbar if option true

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -159,7 +159,9 @@ const Wysiwyg = Widget.extend({
         $(this.odooEditor.dom).on('keydown', this._updateEditorUI.bind(this));
 
         return _super.apply(this, arguments).then(() => {
-            $(document.body).append(this.toolbar.$el);
+            if (this.options.autohideToolbar) {
+                $(document.body).append(this.toolbar.$el);
+            }
         });
     },
     /**
