e14fd91e5738cda61ebc2b71811b85be3a9aea19
Aaron Bohy <aab@odoo.com>
2021-03-12 11:04:12 +0100

    [REF] split app_creator and icon_creator tests

--- addons/wowl/static/tests/web_studio/app_creator_tests.js
+++ addons/wowl/static/tests/web_studio/app_creator_tests.js
@@ -1,12 +1,9 @@
 /** @odoo-module **/
 
 import { AppCreatorWrapper } from "../../src/web_studio/client_action/app_creator/app_creator";
-import { IconCreator } from "../../src/web_studio/client_action/icon_creator/icon_creator";
-import makeTestEnvironment from "web.test_env";
 import testUtils from "web.test_utils";
 
-const { Component, tags } = owl;
-const { xml } = tags;
+const { Component } = owl;
 const sampleIconUrl = "/web_enterprise/Parent.src/img/default_icon_app.png";
 
 async function createAppCreator({ debug, env, events, rpc, state }) {
@@ -62,7 +59,7 @@ QUnit.module("Studio", (hooks) => {
       env: {
         services: {
           blockUI: () => assert.step("UI blocked"),
-          httpRequest: (route, params) => {
+          httpRequest: (route) => {
             if (route === "/web/binary/upload_attachment") {
               assert.step(route);
               return Promise.resolve('[{ "id": 666 }]');
@@ -469,80 +466,5 @@ QUnit.module("Studio", (hooks) => {
     assert.verifySteps(["UI blocked", "new-app-created", "UI unblocked"]);
 
     wrapper.destroy();
-  });
-
-  QUnit.module("IconCreator");
-
-  QUnit.test("icon creator: with initial web icon data", async function (assert) {
-    assert.expect(4);
-
-    class Parent extends Component {
-      constructor() {
-        super(...arguments);
-        this.webIconData = sampleIconUrl;
-      }
-      _onIconChanged(ev) {
-        // default values
-        assert.step("icon-changed");
-        assert.deepEqual(ev.detail, {
-          backgroundColor: "#34495e",
-          color: "#f1c40f",
-          iconClass: "fa fa-diamond",
-          type: "custom_icon",
-        });
-      }
-    }
-    Parent.components = { IconCreator };
-    Parent.env = makeTestEnvironment();
-    Parent.template = xml`
-            <IconCreator
-                editable="true"
-                type="'base64'"
-                webIconData="webIconData"
-                t-on-icon-changed.stop.prevent="_onIconChanged"
-            />`;
-    const parent = new Parent();
-    await parent.mount(testUtils.prepareTarget());
-
-    assert.strictEqual(
-      parent.el.querySelector(".o_web_studio_uploaded_image").style.backgroundImage,
-      `url(\"${sampleIconUrl}\")`,
-      "displayed image should prioritize web icon data"
-    );
-
-    // click on first link: "Design icon"
-    await testUtils.dom.click(parent.el.querySelector(".o_web_studio_upload a"));
-
-    assert.verifySteps(["icon-changed"]);
-
-    parent.destroy();
-  });
-
-  QUnit.test("icon creator: without initial web icon data", async function (assert) {
-    assert.expect(3);
-
-    class Parent extends Component {}
-    Parent.components = { IconCreator };
-    Parent.env = makeTestEnvironment();
-    Parent.template = xml`
-            <IconCreator
-                backgroundColor="'rgb(255, 0, 128)'"
-                color="'rgb(0, 255, 0)'"
-                editable="false"
-                iconClass="'fa fa-heart'"
-                type="'custom_icon'"
-            />`;
-    const parent = new Parent();
-    await parent.mount(testUtils.prepareTarget());
-
-    // Attributes should be correctly set
-    assert.strictEqual(
-      parent.el.querySelector(".o_app_icon").style.backgroundColor,
-      "rgb(255, 0, 128)"
-    );
-    assert.strictEqual(parent.el.querySelector(".o_app_icon i").style.color, "rgb(0, 255, 0)");
-    assert.hasClass(parent.el.querySelector(".o_app_icon i"), "fa fa-heart");
-
-    parent.destroy();
   });
 });
