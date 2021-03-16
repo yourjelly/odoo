a894f5ebb8c67b08c71b87aaf7937c70aa14f028
Tiffany Chang (tic) <tic@odoo.com>
2021-03-16 17:33:10 +0100

    [REF] prevent deleting quants w/ only inventory count

--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -598,8 +598,10 @@ class StockQuant(models.Model):
         """
         precision_digits = max(6, self.sudo().env.ref('product.decimal_product_uom').digits * 2)
         # Use a select instead of ORM search for UoM robustness.
-        query = """SELECT id FROM stock_quant WHERE (round(quantity::numeric, %s) = 0 OR quantity IS NULL) AND round(reserved_quantity::numeric, %s) = 0;"""
-        params = (precision_digits, precision_digits)
+        query = """SELECT id FROM stock_quant WHERE (round(quantity::numeric, %s) = 0 OR quantity IS NULL)
+                                                     AND round(reserved_quantity::numeric, %s) = 0
+                                                     AND (round(inventory_quantity::numeric, %s) = 0 OR inventory_quantity IS NULL);"""
+        params = (precision_digits, precision_digits, precision_digits)
         self.env.cr.execute(query, params)
         quant_ids = self.env['stock.quant'].browse([quant['id'] for quant in self.env.cr.dictfetchall()])
         quant_ids.sudo().unlink()
