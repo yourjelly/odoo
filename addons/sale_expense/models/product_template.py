ff4e7170c5d9a74ce3c7e5159e41b04e9ded7327
Kamlesh Pathekar <kpt@odoo.com>
2021-03-24 10:17:25 +0530

    [FIX]  sale_expense:  Fix display of expense_policy on product

--- addons/sale_expense/models/product_template.py
+++ addons/sale_expense/models/product_template.py
@@ -13,12 +13,13 @@ class ProductTemplate(models.Model):
 
     @api.depends('can_be_expensed')
     def _compute_visible_expense_policy(self):
-        expense_products = self.filtered(lambda p: p.can_be_expensed)
-        for product_template in self - expense_products:
-            product_template.visible_expense_policy = False
+        # expense_products = self.filtered(lambda p: p.can_be_expensed)
+        # for product_template in self - expense_products:
+        #     product_template.visible_expense_policy = False
 
-        super(ProductTemplate, expense_products)._compute_visible_expense_policy()
-        visibility = self.user_has_groups('hr_expense.group_hr_expense_user')
-        for product_template in expense_products:
-            if not product_template.visible_expense_policy:
-                product_template.visible_expense_policy = visibility
+        super(ProductTemplate, self)._compute_visible_expense_policy()
+        visibility = self.env['ir.module.module'].search([('name', '=', 'hr_expense')])
+        if visibility.state == 'uninstalled':
+           self.visible_expense_policy = False
+        else:
+            self.visible_expense_policy = True
