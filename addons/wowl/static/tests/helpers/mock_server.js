8733d13ecdcfec047ba0ef7d24c0a6b5443956ad
Lucas Perais (lpe) <lpe@odoo.com>
2021-03-25 14:24:26 +0100

    MockSearch in mockServer

--- addons/wowl/static/tests/helpers/mock_server.js
+++ addons/wowl/static/tests/helpers/mock_server.js
@@ -375,6 +375,8 @@ class MockServer {
         return Promise.resolve(this.mockLoadAction(args));
       case "/web/dataset/search_read":
         return Promise.resolve(this.mockSearchReadController(args));
+      case "/web/dataset/search":
+        return Promise.resolve(this.mockSearchController(args));
     }
     if (
       route.indexOf("/web/image") >= 0 ||
@@ -393,6 +395,8 @@ class MockServer {
         return Promise.resolve(this.mockOnchange(args.model, args.args, args.kwargs));
       case "read":
         return Promise.resolve(this.mockRead(args.model, args.args));
+      case "search":
+        return Promise.resolve(this.mockSearch(args.model, args.args, args.kwargs));
       case "search_read":
         return Promise.resolve(this.mockSearchRead(args.model, args.args, args.kwargs));
       case "web_search_read":
@@ -786,6 +790,19 @@ class MockServer {
     };
   }
 
+  mockSearch(modelName, args, kwargs) {
+    const result = this.mockSearchController({
+      model: modelName,
+      domain: kwargs.domain || args[0],
+      fields: kwargs.fields || args[1],
+      offset: kwargs.offset || args[2],
+      limit: kwargs.limit || args[3],
+      sort: kwargs.order || args[4],
+      context: kwargs.context,
+    });
+    return result.records;
+  }
+
   mockSearchRead(modelName, args, kwargs) {
     const result = this.mockSearchReadController({
       model: modelName,
@@ -812,7 +829,7 @@ class MockServer {
     return result;
   }
 
-  mockSearchReadController(params) {
+  mockSearchController(params) {
     const model = this.models[params.model];
     let fieldNames = params.fields;
     const offset = params.offset || 0;
@@ -831,7 +848,16 @@ class MockServer {
     const nbRecords = records.length;
     records = records.slice(offset, params.limit ? offset + params.limit : nbRecords);
     return {
+      fieldNames,
       length: nbRecords,
+      records,
+    };
+  }
+
+  mockSearchReadController(params) {
+    const { fieldNames, length, records } = this.mockSearchController(params);
+    return {
+      length,
       records: this.mockRead(params.model, [records.map((r) => r.id), fieldNames]),
     };
   }
