a4e8efc30d3814c04c9e1c27dc73ad695f39ebd3
Lucas Perais (lpe) <lpe@odoo.com>
2021-04-06 15:51:40 +0200

    [FIX] messagingMenu is directly a owl Component with the right env

--- addons/mail/static/src/js/systray/systray_activity_menu.js
+++ addons/mail/static/src/js/systray/systray_activity_menu.js
@@ -191,6 +191,9 @@ var ActivityMenu = Widget.extend({
     },
 });
 
+// LPE: this is to give the WOWL systray registry the right sequence
+ActivityMenu.prototype.sequence = 11;
+
 SystrayMenu.Items.push(ActivityMenu);
 
 export default ActivityMenu;
