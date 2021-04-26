fc7da91d2a3f2f76c1825132273c18f539c2d76e
Simon Genin (ges) <ges@odoo.com>
2021-04-26 13:59:35 +0200

    [DOC] web: add comments for main.js and setup.js

--- addons/web/static/src/main.js
+++ addons/web/static/src/main.js
@@ -4,4 +4,10 @@
 import { startWebClient } from "./setup";
 import { WebClient } from "./webclient/webclient";
 
+/**
+ * This file starts the webclient. It is in its own file to allow its replacement 
+ * in enterprise. The enterprise version of the file uses its own webclient import,
+ * which is a subclass of the above Webclient.
+ */
+
 startWebClient(WebClient);
