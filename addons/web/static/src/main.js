12ae83dabe7d0b88e4caa50fbe3fcdb93494f04c
Simon Genin (ges) <ges@odoo.com>
2021-04-28 16:48:52 +0200

    [DOC] web: Add some documentation in the code

--- addons/web/static/src/main.js
+++ addons/web/static/src/main.js
@@ -1,11 +1,10 @@
 /** @odoo-module **/
 
-// #TODODESCR
 import { startWebClient } from "./setup";
 import { WebClient } from "./webclient/webclient";
 
 /**
- * This file starts the webclient. It is in its own file to allow its replacement 
+ * This file starts the webclient. It is in its own file to allow its replacement
  * in enterprise. The enterprise version of the file uses its own webclient import,
  * which is a subclass of the above Webclient.
  */
