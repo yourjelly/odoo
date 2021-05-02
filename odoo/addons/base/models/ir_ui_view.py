f07d5e77a461fffb07c150e0e23520c322fd4c49
Fabien Pinckaers <fp@openerp.com>
2021-05-02 21:42:48 +0200

    fix

--- odoo/addons/base/models/ir_ui_view.py
+++ odoo/addons/base/models/ir_ui_view.py
@@ -990,7 +990,7 @@ ORDER BY v.priority, v.id
                     if not node.get('on_change'):
                         node.set('on_change', '1')
 
-        fdata = fields and model.fields_get(fields.keys(), attributes=[]) or []
+        fdata = fields and model.fields_get(fields.keys()) or []
         for f in fdata:
             fdata[f].update(fields[f])
         return fdata
