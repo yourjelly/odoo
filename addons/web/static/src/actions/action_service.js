f30863468a3308849d7e2dfbc65d97a191bbf693
Aaron Bohy <aab@odoo.com>
2021-05-06 11:24:14 +0200

    Revert "[FIX] web: display RPCError throw by a doActionButton"
    This reverts commit 777cafed462eea549706ec27351e97219fbe5468.

--- addons/web/static/src/actions/action_service.js
+++ addons/web/static/src/actions/action_service.js
@@ -974,8 +974,7 @@ function makeActionManager(env) {
         model: params.model,
       });
       action = await keepLast.add(callProm);
-      action =
-        action && typeof action === "object" ? action : { type: "ir.actions.act_window_close" };
+      action = action || { type: "ir.actions.act_window_close" };
     } else if (params.type === "action") {
       // execute a given action, so load it first
       context.active_id = params.recordId || null;
