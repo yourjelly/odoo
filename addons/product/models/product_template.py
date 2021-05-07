ae586916dd9663d36fa0d96f4b02bbeebf69dca3
Jeremy Kersten <jke@odoo.com>
2021-05-07 12:46:13 +0200

    [TEST] new placeholder product

--- addons/product/models/product_template.py
+++ addons/product/models/product_template.py
@@ -18,6 +18,12 @@ class ProductTemplate(models.Model):
     _description = "Product Template"
     _order = "priority desc, name"
 
+    def _get_placeholder_filename(self, field=None):
+        image_fields = ['image_%s' % size for size in [1920, 1024, 512, 256, 128]]
+        if field in image_fields:
+            return 'product/static/img/placeholder.png'
+        return super()._get_placeholder_filename(field=field)
+
     @tools.ormcache()
     def _get_default_category_id(self):
         # Deletion forbidden (at least through unlink)
