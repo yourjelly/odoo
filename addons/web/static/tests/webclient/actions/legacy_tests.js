ade3cc5997b18a736b84f76b3a343871e2e5cf51
Lucas Perais (lpe) <lpe@odoo.com>
2021-05-11 18:40:36 +0200

    [FIX] web: open a dialog does not unset the legacy widget

--- addons/web/static/tests/webclient/actions/legacy_tests.js
+++ addons/web/static/tests/webclient/actions/legacy_tests.js
@@ -4,9 +4,16 @@ import { DialogContainer } from "@web/core/dialog/dialog_service";
 import { registry } from "@web/core/registry";
 import { NotificationContainer } from "@web/core/notifications/notification_container";
 import { getLegacy } from "web.test_legacy";
-import { legacyExtraNextTick, patchWithCleanup } from "../../helpers/utils";
+import { click, legacyExtraNextTick, patchWithCleanup } from "../../helpers/utils";
 import { clearRegistryWithCleanup } from "../../helpers/mock_env";
 import { createWebClient, doAction, getActionManagerTestConfig } from "./helpers";
+
+import { ClientActionAdapter } from "@web/legacy/action_adapters";
+import { useDebugManager } from "@web/core/debug/debug_menu";
+import { debugService } from "@web/core/debug/debug_service";
+
+import core from "web.core";
+import AbstractAction from "web.AbstractAction";
 
 let testConfig;
 // legacy stuff
@@ -81,5 +88,58 @@ QUnit.module("ActionManager", (hooks) => {
         assert.containsOnce(document.body, ".modal");
         assert.strictEqual($(".modal-title").text(), "Warning!!!");
         assert.strictEqual($(".modal-body").text(), "This is a warning...");
+    });
+
+    QUnit.test("redraw a controller and open debugManager does not crash", async (assert) => {
+        assert.expect(11);
+
+        const LegacyAction = AbstractAction.extend({
+            start() {
+                const ret = this._super(...arguments);
+                const el = document.createElement("div");
+                el.classList.add("custom-action");
+                this.el.append(el);
+                return ret;
+            },
+        });
+        core.action_registry.add("customLegacy", LegacyAction);
+
+        patchWithCleanup(ClientActionAdapter.prototype, {
+            setup() {
+                useDebugManager((accessRights) => {
+                    assert.step("debugItems executed");
+                    assert.ok(this.widget);
+                    return [];
+                });
+                this._super();
+            },
+        });
+
+        registry.category("services").add("debug", debugService);
+        testConfig.debug = true;
+
+        const mockRPC = (route, args) => {
+            if (route.includes("check_access_rights")) {
+                return true;
+            }
+        };
+
+        const webClient = await createWebClient({ testConfig, mockRPC });
+        await doAction(webClient, "customLegacy");
+        assert.containsOnce(webClient, ".custom-action");
+        assert.verifySteps([]);
+
+        await click(webClient.el, ".o_debug_manager button");
+        assert.verifySteps(["debugItems executed"]);
+
+        await doAction(webClient, 5); // action in Dialog
+        await click(webClient.el, ".modal .o_form_button_cancel");
+        assert.containsNone(webClient, ".modal");
+        assert.containsOnce(webClient, ".custom-action");
+        assert.verifySteps([]);
+
+        await click(webClient.el, ".o_debug_manager button");
+        assert.verifySteps(["debugItems executed"]);
+        delete core.action_registry.map.customLegacy;
     });
 });
