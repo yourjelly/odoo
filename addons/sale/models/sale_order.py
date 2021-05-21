11060383cd6760a199de5d5a5590168cb1431055
Victor Feyens <vfe@odoo.com>
2021-05-21 11:05:11 +0200

    [TODO] test perf: order amounts

--- addons/sale/models/sale_order.py
+++ addons/sale/models/sale_order.py
@@ -181,14 +181,11 @@ class SaleOrder(models.Model):
 
     @api.depends('order_line.price_total')
     def _amount_all(self):
-        """
-        Compute the total amounts of the SO.
-        """
+        """Compute the total amounts of the SO."""
         for order in self:
-            amount_untaxed = amount_tax = 0.0
-            for line in order.order_line:
-                amount_untaxed += line.price_subtotal
-                amount_tax += line.price_tax
+            # TODO VFE check perf change
+            amount_untaxed = sum(order.order_line.mapped('price_subtotal'))
+            amount_tax = sum(order.order_line.mapped('price_tax'))
             order.update({
                 'amount_untaxed': amount_untaxed,
                 'amount_tax': amount_tax,
