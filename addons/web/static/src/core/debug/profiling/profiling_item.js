50f5d30146406f396b5e8479680c50c906c90f18
Aaron Bohy <aab@odoo.com>
2021-06-07 19:02:30 +0000

    [FIX] web: adapt debug manager profiling
    closes odoo-dev/odoo#930
    
    Signed-off-by: Géry Debongnie (ged) <ged@openerp.com>

---
+++ addons/web/static/src/core/debug/profiling/profiling_item.js
@@ -0,0 +1,30 @@
+/** @odoo-module **/
+
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
+import { useBus } from "@web/core/bus_hook";
+import { useService } from "@web/core/service_hook";
+
+const { Component } = owl;
+
+export class ProfilingItem extends Component {
+    setup() {
+        this.profiling = useService("profiling");
+        this.actionService = useService("action");
+        useBus(this.props.bus, "UPDATE", this.render);
+    }
+
+    get interval() {
+        return this.profiling.state.params.traces_async_interval;
+    }
+
+    changeTracesInterval(ev) {
+        this.profiling.setParams({
+            traces_async_interval: ev.target.value,
+        });
+    }
+    openProfiles() {
+        this.actionService.doAction("base.action_menu_ir_profile");
+    }
+}
+ProfilingItem.components = { DropdownItem };
+ProfilingItem.template = "web.DebugMenu.ProfilingItem";
