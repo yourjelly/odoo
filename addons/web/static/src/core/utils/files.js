9fd3a74ecabc1005b1ee9d6f6805b6917bfa50a2
Lucas Perais (lpe) <lpe@odoo.com>
2021-06-17 17:31:16 +0200

    file utils

---
+++ addons/web/static/src/core/utils/files.js
@@ -0,0 +1,10 @@
+/** @odoo-module */
+
+export function getFileContent(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result.split(",")[1]);
+        reader.onerror = (error) => reject(error);
+    });
+}
