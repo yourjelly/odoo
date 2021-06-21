3f7fc51a85e41e9e0b2afb8621bdbbce51c01df4
Joseph Caburnay <jcb@odoo.com>
2021-06-21 16:41:05 +0200

    PaymentScreenStatus remove dependency with currentOrder

--- addons/point_of_sale/static/src/js/Screens/PaymentScreen/PaymentScreenStatus.js
+++ addons/point_of_sale/static/src/js/Screens/PaymentScreen/PaymentScreenStatus.js
@@ -6,20 +6,13 @@ odoo.define('point_of_sale.PaymentScreenStatus', function(require) {
 
     class PaymentScreenStatus extends PosComponent {
         get changeText() {
-            return this.env.pos.format_currency(this.currentOrder.get_change());
+            return this.env.pos.format_currency(this.props.change);
         }
         get totalDueText() {
-            return this.env.pos.format_currency(
-                this.currentOrder.get_total_with_tax() + this.currentOrder.get_rounding_applied()
-            );
+            return this.env.pos.format_currency(this.props.total);
         }
         get remainingText() {
-            return this.env.pos.format_currency(
-                this.currentOrder.get_due() > 0 ? this.currentOrder.get_due() : 0
-            );
-        }
-        get currentOrder() {
-            return this.env.pos.get_order();
+            return this.env.pos.format_currency(this.props.due > 0 ? this.props.due: 0);
         }
     }
     PaymentScreenStatus.template = 'PaymentScreenStatus';
