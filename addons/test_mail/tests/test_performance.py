f41cb34b6991c52ad59aa4dc0cd32b75f722aaee
Nicolas Bayet <nby@odoo.com>
2021-06-23 16:29:21 +0200

    remove |safe

--- addons/test_mail/tests/test_performance.py
+++ addons/test_mail/tests/test_performance.py
@@ -200,9 +200,9 @@ class TestMailAPIPerformance(BaseMailPerformance):
             'model_id': self.env['ir.model']._get('mail.test.ticket').id,
             'subject': 'About ${object.name}',
             'body_html': '<p>Hello <t t-esc="object.name"/></p>',
-            'email_from': '${object.user_id.email_formatted | safe}',
+            'email_from': '${object.user_id.email_formatted}',
             'partner_to': '${object.customer_id.id}',
-            'email_to': '${("%s Customer <%s>" % (object.name, object.email_from)) | safe}',
+            'email_to': '${("%s Customer <%s>" % (object.name, object.email_from))}',
         })
 
     @users('__system__', 'emp')
