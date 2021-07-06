84c50d7d270a3a7520b6ac32c3269482df75e264
Mathieu Duckerts-Antoine <dam@odoo.com>
2021-07-06 13:36:58 +0000

    [REF] project: BurndownChartView

--- addons/project/static/src/js/burndown_chart/burndown_chart_view.js
+++ addons/project/static/src/js/burndown_chart/burndown_chart_view.js
@@ -1,13 +1,12 @@
 /** @odoo-module **/
 
-import { GraphView } from "@web/views/graph/graph_view";
 import { BurndownChartRenderer } from "./burndown_chart_renderer";
+import { GraphView } from "@web/views/graph/graph_view";
 import { registry } from "@web/core/registry";
 
 const viewRegistry = registry.category("views");
 
-class BurndownChartView extends GraphView {};
-BurndownChartView.components = { BurndownChartRenderer };
+class BurndownChartView extends GraphView {}
 BurndownChartView.Renderer = BurndownChartRenderer;
 BurndownChartView.buttonTemplate = "project.BurndownChartView.Buttons";
 
