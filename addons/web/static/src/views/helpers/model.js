debd0f6b847971cd442ebbc8d81872d9f42c6473
Géry Debongnie <ged@odoo.com>
2021-07-09 13:49:30 +0000

    [IMP] web: simplify model and useModel hook

--- addons/web/static/src/views/helpers/model.js
+++ addons/web/static/src/views/helpers/model.js
@@ -6,33 +6,29 @@ import { useService } from "@web/core/service_hook";
 
 const { core, hooks } = owl;
 const { EventBus } = core;
-const { useComponent, onWillStart, onWillUpdateProps } = hooks;
+const { onWillStart, onWillUpdateProps, useComponent } = hooks;
 
 export class Model extends EventBus {
     /**
      * @param {Object} env
      * @param {Object} services
      */
-    constructor(env, services) {
+    constructor(env, params, services) {
         super();
         this.env = env;
-        this.setup(services);
+        this.setup(params, services);
     }
 
     /**
+     * @param {Object} params
      * @param {Object} services
      */
-    setup(services) {}
+    setup(params, services) {}
 
     /**
-     * @param {Object} params
+     * @param {Object} searchParams
      */
-    load(params) {}
-
-    /**
-     * @param {Object} params
-     */
-    reload(params) {}
+    load(searchParams) {}
 
     notify() {
         this.trigger("update");
@@ -40,15 +36,23 @@ export class Model extends EventBus {
 }
 Model.services = [];
 
+function getSearchParams(props) {
+    const params = {};
+    for (const key of SEARCH_KEYS) {
+        params[key] = props[key];
+    }
+    return params;
+}
+
 /**
  * @template {Model} T
- * @param {new (env: Object, services: Object) => T} ModelClass
- * @param {Object} loadParams
+ * @param {new (env: Object, params: Object, services: Object) => T} ModelClass
+ * @param {Object} params
  * @param {Object} [options]
  * @param {Function} [options.onUpdate]
  * @returns {T}
  */
-export function useModel(ModelClass, loadParams, options = {}) {
+export function useModel(ModelClass, params, options = {}) {
     const component = useComponent();
     if (!(ModelClass.prototype instanceof Model)) {
         throw new Error(`the model class should extend Model`);
@@ -57,25 +61,18 @@ export function useModel(ModelClass, loadParams, options = {}) {
     for (const key of ModelClass.services) {
         services[key] = useService(key);
     }
-    const model = new ModelClass(component.env, services);
+    const model = new ModelClass(component.env, params, services);
     useBus(model, "update", options.onUpdate || component.render);
 
-    const initialGroupBy = (loadParams.groupBy || component.props.groupBy).slice();
-
     onWillStart(() => {
-        return model.load(loadParams);
+        const searchParams = getSearchParams(component.props);
+        return model.load(searchParams);
     });
 
     onWillUpdateProps((nextProps) => {
-        const params = {};
-        for (const key of SEARCH_KEYS) {
-            params[key] = nextProps[key];
-        }
-        if (params.groupBy && params.groupBy.length === 0) {
-            params.groupBy = initialGroupBy;
-        }
-        params.useSampleModel = false;
-        return model.reload(params);
+        const searchParams = getSearchParams(nextProps);
+        searchParams.useSampleModel = false; // not sure it is good --> we'll know when implementing dashboard
+        return model.load(searchParams);
     });
     return model;
 }
