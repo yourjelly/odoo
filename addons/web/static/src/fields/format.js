528a73535697438d9394a749cf261b5d7f8b7a9b
Aaron Bohy <aab@odoo.com>
2021-07-09 11:35:14 +0000

    [IMP] web: add some field formatters

--- addons/web/static/src/fields/format.js
+++ addons/web/static/src/fields/format.js
@@ -1,5 +1,120 @@
 /** @odoo-module **/
 
+import {
+    formatFloat as _formatFloat,
+    formatInteger as _formatInteger,
+    humanNumber,
+} from "@web/core/l10n/numbers";
+import { registry } from "@web/core/registry";
+
+/**
+ * Returns a string representing a float.  The result takes into account the
+ * user settings (to display the correct decimal separator).
+ *
+ * @param {float|false} value the value that should be formatted
+ * @param {Object} [field] a description of the field (returned by fields_get
+ *   for example).  It may contain a description of the number of digits that
+ *   should be used.
+ * @param {Object} [options]
+ * @param {integer[]} [options.digits] the number of digits that should be used,
+ *   instead of the default digits precision in the field.
+ * @param {function} [options.humanReadable] function that takes the value in
+ *   argument, and if it returns true, the formatter acts like humanNumber
+ * @returns {string}
+ */
+export function formatFloat(value, field, options = {}) {
+    if (value === false) {
+        return "";
+    }
+    if (options.humanReadable && options.humanReadable(value)) {
+        return humanNumber(value, options);
+    }
+    let precision;
+    if (options.digits) {
+        precision = options.digits[1];
+    } else if (field && field.digits) {
+        precision = field.digits[1];
+    } else {
+        precision = 2;
+    }
+    return _formatFloat(value, { precision });
+}
+
+/**
+ * Returns a string representing a float value, from a float converted with a
+ * factor.
+ *
+ * @param {number} value
+ * @param {Object} [field] a description of the field
+ * @param {number} [options.factor=1.0] conversion factor
+ * @returns {string}
+ */
+export function formatFloatFactor(value, field, options = {}) {
+    if (value === false) {
+        return "";
+    }
+    const factor = options.factor || 1;
+    return formatFloat(value * factor, field, options);
+}
+
+/**
+ * Returns a string representing a time value, from a float.  The idea is that
+ * we sometimes want to display something like 1:45 instead of 1.75, or 0:15
+ * instead of 0.25.
+ *
+ * @param {float} value
+ * @param {Object} [field] a description of the field
+ * @param {Object} [options]
+ * @param {boolean} [options.noLeadingZeroHour] if true, format like 1:30
+ *   otherwise, format like 01:30
+ * @returns {string}
+ */
+export function formatFloatTime(value, field, options = {}) {
+    if (value === false) {
+        return "";
+    }
+    const isNegative = value < 0;
+    if (isNegative) {
+        value = Math.abs(value);
+    }
+    let hour = Math.floor(value);
+    let min = Math.round((value % 1) * 60);
+    if (min === 60) {
+        min = 0;
+        hour = hour + 1;
+    }
+    min = `${min}`.padStart(2, "0");
+    if (!options.noLeadingZeroHour) {
+        hour = `${hour}`.padStart(2, "0");
+    }
+    return `${isNegative ? "-" : ""}${hour}:${min}`;
+}
+
+/**
+ * Returns a string representing an integer.  If the value is false, then we
+ * return an empty string.
+ *
+ * @param {integer|false} value
+ * @param {Object} [field] a description of the field
+ * @param {Object} [options]
+ * @param {function} [options.humanReadable] function that takes the value in
+ *   argument, and if it returns true, the formatter acts like humanNumber
+ * @param {boolean} [options.isPassword=false] if returns true, acts like
+ * @returns {string}
+ */
+export function formatInteger(value, field, options = {}) {
+    if (value === false) {
+        return "";
+    }
+    if (options.isPassword) {
+        return new Array(value.length + 1).join("*");
+    }
+    if (options.humanReadable && options.humanReadable(value)) {
+        return humanNumber(value, options);
+    }
+    return _formatInteger(value);
+}
+
 /**
  * Returns a string representing an many2one.  If the value is false, then we
  * return an empty string. Note that it accepts two types of input parameters:
@@ -8,11 +123,12 @@
  * case, we assume that it is a record datapoint from a BasicModel.
  *
  * @param {Array|Object|false} value
+ * @param {Object} [field] a description of the field
  * @param {{escape?: boolean}} [options] additional options
  * @param {boolean} [options.escape=false] if true, escapes the formatted value
  * @returns {string}
  */
-export function formatMany2one(value, options) {
+export function formatMany2one(value, field, options) {
     if (!value) {
         value = "";
     } else if (Array.isArray(value)) {
@@ -30,3 +146,11 @@ export function formatMany2one(value, options) {
     }
     return value;
 }
+
+registry
+    .category("formatters")
+    .add("float", formatFloat)
+    .add("float_factor", formatFloatFactor)
+    .add("float_time", formatFloatTime)
+    .add("integer", formatInteger)
+    .add("many2one", formatMany2one);
--- addons/web/static/tests/fields/format_tests.js
+++ addons/web/static/tests/fields/format_tests.js
@@ -1,8 +1,74 @@
 /** @odoo-module **/
 
-import { formatMany2one } from "@web/fields/format";
+import { localization } from "@web/core/l10n/localization";
+import {
+    formatFloat,
+    formatFloatFactor,
+    formatFloatTime,
+    formatInteger,
+    formatMany2one,
+} from "@web/fields/format";
+import { defaultLocalization } from "../helpers/mock_services";
+import { patchWithCleanup } from "../helpers/utils";
 
-QUnit.module("Format Fields", {}, () => {
+QUnit.module("Format Fields", (hooks) => {
+    hooks.beforeEach(() => {
+        patchWithCleanup(localization, defaultLocalization);
+    });
+
+    QUnit.test("formatFloat", function (assert) {
+        patchWithCleanup(localization, { grouping: [3, 3, 3, 3] });
+        assert.strictEqual(formatFloat(1000000), "1,000,000.00");
+
+        patchWithCleanup(localization, { grouping: [3, 2, -1] });
+        assert.strictEqual(formatFloat(106500), "1,06,500.00");
+
+        patchWithCleanup(localization, { grouping: [1, 2, -1] });
+        assert.strictEqual(formatFloat(106500), "106,50,0.00");
+
+        patchWithCleanup(localization, {
+            grouping: [3, 0],
+            decimalPoint: ",",
+            thousandsSep: ".",
+        });
+        assert.strictEqual(formatFloat(6000), "6.000,00");
+        assert.strictEqual(formatFloat(false), "");
+    });
+
+    QUnit.test("formatFloatFactor", function (assert) {
+        assert.strictEqual(formatFloatFactor(false), "");
+        assert.strictEqual(formatFloatFactor(6000), "6,000.00");
+        assert.strictEqual(formatFloatFactor(6000, null, { factor: 3 }), "18,000.00");
+        assert.strictEqual(formatFloatFactor(6000, null, { factor: 0.5 }), "3,000.00");
+    });
+
+    QUnit.test("formatFloatTime", function (assert) {
+        assert.strictEqual(formatFloatTime(2), "02:00");
+        assert.strictEqual(formatFloatTime(3.5), "03:30");
+        assert.strictEqual(formatFloatTime(0.25), "00:15");
+
+        assert.strictEqual(formatFloatTime(-0.5), "-00:30");
+
+        const options = { noLeadingZeroHour: true };
+        assert.strictEqual(formatFloatTime(2, null, options), "2:00");
+        assert.strictEqual(formatFloatTime(3.5, null, options), "3:30");
+        assert.strictEqual(formatFloatTime(-0.5, null, options), "-0:30");
+    });
+
+    QUnit.test("formatInteger", function (assert) {
+        patchWithCleanup(localization, { grouping: [3, 3, 3, 3] });
+        assert.strictEqual(formatInteger(1000000), "1,000,000");
+
+        patchWithCleanup(localization, { grouping: [3, 2, -1] });
+        assert.strictEqual(formatInteger(106500), "1,06,500");
+
+        patchWithCleanup(localization, { grouping: [1, 2, -1] });
+        assert.strictEqual(formatInteger(106500), "106,50,0");
+
+        assert.strictEqual(formatInteger(0), "0");
+        assert.strictEqual(formatInteger(false), "");
+    });
+
     QUnit.test("formatMany2one", function (assert) {
         assert.strictEqual(formatMany2one(null), "");
         assert.strictEqual(formatMany2one([1, "A M2O value"]), "A M2O value");
@@ -13,12 +79,16 @@ QUnit.module("Format Fields", {}, () => {
             "A M2O value"
         );
 
-        assert.strictEqual(formatMany2one([1, "A M2O value"], { escape: true }), "A%20M2O%20value");
+        assert.strictEqual(
+            formatMany2one([1, "A M2O value"], null, { escape: true }),
+            "A%20M2O%20value"
+        );
         assert.strictEqual(
             formatMany2one(
                 {
                     data: { display_name: "A M2O value" },
                 },
+                null,
                 { escape: true }
             ),
             "A%20M2O%20value"
