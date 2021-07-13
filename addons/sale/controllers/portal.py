a3b8c0bf8951f73c4f0f4ad8bb55e2f8a36a5183
Julien Banken <jbn@odoo.com>
2021-07-13 14:01:09 +0200

    wip: sales

--- addons/sale/controllers/portal.py
+++ addons/sale/controllers/portal.py
@@ -39,56 +39,6 @@ class CustomerPortal(portal.CustomerPortal):
     # Quotations and Sales Orders
     #
 
-    @http.route(['/my/quotes', '/my/quotes/page/<int:page>'], type='http', auth="user", website=True)
-    def portal_my_quotes(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
-        values = self._prepare_portal_layout_values()
-        partner = request.env.user.partner_id
-        SaleOrder = request.env['sale.order']
-
-        domain = [
-            ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
-            ('state', 'in', ['sent', 'cancel'])
-        ]
-
-        searchbar_sortings = {
-            'date': {'label': _('Order Date'), 'order': 'date_order desc'},
-            'name': {'label': _('Reference'), 'order': 'name'},
-            'stage': {'label': _('Stage'), 'order': 'state'},
-        }
-
-        # default sortby order
-        if not sortby:
-            sortby = 'date'
-        sort_order = searchbar_sortings[sortby]['order']
-
-        if date_begin and date_end:
-            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
-
-        # count for pager
-        quotation_count = SaleOrder.search_count(domain)
-        # make pager
-        pager = portal_pager(
-            url="/my/quotes",
-            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
-            total=quotation_count,
-            page=page,
-            step=self._items_per_page
-        )
-        # search the count to display, according to the pager data
-        quotations = SaleOrder.search(domain, order=sort_order, limit=self._items_per_page, offset=pager['offset'])
-        request.session['my_quotations_history'] = quotations.ids[:100]
-
-        values.update({
-            'date': date_begin,
-            'quotations': quotations.sudo(),
-            'page_name': 'quote',
-            'pager': pager,
-            'default_url': '/my/quotes',
-            'searchbar_sortings': searchbar_sortings,
-            'sortby': sortby,
-        })
-        return request.render("sale.portal_my_quotations", values)
-
     @http.route(['/my/orders', '/my/orders/page/<int:page>'], type='http', auth="user", website=True)
     def portal_my_orders(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
         values = self._prepare_portal_layout_values()
@@ -97,7 +47,7 @@ class CustomerPortal(portal.CustomerPortal):
 
         domain = [
             ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
-            ('state', 'in', ['sale', 'done'])
+            ('state', '!=', 'draft')
         ]
 
         searchbar_sortings = {
