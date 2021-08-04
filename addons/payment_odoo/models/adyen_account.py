ac92dbbe99e9eec768743d2f80c0b22d7aad6f0b
Victor Feyens <vfe@odoo.com>
2021-08-04 14:51:02 +0200

    [WIP] payment_odoo: enable acquirer in test mode after acc creation.

--- addons/payment_odoo/models/adyen_account.py
+++ addons/payment_odoo/models/adyen_account.py
@@ -1,10 +1,32 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import logging
+from odoo import api, models
 
-from odoo import models
+_logger = logging.getLogger(__name__)
 
 
 class AdyenAccount(models.Model):
     _inherit = 'adyen.account'
+
+    @api.model
+    def create(self, values):
+        account = super().create(values)
+
+        # searching on adyen_account_id doesn't work
+        # to investigate if we have time
+        # or use direct ref ???
+        acqs = self.env['payment.acquirer'].search([
+            ('provider', '=', 'odoo'),
+            ('company_id', '=', self.env.company.id)
+        ])
+        if not acqs:
+            _logger.warning(
+                "Couldn't enable Odoo Payments since the acquirer wasn't found.")
+        # FIXME ANVFE: there is no journal on the acquirer by default, how to ensure we have one ?
+        # NOTE: the journal_id requirement is only imposed through the acquirer view,
+        # not through python code.
+        acqs.state = 'test'
+        return account
 
     def unlink(self):
         acquirer = self.env['payment.acquirer'].search([
