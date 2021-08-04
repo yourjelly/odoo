3199e875e8280463290ad15a1af39217bc5bc24b
Victor Feyens <vfe@odoo.com>
2021-08-04 14:48:46 +0200

    [IMP] payment_odoo: linting/cleanup

--- addons/payment_odoo/models/adyen_account.py
+++ addons/payment_odoo/models/adyen_account.py
@@ -7,6 +7,8 @@ class AdyenAccount(models.Model):
     _inherit = 'adyen.account'
 
     def unlink(self):
-        acquirer = self.env['payment.acquirer'].search([('provider', '=', 'odoo'), ('odoo_adyen_account_id', 'in', self.ids)])
+        acquirer = self.env['payment.acquirer'].search([
+            ('provider', '=', 'odoo'),
+            ('odoo_adyen_account_id', 'in', self.ids)])
         acquirer.state = 'disabled'
         return super().unlink()
