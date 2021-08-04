d8daf46a72d98575ecd71d38544fc693c9c812b3
Victor Feyens <vfe@odoo.com>
2021-08-04 14:50:00 +0200

    [IMP][POC] payment,sale: clean payment onboarding wizard
    and restrict acquirers to Adyen ?

--- addons/payment/wizards/payment_acquirer_onboarding_wizard.py
+++ addons/payment/wizards/payment_acquirer_onboarding_wizard.py
@@ -6,28 +6,36 @@ from odoo.exceptions import UserError
 
 class PaymentWizard(models.TransientModel):
     _name = 'payment.acquirer.onboarding.wizard'
-    _description = 'Payment acquire onboarding wizard'
+    _description = 'Payment acquirer onboarding wizard'
+
+    @api.model
+    def _default_payment_method(self):
+        return self._get_default_payment_acquirer_onboarding_value('payment_method')
 
     payment_method = fields.Selection([
         ('odoo', "Credit & Debit Card via Odoo Payments"),
         ('manual', "Custom payment instructions"),
-    ], string="Payment Method", default=lambda self: self._get_default_payment_acquirer_onboarding_value('payment_method'))
+    ], string="Payment Method", default=_default_payment_method)
 
     manual_name = fields.Char("Method", default=lambda self: self._get_default_payment_acquirer_onboarding_value('manual_name'))
     journal_name = fields.Char("Bank Name", default=lambda self: self._get_default_payment_acquirer_onboarding_value('journal_name'))
     acc_number = fields.Char("Account Number", default=lambda self: self._get_default_payment_acquirer_onboarding_value('acc_number'))
-    manual_post_msg = fields.Html("Payment Instructions")
+    manual_post_msg = fields.Html("Payment Instructions", compute="_compute_manual_post_msg", store=True, readonly=False)
 
     _data_fetched = fields.Boolean(store=False)
 
-    @api.onchange('journal_name', 'acc_number')
-    def _set_manual_post_msg_value(self):
-        self.manual_post_msg = _(
-            '<h3>Please make a payment to: </h3><ul><li>Bank: %s</li><li>Account Number: %s</li><li>Account Holder: %s</li></ul>',
-            self.journal_name or _("Bank"),
-            self.acc_number or _("Account"),
-            self.env.company.name
-        )
+    @api.depends('journal_name', 'acc_number')
+    def _compute_manual_post_msg(self):
+        for wizard in self:
+            if wizard.payment_method == 'manual':
+                wizard.manual_post_msg = _(
+                    '<h3>Please make a payment to: </h3><ul><li>Bank: %s</li><li>Account Number: %s</li><li>Account Holder: %s</li></ul>',
+                    wizard.journal_name or _("Bank"),
+                    wizard.acc_number or _("Account"),
+                    wizard.env.company.name
+                )
+            else:
+                wizard.manual_post_msg = ''
 
     _payment_acquirer_onboarding_cache = {}
 
@@ -35,7 +43,8 @@ class PaymentWizard(models.TransientModel):
         if env is None:
             env = self.env
         module_id = env.ref('base.module_payment_transfer').id
-        return env['payment.acquirer'].search([('module_id', '=', module_id),
+        return env['payment.acquirer'].search([
+            ('module_id', '=', module_id),
             ('company_id', '=', env.company.id)], limit=1)
 
     def _get_default_payment_acquirer_onboarding_value(self, key):
@@ -60,62 +69,66 @@ class PaymentWizard(models.TransientModel):
         return self._payment_acquirer_onboarding_cache.get(key, '')
 
     def _install_module(self, module_name):
-        module = self.env['ir.module.module'].sudo().search([('name', '=', module_name)])
+        module = self.env['ir.module.module'].sudo().search([('name', '=', module_name)], limit=1)
         if module.state not in ('installed', 'to install', 'to upgrade'):
             module.button_immediate_install()
+            return module
+        return self.env["ir.module.module"]
 
-    def _on_save_payment_acquirer(self):
-        self._install_module('account_payment')
+    def _setup_payment_transfer(self, new_env):
+        manual_acquirer = self._get_manual_payment_acquirer(new_env)
+        if not manual_acquirer:
+            raise UserError(_(
+                'No manual payment method could be found for this company. '
+                'Please create one from the Payment Acquirer menu.'
+            ))
 
-    def add_payment_methods(self):
-        """ Install required payment acquiers, configure them and mark the
-            onboarding step as done."""
+        manual_acquirer.name = self.manual_name
+        manual_acquirer.pending_msg = self.manual_post_msg
+        manual_acquirer.state = 'enabled'
 
-        if self.payment_method == 'odoo':
-            self._install_module('payment_odoo')
-
-        if self.payment_method in ('odoo', 'manual'):
-
-            self._on_save_payment_acquirer()
-
-            self.env.company.payment_onboarding_payment_method = self.payment_method
-
-            # create a new env including the freshly installed module(s)
-            new_env = api.Environment(self.env.cr, self.env.uid, self.env.context)
-
-            if self.payment_method == 'odoo':
-                # TODO do the correct stuff here for sale onboarding
-                new_env.ref('payment.payment_acquirer_odoo').write({
-                    'state': 'test',
-                })
-            if self.payment_method == 'manual':
-                manual_acquirer = self._get_manual_payment_acquirer(new_env)
-                if not manual_acquirer:
-                    raise UserError(_(
-                        'No manual payment method could be found for this company. '
-                        'Please create one from the Payment Acquirer menu.'
-                    ))
-                manual_acquirer.name = self.manual_name
-                manual_acquirer.pending_msg = self.manual_post_msg
-                manual_acquirer.state = 'enabled'
-
-                journal = manual_acquirer.journal_id
-                if journal:
-                    journal.name = self.journal_name
-                    journal.bank_acc_number = self.acc_number
-                else:
-                    raise UserError(_("You have to set a journal for your payment acquirer %s.", self.manual_name))
-
-            # delete wizard data immediately to get rid of residual credentials
-            self.sudo().unlink()
-        # the user clicked `apply` and not cancel so we can assume this step is done.
-        self._set_payment_acquirer_onboarding_step_done()
-        return {'type': 'ir.actions.act_window_close'}
+        journal = manual_acquirer.journal_id
+        if journal:
+            journal.name = self.journal_name
+            journal.bank_acc_number = self.acc_number
+        else:
+            raise UserError(_("You have to set a journal for your payment acquirer %s.", self.manual_name))
 
     def _set_payment_acquirer_onboarding_step_done(self):
         self.env.company.sudo().set_onboarding_step_done('payment_acquirer_onboarding_state')
 
-    def action_onboarding_other_payment_acquirer(self):
+    def add_payment_methods(self):
+        """ Install required payment acquirers, configure them and mark the
+            onboarding step as done."""
+
+        # TODO ANVFE first require proper company setup before trying to create an adyen account
+        # if payment meth is odoo?
+
+        self.env.company.payment_onboarding_payment_method = self.payment_method
+
+        installed_modules = self.env['ir.module.module']
+
+        if self.payment_method == 'odoo':
+            installed_modules += self._install_module('payment_odoo')
+
+        installed_modules += self._install_module('account_payment')
+
+        if installed_modules:
+            # create a new env including the freshly installed module(s)
+            new_env = api.Environment(self.env.cr, self.env.uid, self.env.context)
+            # self = self.with_env(new_env) ???
+        else:
+            new_env = self.env
+
+        if self.payment_method == 'manual':
+            self._setup_payment_transfer(new_env)
+
+        # delete wizard data immediately to get rid of residual credentials
+        # self.sudo().unlink()
+
+        # the user clicked `apply` and not cancel so we can assume this step is done.
+        # NOTE ANVFE: do we wait for adyen account creation to specify onboarding step as done ?
         self._set_payment_acquirer_onboarding_step_done()
-        action = self.env["ir.actions.actions"]._for_xml_id("payment.action_payment_acquirer")
-        return action
+        if self.payment_method == 'odoo':
+            return new_env['payment.acquirer'].odoo_create_adyen_account()
+        return {'type': 'ir.actions.act_window_close'}
