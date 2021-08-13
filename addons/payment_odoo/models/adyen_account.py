088f5588c9993c40d5541cd32c07ef2cb08fc26a
Victor Feyens <vfe@odoo.com>
2021-08-13 17:59:31 +0200

    TEST accounts + cleanup(s)

--- addons/payment_odoo/models/adyen_account.py
+++ addons/payment_odoo/models/adyen_account.py
@@ -8,25 +8,23 @@ _logger = logging.getLogger(__name__)
 class AdyenAccount(models.Model):
     _inherit = 'adyen.account'
 
-    @api.model
-    def create(self, values):
-        account = super().create(values)
+    # @api.model
+    # def create(self, values):
+    #     account = super().create(values)
 
-        # searching on adyen_account_id doesn't work
-        # to investigate if we have time
-        # or use direct ref ???
-        acqs = self.env['payment.acquirer'].search([
-            ('provider', '=', 'odoo'),
-            ('company_id', '=', self.env.company.id)
-        ])
-        if not acqs:
-            _logger.warning(
-                "Couldn't enable Odoo Payments since the acquirer wasn't found.")
-        # FIXME ANVFE: there is no journal on the acquirer by default, how to ensure we have one ?
-        # NOTE: the journal_id requirement is only imposed through the acquirer view,
-        # not through python code.
-        acqs.state = 'test'
-        return account
+    #     if account.is_test:
+    #         acqs = self.env['payment.acquirer'].search([
+    #             ('provider', '=', 'odoo'),
+    #             ('company_id', '=', self.env.company.id)
+    #         ])
+    #         if not acqs:
+    #             _logger.warning(
+    #                 "Couldn't enable Odoo Payments since the acquirer wasn't found.")
+    #         # FIXME ANVFE: there is no journal on the acquirer by default, how to ensure we have one ?
+    #         # NOTE: the journal_id requirement is only imposed through the acquirer view,
+    #         # not through python code.
+    #         acqs.state = 'test'
+    #     return account
 
     def unlink(self):
         acquirer = self.env['payment.acquirer'].search([
