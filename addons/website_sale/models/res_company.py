572736a90beb703967bd020d718876b7d2d3f411
Rémy Baranx (bar) <bar@odoo.com>
2021-09-03 11:10:10 +0200

    [IMP] odoo_payments,payment,payment_odoo,sale,website_sale: ui improvements

--- addons/website_sale/models/res_company.py
+++ addons/website_sale/models/res_company.py
@@ -10,8 +10,18 @@ class ResCompany(models.Model):
     website_sale_onboarding_payment_acquirer_state = fields.Selection([('not_done', "Not done"), ('just_done', "Just done"), ('done', "Done")], string="State of the website sale onboarding payment acquirer step", default='not_done')
 
     @api.model
+    def is_odoo_payment_available(self):
+        payments_methods = [m for m, _ in self.env["payment.acquirer.onboarding.wizard"]._selection_payment_methods()]
+        return "odoo" in payments_methods
+
+    @api.model
     def action_open_website_sale_onboarding_payment_acquirer(self):
         """ Called by onboarding panel above the quotation list."""
         self.env.company.get_chart_of_accounts_or_fail()
-        action = self.env["ir.actions.actions"]._for_xml_id("website_sale.action_open_website_sale_onboarding_payment_acquirer_wizard")
-        return action
+
+        if self.is_odoo_payment_available():
+            wizard = self.env["payment.acquirer.onboarding.wizard"].create({'payment_method': 'odoo'})
+            return wizard.add_payment_methods()
+        else:
+            action = self.env["ir.actions.actions"]._for_xml_id("website_sale.action_open_website_sale_onboarding_payment_acquirer_wizard")
+            return action
