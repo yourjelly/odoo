5cc725b5634c63d5da17ec9b857ec46db434305a
Lucas Perais (lpe) <lpe@odoo.com>
2021-09-07 14:30:43 +0200

    [FIX] web: introduce useActionLinks view hook
    The view Component supports clicking on links `a`
    that have a type="action"
    
    The feature is a lighter version of execute-action, since it doesn't support
    dynamic information from the model (e.g. basicModel)

--- addons/web/static/src/views/helpers/view_hook.js
+++ addons/web/static/src/views/helpers/view_hook.js
@@ -3,6 +3,9 @@
 import { useDebugCategory } from "@web/core/debug/debug_context";
 import { useSetupAction } from "@web/webclient/actions/action_hook";
 import { registry } from "@web/core/registry";
+import { useListener, useService } from "@web/core/utils/hooks";
+import { KeepLast } from "@web/core/utils/concurrency";
+import { evaluateExpr } from "@web/core/py_js/py";
 
 const { useComponent } = owl.hooks;
 
@@ -35,4 +38,81 @@ export function useViewArch(arch, params = {}) {
     }
 
     return processedArch;
+}
+
+/**
+ * Allows for a topmost component exclusively (usually a View component)
+ * to handle links with attribute type="action". This is used to support onboarding banners
+ * and content helpers.
+ *
+ * Note that this is similar but quite different from action buttons, since action links
+ * are not dynamic according to the record.
+ * @param {Object} params
+ * @param  {String} params.resModel The default resModel to which actions will apply
+ * @param  {KeepLast} [params.keepLast] The view's keeplast to allow concurrency management.
+ *   A new one is created if none is passed
+ */
+export function useActionLinks({ keepLast, resModel }) {
+    const selector = `a[type="action"]`;
+    const component = owl.hooks.useComponent();
+    const orm = useService("orm");
+    const { doAction } = useService("action");
+    keepLast = keepLast || new KeepLast();
+
+    async function handler(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+        const target = ev.target;
+        const data = target.dataset;
+
+        if (data.method !== undefined && data.model !== undefined) {
+            const options = {};
+            if (data.reloadOnClose) {
+                options.onClose = () => component.render();
+            }
+            const action = await keepLast.add(orm.call(data.model, data.method));
+            if (action !== undefined) {
+                keepLast.add(doAction(action, options));
+            }
+        } else if (target.getAttribute("name")) {
+            const options = {};
+            if (data.context) {
+                options.additionalContext = evaluateExpr(data.context);
+            }
+            keepLast.add(doAction(target.getAttribute("name"), options));
+        } else {
+            let views;
+            const resId = data.resid ? parseInt(data.resid, 10) : null;
+            if (data.views) {
+                views = evaluateExpr(data.views);
+            } else {
+                views = resId
+                    ? [[false, "form"]]
+                    : [
+                          [false, "list"],
+                          [false, "form"],
+                      ];
+            }
+
+            const action = {
+                name: target.getAttribute("title") || target.textContent.trim(),
+                type: "ir.actions.act_window",
+                res_model: data.model || resModel,
+                target: "current", // TODO: make customisable?
+                views,
+                domain: data.domain ? evaluateExpr(data.domain) : [],
+            };
+            if (resId) {
+                action.res_id = resId;
+            }
+
+            const options = {};
+            if (data.context) {
+                options.additionalContext = evaluateExpr(data.context);
+            }
+            keepLast.add(doAction(action, options));
+        }
+    }
+
+    useListener("click", selector, handler);
 }
