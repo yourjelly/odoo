ba80413dbc59d1ede0e39266c1febd9837b38fdf
Sébastien Geelen (sge) <sge@odoo.com>
2021-09-22 12:04:35 +0200

    [IMP] web,web_editor,mass_mailing: new dynamic placeholder
    Introduce a new Dynamic Placeholder Generator
    in mass mailing and SMS creation.
    
    Using a specific character (#) in subject or body will trigger
    the opening of the field selector popup, which can be use to select
    the wanted field and enter a default value. on validation the dynamic
    placeholder will be inserted in the corresponding input/editable.
    
    task-2154624

--- addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
+++ addons/web_editor/static/lib/odoo-editor/src/OdooEditor.js
@@ -2525,6 +2525,11 @@ export class OdooEditor extends EventTarget {
         for (let child of el.childNodes) {
             if (child.nodeType === Node.TEXT_NODE && child.length > 3) {
                 const childStr = child.nodeValue;
+                // avoid transforming dynamic placeholder pattern to url (in mass_mailing)
+                // eg. ${object.country_id.code}
+                if( childStr.match(/\${.*}/gi)) {
+                    continue;
+                }
                 const matches = getUrlsInfosInString(childStr);
                 if (matches.length) {
                     // We only to take care of the first match.
@@ -2572,8 +2577,13 @@ export class OdooEditor extends EventTarget {
             this.execCommand('insertHTML', this._prepareClipboardData(clipboardData));
         } else {
             const text = ev.clipboardData.getData('text/plain');
-            const splitAroundUrl = text.split(URL_REGEX);
+            let splitAroundUrl = [text];
             const linkAttributes = this.options.defaultLinkAttributes || {};
+
+            // avoid transforming dynamic placeholder pattern to url
+            if(!text.match(/\${.*}/gi)) {
+                splitAroundUrl = text.split(URL_REGEX);
+            }
 
             for (let i = 0; i < splitAroundUrl.length; i++) {
                 // Even indexes will always be plain text, and odd indexes will always be URL.
