ad0b4cecb8cd6193ff15cbea0792d05b0caad9f5
Antoine Guenet <age@odoo.com>
2021-09-24 21:27:55 +0200

    [IMP] mass_mailing: multiple transpiler improvements

--- addons/mass_mailing/static/src/js/mass_mailing_widget.js
+++ addons/mass_mailing/static/src/js/mass_mailing_widget.js
@@ -70,13 +70,22 @@ var MassMailingFieldHtml = FieldHtml.extend({
             convertInline.attachmentThumbnailToLinkImg($editable);
             convertInline.fontToImg($editable);
             convertInline.classToStyle($editable);
+            convertInline.bootstrapToTable($editable);
+            convertInline.cardToTable($editable);
+            convertInline.listGroupToTable($editable);
+            convertInline.addTables($editable);
+            convertInline.formatTables($editable);
+            convertInline.normalizeColors($editable);
+            convertInline.normalizeRem($editable);
 
-            // fix outlook image rendering bug
+            // fix outlook image rendering bug (this change will be kept in both
+            // fields)
             _.each(['width', 'height'], function (attribute) {
-                $editable.find('img[style*="width"], img[style*="height"]').attr(attribute, function () {
+                $editable.find('img').attr(attribute, function () {
                     return $(this)[attribute]();
                 }).css(attribute, function () {
-                    return $(this).get(0).style[attribute] || 'auto';
+                    const val = $(this).get(0).style[attribute];
+                    return val && val !== 'auto' ? val : '100%';
                 });
             });
 
