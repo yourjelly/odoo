1c26a0b868f28f471317f77c656ad213e63c77f4
Aaron Bohy <aab@odoo.com>
2021-10-20 14:37:39 +0200

    [FIX] add props validation + fix broken already converted views

--- addons/web/static/src/views/view.js
+++ addons/web/static/src/views/view.js
@@ -255,7 +255,6 @@ export class View extends Component {
             info: { actionMenus, mode: this.props.display.mode },
             arch,
             fields,
-            fieldsView: viewDescription[type],
             resModel,
             useSampleModel: false,
             className: `${this.props.className} o_view_controller o_${this.env.config.viewType}_view`,
