269afbc01ec5033ec58b4fb609d6155d48d31def
Jacky (trj) <trj@odoo.com>
2021-11-19 12:42:54 +0100

    Formatting clean up in model loadings

--- addons/pos_coupon/models/pos_session.py
+++ addons/pos_coupon/models/pos_session.py
@@ -6,7 +6,7 @@ from odoo.osv.expression import OR
 
 
 class PosSession(models.Model):
-    _inherit = "pos.session"
+    _inherit = 'pos.session'
 
     def _pos_ui_models_to_load(self):
         result = super()._pos_ui_models_to_load()
@@ -17,10 +17,10 @@ class PosSession(models.Model):
         return result
 
     def _loader_params_coupon_program(self):
-        return {'domain': [("id", "in", self.config_id.program_ids.ids), ("active", "=", True)], "fields": []}
+        return {'domain': [('id', 'in', self.config_id.program_ids.ids), ('active', '=', True)], 'fields': []}
 
     def _get_pos_ui_coupon_program(self, params):
-        return self.env["coupon.program"].search_read(params["domain"], params["fields"])
+        return self.env['coupon.program'].search_read(params['domain'], params['fields'])
 
     def _loader_params_product_product(self):
         result = super(PosSession, self)._loader_params_product_product()
@@ -28,5 +28,5 @@ class PosSession(models.Model):
             discount_product_ids = self.config_id.program_ids.mapped(lambda program: program.discount_line_product_id.id)
             reward_product_ids = self.config_id.program_ids.mapped(lambda program: program.reward_product_id.id)
             product_ids = [id for id in [*discount_product_ids, *reward_product_ids] if id]
-            result["domain"] = OR([result["domain"], [("id", "in", product_ids)]])
+            result['domain'] = OR([result['domain'], [('id', 'in', product_ids)]])
         return result
