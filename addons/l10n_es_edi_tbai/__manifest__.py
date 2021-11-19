a309c4c15f0a8b3b2f22d7cd7c837dfdbab3d141
Stanislas Gueniffey <stgu@odoo.com>
2021-11-19 11:40:36 +0100

    [ADD] Create l10n_es_edi_tbai for Bask Country EDI
    Add Tax administration configuration in res_company
    Re-use l10n_es_edi_sii certificates for now (to be adapted)

---
+++ addons/l10n_es_edi_tbai/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Thanks to Landoo and the Spanish community
+# Specially among others Aritz Olea, Luis Salvatierra, Josean Soroa
+# TODO more thanks, add them to module description ?
+
+{
+    'name': "Spain - TicketBAI",
+    'version': '1.0',
+    'category': 'Accounting/Localizations/EDI',
+    'application': False,
+    'description': """
+    This module sends invoices and vendor bills to the "Diputaciones
+    Forales" of Araba/Álava, Bizkaia and Gipuzkoa.
+
+    Invoices and bills get converted to XML and regularly sent to the
+    Bask government servers which provides them with a unique identifier.
+    A hash chain ensures the continuous nature of the invoice/bill
+    sequences. QR codes are added to emitted (sent/printed) invoices,
+    bills and tickets to allow anyone to check they have been declared.
+
+    You need to configure your certificate and the tax agency.
+    """,
+    'depends': [
+        'l10n_es',
+        'account_edi'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/l10n_es_tbai_certificate_views.xml',
+        'views/res_config_settings_views.xml'
+    ],
+    'license': 'LGPL-3',
+}
---
+++ addons/l10n_es_edi_tbai/models/res_company.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+TBAI_VERSION = 1.2
+TBAI_URLS = {
+    'invoice_test': {
+        'araba': '',
+        'bizkaia': '',
+        'gipuzkoa': 'https://tbai-prep.egoitza.gipuzkoa.eus/WAS/HACI/HTBRecepcionFacturasWEB/rest/recepcionFacturas/alta'
+    },
+    'invoice_prod': {
+        'araba': '',
+        'bizkaia': '',
+        'gipuzkoa': 'https://tbai-z.egoitza.gipuzkoa.eus/sarrerak/alta'
+    },
+    'qr_test': {
+        'araba': '',
+        'bizkaia': '',
+        'gipuzkoa': 'https://tbai.prep.gipuzkoa.eus/qr/'
+    },
+    'qr_prod': {
+        'araba': '',
+        'bizkaia': '',
+        'gipuzkoa': 'https://tbai.egoitza.gipuzkoa.eus/qr/'
+    },
+    'cancel_test': {
+        'araba': '',
+        'bizkaia': '',
+        'gipuzkoa': 'https://tbai-prep.egoitza.gipuzkoa.eus/WAS/HACI/HTBRecepcionFacturasWEB/rest/recepcionFacturas/anulacion'
+    },
+    'cancel_prod': {
+        'araba': '',
+        'bizkaia': '',
+        'gipuzkoa': 'https://tbai-z.egoitza.gipuzkoa.eus/sarrerak/baja'
+    }
+}
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    # === TBAI config ===
+    l10n_es_tbai_tax_agency = fields.Selection(
+        string="Tax Agency for TBAI",
+        selection=[
+            ('araba', "Hacienda Foral de Araba"),
+            ('bizkaia', "Hacienda Foral de Bizkaia"),
+            ('gipuzkoa', "Hacienda Foral de Gipuzkoa")
+        ],
+        default=False,  # TODO set default based on region
+    )
+
+    l10n_es_tbai_test_env = fields.Boolean(
+        string="Test Mode",
+        help="Use the test environment",
+        copy=False
+    )
+
+    l10n_es_tbai_url_invoice = fields.Char(
+        string="URL for submitting invoices",
+        readonly=True,
+        compute="_compute_l10n_es_tbai_url_invoice"
+    )
+
+    l10n_es_tbai_url_cancel = fields.Char(
+        string="URL for canceling invoices",
+        readonly=True,
+        compute="_compute_l10n_es_tbai_url_cancel"
+    )
+
+    l10n_es_tbai_url_qr = fields.Char(
+        string="URL for generating QR code",
+        readonly=True,
+        compute="_compute_l10n_es_tbai_url_qr"
+    )
+
+    # === CERTIFICATES ===
+    l10n_es_tbai_certificate_id = fields.Many2one(
+        string="Certificate (ES)",
+        store=True,
+        readonly=False,
+        comodel_name='l10n_es_edi_tbai.certificate',
+        compute="_compute_l10n_es_tbai_certificate",
+    )
+    l10n_es_tbai_certificate_ids = fields.One2many(
+        comodel_name='l10n_es_edi_tbai.certificate',
+        inverse_name='company_id',
+    )
+
+    def _compute_l10n_es_tbai_url(self, company, prefix):
+        if company.country_code == 'ES':
+            suffix = 'test' if company.l10n_es_edi_test_env else 'prod'
+            return TBAI_URLS[prefix + suffix][company.l10n_es_edi_tax_agency]
+        else:
+            return False
+
+    @api.depends('country_id', 'l10n_es_tbai_tax_agency', 'l10n_es_tbai_test_env')
+    def _compute_l10n_es_tbai_url_invoice(self):
+        for company in self:
+            company.l10n_es_tbai_url_invoice = self._compute_l10n_es_tbai_url('invoice_')
+
+    @api.depends('country_id', 'l10n_es_tbai_tax_agency', 'l10n_es_tbai_test_env')
+    def _compute_l10n_es_tbai_url_cancel(self):
+        for company in self:
+            company.l10n_es_tbai_url_cancel = self._compute_l10n_es_tbai_url('cancel_')
+
+    @api.depends('country_id', 'l10n_es_tbai_tax_agency', 'l10n_es_tbai_test_env')
+    def _compute_l10n_es_tbai_url_qr(self):
+        for company in self:
+            company.l10n_es_tbai_url_qr = self._compute_l10n_es_tbai_url('qr_')
+
+    @api.depends('country_id', 'l10n_es_tbai_certificate_ids')
+    def _compute_l10n_es_tbai_certificate(self):
+        for company in self:
+            if company.country_code == 'ES':
+                company.l10n_es_tbai_certificate_id = self.env['l10n_es_edi_tbai.certificate'].search(
+                    [('company_id', '=', company.id)],
+                    order='date_end desc',
+                    limit=1,
+                )
+            else:
+                company.l10n_es_tbai_certificate_id = False
