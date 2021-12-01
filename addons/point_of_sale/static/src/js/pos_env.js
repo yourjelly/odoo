71804ab6f6c6b9dc85a7635e49efc5574f9e31bd
Joseph Caburnay <jcb@odoo.com>
2021-12-01 14:58:58 +0100

    move posbos and posMutex to pos_env

--- addons/point_of_sale/static/src/js/pos_env.js
+++ addons/point_of_sale/static/src/js/pos_env.js
@@ -4,16 +4,20 @@
 // that are specific to point_of_sale and extensions.
 
 import env from 'web.env';
+import concurrency from 'web.concurrency';
 import devices from 'point_of_sale.devices';
 import BarcodeReader from 'point_of_sale.BarcodeReader';
 
-// TODO-REF: Move here posbus and posMutex from `point_of_sale.utils`.
 const proxy_queue = new devices.JobQueue(); // used to prevent parallels communications to the proxy
 const proxy = new devices.ProxyDevice(); // used to communicate to the hardware devices via a local proxy
 const barcode_reader = new BarcodeReader({ proxy });
+const posbus = new owl.core.EventBus();
+const posMutex = new concurrency.Mutex();
 
 export default Object.assign(env, {
     proxy_queue,
     proxy,
     barcode_reader,
+    posMutex,
+    posbus
 });
