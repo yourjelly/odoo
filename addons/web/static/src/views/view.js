a5f2cbbc7c88c99543e29502ff00da8d32aad877
Aaron Bohy <aab@odoo.com>
2021-12-06 09:18:12 +0100

    [FIX] web: searchMenuTypes prop should not go to concrete view

--- addons/web/static/src/views/view.js
+++ addons/web/static/src/views/view.js
@@ -119,6 +119,7 @@ const STANDARD_PROPS = [
     "globalState",
 
     "activateFavorite",
+    "searchMenuTypes",
 
     // LEGACY: remove this later (clean when mappings old state <-> new state are established)
     "searchPanel",
