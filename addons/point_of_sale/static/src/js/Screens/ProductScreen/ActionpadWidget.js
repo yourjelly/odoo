15cc6d37797de9bb9b39eacd1507681c30e4d470
Joseph Caburnay <jcb@odoo.com>
2021-12-06 14:36:04 +0100

    some clean up

--- addons/point_of_sale/static/src/js/Screens/ProductScreen/ActionpadWidget.js
+++ addons/point_of_sale/static/src/js/Screens/ProductScreen/ActionpadWidget.js
@@ -10,13 +10,8 @@ odoo.define('point_of_sale.ActionpadWidget', function(require) {
      * @emits click-pay
      */
     class ActionpadWidget extends PosComponent {
-        setup() {
-        }
         get isLongName() {
-            return this.client && this.client.name.length > 10;
-        }
-        get client() {
-            return this.props.client;
+            return this.props.client && this.props.client.name.length > 10;
         }
     }
     ActionpadWidget.template = 'ActionpadWidget';
