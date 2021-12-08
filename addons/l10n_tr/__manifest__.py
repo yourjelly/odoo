90b2f42f773f6f1d00e3e1944b3e09cebae26c64
Pouya Malekinejad (poma) <poma@odoo.com>
2021-12-08 21:38:26 +0330

    [imp] Correct CoA, 7A and 7/B options [rfr] New structure and naming convention(wan@odoo.com)

--- addons/l10n_tr/__manifest__.py
+++ addons/l10n_tr/__manifest__.py
@@ -19,12 +19,25 @@ Bu modül kurulduktan sonra, Muhasebe yapılandırma sihirbazı çalışır
         'account',
     ],
     'data': [
-        'data/l10n_tr_chart_data.xml',
-        'data/account.account.template.csv',
-        'data/l10n_tr_chart_post_data.xml',
-        'data/account_tax_group_data.xml',
-        'data/account_tax_template_data.xml',
+        # Chart of Accounts
         'data/account_chart_template_data.xml',
+        "data/account_account_tag_data.xml",
+        "data/account.account.template-common.csv",
+        "data/account.account.template-7a.csv",
+        "data/account.account.template-7b.csv",
+        "data/account.group.template.csv",
+
+        # Taxes
+        # "data/account_tax_group_data.xml",
+        # "data/account_tax_report_data.xml",
+        # "data/account_tax_template_data.xml",
+        # "data/account_fiscal_position_template_data.xml",
+        # "data/account_account_template_post_data.xml",
+
+        # post processing
+        "data/account_chart_post_data.xml",
+        "data/account_chart_template_try_loading.xml",
+
     ],
     'demo': [
         'demo/demo_company.xml',
