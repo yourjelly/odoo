2d3070a9b78e3e7487be901432ade5aac12c3986
FrancoisGe <fge@odoo.com>
2021-12-09 18:05:27 +0100

    Add Action Menus

--- addons/web/static/tests/views/helpers.js
+++ addons/web/static/tests/views/helpers.js
@@ -4,6 +4,7 @@ import { registry } from "@web/core/registry";
 import { makeTestEnv } from "@web/../tests/helpers/mock_env";
 import { getFixture, mount } from "@web/../tests/helpers/utils";
 import { getDefaultConfig, View } from "@web/views/view";
+import { MainComponentsContainer } from "@web/core/main_components_container";
 import { _getView } from "../helpers/mock_server";
 import {
     setupControlPanelFavoriteMenuRegistry,
@@ -65,6 +66,12 @@ export const makeView = async (params) => {
         props.arch = view.arch;
         props.searchViewArch = props.searchViewArch || "<search/>";
         props.searchViewFields = props.searchViewFields || Object.assign({}, props.fields);
+        if (props.loadActionMenus) {
+            props.actionMenus = props.actionMenus === undefined ? {} : props.actionMenus;
+        }
+        if (props.loadIrFilters) {
+            props.irFilters = props.irFilters === undefined ? [] : props.irFilters;
+        }
     }
 
     /** Legacy Environment, for compatibility sakes
@@ -83,7 +90,10 @@ export const makeView = async (params) => {
     }
     addLegacyMockEnvironment(env, legacyParams);
 
-    const view = await mount(View, getFixture(), { env, props });
+    const target = getFixture();
+    const view = await mount(View, target, { env, props });
+    await mount(MainComponentsContainer, target, { env, props });
+
     const viewNode = view.__owl__;
     const withSearchNode = Object.values(viewNode.children)[0];
     const concreteViewNode = Object.values(withSearchNode.children)[0];
