d979d4b9b00131ee5c50a9292a5e90e975d2b987
Victor Feyens <vfe@odoo.com>
2021-12-17 16:21:14 +0100

    [MOV] sale_stock: split sale_order.py

--- addons/sale_stock/models/__init__.py
+++ addons/sale_stock/models/__init__.py
@@ -4,6 +4,7 @@
 from . import account_move
 from . import res_company
 from . import sale_order
+from . import sale_order_line
 from . import res_config_settings
 from . import stock
 from . import product_template
