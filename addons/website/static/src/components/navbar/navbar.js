1caa397ce2d10849c63a5bb3c186cb026bdbb2e9
Younn Olivier <yol@odoo.com>
2021-12-31 17:37:01 +0100

    [IMP] website: patch navbar to display a specific website systray
    This commit adds a new website_systray registry, that the patched navbar
    will use when the user is editing the website through the WebsitePreview
    client action.
    
    task-2687506

---
+++ addons/website/static/src/components/navbar/navbar.js
@@ -0,0 +1,35 @@
+/** @odoo-module **/
+
+import { NavBar } from '@web/webclient/navbar/navbar';
+import { useService, useBus } from '@web/core/utils/hooks';
+import { registry } from "@web/core/registry";
+
+import { patch } from 'web.utils';
+
+const websiteSystrayRegistry = registry.category('website_systray');
+
+patch(NavBar.prototype, 'website_navbar', {
+    setup() {
+        this._super();
+        this.websiteService = useService('website');
+
+        // The navbar is rerendered with an event, as it can not naturally be
+        // with props/state (the WebsitePreview client action and the navbar
+        // are not related).
+        useBus(websiteSystrayRegistry, 'EDIT-WEBSITE', () => this.render(true));
+    },
+
+    /**
+     * @override
+     */
+    get systrayItems() {
+        if (this.websiteService.currentWebsite) {
+            return websiteSystrayRegistry
+                .getEntries()
+                .map(([key, value]) => ({ key, ...value }))
+                .filter((item) => ('isDisplayed' in item ? item.isDisplayed(this.env) : true))
+                .reverse();
+        }
+        return this._super();
+    }
+});
