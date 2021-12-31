20728cdbc8bf66cccb567b3e89aea90690c96069
Younn Olivier <yol@odoo.com>
2021-12-31 09:41:40 +0100

    [IMP] website, *: handle url redirections from website iframe
    *: portal, auth_signup, website_knowledge
    
    When navigating in the iframe, only same origin redirections should be
    open in the iframe contentWindow. External redirections should be done
    in the top window.
    
    Some internal pages had to be served with X-Frame-Options header set to
    SAMEORIGIN, and Content-Security-Policy to "frame-ancestors 'self'" (see
    [1]).
    
    All the links that are redirecting to another host, and the client
    actions, are opened in the top window.
    Exemples that will be opened in the iframe's top window:
    - Clicking on a link google.be that should not open in a new tab
    - Clicking on the language selector and adding a new one, or
    clicking on "logout".
    
    For that, an overridable _isTopWindow method is introduced on the client
    action, which will also be used to redirect when a wrong path param is
    given to the client action.
    
    task-2687506
    
    Co-authored-by: Arthur Detroux <ard@odoo.com>

--- addons/website/static/src/client_actions/website_preview/website_preview.js
+++ addons/website/static/src/client_actions/website_preview/website_preview.js
@@ -3,7 +3,7 @@
 import { registry } from '@web/core/registry';
 import { useService } from '@web/core/utils/hooks';
 
-const { Component, onWillStart, useEffect, useRef } = owl;
+const { Component, onWillStart, useRef } = owl;
 
 export class WebsitePreview extends Component {
     setup() {
@@ -15,15 +15,6 @@ export class WebsitePreview extends Component {
             await this.websiteService.fetchWebsites();
             this.initialUrl = `/website/force/${this.websiteId}?path=${this.path}`;
         });
-
-        useEffect(() => {
-            this.iframe.el.addEventListener('load', () => {
-                // This replaces the browser url (/web#action=website...) with
-                // the iframe's url (it is clearer for the user).
-                this.currentUrl = this.iframe.el.contentDocument.location.href;
-                history.replaceState({}, this.props.action.display_name, this.currentUrl);
-            });
-        }, () => []);
     }
 
     get websiteId() {
@@ -36,10 +27,55 @@ export class WebsitePreview extends Component {
 
     get path() {
         let path = this.props.action.context.params && this.props.action.context.params.path;
-        if (!path) {
+        if (path) {
+            const url = new URL(path, window.location.origin);
+            // If a path with an external domain, or matching a backend route
+            // that should be opened in the top window, is passed as a
+            // paramater, it is likely that the user did not do that
+            // intentionally. He is redirected to his homepage.
+            if (this._isTopWindow(url)) {
+                path = '/';
+            }
+        } else {
             path = '/';
         }
         return path;
+    }
+
+    /**
+     * Returns true if the url should be opened in the top
+     * window.
+     *
+     * @param host {string} host of the route.
+     * @param pathname {string} path of the route.
+     * @private
+     */
+    _isTopWindow({ host, pathname }) {
+        const backendRoutes = ['/web', '/web/session/logout'];
+        return host !== window.location.host || (pathname && backendRoutes.includes(pathname));
+    }
+
+    _onPageLoaded() {
+        // This replaces the browser url (/web#action=website...) with
+        // the iframe's url (it is clearer for the user).
+        this.currentUrl = this.iframe.el.contentDocument.location.href;
+        history.replaceState({}, this.props.action.display_name, this.currentUrl);
+
+        // The clicks on the iframe are listened, so that links with external
+        // redirections can be opened in the top window.
+        this.iframe.el.contentDocument.addEventListener('click', (ev) => {
+            const linkEl = ev.target.closest('[href]');
+            if (!linkEl) {
+                return;
+            }
+
+            const { href, target } = linkEl;
+            if (href && target !== '_blank' && !this.websiteContext.edition && this._isTopWindow(linkEl)) {
+                ev.preventDefault();
+                ev.stopPropagation();
+                window.location.replace(href);
+            }
+        });
     }
 }
 WebsitePreview.template = 'website.WebsitePreview';
