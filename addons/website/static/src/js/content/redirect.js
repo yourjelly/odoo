1a422911e981630c4f7bbbea652ed628a2b7990a
Younn Olivier <yol@odoo.com>
2021-12-31 09:41:40 +0100

    [IMP] website, *: handle url redirections from website iframe
    *: portal, auth_signup
    
    In order to navigate properly inside the iframe, some internal pages had
    to be served with X-Frame-Options header set to SAMEORIGIN.
    
    task-2687506

--- addons/website/static/src/js/content/redirect.js
+++ addons/website/static/src/js/content/redirect.js
@@ -2,13 +2,28 @@
 import { session } from '@web/session';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const htmlEl = document.documentElement;
-    if (!session.is_website_user && !window.frameElement) {
-        const websiteId = htmlEl.dataset.websiteId;
+    if (session.is_website_user) {
+        return;
+    }
+
+    if (!window.frameElement) {
+        const websiteId = document.documentElement.dataset.websiteId;
         const {pathname, search} = window.location;
         const params = new URLSearchParams(search);
         document.body.innerHTML = '';
 
         window.location.replace(`/web#action=website.website_editor&path=${encodeURIComponent(pathname + '?' + params.toString())}&website_id=${websiteId}`);
+    } else {
+        document.addEventListener('click', (ev) => {
+            const isEditorEnabled = document.body.classList.contains('editor_enable');
+            const {href, host, target} = ev.target;
+            const isNewWindow = target === '_blank';
+            const isInIframe = host === window.location.host;
+
+            if (href && !isEditorEnabled && !isNewWindow && !isInIframe) {
+                window.top.location.replace(href);
+            }
+        });
+
     }
 });
