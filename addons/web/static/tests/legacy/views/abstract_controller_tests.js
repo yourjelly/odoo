044f222c626efac52ab01f325150c477da6f2c08
Jorge Pinna Puissant <jpp@odoo.com>
2022-01-06 16:04:42 +0100

    search panel focus -- nextTick ?

--- addons/web/static/tests/legacy/views/abstract_controller_tests.js
+++ addons/web/static/tests/legacy/views/abstract_controller_tests.js
@@ -9,6 +9,7 @@ var BasicView = require("web.BasicView");
 var BasicRenderer = require("web.BasicRenderer");
 const AbstractRenderer = require('web.AbstractRendererOwl');
 const RendererWrapper = require('web.RendererWrapper');
+const { nextTick } = require('web.test_utils');
 
 function getHtmlRenderer(html) {
     return BasicRenderer.extend({
@@ -132,6 +133,7 @@ QUnit.module("Views", {
             model: "test_model",
             arch: "<test/>",
         });
+        await nextTick(); //NXOWL I'm not sure why we need to wait now, and we didn't need this before
         assert.hasClass(document.activeElement, "o_searchview_input");
         view.destroy();
     });
