7be6e098b77096db75acd0726e5eb63b82babdea
Joseph Caburnay <jcb@odoo.com>
2022-01-07 11:31:00 +0100

    fix pos_cache after moving 'loaded' methods to backend

--- addons/pos_cache/static/src/js/pos_cache.js
+++ addons/pos_cache/static/src/js/pos_cache.js
@@ -2,35 +2,12 @@ odoo.define('pos_cache.pos_cache', function (require) {
 "use strict";
 
 var models = require('point_of_sale.models');
-var core = require('web.core');
-var { Gui } = require('point_of_sale.Gui');
 const Registries = require('point_of_sale.Registries');
-var _t = core._t;
 var pos_env = require('point_of_sale.env');
 
-function roundUpDiv(y, x) {
-    const remainder = y % x;
-    return (y - remainder) / x + (remainder > 0 ? 1 : 0);
-}
 
 Registries.PosModelRegistry.extend(models.PosGlobalState, (PosGlobalState) => {
 class PosCachePosModel extends PosGlobalState {
-    async loadProductsBackground() {
-        if (this.config.limited_products_loading) {
-            // Just do the native way of loading products when limited_product_loading is active.
-            return super.loadProductsBackground(...arguments);
-        }
-        const nInitiallyLoaded = Object.keys(this.db.product_by_id).length;
-        const totalProductsCount = await this._getTotalProductsCount();
-        const nRemaining = totalProductsCount - nInitiallyLoaded;
-        if (!(nRemaining > 0)) return;
-        const multiple = 100000;
-        const nLoops = roundUpDiv(nRemaining, multiple);
-        for (let i = 0; i < nLoops; i++) {
-            await this._loadCachedProducts(i * multiple + nInitiallyLoaded, (i + 1) * multiple + nInitiallyLoaded);
-        }
-        Gui.showNotification(_t('All products are loaded.'), 5000);
-    }
     async _getTotalProductsCount() {
         return pos_env.services.rpc({
             model: 'pos.session',
