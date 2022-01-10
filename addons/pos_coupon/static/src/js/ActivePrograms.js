879217b7b2e55f244413f8c09b2c97ade2fdefbf
Joseph Caburnay <jcb@odoo.com>
2022-01-10 11:06:42 +0100

    pos_coupon: remove TODO-REF and no need for toJson

--- addons/pos_coupon/static/src/js/ActivePrograms.js
+++ addons/pos_coupon/static/src/js/ActivePrograms.js
@@ -4,8 +4,7 @@ odoo.define('pos_coupon.ActivePrograms', function (require) {
     const PosComponent = require('point_of_sale.PosComponent');
     const Registries = require('point_of_sale.Registries');
 
-    // TODO-REF: We can just actually put the template of this
-    // component to the OrderWidget template.
+    // TODO: Remove this component and put its contents to the OrderWidget template.
     class ActivePrograms extends PosComponent {}
     ActivePrograms.template = 'ActivePrograms';
 
