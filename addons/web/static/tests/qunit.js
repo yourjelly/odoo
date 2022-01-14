990a60024c855f40f60963f90ea023295fa66c7d
Aaron Bohy <aab@odoo.com>
2022-01-14 09:15:59 +0100

    [REMOVEME]

--- addons/web/static/tests/qunit.js
+++ addons/web/static/tests/qunit.js
@@ -320,6 +320,10 @@
         QUnit.only(name, cb);
     };
 
+    QUnit.skipNXOWL = (name, cb) => {
+        QUnit.skip(name, cb);
+    };
+
     // Override global UnhandledRejection that is assigned wayyy before this file
     // Do not really crash on non-errors rejections
     const qunitUnhandledReject = QUnit.onUnhandledRejection;
