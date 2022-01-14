6d2848bf091ca41ea37921f01ab46ae2b8d083b6
Christophe Monniez <moc@odoo.com>
2022-01-14 15:05:19 +0100

    randomizeapps

--- addons/web/tests/test_click_everywhere.py
+++ addons/web/tests/test_click_everywhere.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import logging
+import random
 import odoo.tests
 
 _logger = logging.getLogger(__name__)
@@ -11,12 +12,14 @@ class TestMenusAdmin(odoo.tests.HttpCase):
 
     def test_01_click_everywhere_as_admin(self):
         menus = self.env['ir.ui.menu'].load_menus(False)
-        for app_id in menus['root']['children']:
-            if menus[app_id]['name'] in ('Payroll', 'Settings'):
-                with self.subTest(app=menus[app_id]['name']):
-                    _logger.runbot('Testing %s', menus[app_id]['name'])
-                    self.browser_js("/web", "odoo.__DEBUG__.services['web.clickEverywhere']('%s');" % menus[app_id]['xmlid'], "odoo.isReady === true", login="admin", timeout=300)
-                    self.terminate_browser()
+        target_menus = {x for x in menus['root']['children'] if menus[x]['name'] in ('Payroll', 'Settings')}
+        random_menus= set(random.choices(menus['root']['children'], k=10))
+
+        for app_id in target_menus | random_menus:
+            with self.subTest(app=menus[app_id]['name']):
+                _logger.runbot('Testing %s', menus[app_id]['name'])
+                self.browser_js("/web", "odoo.__DEBUG__.services['web.clickEverywhere']('%s');" % menus[app_id]['xmlid'], "odoo.isReady === true", login="admin", timeout=300)
+                self.terminate_browser()
 
 
 @odoo.tests.tagged('click_all', 'post_install', '-at_install', '-standard')
