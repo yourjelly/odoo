44546cd81f0b0d03c05cd59e2be2aaf0662df3ff
Younn Olivier <yol@odoo.com>
2022-01-17 10:59:08 +0100

    [IMP] website: switch to fullscreen when pressing esc
    task-2687506

--- addons/website/static/src/js/content/redirect.js
+++ addons/website/static/src/js/content/redirect.js
@@ -7,6 +7,8 @@ const _t = str => str;
 /**
  * This script, served with frontend pages, displays buttons in the top left
  * corner to provide the authenticated user an access to his odoo backend.
+ * In the case of the page being viewed in the website_preview client action,
+ * it will forward some events to its parent.
  */
 document.addEventListener('DOMContentLoaded', () => {
     if (session.is_website_user) {
@@ -56,6 +58,12 @@ document.addEventListener('DOMContentLoaded', () => {
             if (href && !isEditorEnabled && !isNewWindow && !isInIframe) {
                 window.top.location.replace(href);
             }
+        });
+        document.addEventListener('keydown', ev => {
+            window.frameElement.dispatchEvent(new KeyboardEvent('keydown', ev));
+        });
+        document.addEventListener('keyup', ev => {
+            window.frameElement.dispatchEvent(new KeyboardEvent('keyup', ev));
         });
     }
 });
