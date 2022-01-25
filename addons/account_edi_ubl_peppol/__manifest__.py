58693b20e6492fd21ef4ee32d0029307db623840
Laurent Smet <las@odoo.com>
2022-01-25 12:00:20 +0100

    m

---
+++ addons/account_edi_ubl_peppol/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Import/Export invoices with Peppol",
+    'version': '1.0',
+    'category': 'Accounting/Accounting',
+    'depends': ['account_edi'],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
