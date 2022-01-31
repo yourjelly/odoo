14b7ffa35f4e78207954ef6fd49d3298dab5b254
Aaron Bohy <aab@odoo.com>
2022-01-31 15:01:10 +0100

    [FIX] web: ControlPanel: do not call hook outside setup

--- addons/web/static/src/legacy/js/control_panel/control_panel.js
+++ addons/web/static/src/legacy/js/control_panel/control_panel.js
@@ -99,11 +99,17 @@ odoo.define('web.ControlPanel', function (require) {
         setup() {
             this.additionalContent = getAdditionalContent(this.props);
 
-            useSubEnv({
+            const subEnv = {
                 action: this.props.action,
                 searchModel: this.props.searchModel,
-                view: this.props.view,
+            };
+            this.subEnvView = this.props.view;
+            Object.defineProperty(subEnv, "view", {
+                get() {
+                    return this.subEnvView;
+                },
             });
+            useSubEnv(subEnv);
 
             // Connect to the model
             // TODO: move this in enterprise whenever possible
@@ -155,9 +161,7 @@ odoo.define('web.ControlPanel', function (require) {
             // the lifespan of a ControlPanel instance, so we only need to update
             // the view information.
             if ('view' in nextProps) {
-                useSubEnv({
-                    view: nextProps.view,
-                });
+                this.subEnvView = nextProps.view;
             }
             if ('fields' in nextProps) {
                 this.fields = this._formatFields(nextProps.fields);
