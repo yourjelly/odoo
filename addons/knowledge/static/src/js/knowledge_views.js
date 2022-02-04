82bc93fba96a452103b1755afe817465feb4a386
Julien Banken <jbn@odoo.com>
2022-02-04 10:04:07 +0100

    [IMP] knowledge: add breadcrumb
    On the form view, the breadcrumb will be located in the control panel.
    For the knowledge module, we needed a headless layout. In the mockup,
    the breadcrumb has to be placed within the form sheet.
    
    Unfortunately, the breadcumb is a owl component and its logic is
    encapsulated in a function which makes it uneditable.
    
    To avoid having to update the core, we will render a custom breadcumb
    that will be generated based on the `breadcrumb` parameter passed to the
    `init` function of the form view.

--- addons/knowledge/static/src/js/knowledge_views.js
+++ addons/knowledge/static/src/js/knowledge_views.js
@@ -12,6 +12,25 @@ const KnowledgeFormView = FormView.extend({
         Model: KnowledgeFormModel,
         Renderer: KnowledgeFormRenderer,
     }),
+    /**
+     * For the knowledge module, we want to display a headless layout.
+     * To get a headless layout, the `init` function will deactivate the
+     * control panel as well as the search bar and the default breadcrumb.
+     * The form view will directly be open in the 'readonly' mode
+     * to ensure that the user will not unintentionally edit the
+     * document while doing a copy-past for instance.
+     * @override
+     * @param {Object} viewInfo
+     * @param {Object} params
+     */
+    init: function (viewInfo, params) {
+        params.withBreadcrumbs = false;
+        params.withControlPanel = false;
+        params.withSearchBar = false;
+        params.mode = 'readonly';
+        this._super.apply(this, arguments);
+        this.rendererParams.breadcrumbs = params.breadcrumbs;
+    },
 });
 
 viewRegistry.add('knowledge_view_form', KnowledgeFormView);
