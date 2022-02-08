1b7b1dc04f7731492a2d2adea1c6cfacd00e773b
Bruno Boi <boi@odoo.com>
2022-02-08 13:59:21 +0100

    use standaloneAdapter as web root.widget
    https://github.com/odoo/odoo/pull/80156#discussion_r797706488

--- addons/web/static/src/legacy/root_widget.js
+++ addons/web/static/src/legacy/root_widget.js
@@ -1,6 +1,6 @@
 odoo.define("root.widget", function (require) {
     require("web.legacySetup");
-    const { ComponentAdapter } = require("web.OwlCompatibility");
-
-    return Object.create(ComponentAdapter); // for its method _trigger_up
+    const { standaloneAdapter } = require("web.OwlCompatibility");
+    const { Component } = owl;
+    return standaloneAdapter({ Component }); // for its method _trigger_up
 });
