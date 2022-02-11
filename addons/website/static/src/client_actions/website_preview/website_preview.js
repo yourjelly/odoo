9af09eb44bbf381ca5b1b291533f20c8c8cabdcf
Younn Olivier <yol@odoo.com>
2022-02-11 11:01:53 +0100

    [IMP] website: display website iframe url from website_editor action
    The browser's url is replaced when navigating inside the website from
    the client action's iframe.
    Exemple:
    "/web#action=website.website_preview&path=/shop" becomes "/shop".
    
    task-2687506

--- addons/website/static/src/client_actions/website_preview/website_preview.js
+++ addons/website/static/src/client_actions/website_preview/website_preview.js
@@ -3,16 +3,27 @@
 import { registry } from '@web/core/registry';
 import { useService } from '@web/core/utils/hooks';
 
-const { Component, onWillStart } = owl;
+const { Component, onWillStart, useEffect, useRef } = owl;
 
 export class WebsitePreview extends Component {
     setup() {
         this.websiteService = useService('website');
 
+        this.iframe = useRef('iframe');
+
         onWillStart(async () => {
             await this.websiteService.fetchWebsites();
             this.initialUrl = `/website/force/${this.websiteId}?path=${this.path}`;
         });
+
+        useEffect(() => {
+            this.iframe.el.addEventListener('load', () => {
+                // This replaces the browser url (/web#action=website...) with
+                // the iframe's url (it is clearer for the user).
+                this.currentUrl = this.iframe.el.contentDocument.location.href;
+                history.pushState({}, this.props.action.display_name, this.currentUrl);
+            });
+        }, () => []);
     }
 
     get websiteId() {
