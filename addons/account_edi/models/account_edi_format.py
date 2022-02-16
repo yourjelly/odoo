85b887d07403a502475625b54f62a1f40da9b2b0
roen-odoo <roen@odoo.com>
2022-02-16 15:36:07 +0100

    [FIX] : test3
    Current behavior:
    
    Steps to reproduce:
    
    opw-12

--- addons/account_edi/models/account_edi_format.py
+++ addons/account_edi/models/account_edi_format.py
@@ -19,7 +19,7 @@ _logger = logging.getLogger(__name__)
 class AccountEdiFormat(models.Model):
     _name = 'account.edi.format'
     _description = 'EDI format'
-azeazaze
+azeazazeaze
     name = fields.Char()
     code = fields.Char(required=True)
 
