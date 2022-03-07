949e1bf7e4b62e75be54ad7683014bf48ed22cd8
qsm-odoo <qsm@odoo.com>
2022-03-07 12:04:46 +0100

    [WIP] website: update mini-nav of previous commit

--- addons/website/static/src/js/content/redirect.js
+++ addons/website/static/src/js/content/redirect.js
@@ -22,29 +22,17 @@ document.addEventListener('DOMContentLoaded', () => {
             const frontendToBackendNavEl = document.createElement('div');
             frontendToBackendNavEl.classList.add('o_frontend_to_backend_nav');
 
-            const backendLogoEl = document.createElement('div');
-            backendLogoEl.classList.add('o_frontend_to_backend_logo');
-            frontendToBackendNavEl.appendChild(backendLogoEl);
-
-            const backendButtonEl = document.createElement('a');
-            backendButtonEl.href = `/web#action=website.website_editor&path=${encodeURIComponent(pathname + '?' + params.toString())}&website_id=${websiteId}`;
-            backendButtonEl.title = _t("Configure your website");
-            backendButtonEl.classList.add('o_frontend_to_backend_btn');
-            frontendToBackendNavEl.appendChild(backendButtonEl);
-
-            const backendEditButtonEl = document.createElement('a');
-            backendEditButtonEl.href = `/web#action=website.website_editor&path=${encodeURIComponent(pathname + '?' + params.toString())}&website_id=${websiteId}`;
-            backendEditButtonEl.title = _t("Edit your page content");
-            backendEditButtonEl.classList.add('o_frontend_to_backend_edit_btn', 'fa', 'fa-pencil-square-o');
-            frontendToBackendNavEl.appendChild(backendEditButtonEl);
-
             const backendAppsButtonEl = document.createElement('a');
             backendAppsButtonEl.href = '/web';
             backendAppsButtonEl.title = _t("Go to your Odoo Apps");
             backendAppsButtonEl.classList.add('o_frontend_to_backend_apps_btn', 'fa', 'fa-th');
             frontendToBackendNavEl.appendChild(backendAppsButtonEl);
 
-            backendLogoEl.addEventListener('click', () => backendButtonEl.click());
+            const backendEditButtonEl = document.createElement('a');
+            backendEditButtonEl.href = `/web#action=website.website_editor&path=${encodeURIComponent(pathname + '?' + params.toString())}&website_id=${websiteId}`;
+            backendEditButtonEl.title = _t("Edit your page content");
+            backendEditButtonEl.classList.add('o_frontend_to_backend_edit_btn', 'fa', 'fa-pencil-square-o');
+            frontendToBackendNavEl.appendChild(backendEditButtonEl);
 
             document.body.appendChild(frontendToBackendNavEl);
         }
