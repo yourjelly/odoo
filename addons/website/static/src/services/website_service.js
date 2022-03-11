5e26fcee8491bf93495aecb29a342a6c53afa8f0
Younn Olivier <yol@odoo.com>
2022-03-11 11:14:52 +0100

    [FIX] website: return to the correct url after editing in backend
    Before this commit, clicking on the breadcrumb item "Website Editor"
    after an "Edit in backend" systray item click would not redirect to the
    edited object path. It would redo the website_editor action with the
    inital params (most often path is undefined, and the component will by
    default redirect to '/').
    
    Now, when clicking on the "Edit in backend" systray, the frontend path
    of the edited object is saved in the service. The client action
    component takes it as the default path for the iframe if there is one.
    
    task-2687506

--- addons/website/static/src/services/website_service.js
+++ addons/website/static/src/services/website_service.js
@@ -24,6 +24,8 @@ export const websiteService = {
         let currentWebsiteId;
         let currentMetadata = {};
         let pageDocument;
+        let contentWindow;
+        let editedObjectPath;
         const context = reactive({
             showNewContentModal: false,
         });
@@ -65,6 +67,18 @@ export const websiteService = {
             },
             get pageDocument() {
                 return pageDocument;
+            },
+            set contentWindow(window) {
+                contentWindow = window;
+            },
+            get contentWindow() {
+                return contentWindow;
+            },
+            set editedObjectPath(path) {
+                editedObjectPath = path;
+            },
+            get editedObjectPath() {
+                return editedObjectPath;
             },
             goToWebsite({ websiteId = currentWebsiteId || websites[0].id, path = '/' } = {}) {
                 action.doAction('website.website_editor', {
