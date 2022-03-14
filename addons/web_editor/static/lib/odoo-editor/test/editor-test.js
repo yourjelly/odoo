7917c865f980b49a962f90de6cf0894d7afb6b17
abd-msyukyu-odoo <abd@odoo.com>
2022-03-14 18:57:15 +0100

    [IMP] web_editor: add navigation node
    Since new commands for the Knowledge module (like /template and /file) will be
    CONTENTEDITABLE=FALSE, it may be difficult to add a text node between such
    blocks once they are siblings.
    
    This new concept uses a temporary `<P>` node that will appear in between blocks
    or while exiting a block using ArrowUp and ArrowDown keys. This node is
    temporary and will disappear if the selection change. If the user starts typing
    in such a block, it will be validated in the dom and won't be removed anymore.
    
    Such a node will also be useful with existing blocks (table, code, ...)
    
    Task-ID: 2794990

--- addons/web_editor/static/lib/odoo-editor/test/editor-test.js
+++ addons/web_editor/static/lib/odoo-editor/test/editor-test.js
@@ -12,6 +12,7 @@ import './spec/autostep.test.js';
 import './spec/urlRegex.test.js';
 import './spec/collab.test.js';
 import './spec/unremovable.test.js';
+import './spec/navigationNode.test.js';
 
 mocha.run(failures => {
     if (failures) {
