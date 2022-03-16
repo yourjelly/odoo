6b2d1edbd68ff9a8f0b2b4e05616f7ab299a0b52
Julien Banken <jbn@odoo.com>
2022-03-16 21:50:29 +0100

    [IMP] knowledge: move chatter to the right
    [FIX] knowledge: click on chatter icon
    
    Purpose
    =======
    For some weird reason the icon on the chatter didn't work when first
    importing the module. When removing the import lines, it works again.

--- addons/knowledge/static/src/models/activity/chatter.js
+++ addons/knowledge/static/src/models/activity/chatter.js
@@ -3,9 +3,6 @@
 import { addRecordMethods } from '@mail/model/model_core';
 import core from 'web.core';
 
-// ensure that the model definition is loaded before the patch
-import '@mail/models/chatter/chatter';
-
 addRecordMethods('Chatter', {
     onClickChatterSearchArticle(event) {
         core.bus.trigger("openMainPalette", {
