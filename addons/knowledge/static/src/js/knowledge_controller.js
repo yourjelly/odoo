e7d3893c55459a83087ff6a43a5aee6d3b32bc68
Julien Banken <jbn@odoo.com>
2022-03-17 16:00:50 +0100

    [IMP] knowledge: open article link in editor

--- addons/knowledge/static/src/js/knowledge_controller.js
+++ addons/knowledge/static/src/js/knowledge_controller.js
@@ -21,6 +21,7 @@ const KnowledgeFormController = FormController.extend({
     custom_events: Object.assign({}, FormController.prototype.custom_events, {
         create: '_onCreate',
         move: '_onMove',
+        open: '_onOpen',
     }),
 
     /**
@@ -94,6 +95,18 @@ const KnowledgeFormController = FormController.extend({
      */
     _onMove: async function (event) {
         await this._move(event.data);
+    },
+
+    /**
+     * @param {Event} event
+     */
+    _onOpen: async function (event) {
+        const { data } = event;
+        this.do_action('knowledge.action_home_page', {
+            additional_context: {
+                res_id: data.article_id
+            }
+        });
     },
 
     /**
