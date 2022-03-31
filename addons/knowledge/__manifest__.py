809258cc24513ff295741a4f84c89268005fa557
Fabio Barbero <faba@odoo.com>
2022-03-31 15:46:14 +0200

    [IMP] knowledge: add data
    Add new data for knowledge, remove demo data.

--- addons/knowledge/__manifest__.py
+++ addons/knowledge/__manifest__.py
@@ -21,7 +21,6 @@
         'security/ir.model.access.csv',
         'security/security.xml',
     ],
-    'demo': ['data/knowledge_demo.xml'],
     'installable': True,
     'application': True,
     'auto_install': False,
