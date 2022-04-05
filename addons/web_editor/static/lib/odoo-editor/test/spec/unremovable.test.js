f210757a7068110714e889e75b2f74a7dacf5b17
abd-msyukyu-odoo <abd@odoo.com>
2022-04-05 16:37:30 +0200

    [FIX] web_editor: flag only editable as unremovable
    Prior to this change, if a contenteditable=true element was a child of a
    contenteditable=false element, it was always flagged as unremovable=true.
    
    In fact what should happen is that this.editable should be unremovable, but one
    of its subchildren can be contenteditable=true while having a parent
    contenteditable=false and not be unremovable.
    
    Task-ID: 2794990

--- addons/web_editor/static/lib/odoo-editor/test/spec/unremovable.test.js
+++ addons/web_editor/static/lib/odoo-editor/test/spec/unremovable.test.js
@@ -7,6 +7,21 @@ import {
 
 describe('Unremovables', () => {
     describe('deleterange', () => {
+        it('should remove element which is contenteditable=true even if their parent is contenteditable=false', async () => {
+            await testEditor(BasicEditor, {
+                contentBefore: unformat(`
+                    <p>before[o</p>
+                    <div contenteditable="false">
+                        <div contenteditable="true"><p>intruder</p></div>
+                    </div>
+                    <p>o]after</p>`),
+                stepFunction: async editor => {
+                    await deleteBackward(editor);
+                },
+                contentAfter: unformat(`
+                    <p>before[]after</p>`),
+            });
+        });
         describe('historyUndo', () => {
             it('should be unchanged after a deleterange and a historyUndo', async () => {
                 await testEditor(BasicEditor, {
