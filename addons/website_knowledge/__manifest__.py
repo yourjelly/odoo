30ae42fc002041d3af05a756c6b62fcc1e1cd706
Julien Banken <jbn@odoo.com>
2022-04-06 22:13:55 +0200

    [IMP] website_knowledge: allow public users to access published articles

---
+++ addons/website_knowledge/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Knowledge website',
+    'summary': 'Publish your articles on the web',
+    'version': '0.1',
+    'depends': ['knowledge', 'website'],
+    'data': [
+        'views/knowledge_views.xml',
+        'views/knowledge_templates_frontend.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'license': 'LGPL-3',
+    'assets': {},
+}
