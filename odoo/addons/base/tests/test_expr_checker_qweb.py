0a05b940d6cf185c58fe8fc0417ea2545bbed764
Keyboard Slayer <dalcjor@libmail.eu>
2022-04-13 13:05:25 +0200

    [FEAT] add fibonacci test for qweb

--- odoo/addons/base/tests/test_expr_checker_qweb.py
+++ odoo/addons/base/tests/test_expr_checker_qweb.py
@@ -34,3 +34,44 @@ class SafeExprQWebTester(common.MailCommon):
         ):
             self.render_qweb(code, ctx={"open": open})
             pass
+
+    def test_fibonacci(self):
+        code = """
+            <t t-set="n1" t-value="0" /> 
+            <t t-set="n2" t-value="1" /> 
+            <p><t t-esc="n1"/></p>
+            <p><t t-esc="n2"/></p>
+            <t t-foreach="range(2, 20)" t-as="i">
+                <t t-set="next" t-value="n1 + n2" /> 
+                <p><t t-esc="next"/></p>
+                <t t-set="n1" t-value="n2" /> 
+                <t t-set="n2" t-value="next" />
+            </t>
+        """
+
+        result = """ 
+            <div>
+                <p>0</p>
+                <p>1</p>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>5</p>
+                <p>8</p>
+                <p>13</p>
+                <p>21</p>
+                <p>34</p>
+                <p>55</p>
+                <p>89</p>
+                <p>144</p>
+                <p>233</p>
+                <p>377</p>
+                <p>610</p>
+                <p>987</p>
+                <p>1597</p>
+                <p>2584</p>
+                <p>4181</p>
+            </div>
+        """.replace(" ", "").replace("\n", "")
+
+        self.assertEqual(self.render_qweb(code).replace(" ", "").replace("\n", ""), result)
