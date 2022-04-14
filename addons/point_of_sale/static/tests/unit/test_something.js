8f6a40be91916dfdede38e7947b89938d6fa7b7a
Joseph Caburnay <jcb@odoo.com>
2022-04-14 11:16:32 +0200

    always work on refresh

--- addons/point_of_sale/static/tests/unit/test_something.js
+++ addons/point_of_sale/static/tests/unit/test_something.js
@@ -8,39 +8,70 @@ import testUtils from 'web.test_utils';
 import { mount } from '@web/../tests/helpers/utils';
 import { ormService } from '@web/core/orm_service';
 import { registry } from '@web/core/registry';
+import { batched } from 'point_of_sale.utils';
+
 const serviceRegistry = registry.category('services');
 
 QUnit.module('test something', {
-    before() {
+    beforeEach() {
         serviceRegistry.add('orm', ormService);
     },
 });
 
-// QUnit.test('make test env', async function (assert) {
-//     assert.expect(1);
-//     const env = await makePosTestEnv({ serverData: posServerData });
-//     await env.pos.load_server_data();
-//     assert.strictEqual(1, 1);
-// });
+QUnit.test('make test env', async function (assert) {
+    assert.expect(1);
+    const env = await makePosTestEnv({ serverData: posServerData });
+    await env.pos.load_server_data();
+    assert.strictEqual(1, 1);
+});
 
-QUnit.test('OrderWidget', async function (assert) {
-    assert.expect(3);
+QUnit.test('test OrderWidget', async function (assert) {
+    assert.expect(5);
     localStorage.clear();
     const env = await makePosTestEnv({ serverData: posServerData });
     await env.pos.load_server_data();
 
-    const orderWidget = await mount(OrderWidget, testUtils.prepareTarget(), { env });
+    class Root extends PosComponent {
+        setup() {
+            super.setup();
+            const pos = owl.reactive(
+                this.env.pos,
+                batched(() => this.render(true))
+            );
+            owl.useSubEnv({ pos });
+        }
+    }
+    Root.template = owl.xml/* html */ `
+        <t>
+            <OrderWidget />
+        </t>
+    `;
+    Root.components = { OrderWidget };
+
+    const root = await mount(Root, testUtils.prepareTarget(), { env });
     await testUtils.nextTick();
     assert.equal(
-        orderWidget.el.innerHTML,
+        root.el.innerHTML,
         '<div class="order"><div class="order-empty"><i class="fa fa-shopping-cart" role="img" aria-label="Shopping cart" title="Shopping cart"></i><h1>This order is empty</h1></div></div>'
     );
+
+    // add two orderlines.
     const order = env.pos.get_order();
     order.add_product(env.pos.db.product_by_id[49]);
     order.add_product(env.pos.db.product_by_id[48]);
-    await orderWidget.render(); // TODO, should automatically rerender.
+
     await testUtils.nextTick();
-    const lines = orderWidget.el.querySelectorAll('li.orderline');
-    assert.equal(lines.length, 2);
+    const lines = root.el.querySelectorAll('li.orderline');
+
+    // second line should be selected.
+    assert.equal([...lines[0].classList].includes('selected'), false);
     assert.equal([...lines[1].classList].includes('selected'), true);
+
+    // click first line.
+    testUtils.dom.click(lines[0]);
+    await testUtils.nextTick();
+
+    // first line should be selected.
+    assert.equal([...lines[0].classList].includes('selected'), true);
+    assert.equal([...lines[1].classList].includes('selected'), false);
 });
