73d69c62c0676e94670891dc56486125d5088d22
Arthur Detroux (ard) <ard@odoo.com>
2022-04-21 15:09:50 +0200

    [ADD] website, web_editor: Add WebsiteEditor components
    This commit adds 2 components essential to edit a website.
    
    - WysiwygAdapterComponent: Allows the legacy @web_editor/wysiwyg widget
        to work in an OWL context. It will be further expanded in the next
        commit to fully replace the entire frontend edition system.
    
    - WebsiteEditorComponent: This component is in between the Wysiwyg and
        the adapter. It allows some trick such as closing the wysiyg during
        a reload while keeping the edition going.
    
    The reason this is split in two component is because, for now, when
    the page needs reloading, the editor component will clone the content
    of the widget, quit the Wysiwyg, and restart it. This ensure that every
    event is properly bound and gives the illusion that the wysiwyg is
    never left.

--- addons/website/static/src/services/website_service.js
+++ addons/website/static/src/services/website_service.js
@@ -1,6 +1,9 @@
 /** @odoo-module **/
 
 import { registry } from '@web/core/registry';
+import core from 'web.core';
+import ajax from 'web.ajax';
+import { getWysiwygClass } from 'web_editor.loader';
 
 const { reactive } = owl;
 
@@ -25,10 +28,15 @@ export const websiteService = {
         let currentMetadata = {};
         const context = reactive({
             showNewContentModal: false,
+            edition: false,
+            isPublicRootReady: false,
+            snippetsLoaded: false,
         });
         let pageDocument;
         let contentWindow;
         let editedObjectPath;
+        let websiteRootInstance;
+        let Wysiwyg;
         return {
             set currentWebsiteId(id) {
                 currentWebsiteId = id;
@@ -70,13 +78,23 @@ export const websiteService = {
             get contentWindow() {
                 return contentWindow;
             },
+            get websiteRootInstance() {
+                return websiteRootInstance;
+            },
+            set websiteRootInstance(rootInstance) {
+                websiteRootInstance = rootInstance;
+                context.isPublicRootReady = !!rootInstance;
+            },
             set editedObjectPath(path) {
                 editedObjectPath = path;
             },
             get editedObjectPath() {
                 return editedObjectPath;
             },
-            goToWebsite({ websiteId = currentWebsiteId || websites[0].id, path = '/' } = {}) {
+            get wysiwygLoaded() {
+                return !!Wysiwyg;
+            },
+            goToWebsite({ websiteId, path } = {}) {
                 action.doAction('website.website_editor', {
                     clearBreadcrumbs: true,
                     additionalContext: {
@@ -95,6 +113,13 @@ export const websiteService = {
                 websites = [...allWebsites];
                 currentWebsiteId = unslugHtmlDataObject(currentWebsiteRepr).id;
             },
+            async loadWysiwyg() {
+                if (!Wysiwyg) {
+                    await ajax.loadXML('/website/static/src/xml/website.editor.xml', core.qweb);
+                    Wysiwyg = await getWysiwygClass({wysiwygAlias: 'website.wysiwyg'}, ['website.compiled_assets_wysiwyg']);
+                }
+                return Wysiwyg;
+            }
         };
     },
 };
