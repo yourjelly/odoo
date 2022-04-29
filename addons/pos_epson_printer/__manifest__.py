a27da5c5c73f51495b320eafa58144c7db8282e4
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2022-04-29 10:06:17 +0000

    [FIX] account, sale: heterogeneous tax mapping
    Have the taxes:
    - [Ftax] any% included in price
    - [TAX1] 15% not included price
    - [TAX2] 15% included in price
    
    Apply [Ftax] and [TAX1] to a product having product price [PRI]
    Have a fiscal position mapping [TAX1] to [TAX2]
    Make a SO with the fiscal position, add in a line the product
    
    The unit price will not be [PRI] but will increment. This occur because
    when doing the mapping we apply the computation for incl->* mapping
    when just one tax is included in price
    
    opw-2797237
    
    closes odoo/odoo#90538
    
    X-original-commit: d0497acb483b0c99141be391919ed3de6053fcf3
    Signed-off-by: Laurent Smet <las@odoo.com>
    Signed-off-by: Grazioso Andrea (agr) <agr@odoo.com>

--- addons/pos_epson_printer/__manifest__.py
+++ addons/pos_epson_printer/__manifest__.py
@@ -17,6 +17,7 @@ Use Epson ePOS Printers without the IoT Box in the Point of Sale
         'views/point_of_sale_assets.xml',
         'views/pos_config_views.xml',
     ],
+    'qweb': ['static/src/xml/pos_epson.xml'],
     'installable': True,
     'auto_install': True,
     'license': 'LGPL-3',
