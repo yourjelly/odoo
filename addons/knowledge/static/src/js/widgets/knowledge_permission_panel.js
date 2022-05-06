65c0955ca2a2e41de87db121d1500f3e48fe34ac
Julien Banken <jbn@odoo.com>
2022-05-06 09:17:57 +0200

    [IMP] knowledge: remove listener when destroying the permission panel

--- addons/knowledge/static/src/js/widgets/knowledge_permission_panel.js
+++ addons/knowledge/static/src/js/widgets/knowledge_permission_panel.js
@@ -14,15 +14,26 @@ const PermissionPanelWidget = Widget.extend(WidgetAdapterMixin, {
         this._super.apply(this, arguments);
         this.options = options;
     },
+
     /**
      * @override
      */
     start: function () {
         this.component = new ComponentWrapper(this, PermissionPanel, this.options);
-        this.component.env.bus.on('reload_tree', this, () => {
-            this.trigger_up('reload_tree', {});
-        });
+        this.component.env.bus.on('reload_tree', this, this._onReloadTree);
         return this.component.mount(this.el);
+    },
+
+    /**
+     * @override
+     */
+    destroy: function () {
+        this.component.env.bus.off('reload_tree', this._onReloadTree);
+        return this._super(...arguments);
+    },
+
+    _onReloadTree: function () {
+        this.trigger_up('reload_tree', {});
     },
 });
 
