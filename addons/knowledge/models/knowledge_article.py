eff5dd0d3bd688245252b4414c4a42e317b7269a
Julien Banken <jbn@odoo.com>
2022-05-09 14:53:12 +0200

    [IMP] knowledge: "move to" rework

--- addons/knowledge/models/knowledge_article.py
+++ addons/knowledge/models/knowledge_article.py
@@ -1356,7 +1356,8 @@ class Article(models.Model):
                     ['name', '=ilike', '%%%s%%' % term],
                     ['id', 'not in', (self._get_descendants() + self).ids]
                    ],
-            fields=['id', 'icon', 'name'],
+            fields=['id', 'icon', 'name', 'category'],
+            limit=15,
         )
 
     def _get_descendants(self):
