cdd19452a131ac9173c4f70b40b0c6341198e502
Rémy Voet (ryv) <ryv@odoo.com>
2022-05-11 13:28:51 +0200

    [REM] fields.py: remove deadcode
    Integer selection doesn't exist since https://github.com/odoo/odoo/commit/a0e05e2ab9055fa060b1f3b5adf7aa322e2617fd

--- odoo/fields.py
+++ odoo/fields.py
@@ -2615,8 +2615,6 @@ class Selection(Field):
     def convert_to_cache(self, value, record, validate=True):
         if not validate:
             return value or None
-        if value and self.column_type[0] == 'int4':
-            value = int(value)
         if value in self.get_values(record.env):
             return value
         elif not value:
