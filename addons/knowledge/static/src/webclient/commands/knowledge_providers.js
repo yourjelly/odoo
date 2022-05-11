cffdb7b1be33340ccc3eb69d5b49f29900766abd
Fabio Barbero <faba@odoo.com>
2022-05-11 15:29:23 +0200

    [IMP] knowledge: feedback from lna

--- addons/knowledge/static/src/webclient/commands/knowledge_providers.js
+++ addons/knowledge/static/src/webclient/commands/knowledge_providers.js
@@ -11,46 +11,46 @@ const { Component, xml } = owl;
 // Articles command
 class KnowledgeCommand extends Component {}
 KnowledgeCommand.template = xml`
-    <div class="o_command_left d-flex align-items-center">
-        <span class="pr-2" t-if="props.icon_string" t-out="props.icon_string"/>
-        <span class="pr-2" t-else="">📄</span>
-        <span class="pr-2"><t t-slot="name"/></span>
-        <icon t-if="props.isFavorite" class="fa fa-star o_favorite pr-2 position-static"/>
-        <span t-if="props.subjectName" t-out="'— '" class="text-muted small pr-2" />
-        <span t-if="props.subjectName" class="o_command_name text-muted small">
-            <t t-foreach="props.splitSubjectName" t-as="name" t-key="name_index">
-                <b t-if="name_index % 2" t-out="name"/>
-                <t t-else="" t-out="name"/>
-            </t>
-        </span>
-    </div>
+<div class="o_command_left d-flex align-items-center">
+<span class="pr-2" t-if="props.icon_string" t-out="props.icon_string"/>
+<span class="pr-2" t-else="">📄</span>
+<span class="pr-2 test-ellipsis"><t t-slot="name"/></span>
+<icon t-if="props.isFavorite" class="fa fa-star o_favorite pr-2 position-static"/>
+<span t-if="props.subjectName" t-out="'— '" class="text-muted small pr-2" />
+<span t-if="props.subjectName" class="o_command_name test-ellipsis text-muted small">
+<t t-foreach="props.splitSubjectName" t-as="name" t-key="name_index">
+<b t-if="name_index % 2" t-out="name"/>
+<t t-else="" t-out="name"/>
+</t>
+</span>
+</div>
 `;
 
 // "Not found, create one" command
 class Knowledge404Command extends Component {}
 Knowledge404Command.template = xml`
-    <div class="o_command_hotkey">
-        <span>
-            No Article found. <span class="text-primary">Create "<u t-out="props.articleName"/>"</span>
-        </span>
-    </div>
+<div class="o_command_hotkey">
+<span>
+No Article found. <span class="text-primary">Create "<u t-out="props.articleName"/>"</span>
+</span>
+</div>
 `;
 
 // Advanced search command
 class KnowledgeExtraCommand extends HotkeyCommandItem {}
 KnowledgeExtraCommand.template = xml`
-    <div class="o_command_hotkey">
-        <span>
-            <icon class="fa fa-arrows-alt pr-2" />
-            <t t-out="props.name" />
-        </span>
-        <span>
-            <t t-foreach="getKeysToPress(props)" t-as="key" t-key="key_index">
-                <kbd t-out="key" />
-                <span t-if="!key_last"> + </span>
-            </t>
-        </span>
-    </div>
+<div class="o_command_hotkey">
+<span>
+<icon class="fa fa-arrows-alt pr-2" />
+<t t-out="props.name" />
+</span>
+<span>
+<t t-foreach="getKeysToPress(props)" t-as="key" t-key="key_index">
+<kbd t-out="key" />
+<span t-if="!key_last"> + </span>
+</t>
+</span>
+</div>
 `;
 
 const commandSetupRegistry = registry.category("command_setup");
@@ -92,9 +92,8 @@ commandProviderRegistry.add("knowledge", {
                         const articleId = await Component.env.services.rpc({
                             model: 'knowledge.article',
                             method: 'article_create',
-                            args: [[]],
+                            args: [[options.searchValue]],
                             kwargs: {
-                                title: options.searchValue,
                                 is_private: true
                             },
                         });
