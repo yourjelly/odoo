0a3aa01f9124ee636cc2904cad9d8a083a718bdd
David Beguin <dbe@odoo.com>
2022-05-11 11:35:11 +0200

    update query counter - with tests

--- addons/knowledge/tests/test_knowledge_performance.py
+++ addons/knowledge/tests/test_knowledge_performance.py
@@ -72,14 +72,14 @@ class KnowledgePerformanceCase(KnowledgeCommonWData):
     @users('employee')
     @warmup
     def test_article_favorite(self):
-        with self.assertQueryCount(employee=8):  # knowledge only: 7
+        with self.assertQueryCount(employee=9):  # knowledge only: 7
             article = self.shared_children.with_env(self.env)
             article.action_toggle_favorite()
 
     @users('employee')
     @warmup
     def test_article_invite_members(self):
-        with self.assertQueryCount(employee=109):
+        with self.assertQueryCount(employee=113):
             article = self.shared_children.with_env(self.env)
             partners = (self.customer + self.partner_employee_manager + self.partner_employee2).with_env(self.env)
             article.invite_members(partners, 'write')
@@ -88,6 +88,6 @@ class KnowledgePerformanceCase(KnowledgeCommonWData):
     @warmup
     def test_article_move_to(self):
         before_id = self.workspace_children[0].id
-        with self.assertQueryCount(employee=19):
+        with self.assertQueryCount(employee=25):
             article = self.workspace_children[1].with_env(self.env)
             article.move_to(parent_id=article.parent_id.id, before_article_id=before_id)
