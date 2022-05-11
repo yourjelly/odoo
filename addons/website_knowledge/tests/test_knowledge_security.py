de85753f069f0304148736652fc4155663c94577
David Beguin <dbe@odoo.com>
2022-05-11 20:05:11 +0200

    add test for public users - access to articles when website_published

--- addons/website_knowledge/tests/test_knowledge_security.py
+++ addons/website_knowledge/tests/test_knowledge_security.py
@@ -11,6 +11,22 @@ from odoo.tools import mute_logger
 class TestWKnowledgeSecurity(KnowledgeArticlePermissionsCase):
 
     @mute_logger('odoo.addons.base.models.ir_model', 'odoo.addons.base.models.ir_rule')
+    @users('user_public')
+    def test_models_as_public(self):
+        """ Test publish flag giving read access to articles """
+        article_shared = self.article_roots[2].with_env(self.env)
+        with self.assertRaises(exceptions.AccessError,
+                               msg="ACLs: Internal permission 'none', not for public"):
+            article_shared.body  # access body should trigger acls
+
+        article_shared.sudo().website_published = True
+        article_shared.body  # access body should not trigger acls
+
+        # FAVOURITE
+        with self.assertRaises(exceptions.AccessError, msg='ACLs: No favorite access to public'):
+            article_shared.is_user_favorite
+
+    @mute_logger('odoo.addons.base.models.ir_model', 'odoo.addons.base.models.ir_rule')
     @users('portal_test')
     def test_models_as_portal(self):
         """ Test publish flag giving read access to articles """
