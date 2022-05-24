bb1e2a482906c5f3c69cc15c3dd7e4eca4410bb0
Bruno Boi <boi@odoo.com>
2022-05-24 16:20:21 +0200

    fix checkbox api using new hotkey option + adapt boolean fields

--- addons/web/static/src/fields/boolean_field.js
+++ addons/web/static/src/fields/boolean_field.js
@@ -5,39 +5,14 @@ import { _lt } from "@web/core/l10n/translation";
 import { standardFieldProps } from "./standard_field_props";
 import { CheckBox } from "@web/core/checkbox/checkbox";
 
-const { Component, useExternalListener, useRef } = owl;
+const { Component } = owl;
 
 export class BooleanField extends Component {
-    setup() {
-        this.root = useRef("root");
-        if (this.props.isReadonlyEnabled) {
-            useExternalListener(window, "click", (ev) => this.onClick(ev), { capture: true });
-        }
-    }
     /**
      * @param {boolean} newValue
      */
     onChange(newValue) {
         this.props.update(newValue);
-    }
-    /**
-     * @param {Event} ev
-     */
-    onClick(ev) {
-        if (ev.composedPath().includes(this.root.el)) {
-            this.props.update(!this.props.value);
-            ev.preventDefault();
-        }
-    }
-    /**
-     * @param {{ key: string }} params
-     */
-    onKeydown({ key }) {
-        switch (key) {
-            case "Enter":
-                this.props.update(!this.props.value);
-                break;
-        }
     }
 }
 
