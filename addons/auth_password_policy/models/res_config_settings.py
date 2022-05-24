50d6dc323a8cc65bb1cb115a59f8a4f4612e5d6f
Martin Trigaux <mat@odoo.com>
2022-05-24 17:33:34 +0200

    test haveibeenpwnd

--- addons/auth_password_policy/models/res_config_settings.py
+++ addons/auth_password_policy/models/res_config_settings.py
@@ -8,6 +8,10 @@ class ResConfigSettings(models.TransientModel):
         "Minimum Password Length", config_parameter="auth_password_policy.minlength", default=0,
         help="Minimum number of characters passwords must contain, set to 0 to disable.")
 
+    haveibeenpwned = fields.Boolean(
+        "Verify if Password Vulnerable", config_parameter="auth_password_policy.haveibeenpwned",
+        help="Verify the password on a database of leaked password and warn the user.")
+
     @api.onchange('minlength')
     def _on_change_mins(self):
         """ Password lower bounds must be naturals
--- addons/auth_password_policy/models/res_users.py
+++ addons/auth_password_policy/models/res_users.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from odoo import api, models, _
 from odoo.exceptions import UserError
+from odoo.tools import consteq
+
+import logging
+from hashlib import sha1
+import requests
+
+_logger = logging.getLogger(__name__)
 
 
 class ResUsers(models.Model):
@@ -18,16 +25,32 @@ class ResUsers(models.Model):
 
         super(ResUsers, self)._set_password()
 
+    def _verify_password_haveibeenpwned(self, password):
+        hashdigest = sha1(password.encode()).hexdigest()
+        prefix, suffix = hashdigest[:5], hashdigest[5:].upper()
+        req = requests.get(f"https://api.pwnedpasswords.com/range/{prefix}", timeout=2)
+        if req.status_code == 429:
+            res = req.json()
+            _logger.error("Could not reach pwnedpasswords.com, %s", res.get("message", "API rate limit reached"))
+        for line in req.text.split("\r\n"):
+            if consteq(line.split(":")[0], suffix):
+                return False
+        return True
+
     def _check_password_policy(self, passwords):
         failures = []
         params = self.env['ir.config_parameter'].sudo()
 
         minlength = int(params.get_param('auth_password_policy.minlength', default=0))
+        haveibeenpwned = bool(params.get_param('auth_password_policy.haveibeenpwned', default=False))
         for password in passwords:
             if not password:
                 continue
             if len(password) < minlength:
                 failures.append(_(u"Passwords must have at least %d characters, got %d.") % (minlength, len(password)))
+            if haveibeenpwned:
+                if not self._verify_password_haveibeenpwned(password):
+                    failures.append(_("Password appears in a data breach, choose another one."))
 
         if failures:
             raise UserError(u'\n\n '.join(failures))
