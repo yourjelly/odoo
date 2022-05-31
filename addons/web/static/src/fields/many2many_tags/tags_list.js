7b7f45ac97eacb2e081598ba57e740a5cd2c5090
luvi <luvi@odoo.com>
2022-05-31 19:54:36 +0200

    move TagsList component back to m2m_tags folder + simplify it
    There is no need to keep this component as a "core" component
    as it is used mainly for m2m_tags(_avatar) fields

--- addons/web/static/src/fields/many2many_tags/tags_list.js
+++ addons/web/static/src/fields/many2many_tags/tags_list.js
@@ -1,26 +1,22 @@
 /** @odoo-module **/
-import { TagItem } from "./tag_item";
 
 const { Component } = owl;
 
 export class TagsList extends Component {
     get visibleTags() {
-        if (this.props.visibleTags && this.props.tags.length > this.props.visibleTags) {
-            return this.props.tags.slice(0, this.props.visibleTags - 1);
+        if (this.props.itemsVisible && this.props.tags.length > this.props.itemsVisible) {
+            return this.props.tags.slice(0, this.props.itemsVisible - 1);
         }
         return this.props.tags;
     }
     get otherTags() {
-        if (!this.props.visibleTags || this.props.tags.length <= this.props.visibleTags) return;
-        return this.props.tags.slice(this.props.visibleTags - 1);
+        if (!this.props.itemsVisible || this.props.tags.length <= this.props.itemsVisible) return;
+        return this.props.tags.slice(this.props.itemsVisible - 1);
     }
     get tooltip() {
         return this.otherTags.map((i) => i.text).join("<br/>");
     }
 }
-TagsList.components = {
-    TagItem,
-};
 TagsList.template = "web.TagsList";
 TagsList.defaultProps = {
     className: "",
@@ -28,7 +24,7 @@ TagsList.defaultProps = {
 TagsList.props = {
     className: { type: String, optional: true },
     name: { type: String, optional: true },
-    visibleTags: { type: Number, optional: true },
+    itemsVisible: { type: Number, optional: true },
     slots: { type: Object, optional: true },
     tags: { type: Object, optional: true },
 };
