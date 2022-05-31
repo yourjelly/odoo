235e7f51d547334f9e191cab3c5889088994199c
Aaron Bohy <aab@odoo.com>
2022-05-31 12:01:23 +0200

    enable chatter

--- addons/mail/static/src/widgets/form_renderer/wowl_form_renderer.js
+++ addons/mail/static/src/widgets/form_renderer/wowl_form_renderer.js
@@ -3,20 +3,29 @@
 import { registry } from "@web/core/registry";
 import { FormRenderer } from "@web/views/form/form_renderer";
 import { getMessagingComponent } from "@mail/utils/messaging_component";
+import "@mail/components/chatter_container/chatter_container";
+import {
+    createElement,
+} from "@web/core/utils/xml";
+import { append } from "@web/views/helpers/view_compiler";
 
 const ChatterContainer = getMessagingComponent("ChatterContainer");
 
 function compileChatter(node, params) {
     node.classList.remove("oe_chatter");
-    const container = this.document.createElement("div");
+    const container = createElement("div");
     container.classList.add("o_FormRenderer_chatterContainer");
 
-    const chatter = this.document.createElement("ChatterContainer");
+    const chatter = createElement("ChatterContainer");
     chatter.setAttribute("threadModel", "props.record.resModel");
     chatter.setAttribute("threadId", "props.record.resId");
     // TODO: pass chatterFields equivalent in props
 
-    this.append(container, chatter);
+    // chatter.setAttribute("record", "props.record"); // props.record.model.load() to reload the form
+    // in ChatterContainer setup:
+    //   this.messagingService = useService("messaging");
+
+    append(container, chatter);
     return container;
 }
 
@@ -26,16 +35,4 @@ registry.category("form_compilers").add("chatter_compiler", {
     fn: compileChatter,
 });
 
-class ChatterContainerLegacy extends owl.Component {
-    setup() {
-        this.env = owl.Component.env;
-    }
-}
-ChatterContainerLegacy.template = owl.xml`
-    <t>
-        <ChatterContainer t-if="props.resId" t-props="props"/>
-        <div t-else=""/>
-    </t>`;
-ChatterContainerLegacy.components = { ChatterContainer };
-
-FormRenderer.components.ChatterContainer = ChatterContainerLegacy;
+FormRenderer.components.ChatterContainer = ChatterContainer;
