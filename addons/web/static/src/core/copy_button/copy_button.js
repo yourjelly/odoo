7700bdd366ca43b40535ae9d16d49293ab7c33a7
luvi <luvi@odoo.com>
2022-05-31 12:00:43 +0200

    improve CopyButton component
    This component is now using the native Clipboard API and
    should be able to support any kind of content to copy

--- addons/web/static/src/core/copy_button/copy_button.js
+++ addons/web/static/src/core/copy_button/copy_button.js
@@ -1,29 +1,40 @@
 /** @odoo-module **/
 
-const { Component, useEffect, useRef, useState } = owl;
+const { Component, useEffect, useState } = owl;
 
 export class CopyButton extends Component {
     setup() {
-        this.copyRef = useRef("copyBtn");
         this.state = useState({
             isCopied: false,
         });
         useEffect(() => {
-            if (!this.copyRef.el) return;
-            this.clipboard = new ClipboardJS(this.copyRef.el);
-            this.clipboard.on("success", () => {
-                this.state.isCopied = true;
-                this.timeoutId = setTimeout(() => (this.state.isCopied = false), 800);
-            });
             return () => clearTimeout(this.timeoutId), () => [this.state.isCopied];
         });
     }
+
     get isInline() {
         return ["char", "url"].includes(this.props.type);
+    }
+
+    async onClick() {
+        // any kind of content can be copied into the clipboard using
+        // the appropriate native methods
+        if (typeof this.props.content === "string") {
+            navigator.clipboard.writeText(this.props.content).then(() => {
+                this.state.isCopied = true;
+            });
+        } else {
+            navigator.clipboard.write(this.props.content).then(() => {
+                this.state.isCopied = true;
+            });
+        }
+        this.timeoutId = setTimeout(() => (this.state.isCopied = false), 800);
     }
 }
 CopyButton.template = "web.CopyButton";
 CopyButton.props = {
     className: { type: String, optional: true },
-    value: String,
+    copyText: { type: String, optional: true },
+    successText: { type: String, optional: true },
+    content: {},
 };
--- addons/web/static/src/fields/copy_clipboard_field.js
+++ addons/web/static/src/fields/copy_clipboard_field.js
@@ -11,37 +11,36 @@ import { TextField } from "./text_field";
 
 const { Component } = owl;
 
-export class CopyClipboardCharField extends Component {}
-
-CopyClipboardCharField.components = { Field: CharField, CopyButton };
-CopyClipboardCharField.props = {
+class CopyClipboardField extends Component {
+    setup() {
+        this.copyText = this.env._t("Copy");
+        this.successText = this.env._t("Copied");
+    }
+}
+CopyClipboardField.template = "web.CopyClipboardField";
+CopyClipboardField.props = {
     ...standardFieldProps,
 };
-CopyClipboardCharField.template = "web.CopyClipboardField";
+
+export class CopyClipboardCharField extends CopyClipboardField {}
+
+CopyClipboardCharField.components = { Field: CharField, CopyButton };
 CopyClipboardCharField.displayName = _lt("Copy Text to Clipboard");
 CopyClipboardCharField.supportedTypes = ["char"];
 
 registry.category("fields").add("CopyClipboardChar", CopyClipboardCharField);
 
-export class CopyClipboardTextField extends Component {}
+export class CopyClipboardTextField extends CopyClipboardField {}
 
 CopyClipboardTextField.components = { Field: TextField, CopyButton };
-CopyClipboardTextField.props = {
-    ...standardFieldProps,
-};
-CopyClipboardTextField.template = "web.CopyClipboardField";
 CopyClipboardTextField.displayName = _lt("Copy Multiline Text to Clipboard");
 CopyClipboardTextField.supportedTypes = ["text"];
 
 registry.category("fields").add("CopyClipboardText", CopyClipboardTextField);
 
-export class CopyClipboardURLField extends Component {}
+export class CopyClipboardURLField extends CopyClipboardField {}
 
 CopyClipboardURLField.components = { Field: UrlField, CopyButton };
-CopyClipboardURLField.props = {
-    ...standardFieldProps,
-};
-CopyClipboardURLField.template = "web.CopyClipboardField";
 CopyClipboardURLField.displayName = _lt("Copy URL to Clipboard");
 CopyClipboardURLField.supportedTypes = ["char"];
 
