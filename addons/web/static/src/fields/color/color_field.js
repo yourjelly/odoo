d439134f4a5a7eb8fd2cdeaf9e131a8bcfb70574
luvi <luvi@odoo.com>
2022-06-01 10:42:04 +0200

    refactor ColorField with native color input

--- addons/web/static/src/fields/color/color_field.js
+++ addons/web/static/src/fields/color/color_field.js
@@ -1,26 +1,14 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { useService } from "@web/core/utils/hooks";
 import { standardFieldProps } from "../standard_field_props";
 
-import { ColorPickerDialog } from "@web/core/colorpicker/colorpicker_dialog";
-
-const { Component } = owl;
+const { Component, useState } = owl;
 
 export class ColorField extends Component {
     setup() {
-        this.dialogService = useService("dialog");
-    }
-
-    onClick() {
-        if (this.props.readonly) return;
-        const self = this;
-        this.dialogService.add(ColorPickerDialog, {
-            onColorSelected(hex) {
-                self.props.update(hex);
-            },
-            color: self.props.value || "#ffffff",
+        this.state = useState({
+            color: this.props.value || "#000000",
         });
     }
 }
