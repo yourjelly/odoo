88f914ff4e223b85ae7e48756c56e85384c7c4bc
Aaron Bohy <aab@odoo.com>
2022-06-01 08:35:20 +0200

    ref utils ui

--- addons/web/static/src/core/utils/ui.js
+++ addons/web/static/src/core/utils/ui.js
@@ -4,6 +4,26 @@ import { clamp } from "@web/core/utils/numbers";
 import { debounce } from "@web/core/utils/timing";
 
 /**
+ * @param {Document} activeElement
+ * @param {DOMString} selector
+ * @returns all selected and visible elements present in the activeElement
+ */
+export function getVisibleElements(activeElement, selector) {
+    const visibleElements = [];
+    for (const el of activeElement.querySelectorAll(selector)) {
+        const isVisible = el.offsetWidth > 0 && el.offsetHeight > 0;
+        if (isVisible) {
+            visibleElements.push(el);
+        }
+    }
+    return visibleElements;
+}
+
+// -----------------------------------------------------------------------------
+// useSortable
+// -----------------------------------------------------------------------------
+
+/**
  * @typedef SortableParams
  *
  * MANDATORY
@@ -103,22 +123,6 @@ function computeParams(params) {
  */
 function cssValueToNumber(val) {
     return Number(val.slice(0, -2));
-}
-
-/**
- * @param {Document} activeElement
- * @param {DOMString} selector
- * @returns all selected and visible elements present in the activeElement
- */
-export function getVisibleElements(activeElement, selector) {
-    const visibleElements = [];
-    for (const el of activeElement.querySelectorAll(selector)) {
-        const isVisible = el.offsetWidth > 0 && el.offsetHeight > 0;
-        if (isVisible) {
-            visibleElements.push(el);
-        }
-    }
-    return visibleElements;
 }
 
 /**
