786d6d50401af16115d8eb59018cff54cd55b278
Arthur Detroux (ard) <ard@odoo.com>
2022-06-07 15:12:08 +0200

    [IMP] website, web_editor: improve startup time of website editor.
    By starting the get_switchable_related_views request when the
    WysiwygAdapter is started, and awaiting it only when we need to know,
    we can improve the initial loading time of the editor.
    
    task-2687506

--- addons/website/static/src/services/website_service.js
+++ addons/website/static/src/services/website_service.js
@@ -32,6 +32,7 @@ export const websiteService = {
         let websiteRootInstance;
         let Wysiwyg;
         let isPublisher;
+        let isDesigner;
         let hasMultiWebsites;
         const context = reactive({
             showNewContentModal: false,
@@ -110,6 +111,9 @@ export const websiteService = {
             get isPublisher() {
                 return isPublisher === true;
             },
+            get isDesigner() {
+                return isDesigner === true;
+            },
             get hasMultiWebsites() {
                 return hasMultiWebsites === true;
             },
@@ -131,8 +135,9 @@ export const websiteService = {
             },
             async fetchWebsites() {
                 // Fetch user groups, before fetching the websites.
-                [isPublisher, hasMultiWebsites] = await Promise.all([
+                [isPublisher, isDesigner, hasMultiWebsites] = await Promise.all([
                     user.hasGroup('website.group_website_publisher'),
+                    user.hasGroup('website.group_website_designer'),
                     user.hasGroup('website.group_multi_website'),
                 ]);
 
