0ca8740bc35eba371dd3a676d4589fbba2f82dc1
Aaron Bohy <aab@odoo.com>
2022-06-10 08:29:02 +0200

    adapt set defaults debug item + make debug items work in legacy

--- addons/mail/static/src/js/tools/debug_manager.js
+++ addons/mail/static/src/js/tools/debug_manager.js
@@ -2,7 +2,7 @@
 
 import { registry } from "@web/core/registry";
 
-export function manageMessages({ action, component, env }) {
+export function manageMessages({ component, env }) {
     const resId = component.model.root.resId;
     if (!resId) {
         return null; // No record
@@ -35,3 +35,37 @@ export function manageMessages({ action, component, env }) {
 }
 
 registry.category("debug").category("form").add("mail.manageMessages", manageMessages);
+
+// Legacy form views compatibility: remove as soon as we no longer support legacy views
+function legacyManageMessages({ action, component, env }) {
+    const selectedIds = component.widget.getSelectedIds();
+    if (!selectedIds.length) {
+        return null; // No record
+    }
+    const description = env._t("Manage Messages");
+    return {
+        type: "item",
+        description,
+        callback: () => {
+            env.services.action.doAction({
+                res_model: "mail.message",
+                name: description,
+                views: [
+                    [false, "list"],
+                    [false, "form"],
+                ],
+                type: "ir.actions.act_window",
+                domain: [
+                    ["res_id", "=", selectedIds[0]],
+                    ["model", "=", action.res_model],
+                ],
+                context: {
+                    default_res_model: action.res_model,
+                    default_res_id: selectedIds[0],
+                },
+            });
+        },
+        sequence: 325,
+    };
+}
+registry.category("debug").category("form_legacy").add("mail.manageMessages", legacyManageMessages);
