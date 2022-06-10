84179bf6e53e3e03c1721b3dafc110961a323131
Aaron Bohy <aab@odoo.com>
2022-06-10 13:32:36 +0200

    [FIX] web: kanban: properly handle class attribute on field

--- addons/web/static/src/views/kanban/kanban_compiler.js
+++ addons/web/static/src/views/kanban/kanban_compiler.js
@@ -9,19 +9,19 @@ export class KanbanCompiler extends ViewCompiler {
      */
     compileField(el, params) {
         const compiled = super.compileField(el, params);
-        const classAttr = compiled.getAttribute("class") || "";
-        const classSet = new Set();
         const { bold, display } = extractAttributes(el, ["bold", "display"]);
+        const classNames = [];
         if (display === "right") {
-            classSet.add("float-right");
+            classNames.push("float-right");
         } else if (display === "full") {
-            classSet.add("o_text_block");
+            classNames.push("o_text_block");
         }
         if (bold) {
-            classSet.add("o_text_bold");
+            classNames.push("o_text_bold");
         }
-        const classAttrs = [classAttr, `'${[...classSet].join(" ")}'`].filter(Boolean);
-        compiled.setAttribute("class", classAttrs.join("+"));
+        if (classNames.length > 0) {
+            compiled.setAttribute("class", `'${classNames.join(" ")}'`);
+        }
         return compiled;
     }
 
