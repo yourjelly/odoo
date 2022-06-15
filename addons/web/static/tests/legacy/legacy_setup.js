1fd3668731fb09d675e8e28a9be91569083b2079
Aaron Bohy <aab@odoo.com>
2022-06-15 09:09:21 +0200

    fix lint

--- addons/web/static/tests/legacy/legacy_setup.js
+++ addons/web/static/tests/legacy/legacy_setup.js
@@ -1,6 +1,4 @@
 /** @odoo-module alias=web.legacySetup **/
-import { registry } from "@web/core/registry";
-import legacyViewRegistry from "web.view_registry";
 
 // in tests, there's nothing to setup globally (we don't want to deploy services),
 // but this module must exist has it is required by other modules
