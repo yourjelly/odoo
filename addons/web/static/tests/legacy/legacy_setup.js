899e6b9fe6447d22bf7f5cd474b6bfe8e8987fd0
Aaron Bohy <aab@odoo.com>
2022-06-15 08:27:42 +0200

    remove useLegacyViews

--- addons/web/static/tests/legacy/legacy_setup.js
+++ addons/web/static/tests/legacy/legacy_setup.js
@@ -5,10 +5,3 @@ import legacyViewRegistry from "web.view_registry";
 // in tests, there's nothing to setup globally (we don't want to deploy services),
 // but this module must exist has it is required by other modules
 export const legacySetupProm = Promise.resolve();
-
-export function useLegacyViews(views = ["list", "form"]) {
-  for (const vname of views) {
-    registry.category("views").remove(vname); // remove new view from registry
-    legacyViewRegistry.add(vname, legacyViewRegistry.get(vname)); // add legacy view -> will be wrapped and added to new registry
-  }
-}
