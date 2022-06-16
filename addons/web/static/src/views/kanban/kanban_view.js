9d0fa31b126e65d2ec3c46afbcef650076a2db7f
Mathieu Duckerts-Antoine <dam@odoo.com>
2022-06-16 10:51:08 +0200

    some ref of X2ManyField and kanban view

--- addons/web/static/src/views/kanban/kanban_view.js
+++ addons/web/static/src/views/kanban/kanban_view.js
@@ -1,26 +1,29 @@
 /** @odoo-module **/
 
-import { registry } from "@web/core/registry";
 import { KanbanArchParser } from "./kanban_arch_parser";
 import { KanbanController } from "./kanban_controller";
 import { KanbanModel } from "./kanban_model";
 import { KanbanRenderer } from "./kanban_renderer";
+import { registry } from "@web/core/registry";
 
 export const kanbanView = {
     type: "kanban",
+
     display_name: "Kanban",
     icon: "oi oi-view-kanban",
-    multiRecord: true,
     isMobileFriendly: true,
-    Controller: KanbanController,
-    Renderer: KanbanRenderer,
-    Model: KanbanModel,
+    multiRecord: true,
+
     ArchParser: KanbanArchParser,
+    Controller: KanbanController,
+    Model: KanbanModel,
+    Renderer: KanbanRenderer,
+
     buttonTemplate: "web.KanbanView.Buttons",
 
     props: (genericProps, view) => {
-        const { ArchParser } = view;
         const { arch, relatedModels, resModel } = genericProps;
+        const { ArchParser } = view;
         const archInfo = new ArchParser().parse(arch, relatedModels, resModel);
         const defaultGroupBy =
             genericProps.searchMenuTypes.includes("groupBy") && archInfo.defaultGroupBy;
