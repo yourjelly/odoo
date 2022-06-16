c8618e55a481500fb170276fd902ec02b48b77b4
Habib (ayh) <ayh@odoo.com>
2022-06-16 09:06:51 +0200

    add the position hook popup, try listrenderer, fail, diy table

--- addons/analytic/static/src/js/analytic_o2m/analytic_o2m.js
+++ addons/analytic/static/src/js/analytic_o2m/analytic_o2m.js
@@ -5,8 +5,11 @@ import { standardFieldProps } from "@web/fields/standard_field_props";
 import { AutoComplete } from "@web/core/autocomplete/autocomplete";
 import { TagsList } from "@web/fields/many2many_tags/tags_list";
 import { useService } from "@web/core/utils/hooks";
+import { usePosition } from "@web/core/position_hook";
+//import { ListArchParser } from "@web/views/list/list_arch_parser";
+//import { ListRenderer } from "@web/views/list/list_renderer";
 
-const { Component, useState } = owl;
+const { Component, useState, useRef } = owl;
 
 
 export class AnalyticO2M extends Component {
@@ -14,8 +17,21 @@ export class AnalyticO2M extends Component {
         this.orm = useService("orm");
         this.state = useState({
             autocompleteValue: "",
+            isOpened: false,
         });
+        this.widgetRef = useRef("analyticO2m");
+        usePosition(() => this.widgetRef.el, {
+            popper: "analyticPop",
+        });
+        //this.activeField = this.props.record.activeFields[this.props.name];
+        //this.fetchTreeArch();
     }
+
+    //failed attempt to load the arch manually - needs models - 
+    // async fetchTreeArch(){
+    //     let rawArch = await this.orm.call(this.activeField.relation, "get_view", [], {view_type: 'tree'});
+    //     this.treeArch = await new ListArchParser().parse(rawArch.arch);
+    // }
 
     get tags() {
         return this.props.value.records.map((record) => ({
@@ -119,16 +135,57 @@ export class AnalyticO2M extends Component {
     onInput({ inputValue }) {
         console.log('onInput');
         console.log(inputValue);
+        if (inputValue.length) {
+            this.onWidgetBlur();
+        } else { this. onWidgetFocus();}
     }
 
-    onSelect(option, params) {
+    async onSelect(option, params) {
         console.log('OnSelect');
         console.log(option);
         let selected_option = Object.getPrototypeOf(option)
         let changes = {};
         changes[selected_option.field_to_update] = [selected_option.value, selected_option.label];
-        this.props.record.update(changes);
+        await this.props.record.update(changes);
+        this.onWidgetFocus();
     }
+
+    onWidgetFocus() {
+        if (this.tags.length) {
+            this.state.isOpened = true;
+        }
+        console.log(this.props.value);
+    }
+
+    onWidgetBlur() {
+        if (!this.preventClosePopup) {
+            this.state.isOpened = false;
+        }
+    }
+
+    focusInput() {
+        this.preventClosePopup = true;
+    }
+
+    blurInput() {
+        this.preventClosePopup = false;
+    }
+
+    // get rendererProps() {
+    //     const archInfo = this.activeField.views[this.activeField.viewMode];
+
+    //     let columns = archInfo.columns;
+    //     const props = {
+    //         activeActions: [],
+    //         editable: !this.props.readonly,
+    //         archInfo: { ...archInfo, columns },
+    //         list: this.props.value,
+    //         openRecord: ()=>{},
+    //         onAdd: ()=>{}, //this.onAdd.bind(this),
+    //     };
+
+    //     return props;
+    // }
 }
 AnalyticO2M.template = "analytic_o2m";
 AnalyticO2M.supportedTypes = ["one2many"];
@@ -137,28 +194,22 @@ AnalyticO2M.props = {
     search_field: { type: String, optional: true },
     template_field: { type: String, optional: true },
 }
-AnalyticO2M.components = { AutoComplete, TagsList };
+AnalyticO2M.components = { 
+    AutoComplete,
+    TagsList,
+    // ListRenderer 
+};
 AnalyticO2M.fieldsToFetch = {
     display_name: { name: "display_name", type: "char" },
     percentage: { name: "percentage", type: "float" },
     color: { name: "color", type: "integer" },
+    analytic_account_id: { name: 'analytic_account_id', type: "many2one"}
 };
 
 AnalyticO2M.extractProps = (fieldName, record, attrs) => {
-    // const activeField = record.activeFields[fieldName];
     return {
         search_field: attrs.options.search_field || null,
         template_field: attrs.options.template_field || null,
-        // colorField: attrs.options.color_field,
-        // nameCreateField: attrs.options.create_name_field,
-        // canEditColor: !attrs.options.no_edit_color && activeField.viewType !== "list",
-        // relation: activeField.relation,
-        // domain: record.getFieldDomain(fieldName),
-        // context: record.getFieldContext(fieldName),
-        // canQuickCreate: !attrs.options.no_quick_create,
-        // createDomain: attrs.options.create,
-        // evalContext: record.evalContext,
-        // string: activeField.string,
     };
 };
 
