2cb5871fc8ff24ee0f767fe07c4bff22830a3b4f
PoMa <poma@odoo.com>
2022-06-16 14:24:34 +0430

    Revert "WIP savepoint"
    This reverts commit 24e2ba65c1ea84ba0485930961bd4c3e7c5ca5a0.

--- addons/point_of_sale/wizard/pos_box.py
+++ addons/point_of_sale/wizard/pos_box.py
@@ -1,61 +1,33 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models, _
+from odoo import api, _
 from odoo.exceptions import UserError
 
+from odoo.addons.account.wizard.pos_box import CashBox
 
-class PosBoxOut(models.TransientModel):
-    _name = 'cash.box.out'
-    _description = 'Cash Box Out'
+
+class PosBox(CashBox):
     _register = False
 
-    name = fields.Char(string='Reason', required=True)
-    amount = fields.Float(string='Amount', digits=0, required=True)
-
     def run(self):
-        context = dict(self._context or {})
-        active_model = context.get('active_model', False)
-        active_ids = context.get('active_ids', [])
-
-        records = self.env[active_model].browse(active_ids)
+        active_model = self.env.context.get('active_model', False)
+        active_ids = self.env.context.get('active_ids', [])
 
         if active_model == 'pos.session':
-            records = [session.cash_register_id for session in self.env[active_model].browse(active_ids) if
-                       session.cash_register_id]
-            if not records:
+            bank_statements = [session.cash_register_id for session in self.env[active_model].browse(active_ids) if session.cash_register_id]
+            if not bank_statements:
                 raise UserError(_("There is no cash register for this PoS Session"))
-        return self._run(records)
+            return self._run(bank_statements)
+        else:
+            return super(PosBox, self).run()
 
-    def _run(self, records):
-        for box in self:
-            for record in records:
-                if not record.journal_id:
-                    raise UserError(_("Please check that the field 'Journal' is set on the Bank Statement"))
-                if not record.journal_id.company_id.transfer_account_id:
-                    raise UserError(_("Please check that the field 'Transfer Account' is set on the company."))
-                box._create_bank_statement_line(record)
-        return {}
 
-    def _create_bank_statement_line(self, record):
-        for box in self:
-            if record.state == 'confirm':
-                raise UserError(_("You cannot put/take money in/out for a bank statement which is closed."))
-            values = box._calculate_values_for_statement_line(record)
-            self.env['account.bank.statement.line'].sudo().create(values)
+class PosBoxOut(PosBox):
+    _inherit = 'cash.box.out'
 
     def _calculate_values_for_statement_line(self, record):
-        if not record.journal_id.company_id.transfer_account_id:
-            raise UserError(_("You have to define an 'Internal Transfer Account' in your cash register's journal."))
-
-        amount = self.amount or 0.0
-        values = {
-            'date': record.date,
-            'statement_id': record.id,
-            'journal_id': record.journal_id.id,
-            'amount': amount,
-            'payment_ref': self.name,
-        }
+        values = super(PosBoxOut, self)._calculate_values_for_statement_line(record)
         active_model = self.env.context.get('active_model', False)
         active_ids = self.env.context.get('active_ids', [])
         if active_model == 'pos.session' and active_ids:
