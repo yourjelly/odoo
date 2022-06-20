bd907e395aa21bce5b9f75bebc28b978a1bc54c7
luvi <luvi@odoo.com>
2022-06-20 10:42:38 +0200

    [FIX] web: add class to a button with type="action"
    Before this commit, the action button with `a` html tag has no class set
    the component used for those button is the `ViewButton` and its
    `getClassNames` method only returns classNames if the tag is
    `button` only.
    
    This commit uses the classNames contained in the `props` to get the
    classes for those buttons.=

--- addons/web/static/tests/views/form/form_view_tests.js
+++ addons/web/static/tests/views/form/form_view_tests.js
@@ -7009,7 +7009,7 @@ QUnit.module("Views", (hooks) => {
     );
 
     QUnit.test("support anchor tags with action type", async function (assert) {
-        assert.expect(1);
+        assert.expect(2);
 
         const actionService = {
             start() {
@@ -7024,11 +7024,16 @@ QUnit.module("Views", (hooks) => {
             serverData,
             arch: `
                 <form>
-                    <a type="action" name="42"><i class="fa fa-arrow-right"/> Click me !</a>
+                    <a type="action" name="42" class="btn-primary"><i class="fa fa-arrow-right"/> Click me !</a>
                 </form>`,
             resId: 1,
         });
-        await click(target.querySelector('a[type="action"]'));
+        await click(target.querySelector("a[type='action']"));
+        assert.hasClass(
+            target.querySelector("a[type='action']"),
+            "btn-primary",
+            "classname is given if set on the <a> element"
+        );
     });
 
     QUnit.test(
