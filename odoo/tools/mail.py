485041222cc4287aa0b609f0831facab1346fefd
std-odoo <std@odoo.com>
2022-06-21 09:15:58 +0200

    [IMP] base: replace LXML HTML cleaner by bleach
    Purpose
    =======
    LXML has officially declared that the Cleaner is not appropriate for
    untrusted inputs. Because of that we want to refactor our HTML
    sanitizer.
    
    Changes
    =======
    Now, empty tag are automatically closed `<br>` => `<br />`
    
    If the content contains only whitespaces, they are all preserved.
    
    When sanitize_attributes is set to False, we use an extended list
    of safe attributes.
    
    The single quote and double quote are escaped when they are in the
    HTML element content. This is wanted as it can be used to trick the
    browser parser.
    
    The Bleach version we are using (4.1.0) automatically sorts the
    attributes. This will create some additional diff during our library
    change but there is nothing we can do to turn that off.
    (The future version of Bleach will apparently stop doing that).
    
    Support for XSS on old IE versions has been removed.
    
    Technical
    =========
    The Bleach sanitizer has an option to strip the not allowed elements.
    But it will just remove the element itself and not its children. Also,
    we want to have more control on what we do. E.G. we want to allow the
    "data:" protocol only for non-svg image and bleach just offer you to
    block the entire protocol or not.
    
    For those reason, we need to use directly `BleachSanitizerFilter` and
    to overwrite some method to have the behavior we want. Because of those
    overwrite, the sanitizer may not be forward-compatible and might be
    adapted for future bleach version.
    
    We have a list of "kill tags". When those tags are removed, we also
    remove all of their children.
    
    Performance
    ===========
    The bleach sanitizer is slower than the LXML sanitizer, the sanitization
    of the stackoverflow home page (~172 Kb) take 15ms with the LXML
    sanitizer and now take 70ms with bleach.
    
    The most of execution time is spent in the library bleach use, so
    there's nothing we can do to improve it significantly.
    
    Task-2812488
    
    Co-authored-by: Pierre-Yves Dufays <pydu@odoo.com>

--- odoo/tools/mail.py
+++ odoo/tools/mail.py
@@ -2,283 +2,26 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import base64
-import collections
+import html
 import logging
 import random
 import re
 import socket
-import threading
 import time
 from email.utils import getaddresses
 from urllib.parse import urlparse
 
 import idna
 import markupsafe
-from lxml import etree, html
-from lxml.html import clean
+from lxml import etree
 from werkzeug import urls
 
-import odoo
 from odoo.loglevels import ustr
 from odoo.tools import misc
 
+from .html_sanitize import html_sanitize
+
 _logger = logging.getLogger(__name__)
-
-#----------------------------------------------------------
-# HTML Sanitizer
-#----------------------------------------------------------
-
-safe_attrs = clean.defs.safe_attrs | frozenset(
-    ['style',
-     'data-o-mail-quote',  # quote detection
-     'data-oe-model', 'data-oe-id', 'data-oe-field', 'data-oe-type', 'data-oe-expression', 'data-oe-translation-initial-sha', 'data-oe-nodeid',
-     'data-publish', 'data-id', 'data-res_id', 'data-interval', 'data-member_id', 'data-scroll-background-ratio', 'data-view-id',
-     'data-class', 'data-mimetype', 'data-original-src', 'data-original-id', 'data-gl-filter', 'data-quality', 'data-resize-width',
-     'data-shape', 'data-shape-colors', 'data-file-name', 'data-original-mimetype',
-     'data-oe-protected',  # editor
-     'data-behavior-props', 'data-prop-name',  # knowledge commands
-     ])
-SANITIZE_TAGS = {
-    # allow new semantic HTML5 tags
-    'allow_tags': clean.defs.tags | frozenset('article bdi section header footer hgroup nav aside figure main'.split() + [etree.Comment]),
-    'kill_tags': ['base', 'embed', 'frame', 'head', 'iframe', 'link', 'meta',
-                  'noscript', 'object', 'script', 'style', 'title'],
-    'remove_tags': ['html', 'body'],
-}
-
-
-class _Cleaner(clean.Cleaner):
-
-    _style_re = re.compile(r'''([\w-]+)\s*:\s*((?:[^;"']|"[^";]*"|'[^';]*')+)''')
-
-    _style_whitelist = [
-        'font-size', 'font-family', 'font-weight', 'font-style', 'background-color', 'color', 'text-align',
-        'line-height', 'letter-spacing', 'text-transform', 'text-decoration', 'text-decoration', 'opacity',
-        'float', 'vertical-align', 'display',
-        'padding', 'padding-top', 'padding-left', 'padding-bottom', 'padding-right',
-        'margin', 'margin-top', 'margin-left', 'margin-bottom', 'margin-right',
-        'white-space',
-        # box model
-        'border', 'border-color', 'border-radius', 'border-style', 'border-width', 'border-top', 'border-bottom',
-        'height', 'width', 'max-width', 'min-width', 'min-height',
-        # tables
-        'border-collapse', 'border-spacing', 'caption-side', 'empty-cells', 'table-layout']
-
-    _style_whitelist.extend(
-        ['border-%s-%s' % (position, attribute)
-            for position in ['top', 'bottom', 'left', 'right']
-            for attribute in ('style', 'color', 'width', 'left-radius', 'right-radius')]
-    )
-
-    strip_classes = False
-    sanitize_style = False
-
-    def __call__(self, doc):
-        super(_Cleaner, self).__call__(doc)
-
-        # if we keep attributes but still remove classes
-        if not getattr(self, 'safe_attrs_only', False) and self.strip_classes:
-            for el in doc.iter(tag=etree.Element):
-                self.strip_class(el)
-
-        # if we keep style attribute, sanitize them
-        if not self.style and self.sanitize_style:
-            for el in doc.iter(tag=etree.Element):
-                self.parse_style(el)
-
-    def strip_class(self, el):
-        if el.attrib.get('class'):
-            del el.attrib['class']
-
-    def parse_style(self, el):
-        attributes = el.attrib
-        styling = attributes.get('style')
-        if styling:
-            valid_styles = collections.OrderedDict()
-            styles = self._style_re.findall(styling)
-            for style in styles:
-                if style[0].lower() in self._style_whitelist:
-                    valid_styles[style[0].lower()] = style[1]
-            if valid_styles:
-                el.attrib['style'] = '; '.join('%s:%s' % (key, val) for (key, val) in valid_styles.items())
-            else:
-                del el.attrib['style']
-
-
-def tag_quote(el):
-    def _create_new_node(tag, text, tail=None, attrs=None):
-        new_node = etree.Element(tag)
-        new_node.text = text
-        new_node.tail = tail
-        if attrs:
-            for key, val in attrs.items():
-                new_node.set(key, val)
-        return new_node
-
-    def _tag_matching_regex_in_text(regex, node, tag='span', attrs=None):
-        text = node.text or ''
-        if not re.search(regex, text):
-            return
-
-        child_node = None
-        idx, node_idx = 0, 0
-        for item in re.finditer(regex, text):
-            new_node = _create_new_node(tag, text[item.start():item.end()], None, attrs)
-            if child_node is None:
-                node.text = text[idx:item.start()]
-                new_node.tail = text[item.end():]
-                node.insert(node_idx, new_node)
-            else:
-                child_node.tail = text[idx:item.start()]
-                new_node.tail = text[item.end():]
-                node.insert(node_idx, new_node)
-            child_node = new_node
-            idx = item.end()
-            node_idx = node_idx + 1
-
-    el_class = el.get('class', '') or ''
-    el_id = el.get('id', '') or ''
-
-    # gmail or yahoo // # outlook, html // # msoffice
-    if 'gmail_extra' in el_class or \
-            'divRplyFwdMsg' in el_id or \
-            ('SkyDrivePlaceholder' in el_class or 'SkyDrivePlaceholder' in el_class):
-        el.set('data-o-mail-quote', '1')
-        if el.getparent() is not None:
-            el.getparent().set('data-o-mail-quote-container', '1')
-
-    if (el.tag == 'hr' and ('stopSpelling' in el_class or 'stopSpelling' in el_id)) or \
-       'yahoo_quoted' in el_class:
-        # Quote all elements after this one
-        el.set('data-o-mail-quote', '1')
-        for sibling in el.itersiblings(preceding=False):
-            sibling.set('data-o-mail-quote', '1')
-
-    # html signature (-- <br />blah)
-    signature_begin = re.compile(r"((?:(?:^|\n)[-]{2}[\s]?$))")
-    if el.text and el.find('br') is not None and re.search(signature_begin, el.text):
-        el.set('data-o-mail-quote', '1')
-        if el.getparent() is not None:
-            el.getparent().set('data-o-mail-quote-container', '1')
-
-    # text-based quotes (>, >>) and signatures (-- Signature)
-    text_complete_regex = re.compile(r"((?:\n[>]+[^\n\r]*)+|(?:(?:^|\n)[-]{2}[\s]?[\r\n]{1,2}[\s\S]+))")
-    if not el.get('data-o-mail-quote'):
-        _tag_matching_regex_in_text(text_complete_regex, el, 'span', {'data-o-mail-quote': '1'})
-
-    if el.tag == 'blockquote':
-        # remove single node
-        el.set('data-o-mail-quote-node', '1')
-        el.set('data-o-mail-quote', '1')
-    if el.getparent() is not None and (el.getparent().get('data-o-mail-quote') or el.getparent().get('data-o-mail-quote-container')) and not el.getparent().get('data-o-mail-quote-node'):
-        el.set('data-o-mail-quote', '1')
-
-
-def html_normalize(src, filter_callback=None):
-    """ Normalize `src` for storage as an html field value.
-
-    The string is parsed as an html tag soup, made valid, then decorated for
-    "email quote" detection, and prepared for an optional filtering.
-    The filtering step (e.g. sanitization) should be performed by the
-    `filter_callback` function (to avoid multiple parsing operations, and
-    normalize the result).
-
-    :param src: the html string to normalize
-    :param filter_callback: optional callable taking a single `etree._Element`
-        document parameter, to be called during normalization in order to
-        filter the output document
-    """
-
-    if not src:
-        return src
-
-    src = ustr(src, errors='replace')
-    # html: remove encoding attribute inside tags
-    doctype = re.compile(r'(<[^>]*\s)(encoding=(["\'][^"\']*?["\']|[^\s\n\r>]+)(\s[^>]*|/)?>)', re.IGNORECASE | re.DOTALL)
-    src = doctype.sub(u"", src)
-
-    try:
-        doc = html.fromstring(src)
-    except etree.ParserError as e:
-        # HTML comment only string, whitespace only..
-        if 'empty' in str(e):
-            return u""
-        raise
-
-    # perform quote detection before cleaning and class removal
-    if doc is not None:
-        for el in doc.iter(tag=etree.Element):
-            tag_quote(el)
-
-    if filter_callback:
-        doc = filter_callback(doc)
-
-    src = html.tostring(doc, encoding='unicode')
-
-    # this is ugly, but lxml/etree tostring want to put everything in a
-    # 'div' that breaks the editor -> remove that
-    if src.startswith('<div>') and src.endswith('</div>'):
-        src = src[5:-6]
-
-    # html considerations so real html content match database value
-    src = src.replace(u'\xa0', u'&nbsp;')
-
-    return src
-
-
-def html_sanitize(src, silent=True, sanitize_tags=True, sanitize_attributes=False, sanitize_style=False, sanitize_form=True, strip_style=False, strip_classes=False):
-    if not src:
-        return src
-
-    logger = logging.getLogger(__name__ + '.html_sanitize')
-
-    def sanitize_handler(doc):
-        kwargs = {
-            'page_structure': True,
-            'style': strip_style,              # True = remove style tags/attrs
-            'sanitize_style': sanitize_style,  # True = sanitize styling
-            'forms': sanitize_form,            # True = remove form tags
-            'remove_unknown_tags': False,
-            'comments': False,
-            'processing_instructions': False
-        }
-        if sanitize_tags:
-            kwargs.update(SANITIZE_TAGS)
-
-        if sanitize_attributes:  # We keep all attributes in order to keep "style"
-            if strip_classes:
-                current_safe_attrs = safe_attrs - frozenset(['class'])
-            else:
-                current_safe_attrs = safe_attrs
-            kwargs.update({
-                'safe_attrs_only': True,
-                'safe_attrs': current_safe_attrs,
-            })
-        else:
-            kwargs.update({
-                'safe_attrs_only': False,  # keep oe-data attributes + style
-                'strip_classes': strip_classes,  # remove classes, even when keeping other attributes
-            })
-
-        cleaner = _Cleaner(**kwargs)
-        cleaner(doc)
-        return doc
-
-    try:
-        sanitized = html_normalize(src, filter_callback=sanitize_handler)
-    except etree.ParserError:
-        if not silent:
-            raise
-        logger.warning(u'ParserError obtained when sanitizing %r', src, exc_info=True)
-        sanitized = '<p>ParserError when sanitizing</p>'
-    except Exception:
-        if not silent:
-            raise
-        logger.warning(u'unknown error obtained when sanitizing %r', src, exc_info=True)
-        sanitized = '<p>Unknown error when sanitizing</p>'
-
-    return markupsafe.Markup(sanitized)
-
 
 #----------------------------------------------------------
 # Email Quote
@@ -416,15 +159,16 @@ def html_keep_url(text):
     return final
 
 
-def html_to_inner_content(html):
+def html_to_inner_content(html_content):
     """Returns unformatted text after removing html tags and excessive whitespace from a
     string/Markup. Passed strings will first be sanitized.
     """
-    if is_html_empty(html):
+    if is_html_empty(html_content):
         return ''
-    if not isinstance(html, markupsafe.Markup):
-        html = html_sanitize(html)
-    processed = re.sub(HTML_NEWLINES_REGEX, ' ', html)
+    if not isinstance(html_content, markupsafe.Markup):
+        html_content = html.unescape(html_content)
+        html_content = html_sanitize(html_content)
+    processed = re.sub(HTML_NEWLINES_REGEX, ' ', html_content)
     processed = re.sub(HTML_TAGS_REGEX, '', processed)
     processed = re.sub(r' {2,}|\t', ' ', processed)
     processed = processed.strip()
