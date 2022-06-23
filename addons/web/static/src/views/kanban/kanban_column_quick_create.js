3b673794cebf3dd2833bf383a41093cdbf7c856e
Aaron Bohy <aab@odoo.com>
2022-06-23 08:51:33 +0200

    [REF] web: kanban, list and form views in owl
    This commit introduces the new kanban, list and form views written
    in owl. Even though it contains the implementation of those 3 views,
    only the kanban view is activated for now (the list and form views
    aren't 100% ready yet, so they aren't added to the view registry).
    
    Alongside the views, the fields (<field name="..."/> in archs) and
    widgets (<widget name="..." in archs) of web/ have been implemented
    in owl as well. Legacy ones remaining in other addons are supported
    in our new views, thanks to a compatibility layer. The goal is to convert
    them asap though.
    
    Legacy views, fields and widgets are kept for now, which explains the
    number of added lines in this PR (around half of them concern tests).
    They are still extended by custom code in other addons, that still need
    to be converted (a lot of them are already on the way). Moreover,
    they are still used in Studio as well. The plan is to lazy load them in
    the Studio bundle when all custom code extending them will be
    converted. Studio will be converted for v17.
    
    Co-authored-by: Aaron Bohy <aab@odoo.com>
    Co-authored-by: Bruno Boi <boi@odoo.com>
    Co-authored-by: Géry Debongnie <ged@odoo.com>
    Co-authored-by: Samuel Degueldre <sad@odoo.com>
    Co-authored-by: Mathieu Duckerts-Antoine <dam@odoo.com>
    Co-authored-by: Simon Genin (ges) <ges@odoo.com>
    Co-authored-by: Francois (fge) <fge@odoo.com>
    Co-authored-by: Michael Mattiello (mcm) <mcm@odoo.com>
    Co-authored-by: Julien Mougenot <jum@odoo.com>
    Co-authored-by: Lucas Perais (lpe) <lpe@odoo.com>
    Co-authored-by: Jorge Pinna Puissant <jpp@odoo.com>
    Co-authored-by: luvi <luvi@odoo.com>

---
+++ addons/web/static/src/views/kanban/kanban_column_quick_create.js
@@ -0,0 +1,118 @@
+/** @odoo-module */
+import { Dialog } from "@web/core/dialog/dialog";
+import { Notebook } from "@web/core/notebook/notebook";
+import { useHotkey } from "@web/core/hotkeys/hotkey_hook";
+import { _lt } from "@web/core/l10n/translation";
+import { useAutofocus, useService } from "@web/core/utils/hooks";
+
+const { Component, useExternalListener, useState, useRef } = owl;
+
+export class KanbanColumnQuickCreate extends Component {
+    setup() {
+        this.dialog = useService("dialog");
+        this.root = useRef("root");
+        this.state = useState({
+            columnTitle: "",
+        });
+
+        useAutofocus();
+
+        // Close on outside click
+        useExternalListener(window, "mousedown", (/** @type {MouseEvent} */ ev) => {
+            // This target is kept in order to impeach close on outside click behavior if the click
+            // has been initiated from the quickcreate root element (mouse selection in an input...)
+            this.mousedownTarget = ev.target;
+        });
+        useExternalListener(
+            window,
+            "click",
+            (/** @type {MouseEvent} */ ev) => {
+                const target = this.mousedownTarget || ev.target;
+                const gotClickedInside = this.root.el.contains(target);
+                if (!gotClickedInside) {
+                    this.fold();
+                }
+                this.mousedownTarget = null;
+            },
+            { capture: true }
+        );
+
+        // Key Navigation
+        // FIXME ? Maybe it will also validate even if enter is pressed outside of the quick create machin
+        useHotkey("enter", () => this.validate());
+        useHotkey("escape", () => this.fold());
+    }
+
+    fold() {
+        this.props.onFoldChange(true);
+    }
+
+    unfold() {
+        this.props.onFoldChange(false);
+    }
+
+    validate() {
+        if (this.state.columnTitle.length) {
+            this.props.onValidate(this.state.columnTitle);
+            this.state.columnTitle = "";
+        }
+    }
+
+    showExamples() {
+        this.dialog.add(KanbanColumnExamplesDialog, {
+            examples: this.props.exampleData.examples,
+            applyExamplesText:
+                this.props.exampleData.applyExamplesText || this.env._t("Use This For My Kanban"),
+            applyExamples: (index) => {
+                for (const groupName of this.props.exampleData.examples[index].columns) {
+                    this.props.onValidate(groupName);
+                }
+            },
+        });
+    }
+}
+KanbanColumnQuickCreate.template = "web.KanbanColumnQuickCreate";
+
+class KanbanExamplesNotebookTemplate extends Component {
+    random(min, max) {
+        return new Array(Math.floor(Math.random() * (max - min) + min));
+    }
+}
+KanbanExamplesNotebookTemplate.template = "web.KanbanExamplesNotebookTemplate";
+
+class KanbanColumnExamplesDialog extends Component {
+    setup() {
+        this.navList = useRef("navList");
+        this.pages = [];
+        this.activePage = null;
+        this.props.examples.forEach((eg) => {
+            eg.randomNumber = this.random(1, 4);
+            this.pages.push({
+                Component: KanbanExamplesNotebookTemplate,
+                props: {
+                    ...eg,
+                    isVisible: true,
+                    title: eg.name,
+                },
+                id: eg.name,
+            });
+        });
+    }
+
+    onPageUpdate(page) {
+        this.activePage = page;
+    }
+
+    random(min, max) {
+        return new Array(Math.floor(Math.random() * (max - min) + min));
+    }
+
+    applyExamples() {
+        const index = this.props.examples.findIndex((e) => e.name === this.activePage);
+        this.props.applyExamples(index);
+        this.props.close();
+    }
+}
+KanbanColumnExamplesDialog.template = "web.KanbanColumnExamplesDialog";
+KanbanColumnExamplesDialog.components = { Dialog, Notebook };
+KanbanColumnExamplesDialog.title = _lt("Kanban Examples");
