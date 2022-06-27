b0d2d45980546d1a7987a90fa6ad732123ae8169
PoMa <poma@odoo.com>
2022-06-27 14:08:30 +0430

    temporarily disable statement balance check

--- addons/account/models/account_bank_statement.py
+++ addons/account/models/account_bank_statement.py
@@ -331,11 +331,11 @@ class AccountBankStatement(models.Model):
                 else:
                     balance_end_real = formatLang(self.env, stmt.balance_end_real, currency_obj=stmt.currency_id)
                     balance_end = formatLang(self.env, stmt.balance_end, currency_obj=stmt.currency_id)
-                    raise UserError(_(
-                        'The ending balance is incorrect !\nThe expected balance (%(real_balance)s) is different from the computed one (%(computed_balance)s).',
-                        real_balance=balance_end_real,
-                        computed_balance=balance_end
-                    ))
+                    # raise UserError(_(
+                    #     'The ending balance is incorrect !\nThe expected balance (%(real_balance)s) is different from the computed one (%(computed_balance)s).',
+                    #     real_balance=balance_end_real,
+                    #     computed_balance=balance_end
+                    # ))
         return True
 
     @api.ondelete(at_uninstall=False)
