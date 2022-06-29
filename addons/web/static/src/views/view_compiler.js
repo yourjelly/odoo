74fd8a3d59dca39dc984a22c7343c7a84995a29a
Julien Mougenot <jum@odoo.com>
2022-06-29 16:23:07 +0200

    [FIX] web: Remove console.dirxml

--- addons/web/static/src/views/view_compiler.js
+++ addons/web/static/src/views/view_compiler.js
@@ -452,7 +452,6 @@ export function useViewCompiler(ViewCompiler, templateKey, xmlDoc, params) {
     if (!templateIds[templateKey]) {
         const compiledDoc = new ViewCompiler().compile(xmlDoc, params);
         templateIds[templateKey] = xml`${compiledDoc.outerHTML}`;
-        console.dirxml(compiledDoc);
     }
     return templateIds[templateKey];
 }
