e62dceca7aee5e328b93b67cd06f05ee81d4ad4f
Jorge Pinna Puissant <jpp@odoo.com>
2022-07-11 11:33:05 +0200

    fixup! [REF] base,web: settings form view

--- addons/web/static/src/webclient/settings_form_view/highlight_text/settings_radio_field.js
+++ addons/web/static/src/webclient/settings_form_view/highlight_text/settings_radio_field.js
@@ -6,11 +6,10 @@ import { FormLabelHighlightText } from "./form_label_highlight_text";
 
 export class SettingsRadioField extends RadioField {}
 
-// SettingsRadioField.extractStringExpr = (fieldName, record) => {
-//     const activeField = record.activeFields[fieldName];
-//     const extractProps = SettingsRadioField.extractProps(fieldName, record, activeField);
-//     return extractProps.radioItems.map((r) => r[1]);
-// };
+SettingsRadioField.extractStringExpr = (fieldName, record) => {
+    const radioItems = SettingsRadioField.getItems(fieldName, record);
+    return radioItems.map((r) => r[1]);
+};
 SettingsRadioField.template = "web.SettingsRadioField";
 SettingsRadioField.components = { ...RadioField.components, FormLabelHighlightText };
 
