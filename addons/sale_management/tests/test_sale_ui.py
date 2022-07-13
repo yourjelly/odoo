1263329ccd30e306855f29fab7eb8951deaff5e6
Mathieu Duckerts-Antoine <dam@odoo.com>
2022-07-13 13:33:33 +0200

    [FIX] sale_management: unskip test_01_sale_tour

--- addons/sale_management/tests/test_sale_ui.py
+++ addons/sale_management/tests/test_sale_ui.py
@@ -6,5 +6,4 @@ import odoo.tests
 class TestUi(odoo.tests.HttpCase):
 
     def test_01_sale_tour(self):
-        import unittest; raise unittest.SkipTest("skipWOWL")
         self.start_tour("/web", 'sale_tour', login="admin", step_delay=100)
