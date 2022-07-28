309f8e6d349c80e8e17919236017205bdae48eba
Antoine Guenet <age@odoo.com>
2022-07-28 12:54:07 +0200

    [FIX] web_editor: only correct triple click when triple click
    The triple click correction algorithm in `getDeepRange` is supposed to
    do be triggered only when a selection spans multiple nodes and ends at
    position 0 of a node. But it only checked the end of the selection and
    failed to check if the selection actually did span multiple nodes.
    
    task-2921844

--- addons/web_editor/static/lib/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/lib/odoo-editor/src/utils/utils.js
@@ -654,6 +654,7 @@ export function getDeepRange(editable, { range, sel, splitText, select, correctT
     if (
         correctTripleClick &&
         !endOffset &&
+        (start !== end || startOffset !== endOffset) &&
         (!beforeEnd || (beforeEnd.nodeType === Node.TEXT_NODE && !isVisibleStr(beforeEnd)))
     ) {
         const previous = previousLeaf(end, editable, true);
