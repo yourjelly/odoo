7fd4dfbc950e6b6db7a0b1b85ada993f32f8be6e
tsm-odoo <tsm@odoo.com>
2022-08-03 15:49:57 +0200

    [IMP] bus, *: adapt server to use websocket instead of longpolling
    *: hr_presence, web_editor.
    
    This commit is part of the websocket integration in Odoo.
    It focuses on adapting the bus to support websockets:
       - last notification id is now kept on the server
       - channel list is built by overriding the `_get_backend_websocket_channels`
         method of the `ir_websocket` model instead of overriding the `_poll`
         method of the bus controller.
       - The bus presence was updated during polls, since there is no more poll,
         bus presence update will be the responsability of the client.
       - The `/websocket/peek_notifications`, `/websocket/update_bus_presence`
         routes will be available so that odoo sh can access notifications/update presence
          from http requests.
       - /longpolling routes are now prefixed with /bus thus won't be redirected to the
         gevent worker anymore.
    
    Since websocket now handle incoming messages, a way to manage authentication
    have been introduced :
        - The session is retrieved from the HTTP handshake.
        - When a websocket message comes/leaves the session is retrieved
          on the file system so that we're sure it still exists and that
          it is up to date.
        - The session is checked
        - If no session is found on the file system or `check_session`
          fails, the websocket connection is closed with the `SESSION_EXPIRED`
          close code (which is a custom close code: 4001).
        - Note that websocket connections are closed every `KEEP_ALIVE_TIMEOUT`
          seconds to ensure no websocket connection will stay open if the user
          clears its cookies.
        - Note that a wsrequest object is available when processing incoming
          messages. It is similar to the http request and contains various
          useful informations (session, env, ...).

--- addons/bus/controllers/websocket.py
+++ addons/bus/controllers/websocket.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.http import Controller, request, route
+from ..models.bus import channel_with_db
 from ..websocket import WebsocketConnectionHandler
 
 
@@ -12,3 +13,16 @@ class WebsocketController(Controller):
         successfull.
         """
         return WebsocketConnectionHandler.open_connection(request)
+
+    @route('/websocket/peek_notifications', type='json', auth='public', cors='*')
+    def peek_notifications(self, channels, last):
+        channels = list(set(
+            channel_with_db(request.db, c)
+            for c in request.env['ir.websocket']._build_websocket_channel_list(channels)
+        ))
+        notifications = request.env['bus.bus']._poll(channels, last)
+        return {'channels': channels, 'notifications': notifications}
+
+    @route('/websocket/update_bus_presence', type='http', auth='public', cors='*')
+    def update_bus_presence(self, inactivity_period):
+        request.env['ir.websocket']._update_bus_presence(inactivity_period)
