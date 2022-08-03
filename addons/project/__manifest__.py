43165d375a12665b4266d91ea66b9c3aa5edae90
Damien Raquet <damr@odoo.com>
2022-08-03 11:20:41 +0200

    [REM] milestone_reached_wizard: remove the milestone_reach_wizard window
    this pop-up window was removed because it was not really usefull and
    could become annoying for project with many short-term milestones

--- addons/project/__manifest__.py
+++ addons/project/__manifest__.py
@@ -48,7 +48,6 @@
         'data/mail_template_data.xml',
         'data/project_data.xml',
         'wizard/project_task_type_delete_views.xml',
-        'wizard/project_milestone_reach_wizard.xml',
     ],
     'demo': [
         'data/mail_template_demo.xml',
