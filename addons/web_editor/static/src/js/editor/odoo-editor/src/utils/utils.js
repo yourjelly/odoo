f581cb9b57c3d5f6a745e55bd1cf6864571db06f
dhba-odoo <dhba@odoo.com>
2022-09-26 18:01:54 +0530

    [FIX] website: s_social icons shouldn't be deletable
    Before this commit:
    
    Clicking on an icon in the social media snippet and typing a letter, the
    icons will be removed.
    
    After this commit:
    
    Now icons will not remove anymore.
    
    Task-2977241

--- addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
@@ -1197,7 +1197,8 @@ export function isBlock(node) {
         return false;
     }
     // The node might not be in the DOM, in which case it has no CSS values.
-    if (window.document !== node.ownerDocument) {
+    const activeWindow = window.document.activeElement.contentWindow;
+    if (!activeWindow && window.document !== node.ownerDocument || activeWindow && activeWindow.document != node.ownerDocument) {
         return blockTagNames.includes(tagName);
     }
     // We won't call `getComputedStyle` more than once per node.
