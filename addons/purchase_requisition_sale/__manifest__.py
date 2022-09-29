0d3124ce66c2232ce4bf4418d55eb5786607b8e6
clesgow <quwo@odoo.com>
2022-09-29 15:33:34 +0200

    [FIX] purchase_requisition{,_sale}: Link SO to alternative RFQ
    Steps to reproduce:
    - Create a service that is subcontracted `service_to_purchase` = true
    - Create a SO containing this service
    - Go to the newly created RFQ
    - Go to Alternatives -> Create Alternative and select a new vendor
    - The alternative RFQ doesn't show a link to the SO, nor the SO to this
    RFQ
    
    Initially, when the alternative RFQ is created, only the `product_id`
    and its quantity is copied to the new RFQ. Now we add the reference to
    the SO as well so the RFQ is properly linked to the original SO.
    
    Part of task-2985735

---
+++ addons/purchase_requisition_sale/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Purchase Requisition Sale',
+    'description': "Bridge module for Purchase requisition and Sales",
+    'version': '1.0',
+    'category': 'Inventory/Purchase',
+    'sequence': 70,
+    'depends': ['purchase_requisition', 'sale_purchase'],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
