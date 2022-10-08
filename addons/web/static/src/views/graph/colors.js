5578a4d7c18be257e00b006672ab2d6a231b740f
Brieuc-brd <brd@odoo.com>
2022-10-08 13:03:44 +0200

    remove grap color custo

--- addons/web/static/src/views/graph/colors.js
+++ addons/web/static/src/views/graph/colors.js
@@ -1,54 +1,23 @@
 /** @odoo-module **/
 
-import { getCookie } from "web.utils.cookies";
-
-const COLORS_BRIGHT = [
+export const COLORS = [
     "#1f77b4",
     "#ff7f0e",
     "#aec7e8",
-    "#ffbb78",
-    "#2ca02c",
-    "#98df8a",
-    "#d62728",
     "#ff9896",
     "#9467bd",
     "#c5b0d5",
-    "#875a7b", // ~ Enterprise
+    "#8c564b",
     "#c49c94",
     "#e377c2",
-    "#dcd0d9", // Dashboards Primary
+    "#f7b6d2",
     "#7f7f7f",
     "#c7c7c7",
     "#bcbd22",
     "#dbdb8d",
     "#17becf",
-    "#a5d8d7", // Dashboards Secondary
+    "#9edae5",
 ];
-
-const COLORS_DARK = [
-    "#00ffff",
-    "#ff6347",
-    "#00ced1",
-    "#ffd700",
-    "#29ef29",
-    "#c5fabb",
-    "#fe4b4c",
-    "#ffb6c1",
-    "#ba87e9",
-    "#eadbf6",
-    "#c568af", // ~ Enterprise
-    "#ecc1b8",
-    "#fda9e3",
-    "#BB86FC", // Dashboards Primary
-    "#808080",
-    "#f2e8e8",
-    "#fcfe2d",
-    "#f8f8bc",
-    "#17becf",
-    "#10efed", // Dashboards Secondary
-];
-
-export const COLORS = (getCookie("color_scheme") === "light" ? COLORS_BRIGHT : COLORS_DARK);
 
 /**
  * @param {number} index
@@ -60,7 +29,7 @@ export function getColor(index) {
 
 export const DEFAULT_BG = "#d3d3d3";
 
-export const BORDER_WHITE = (getCookie("color_scheme") === "light" ? "rgba(255,255,255,0.6)" : "rgba(0, 0, 0, 0.6)");
+export const BORDER_WHITE = "rgba(255,255,255,0.6)";
 
 const RGB_REGEX = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
 
