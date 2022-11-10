0f03bb8f47bce058c18dc9194dddb3ae400816d3
Jeremy Kersten <jke@odoo.com>
2022-11-10 10:57:30 +0100

    [IMP] http_routing: support redirect of double slash in middle of path
    Move code to support only the redirect from url containing double '/' in
    the middle of the path.
    Keep same behavior than v15 and default Apache behavior.
    
    domain.com//shop/product/1 -> 404
    domain.com/shop//product/1 -> 301 -> /shop/product/1
    
    opw-3063387

--- odoo/http.py
+++ odoo/http.py
@@ -1562,6 +1562,12 @@ class Request:
         """
         ir_http = self.registry['ir.http']
 
+        # Some URLs in website are concatenated, first url ends with /,
+        # second url starts with /, resulting url contains two following
+        # slashes that must be merged.
+        if self.httprequest.method == 'GET' and '//' in self.httprequest.path:
+            return self.redirect(self.httprequest.path.replace('//', '/'), 301, local=True)
+
         try:
             rule, args = ir_http._match(self.httprequest.path)
         except NotFound:
@@ -1938,14 +1944,6 @@ class Application:
             def fake_start_response(status, headers):
                 return
             ProxyFix(fake_app)(environ, fake_start_response)
-
-        # Some URLs in website are concatenated, first url ends with /,
-        # second url starts with /, resulting url contains two following
-        # slashes that must be merged.
-        if environ['REQUEST_METHOD'] == 'GET' and '//' in environ['PATH_INFO']:
-            response = werkzeug.utils.redirect(
-                environ['PATH_INFO'].replace('//', '/'), 301)
-            return response(environ, start_response)
 
         httprequest = werkzeug.wrappers.Request(environ)
         httprequest.user_agent_class = UserAgent  # use vendored userAgent since it will be removed in 2.1
