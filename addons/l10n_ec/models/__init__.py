a28b434ebf0a01888f6a6f0136a701ba399f72c6
Josse Colpaert <jco@odoo.com>
2022-11-22 22:45:59 +0100

    further cleaning suggestions for master

--- addons/l10n_ec/models/__init__.py
+++ addons/l10n_ec/models/__init__.py
@@ -6,7 +6,6 @@ from . import l10n_ec_sri_payment
 from . import l10n_latam_document_type
 from . import account_move
 from . import account_tax
-from . import account_tax_group
 from . import res_company
 from . import account_journal
 from . import account_chart_template
--- addons/l10n_ec/models/account_tax.py
+++ addons/l10n_ec/models/account_tax.py
@@ -3,6 +3,36 @@
 
 from odoo import fields, models
 
+_TYPE_EC = [
+    ("vat", "Non-zero VAT"),
+    ("zero_vat", "VAT 0%"),
+    ("not_charged_vat", "VAT Not Charged"),
+    ("exempt_vat", "VAT Exempt"),
+    ("ice", "Special Consumptions Tax (ICE)"),
+    ("irbpnr", "Plastic Bottles (IRBPNR)"),
+    ("withhold_vat_sale", "VAT Withhold on Sales"),
+    ("withhold_vat_purchase", "VAT Withhold on Purchases"),
+    ("withhold_income_sale", "Profit Withhold on Sales"),
+    ("withhold_income_purchase", "Profit Withhold on Purchases"),
+    ("outflows_tax", "Exchange Outflows"),
+    ("other", "Others"),
+]
+
+L10N_EC_TAXSUPPORTS = [
+    ('01', '01 Tax credit for VAT declaration (services and goods other than inventories and fixed assets)'),
+    ('02', '02 Cost or Expense for IR declaration (services and goods other than inventories and fixed assets)'),
+    ('03', '03 Fixed Asset - Tax Credit for VAT return'),
+    ('04', '04 Fixed Asset - Cost or Expense for IR declaration'),
+    ('05', '05 Settlement of travel, lodging and food expenses IR expenses (on behalf of employees and not of the company)'),
+    ('06', '06 Inventory - Tax Credit for VAT return'),
+    ('07', '07 Inventory - Cost or Expense for IR declaration'),
+    ('08', '08 Amount paid to request Expense Reimbursement (intermediary)'),
+    ('09', '09 Claims Reimbursement'),
+    ('10', '10 Distribution of Dividends, Benefits or Profits'),
+    ('15', '15 Payments made for own and third-party consumption of digital services'),
+    ('00', '00 Special cases whose support does not apply to the above options')
+]
+
 
 class AccountTax(models.Model):
 
@@ -20,24 +50,19 @@ class AccountTax(models.Model):
         string="Code ATS",
         help="Tax Identification Code for the Simplified Transactional Annex",
     )
+    l10n_ec_type = fields.Selection(
+        _TYPE_EC, string="Type Ecuadorian Tax", help="Ecuadorian taxes subtype"
+    )
+    l10n_ec_taxsupport_code = fields.Selection(
+        L10N_EC_TAXSUPPORTS,
+        string='Tax Support',
+        help='Indicates if the purchase invoice supports tax credit or cost or expenses, conforming table 5 of ATS'
+    )
+
 
 
 class AccountTaxTemplate(models.Model):
-
     _inherit = "account.tax.template"
-
-    def _get_tax_vals(self, company, tax_template_to_tax):
-        vals = super(AccountTaxTemplate, self)._get_tax_vals(
-            company, tax_template_to_tax
-        )
-        vals.update(
-            {
-                "l10n_ec_code_base": self.l10n_ec_code_base,
-                "l10n_ec_code_applied": self.l10n_ec_code_applied,
-                "l10n_ec_code_ats": self.l10n_ec_code_ats,
-            }
-        )
-        return vals
 
     l10n_ec_code_base = fields.Char(
         string="Code base",
@@ -51,3 +76,27 @@ class AccountTaxTemplate(models.Model):
         string="Code ATS",
         help="Tax Identification Code for the Simplified Transactional Annex",
     )
+    l10n_ec_type = fields.Selection(
+        _TYPE_EC, string="Type Ecuadorian Tax", help="Ecuadorian taxes subtype"
+    )
+    l10n_ec_taxsupport_code = fields.Selection(
+        L10N_EC_TAXSUPPORTS,
+        string='Tax Support',
+        help='Indicates if the purchase invoice supports tax credit or cost or expenses, conforming table 5 of ATS'
+    )
+
+
+    def _get_tax_vals(self, company, tax_template_to_tax):
+        vals = super(AccountTaxTemplate, self)._get_tax_vals(
+            company, tax_template_to_tax
+        )
+        vals.update(
+            {
+                "l10n_ec_code_base": self.l10n_ec_code_base,
+                "l10n_ec_code_applied": self.l10n_ec_code_applied,
+                "l10n_ec_code_ats": self.l10n_ec_code_ats,
+                "l10n_ec_type": self.l10n_ec_type,
+                "l10n_ec_taxsupport_code": self.l10n_ec_taxsupport_code,
+            }
+        )
+        return vals
