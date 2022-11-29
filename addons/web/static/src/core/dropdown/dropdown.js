2ba597236e1317c04532e195be74ef4fe89446f1
Sébastien Theys <seb@odoo.com>
2022-11-29 12:52:34 +0100

    [IMP] web: TO MOVE IN MASTER allow dropdown button to be disabled

--- addons/web/static/src/core/dropdown/dropdown.js
+++ addons/web/static/src/core/dropdown/dropdown.js
@@ -321,12 +321,17 @@ export class Dropdown extends Component {
 }
 Dropdown.bus = new EventBus();
 Dropdown.defaultProps = {
+    menuDisplay: "d-block",
     onOpened: () => {},
     onScroll: () => {},
 };
 Dropdown.props = {
     class: {
         type: String,
+        optional: true,
+    },
+    isDisabled: {
+        type: Boolean,
         optional: true,
     },
     toggler: {
@@ -347,6 +352,10 @@ Dropdown.props = {
         optional: true,
     },
     menuClass: {
+        type: String,
+        optional: true,
+    },
+    menuDisplay: {
         type: String,
         optional: true,
     },
