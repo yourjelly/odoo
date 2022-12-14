0957fe0145a2b82f52ff27efd912b38bd7668a4e
Louis Wicket (wil) <wil@odoo.com>
2022-12-14 16:16:04 +0100

    [IMP] mail: turn Message/isNotification into getter

--- addons/mail/static/src/new/core/message_model.js
+++ addons/mail/static/src/new/core/message_model.js
@@ -28,8 +28,6 @@ export class Message {
     /** @type {boolean} */
     isNote;
     /** @type {boolean} */
-    isNotification;
-    /** @type {boolean} */
     isStarred;
     /** @type {boolean} */
     isTransient;
@@ -113,7 +111,6 @@ export class Message {
             body,
             isDiscussion,
             isNote,
-            isNotification: type === "notification" && resModel === "mail.channel",
             isStarred: starred_partner_ids.includes(this._state.user.partnerId),
             isTransient,
             linkPreviews: linkPreviews.map((data) => new LinkPreview(data)),
@@ -204,6 +201,13 @@ export class Message {
      */
     get isNeedaction() {
         return this.needaction_partner_ids.includes(this._state.user.partnerId);
+    }
+
+    /**
+     * @returns {boolean}
+     */
+    get isNotification() {
+        return this.type === "notification" && this.resModel === "mail.channel";
     }
 
     get isSubjectSimilarToOriginThreadName() {
