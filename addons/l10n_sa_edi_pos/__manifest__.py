d1578405036a66a3830fd00ecec25ade94f2c3d4
Mehdi Bendali Hacine <mebe@odoo.com>
2022-12-23 16:09:42 +0400

    [ADD] l10n_sa_edi: Implement Saudi ZATCA invoicing standards

---
+++ addons/l10n_sa_edi_pos/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Saudi Arabia - E-invoicing (Simplified)',
+    'icon': '/l10n_sa/static/description/icon.png',
+    'version': '0.1',
+    'depends': [
+        'l10n_sa_pos',
+        'l10n_sa_edi',
+    ],
+    'author': 'Odoo',
+    'summary': """
+        ZATCA E-Invoicing, support for Simplified Invoices
+    """,
+    'description': """
+        E-invoice implementation for the Kingdom of Saudi Arabia (Simplified Invoices)
+    """,
+    'category': 'Accounting/Localizations/EDI',
+    'license': 'LGPL-3',
+}
