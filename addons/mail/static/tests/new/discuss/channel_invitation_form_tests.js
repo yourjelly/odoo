363cb090e28a7a80850a5bcf26682cd6f1f790a8
Maryam Kia <maki@odoo.com>
2023-01-05 10:14:51 +0100

    [IMP] mail: unskip the remain test of channel invitation form

--- addons/mail/static/tests/new/discuss/channel_invitation_form_tests.js
+++ addons/mail/static/tests/new/discuss/channel_invitation_form_tests.js
@@ -108,3 +108,44 @@ QUnit.test("Invitation form should display channel group restriction", async fun
     await click(".o-mail-discuss-actions button[title='Add Users']");
     assert.containsOnce(target, ".o-mail-channel-invitation-form-access-restricted");
 });
+
+QUnit.test(
+    "should be able to create a new group chat from an existing chat",
+    async function (assert) {
+        const pyEnv = await startServer();
+        const resPartnerId1 = pyEnv["res.partner"].create({
+            email: "testpartner@odoo.com",
+            name: "TestPartner",
+        });
+        const resPartnerId2 = pyEnv["res.partner"].create({
+            email: "testpartner2@odoo.com",
+            name: "TestPartner2",
+        });
+        pyEnv["res.users"].create({ partner_id: resPartnerId1 });
+        pyEnv["res.users"].create({ partner_id: resPartnerId2 });
+        const mailChannelId = pyEnv["mail.channel"].create({
+            name: "TestChanel",
+            channel_member_ids: [
+                [0, 0, { partner_id: pyEnv.currentPartnerId }],
+                [0, 0, { partner_id: resPartnerId1 }],
+            ],
+            channel_type: "chat",
+        });
+        const { openDiscuss } = await start({
+            discuss: {
+                params: {
+                    default_active_id: `mail.channel_${mailChannelId}`,
+                },
+            },
+        });
+        await openDiscuss();
+        await click(".o-mail-discuss-actions button[data-action='add-users']");
+        await insertText(".o-mail-channel-invitation-form-search-input", "TestPartner2");
+        await click(".form-check-input");
+        await click("button[title='Create Group Chat']");
+        assert.strictEqual(
+            target.querySelector(".o-mail-discuss-thread-name").value,
+            "Mitchell Admin, TestPartner, TestPartner2"
+        );
+    }
+);
