06709a227288388ddb64f4ed4d0075b8fe6cccad
kir-odoo <kir@odoo.com>
2023-01-25 12:30:13 +0530

    [ADD] l10n_in: merge l10n_in_upi l10n_in
    merge l10n_in_upi into l10n_in

--- addons/l10n_in/models/account_invoice.py
+++ addons/l10n_in/models/account_invoice.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import base64
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, RedirectWarning
+from odoo.tools.image import image_data_uri
 
 
 class AccountMove(models.Model):
@@ -85,3 +87,16 @@ class AccountMove(models.Model):
         # TO OVERRIDE
         self.ensure_one()
         return False
+
+    def _generate_qr_code(self):
+        self.ensure_one()
+        if self.company_id.country_code == 'IN':
+            payment_url = 'upi://pay?pa=%s&pn=%s&am=%s&tr=%s&tn=%s' % (
+                self.company_id.l10n_in_upi_id,
+                self.company_id.name,
+                self.amount_residual,
+                self.payment_reference or self.name,
+                ("Payment for %s" % self.name))
+            barcode = self.env['ir.actions.report'].barcode(barcode_type="QR", value=payment_url, width=120, height=120)
+            return image_data_uri(base64.b64encode(barcode))
+        return super()._generate_qr_code()
