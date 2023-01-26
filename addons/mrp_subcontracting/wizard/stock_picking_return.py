b0dbdcb03ddfe80dad76253e55e76ffd6833ca7f
Tiffany Chang (tic) <tic@odoo.com>
2023-01-26 11:01:38 +0100

    [REF] stock: cleanup return wizard
    Clean wizard by:
    - replacing onchange with compute so wizard can be used directly
    - removing no longer used variable `parent_location_id`
    - properly removing `original_location_id` from form view when needed
      (i.e. not `groups="stock.group_stock_multi_locations"`)

--- addons/mrp_subcontracting/wizard/stock_picking_return.py
+++ addons/mrp_subcontracting/wizard/stock_picking_return.py
@@ -7,21 +7,16 @@ from odoo import api, models, fields
 class ReturnPicking(models.TransientModel):
     _inherit = 'stock.return.picking'
 
-    subcontract_location_id = fields.Many2one('stock.location', compute='_compute_subcontract_location_id')
+    subcontract_location_id = fields.Many2one('stock.location', compute='_compute_picking_dependancies')
 
-    @api.depends('picking_id')
-    def _compute_subcontract_location_id(self):
-        for record in self:
-            record.subcontract_location_id = record.picking_id.partner_id.with_company(
-                record.picking_id.company_id
+    def _compute_picking_dependancies(self):
+        super()._compute_picking_dependancies()
+        for r in self:
+            r.subcontract_location_id = r.picking_id.partner_id.with_company(
+                r.picking_id.company_id
             ).property_stock_subcontractor
-
-    @api.onchange('picking_id')
-    def _onchange_picking_id(self):
-        res = super(ReturnPicking, self)._onchange_picking_id()
-        if any(return_line.quantity > 0 and return_line.move_id.is_subcontract for return_line in self.product_return_moves):
-            self.location_id = self.picking_id.partner_id.with_company(self.picking_id.company_id).property_stock_subcontractor
-        return res
+            if any(return_line.quantity > 0 and return_line.move_id.is_subcontract for return_line in r.product_return_moves):
+                r.location_id = r.picking_id.partner_id.with_company(r.picking_id.company_id).property_stock_subcontractor
 
     def _prepare_move_default_values(self, return_line, new_picking):
         vals = super(ReturnPicking, self)._prepare_move_default_values(return_line, new_picking)
--- addons/stock/wizard/stock_picking_return.py
+++ addons/stock/wizard/stock_picking_return.py
@@ -34,46 +34,47 @@ class ReturnPicking(models.TransientModel):
         return res
 
     picking_id = fields.Many2one('stock.picking')
-    product_return_moves = fields.One2many('stock.return.picking.line', 'wizard_id', 'Moves')
-    move_dest_exists = fields.Boolean('Chained Move Exists', readonly=True)
-    original_location_id = fields.Many2one('stock.location')
-    parent_location_id = fields.Many2one('stock.location')
+    product_return_moves = fields.One2many('stock.return.picking.line', 'wizard_id', 'Moves', compute='_compute_picking_dependancies', readonly=False)
+    move_dest_exists = fields.Boolean('Chained Move Exists', compute='_compute_picking_dependancies')
+    original_location_id = fields.Many2one('stock.location', compute='_compute_picking_dependancies')
     company_id = fields.Many2one(related='picking_id.company_id')
     location_id = fields.Many2one(
         'stock.location', 'Return Location',
         domain="['|', ('id', '=', original_location_id), '|', '&', ('return_location', '=', True), ('company_id', '=', False), '&', ('return_location', '=', True), ('company_id', '=', company_id)]")
 
-    @api.onchange('picking_id')
-    def _onchange_picking_id(self):
-        move_dest_exists = False
-        product_return_moves = [(5,)]
-        if self.picking_id and self.picking_id.state != 'done':
-            raise UserError(_("You may only return Done pickings."))
-        # In case we want to set specific default values (e.g. 'to_refund'), we must fetch the
-        # default values for creation.
-        line_fields = [f for f in self.env['stock.return.picking.line']._fields.keys()]
-        product_return_moves_data_tmpl = self.env['stock.return.picking.line'].default_get(line_fields)
-        for move in self.picking_id.move_ids:
-            if move.state == 'cancel':
+    @api.depends('picking_id')
+    def _compute_picking_dependancies(self):
+        for r in self:
+            if not r.picking_id:
                 continue
-            if move.scrapped:
-                continue
-            if move.move_dest_ids:
-                move_dest_exists = True
-            product_return_moves_data = dict(product_return_moves_data_tmpl)
-            product_return_moves_data.update(self._prepare_stock_return_picking_line_vals_from_move(move))
-            product_return_moves.append((0, 0, product_return_moves_data))
-        if self.picking_id and not product_return_moves:
-            raise UserError(_("No products to return (only lines in Done state and not fully returned yet can be returned)."))
-        if self.picking_id:
-            self.product_return_moves = product_return_moves
-            self.move_dest_exists = move_dest_exists
-            self.parent_location_id = self.picking_id.picking_type_id.warehouse_id and self.picking_id.picking_type_id.warehouse_id.view_location_id.id or self.picking_id.location_id.location_id.id
-            self.original_location_id = self.picking_id.location_id.id
-            location_id = self.picking_id.location_id.id
-            if self.picking_id.picking_type_id.return_picking_type_id.default_location_dest_id.return_location:
-                location_id = self.picking_id.picking_type_id.return_picking_type_id.default_location_dest_id.id
-            self.location_id = location_id
+            if r.picking_id.state != 'done':
+                raise UserError(_("You may only return Done pickings."))
+            move_dest_exists = False
+            product_return_moves = [(5,)]
+            # In case we want to set specific default values (e.g. 'to_refund'), we must fetch the
+            # default values for creation.
+            line_fields = [f for f in self.env['stock.return.picking.line']._fields.keys()]
+            product_return_moves_data_tmpl = self.env['stock.return.picking.line'].default_get(line_fields)
+            for move in r.picking_id.move_ids:
+                if move.state == 'cancel':
+                    continue
+                if move.scrapped:
+                    continue
+                if move.move_dest_ids:
+                    move_dest_exists = True
+                product_return_moves_data = dict(product_return_moves_data_tmpl)
+                product_return_moves_data.update(r._prepare_stock_return_picking_line_vals_from_move(move))
+                product_return_moves.append((0, 0, product_return_moves_data))
+            if r.picking_id and not product_return_moves:
+                raise UserError(_("No products to return (only lines in Done state and not fully returned yet can be returned)."))
+            if r.picking_id:
+                r.product_return_moves = product_return_moves
+                r.move_dest_exists = move_dest_exists
+                r.original_location_id = r.picking_id.location_id.id
+                location_id = r.picking_id.location_id.id
+                if r.picking_id.picking_type_id.return_picking_type_id.default_location_dest_id.return_location:
+                    location_id = r.picking_id.picking_type_id.return_picking_type_id.default_location_dest_id.id
+                r.location_id = location_id
 
     @api.model
     def _prepare_stock_return_picking_line_vals_from_move(self, stock_move):
