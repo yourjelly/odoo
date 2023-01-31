26ece4691e414c791e9263337ca4ce81bf236591
yhu-odoo <yhu@odoo.com>
2023-01-31 13:15:23 +0100

    fixtest

--- addons/mrp_account/models/stock_move.py
+++ addons/mrp_account/models/stock_move.py
@@ -26,22 +26,5 @@ class StockMove(models.Model):
             return account
         return super()._get_analytic_account()
 
-    def _get_src_account(self, accounts_data):
-        if not self.unbuild_id:
-            return super()._get_src_account(accounts_data)
-        else:
-            return self.location_dest_id.valuation_out_account_id.id or accounts_data['stock_input'].id
-
-    def _get_dest_account(self, accounts_data):
-        if not self.unbuild_id:
-            return super()._get_dest_account(accounts_data)
-        else:
-            return self.location_id.valuation_in_account_id.id or accounts_data['stock_output'].id
-
-    def _is_returned(self, valued_type):
-        if self.unbuild_id:
-            return True
-        return super()._is_returned(valued_type)
-
     def _ignore_automatic_valuation(self):
         return bool(self.raw_material_production_id)
