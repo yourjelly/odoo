5ffbc41dec2105bab0c3db553c8b1dd04b07975c
bat-odoo <bat@odoo.com>
2023-02-03 18:42:26 +0530

    [WIP] stock: stock variation report

--- addons/stock/models/stock_report.py
+++ addons/stock/models/stock_report.py
@@ -2,7 +2,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import re
+from babel.dates import get_quarter_names
 from datetime import datetime
+from odoo.tools import date_utils
 from odoo import models, fields, api, _, osv, Command
 
 
@@ -12,7 +14,33 @@ class StockReportNew(models.AbstractModel):
 
     #  CORE ==========================================================================================================================================
 
-    def get_report_informations(self):
+    def _get_options(self, previous_options=None):
+        # Create default options.
+        options = previous_options or {}
+
+        warehouse_data = self.env['stock.warehouse'].search_read([], fields=["id", "name"])
+
+        if 'available_warehouse' not in options:
+            options.update({
+                'name': warehouse_data[0]['name'],
+                'warehouse_id': warehouse_data[0]['id'],
+                'available_warehouse': warehouse_data,
+            })
+
+        options.update({
+            'product': True,
+            'product_ids': [],
+            'product_categories': [],
+        })
+
+        for initializer in self._get_options_initializers_in_sequence():
+            initializer(options, previous_options=previous_options)
+
+        return options
+
+    def get_report_informations(self, previous_options=None):
+        options = self._get_options(previous_options)
+
         lot_stock_id = self.env.ref('stock.warehouse0').lot_stock_id
         moves_data = self.env['stock.move'].search_read(['&', '&', ('date', '>', '2023-01-01'), ('picking_code', 'in', ['incoming', 'outgoing']), '|', ('location_id', '=', lot_stock_id.id), ('location_dest_id', '=', lot_stock_id.id)], fields=['product_id', 'product_qty', 'picking_code'])
         product_data = self.env['product.product'].with_context(to_date=datetime(2023,1,1)).search_read(
@@ -45,7 +73,175 @@ class StockReportNew(models.AbstractModel):
 
         main_html = self.env['ir.qweb']._render("stock.stock_main_template", {'product_data': product_data, 'in_qty': in_qty, 'out_qty': out_qty})
         info = {
+            'options': options,
             'main_html': main_html,
-            'searchview_html': self.env['ir.ui.view']._render_template('stock.search_template', values={}),
+            'searchview_html': self.env['ir.ui.view']._render_template('stock.search_template', {'options': options}),
         }
         return info
+
+    def _get_options_initializers_in_sequence(self):
+        initializer_prefix = '_init_options_'
+        initializers = [
+            getattr(self, attr) for attr in dir(self)
+            if attr.startswith(initializer_prefix)
+        ]
+
+        # Order them in a dependency-compliant way
+        forced_sequence_map = self._get_options_initializers_forced_sequence_map()
+        initializers.sort(key=lambda x: forced_sequence_map.get(x, forced_sequence_map.get('default')))
+
+        return initializers
+
+    def _get_options_initializers_forced_sequence_map(self):
+        return {
+            self._init_options_date: 30,
+            'default': 200,
+        }
+
+    def _init_options_date(self, options, previous_options=None):
+        """ Initialize the 'date' options key.
+
+        :param options:             The current report options to build.
+        :param previous_options:    The previous options coming from another report.
+        """
+        previous_date = (previous_options or {}).get('date', {})
+        previous_date_to = previous_date.get('date_to')
+        previous_date_from = previous_date.get('date_from')
+        previous_mode = previous_date.get('mode')
+        previous_filter = previous_date.get('filter', 'custom')
+
+        default_filter = 'this_year' # self.default_opening_date_filter
+        # options_mode = 'range' if self.filter_date_range else 'single'
+        options_mode = 'range' if True else 'single'
+        date_from = date_to = period_type = False
+
+        if previous_mode == 'single' and options_mode == 'range':
+            # 'single' date mode to 'range'.
+
+            if previous_filter:
+                date_to = fields.Date.from_string(previous_date_to or previous_date_from)
+                date_from = self.env.company.compute_fiscalyear_dates(date_to)['date_from']
+                options_filter = 'custom'
+            else:
+                options_filter = default_filter
+
+        elif previous_mode == 'range' and options_mode == 'single':
+            # 'range' date mode to 'single'.
+
+            if previous_filter == 'custom':
+                date_to = fields.Date.from_string(previous_date_to or previous_date_from)
+                date_from = date_utils.get_month(date_to)[0]
+                options_filter = 'custom'
+            elif previous_filter:
+                options_filter = previous_filter
+            else:
+                options_filter = default_filter
+
+        elif (previous_mode is None or previous_mode == options_mode) and previous_date:
+            # Same date mode.
+
+            if previous_filter == 'custom':
+                if options_mode == 'range':
+                    date_from = fields.Date.from_string(previous_date_from)
+                    date_to = fields.Date.from_string(previous_date_to)
+                else:
+                    date_to = fields.Date.from_string(previous_date_to or previous_date_from)
+                    date_from = date_utils.get_month(date_to)[0]
+                options_filter = 'custom'
+            else:
+                options_filter = previous_filter
+
+        else:
+            # Default.
+            options_filter = default_filter
+
+        # Compute 'date_from' / 'date_to'.
+        if not date_from or not date_to:
+            if options_filter == 'today':
+                date_to = fields.Date.context_today(self)
+                date_from = self.env.company.compute_fiscalyear_dates(date_to)['date_from']
+                period_type = 'today'
+            elif 'month' in options_filter:
+                date_from, date_to = date_utils.get_month(fields.Date.context_today(self))
+                period_type = 'month'
+            elif 'quarter' in options_filter:
+                date_from, date_to = date_utils.get_quarter(fields.Date.context_today(self))
+                period_type = 'quarter'
+            elif 'year' in options_filter:
+                company_fiscalyear_dates = self.env.company.compute_fiscalyear_dates(fields.Date.context_today(self))
+                date_from = company_fiscalyear_dates['date_from']
+                date_to = company_fiscalyear_dates['date_to']
+
+        options['date'] = self._get_dates_period(
+            date_from,
+            date_to,
+            options_mode,
+            period_type=period_type,
+        )
+        if 'last' in options_filter:
+            options['date'] = self._get_dates_previous_period(options, options['date'])
+        options['date']['filter'] = options_filter
+
+    @api.model
+    def _get_dates_period(self, date_from, date_to, mode, period_type=None):
+        def match(dt_from, dt_to):
+            return (dt_from, dt_to) == (date_from, date_to)
+
+        string = None
+        # If no date_from or not date_to, we are unable to determine a period
+        if not period_type or period_type == 'custom':
+            date = date_to or date_from
+            company_fiscalyear_dates = self.env.company.compute_fiscalyear_dates(date)
+            if match(company_fiscalyear_dates['date_from'], company_fiscalyear_dates['date_to']):
+                period_type = 'fiscalyear'
+                if company_fiscalyear_dates.get('record'):
+                    string = company_fiscalyear_dates['record'].name
+            elif match(*date_utils.get_month(date)):
+                period_type = 'month'
+            elif match(*date_utils.get_quarter(date)):
+                period_type = 'quarter'
+            elif match(*date_utils.get_fiscal_year(date)):
+                period_type = 'year'
+            elif match(date_utils.get_month(date)[0], fields.Date.today()):
+                period_type = 'today'
+            else:
+                period_type = 'custom'
+        elif period_type == 'fiscalyear':
+            date = date_to or date_from
+            company_fiscalyear_dates = self.env.company.compute_fiscalyear_dates(date)
+            record = company_fiscalyear_dates.get('record')
+            string = record and record.name
+
+        if not string:
+            fy_day = self.env.company.fiscalyear_last_day
+            fy_month = int(self.env.company.fiscalyear_last_month)
+            if mode == 'single':
+                string = _('As of %s') % (format_date(self.env, fields.Date.to_string(date_to)))
+            elif period_type == 'year' or (
+                    period_type == 'fiscalyear' and (date_from, date_to) == date_utils.get_fiscal_year(date_to)):
+                string = date_to.strftime('%Y')
+            elif period_type == 'fiscalyear' and (date_from, date_to) == date_utils.get_fiscal_year(date_to, day=fy_day, month=fy_month):
+                string = '%s - %s' % (date_to.year - 1, date_to.year)
+            elif period_type == 'month':
+                string = format_date(self.env, fields.Date.to_string(date_to), date_format='MMM yyyy')
+            elif period_type == 'quarter':
+                quarter_names = get_quarter_names('abbreviated', locale=get_lang(self.env).code)
+                string = u'%s\N{NO-BREAK SPACE}%s' % (
+                    quarter_names[date_utils.get_quarter_number(date_to)], date_to.year)
+            else:
+                dt_from_str = format_date(self.env, fields.Date.to_string(date_from))
+                dt_to_str = format_date(self.env, fields.Date.to_string(date_to))
+                string = _('From %s\nto  %s') % (dt_from_str, dt_to_str)
+
+        return {
+            'string': string,
+            'period_type': period_type,
+            'mode': mode,
+            'date_from': date_from and fields.Date.to_string(date_from) or False,
+            'date_to': fields.Date.to_string(date_to),
+        }
+
+    def format_date(self, options, dt_filter='date'):
+        date_from = fields.Date.from_string(options[dt_filter]['date_from'])
+        date_to = fields.Date.from_string(options[dt_filter]['date_to'])
+        return self._get_dates_period(date_from, date_to, options['date']['mode'])['string']
--- addons/stock/static/src/js/stock_report.js
+++ addons/stock/static/src/js/stock_report.js
@@ -2,27 +2,147 @@ odoo.define('stock.stock_report_new', function (require) {
 "use strict";
 
 var core = require('web.core');
+var Context = require('web.Context');
 var AbstractAction = require('web.AbstractAction');
+var Dialog = require('web.Dialog');
+var datepicker = require('web.datepicker');
+var session = require('web.session');
+var field_utils = require('web.field_utils');
+var RelationalFields = require('web.relational_fields');
+var StandaloneFieldManagerMixin = require('web.StandaloneFieldManagerMixin');
+var { WarningDialog } = require("@web/legacy/js/_deprecated/crash_manager_warning_dialog");
+var Widget = require('web.Widget');
 
+var QWeb = core.qweb;
+var _t = core._t;
+
+var M2MFilters = Widget.extend(StandaloneFieldManagerMixin, {
+    /**
+     * @constructor
+     * @param {Object} fields
+     */
+    init: function (parent, fields, change_event) {
+        this._super.apply(this, arguments);
+        StandaloneFieldManagerMixin.init.call(this);
+        this.fields = fields;
+        this.widgets = {};
+        this.change_event = change_event;
+    },
+    /**
+     * @override
+     */
+    willStart: function () {
+        var self = this;
+        var defs = [this._super.apply(this, arguments)];
+        _.each(this.fields, function (field, fieldName) {
+            defs.push(self._makeM2MWidget(field, fieldName));
+        });
+        return Promise.all(defs);
+    },
+    /**
+     * @override
+     */
+    start: function () {
+        var self = this;
+        var $content = $(QWeb.render("m2mWidgetTable", {fields: this.fields}));
+        self.$el.append($content);
+        _.each(this.fields, function (field, fieldName) {
+            self.widgets[fieldName].appendTo($content.find('#'+fieldName+'_field'));
+        });
+        return this._super.apply(this, arguments);
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * This method will be called whenever a field value has changed and has
+     * been confirmed by the model.
+     *
+     * @private
+     * @override
+     * @returns {Promise}
+     */
+    _confirmChange: function () {
+        var self = this;
+        var result = StandaloneFieldManagerMixin._confirmChange.apply(this, arguments);
+        var data = {};
+        _.each(this.fields, function (filter, fieldName) {
+            data[fieldName] = self.widgets[fieldName].value.res_ids;
+        });
+        this.trigger_up(this.change_event, data);
+        return result;
+    },
+    /**
+     * This method will create a record and initialize M2M widget.
+     *
+     * @private
+     * @param {Object} fieldInfo
+     * @param {string} fieldName
+     * @returns {Promise}
+     */
+    _makeM2MWidget: function (fieldInfo, fieldName) {
+        var self = this;
+        var options = {};
+        options[fieldName] = {
+            options: {
+                no_create_edit: true,
+                no_create: true,
+            }
+        };
+        return this.model.makeRecord(fieldInfo.modelName, [{
+            fields: [{
+                name: 'id',
+                type: 'integer',
+            }, {
+                name: 'display_name',
+                type: 'char',
+            }],
+            name: fieldName,
+            relation: fieldInfo.modelName,
+            type: 'many2many',
+            value: fieldInfo.value,
+        }], options).then(function (recordID) {
+            self.widgets[fieldName] = new RelationalFields.FieldMany2ManyTags(self,
+                fieldName,
+                self.model.get(recordID),
+                {mode: 'edit',}
+            );
+            self._registerWidget(recordID, fieldName, self.widgets[fieldName]);
+        });
+    },
+});
 
 const StockVariationReport = AbstractAction.extend({
     hasControlPanel: true,
+
+    custom_events: {
+        'product_filter_changed': function(ev) {
+             var self = this;
+             self.report_options.product_ids = ev.data.product_ids;
+             self.report_options.product_categories = ev.data.product_categories;
+             return self.reload().then(function () {
+                 self.$searchview_buttons.find('.stock_product_filter').click();
+             });
+        },
+    },
 
     willStart: async function () {
         const reportsInfoPromise = this._rpc({
             model: 'stock.report.new',
             method: 'get_report_informations',
-            args: [this.id],
+            args: [this.id, this.report_options],
         }).then(res => this.parse_report_informations(res));
         const parentPromise = this._super(...arguments);
         return Promise.all([reportsInfoPromise, parentPromise]);
     },
     parse_report_informations: function(values) {
         this.main_html = values.main_html;
+        this.report_options = values.options;
         this.$searchview_buttons = $(values.searchview_html);
     },
     start: async function() {
-        debugger;
         this.controlPanelProps.cp_content = {
             $searchview_buttons: this.$searchview_buttons,
         };
@@ -32,6 +152,7 @@ const StockVariationReport = AbstractAction.extend({
 
     render: function() {
         this.render_template();
+        this.render_searchview_buttons();
     },
 
     render_template: function() {
@@ -54,7 +175,7 @@ const StockVariationReport = AbstractAction.extend({
         return this._rpc({
                 model: 'stock.report.new',
                 method: 'get_report_informations',
-                args: [self.id],
+                args: [self.id, self.report_options],
                 context: self.odoo_context,
             })
             .then(function(result){
@@ -62,6 +183,43 @@ const StockVariationReport = AbstractAction.extend({
                 self.render();
                 return self.update_cp();
             });
+    },
+
+    render_searchview_buttons: function() {
+        var self = this;
+        // product filter
+        if (this.report_options.product) {
+            if (!this.products_m2m_filter) {
+                var fields = {};
+                if ('product_ids' in this.report_options) {
+                    fields['product_ids'] = {
+                        label: _t('Products'),
+                        modelName: 'product.product',
+                        value: this.report_options.product_ids.map(Number),
+                    };
+                }
+                if ('product_categories' in this.report_options) {
+                    fields['product_categories'] = {
+                        label: _t('Product Category'),
+                        modelName: 'product.category',
+                        value: this.report_options.product_categories.map(Number),
+                    };
+                }
+                if (!_.isEmpty(fields)) {
+                    this.products_m2m_filter = new M2MFilters(this, fields, 'product_filter_changed');
+                    this.products_m2m_filter.appendTo(this.$searchview_buttons.find('.js_stock_product_m2m'));
+                }
+            } else {
+                this.$searchview_buttons.find('.js_stock_product_m2m').append(this.products_m2m_filter.$el);
+            }
+        }
+
+        // events
+        this.$searchview_buttons.find('.js_stock_reports_one_choice_filter').click(function (event) {
+            var warehouse_id = $(this).data('filter');
+            self.report_options[warehouse_id] = $(this).data('warehouse_id');
+            self.reload();
+        });
     },
 
 });
