4823c85b22aa9e65f0e2e5577a286723c0262d9d
Géry Debongnie <ged@odoo.com>
2023-02-07 09:29:13 +0100

    [REF] web: rewrite module loader

--- addons/web/static/src/env.js
+++ addons/web/static/src/env.js
@@ -62,7 +62,6 @@ export async function startServices(env) {
     // one another are added to the registry, they're all present before we
     // start them regardless of the order they're added to the registry.
     await Promise.resolve();
-
     const toStart = new Set();
     serviceRegistry.addEventListener("UPDATE", async (ev) => {
         // Wait for all synchronous code so that if new services that depend on
