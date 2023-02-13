a34be0b272e753f6bccbac5ee97f26cebdef75c8
stefanorigano (SRI) <sri@odoo.com>
2023-02-13 11:53:17 +0100

    [REF][TMP] web, mail: top-menu design
    TODO: review mobile and community
    
    task-2818586

--- odoo/addons/test_apikeys/static/tests/apikey_flow.js
+++ odoo/addons/test_apikeys/static/tests/apikey_flow.js
@@ -9,7 +9,7 @@ registry.category("web_tour.tours").add('apikeys_tour_setup', {
     url: '/web?debug=1', // Needed as API key part is now only displayed in debug mode
     steps: [{
     content: 'Open user account menu',
-    trigger: '.o_user_menu .oe_topbar_name',
+    trigger: '.o_user_menu .dropdown-toggle',
     run: 'click',
 }, {
     content: "Open preferences / profile screen",
@@ -58,7 +58,7 @@ registry.category("web_tour.tours").add('apikeys_tour_setup', {
     }
 }, {
     content: 'Re-open preferences',
-    trigger: '.o_user_menu .oe_topbar_name',
+    trigger: '.o_user_menu .dropdown-toggle',
 }, {
     trigger: '[data-menu=settings]',
 }, {
@@ -76,7 +76,7 @@ registry.category("web_tour.tours").add('apikeys_tour_teardown', {
     url: '/web?debug=1', // Needed as API key part is now only displayed in debug mode
     steps: [{
     content: 'Open preferences',
-    trigger: '.o_user_menu .oe_topbar_name',
+    trigger: '.o_user_menu .dropdown-toggle',
 }, {
     trigger: '[data-menu=settings]',
 }, {
@@ -96,7 +96,7 @@ registry.category("web_tour.tours").add('apikeys_tour_teardown', {
     trigger: 'button:contains(Confirm Password)',
 }, {
     content: 'Re-open preferences again',
-    trigger: '.o_user_menu .oe_topbar_name',
+    trigger: '.o_user_menu .dropdown-toggle',
 }, {
     trigger: '[data-menu=settings]',
 }, {
