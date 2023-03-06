74527ea779f7471ed6ec7cd6833776b3b9c71127
Jorge Pinna Puissant <jpp@odoo.com>
2023-03-06 15:31:51 +0100

    TEST Validation

--- addons/web/static/src/views/fields/standard_field_props.js
+++ addons/web/static/src/views/fields/standard_field_props.js
@@ -1,9 +1,12 @@
 /** @odoo-module **/
 
+import { Record as BasicRelationalModelRecord } from "../basic_relational_model";
+import { Record as RelationalModelRecord } from "../relational_model";
+
 export const standardFieldProps = {
     id: { type: String, optional: true },
     name: { type: String, optional: true },
     readonly: { type: Boolean, optional: true },
-    record: { type: Object },
+    record: { type: [RelationalModelRecord, BasicRelationalModelRecord] },
     value: true,
 };
--- addons/web/static/src/views/widgets/standard_widget_props.js
+++ addons/web/static/src/views/widgets/standard_widget_props.js
@@ -1,6 +1,9 @@
 /** @odoo-module **/
 
+import { Record as BasicRelationalModelRecord } from "../basic_relational_model";
+import { Record as RelationalModelRecord } from "../relational_model";
+
 export const standardWidgetProps = {
     readonly: { type: Boolean, optional: true },
-    record: { type: Object },
+    record: { type: [RelationalModelRecord, BasicRelationalModelRecord] },
 };
