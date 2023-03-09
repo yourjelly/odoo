60c99d11a7ee5d453c660a3beea28b5078f8729d
Géry Debongnie <ged@odoo.com>
2023-03-09 13:24:59 +0100

    [IMP] web: update owl from v2.0.7 to v2.0.9
    This update contains the following commits:
    
    [IMP] implement .alike suffix on props
    [IMP] release: add version number on App
    [IMP] app: add name as a config option
    [FIX] runtime, compiler: fix refs getting set or unset incorrectly
    [FIX] compiler: call translate function with correct string
    [FIX] compiler: properly handle readonly attribute/readOnly property
    [REF] blockdom,compiler: implement properties
    [REF] tests: move properties tests in own file
    [FIX] compiler: dynamic value on inputs doesn't turn 0 into empty string
    [FIX] components: do not crash when binding anonymous function
    
    More details at: https://github.com/odoo/owl/releases/tag/v2.0.9
    
    Note that this owl update required a few adaptations in Odoo code. The
    main problem was that some code would access references after the
    component was unmounted. However, Owl is now stricter and properly
    remove the reference.

--- addons/web/static/src/core/dropdown/dropdown.js
+++ addons/web/static/src/core/dropdown/dropdown.js
@@ -234,7 +234,7 @@ export class Dropdown extends Component {
      * @param {DropdownStateChangedPayload} args
      */
     onDropdownStateChanged(args) {
-        if (this.rootRef.el.contains(args.emitter.rootRef.el)) {
+        if (!this.rootRef.el || this.rootRef.el.contains(args.emitter.rootRef.el)) {
             // Do not listen to events emitted by self or children
             return;
         }
