4b81d3fb5e0e058c2df4c75c10d6e1a5a6bef430
Didier (did) <did@odoo.com>
2023-03-13 14:49:13 +0100

    [IMP] mail: sort activities by state and deadline

--- addons/mail/static/src/new/web/thread_model_patch.js
+++ addons/mail/static/src/new/web/thread_model_patch.js
@@ -9,8 +9,15 @@ patch(Thread.prototype, "mail/web", {
      * @returns {import("@mail/new/web/activity/activity_model").Activity[]}
      */
     get activities() {
-        return Object.values(this._store.activities).filter((activity) => {
-            return activity.res_model === this.model && activity.res_id === this.id;
-        });
+        return Object.values(this._store.activities)
+            .filter((activity) => {
+                return activity.res_model === this.model && activity.res_id === this.id;
+            })
+            .sort(function (a, b) {
+                if (a.date_deadline === b.date_deadline) {
+                    return a.id - b.id;
+                }
+                return a.date_deadline < b.date_deadline ? -1 : 1;
+            });
     },
 });
