d988371d55497e65bd991a80254ea317f0b81d8a
Alexandre Kühn <aku@odoo.com>
2023-03-14 15:24:03 +0100

    [FIX] im_livechat: make runbot green (typo in livechat)

--- addons/im_livechat/static/src/new/discuss/sidebar.js
+++ addons/im_livechat/static/src/new/discuss/sidebar.js
@@ -5,8 +5,8 @@ import { patch } from "@web/core/utils/patch";
 
 patch(Sidebar.prototype, "im_livechat", {
     get shouldDisplayLivechatCategory() {
-        return this.store.discuss.livechat.threads.some((localId) => {
-            this.store.threads[localId]?.is_pinned;
-        });
+        return this.store.discuss.livechat.threads.some(
+            (localId) => this.store.threads[localId]?.is_pinned
+        );
     },
 });
