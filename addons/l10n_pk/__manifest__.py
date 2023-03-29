38092c7ff3be4150e29e23c3de0c0edb9fda319e
rito-odoo <rito@odoo.com>
2023-03-29 12:34:13 +0400

    [ADD] 16.0 pakistan report

--- addons/l10n_pk/__manifest__.py
+++ addons/l10n_pk/__manifest__.py
@@ -15,6 +15,7 @@
         'data/account_tax_group.xml',
         'data/account_tax_template_data.xml',
         'data/account_chart_template_configure_data.xml',
+        'data/account_tax_report_data.xml',
     ],
     'demo': [
         'demo/demo_company.xml'
