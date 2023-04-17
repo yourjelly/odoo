ba517f9c652fe4c49a4daadff5edea8f9ff5fa12
Michael (mcm) <mcm@odoo.com>
2023-04-17 08:52:54 +0200

    lint

--- addons/payment_demo/static/src/js/payment_demo_mixin.js
+++ addons/payment_demo/static/src/js/payment_demo_mixin.js
@@ -1,27 +1,27 @@
 /** @odoo-module **/
 
-    import rpc from "web.rpc";
+import rpc from "web.rpc";
 
-    export const paymentDemo = {
-        /**
-         * Simulate a feedback from a payment provider and redirect the customer to the status page.
-         *
-         * @private
-         * @param {object} processingValues - The processing values of the transaction
-         * @return {Promise}
-         */
-        async processDemoPayment(processingValues) {
-            const customerInput = document.getElementById('customer_input').value;
-            const simulatedPaymentState = document.getElementById('simulated_payment_state').value;
+export const paymentDemo = {
+    /**
+     * Simulate a feedback from a payment provider and redirect the customer to the status page.
+     *
+     * @private
+     * @param {object} processingValues - The processing values of the transaction
+     * @return {Promise}
+     */
+    async processDemoPayment(processingValues) {
+        const customerInput = document.getElementById("customer_input").value;
+        const simulatedPaymentState = document.getElementById("simulated_payment_state").value;
 
-            await rpc.query({
-                route: '/payment/demo/simulate_payment',
-                params: {
-                    'reference': processingValues.reference,
-                    'payment_details': customerInput,
-                    'simulated_state': simulatedPaymentState,
-                },
-            });
-            window.location = '/payment/status';
-        },
-    };
+        await rpc.query({
+            route: "/payment/demo/simulate_payment",
+            params: {
+                reference: processingValues.reference,
+                payment_details: customerInput,
+                simulated_state: simulatedPaymentState,
+            },
+        });
+        window.location = "/payment/status";
+    },
+};
