efaf44897be7e884674e188b599cfa5f7b70c487
Arnold Moyaux <arm@odoo.com>
2023-04-17 14:30:24 +0200

    [WIP] mrp: move display to enterprise

--- addons/mrp/models/stock_picking.py
+++ addons/mrp/models/stock_picking.py
@@ -26,14 +26,6 @@ class StockPickingType(models.Model):
         help="Allow automatic consumption of tracked components that are reserved",
     )
 
-    def action_mrp_overview(self):
-        routing_count = self.env['stock.picking.type'].search_count([('code', '=', 'mrp_operation')])
-        if routing_count == 1:
-            return self.env['ir.actions.actions']._for_xml_id('mrp.action_mrp_display')
-        action = self.env['ir.actions.actions']._for_xml_id('mrp.mrp_stock_picking_type_action')
-        action['domain'] = [('code', '=', 'mrp_operation')]
-        return action
-
     def _get_mo_count(self):
         mrp_picking_types = self.filtered(lambda picking: picking.code == 'mrp_operation')
         remaining = (self - mrp_picking_types)
