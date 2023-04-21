15462905fb29ca43a300e5ad1ae80c2071943205
miad-odoo <miad@odoo.com>
2023-04-21 10:49:07 +0200

    add tests

--- addons/website_mass_mailing/models/mass_mailing.py
+++ addons/website_mass_mailing/models/mass_mailing.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class MassMailing(models.Model):
     _inherit = "mailing.mailing"
 
-    website_id = fields.Integer()
+    website_id = fields.Many2one('website', string="Website")
