aabf846890173fa4cf67bd08a1cb83a1f8ec7e91
Jérémy Hennecart (jeh) <jeh@odoo.com>
2023-04-26 16:48:51 +0200

    wip

--- addons/website_event/models/event_tag_category.py
+++ addons/website_event/models/event_tag_category.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import fields, models
 
 
 class EventTagCategory(models.Model):
     _name = 'event.tag.category'
     _inherit = ['event.tag.category', 'website.published.mixin']
 
+    def default_get(self, fields_list):
+        result = super().default_get(fields_list)
+        if self.env.context.get('propagate_website_id'):
+            result['website_id'] = self.env.context.get('propagate_website_id')
+        return result
+
     def _default_is_published(self):
         return True
+
+    website_id = fields.Many2one('website', string='Website')
