08dc78faab231b2a5a672aaf2b225644bc6ab140
Damien Bouvy <dbo@odoo.com>
2023-05-02 13:40:29 +0200

    [FIX] web: coherent breakpoints for searchbar
    Hide the searhbar by default  once we go under the LG breakpoint,
    so that it does not cause re-flows when a user selects a record's
    checkbox in list view.

--- addons/web/static/src/search/search_bar/search_bar.js
+++ addons/web/static/src/search/search_bar/search_bar.js
@@ -9,6 +9,7 @@ import { fuzzyTest } from "@web/core/utils/search";
 import { SearchBarMenu } from "../search_bar_menu/search_bar_menu";
 import { useDebounced } from "@web/core/utils/timing";
 import { browser } from "@web/core/browser/browser";
+import { SIZES } from "@web/core/ui/ui_service";
 
 import { Component, onMounted, onWillUnmount, useExternalListener, useRef, useState } from "@odoo/owl";
 const parsers = registry.category("parsers");
@@ -23,13 +24,14 @@ export class SearchBar extends Component {
         this.fields = this.env.searchModel.searchViewFields;
         this.searchItemsFields = this.env.searchModel.getSearchItems((f) => f.type === "field");
         this.root = useRef("root");
+        this.ui = useService("ui");
 
         // core state
         this.state = useState({
             expanded: [],
             focusedIndex: 0,
             query: "",
-            showSearchBar: !this.env.isSmall,
+            showSearchBar: this.ui.size > SIZES.SM,
         });
 
         // derived state
@@ -56,10 +58,6 @@ export class SearchBar extends Component {
         this.onResize = useDebounced(this.onResize, 200);
         onMounted(() => browser.addEventListener("resize", this.onResize));
         onWillUnmount(() => browser.removeEventListener("resize", this.onResize));
-    }
-
-    onResize() {
-        this.state.showSearchBar = !this.env.isSmall;
     }
 
     /**
@@ -416,6 +414,10 @@ export class SearchBar extends Component {
     onItemMousemove(focusedIndex) {
         this.state.focusedIndex = focusedIndex;
         this.inputRef.el.focus();
+    }
+
+    onResize() {
+        this.state.showSearchBar = this.ui.size > SIZES.SM;
     }
 
     /**
