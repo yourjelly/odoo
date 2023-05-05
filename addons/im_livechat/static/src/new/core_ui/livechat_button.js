23fddaa551fc6a24b177d88857f74a7f9fe2000d
tsm-odoo <tsm@odoo.com>
2023-05-05 11:29:31 +0200

    [IMP] im_livechat: don't wait for livechat init

--- addons/im_livechat/static/src/new/core_ui/livechat_button.js
+++ addons/im_livechat/static/src/new/core_ui/livechat_button.js
@@ -23,6 +23,7 @@ export class LivechatButton extends Component {
 
     get isShown() {
         return (
+            this.livechatService.initialized &&
             this.livechatService.available &&
             this.livechatService.state !== SESSION_STATE.CLOSED &&
             this.store.chatWindows.length === 0
