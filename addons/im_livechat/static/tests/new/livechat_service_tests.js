d4d18f6764a8662221531e9dd081235901771393
tsm-odoo <tsm@odoo.com>
2023-05-08 13:53:50 +0200

    [IMP] im_livechat: improve previous operator test

--- addons/im_livechat/static/tests/new/livechat_service_tests.js
+++ addons/im_livechat/static/tests/new/livechat_service_tests.js
@@ -9,13 +9,22 @@ import {
     setCookie,
 } from "@im_livechat/../tests/helpers/new/test_utils";
 
-import { nextTick } from "@web/../tests/helpers/utils";
+import { Command } from "@mail/../tests/helpers/command";
 
 QUnit.module("livechat service");
 
 QUnit.test("persisted session history", async (assert) => {
     const pyEnv = await startServer();
-    const channelId = await loadDefaultConfig();
+    const livechatChannelId = await loadDefaultConfig();
+    const channelId = pyEnv["discuss.channel"].create({
+        channel_member_ids: [
+            Command.create({ partner_id: pyEnv.currentPartnerId }),
+            Command.create({ partner_id: pyEnv.publicPartnerId }),
+        ],
+        channel_type: "livechat",
+        livechat_channel_id: livechatChannelId,
+        livechat_operator_id: pyEnv.currentPartnerId,
+    });
     const [channelInfo] = pyEnv.mockServer._mockDiscussChannelChannelInfo([channelId]);
     setCookie("im_livechat_session", JSON.stringify(channelInfo));
     pyEnv["mail.message"].create({
@@ -30,23 +39,14 @@ QUnit.test("persisted session history", async (assert) => {
     assert.containsOnce(root, ".o-mail-Message:contains(Old message in history)");
 });
 
-QUnit.test("previous operator used when available", async (assert) => {
+QUnit.test("previous operator prioritized", async (assert) => {
     const pyEnv = await startServer();
-    const channelId = await loadDefaultConfig();
-    const [channel] = pyEnv["discuss.channel"].searchRead([["id", "=", channelId]]);
-    setCookie("im_livechat_previous_operator_pid", JSON.stringify(channel.livechat_operator_id[0]));
-    await start({
-        mockRPC(route, args) {
-            if (route === "/im_livechat/get_session") {
-                assert.step("get_session");
-                assert.strictEqual(
-                    parseInt(args.previous_operator_id),
-                    channel.livechat_operator_id[0]
-                );
-            }
-        },
-    });
+    const livechatChannelId = await loadDefaultConfig();
+    const userId = pyEnv["res.users"].create({ name: "John Doe", im_status: "online" });
+    const previousOperatorId = pyEnv["res.partner"].create({ user_ids: [userId] });
+    pyEnv["im_livechat.channel"].write([livechatChannelId], { user_ids: [Command.link(userId)] });
+    setCookie("im_livechat_previous_operator_pid", JSON.stringify(previousOperatorId));
+    const { root } = await start();
     await click(".o-livechat-LivechatButton");
-    await nextTick();
-    assert.verifySteps(["get_session"]);
+    assert.containsOnce(root, ".o-mail-Message-author:contains(John Doe)");
 });
