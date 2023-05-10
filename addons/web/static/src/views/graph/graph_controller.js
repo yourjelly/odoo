955f756ba24d35ffc2293aa1265d3646ba5af0d4
Romeo Fragomeli <rfr@odoo.com>
2023-05-10 16:54:42 +0200

    [FIX] web: refactor search bar toggler

--- addons/web/static/src/views/graph/graph_controller.js
+++ addons/web/static/src/views/graph/graph_controller.js
@@ -5,6 +5,7 @@ import { useModel } from "@web/views/model";
 import { standardViewProps } from "@web/views/standard_view_props";
 import { useSetupView } from "@web/views/view_hook";
 import { SearchBar } from "@web/search/search_bar/search_bar";
+import { useSearchBarToggler } from "@web/search/search_bar/search_bar_toggler";
 import { CogMenu } from "@web/search/cog_menu/cog_menu";
 
 import { Component, useRef } from "@odoo/owl";
@@ -20,6 +21,7 @@ export class GraphController extends Component {
             },
             getContext: () => this.getContext(),
         });
+        this.searchBarToggler = useSearchBarToggler();
     }
 
     /**
