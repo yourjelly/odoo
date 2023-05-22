1570a1472af5cae0187d79538091212f2eba2e34
Merel Geens (mege) <mege@odoo.com>
2023-05-22 19:15:23 +0200

    Make getting stock moves for purchase orders include returns for invoices and regular deliveries for credit notes

--- addons/purchase_stock/models/account_invoice.py
+++ addons/purchase_stock/models/account_invoice.py
@@ -177,8 +177,7 @@ class AccountMove(models.Model):
         """ Overridden from stock_account.
         Returns the stock moves associated to this invoice."""
         rslt = super(AccountMove, self)._stock_account_get_last_step_stock_moves()
-        for invoice in self.filtered(lambda x: x.move_type == 'in_invoice'):
-            rslt += invoice.mapped('invoice_line_ids.purchase_line_id.move_ids').filtered(lambda x: x.state == 'done' and x.location_id.usage == 'supplier')
-        for invoice in self.filtered(lambda x: x.move_type == 'in_refund'):
-            rslt += invoice.mapped('invoice_line_ids.purchase_line_id.move_ids').filtered(lambda x: x.state == 'done' and x.location_dest_id.usage == 'supplier')
+        for invoice in self.filtered(lambda x: x.move_type in ['in_invoice', 'in_refund']):
+            rslt += invoice.mapped('invoice_line_ids.purchase_line_id.move_ids').filtered(
+                lambda x: x.state == 'done' and (x.location_id.usage == 'supplier' or x.location_dest_id.usage == 'supplier'))
         return rslt
