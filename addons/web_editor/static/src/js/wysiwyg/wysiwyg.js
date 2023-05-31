c47595b055dc7461d8c3c95998b8c72d20263f50
David Beguin <dbe@odoo.com>
2023-05-31 14:23:08 +0200

    make first line h4 by default and adapt/align icon

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -2090,7 +2090,7 @@ const Wysiwyg = Widget.extend({
                     iconClass: iconClass
                 }))[0]
                 this.odooEditor.execCommand('insert', bannerHTML);
-                setSelection(this.odooEditor._latestComputedSelectionInEditable.focusNode.previousSibling.querySelector('p'), 0);
+                setSelection(this.odooEditor._latestComputedSelectionInEditable.focusNode.previousSibling.querySelector('.o_editor_banner > div'), 0);
             },
         }
     },
