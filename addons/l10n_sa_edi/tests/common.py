7cc1453414601c30af977eeba72d3df6f0b4e3da
smdc-odoo <smdc@odoo.com>
2023-06-06 16:43:33 +0200

    [IMP] l10n_sa_edi: Add tests compliance files

--- addons/l10n_sa_edi/tests/common.py
+++ addons/l10n_sa_edi/tests/common.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 from datetime import datetime
-from pytz import timezone
 
 from odoo import Command
 from odoo.tests import tagged
@@ -13,10 +12,9 @@ class TestSaEdiCommon(AccountEdiTestCommon):
     @classmethod
     def setUpClass(cls, chart_template_ref='l10n_sa.sa_chart_template_standard', edi_format_ref='l10n_sa_edi.edi_sa_zatca'):
         super().setUpClass(chart_template_ref=chart_template_ref, edi_format_ref=edi_format_ref)
-
-        cls.frozen_date = datetime(year=2022, month=9, day=5, hour=8, minute=20, second=2, tzinfo=timezone('Etc/GMT-3'))
         # Setup company
         cls.company = cls.company_data['company']
+        cls.company.name = 'SA Company Test'
         cls.company.country_id = cls.env.ref('base.sa')
         cls.company.email = "info@company.saexample.com"
         cls.company.phone = '+966 51 234 5678'
@@ -27,8 +25,9 @@ class TestSaEdiCommon(AccountEdiTestCommon):
         cls.company.l10n_sa_additional_identification_number = '2525252525252'
         cls.company.l10n_sa_additional_identification_scheme = 'CRN'
         cls.company.vat = '311111111111113'
+        cls.company.l10n_sa_private_key = cls.env['res.company']._l10n_sa_generate_private_key()
         cls.company.state_id = cls.env['res.country.state'].create({
-            'name': 'riyadh',
+            'name': 'Riyadh',
             'code': 'RYA',
             'country_id': cls.company.country_id.id
         })
@@ -74,6 +73,17 @@ class TestSaEdiCommon(AccountEdiTestCommon):
             'lang': 'en_US',
         })
 
+        cls.partner_sa_simplified = cls.env['res.partner'].create({
+            'name': 'Mohammed Ali',
+            'ref': 'Mohammed Ali',
+            'country_id': cls.env.ref('base.sa').id,
+            'l10n_sa_additional_identification_scheme': 'MOM',
+            'l10n_sa_additional_identification_number': '3123123213131',
+            'state_id': cls.company.state_id.id,
+            'company_type': 'person',
+            'lang': 'en_US',
+        })
+
         # 15% tax
         cls.tax_15 = cls.env['account.tax'].search([('company_id', '=', cls.company.id), ('name', '=', 'Sales Tax 15%')])
 
@@ -91,41 +101,51 @@ class TestSaEdiCommon(AccountEdiTestCommon):
             'default_code': 'P0002',
         })
 
+        cls.product_burger = cls.env['product.product'].create({
+            'name': 'Burger',
+            'uom_id': cls.env.ref('uom.product_uom_unit').id,
+            'standard_price': 265.00,
+        })
+
+        cls.remove_ubl_extensions_xpath = '''<xpath expr="//*[local-name()='UBLExtensions']" position="replace"/>'''
 
     def _create_invoice(self, **kwargs):
         vals = {
-            'name': 'INV/2022/00014',
+            'name': kwargs['name'],
             'move_type': 'out_invoice',
             'company_id': self.company,
-            'partner_id': self.partner_us,
-            'invoice_date_due': '2022-09-22',
+            'partner_id': kwargs['partner_id'],
+            'invoice_date': kwargs['date'],
+            'invoice_date_due': kwargs['date_due'],
             'currency_id': self.company.currency_id,
             'invoice_line_ids': [Command.create({
-                'product_id': self.product_a.id,
-                'price_unit': 320.0,
+                'product_id': kwargs['product_id'].id,
+                'price_unit': kwargs['price'],
+                'quantity': kwargs.get('quantity', 1.0),
                 'tax_ids': [Command.set(self.tax_15.ids)],
             }),
             ],
         }
-        vals.update(kwargs)
-        return self.env['account.move'].create(vals)
+        move = self.env['account.move'].create(vals)
+        move.state = 'posted'
+        move.l10n_sa_confirmation_datetime = datetime.now()
+        # move.payment_reference = move.name
+        return move
 
     def _create_debit_note(self, **kwargs):
         invoice = self._create_invoice(**kwargs)
-        invoice.action_post()
 
         debit_note_wizard = self.env['account.debit.note'].with_context(
             {'active_ids': [invoice.id], 'active_model': 'account.move', 'default_copy_lines': True}).create({
                 'reason': 'Totes forgot'})
         res = debit_note_wizard.create_debit()
         debit_note = self.env['account.move'].browse(res['res_id'])
+        debit_note.l10n_sa_confirmation_datetime = datetime.now()
+        debit_note.state = 'posted'
         return debit_note
 
     def _create_credit_note(self, **kwargs):
         move = self._create_invoice(**kwargs)
-        move.action_post()
-        reverse_move = move._reverse_moves()
-
         move_reversal = self.env['account.move.reversal'].with_context(active_model="account.move", active_ids=move.ids).create({
             'reason': 'no reason',
             'refund_method': 'refund',
@@ -133,4 +153,6 @@ class TestSaEdiCommon(AccountEdiTestCommon):
         })
         reversal = move_reversal.reverse_moves()
         reverse_move = self.env['account.move'].browse(reversal['res_id'])
+        reverse_move.l10n_sa_confirmation_datetime = datetime.now()
+        reverse_move.state = 'posted'
         return reverse_move
