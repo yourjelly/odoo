4f0831d2faf5d7ef2c7a593e5f6b48c0ff811618
Nicolas Bayet <nby@odoo.com>
2023-06-14 15:08:08 +0200

    remove wysiwyg_iframe_content.js

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg_iframe.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg_iframe.js
@@ -3,7 +3,6 @@
 import { Wysiwyg } from '@web_editor/js/wysiwyg/wysiwyg';
 import { patch } from "@web/core/utils/patch";
 import { getBundle } from "@web/core/assets";
-import { getWysiwygIframeContent } from "@web_editor/js/wysiwyg/wysiwyg_iframe_content";
 import { isMobileOS } from "@web/core/browser/feature_detection";
 
 var promiseJsAssets;
@@ -182,3 +181,67 @@ patch(Wysiwyg.prototype, 'wysiwyg_iframe.js', {
         }
     },
 });
+
+function getWysiwygIframeContent(params) {
+    const assets = {
+        cssLibs: [],
+        cssContents: [],
+        jsLibs: [],
+        jsContents: [],
+    };
+    for (const asset of params.assets) {
+        for (const cssLib of asset.cssLibs) {
+            assets.cssLibs.push(`<link type="text/css" rel="stylesheet" href="${cssLib}"/>`);
+        }
+        for (const cssContent of asset.cssContents) {
+            assets.cssContents.push(`<style type="text/css">${cssContent}</style>`);
+        }
+        for (const jsLib of asset.jsLibs) {
+            assets.jsLibs.push(`<script type="text/javascript" src="${jsLib}"/>`);
+        }
+        for (const jsContent of asset.jsContents) {
+            if (jsContent.indexOf('inline asset') !== -1) {
+                assets.jsContents.push(`<script type="text/javascript">${jsContent}</script>`);
+            }
+        }
+    }
+    return `
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+        ${assets.cssLibs.join('\n')}
+        ${assets.cssContents.join('\n')}
+        ${assets.jsLibs.join('\n')}
+        ${assets.jsContents.join('\n')}
+
+        <script type="text/javascript">
+            odoo.define('web.session', [], function () {
+                return window.top.odoo.__DEBUG__.services['web.session'];
+            });
+
+            odoo.define('root.widget', ['web.Widget'], function (require) {
+                'use strict';
+                var Widget = require('web.Widget');
+                var widget = new Widget();
+                widget.appendTo(document.body);
+                return widget;
+            });
+
+            odoo.define('web.core.top', ['web.core'], function (require) {
+                var core = require('web.core');
+                core.qweb.templates = window.top.odoo.__DEBUG__.services['web.core'].qweb.templates;
+            });
+        </script>
+    </head>
+    <body class="o_in_iframe">
+        <div id="iframe_target"/>
+        <script type="text/javascript">
+            odoo.define('web_editor.wysiwyg.iniframe', [], function (require) {
+                'use strict';
+                if (window.top.${params.updateIframeId}) {
+                    window.top.${params.updateIframeId}(${params.avoidDoubleLoad});
+                }
+            });
+        </script>
+    </body>`;
+}
