7645a16c9c8b766487926bacdd15b13266acbc2c
Kevin Baptiste <kba@odoo.com>
2023-06-21 09:30:43 +0200

    m

--- addons/hr_recruitment/models/hr_job.py
+++ addons/hr_recruitment/models/hr_job.py
@@ -44,9 +44,6 @@ class Job(models.Model):
         'hr.employee', related='department_id.manager_id', string="Department Manager",
         readonly=True, store=True)
     user_id = fields.Many2one('res.users', "Recruiter", domain="[('share', '=', False), ('company_ids', 'in', company_id)]", tracking=True, help="The Recruiter will be the default value for all Applicants Recruiter's field in this job position. The Recruiter is automatically added to all meetings with the Applicant.")
-    hr_responsible_id = fields.Many2one(
-        'res.users', "HR Responsible", tracking=True,
-        help="Person responsible of validating the employee's contracts.")
     document_ids = fields.One2many('ir.attachment', compute='_compute_document_ids', string="Documents", readonly=True)
     documents_count = fields.Integer(compute='_compute_document_ids', string="Document Count")
     alias_id = fields.Many2one(
