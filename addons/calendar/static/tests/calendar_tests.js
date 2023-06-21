fb5f6b23c8c872610be968a1aeae1a740b8bed79
FrancoisGe <fge@odoo.com>
2023-06-21 14:36:15 +0200

    fix Many2ManyAttendee

--- addons/calendar/static/tests/calendar_tests.js
+++ addons/calendar/static/tests/calendar_tests.js
@@ -49,7 +49,7 @@ QUnit.module(
         },
     },
     function () {
-        QUnit.tttt("Many2ManyAttendee: basic rendering", async function (assert) {
+        QUnit.test("Many2ManyAttendee: basic rendering", async function (assert) {
             await makeView({
                 type: "form",
                 resModel: "event",
