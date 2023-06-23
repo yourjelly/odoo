4078379eaa1f6078ea29e9ee69a3e3a8573d40d5
Surabhi Varma <suva@odoo.com>
2023-06-23 17:53:25 +0530

    [IMP] l10n_in_ewaybill: ewaybill for stock
    In this commit
    =================
    - Added a new button in stock picking so that ewaybill can be created for stock
    movement
    - Created a view to print the ewaybill
    - Added the cancel, update part-b, update transporter and extend validity
    functionality to the ewaybill
    
    task-3376530

---
+++ addons/l10n_in_ewaybill/wizard/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import ewaybill_update_part_b
+from . import ewaybill_update_transporter
+from . import ewaybill_extend_validity
+
