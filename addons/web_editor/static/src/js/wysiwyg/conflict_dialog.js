c116d5bd2457e9fe84d7109d9252fc44a00e7bf1
Nicolas Bayet <nby@odoo.com>
2023-06-26 14:57:42 +0200

    eslint

--- addons/web_editor/static/src/js/wysiwyg/conflict_dialog.js
+++ addons/web_editor/static/src/js/wysiwyg/conflict_dialog.js
@@ -1,6 +1,6 @@
 /* @odoo-module */
 
-import { Component, xml } from "@odoo/owl";
+import { Component } from "@odoo/owl";
 import { Dialog } from "@web/core/dialog/dialog";
 
 export class ConflictDialog extends Component {
