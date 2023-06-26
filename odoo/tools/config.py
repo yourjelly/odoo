f1b6f2d054f434df172f8065db1d5edd657cc7bf
Julien Castiaux <juc@odoo.com>
2023-06-26 16:15:45 +0200

    [REF] core: ConfigCleaner(2) make root_path a property
    This commit is part of a larger refactor, see associated PR.
    
    While `root_path` was defined as an option, in reality it cannot be set
    nor via the CLI, nor via the config file. Its value is fully defined at
    runtime and isn't modified anywhere.

--- odoo/tools/config.py
+++ odoo/tools/config.py
@@ -74,7 +74,6 @@ class configmanager:
             'csv_internal_sep': ',',
             'publisher_warranty_url': 'http://services.openerp.com/publisher-warranty/',
             'reportgz': False,
-            'root_path': None,
             'websocket_keep_alive_timeout': 3600,
             'websocket_rate_limit_burst': 10,
             'websocket_rate_limit_delay': 0.2,
@@ -82,7 +81,7 @@ class configmanager:
 
         # Not exposed in the configuration file.
         self.blacklist_for_save = set([
-            'publisher_warranty_url', 'load_language', 'root_path',
+            'publisher_warranty_url', 'load_language',
             'init', 'save', 'config', 'update', 'stop_after_init', 'dev_mode', 'shell_interface',
             'longpolling_port',
         ])
@@ -523,13 +522,12 @@ class configmanager:
             elif isinstance(self.options[arg], str) and self.casts[arg].type in optparse.Option.TYPE_CHECKER:
                 self.options[arg] = optparse.Option.TYPE_CHECKER[self.casts[arg].type](self.casts[arg], arg, self.options[arg])
 
-        self.options['root_path'] = self._normalize(os.path.join(os.path.dirname(__file__), '..'))
         if not self.options['addons_path'] or self.options['addons_path']=='None':
             default_addons = []
-            base_addons = os.path.join(self.options['root_path'], 'addons')
+            base_addons = os.path.join(self.root_path, 'addons')
             if os.path.exists(base_addons):
                 default_addons.append(base_addons)
-            main_addons = os.path.abspath(os.path.join(self.options['root_path'], '../addons'))
+            main_addons = os.path.abspath(os.path.join(self.root_path, '../addons'))
             if os.path.exists(main_addons):
                 default_addons.append(main_addons)
             self.options['addons_path'] = ','.join(default_addons)
@@ -722,6 +720,10 @@ class configmanager:
 
     def __getitem__(self, key):
         return self.options[key]
+
+    @property
+    def root_path(self):
+        return self._normalize(os.path.join(os.path.dirname(__file__), '..'))
 
     @property
     def addons_data_dir(self):
