3863b98817d608e2e8f65ec41ee06d37154d5514
pash-odoo <pash@odoo.com>
2023-07-17 18:17:58 +0530

    [IMP] project_todo: prevent deletion/archiving of reminder activity type
    Before this commit, If we create a to-do from activity menu then reminder type
    activity get created. if reminder activity type is archive or deleted then an
    activity with no type is generated.
    Prevented reminder type activity from deletion and archiving so the problem of
    activity with no type is not get created.
    
    task-3196864

--- addons/project_todo/models/mail_activity_type.py
+++ addons/project_todo/models/mail_activity_type.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
-
+from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class MailActivityType(models.Model):
     _inherit = "mail.activity.type"
 
     category = fields.Selection(selection_add=[('reminder', 'Reminder')])
+
+    @api.ondelete(at_uninstall=False)
+    def _unlink_except_reminder(self):
+        if self.env.ref('project_todo.mail_activity_data_reminder') in self:
+            raise UserError("You can not delete Reminder type activity.")
+
+    def action_archive(self):
+        if self.env.ref('project_todo.mail_activity_data_reminder') in self:
+            raise UserError("You can not archive Reminder type activity.")
+        return super().action_archive()
