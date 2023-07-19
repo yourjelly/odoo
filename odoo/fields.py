d9fdc1495c26a20995ef66dcbe2224df6c9eeeef
Aaron Bohy <aab@odoo.com>
2023-07-19 14:34:14 +0200

    Revert "[FIX] web: web_read() on new records with _inherits"
    This reverts commit 8000ca089dd72686d7ea2b251f225b8c24ddd00d.

--- odoo/fields.py
+++ odoo/fields.py
@@ -3037,8 +3037,6 @@ class Many2one(_Relational):
             except MissingError:
                 # Should not happen, unless the foreign key is missing.
                 return False
-        elif self.delegate:
-            return value.id
         else:
             return value._origin.id
 
