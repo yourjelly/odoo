ad6f04f3d600e1e2a0cfa368327b76fc77769ff9
Pierre Pulinckx (pipu) <pipu@odoo.com>
2023-08-04 08:16:42 +0200

    [ADD] web: human readable integer field
    Allow developers to format integers/float
    using "human_readable" options for integer
    field (e.G. 500G instead of 500,000,000,000)
    Use decimals options to choose how many float
    number are displayed.
    Let's see in unit test for examples.
    
    task-3418678

--- addons/web/static/src/views/fields/integer/integer_field.js
+++ addons/web/static/src/views/fields/integer/integer_field.js
@@ -8,23 +8,38 @@ import { useInputField } from "../input_field_hook";
 import { standardFieldProps } from "../standard_field_props";
 import { useNumpadDecimal } from "../numpad_decimal_hook";
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class IntegerField extends Component {
     static template = "web.IntegerField";
     static props = {
         ...standardFieldProps,
         formatNumber: { type: Boolean, optional: true },
+        humanReadable: { type: Boolean, optional: true },
+        decimals: { type: Number, optional: true },
         inputType: { type: String, optional: true },
         step: { type: Number, optional: true },
         placeholder: { type: String, optional: true },
     };
     static defaultProps = {
         formatNumber: true,
+        humanReadable: false,
         inputType: "text",
+        decimals: 0, 
     };
 
+    onFocusIn() {
+        this.state.hasFocus = true
+    }
+
+    onFocusOut() {
+        this.state.hasFocus = false
+    }
+
     setup() {
+        this.state = useState({
+            hasFocus: false, 
+        })
         useInputField({
             getValue: () => this.formattedValue,
             refName: "numpadDecimal",
@@ -40,7 +55,11 @@ export class IntegerField extends Component {
         ) {
             return this.props.record.data[this.props.name];
         }
-        return formatInteger(this.props.record.data[this.props.name]);
+        if (this.props.humanReadable && !this.state.hasFocus) {
+            return formatInteger(this.props.record.data[this.props.name], {humanReadable: true, decimals: this.props.decimals });
+        } else {
+            return formatInteger(this.props.record.data[this.props.name], {humanReadable: false});
+        }
     }
 }
 
@@ -71,9 +90,11 @@ export const integerField = {
     extractProps: ({ attrs, options }) => ({
         formatNumber:
             options?.enable_formatting !== undefined ? Boolean(options.enable_formatting) : true,
+        humanReadable: options?.human_readable !== undefined ? Boolean(options.human_readable) : false,
         inputType: options.type,
         step: options.step,
         placeholder: attrs.placeholder,
+        decimals: options?.decimals > 0 ? +Math.ceil(options.decimals) : 0,
     }),
 };
 
