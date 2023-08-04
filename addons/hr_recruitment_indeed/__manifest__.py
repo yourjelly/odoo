d89efa98ae377f314d1c8e6d23413a4512dec850
Aman Patel <ampa@odoo.com>
2023-08-04 18:35:31 +0530

    [IMP] indeed_hr_recruitment: indeed integration Added code for indeed integration with hr_recruitment
    task-3422081

---
+++ addons/hr_recruitment_indeed/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Indeed Recruitment',
+    'version': '1.1',
+    'category': 'Human Resources/Recruitment',
+    'sequence': 90,
+    'website': 'https://www.odoo.com/app/recruitment',
+    'depends': [
+        'website_hr_recruitment',
+    ],
+    'data': [
+        'data/hr_demo.xml',
+        'data/job_feed_template.xml',
+        'views/res_config_settings_views.xml',
+        'views/hr_job_views.xml',
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
---
+++ addons/hr_recruitment_indeed/models/res_config_settings.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models, api
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = ['res.config.settings']
+
+    module_hr_recruitment_indeed = fields.Boolean(string="Indeed HR Recruitment")
+    indeed_api_token = fields.Char(compute="_compute_indeed_api_token", inverse="_inverse_indeed_api_token")
+    indeed_secret_key = fields.Char(compute="_compute_indeed_secret_key", inverse="_inverse_indeed_secret_key")
+
+    @api.depends('module_hr_recruitment_indeed')
+    def _compute_indeed_api_token(self):
+        indeed_api_token = self.env['ir.config_parameter'].sudo().get_param('hr_recruitment_indeed.indeed_api_token')
+        for setting in self:
+            setting.indeed_api_token = indeed_api_token
+
+    def _inverse_indeed_api_token(self):
+        for setting in self:
+            self.env['ir.config_parameter'].sudo().set_param('hr_recruitment_indeed.indeed_api_token', setting.indeed_api_token)
+
+    @api.depends('module_hr_recruitment_indeed')
+    def _compute_indeed_secret_key(self):
+        indeed_secret_key = self.env['ir.config_parameter'].sudo().get_param('hr_recruitment_indeed.indeed_secret_key')
+        for setting in self:
+            setting.indeed_secret_key = indeed_secret_key
+
+    def _inverse_indeed_secret_key(self):
+        for setting in self:
+            self.env['ir.config_parameter'].sudo().set_param('hr_recruitment_indeed.indeed_secret_key', setting.indeed_secret_key)
