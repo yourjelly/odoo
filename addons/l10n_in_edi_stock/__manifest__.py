4279d3c438f7bc179218c5abb6e9583d169a5475
Bhavin Patel <bat@odoo.com>
2023-09-11 12:40:53 +0530

    Updated __manifest__.py

--- addons/l10n_in_edi_stock/__manifest__.py
+++ addons/l10n_in_edi_stock/__manifest__.py
@@ -7,11 +7,11 @@
     "description": """
 Indian - E-Waybill
 ====================
-To submit E-Waybill through API to the government.
+To submit an E-Waybill through API to the government.
 We use "Tera Software Limited" as GSP
 
 Step 1: First you need to create an API username and password in the E-Waybill portal.
-Step 2: Switch to company related to that GST number
+Step 2: Switch to the company related to that GST number
 Step 3: Set that username and password in Odoo (Goto: Inventory/Configuration -> Settings -> Indian E-Waybill or find "E-Waybill" in search bar)
 Step 4: Repeat steps 1,2,3 for all GSTIN you have in odoo. If you have a multi-company with the same GST number then perform step 1 for the first company only.
     """,
