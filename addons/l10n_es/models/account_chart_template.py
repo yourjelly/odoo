2b9d19dfbd09d46be0d929c495fcedf85946ed7e
Josse Colpaert <jco@odoo.com>
2023-09-19 15:33:05 +0200

    wip

--- addons/l10n_es/models/account_chart_template.py
+++ addons/l10n_es/models/account_chart_template.py
@@ -3,15 +3,21 @@
 
 from odoo import models, Command
 
+from odoo.addons.account.models.chart_template import template
+
 
 class AccountChartTemplate(models.AbstractModel):
-    _inherit = 'account.chart.template'
+    _inherit = ('account.chart.template')
 
-    def _load(self, template_code, company, install_demo):
-        result = super()._load(template_code, company, install_demo)
-        if template_code.startswith('es'):
-            product_21 = self.env.ref('l10n_es.product_dua_valuation_21')
-            default_tax_21 = self.env.ref('account.' + str(company.id) + '_account_tax_template_p_iva0_ibc_group')
-            product_21.with_context(allowed_company_ids=company.ids).supplier_taxes_id = [Command.set(default_tax_21.ids)]
-
-        return result
+    @template('es_common', model='product.product')
+    def _get_product(self):
+        print({
+            'l10n_es.product_dua_valuation_4': {'supplier_taxes_id': [Command.link('account_tax_template_p_iva4_ibc_group')]},
+            'l10n_es.product_dua_valuation_10': {'supplier_taxes_id': [Command.link('account_tax_template_p_iva10_ibc_group')]},
+            'l10n_es.product_dua_valuation_21': {'supplier_taxes_id': [Command.link('account_tax_template_p_iva21_ibc_group')]},
+        })
+        return {
+            'l10n_es.product_dua_valuation_4': {'supplier_taxes_id': [Command.clear(), Command.set(['account_tax_template_p_iva4_ibc_group'])]},
+            'l10n_es.product_dua_valuation_10': {'supplier_taxes_id': [Command.set(['account_tax_template_p_iva10_ibc_group'])]},
+            'l10n_es.product_dua_valuation_21': {'supplier_taxes_id': [Command.link('account_tax_template_p_iva21_ibc_group')]},
+        }
