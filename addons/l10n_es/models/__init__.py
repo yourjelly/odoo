8e2914a195833a7a629e44fde1225100e29b7bb7
Josse Colpaert <jco@odoo.com>
2023-09-19 18:34:32 +0200

    fix

--- addons/l10n_es/models/__init__.py
+++ addons/l10n_es/models/__init__.py
@@ -1,5 +1,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from . import account_chart_template
 from . import template_es_assec
 from . import template_es_common
 from . import template_es_full
