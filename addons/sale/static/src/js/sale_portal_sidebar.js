a3840fd76b257d7baed51aa048afb2a198e49398
Chrysanthe (chgo) <chgo@odoo.com>
2023-10-02 14:09:28 +0200

    [IMP] sale: portal redesign

--- addons/sale/static/src/js/sale_portal_sidebar.js
+++ addons/sale/static/src/js/sale_portal_sidebar.js
@@ -86,7 +86,7 @@ publicWidget.registry.SalePortalSidebar = PortalSidebar.extend({
                         if (!lastUL) {
                             lastUL = $("<ul class='nav flex-column'>").appendTo(lastLI);
                         }
-                        $("<li class='nav-item'>").append($('<a class="nav-link" style="max-width: 200px;" href="#' + id + '"/>').text(text)).appendTo(lastUL);
+                        $("<li class='nav-item'>").append($('<a class="nav-link p-0" href="#' + id + '"/>').text(text)).appendTo(lastUL);
                     }
                     break;
             }
