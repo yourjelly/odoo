e1997fdee0a836e19d1bae47c7e070ca60be8ee6
lejeune quentin <qle@odoo.com>
2023-10-06 16:43:04 +0200

    [IMP] point_of_sale: New build for IoT v23_11
    Adapts the build of the iot to meet the requirements of version 17 of Odoo
    notably python >=3.10

--- addons/hw_drivers/iot_handlers/interfaces/DisplayInterface_L.py
+++ addons/hw_drivers/iot_handlers/interfaces/DisplayInterface_L.py
@@ -3,6 +3,7 @@
 
 from re import sub, finditer
 import subprocess
+from vcgencmd import Vcgencmd
 
 from odoo.addons.hw_drivers.interface import Interface
 
@@ -13,21 +14,18 @@ class DisplayInterface(Interface):
 
     def get_devices(self):
         display_devices = {}
-        displays = subprocess.check_output(['tvservice', '-l']).decode()
+        hdmi_port = {'hdmi_0' : 2, 'hdmi_1': 7}
+        # RPI 3B+ response on for booth hdmi port
+        power_state_hdmi_0 = Vcgencmd().display_power_state(hdmi_port.get('hdmi_0'))
         x_screen = 0
-        for match in finditer('Display Number (\d), type HDMI (\d)', displays):
-            display_id, hdmi_id = match.groups()
-            tvservice_output = subprocess.check_output(['tvservice', '-nv', display_id]).decode().strip()
-            if tvservice_output:
-                display_name = tvservice_output.split('=')[1]
-                display_identifier = sub('[^a-zA-Z0-9 ]+', '', display_name).replace(' ', '_') + "_" + str(hdmi_id)
-                iot_device = {
-                    'identifier': display_identifier,
-                    'name': display_name,
-                    'x_screen': str(x_screen),
-                }
-                display_devices[display_identifier] = iot_device
-                x_screen += 1
+        if power_state_hdmi_0 == 'on':
+            iot_device = {
+                'identifier': 'hdmi_0',
+                'name': 'Display hdmi 0',
+                'x_screen': str(x_screen),
+            }
+            display_devices['hdmi_0'] = iot_device
+            x_screen += 1
 
         if not len(display_devices):
             # No display connected, create "fake" device to be accessed from another computer
