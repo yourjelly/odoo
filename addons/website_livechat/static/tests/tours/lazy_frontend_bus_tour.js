2533d5d7bf4acc9459db9b53d39982ffd15ee421
tsm-odoo <tsm@odoo.com>
2023-10-09 14:17:51 +0200

    wip

--- addons/website_livechat/static/tests/tours/lazy_frontend_bus_tour.js
+++ addons/website_livechat/static/tests/tours/lazy_frontend_bus_tour.js
@@ -1,6 +1,18 @@
 /* @odoo-module */
 
+import { busService } from "@bus/services/bus_service";
 import { registry } from "@web/core/registry";
+
+let busServiceInstance;
+const patchedBusService = {
+    ...busService,
+    start(env) {
+        busServiceInstance = busService.start(...arguments);
+        busServiceInstance.env = env;
+        return busServiceInstance;
+    },
+};
+registry.category("services").add("bus_service", patchedBusService, { force: true });
 
 registry.category("web_tour.tours").add("website_livechat.lazy_frontend_bus", {
     test: true,
@@ -9,8 +21,8 @@ registry.category("web_tour.tours").add("website_livechat.lazy_frontend_bus", {
         {
             trigger: "body",
             async run() {
-                await odoo.__WOWL_DEBUG__.root.env.services["mail.messaging"].isReady;
-                if (odoo.__WOWL_DEBUG__.root.env.services.bus_service.isActive) {
+                await busServiceInstance.env.services["mail.messaging"].isReady;
+                if (busServiceInstance.isActive) {
                     throw new Error("Bus service should not be started eagerly");
                 }
             },
