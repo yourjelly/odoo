f4d3e52bf8835eaeb77083014ed13eee68b474a4
Raphael Collet <rco@odoo.com>
2023-10-10 14:45:09 +0200

    [IMP] base: use env.flush_fields() in _get_inheriting_views()

--- odoo/addons/base/models/ir_ui_view.py
+++ odoo/addons/base/models/ir_ui_view.py
@@ -23,7 +23,7 @@ from odoo.exceptions import ValidationError, AccessError, UserError
 from odoo.http import request
 from odoo.modules.module import get_resource_from_path
 from odoo.tools import config, ConstantMapping, get_diff, pycompat, apply_inheritance_specs, locate_node, str2bool
-from odoo.tools import safe_eval, lazy, lazy_property, frozendict
+from odoo.tools import safe_eval, lazy, lazy_property, frozendict, SQL
 from odoo.tools.convert import _fix_multiple_roots
 from odoo.tools.misc import file_path
 from odoo.tools.translate import xml_translate, TRANSLATED_ATTRS
@@ -590,30 +590,34 @@ actual arch.
         """
         self.check_access_rights('read')
         domain = self._get_inheriting_views_domain()
-        e = expression(domain, self.env['ir.ui.view'])
-        from_clause, where_clause, where_params = e.query.get_sql()
-        assert from_clause == '"ir_ui_view"', f"Unexpected from clause: {from_clause}"
+        query = expression(domain, self.env['ir.ui.view']).query
 
-        self._flush_search(domain, fields=['inherit_id', 'priority', 'model', 'mode'])
-        query = f"""
+        sql = SQL("""
             WITH RECURSIVE ir_ui_view_inherits AS (
                 SELECT id, inherit_id, priority, mode, model
-                FROM ir_ui_view
-                WHERE id IN %s AND ({where_clause})
+                FROM %(from_clause)s
+                WHERE id IN %(ids)s AND %(where_clause)s
             UNION
                 SELECT ir_ui_view.id, ir_ui_view.inherit_id, ir_ui_view.priority,
                        ir_ui_view.mode, ir_ui_view.model
-                FROM ir_ui_view
+                FROM %(from_clause)s
                 INNER JOIN ir_ui_view_inherits parent ON parent.id = ir_ui_view.inherit_id
                 WHERE coalesce(ir_ui_view.model, '') = coalesce(parent.model, '')
                       AND ir_ui_view.mode = 'extension'
-                      AND ({where_clause})
+                      AND %(where_clause)s
             )
             SELECT
                 v.id, v.inherit_id, v.mode
             FROM ir_ui_view_inherits v
             ORDER BY v.priority, v.id
-        """
+        """,
+            from_clause=query.from_clause,
+            where_clause=query.where_clause or SQL("TRUE"),
+            ids=tuple(self.ids),
+            to_flush={
+                'ir.ui.view': ['inherit_id', 'mode', 'model', 'priority'],
+            },
+        )
         # ORDER BY v.priority, v.id:
         # 1/ sort by priority: abritrary value set by developers on some
         #    views to solve "dependency hell" problems and force a view
@@ -622,7 +626,8 @@ actual arch.
         # 2/ sort by view id: the order the views were inserted in the
         #    database. e.g. base views are placed before stock ones.
 
-        self.env.cr.execute(query, [tuple(self.ids)] + where_params + where_params)
+        self.env.flush_fields(sql.to_flush)
+        self.env.cr.execute(sql)
         rows = self.env.cr.fetchall()
 
         views = self.browse(row[0] for row in rows)
