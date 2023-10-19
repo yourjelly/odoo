24e1a460813ae7b1765bf809b9c499d6cf9ba5af
Steven Sameh <stevensameh1512@gmail.com>
2023-10-19 20:50:51 +0300

    [IMP] Real Estate: Create Properties menus and action
    This is Chapter 6: Finally, Some UI To Play With which includes creating menus in order to access a default list and form view.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,8 +5,10 @@
         'base'
     ],
     'data' : [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
-    'application': True
+    'application': True,
 }
 
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,25 +2,42 @@ from odoo import models, fields
 
 
 class EstateProperty(models.Model):
-    _name = 'estate_property'
+    _name = 'estate.property'
     _description = "This is a real estate property model"
 
-    name = fields.Char(string="Name", required=True)
+    name = fields.Char(string="Name", required=True, default="Unknown")
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Date Availability")
+    date_availability = fields.Date(string="Date Availability", copy=False,
+                                     default=lambda self: fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float(string="selling_price")
-    bedrooms = fields.Integer(string="Bedrooms")
+    selling_price = fields.Float(string="selling_price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
     garden = fields.Boolean(string="Garden")
     garden_area = fields.Boolean(string="Garden Area")
+    active = fields.Boolean(default=True)
     garden_orientation = fields.Selection(
         string="Garden Orientation",
-        selection=[('north', 'North'),
-                   ('south', 'South'),
-                   ('east', 'East'),
-                   ('west', 'West')],
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West'),
+        ],
+    )
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ('new', 'New'),
+            ('offer received', 'Offer Received'),
+            ('offer accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled'),
+        ],
+        copy=False,
+        required=True,
+        default='new',
     )
