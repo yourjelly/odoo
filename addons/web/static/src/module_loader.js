36756ed10616992b46fe25526372fbfe7b1d987c
Aaron Bohy <aab@odoo.com>
2023-10-25 11:25:44 +0200

    Revert "topological sort modules"
    This reverts commit fa937d087dd3a71e2a40e87a4f950a94eec2ac57.

--- addons/web/static/src/module_loader.js
+++ addons/web/static/src/module_loader.js
@@ -6,28 +6,6 @@
 (function () {
     "use strict";
 
-    function topologicalSort(elems, getDependencies) {
-        const result = [];
-        const visited = new Set();
-        function visit(n) {
-            if (visited.has(n)) {
-                return;
-            }
-            visited.add(n);
-            // first visit all dependencies of n, then append n to result
-            for (const dep of getDependencies(n)) {
-                visit(dep);
-            }
-            result.push(n);
-        }
-
-        for (const el of elems) {
-            visit(el);
-        }
-
-        return result;
-    }
-
     class ModuleLoader {
         /** @type {Map<string,{fn: Function, deps: string[]}>} mapping name => deps/fn */
         factories = new Map();
@@ -49,100 +27,53 @@
          * @param {Function} factory
          */
         define(name, deps, factory) {
-            if (odoo.debug) {
-                if (typeof name !== "string") {
-                    throw new Error(`Invalid name definition: ${name} (should be a string)"`);
-                }
-                if (!(deps instanceof Array)) {
-                    throw new Error(`Dependencies should be defined by an array: ${deps}`);
-                }
-                if (typeof factory !== "function") {
-                    throw new Error(`Factory should be defined by a function ${factory}`);
-                }
+            if (typeof name !== "string") {
+                throw new Error(`Invalid name definition: ${name} (should be a string)"`);
+            }
+            if (!(deps instanceof Array)) {
+                throw new Error(`Dependencies should be defined by an array: ${deps}`);
+            }
+            if (typeof factory !== "function") {
+                throw new Error(`Factory should be defined by a function ${factory}`);
             }
             if (!this.factories.has(name)) {
                 this.factories.set(name, {
-                    name,
                     deps,
                     fn: factory,
                     ignoreMissingDeps: globalThis.__odooIgnoreMissingDependencies,
                 });
+                this.addJob(name);
                 this.checkErrorProm ||= Promise.resolve().then(() => {
                     this.checkAndReportErrors();
                     this.checkErrorProm = null;
                 });
-                if (!this.started) {
-                    this.started = true;
-                    Promise.resolve().then(() => {
-                        this.startModules();
-                        this.started = false;
-                    });
-                }
             }
         }
 
-        // addJob(name) {
-        //     // this.jobs.add(name);
-        //     this.startModules();
-        // }
+        addJob(name) {
+            this.jobs.add(name);
+            this.startModules();
+        }
 
-        // findJob() {
-        //     for (const job of this.jobs) {
-        //         if (this.factories.get(job).deps.every((dep) => this.modules.has(dep))) {
-        //             return job;
-        //         }
-        //     }
-        //     return null;
-        // }
-
-        sortModules() {}
-
-        async startModules() {
-            // const factories = [...this.factories.values()];
-            // window.primaryFactories = factories.filter((factory) => {
-            //     return factory.name === "@odoo/owl" || factory.name === "@web/start" || factory.deps.includes("@web/core/registry")
-            // });
-            // const primaryFactoryNames = primaryFactories.map((f) => f.name);
-            // // should iterate until stabilization
-            // window.secondaryFactories = factories.filter((factory) => {
-            //     return primaryFactoryNames.includes(factory.name) || factory.deps.some((dep) => primaryFactoryNames.includes(dep));
-            // })
-            // window.lastFactories = factories.filter((factory) => {
-            //     return !secondaryFactories.includes(factory);
-            // })
-            // let sortedFactories = topologicalSort(secondaryFactories, (factory) => {
-            //     return factory.deps.map((dep) => this.factories.get(dep));
-            // });
-            const mainFactory = this.factories.get("@web_enterprise/main");
-            this.factories.delete("@web_enterprise/main");
-            const factories = [mainFactory, ...this.factories.values()];
-            this.factories.set("@web_enterprise/main", mainFactory);
-            console.time("topological sort");
-            const sortedFactories = topologicalSort(factories, (factory) => {
-                return factory.deps.map((dep) => this.factories.get(dep));
-            });
-            console.timeEnd("topological sort");
-            for (const factory of sortedFactories) {
-                this.startModule(factory.name);
+        findJob() {
+            for (const job of this.jobs) {
+                if (this.factories.get(job).deps.every((dep) => this.modules.has(dep))) {
+                    return job;
+                }
             }
-            // odoo.loader.modules.get("@web/core/network/rpc_service").jsonrpc("/totphook");
-            // await new Promise((r) => setTimeout(r));
-            // sortedFactories = topologicalSort(lastFactories, (factory) => {
-            //     return factory.deps.map((dep) => this.factories.get(dep));
-            // });
-            // for (const factory of sortedFactories) {
-            //     this.startModule(factory.name);
-            // }
-            // debugger
-            // let job;
-            // while ((job = this.findJob())) {
-            //     this.startModule(job);
-            // }
+            return null;
+        }
+
+        startModules() {
+            let job;
+            while ((job = this.findJob())) {
+                this.startModule(job);
+            }
         }
 
         startModule(name) {
             const require = (name) => this.modules.get(name);
-            // this.jobs.delete(name);
+            this.jobs.delete(name);
             const factory = this.factories.get(name);
             let value = null;
             try {
