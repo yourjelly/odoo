706a7b89599b2b5d260f50b409151d438de506a3
Adrien Guilliams (adgu) <adgu@odoo.com>
2023-10-25 13:04:56 +0200

    [IMP] pos_self_order: display attributes with long text
    In this commit, we change the display of product attributes with long
    text. Now, if an attribute has a long text (>15 characters),
    the attributes will be displayed in 1 column.
    We also prevent the user from adding a product with attributes
    to the cart if some attributes are not selected.

--- addons/pos_self_order/static/src/app/components/attribute_selection/attribute_selection.js
+++ addons/pos_self_order/static/src/app/components/attribute_selection/attribute_selection.js
@@ -1,6 +1,6 @@
 /** @odoo-module */
 
-import { Component, useState } from "@odoo/owl";
+import { Component, onMounted, useRef, useState } from "@odoo/owl";
 import { ProductCustomAttribute } from "@point_of_sale/app/store/models/product_custom_attribute";
 import { useSelfOrder } from "@pos_self_order/app/self_order_service";
 import { attributeFlatter, attributeFormatter } from "@pos_self_order/app/utils";
@@ -14,6 +14,16 @@ export class AttributeSelection extends Component {
         this.numberOfAttributes = this.props.product.attributes.length;
         this.currentAttribute = 0;
 
+        this.gridsRef = {};
+        this.valuesRef = {};
+        for (const attr of this.props.product.attributes) {
+            this.gridsRef[attr.id] = useRef(`attribute_grid_${attr.id}`);
+            this.valuesRef[attr.id] = {};
+            for (const value of attr.values) {
+                this.valuesRef[attr.id][value.id] = useRef(`value_${attr.id}_${value.id}`);
+            }
+        }
+
         this.state = useState({
             showNext: false,
             showCustomInput: false,
@@ -22,6 +32,35 @@ export class AttributeSelection extends Component {
         this.selectedValues = useState(this.env.selectedValues);
 
         this.initAttribute();
+        onMounted(this.onMounted);
+    }
+
+    onMounted() {
+        for (const attr of Object.entries(this.valuesRef)) {
+            let classicValue = 0;
+            for (const valueRef of Object.values(attr[1])) {
+                if (valueRef.el) {
+                    if (classicValue === 0) {
+                        classicValue = valueRef.el.parentNode.offsetHeight;
+                    } else {
+                        if (valueRef.el.parentNode.offsetHeight !== classicValue) {
+                            this.gridsRef[attr[0]].el.classList.remove(
+                                "row-cols-2",
+                                "row-cols-sm-3",
+                                "row-cols-md-4",
+                                "row-cols-xl-5",
+                                "row-cols-xxl-6"
+                            );
+                            this.gridsRef[attr[0]].el.classList.add("row-cols-1");
+                            for (const gridValueRef of Object.values(attr[1])) {
+                                gridValueRef.el.classList.remove("ratio", "ratio-16x9");
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     get showNextBtn() {
@@ -89,5 +128,14 @@ export class AttributeSelection extends Component {
         return attribute.display_type === "multi"
             ? this.selectedValues[attribute.id][value.id]
             : parseInt(this.selectedValues[attribute.id]) === value.id;
+    }
+
+    isAttributeLong(attribute) {
+        for (const attr of this.availableAttributeValue(attribute)) {
+            if (attr.name.length > 15) {
+                return true;
+            }
+        }
+        return false;
     }
 }
