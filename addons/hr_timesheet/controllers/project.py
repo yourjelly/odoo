a8cf7c28c31841db1fce46bdcdbd9f26d7428e0a
Raja Prasad <rajp@odoo.com>
2023-11-20 14:42:52 +0530

    [FIX] hr_timesheet:  remove non billable timsheet from task on portal
    Steps to reproduce:
    - add non billable timesheet to any task
    - share that task to show on portal
    - non-billable timesheet are also shown
    
    Issue:
    - It should only show billable timesheet on task
    
    Solution:
    - add domain 'so_line' in timesheet when getting from database
    - timesheet having 'so_line' true will be only shown on portal
    
    Task: 3602575

--- addons/hr_timesheet/controllers/project.py
+++ addons/hr_timesheet/controllers/project.py
@@ -13,7 +13,7 @@ class ProjectCustomerPortal(CustomerPortal):
     def _task_get_page_view_values(self, task, access_token, **kwargs):
         values = super(ProjectCustomerPortal, self)._task_get_page_view_values(task, access_token, **kwargs)
         domain = request.env['account.analytic.line']._timesheet_get_portal_domain()
-        task_domain = expression.AND([domain, [('task_id', '=', task.id)]])
+        task_domain = expression.AND([domain, [('task_id', '=', task.id), ('so_line', '!=', False)]])
         subtask_domain = expression.AND([domain, [('task_id', 'in', task.child_ids.ids)]])
         timesheets = request.env['account.analytic.line'].sudo().search(task_domain)
         subtasks_timesheets = request.env['account.analytic.line'].sudo().search(subtask_domain)
