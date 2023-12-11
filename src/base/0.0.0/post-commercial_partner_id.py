fe0a20452d33d811b33b9bd60b64a44d5265c7b7
sagu-odoo <sagu@odoo.com>
2023-12-11 07:07:15 +0000

    [FIX] base/0.0.0/post-commercial_partner_id: Fix Memory Error
    If the records of the 'res_partner' table are more
    then '2700000' due to this will give memory error ``` [id_ for id_, in cr.fetchall()] ```
    for avoiding this memory error filtering the record by computing commerical_partner_id
    using this condition [field who don't have parent and have company](https://github.com/odoo/odoo/blob/f8a372d2be268a8c39244bc9240db41fd93efdd8/odoo/addons/base/models/res_p>
    in early(sql) and rest res.partner record who don't fulfil the condition will compute by
    compute method

--- src/base/0.0.0/post-commercial_partner_id.py
+++ src/base/0.0.0/post-commercial_partner_id.py
@@ -5,6 +5,19 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # The `commercial_partner_id` field is expected to always be set. Although the column is not marked as `NOT NULL`.
     # Fight the Murphy's Law, and recompute the value on partners with a NULL value.
+    util.explode_execute(
+        cr,
+        """
+        UPDATE res_partner
+           SET commercial_partner_id = id
+         WHERE commercial_partner_id IS NULL
+           AND ( is_company = 't'
+                OR parent_id IS NULL)
+        """,
+        table="res_partner",
+    )
     cr.execute("SELECT id FROM res_partner WHERE commercial_partner_id IS NULL")
     if cr.rowcount:
-        util.recompute_fields(cr, "res.partner", ["commercial_partner_id"], ids=[id_ for id_, in cr.fetchall()])
+        util.recompute_fields(
+            cr, "res.partner", ["commercial_partner_id"], ids=[id_ for id_, in cr.fetchall()], strategy="commit"
+        )
