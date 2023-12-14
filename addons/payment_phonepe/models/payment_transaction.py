50f11ed6cf811628ca3e191e564b8b6c79ecd52b
Gurpreet Singh <gusi@odoo.com>
2023-12-14 15:23:05 +0530

    [IMP] payment_phonepe: WIP

--- addons/payment_phonepe/models/payment_transaction.py
+++ addons/payment_phonepe/models/payment_transaction.py
@@ -61,14 +61,14 @@ class PaymentTransaction(models.Model):
             return res
         converted_amount = payment_utils.to_minor_currency_units(self.amount, self.currency_id)
         base_url = self.get_base_url()
-        # payment_redirect = "/payment/status"
+        payment_redirect = "/payment/status"
         payload = {
             'merchantId': self.provider_id.phonepe_merchant_id,
             'merchantTransactionId': self.reference,
             'merchantUserId': self.partner_email,
             'amount': converted_amount,
-            # 'redirectUrl': url_join(base_url, PhonePeController._return_url, payment_redirect),
-            # 'redirectMode': 'POST',
+            'redirectUrl': url_join(base_url, PhonePeController._callback_url),
+            'redirectMode': 'REDIRECT',
             'callbackUrl': url_join(base_url, PhonePeController._callback_url),
             'mobileNumber': self.partner_phone,
             'paymentInstrument': {
