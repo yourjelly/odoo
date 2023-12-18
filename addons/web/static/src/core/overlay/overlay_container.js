8e3e2091effe0363903c578641d4f4e4a550f930
FrancoisGe <fge@odoo.com>
2023-12-18 15:46:22 +0100

    WIP

--- addons/web/static/src/core/overlay/overlay_container.js
+++ addons/web/static/src/core/overlay/overlay_container.js
@@ -1,6 +1,8 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, onPatched, useState } from "@odoo/owl";
+import { effect } from "@web/core/utils/reactive";
+import { batched } from "@web/core/utils/timing";
 import { sortBy } from "../utils/arrays";
 import { ErrorHandler } from "../utils/components";
 
@@ -9,8 +11,37 @@ export class OverlayContainer extends Component {
     static components = { ErrorHandler };
     static props = { overlays: Object };
 
+    setup() {
+        this.state = useState({
+            overlays: Object.values(this.props.overlays),
+        });
+
+        let prom;
+
+        let toResolve;
+        effect(
+            batched(async (overlays) => {
+                if (prom) {
+                    await prom;
+                }
+                this.state.overlays = Object.values(overlays);
+
+                prom = new Promise((resolve) => {
+                    toResolve = resolve;
+                });
+            }),
+            [this.props.overlays]
+        );
+
+        onPatched(() => {
+            if (toResolve) {
+                toResolve();
+            }
+        });
+    }
+
     get sortedOverlays() {
-        return sortBy(Object.values(this.props.overlays), (overlay) => overlay.sequence);
+        return sortBy(this.state.overlays, (overlay) => overlay.sequence);
     }
 
     handleError(overlay, error) {
