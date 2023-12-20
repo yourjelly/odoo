65722cb60b975e076aa0d2ed7bdda3fc28130e94
shap@odoo.com <shap@odoo.com>
2023-12-20 13:29:06 +0530

    [IMP] website_form_project: added some additional fields on create task action
    Added some fields to collect the customer contact details
    
    task-3635784

--- addons/website_form_project/static/src/js/website_form_project_editor.js
+++ addons/website_form_project/static/src/js/website_form_project_editor.js
@@ -10,17 +10,37 @@ FormEditorRegistry.add('create_task', {
         name: 'name',
         string: _t('Task Title'),
     }, {
+        type: 'char',
+        modelRequired: true,
+        fillWith: 'name', 
+        name: 'your_name', 
+        string: _t('Your Name'),
+    },{
+        type: 'char',
+        fillWith: 'phone', 
+        name: 'phone_number',  
+        string: _t('Your Phone Number'),
+    },{
         type: 'email',
         custom: true,
         required: true,
         fillWith: 'email',
         name: 'email_from',
         string: _t('Your Email'),
+    },{
+        type: 'char',
+        fillWith: 'company_name',
+        name: 'company_name', 
+        string: _t('Company Name'),
     }, {
         type: 'char',
         name: 'description',
         string: _t('Description'),
-    }],
+    },{
+        type: 'char',
+        name: 'ask_question', 
+        string: _t('Ask Your Question'),
+    },],
     fields: [{
         name: 'project_id',
         type: 'many2one',
