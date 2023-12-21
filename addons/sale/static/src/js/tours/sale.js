4154301fd84eb0f0a0712bfec05c1cf8cd65a03f
mjvi-odoo <mjvi@odoo.com>
2023-12-21 16:53:11 +0530

    [IMP] sale: quotation list view onboarding changes
    - Remove the configuration bar.
    
    - Add a link button to download a sample PDF quotation.
    
    - Replace text on the list view page:
      before changes:
        'Once the quotation is confirmed, it becomes a rental order.
        You will be able to create an invoice and collect the payment.'
      after changes:
        'Boost sales with beautiful quotations, online payments or signatures,
        upsells, and a great customer portal.'
    
    task-3645924

--- addons/sale/static/src/js/tours/sale.js
+++ addons/sale/static/src/js/tours/sale.js
@@ -6,83 +6,26 @@ import { stepUtils } from "@web_tour/tour_service/tour_utils";
 import { markup } from "@odoo/owl";
 
 registry.category("web_tour.tours").add("sale_tour", {
-    url: "/web",
-    rainbowMan: false,
-    sequence: 20,
-    steps: () => [stepUtils.showAppsMenuItem(), {
-    trigger: ".o_app[data-menu-xmlid='sale.sale_menu_root']",
-    content: _t("Let's create a beautiful quotation in a few clicks."),
-    position: "right",
-    edition: "community"
-}, {
-    trigger: ".o_app[data-menu-xmlid='sale.sale_menu_root']",
-    content: _t("Let's create a beautiful quotation in a few clicks."),
-    position: "bottom",
-    edition: "enterprise"
-}, {
-    trigger: 'a.o_onboarding_step_action.btn[data-method=action_open_step_company_data]',
-    extra_trigger: ".o_sale_order",
-    content: _t("Start by checking your company's data."),
-    position: "bottom",
-    skip_trigger: 'a[data-method=action_open_step_company_data].o_onboarding_step_action__done',
-}, {
-    trigger: 'input[id=street_0]',
-    content: _t("Complete your company's data"),
-    position: "bottom",
-    skip_trigger: 'a[data-method=action_open_step_company_data].o_onboarding_step_action__done',
-}, {
-    trigger: ".modal-content button.o_form_button_save",
-    content: _t("Looks good. Let's continue."),
-    position: "left",
-    skip_trigger: 'a[data-method=action_open_step_company_data].o_onboarding_step_action__done',
-}, stepUtils.showAppsMenuItem(),
-{
-    trigger: ".o_app[data-menu-xmlid='sale.sale_menu_root']",
-    skip_trigger: 'a[data-method=action_open_step_company_data].o_onboarding_step_action__done',
-    edition: "community",
-    auto: true,
-}, {
-    trigger: ".o_app[data-menu-xmlid='sale.sale_menu_root']",
-    skip_trigger: 'a[data-method=action_open_step_company_data].o_onboarding_step_action__done',
-    edition: "enterprise",
-    auto: true,
-}, {
-    trigger: 'a.o_onboarding_step_action.btn[data-method=action_open_step_base_document_layout]',
-    extra_trigger: ".o_sale_order",
-    content: _t("Customize your quotes and orders."),
-    position: "bottom",
-    skip_trigger: 'a[data-method=action_open_step_base_document_layout].o_onboarding_step_action__done',
-}, {
-    trigger: "button[name='document_layout_save']",
-    extra_trigger: ".o_sale_order",
-    content: _t("Good job, let's continue."),
-    position: "top", // dot NOT move to bottom, it would cause a resize flicker
-    skip_trigger: 'a[data-method=action_open_step_base_document_layout].o_onboarding_step_action__done',
-}, {
-    trigger: 'a.o_onboarding_step_action.btn[data-method=action_open_step_sale_order_confirmation]',
-    extra_trigger: ".o_sale_order",
-    content: _t("To speed up order confirmation, we can activate electronic signatures or payments."),
-    position: "bottom",
-    skip_trigger: 'a[data-method=action_open_step_sale_order_confirmation].o_onboarding_step_action__done',
-}, {
-    trigger: "button[name='add_payment_methods']",
-    extra_trigger: ".o_sale_order",
-    content: _t("Lets keep electronic signature for now."),
-    position: "bottom",
-    skip_trigger: 'a[data-method=action_open_step_sale_order_confirmation].o_onboarding_step_action__done',
-}, {
-    trigger: 'a.o_onboarding_step_action.btn[data-method=action_open_step_sample_quotation]',
-    extra_trigger: ".o_sale_order",
-    content: _t("Now, we'll create a sample quote."),
-    position: "bottom",
-}]});
-
-registry.category("web_tour.tours").add("sale_quote_tour", {
-        url: "/web#action=sale.action_quotations_with_onboarding&view_type=form",
+        url: "/web",
         rainbowMan: true,
         rainbowManMessage: () => markup(_t("<b>Congratulations</b>, your first quotation is sent!<br>Check your email to validate the quote.")),
         sequence: 30,
-        steps: () => [{
+        steps: () => [stepUtils.showAppsMenuItem(), {
+        trigger: ".o_app[data-menu-xmlid='sale.sale_menu_root']",
+        content: _t("Let's create a beautiful quotation in a few clicks."),
+        position: "right",
+        edition: "community"
+    }, {
+        trigger: ".o_app[data-menu-xmlid='sale.sale_menu_root']",
+        content: _t("Let's create a beautiful quotation in a few clicks."),
+        position: "bottom",
+        edition: "enterprise"
+    }, {
+        trigger: "button.o_list_button_add",
+        extra_trigger: ".o_sale_order",
+        content: _t("Build your first quotation right here!"),
+        position: "bottom",
+    }, {
         trigger: ".o_field_res_partner_many2one[name='partner_id']",
         extra_trigger: ".o_sale_order",
         content: _t("Write a company name to create one, or see suggestions."),
