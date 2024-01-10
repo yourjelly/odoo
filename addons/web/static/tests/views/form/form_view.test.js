60294a851425af09232a47548965fc4315e8385b
Julien Mougenot <jum@odoo.com>
2024-01-10 16:03:05 +0100

    fixup! [WIP] tests with components (BREAKING)

--- addons/web/static/tests/views/form/form_view.test.js
+++ addons/web/static/tests/views/form/form_view.test.js
@@ -1,7 +1,30 @@
 /** @odoo-module */
 
 import { expect, test } from "@odoo/hoot";
+import { clickSave, defineModels, fields, models, mountView } from "@web/../tests/web_test_helpers";
 
-test("Form view", () => {
-    expect(true).toBe(true, { message: "All form view tests have passed" });
+class ResPartner extends models.Model {
+    _name = "res.partner";
+
+    int_field = fields.Integer();
+}
+
+defineModels([ResPartner]);
+
+test("no autofocus with disable_autofocus option", async () => {
+    await mountView({
+        type: "form",
+        resModel: "res.partner",
+        arch: /* xml */ `
+            <form disable_autofocus="1">
+                <field name="int_field" />
+            </form>
+        `,
+    });
+
+    expect(`.o_field_widget[name="int_field"] input`).not.toBeFocused();
+
+    await clickSave();
+
+    expect(`.o_field_widget[name="int_field"] input`).not.toBeFocused();
 });
