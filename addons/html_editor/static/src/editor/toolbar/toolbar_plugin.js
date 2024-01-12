9b4783bf28492e8458d2e0bffaa16391a400d718
Rodolpho Lima <rcdl@odoo.com>
2024-01-12 17:37:10 +0100

    Toolbar buttons and active state as props
    - Replace hardcoded buttons in the toolbar template for a list of buttons
    received as props.
    
    - Buttons active state managed by the toolbar plugin.

--- addons/html_editor/static/src/editor/toolbar/toolbar_plugin.js
+++ addons/html_editor/static/src/editor/toolbar/toolbar_plugin.js
@@ -2,14 +2,64 @@
 
 import { Plugin } from "../plugin";
 import { Toolbar } from "./toolbar";
+import { reactive } from "@odoo/owl";
+
+// TODO: This comes from a command registry?
+const buttons = [
+    {
+        id: "bold",
+        cmd: "TOGGLE_BOLD",
+        icon: "fa-bold",
+        name: "Toggle bold",
+        isFormatApplied: hasTag("strong"),
+    },
+    {
+        id: "italic",
+        cmd: "TOGGLE_ITALIC",
+        icon: "fa-italic",
+        name: "Toggle italic",
+        isFormatApplied: hasTag("em"),
+    },
+    {
+        id: "underline",
+        cmd: "TOGGLE_UNDERLINE",
+        icon: "fa-underline",
+        name: "Toggle underline",
+        isFormatApplied: hasTag("u"),
+    },
+    {
+        id: "strikethrough",
+        cmd: "TOGGLE_STRIKETHROUGH",
+        icon: "fa-strikethrough",
+        name: "Toggle strikethrough",
+        isFormatApplied: hasTag("s"),
+    },
+];
+
+// TODO: This is a temporary naive solution to generate isFormatApplied callback.
+function hasTag(tagName) {
+    return (range) => {
+        let el = range.startContainer;
+        if (el.nodeType !== Node.ELEMENT_NODE) {
+            el = el.parentElement;
+        }
+        return !!el.closest(tagName);
+    };
+}
 
 export class ToolbarPlugin extends Plugin {
     static name = "toolbar";
     static dependencies = ["overlay"];
 
     setup() {
+        this.buttons = buttons;
+        this.buttonsActiveState = reactive(Object.fromEntries(this.buttons.map(b => [b.id, false])));
         /** @type {import("../core/overlay_plugin").Overlay} */
-        this.overlay = this.shared.createOverlay(Toolbar, "top", { dispatch: this.dispatch });
+        this.overlay = this.shared.createOverlay(Toolbar, "top", {
+            dispatch: this.dispatch,
+            buttons: this.buttons,
+            buttonsActiveState: this.buttonsActiveState,
+        });
         this.addDomListener(document, "selectionchange", this.handleSelectionChange);
     }
 
@@ -28,6 +78,13 @@ export class ToolbarPlugin extends Plugin {
 
     handleSelectionChange() {
         const range = window.getSelection().getRangeAt(0);
+        this.updateToolbarVisibility(range);
+        if (this.overlay.isOpen) {
+            this.updateButtonsActiveState(range);
+        }
+    }
+
+    updateToolbarVisibility(range) {
         const inEditor = this.el.contains(range.commonAncestorContainer);
         if (this.overlay.isOpen) {
             if (!inEditor || range.collapsed) {
@@ -37,6 +94,12 @@ export class ToolbarPlugin extends Plugin {
             }
         } else if (inEditor && !range.collapsed) {
             this.overlay.open();
+        }
+    }
+
+    updateButtonsActiveState(range) {
+        for (const button of this.buttons) {
+            this.buttonsActiveState[button.id] = button.isFormatApplied(range);
         }
     }
 }
