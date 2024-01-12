99da077ea0d734bbce225499c4614234d276e7ae
Rodolpho Lima <rcdl@odoo.com>
2024-01-12 17:37:10 +0100

    Toolbar buttons and active state as props
    - Replace hardcoded buttons in the toolbar template for a list of buttons
    received as props.
    
    - Buttons active state managed by the toolbar plugin.

--- addons/html_editor/static/src/editor/toolbar/toolbar.js
+++ addons/html_editor/static/src/editor/toolbar/toolbar.js
@@ -1,10 +1,17 @@
 /** @odoo-module */
 
-import { Component, onMounted, useRef } from "@odoo/owl";
+import { Component, onMounted, useRef, useState, useExternalListener } from "@odoo/owl";
 
 export class Toolbar extends Component {
     static template = "html_editor.Toolbar";
-    static props = { onMounted: Function, dispatch: Function, close: Function };
+    static props = {
+        onMounted: Function,
+        dispatch: Function,
+        close: Function,
+        // TODO: more specific prop validation for buttons after its format has been defined.
+        buttons: Array,
+        buttonsActiveState: Object,
+    };
 
     setup() {
         const ref = useRef("root");
