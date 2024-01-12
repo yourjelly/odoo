39d12c9e0cd44094f071a05a4beb6aa2f85062f7
Géry Debongnie <ged@odoo.com>
2024-01-12 09:15:56 +0100

    playground: add possibility to see current editor for comparison purposes

--- addons/html_editor/static/src/playground.js
+++ addons/html_editor/static/src/playground.js
@@ -1,14 +1,21 @@
 /** @odoo-module */
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useWysiwyg } from "./editor/wysiwyg";
 
 export class Playground extends Component {
     static template = "html_editor.Playground";
+    static components = { };
     static props = ["*"];
 
     setup() {
         this.editor = useWysiwyg("html");
+        this.state = useState({ showWysiwyg: false });
+        this.constructor.components.Wysiwyg = odoo.loader.modules.get("@web_editor/js/wysiwyg/wysiwyg").Wysiwyg;
+    }
+
+    toggleWysiwyg() {
+        this.state.showWysiwyg = !this.state.showWysiwyg;
     }
 }
 
