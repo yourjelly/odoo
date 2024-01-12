f1750ed0312c836b47f390a79ad99e5562dde6db
Géry Debongnie <ged@odoo.com>
2024-01-12 14:05:00 +0100

    initialize editable element on start

--- addons/html_editor/static/tests/editor/initialization.test.js
+++ addons/html_editor/static/tests/editor/initialization.test.js
@@ -7,8 +7,8 @@ import { getContent, setupEditor } from "../helpers";
  * content of the "init" sub suite in editor.test.js
  */
 
-test.todo("should transform root <br> into <p>", async () => {
-    const { el, editor } = await setupEditor("ab<br>c");
+test("should transform root <br> into <p>", async () => {
+    const { el } = await setupEditor("ab<br>c");
     expect(getContent(el)).toBe(`<p style="margin-bottom: 0px;">ab</p><p style="margin-bottom: 0px;">c</p>`);
 });
 
