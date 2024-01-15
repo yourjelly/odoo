f372dd5510dce89e8cfffb5f8d64cdb3fc759802
Géry Debongnie <ged@odoo.com>
2024-01-15 13:41:30 +0100

    add toolbar test

---
+++ addons/html_editor/static/tests/toolbar.test.js
@@ -0,0 +1,20 @@
+/** @odoo-module */
+
+import { expect, test } from "@odoo/hoot";
+import { getContent, setRange, setupEditor } from "./helpers";
+import { waitFor } from "@odoo/hoot-dom";
+import { contains } from "@web/../tests/web_test_helpers";
+
+test("toolbar works: can format bold", async () => {
+    const { el } = await setupEditor("<p>test</p>");
+    expect(getContent(el)).toBe("<p>test</p>");
+
+    // set selection to open toolbar
+    expect(".o-we-toolbar").toHaveCount(0);
+    setRange(el, "<p>[test]</p>");
+    await waitFor(".o-we-toolbar");
+
+    // click on toggle bold
+    await contains(".btn[title='Toggle bold']").click();
+    expect(getContent(el)).toBe("<p><strong>[test]</strong></p>");
+});
