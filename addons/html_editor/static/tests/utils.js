2723e0cb392245bcb7ec27f1ae86599a39acc9a6
FrancoisGe <fge@odoo.com>
2024-01-15 11:22:53 +0100

    add format test bold, italic, underline and strick through

---
+++ addons/html_editor/static/tests/utils.js
@@ -0,0 +1,10 @@
+/** @odoo-module */
+
+/**
+ * Unformat the given html in order to use it with `innerHTML`.
+ */
+export function unformat(html) {
+    return html
+        .replace(/(^|[^ ])[\s\n]+([^<>]*?)</g, '$1$2<')
+        .replace(/>([^<>]*?)[\s\n]+([^ ]|$)/g, '>$1$2');
+}
