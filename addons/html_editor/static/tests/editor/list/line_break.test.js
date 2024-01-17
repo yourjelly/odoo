e89db178de2cd2f2a3f1a2e70f48d11ded30e649
Géry Debongnie <ged@odoo.com>
2024-01-17 13:45:39 +0100

    add line break tests

---
+++ addons/html_editor/static/tests/editor/list/line_break.test.js
@@ -0,0 +1,47 @@
+/** @odoo-module */
+
+import { test } from "@odoo/hoot";
+import { testEditor } from "../../helpers";
+
+function insertLineBreak() {
+    throw new Error("need a proper implementation");
+}
+
+test.todo("should insert a <br> into an empty list item", async () => {
+    await testEditor({
+        contentBefore: "<ul><li>[]<br></li></ul>",
+        stepFunction: insertLineBreak,
+        contentAfter: "<ul><li><br>[]<br></li></ul>",
+    });
+});
+test.todo("should insert a <br> at the beggining of a list item", async () => {
+    await testEditor({
+        contentBefore: "<ul><li>[]abc</li></ul>",
+        stepFunction: insertLineBreak,
+        contentAfter: "<ul><li><br>[]abc</li></ul>",
+    });
+});
+test.todo("should insert a <br> within a list item", async () => {
+    await testEditor({
+        contentBefore: "<ul><li>ab[]cd</li></ul>",
+        stepFunction: insertLineBreak,
+        contentAfter: "<ul><li>ab<br>[]cd</li></ul>",
+    });
+});
+test.todo("should insert a line break (2 <br>) at the end of a list item", async () => {
+    await testEditor({
+        contentBefore: "<ul><li>abc[]</li></ul>",
+        stepFunction: insertLineBreak,
+        // The second <br> is needed to make the first
+        // one visible.
+        contentAfter: "<ul><li>abc<br>[]<br></li></ul>",
+    });
+});
+
+test.todo("should delete part of a list item, then insert a <br>", async () => {
+    await testEditor({
+        contentBefore: "<ul><li>ab[cd]ef</li></ul>",
+        stepFunction: insertLineBreak,
+        contentAfter: "<ul><li>ab<br>[]ef</li></ul>",
+    });
+});
