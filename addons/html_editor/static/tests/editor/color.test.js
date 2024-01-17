fe110e85f9ed34534ff10e3ed83b67194a95b27a
FrancoisGe <fge@odoo.com>
2024-01-17 16:40:24 +0100

    add color test

---
+++ addons/html_editor/static/tests/editor/color.test.js
@@ -0,0 +1,185 @@
+/** @odoo-module */
+
+import { test } from "@odoo/hoot";
+import { testEditor } from "../helpers";
+import { unformat } from "../utils";
+
+function setColor(color, mode) {
+    return async (editor) => {
+        throw new Error("Not implemented command: applyColor");
+        // editor.dispatch('applyColor', color, mode);
+    };
+}
+
+test.todo("should apply a color to a slice of text in a span in a font", async () => {
+    await testEditor({
+        contentBefore: '<p>a<font class="a">b<span class="b">c[def]g</span>h</font>i</p>',
+        stepFunction: setColor("rgb(255, 0, 0)", "color"),
+        contentAfter:
+            '<p>a<font class="a">b<span class="b">c</span></font>' +
+            '<font class="a" style="color: rgb(255, 0, 0);"><span class="b">[def]</span></font>' +
+            '<font class="a"><span class="b">g</span>h</font>i</p>',
+    });
+});
+
+test.todo("should apply a color to the qweb tag", async () => {
+    await testEditor({
+        contentBefore: `<div><p t-esc="'Test'" contenteditable="false">[Test]</p></div>`,
+        stepFunction: setColor("rgb(255, 0, 0)", "color"),
+        contentAfter: `<div>[<p t-esc="'Test'" contenteditable="false" style="color: rgb(255, 0, 0);">Test</p>]</div>`,
+    });
+
+    await testEditor({
+        contentBefore: `<div><p t-field="record.display_name" contenteditable="false">[Test]</p></div>`,
+        stepFunction: setColor("rgb(255, 0, 0)", "color"),
+        contentAfter: `<div>[<p t-field="record.display_name" contenteditable="false" style="color: rgb(255, 0, 0);">Test</p>]</div>`,
+    });
+});
+
+test.todo("should apply a background color to a slice of text in a span in a font", async () => {
+    await testEditor({
+        contentBefore: '<p>a<font class="a">b<span class="b">c[def]g</span>h</font>i</p>',
+        stepFunction: setColor("rgb(255, 0, 0)", "backgroundColor"),
+        contentAfter:
+            '<p>a<font class="a">b<span class="b">c</span></font>' +
+            '<font class="a" style="background-color: rgb(255, 0, 0);"><span class="b">[def]</span></font>' +
+            '<font class="a"><span class="b">g</span>h</font>i</p>',
+    });
+});
+
+test.todo("should get ready to type with a different color", async () => {
+    await testEditor({
+        contentBefore: "<p>ab[]cd</p>",
+        stepFunction: setColor("rgb(255, 0, 0)", "color"),
+        contentAfter: '<p>ab<font style="color: rgb(255, 0, 0);">[]\u200B</font>cd</p>',
+    });
+});
+
+test.todo("should get ready to type with a different background color", async () => {
+    await testEditor({
+        contentBefore: "<p>ab[]cd</p>",
+        stepFunction: setColor("rgb(255, 0, 0)", "backgroundColor"),
+        contentAfter: '<p>ab<font style="background-color: rgb(255, 0, 0);">[]\u200B</font>cd</p>',
+    });
+});
+
+test.todo("should apply a color on empty selection", async () => {
+    await testEditor({
+        contentBefore: "<p>[<br></p><p><br></p><p>]<br></p>",
+        stepFunction: setColor("rgb(255, 0, 0)", "color"),
+        contentAfterEdit:
+            '<p>[<font data-oe-zws-empty-inline="" style="color: rgb(255, 0, 0);">\u200B</font></p>' +
+            '<p><font data-oe-zws-empty-inline="" style="color: rgb(255, 0, 0);">\u200B</font></p>' +
+            '<p>]<font data-oe-zws-empty-inline="" style="color: rgb(255, 0, 0);">\u200B</font></p>',
+        contentAfter: "<p>[</p><p></p><p>]</p>",
+    });
+});
+
+test.todo("should apply a background color on empty selection", async () => {
+    await testEditor({
+        contentBefore: "<p>[<br></p><p><br></p><p>]<br></p>",
+        stepFunction: setColor("rgb(255, 0, 0)", "backgroundColor"),
+        contentAfterEdit:
+            '<p>[<font data-oe-zws-empty-inline="" style="background-color: rgb(255, 0, 0);">\u200B</font></p>' +
+            '<p><font data-oe-zws-empty-inline="" style="background-color: rgb(255, 0, 0);">\u200B</font></p>' +
+            '<p>]<font data-oe-zws-empty-inline="" style="background-color: rgb(255, 0, 0);">\u200B</font></p>',
+        contentAfter: "<p>[</p><p></p><p>]</p>",
+    });
+});
+
+test.todo("should not merge line on background color change", async () => {
+    await testEditor({
+        contentBefore: "<p><strong>[abcd</strong><br><strong>efghi]</strong></p>",
+        stepFunction: setColor("rgb(255, 0, 0)", "backgroundColor"),
+        contentAfter:
+            '<p><strong><font style="background-color: rgb(255, 0, 0);">[abcd</font></strong><br>' +
+            '<strong><font style="background-color: rgb(255, 0, 0);">efghi]</font></strong></p>',
+    });
+});
+
+test.todo("should not merge line on color change", async () => {
+    await testEditor({
+        contentBefore: "<p><strong>[abcd</strong><br><strong>efghi]</strong></p>",
+        stepFunction: setColor("rgb(255, 0, 0)", "color"),
+        contentAfter:
+            '<p><strong><font style="color: rgb(255, 0, 0);">[abcd</font></strong><br>' +
+            '<strong><font style="color: rgb(255, 0, 0);">efghi]</font></strong></p>',
+    });
+});
+
+test.todo("should not apply color on an uneditable element", async () => {
+    await testEditor({
+        contentBefore: '<p>[a</p><p contenteditable="false">b</p><p>c]</p>',
+        stepFunction: setColor("rgb(255, 0, 0)", "color"),
+        contentAfter: unformat(`
+                <p><font style="color: rgb(255, 0, 0);">[a</font></p>
+                <p contenteditable="false">b</p>
+                <p><font style="color: rgb(255, 0, 0);">c]</font></p>
+            `),
+    });
+});
+
+test.todo(
+    "should not apply background color on an uneditable selected cell in a table",
+    async () => {
+        await testEditor({
+            contentBefore: unformat(`
+                <table><tbody>
+                    <tr><td>[ab</td></tr>
+                    <tr><td contenteditable="false">cd]</td></tr>
+                </tbody></table>
+            `),
+            stepFunction: setColor("rgb(255, 0, 0)", "backgroundColor"),
+            contentAfter: unformat(`
+                <table><tbody>
+                    <tr><td style="background-color: rgb(255, 0, 0);">[]ab</td></tr>
+                    <tr><td contenteditable="false">cd</td></tr>
+                </tbody></table>
+            `),
+        });
+    }
+);
+
+test.todo("should remove font tag after removing font color", async () => {
+    await testEditor({
+        contentBefore: '<p><font style="color: rgb(255, 0, 0);">[abcabc]</font></p>',
+        stepFunction: setColor("", "color"),
+        contentAfter: "<p>[abcabc]</p>",
+    });
+    await testEditor({
+        contentBefore: '<p><font class="text-400">[abcabc]</font></p>',
+        stepFunction: setColor("", "color"),
+        contentAfter: "<p>[abcabc]</p>",
+    });
+});
+
+test.todo("should remove font tag after removing background color applied as style", async () => {
+    await testEditor({
+        contentBefore: '<p><font style="background-color: rgb(255, 0, 0);">[abcabc]</font></p>',
+        stepFunction: setColor("", "backgroundColor"),
+        contentAfter: "<p>[abcabc]</p>",
+    });
+    await testEditor({
+        contentBefore: '<p><font class="bg-200">[abcabc]</font></p>',
+        stepFunction: setColor("", "backgroundColor"),
+        contentAfter: "<p>[abcabc]</p>",
+    });
+});
+
+test.todo(
+    "should remove font tag if font-color and background-color both are removed one by one",
+    async () => {
+        await testEditor({
+            contentBefore:
+                '<p><font style="color: rgb(255, 0, 0);" class="bg-200">[abcabc]</font></p>',
+            stepFunction: setColor("", "backgroundColor") && setColor("", "color"),
+            contentAfter: "<p>[abcabc]</p>",
+        });
+        await testEditor({
+            contentBefore:
+                '<p><font style="background-color: rgb(255, 0, 0);" class="text-900">[abcabc]</font></p>',
+            stepFunction: setColor("", "color") && setColor("", "backgroundColor"),
+            contentAfter: "<p>[abcabc]</p>",
+        });
+    }
+);
