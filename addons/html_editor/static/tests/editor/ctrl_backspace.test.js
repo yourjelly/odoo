1ead0b2b264b87187166567fa3e338f51c88b018
FrancoisGe <fge@odoo.com>
2024-01-18 16:04:02 +0100

    convert ctrl backspace test

---
+++ addons/html_editor/static/tests/editor/ctrl_backspace.test.js
@@ -0,0 +1,109 @@
+/** @odoo-module */
+
+import { test } from "@odoo/hoot";
+import { testEditor } from "../helpers";
+import { unformat } from "../utils";
+import { dispatch } from "@odoo/hoot-dom";
+
+// CTRL+BACKSPACE
+test.todo("should not remove the last p with ctrl+backspace", async () => {
+    await testEditor({
+        contentBefore: unformat(`<p>[]<br></p>`),
+        stepFunction: async (editor) => {
+            editor.editable.addEventListener("keydown", (ev) => {
+                // simulation of the browser default behavior
+                if (ev.key === "Backspace" && ev.ctrlKey === true && !ev.defaultPrevented) {
+                    const sel = document.getSelection();
+                    sel.anchorNode.remove();
+                }
+            });
+            await dispatch(editor.editable, "keydown", { key: "Backspace", ctrlKey: true });
+        },
+        contentAfter: unformat(`<p>[]<br></p>`),
+    });
+});
+
+test.todo(
+    "should not remove the last p enclosed in a contenteditable=false with ctrl+backspace",
+    async () => {
+        await testEditor({
+            contentBefore: unformat(`
+                <p>text</p>
+                <div contenteditable="false"><div contenteditable="true">
+                    <p>[]<br></p>
+                </div></div>`),
+            stepFunction: async (editor) => {
+                editor.editable.addEventListener("keydown", (ev) => {
+                    // simulation of the browser default behavior
+                    if (ev.key === "Backspace" && ev.ctrlKey === true && !ev.defaultPrevented) {
+                        const sel = document.getSelection();
+                        sel.anchorNode.remove();
+                    }
+                });
+                await dispatch(editor.editable, "keydown", { key: "Backspace", ctrlKey: true });
+            },
+            contentAfter: unformat(`
+                <p>text</p>
+                <div contenteditable="false"><div contenteditable="true">
+                    <p>[]<br></p>
+                </div></div>`),
+        });
+    }
+);
+
+test.todo(
+    "should add a <p><br></p> element when deleting the last child of the editable with ctrl+backspace",
+    async () => {
+        await testEditor({
+            contentBefore: unformat(`
+                <blockquote>
+                    []<br>
+                </blockquote>`),
+            stepFunction: async (editor) => {
+                await dispatch(editor.editable, "keydown", { key: "Backspace", ctrlKey: true });
+            },
+            contentAfter: unformat(`<p>[]<br></p>`),
+        });
+    }
+);
+
+test.todo(
+    "should add a <p><br></p> element when deleting the last child of an element with ctrl+backspace",
+    async () => {
+        await testEditor({
+            contentBefore: unformat(`
+                <div contenteditable="false"><div contenteditable="true">
+                    <blockquote>
+                        []<br>
+                    </blockquote>
+                </div></div>`),
+            stepFunction: async (editor) => {
+                await dispatch(editor.editable, "keydown", { key: "Backspace", ctrlKey: true });
+            },
+            contentAfter: unformat(`
+                <div contenteditable="false"><div contenteditable="true">
+                    <p>[]<br></p>
+                </div></div>`),
+        });
+    }
+);
+
+test("should correctly rollback a CTRL+BACKSPACE if the element should not have been removed", async () => {
+    await testEditor({
+        contentBefore: unformat(`
+                <div contenteditable="false"><div contenteditable="true">
+                    <blockquote class="oe_unremovable">
+                        []<br>
+                    </blockquote>
+                </div></div>`),
+        stepFunction: async (editor) => {
+            await dispatch(editor.editable, "keydown", { key: "Backspace", ctrlKey: true });
+        },
+        contentAfter: unformat(`
+                <div contenteditable="false"><div contenteditable="true">
+                    <blockquote class="oe_unremovable">
+                        []<br>
+                    </blockquote>
+                </div></div>`),
+    });
+});
