0437fc0e8af9a98deba27a1e3a993017e0f5ff06
Aaron Bohy <aab@odoo.com>
2024-01-22 10:00:22 +0100

    do no fetch if invisible

--- addons/web/static/tests/views/fields/reference_field_tests.js
+++ addons/web/static/tests/views/fields/reference_field_tests.js
@@ -1026,8 +1026,12 @@ QUnit.module("Fields", (hooks) => {
             await click(target, ".o_list_table td.o_list_many2one");
             await click(target, ".o_list_table .o_list_many2one input");
             //Select the "Partner" option, different from original "Product"
-            const dropdownItems = [...target.querySelectorAll(".o_list_table .o_list_many2one .o_input_dropdown .dropdown-item")];
-            await click(dropdownItems.filter(item => item.text === "Partner")[0]);
+            const dropdownItems = [
+                ...target.querySelectorAll(
+                    ".o_list_table .o_list_many2one .o_input_dropdown .dropdown-item"
+                ),
+            ];
+            await click(dropdownItems.filter((item) => item.text === "Partner")[0]);
             assert.strictEqual(target.querySelector(".reference_field input").value, "");
             assert.strictEqual(target.querySelector(".o_list_many2one input").value, "Partner");
             //Void the associated, required, "reference" field and make sure the form marks the field as required
@@ -1119,5 +1123,27 @@ QUnit.module("Fields", (hooks) => {
 
         await nextTick();
         assert.containsOnce(target, ".o_form_view");
+    });
+
+    QUnit.test("do not ask for display_name if field is invisible", async function (assert) {
+        assert.expect(1);
+
+        await makeView({
+            type: "form",
+            resModel: "partner",
+            resId: 1,
+            serverData,
+            arch: `<form><field name="reference" invisible="1"/></form>`,
+            async mockRPC(route, args) {
+                if (args.method === "web_read") {
+                    assert.deepEqual(args.kwargs.specification, {
+                        display_name: {},
+                        reference: {
+                            fields: {},
+                        },
+                    });
+                }
+            },
+        });
     });
 });
