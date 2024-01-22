66025dd905a309ee0bf9d10e62c0e70f4cb0ccfc
Géry Debongnie <ged@odoo.com>
2024-01-22 10:24:58 +0100

    readd a powerbox test

--- addons/html_editor/static/tests/editor/powerbox.test.js
+++ addons/html_editor/static/tests/editor/powerbox.test.js
@@ -4,6 +4,10 @@ import { expect, test } from "@odoo/hoot";
 import { animationFrame } from "@odoo/hoot-mock";
 import { getContent, insertText, setupEditor } from "../helpers";
 
+function commandNames() {
+    return [...document.querySelectorAll(".o-we-command-name")].map((c) => c.innerText);
+}
+
 test("should open the Powerbox on type `/`", async () => {
     const { el, editor } = await setupEditor("<p>ab[]</p>");
     expect(".o-we-powerbox").toHaveCount(0);
@@ -11,4 +15,14 @@ test("should open the Powerbox on type `/`", async () => {
     insertText(editor, "/");
     await animationFrame();
     expect(".o-we-powerbox").toHaveCount(1);
+});
+
+test("should filter the Powerbox contents with term", async () => {
+    const { el, editor } = await setupEditor("<p>ab[]</p>");
+    await insertText(editor, "/");
+    await animationFrame();
+    expect(commandNames(el).length).toBe(8);
+    await insertText(editor, "head");
+    await animationFrame();
+    expect(commandNames(el)).toEqual(["Heading 1", "Heading 2", "Heading 3"]);
 });
