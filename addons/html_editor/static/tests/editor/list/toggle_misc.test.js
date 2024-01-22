c275c222fe5c727ed09b38cbb34ba107a29c9b63
Rodolpho Lima <rcdl@odoo.com>
2024-01-22 13:31:36 +0100

    Activate lists toggle_misc test suite.

--- addons/html_editor/static/tests/editor/list/toggle_misc.test.js
+++ addons/html_editor/static/tests/editor/list/toggle_misc.test.js
@@ -1,23 +1,11 @@
 /** @odoo-module */
 
 import { describe, test } from "@odoo/hoot";
-import { testEditor } from "../../helpers";
+import { testEditor, toggleUnorderedList, toggleOrderedList, toggleCheckList } from "../../helpers";
 import { unformat } from "../../utils";
 
-function toggleUnorderedList(editor) {
-    throw new Error("toggle UL not done yet");
-}
-
-function toggleOrderedList(editor) {
-    throw new Error("toggle OL not done yet");
-}
-
-function toggleCheckList(editor) {
-    throw new Error("toggle checklins not done yet");
-}
-
 describe("Mixed", () => {
-    test.todo("should turn an ordered list into an unordered list", async () => {
+    test("should turn an ordered list into an unordered list", async () => {
         await testEditor({
             contentBefore: "<ol><li>a[b]c</li></ol>",
             stepFunction: toggleUnorderedList,
@@ -25,7 +13,7 @@ describe("Mixed", () => {
         });
     });
 
-    test.todo("should turn an unordered list into an ordered list", async () => {
+    test("should turn an unordered list into an ordered list", async () => {
         await testEditor({
             contentBefore: "<ul><li>a[b]c</li></ul>",
             stepFunction: toggleOrderedList,
@@ -33,22 +21,17 @@ describe("Mixed", () => {
         });
     });
 
-    test.todo(
-        "should turn a paragraph and an unordered list item into an ordered list and an unordered list",
-        async () => {
-            await testEditor({
-                contentBefore: "<p>a[b</p><ul><li>c]d</li><li>ef</li></ul>",
-                stepFunction: toggleOrderedList,
-                contentAfter: "<ol><li>a[b</li><li>c]d</li><li>ef</li></ol>",
-            });
-        }
-    );
+    test("should turn a paragraph and an unordered list item into an ordered list and an unordered list", async () => {
+        await testEditor({
+            contentBefore: "<p>a[b</p><ul><li>c]d</li><li>ef</li></ul>",
+            stepFunction: toggleOrderedList,
+            contentAfter: "<ol><li>a[b</li><li>c]d</li><li>ef</li></ol>",
+        });
+    });
 
-    test.todo(
-        "should turn a p, an ul list with ao. one nested ul, and another p into one ol with a nested ol",
-        async () => {
-            await testEditor({
-                contentBefore: unformat(`
+    test("should turn a p, an ul list with among others one nested ul, and another p into one ol with a nested ol", async () => {
+        await testEditor({
+            contentBefore: unformat(`
                     <p>a[b</p>
                     <ul>
                         <li>cd</li>
@@ -60,8 +43,8 @@ describe("Mixed", () => {
                         <li>gh</li>
                     </ul>
                     <p>i]j</p>`),
-                stepFunction: toggleOrderedList,
-                contentAfter: unformat(`
+            stepFunction: toggleOrderedList,
+            contentAfter: unformat(`
                     <ol>
                         <li>a[b</li>
                         <li>cd</li>
@@ -73,15 +56,12 @@ describe("Mixed", () => {
                         <li>gh</li>
                         <li>i]j</li>
                     </ol>`),
-            });
-        }
-    );
+        });
+    });
 
-    test.todo(
-        "should turn unordered list into ordered list with block style applied to it",
-        async () => {
-            await testEditor({
-                contentBefore: unformat(`
+    test("should turn unordered list into ordered list with block style applied to it", async () => {
+        await testEditor({
+            contentBefore: unformat(`
                                 <ul>
                                     <li><h1>abc</h1></li>
                                     <li class="oe-nested">
@@ -114,8 +94,8 @@ describe("Mixed", () => {
                                     <li><h1>abc</h1></li>
                                 </ul>
                             `),
-                stepFunction: toggleOrderedList,
-                contentAfter: unformat(`
+            stepFunction: toggleOrderedList,
+            contentAfter: unformat(`
                                 <ol>
                                     <li><h1>abc</h1></li>
                                     <li class="oe-nested">
@@ -147,110 +127,101 @@ describe("Mixed", () => {
                                     </li>
                                     <li><h1>abc</h1></li>
                                 </ol>`),
-            });
-        }
-    );
+        });
+    });
 
-    test.todo(
-        "should turn unordered list into ordered list with block and inline style applied to it",
-        async () => {
-            await testEditor({
-                contentBefore: unformat(`
-                        <ul>
-                            <li><h1><strong>abc</strong></h1></li>
-                            <li class="oe-nested">
-                                <ul>
-                                    <li><h3><strong>a[bc</strong></h3></li>
-                                    <li class="oe-nested">
-                                        <ul>
-                                            <li><h2><em>abc</em></h2></li>
-                                            <li><h2><s>abc</s></h2></li>
-                                            <li><h2><u>abc</u></h2></li>
-                                        </ul>
-                                    </li>
-                                    <li><h1><strong>abc</strong></h1></li>
-                                </ul>
-                            </li>
-                            <li><h1><strong>abc</strong></h1></li>
-                            <li class="oe-nested">
-                                <ul>
-                                    <li><h3><strong>abc</strong></h3></li>
-                                    <li class="oe-nested">
-                                        <ul>
-                                            <li><h2><em>abc</em></h2></li>
-                                            <li><h2><s>abc</s></h2></li>
-                                            <li><h2><u>abc</u></h2></li>
-                                        </ul>
-                                    </li>
-                                    <li><h1><strong>a]bc</strong></h1></li>
-                                </ul>
-                            </li>
-                            <li><h1><strong>abc</strong></h1></li>
-                        </ul>
-                        `),
-                stepFunction: toggleOrderedList,
-                contentAfter: unformat(`
-                        <ol>
-                            <li><h1><strong>abc</strong></h1></li>
-                            <li class="oe-nested">
-                                <ol>
-                                    <li><h3><strong>a[bc</strong></h3></li>
-                                    <li class="oe-nested">
-                                        <ol>
-                                            <li><h2><em>abc</em></h2></li>
-                                            <li><h2><s>abc</s></h2></li>
-                                            <li><h2><u>abc</u></h2></li>
-                                        </ol>
-                                    </li>
-                                    <li><h1><strong>abc</strong></h1></li>
-                                </ol>
-                            </li>
-                            <li><h1><strong>abc</strong></h1></li>
-                            <li class="oe-nested">
-                                <ol>
-                                    <li><h3><strong>abc</strong></h3></li>
-                                    <li class="oe-nested">
-                                        <ol>
-                                            <li><h2><em>abc</em></h2></li>
-                                            <li><h2><s>abc</s></h2></li>
-                                            <li><h2><u>abc</u></h2></li>
-                                        </ol>
-                                    </li>
-                                    <li><h1><strong>a]bc</strong></h1></li>
-                                </ol>
-                            </li>
-                            <li><h1><strong>abc</strong></h1></li>
-                        </ol>`),
-            });
-        }
-    );
+    test("should turn unordered list into ordered list with block and inline style applied to it", async () => {
+        await testEditor({
+            contentBefore: unformat(`
+                    <ul>
+                        <li><h1><strong>abc</strong></h1></li>
+                        <li class="oe-nested">
+                            <ul>
+                                <li><h3><strong>a[bc</strong></h3></li>
+                                <li class="oe-nested">
+                                    <ul>
+                                        <li><h2><em>abc</em></h2></li>
+                                        <li><h2><s>abc</s></h2></li>
+                                        <li><h2><u>abc</u></h2></li>
+                                    </ul>
+                                </li>
+                                <li><h1><strong>abc</strong></h1></li>
+                            </ul>
+                        </li>
+                        <li><h1><strong>abc</strong></h1></li>
+                        <li class="oe-nested">
+                            <ul>
+                                <li><h3><strong>abc</strong></h3></li>
+                                <li class="oe-nested">
+                                    <ul>
+                                        <li><h2><em>abc</em></h2></li>
+                                        <li><h2><s>abc</s></h2></li>
+                                        <li><h2><u>abc</u></h2></li>
+                                    </ul>
+                                </li>
+                                <li><h1><strong>a]bc</strong></h1></li>
+                            </ul>
+                        </li>
+                        <li><h1><strong>abc</strong></h1></li>
+                    </ul>
+                    `),
+            stepFunction: toggleOrderedList,
+            contentAfter: unformat(`
+                    <ol>
+                        <li><h1><strong>abc</strong></h1></li>
+                        <li class="oe-nested">
+                            <ol>
+                                <li><h3><strong>a[bc</strong></h3></li>
+                                <li class="oe-nested">
+                                    <ol>
+                                        <li><h2><em>abc</em></h2></li>
+                                        <li><h2><s>abc</s></h2></li>
+                                        <li><h2><u>abc</u></h2></li>
+                                    </ol>
+                                </li>
+                                <li><h1><strong>abc</strong></h1></li>
+                            </ol>
+                        </li>
+                        <li><h1><strong>abc</strong></h1></li>
+                        <li class="oe-nested">
+                            <ol>
+                                <li><h3><strong>abc</strong></h3></li>
+                                <li class="oe-nested">
+                                    <ol>
+                                        <li><h2><em>abc</em></h2></li>
+                                        <li><h2><s>abc</s></h2></li>
+                                        <li><h2><u>abc</u></h2></li>
+                                    </ol>
+                                </li>
+                                <li><h1><strong>a]bc</strong></h1></li>
+                            </ol>
+                        </li>
+                        <li><h1><strong>abc</strong></h1></li>
+                    </ol>`),
+        });
+    });
 
-    test.todo(
-        "should turn an unordered list item and a paragraph into two list items within an ordered list",
-        async () => {
-            await testEditor({
-                contentBefore: "<ul><li>ab</li><li>c[d</li></ul><p>e]f</p>",
-                stepFunction: toggleOrderedList,
-                contentAfter: "<ol><li>ab</li><li>c[d</li><li>e]f</li></ol>",
-            });
-        }
-    );
+    test("should turn an unordered list item and a paragraph into two list items within an ordered list", async () => {
+        await testEditor({
+            contentBefore: "<ul><li>ab</li><li>c[d</li></ul><p>e]f</p>",
+            stepFunction: toggleOrderedList,
+            contentAfter: "<ol><li>ab</li><li>c[d</li><li>e]f</li></ol>",
+        });
+    });
 
-    test.todo(
-        "should turn an unordered list, a paragraph and an ordered list into one ordered list with three list items",
-        async () => {
-            await testEditor({
-                contentBefore: "<ul><li>a[b</li></ul><p>cd</p><ol><li>e]f</li></ol>",
-                stepFunction: toggleOrderedList,
-                contentAfter: "<ol><li>a[b</li><li>cd</li><li>e]f</li></ol>",
-            });
-        }
-    );
+    test("should turn an unordered list, a paragraph and an ordered list into one ordered list with three list items", async () => {
+        await testEditor({
+            contentBefore: "<ul><li>a[b</li></ul><p>cd</p><ol><li>e]f</li></ol>",
+            stepFunction: toggleOrderedList,
+            contentAfter: "<ol><li>a[b</li><li>cd</li><li>e]f</li></ol>",
+        });
+    });
 
     test.todo(
         "should turn an unordered list item, a paragraph and an ordered list into one ordered list with all three as list items",
         async () => {
             await testEditor({
+                // @todo invalid HTML, fix this
                 contentBefore: "<ul><li>ab<li>c[d</li></ul><p>ef</p><ol><li>g]h</li></ol>",
                 stepFunction: toggleOrderedList,
                 contentAfter: "<ol><li>ab</li><li>c[d</li><li>ef</li><li>g]h</li></ol>",
@@ -258,22 +229,17 @@ describe("Mixed", () => {
         }
     );
 
-    test.todo(
-        "should turn an ordered list, a paragraph and an unordered list item into one ordered list with all three as list items",
-        async () => {
-            await testEditor({
-                contentBefore: "<ol><li>a[b</li></ol><p>cd</p><ul><li>e]f</li><li>gh</li></ul>",
-                stepFunction: toggleOrderedList,
-                contentAfter: "<ol><li>a[b</li><li>cd</li><li>e]f</li><li>gh</li></ol>",
-            });
-        }
-    );
+    test("should turn an ordered list, a paragraph and an unordered list item into one ordered list with all three as list items", async () => {
+        await testEditor({
+            contentBefore: "<ol><li>a[b</li></ol><p>cd</p><ul><li>e]f</li><li>gh</li></ul>",
+            stepFunction: toggleOrderedList,
+            contentAfter: "<ol><li>a[b</li><li>cd</li><li>e]f</li><li>gh</li></ol>",
+        });
+    });
 
-    test.todo(
-        "should turn an unordered list within an unordered list into an ordered list within an unordered list",
-        async () => {
-            await testEditor({
-                contentBefore: unformat(`
+    test("should turn an unordered list within an unordered list into an ordered list within an unordered list", async () => {
+        await testEditor({
+            contentBefore: unformat(`
                     <ul>
                         <li>ab</li>
                         <li class="oe-nested">
@@ -284,8 +250,8 @@ describe("Mixed", () => {
                         </li>
                         <li>gh</li>
                     </ul>`),
-                stepFunction: toggleOrderedList,
-                contentAfter: unformat(`
+            stepFunction: toggleOrderedList,
+            contentAfter: unformat(`
                     <ul>
                         <li>ab</li>
                         <li class="oe-nested">
@@ -296,15 +262,12 @@ describe("Mixed", () => {
                         </li>
                         <li>gh</li>
                     </ul>`),
-            });
-        }
-    );
+        });
+    });
 
-    test.todo(
-        "should turn an unordered list with mixed nested elements into an ordered list with only unordered elements",
-        async () => {
-            await testEditor({
-                contentBefore: unformat(`
+    test("should turn an unordered list with mixed nested elements into an ordered list with only unordered elements", async () => {
+        await testEditor({
+            contentBefore: unformat(`
                     <ul>
                         <li>a[b</li>
                         <li>cd</li>
@@ -329,8 +292,8 @@ describe("Mixed", () => {
                         <li>q]r</li>
                         <li>st</li>
                     </ul>`),
-                stepFunction: toggleOrderedList,
-                contentAfter: unformat(`
+            stepFunction: toggleOrderedList,
+            contentAfter: unformat(`
                     <ol>
                         <li>a[b</li>
                         <li>cd</li>
@@ -355,11 +318,10 @@ describe("Mixed", () => {
                         <li>q]r</li>
                         <li>st</li>
                     </ol>`),
-            });
-        }
-    );
+        });
+    });
 
-    test.todo("should convert within mixed lists", async () => {
+    test("should convert within mixed lists", async () => {
         await testEditor({
             contentBefore: unformat(`
                     <ul>
@@ -415,9 +377,8 @@ describe("Mixed", () => {
         });
     });
 
-    test.todo("should turn an unordered list into a checklist", async () => {
+    test("should turn an unordered list into a checklist", async () => {
         await testEditor({
-            removeCheckIds: true,
             contentBefore: "<ul><li>a[b]c</li></ul>",
             stepFunction: toggleCheckList,
             contentAfter: '<ul class="o_checklist"><li>a[b]c</li></ul>',
