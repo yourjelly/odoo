4e2fd78ab372b26ae2303d3630af3ffc35c77bc1
Nicolas Bayet <nby@odoo.com>
2024-01-25 13:13:30 +0100

    implement insert_line_break

---
+++ addons/html_editor/static/src/editor/utils/zws.js
@@ -0,0 +1,33 @@
+/** @odoo-module */
+
+import { getDeepRange } from "./selection";
+
+/**
+ * Set a deep selection that split the text and collapse it if only one ZWS is
+ * selected.
+ *
+ * @param {HTMLElement} editable
+ * @param {Selection} selection
+ * @returns {boolean} true if the selection has only one ZWS.
+ */
+export function collapseIfZWS(editable, selection) {
+    const range = getDeepRange(editable, {
+        selection,
+        splitText: true,
+        select: true,
+        correctTripleClick: true,
+    });
+
+    if (
+        range &&
+        range.startContainer === range.endContainer &&
+        range.endContainer.nodeType === Node.TEXT_NODE &&
+        range.cloneContents().textContent === "\u200B"
+    ) {
+        // We Collapse the selection and bypass deleteRange
+        // if the range content is only one ZWS.
+        selection.collapseToStart();
+        return true;
+    }
+    return false;
+}
