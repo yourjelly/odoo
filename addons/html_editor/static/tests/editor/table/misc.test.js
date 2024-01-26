7f289099362cca06966368959baa3a6285996985
Géry Debongnie <ged@odoo.com>
2024-01-26 14:01:35 +0100

    fix table code not taking cols/rows into account

---
+++ addons/html_editor/static/tests/editor/table/misc.test.js
@@ -0,0 +1,15 @@
+/** @odoo-module */
+
+import { expect, test } from "@odoo/hoot";
+import { setupEditor } from "../../helpers";
+
+function insertTable(editor, cols, rows) {
+    editor.dispatch("INSERT_TABLE", { cols, rows });
+}
+
+test("can insert a table", async () => {
+    const { el, editor } = await setupEditor("<p>hello[]</p>", {});
+    insertTable(editor, 4, 3);
+    expect(el.querySelectorAll("tr").length).toBe(3);
+    expect(el.querySelectorAll("td").length).toBe(12);
+});
