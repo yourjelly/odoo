09e02e7fed34b35277dd80bd0d9fbfa2a7b4ad38
Kashyap Patel <kasp@odoo.com>
2024-01-29 10:57:17 +0530

    [IMP] stock_barcode: hide theoric quantity and improved inventory adjustments
    Before this commit
    ==================
    - There were no settings to hide theoric quantity.
    - Another issue was that when scanning a location for inventory adjustment and
      finding nothing assigned to them, only the scanned products were displayed.
      This creates a problem where products with expected quantities (qty>0) that
      couldn't be physically found were missed during scanning.
    
    After this commit
    ==================
    - Added settings to hide theoric quantity, which allows to hide the expected
      quantity of a product to count in a location.
    - When scanning a location during an inventory adjustment, the module now loads
      all quants from that location.
    
    task-3479636

--- addons/stock/wizard/stock_request_count.py
+++ addons/stock/wizard/stock_request_count.py
@@ -18,10 +18,15 @@ class StockRequestCount(models.TransientModel):
 
     def action_request_count(self):
         for count_request in self:
+            quants_to_count = count_request._get_quants_to_count()
             if count_request.set_count == 'set':
-                count_request.quant_ids.filtered(lambda q: not q.inventory_quantity_set).action_set_inventory_quantity()
-            count_request.quant_ids.with_context(inventory_mode=True).write(
+                quants_to_count.filtered(lambda q: not q.inventory_quantity_set).action_set_inventory_quantity()
+            quants_to_count.with_context(inventory_mode=True).write(
                 count_request._get_values_to_write())
+
+    def _get_quants_to_count(self):
+        self.ensure_one()
+        return self.quant_ids
 
     def _get_values_to_write(self):
         values = {
