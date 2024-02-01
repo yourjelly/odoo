9ca1005927b8f75e729f6941f0964f238cb9ba59
bram1000 <brvg@odoo.com>
2024-02-01 14:08:03 +0100

    Fixed no access bug

--- addons/webauthn/models/users_webauthn.py
+++ addons/webauthn/models/users_webauthn.py
@@ -7,6 +7,8 @@ class UsersWebauthKeys(models.Model):
 
     @property
     def SELF_WRITEABLE_FIELDS(self):
-        return super().SELF_WRITEABLE_FIELDS + [
-            'webauth_key_ids'
-        ]
+        return super().SELF_WRITEABLE_FIELDS + ['webauth_key_ids']
+
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + ['webauth_key_ids']
