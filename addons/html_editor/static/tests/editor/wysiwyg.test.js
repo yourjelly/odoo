2da05d13dd92baacb07722f5d2ace0ba71a0d407
Géry Debongnie <ged@odoo.com>
2024-02-06 12:39:24 +0100

    fix: make sure toolbar buttons are properly updated on selection change

--- addons/html_editor/static/tests/editor/wysiwyg.test.js
+++ addons/html_editor/static/tests/editor/wysiwyg.test.js
@@ -2,10 +2,10 @@ import { describe, expect, getFixture, test } from "@odoo/hoot";
 import { makeMockEnv } from "@web/../tests/_framework/env_test_helpers";
 import { Wysiwyg, useWysiwyg, wysiwyg } from "@html_editor/editor/wysiwyg";
 import { Component, xml } from "@odoo/owl";
-import { mountWithCleanup } from "@web/../tests/web_test_helpers";
+import { contains, mountWithCleanup } from "@web/../tests/web_test_helpers";
 import { animationFrame } from "@odoo/hoot-mock";
-import { getContent, setContent } from "../test_helpers/selection";
-import { queryOne } from "@odoo/hoot-dom";
+import { getContent, setContent, setSelection } from "../test_helpers/selection";
+import { click, queryOne } from "@odoo/hoot-dom";
 
 describe("wysiwig function", () => {
     test("can edit a element with wysiwyg function", async () => {
@@ -55,23 +55,37 @@ describe("wysiwig hook", () => {
 });
 
 describe("Wysiwyg Component", () => {
+    async function setupWysiwyg(props = {}) {
+        const wysiwyg = await mountWithCleanup(Wysiwyg, { props });
+        const el = queryOne(".odoo-editor-editable");
+        if (props.content) {
+            // force selection to be put properly
+            setContent(el, props.content);
+        }
+        return { wysiwyg, el };
+    }
+
     test("Wysiwyg component can be instantiated", async () => {
-        await mountWithCleanup(Wysiwyg);
+        const { el } = await setupWysiwyg();
         expect(".o-wysiwyg").toHaveCount(1);
         expect(".odoo-editor-editable").toHaveCount(1);
         expect(".o-we-toolbar").toHaveCount(0);
 
         // set the selection to a range, and check that the toolbar
         // is opened
-        const el = queryOne(".odoo-editor-editable");
         expect(getContent(el)).toBe("");
         setContent(el, "hello [hoot]");
         await animationFrame();
         expect(".o-we-toolbar").toHaveCount(1);
     });
 
+    test("Wysiwyg component can be instantiated with initial content", async () => {
+        const { el } = await setupWysiwyg({ content: "<p>hello rodolpho</p>" });
+        expect(el.innerHTML).toBe(`<p>hello rodolpho</p>`);
+    });
+
     test("Wysiwyg component can be instantiated with a permanent toolbar", async () => {
-        await mountWithCleanup(Wysiwyg, { props: { toolbar: true } });
+        await setupWysiwyg({ toolbar: true });
         expect(".o-wysiwyg").toHaveCount(1);
         expect(".odoo-editor-editable").toHaveCount(1);
         // no toolbar yet, since toolbar needs editor to be ready => wait for
@@ -80,5 +94,47 @@ describe("Wysiwyg Component", () => {
         await animationFrame();
         // toolbar should be ready now
         expect(".o-we-toolbar").toHaveCount(1);
+    });
+
+    test("wysiwyg with toolbar: buttons react to selection change", async () => {
+        const { el } = await setupWysiwyg({ toolbar: true, content: "<p>test some text</p>" });
+        expect(el.innerHTML).toBe(`<p>test some text</p>`);
+
+        setContent(el, "<p>test [some] text</p>");
+        await animationFrame();
+        expect(".o-we-toolbar .btn[name='bold']").not.toHaveClass("active");
+
+        await contains(".btn[name='bold']").click();
+        expect(getContent(el)).toBe("<p>test <strong>[some]</strong> text</p>");
+        expect(".o-we-toolbar .btn[name='bold']").toHaveClass("active");
+
+        setContent(el, "<p>test <strong>some</strong> text[]</p>");
+        await animationFrame();
+        expect(".o-we-toolbar .btn[name='bold']").not.toHaveClass("active");
+
+        setContent(el, "<p>test <strong>some[]</strong> text</p>");
+        await animationFrame();
+        expect(".o-we-toolbar .btn[name='bold']").toHaveClass("active");
+    });
+
+    test("wysiwyg with toolbar: properly behave when selection leaves editable", async () => {
+        const { el } = await setupWysiwyg({
+            toolbar: true,
+            content: "<p>test <strong>[some]</strong> text</p>",
+        });
+
+        await animationFrame();
+        expect(".o-we-toolbar .btn[name='bold']").toHaveClass("active");
+
+        click(document.body);
+        setSelection({
+            anchorNode: document.body,
+            anchorOffset: 0,
+            focusNode: document.body,
+            focusOffset: 0,
+        });
+        await animationFrame();
+        expect(getContent(el)).toBe("<p>test <strong>some</strong> text</p>");
+        expect(".o-we-toolbar .btn[name='bold']").toHaveClass("active");
     });
 });
