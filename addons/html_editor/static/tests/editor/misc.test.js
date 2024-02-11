1c4f4fc9d091e25e9932e683d235a0c078b62480
Géry Debongnie <ged@odoo.com>
2024-02-11 19:43:04 +0100

    properly cleanup handlers after destruction

--- addons/html_editor/static/tests/editor/misc.test.js
+++ addons/html_editor/static/tests/editor/misc.test.js
@@ -1,6 +1,8 @@
 import { expect, test } from "@odoo/hoot";
 import { setupEditor, testEditor } from "../test_helpers/editor";
 import { getContent, setContent } from "../test_helpers/selection";
+import { Plugin } from "../../src/editor/plugin";
+import { click } from "@odoo/hoot-dom";
 
 test("can instantiate a Editor", async () => {
     const { el, editor } = await setupEditor("<p>hel[lo] world</p>", {});
@@ -54,45 +56,23 @@ test.todo(
     }
 );
 
-/**
- * To write:
- *
- * EDITOR TESTS
- *
- * (make sure editor is destroyed at end of test)
- *
- * - check el is no content editable, attach editor to el, check that it is
- * - create editor, add listener on CONTENT_UPDATE, attach el, modify el content,
- *   check that listener is called (html change and text node change)
- * - same as above, but add listener after attaching el
- * - create editor, attach el, create range in el, dispatch('SET_BOLD'),
- *   check that range is bold
- *
- * USE_EDITOR TESTS
- *
- * - useEditor on ref, check that ref is content editable
- * - useEditor on ref with initialHtml => check that it is properly set (as html!)
- *
- *- USE_WYSIWYG TESTS
- */
+test("event handlers are properly cleaned up after destruction", async () => {
+    let count = 0;
+    class TestHandlerPlugin extends Plugin {
+        static name = "test_handler";
 
-// import { expect, test, after } from "@odoo/hoot";
-// import { Editor } from "@html_editor/editor/editor";
-// import { getFixture } from "@odoo/hoot/helpers";
+        setup() {
+            this.addDomListener(document.body, "click", () => count++);
+        }
+    }
 
-// function prepare(str) {
-//     const fixture = getFixture();
-//     fixture.innerHTML = str;
-//     const editor = new Editor(fixture);
-//     after(() => editor.destroy());
-//     return { fixture, editor };
-// }
+    const { editor } = await setupEditor("<p></p>", { Plugins: [TestHandlerPlugin] });
+    expect(count).toBe(0);
 
-// test("setting bold on a text node", () => {
-//     const { editor, fixture } = prepare("some text");
-//     const r = new Range();
-//     r.setStart(fixture.firstChild, 2);
-//     r.setEnd(fixture.firstChild, 4);
-//     editor.setBold(r);
-//     expect(fixture.innerHTML).toBe("so<strong>me</strong> text");
-// });
+    click(document.body);
+    expect(count).toBe(1);
+
+    editor.destroy();
+    click(document.body);
+    expect(count).toBe(1);
+});
