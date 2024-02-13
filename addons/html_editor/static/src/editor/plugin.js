ca7bc82d363ceca17cbf38d92b9bc1d9cc8525ec
Géry Debongnie <ged@odoo.com>
2024-02-13 12:36:20 +0100

    add some typings to get autocomplete

--- addons/html_editor/static/src/editor/plugin.js
+++ addons/html_editor/static/src/editor/plugin.js
@@ -1,3 +1,20 @@
+/**
+ * @type { import("./editor").Editor } Editor
+ * @type { import("./core/selection_plugin").SelectionPlugin } SelectionPlugin
+ * @type { import("./core/dom_plugin").DomPlugin } DomPlugin
+ * @type { import("./core/split_block_plugin").SplitBlockPlugin } SplitBlockPlugin
+ * @type { import("./core/overlay_plugin").OverlayPlugin } OverlayPlugin
+ *
+ * @typedef { Object } SharedMethods
+ * @property { SelectionPlugin.prototype.getEditableSelection } getEditableSelection
+ * @property { SelectionPlugin.prototype.setSelection } setSelection
+ * @property { SelectionPlugin.prototype.setCursorStart } setCursorStart
+ * @property { SelectionPlugin.prototype.setCursorEnd } setCursorEnd
+ * @property { DomPlugin.prototype.dom_insert } dom_insert
+ * @property { SplitBlockPlugin.prototype.splitElementBlock } splitElementBlock
+ * @property { OverlayPlugin.prototype.createOverlay } createOverlay
+ */
+
 export class Plugin {
     static name = "";
     static dependencies = [];
@@ -6,16 +23,19 @@ export class Plugin {
     /**
      * @param {Document} document
      * @param {HTMLElement} editable
-     * @param {*} shared
-     * @param {*} dispatch
+     * @param {SharedMethods} shared
+     * @param { Editor.prototype.dispatch } dispatch
      * @param {*} config
      * @param {*} services
      */
     constructor(document, editable, shared, dispatch, config, services) {
+        /** @type { Document } **/
         this.document = document;
+        /** @type { HTMLElement } **/
         this.editable = editable;
         this.config = config;
         this.services = services;
+        /** @type { SharedMethods } **/
         this.shared = shared;
         this.dispatch = dispatch;
         this._cleanups = [];
