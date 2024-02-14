2d0adea21cc93b4ba2e88c375b43b50ca0d7d4d2
Gurpreet Singh <gusi@odoo.com>
2024-02-14 18:35:35 +0530

    [FIX] google_calendar: fix the sync after pause

--- addons/google_calendar/models/res_config_settings.py
+++ addons/google_calendar/models/res_config_settings.py
@@ -11,3 +11,12 @@ class ResConfigSettings(models.TransientModel):
     cal_client_secret = fields.Char("Client_key", config_parameter='google_calendar_client_secret', default='')
     cal_sync_paused = fields.Boolean("Google Synchronization Paused", config_parameter='google_calendar_sync_paused',
         help="Indicates if synchronization with Google Calendar is paused or not.")
+
+def write(self, vals):
+    configs = super().write(vals)
+    if 'cal_sync_paused' in vals and not vals['cal_sync_paused']:
+        # Check if the user has a Google Calendar account and if it is authenticated
+        if self.env.user.google_calendar_account_id and self.env.user.google_calendar_account_id._google_calendar_authenticated():
+            self.env['calendar.recurrence']._restart_google_sync()
+            self.env['calendar.event']._restart_google_sync()
+    return configs
