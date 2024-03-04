fe4301d8fea92303d46afec3653f88bfefde2c0c
FrancoisGe <fge@odoo.com>
2024-03-04 17:00:36 +0100

    remove  comment

--- addons/html_editor/static/tests/editor/comment.test.js
+++ addons/html_editor/static/tests/editor/comment.test.js
@@ -1,7 +1,7 @@
 import { test } from "@odoo/hoot";
 import { testEditor } from "../test_helpers/editor";
 
-test.todo("should remove comment node inside editable content during sanitize", async () => {
+test("should remove comment node inside editable content during sanitize", async () => {
     await testEditor({
         contentBefore: "<p>ab<!-- comment -->cd</p>",
         contentAfter: "<p>abcd</p>",
