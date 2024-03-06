bfca95b94aaedf9fa928579f3b9c88813b2712c2
FrancoisGe <fge@odoo.com>
2024-03-06 11:14:29 +0100

    unskip few tests

--- addons/html_editor/static/tests/editor/list/paragraph_break.test.js
+++ addons/html_editor/static/tests/editor/list/paragraph_break.test.js
@@ -1,11 +1,7 @@
-import { test, describe } from "@odoo/hoot";
+import { describe, test } from "@odoo/hoot";
 import { testEditor } from "../../test_helpers/editor";
 import { unformat } from "../../test_helpers/format";
-import { splitBlock } from "../../test_helpers/user_actions";
-
-function insertText() {
-    throw new Error("need a proper implementation");
-}
+import { insertText, splitBlock } from "../../test_helpers/user_actions";
 
 describe("Selection collapsed", () => {
     describe("Ordered", () => {
@@ -132,7 +128,7 @@ describe("Selection collapsed", () => {
             });
         });
         describe("With attributes", () => {
-            test.todo("should add two list items at the end of a list with a class", async () => {
+            test("should add two list items at the end of a list with a class", async () => {
                 await testEditor({
                     contentBefore: '<ol class="a"><li>abc[]</li></ol>',
                     stepFunction: async (editor) => {
@@ -144,7 +140,7 @@ describe("Selection collapsed", () => {
                 });
             });
 
-            test.todo("should add two list items with a class at the end of a list", async () => {
+            test("should add two list items with a class at the end of a list", async () => {
                 await testEditor({
                     contentBefore: '<ol><li class="a">abc[]</li></ol>',
                     stepFunction: async (editor) => {
@@ -186,11 +182,9 @@ describe("Selection collapsed", () => {
                 });
             });
 
-            test.todo(
-                "should add two list items with a font at the end of a list within a list",
-                async () => {
-                    await testEditor({
-                        contentBefore: unformat(`
+            test("should add two list items with a font at the end of a list within a list", async () => {
+                await testEditor({
+                    contentBefore: unformat(`
                             <ul>
                                 <li>ab</li>
                                 <li class="oe-nested">
@@ -202,12 +196,12 @@ describe("Selection collapsed", () => {
                                 </li>
                                 <li>ef</li>
                             </ul>`),
-                        stepFunction: async (editor) => {
-                            await splitBlock(editor);
-                            await insertText(editor, "b");
-                            await splitBlock(editor);
-                        },
-                        contentAfter: unformat(`
+                    stepFunction: async (editor) => {
+                        await splitBlock(editor);
+                        await insertText(editor, "b");
+                        await splitBlock(editor);
+                    },
+                    contentAfter: unformat(`
                             <ul>
                                 <li>ab</li>
                                 <li class="oe-nested">
@@ -219,9 +213,8 @@ describe("Selection collapsed", () => {
                                 </li>
                                 <li>ef</li>
                             </ul>`),
-                    });
-                }
-            );
+                });
+            });
         });
     });
     describe("Unordered", () => {
@@ -310,7 +303,7 @@ describe("Selection collapsed", () => {
             });
         });
         describe("With attributes", () => {
-            test.todo("should add two list items at the end of a list with a class", async () => {
+            test("should add two list items at the end of a list with a class", async () => {
                 await testEditor({
                     contentBefore: '<ul class="a"><li>abc[]</li></ul>',
                     stepFunction: async (editor) => {
@@ -322,7 +315,7 @@ describe("Selection collapsed", () => {
                 });
             });
 
-            test.todo("should add two list items with a class at the end of a list", async () => {
+            test("should add two list items with a class at the end of a list", async () => {
                 await testEditor({
                     contentBefore: '<ul><li class="a">abc[]</li></ul>',
                     stepFunction: async (editor) => {
@@ -536,38 +529,32 @@ describe("Selection collapsed", () => {
         });
         describe("With attributes", () => {
             describe("after unchecked item", () => {
-                test.todo(
-                    "should add two list items at the end of a checklist with a class",
-                    async () => {
-                        await testEditor({
-                            contentBefore: '<ul class="checklist a"><li>abc[]</li></ul>',
-                            stepFunction: async (editor) => {
-                                await splitBlock(editor);
-                                await insertText(editor, "d");
-                                await splitBlock(editor);
-                            },
-                            contentAfter:
-                                '<ul class="checklist a"><li>abc</li><li>d</li><li>[]<br></li></ul>',
-                        });
-                    }
-                );
+                test("should add two list items at the end of a checklist with a class", async () => {
+                    await testEditor({
+                        contentBefore: '<ul class="checklist a"><li>abc[]</li></ul>',
+                        stepFunction: async (editor) => {
+                            await splitBlock(editor);
+                            await insertText(editor, "d");
+                            await splitBlock(editor);
+                        },
+                        contentAfter:
+                            '<ul class="checklist a"><li>abc</li><li>d</li><li>[]<br></li></ul>',
+                    });
+                });
 
-                test.todo(
-                    "should add two list items with a class at the end of a checklist",
-                    async () => {
-                        await testEditor({
-                            removeCheckIds: true,
-                            contentBefore: '<ul class="o_checklist"><li class="a">abc[]</li></ul>',
-                            stepFunction: async (editor) => {
-                                await splitBlock(editor);
-                                await insertText(editor, "d");
-                                await splitBlock(editor);
-                            },
-                            contentAfter:
-                                '<ul class="o_checklist"><li class="a">abc</li><li class="a">d</li><li class="a">[]<br></li></ul>',
-                        });
-                    }
-                );
+                test("should add two list items with a class at the end of a checklist", async () => {
+                    await testEditor({
+                        removeCheckIds: true,
+                        contentBefore: '<ul class="o_checklist"><li class="a">abc[]</li></ul>',
+                        stepFunction: async (editor) => {
+                            await splitBlock(editor);
+                            await insertText(editor, "d");
+                            await splitBlock(editor);
+                        },
+                        contentAfter:
+                            '<ul class="o_checklist"><li class="a">abc</li><li class="a">d</li><li class="a">[]<br></li></ul>',
+                    });
+                });
 
                 test.todo("should create list items after one with a block in it", async () => {
                     await testEditor({
@@ -600,12 +587,10 @@ describe("Selection collapsed", () => {
                     });
                 });
 
-                test.todo(
-                    "should add two list items with a font at the end of a checklist within a checklist",
-                    async () => {
-                        await testEditor({
-                            removeCheckIds: true,
-                            contentBefore: unformat(`
+                test("should add two list items with a font at the end of a checklist within a checklist", async () => {
+                    await testEditor({
+                        removeCheckIds: true,
+                        contentBefore: unformat(`
                             <ul class="o_checklist">
                                 <li>ab</li>
                                 <li class="oe-nested">
@@ -617,12 +602,12 @@ describe("Selection collapsed", () => {
                                 </li>
                                 <li class="o_checked">ef</li>
                             </ul>`),
-                            stepFunction: async (editor) => {
-                                await splitBlock(editor);
-                                await insertText(editor, "0");
-                                await splitBlock(editor);
-                            },
-                            contentAfter: unformat(`
+                        stepFunction: async (editor) => {
+                            await splitBlock(editor);
+                            await insertText(editor, "0");
+                            await splitBlock(editor);
+                        },
+                        contentAfter: unformat(`
                             <ul class="o_checklist">
                                 <li>ab</li>
                                 <li class="oe-nested">
@@ -634,50 +619,43 @@ describe("Selection collapsed", () => {
                                 </li>
                                 <li class="o_checked">ef</li>
                             </ul>`),
-                        });
-                    }
-                );
+                    });
+                });
             });
             describe("after checked item", () => {
                 // TODO: do not clone the `IsChecked` modifier
                 // on split (waiting for `preserve` property of
                 // `Modifier`).
 
-                test.todo(
-                    "should add two list items at the end of a checklist with a class",
-                    async () => {
-                        await testEditor({
-                            removeCheckIds: true,
-                            contentBefore:
-                                '<ul class="checklist a"><li class="o_checked">abc[]</li></ul>',
-                            stepFunction: async (editor) => {
-                                await splitBlock(editor);
-                                await insertText(editor, "d");
-                                await splitBlock(editor);
-                            },
-                            contentAfter:
-                                '<ul class="checklist a"><li class="o_checked">abc</li><li>d</li><li>[]<br></li></ul>',
-                        });
-                    }
-                );
+                test("should add two list items at the end of a checklist with a class", async () => {
+                    await testEditor({
+                        removeCheckIds: true,
+                        contentBefore:
+                            '<ul class="checklist a"><li class="o_checked">abc[]</li></ul>',
+                        stepFunction: async (editor) => {
+                            await splitBlock(editor);
+                            await insertText(editor, "d");
+                            await splitBlock(editor);
+                        },
+                        contentAfter:
+                            '<ul class="checklist a"><li class="o_checked">abc</li><li>d</li><li>[]<br></li></ul>',
+                    });
+                });
 
-                test.todo(
-                    "should add two list items with a class at the end of a checklist",
-                    async () => {
-                        await testEditor({
-                            removeCheckIds: true,
-                            contentBefore:
-                                '<ul class="o_checklist"><li class="a o_checked">abc[]</li></ul>',
-                            stepFunction: async (editor) => {
-                                await splitBlock(editor);
-                                await insertText(editor, "d");
-                                await splitBlock(editor);
-                            },
-                            contentAfter:
-                                '<ul class="o_checklist"><li class="a o_checked">abc</li><li class="a">d</li><li class="a">[]<br></li></ul>',
-                        });
-                    }
-                );
+                test("should add two list items with a class at the end of a checklist", async () => {
+                    await testEditor({
+                        removeCheckIds: true,
+                        contentBefore:
+                            '<ul class="o_checklist"><li class="a o_checked">abc[]</li></ul>',
+                        stepFunction: async (editor) => {
+                            await splitBlock(editor);
+                            await insertText(editor, "d");
+                            await splitBlock(editor);
+                        },
+                        contentAfter:
+                            '<ul class="o_checklist"><li class="a o_checked">abc</li><li class="a">d</li><li class="a">[]<br></li></ul>',
+                    });
+                });
                 test.skip("should add two list items with a class and a div at the end of a checklist", async () => {
                     await testEditor({
                         removeCheckIds: true,
@@ -707,12 +685,10 @@ describe("Selection collapsed", () => {
                     });
                 });
 
-                test.todo(
-                    "should add two list items with a font at the end of a checklist within a checklist",
-                    async () => {
-                        await testEditor({
-                            removeCheckIds: true,
-                            contentBefore: unformat(`
+                test("should add two list items with a font at the end of a checklist within a checklist", async () => {
+                    await testEditor({
+                        removeCheckIds: true,
+                        contentBefore: unformat(`
                             <ul class="o_checklist">
                                 <li class="o_checked">ab</li>
                                 <li class="oe-nested">
@@ -724,12 +700,12 @@ describe("Selection collapsed", () => {
                                 </li>
                                 <li class="o_checked">ef</li>
                             </ul>`),
-                            stepFunction: async (editor) => {
-                                await splitBlock(editor);
-                                await insertText(editor, "0");
-                                await splitBlock(editor);
-                            },
-                            contentAfter: unformat(`
+                        stepFunction: async (editor) => {
+                            await splitBlock(editor);
+                            await insertText(editor, "0");
+                            await splitBlock(editor);
+                        },
+                        contentAfter: unformat(`
                             <ul class="o_checklist">
                                 <li class="o_checked">ab</li>
                                 <li class="oe-nested">
@@ -741,9 +717,8 @@ describe("Selection collapsed", () => {
                                 </li>
                                 <li class="o_checked">ef</li>
                             </ul>`),
-                        });
-                    }
-                );
+                    });
+                });
             });
         });
     });
