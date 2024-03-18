68fa8c044f27c31393f14870bcf7c569161cd36f
Géry Debongnie <ged@odoo.com>
2024-03-18 16:56:20 +0100

    split collab helpers into separate helper file

--- addons/html_editor/static/tests/editor/collaboration.test.js
+++ addons/html_editor/static/tests/editor/collaboration.test.js
@@ -1,320 +1,15 @@
-/** @odoo-module */
-
-import { describe, test, expect } from "@odoo/hoot";
+import { describe, expect, test } from "@odoo/hoot";
 import { Deferred } from "@web/core/utils/concurrency";
-import { CollaborationPlugin } from "../../src/editor/core/collaboration_plugin";
-import { createDOMPathGenerator } from "../../src/editor/utils/dom_traversal";
-import { DIRECTIONS } from "../../src/editor/utils/position";
-import { setupEditor } from "../test_helpers/editor";
-import { unformat } from "../test_helpers/format";
-import { parseHTML } from "../../src/editor/utils/html";
 import { Plugin } from "../../src/editor/plugin";
+import { parseHTML } from "../../src/editor/utils/html";
+import { unformat } from "../test_helpers/format";
 import { addStep, undo } from "../test_helpers/user_actions";
-import { registry } from "@web/core/registry";
-import { HistoryPlugin } from "../../src/editor/core/history_plugin";
-
-/**
- *
- * @typedef { import("../../src/editor/editor").Editor } Editor
- *
- * @typedef { Object } PeerInfo
- * @property { string } peerId
- * @property { import("../../src/editor/core/history_plugin").HistoryStep[] } steps
- * @property { Editor } editor
- * @property { import("../../src/editor/core/collaboration_plugin").CollaborationPlugin } collaborationPlugin
- * @property { import("../../src/editor/plugin").HistoryPlugin } historyPlugin
- *
- * @typedef { Object } MultiEditorSpec
- * @property { string[] } peerIds
- * @property { string } contentBefore
- * @property { string } contentAfter
- * @property { Plugin[] } Plugins
- * @property { (peerInfos: Record<string, PeerInfo>) => Promise<void> } afterCreate
- * @property { (peerInfos: Record<string, PeerInfo>) => Promise<void> } afterCursorInserted
- *
- * @typedef { Object } EditorSelection
- * @property { Node } anchorNode
- * @property { number } anchorOffset
- * @property { Node } focusNode
- * @property { number } focusOffset
- */
-
-function historyMissingParentSteps(peerInfos, peerInfo, { step, fromStepId }) {
-    const missingSteps = peerInfos[step.peerId].collaborationPlugin.historyGetMissingSteps({
-        fromStepId,
-        toStepId: step.id,
-    });
-    if (missingSteps === -1 || !missingSteps.length) {
-        throw new Error("Impossible to get the missing steps.");
-    }
-    peerInfo.collaborationPlugin.onExternalHistorySteps(missingSteps.concat([step]));
-}
-
-/**
- * @param { MultiEditorSpec } spec
- * @returns { Promise<void> }
- */
-const testMultiEditor = async (spec) => {
-    /** @type { Record<string, PeerInfo> } */
-    const peerInfos = {};
-    const peerIds = spec.peerIds;
-    const initialHystoryPluginGenerateId = CollaborationPlugin.prototype.generateId;
-
-    for (const peerId of peerIds) {
-        const peerInfo = {
-            peerId,
-            steps: [],
-        };
-        peerInfos[peerId] = peerInfo;
-        let n = 0;
-        HistoryPlugin.prototype.generateId = () => `fake_id_${n++}`;
-        let selection;
-        const defaultPlugins = registry.category("phoenix_plugins").getAll();
-        const base = await setupEditor(spec.contentBefore, {
-            inIFrame: true,
-            onMounted: (editable) => {
-                selection = parseMultipleTextualSelection(editable, peerId);
-            },
-            config: {
-                Plugins: [
-                    ...defaultPlugins,
-                    CollaborationPlugin,
-                    class TestHistoryAdapterPlugin extends Plugin {
-                        static name = "test-history-adapter";
-                        handleCommand(commandId, payload) {
-                            switch (commandId) {
-                                case "COLLABORATION_STEP_ADDED":
-                                    peerInfo.steps.push(payload);
-                                    break;
-                                case "HISTORY_MISSING_PARENT_STEP":
-                                    historyMissingParentSteps(peerInfos, peerInfo, payload);
-                                    break;
-                            }
-                        }
-                    },
-                    ...(spec.Plugins || []),
-                ],
-                peerId,
-            },
-        });
-        peerInfo.editor = base.editor;
-        if (selection && selection.anchorNode) {
-            base.editor.shared.setSelection(selection);
-        } else {
-            base.editor.document.getSelection().removeAllRanges();
-        }
-        const getPlugin = (name) => base.editor.plugins.find((x) => x.constructor.name === name);
-        peerInfo.collaborationPlugin = getPlugin("collaboration");
-        peerInfo.historyPlugin = getPlugin("history");
-    }
-
-    const peerInfosList = Object.values(peerInfos);
-
-    // Init the editors
-
-    // From now, any any step from a peer must have a different ID.
-    let concurentNextId = 1;
-    for (const { historyPlugin } of peerInfosList) {
-        historyPlugin.generateId = () => "fake_concurent_id_" + concurentNextId++;
-        historyPlugin.currentStep.id = historyPlugin.generateId();
-    }
-
-    if (spec.afterCreate) {
-        await spec.afterCreate(peerInfos);
-    }
-
-    renderTextualSelection(peerInfosList);
-
-    for (const peerInfo of peerInfosList) {
-        // todo: should probably use only one method (like clean)
-        const el = peerInfo.editor.editable;
-        peerInfo.editor.dispatch("NORMALIZE", { node: el });
-        peerInfo.editor.dispatch("CLEAN", el);
-        peerInfo.editor.dispatch("MERGE_ADJACENT_NODE", { node: el });
-    }
-
-    if (spec.contentAfter) {
-        for (const peerInfo of peerInfosList) {
-            const value = peerInfo.editor.editable.innerHTML;
-            expect(value).toBe(spec.contentAfter, {
-                message: `error with peer ${peerInfo.peerId}`,
-            });
-        }
-    }
-    if (spec.afterCursorInserted) {
-        await spec.afterCursorInserted(peerInfos);
-    }
-    for (const peerInfo of peerInfosList) {
-        peerInfo.editor.destroy();
-    }
-
-    HistoryPlugin.prototype.generateId = initialHystoryPluginGenerateId;
-};
-
-const applyConcurentActions = (peerInfos, concurentActions) => {
-    const peerInfosList = Object.values(peerInfos);
-    for (const peerInfo of peerInfosList) {
-        if (typeof concurentActions[peerInfo.peerId] === "function") {
-            concurentActions[peerInfo.peerId](peerInfo.editor);
-        }
-    }
-};
-
-const mergePeersSteps = (peerInfos) => {
-    const peerInfosList = Object.values(peerInfos);
-    for (const peerInfoA of peerInfosList) {
-        for (const peerInfoB of peerInfosList) {
-            if (peerInfoA === peerInfoB) {
-                continue;
-            }
-            for (const step of peerInfoB.steps) {
-                peerInfoA.collaborationPlugin.onExternalHistorySteps([
-                    JSON.parse(JSON.stringify(step)),
-                ]);
-            }
-        }
-    }
-};
-
-/**
- * @param {Record<string, PeerInfo>} peerInfos
- */
-const testSameHistory = (peerInfos) => {
-    const peerInfosList = Object.values(peerInfos);
-
-    const PeerInfo = peerInfosList[0];
-    const historyLength = PeerInfo.historyPlugin.steps.length;
-
-    for (const peerInfo of peerInfosList.slice(1)) {
-        expect(peerInfo.historyPlugin.steps.length).toBe(historyLength, {
-            message: "The history size should be the same.",
-        });
-        for (let i = 0; i < historyLength; i++) {
-            expect(PeerInfo.historyPlugin.steps[i].id).toBe(peerInfo.historyPlugin.steps[i].id, {
-                message: `History steps are not consistent accross peers.`,
-            });
-        }
-    }
-};
-
-/**
- * @param {Record<string, PeerInfo>[]} peerInfosList
- */
-function renderTextualSelection(peerInfosList) {
-    const cursorNodes = {};
-    for (const peerInfo of peerInfosList) {
-        const iframeDocument = peerInfo.editor.document;
-        const historyPlugin = peerInfo.historyPlugin;
-        const peerSelection = iframeDocument.getSelection();
-        if (peerSelection.anchorNode === null) {
-            continue;
-        }
-
-        const { anchorNode, anchorOffset, focusNode, focusOffset } = peerSelection;
-
-        const peerId = peerInfo.peerId;
-        const focusNodeId = historyPlugin.nodeToIdMap.get(focusNode);
-        const anchorNodeId = historyPlugin.nodeToIdMap.get(anchorNode);
-        cursorNodes[focusNodeId] = cursorNodes[focusNodeId] || [];
-        cursorNodes[focusNodeId].push({ type: "focus", peerId, offset: focusOffset });
-        cursorNodes[anchorNodeId] = cursorNodes[anchorNodeId] || [];
-        cursorNodes[anchorNodeId].push({ type: "anchor", peerId, offset: anchorOffset });
-    }
-
-    for (const nodeId of Object.keys(cursorNodes)) {
-        cursorNodes[nodeId] = cursorNodes[nodeId].sort((a, b) => {
-            return b.offset - a.offset || b.peerId.localeCompare(a.peerId);
-        });
-    }
-
-    for (const peerInfo of peerInfosList) {
-        const historyPlugin = peerInfo.historyPlugin;
-        for (const [nodeId, cursorsData] of Object.entries(cursorNodes)) {
-            const node = historyPlugin.idToNodeMap.get(nodeId);
-            for (const cursorData of cursorsData) {
-                const cursorString =
-                    cursorData.type === "anchor"
-                        ? `[${cursorData.peerId}}`
-                        : `{${cursorData.peerId}]`;
-                insertCharsAt(cursorString, node, cursorData.offset);
-            }
-        }
-    }
-}
-
-/**
- * Inserts the given characters at the given offset of the given node.
- *
- * @param {string} chars
- * @param {Node} node
- * @param {number} offset
- */
-export function insertCharsAt(chars, node, offset) {
-    const document = node.ownerDocument;
-    if (node.nodeType === Node.TEXT_NODE) {
-        const startValue = node.nodeValue;
-        if (offset < 0 || offset > startValue.length) {
-            throw new Error(`Invalid ${chars} insertion in text node`);
-        }
-        node.nodeValue = startValue.slice(0, offset) + chars + startValue.slice(offset);
-    } else {
-        if (offset < 0 || offset > node.childNodes.length) {
-            throw new Error(`Invalid ${chars} insertion in non-text node`);
-        }
-        const textNode = document.createTextNode(chars);
-        if (offset < node.childNodes.length) {
-            node.insertBefore(textNode, node.childNodes[offset]);
-        } else {
-            node.appendChild(textNode);
-        }
-    }
-}
-
-const inScopeTraversal = createDOMPathGenerator(DIRECTIONS.RIGHT, { inScope: true });
-
-/**
- * @param {Node} rootElement
- * @returns {Record<string, EditorSelection>}
- */
-function parseMultipleTextualSelection(rootElement, peerId) {
-    /** @type { EditorSelection } */
-    const selection = {
-        anchorNode: null,
-        anchorOffset: null,
-        focusNode: null,
-        focusOffset: null,
-    };
-    for (const currentNode of [rootElement, ...inScopeTraversal(rootElement, 0)]) {
-        if (currentNode.nodeType === Node.TEXT_NODE) {
-            // Look for special characters in the text content and remove them.
-            let match;
-            const regex = new RegExp(/(?:\[(\w+)\})|(?:\{(\w+)])/, "gd");
-            while ((match = regex.exec(currentNode.textContent))) {
-                regex.lastIndex = 0;
-                const indexes = match.indices[0];
-
-                if (match[0].includes("}")) {
-                    const selectionPeerId = match[1];
-                    if (selectionPeerId === peerId) {
-                        selection.anchorNode = currentNode;
-                        selection.anchorOffset = indexes[0];
-                    }
-                } else {
-                    const selectionPeerId = match[2];
-                    if (selectionPeerId === peerId) {
-                        selection.focusNode = currentNode;
-                        selection.focusOffset = indexes[0];
-                    }
-                }
-                currentNode.textContent =
-                    currentNode.textContent.slice(0, indexes[0]) +
-                    currentNode.textContent.slice(indexes[1]);
-            }
-        }
-    }
-
-    return selection;
-}
+import {
+    applyConcurrentActions,
+    mergePeersSteps,
+    testMultiEditor,
+    testSameHistory,
+} from "../test_helpers/collaboration";
 
 /**
  * @param {Editor} editor
@@ -337,7 +32,7 @@ describe("Conflict resolution", () => {
             peerIds: ["c1", "c2", "c3"],
             contentBefore: "<p><x>a[c1}{c1]</x><y>e[c2}{c2]</y><z>i[c3}{c3]</z></p>",
             afterCreate: (peerInfos) => {
-                applyConcurentActions(peerInfos, {
+                applyConcurrentActions(peerInfos, {
                     c1: (editor) => {
                         insert(editor, "b");
                         insert(editor, "c");
@@ -365,7 +60,7 @@ describe("Conflict resolution", () => {
             peerIds: ["c1", "c2"],
             contentBefore: "<p>ab[c1}{c1]</p><p>cd[c2}{c2]</p>",
             afterCreate: (peerInfos) => {
-                applyConcurentActions(peerInfos, {
+                applyConcurrentActions(peerInfos, {
                     c1: (editor) => {
                         insert(editor, "e");
                     },
@@ -384,7 +79,7 @@ describe("Conflict resolution", () => {
             peerIds: ["c1", "c2"],
             contentBefore: "<p>ab[c1}{c1]</p><p>cd[c2}{c2]</p>",
             afterCreate: (peerInfos) => {
-                applyConcurentActions(peerInfos, {
+                applyConcurrentActions(peerInfos, {
                     c1: (editor) => {
                         insert(editor, "e");
                         insert(editor, "f");
@@ -405,7 +100,7 @@ describe("Conflict resolution", () => {
             peerIds: ["c1", "c2"],
             contentBefore: "<p>ab[c1}{c1][c2}{c2]c</p>",
             afterCreate: (peerInfos) => {
-                applyConcurentActions(peerInfos, {
+                applyConcurrentActions(peerInfos, {
                     c1: (editor) => {
                         insert(editor, "d");
                     },
@@ -424,7 +119,7 @@ describe("Conflict resolution", () => {
             peerIds: ["c1", "c2"],
             contentBefore: "<p>ab[c1}{c1][c2}{c2]c</p>",
             afterCreate: (peerInfos) => {
-                applyConcurentActions(peerInfos, {
+                applyConcurrentActions(peerInfos, {
                     c1: (editor) => {
                         insert(editor, "d");
                         insert(editor, "e");
@@ -446,7 +141,7 @@ test("should not revert the step of another peer", async () => {
         peerIds: ["c1", "c2"],
         contentBefore: "<p><x>a[c1}{c1]</x><y>b[c2}{c2]</y></p>",
         afterCreate: (peerInfos) => {
-            applyConcurentActions(peerInfos, {
+            applyConcurrentActions(peerInfos, {
                 c1: (editor) => {
                     insert(editor, "c");
                 },
@@ -479,7 +174,7 @@ test("should reset from snapshot", async () => {
             peerInfos.c2.collaborationPlugin.resetFromSteps(steps);
 
             expect(peerInfos.c2.historyPlugin.steps.map((x) => x.id)).toEqual([
-                "fake_concurent_id_1",
+                "fake_concurrent_id_1",
             ]);
             expect(peerInfos.c2.historyPlugin.steps[0].mutations.map((x) => x.id)).toEqual([
                 "fake_id_1",
