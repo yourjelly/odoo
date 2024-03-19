2a59dd5e943ff49cdd4a7f55e2acbb1699ea86e4
oco-odoo <oco@odoo.com>
2024-03-19 11:50:34 +0100

    wip

--- addons/account/static/src/components/closing_journal_lock_field/closing_journal_lock_field.js
+++ addons/account/static/src/components/closing_journal_lock_field/closing_journal_lock_field.js
@@ -4,7 +4,7 @@ import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { registry } from "@web/core/registry";
 import { X2ManyField } from "@web/views/fields/x2many/x2many_field";
 
-export class ClosingJournalLockField extends X2ManyField {
+export class ClosingJournalLockField extends X2ManyField { //TODO OCO pas sûr qu'on ait besoin d'hériter de X2ManyField
 
     static template = "account.ClosingJournalLockField";
     static props = { ...standardFieldProps };
@@ -22,3 +22,16 @@ export const closingJournalLockField = {
 };
 
 registry.category("fields").add("closing_journal_lock", closingJournalLockField);
+
+/**
+TODO OCO
+
+pour l'inspiration, tu peux regarder
+  async _addNewRecord(atFirstPosition) {
+    ===> ici dedans, on crée un nouveau record depuis le js
+
+    ====> en fait, this.list.addNewRecord
+
+===> pour la suppression, le widget de rugo impacte directement this.list, semble-t-il (ou this.props.list ?)
+
+**/
