7e7d265b2bc6cd10d00dd37b65c82a6c6157b05b
Géry Debongnie <ged@odoo.com>
2024-03-22 12:49:55 +0100

    move table ui code to table ui plugin

--- addons/html_editor/static/src/editor/table/table_ui_plugin.js
+++ addons/html_editor/static/src/editor/table/table_ui_plugin.js
@@ -1,13 +1,38 @@
+import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { Plugin } from "../plugin";
 import { closestElement } from "../utils/dom_traversal";
 import { TableMenu } from "./table_menu";
+import { TablePicker } from "./table_picker";
 
+/**
+ * This plugin only contains the table ui feature (table picker, menus, ...).
+ * All actual table manipulation code is located in the table plugin.
+ */
 export class TableUIPlugin extends Plugin {
     static name = "table_ui";
     static dependencies = ["overlay"];
+    static resources = (p) => ({
+        powerboxCommands: [
+            {
+                name: _t("Table"),
+                description: _t("Insert a table"),
+                category: "structure",
+                fontawesome: "fa-table",
+                action(dispatch) {
+                    dispatch("OPEN_TABLE_PICKER");
+                },
+            },
+        ],
+    });
 
     setup() {
+        /** @type {import("../core/overlay_plugin").Overlay} */
+        this.picker = this.shared.createOverlay(TablePicker, {
+            dispatch: this.dispatch,
+            el: this.editable,
+        });
+
         this.activeTd = null;
 
         /** @type {import("../core/overlay_plugin").Overlay} */
@@ -16,6 +41,23 @@ export class TableUIPlugin extends Plugin {
             dispatch: this.dispatch,
         });
         this.addDomListener(this.editable, "mousemove", this.onMouseMove);
+    }
+
+    handleCommand(command) {
+        switch (command) {
+            case "OPEN_TABLE_PICKER":
+                this.openPicker();
+                break;
+        }
+    }
+
+    openPicker() {
+        const range = this.document.getSelection().getRangeAt(0);
+        const rect = range.getBoundingClientRect();
+        if (rect.width === 0 && rect.height === 0 && rect.x === 0) {
+            range.startContainer.parentElement.appendChild(this.document.createElement("br"));
+        }
+        this.picker.open();
     }
 
     onMouseMove(ev) {
