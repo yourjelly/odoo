c2c74c3f2df396536377556458e8ce5d1a21c502
Nicolas Bayet <nby@odoo.com>
2024-04-10 13:46:11 +0200

    nby's touch

--- addons/html_editor/static/src/utils/selection.js
+++ addons/html_editor/static/src/utils/selection.js
@@ -447,15 +447,3 @@ export function getSelectedNodes(editable) {
         ),
     ];
 }
-
-// @deprecated
-export function preserveCursor(document) {
-    const sel = document.getSelection();
-    const cursorPos = [sel.anchorNode, sel.anchorOffset, sel.focusNode, sel.focusOffset];
-    return (replace) => {
-        replace = replace || new Map();
-        cursorPos[0] = replace.get(cursorPos[0]) || cursorPos[0];
-        cursorPos[2] = replace.get(cursorPos[2]) || cursorPos[2];
-        return setSelection(...cursorPos, false);
-    };
-}
