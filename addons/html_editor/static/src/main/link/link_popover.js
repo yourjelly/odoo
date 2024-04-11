ff7bd58df932821b990d0147b328064bb9976b66
Jinjiu Liu <jili@odoo.com>
2024-04-11 13:07:41 +0200

    add link url copy

--- addons/html_editor/static/src/main/link/link_popover.js
+++ addons/html_editor/static/src/main/link/link_popover.js
@@ -1,4 +1,7 @@
+import { _t } from "@web/core/l10n/translation";
 import { Component, useState } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+import { browser } from "@web/core/browser/browser";
 
 export class LinkPopover extends Component {
     static template = "html_editor.linkPopover";
@@ -6,6 +9,7 @@ export class LinkPopover extends Component {
         linkEl: { validate: (el) => el.nodeType === Node.ELEMENT_NODE },
         onApply: Function,
         onRemove: Function,
+        onCopy: Function,
     };
     setup() {
         this.state = useState({
@@ -13,6 +17,7 @@ export class LinkPopover extends Component {
             url: this.props.linkEl.href || "",
             label: this.props.linkEl.textContent || "",
         });
+        this.notificationService = useService("notification");
     }
     onClickApply() {
         this.state.editing = false;
@@ -25,6 +30,14 @@ export class LinkPopover extends Component {
         this.state.editing = true;
         this.state.url = this.props.linkEl.href;
         this.state.label = this.props.linkEl.textContent;
+    }
+    async onClickCopy(ev) {
+        ev.preventDefault();
+        await browser.navigator.clipboard.writeText(this.props.linkEl.href || "");
+        this.notificationService.add(_t("Link copied to clipboard."), {
+            type: "success",
+        });
+        this.props.onCopy();
     }
     onClickRemove() {
         this.props.onRemove();
