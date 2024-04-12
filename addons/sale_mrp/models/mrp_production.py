4b720def30556cb5d562f51a30e5ba4100184249
Mathias Mathy (MAMA) <mama@odoo.com>
2024-04-12 14:26:38 +0200

    [IMP] Chained PG : Purchase Order
    pre-clean + SO-PO MTSO

--- addons/sale_mrp/models/mrp_production.py
+++ addons/sale_mrp/models/mrp_production.py
@@ -12,7 +12,7 @@ class MrpProduction(models.Model):
         compute='_compute_sale_order_count',
         groups='sales_team.group_sale_salesman')
 
-    @api.depends('procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_ids')
+    @api.depends('procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id', 'procurement_group_id.group_dest_ids.sale_id')
     def _compute_sale_order_count(self):
         for production in self:
             production.sale_order_count = len(production.get_sale_orders())
@@ -38,5 +38,4 @@ class MrpProduction(models.Model):
         return action
 
     def get_sale_orders(self):
-        return self.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_ids | self.procurement_group_id.sale_ids
-    # TODO : on done, if proc->sale_ids->picking->move is mtso, assign (missing <= produced) qty
+        return self.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id | self.procurement_group_id.group_dest_ids.sale_id
