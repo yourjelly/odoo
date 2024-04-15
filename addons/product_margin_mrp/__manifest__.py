081d2bddf221f65373ca6ce1f94b75e6731cc9ff
dhad-odoo <dhad@odoo.com>
2024-04-15 15:08:28 +0530

    [FIX] product_margin_mrp: remove unnecessary whitespaces
    Remove unnecessary whitespaces and add spacing around operators for improved
    code readability and compatibility with runbot testing
    
    Task ID - 3774631

--- addons/product_margin_mrp/__manifest__.py
+++ addons/product_margin_mrp/__manifest__.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-
 {
     'name': 'Adds MRP cost to Product Margin Report ',
     'description': """
     Adds MRP cost to Product's Margin Report
 """,
-    'depends': ['account','mrp','product_margin'],
+    'depends': ['account', 'mrp', 'product_margin'],
     'data': [
         'views/product_product_views.xml'
     ],
--- addons/product_margin_mrp/models/product_product.py
+++ addons/product_margin_mrp/models/product_product.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-import itertools
 import time
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductProduct(models.Model):
@@ -19,7 +15,7 @@ class ProductProduct(models.Model):
         date_to = self.env.context.get('date_to', time.strftime('%Y-12-31'))
         for product in self:
             product_id = product.id
-            res[product_id]['manufactured_num_products'] =0
+            res[product_id]['manufactured_num_products'] = 0
             manufacture_orders = self.env['mrp.production'].search([
                 ('product_id', '=', product_id),
                 ('state', '=', 'done'),
@@ -27,21 +23,21 @@ class ProductProduct(models.Model):
                 ('date_finished', '<=', date_to)
             ])
             total_manufacture_cost = 0
-            nos_of_products_manufactured=0
+            nos_of_products_manufactured = 0
             if manufacture_orders:
                 for order in manufacture_orders:
                     res1 = self.env['report.mrp.report_mo_overview'].get_report_values(production_id=order.id)
-                    total_manufacture_cost += res1['data']['summary']['mo_cost']
-                    nos_of_products_manufactured+= res1['data']['summary']['quantity']
-                    if order.scrap_count >0:
-                        scrap_orders=self.env['stock.scrap'].search([('production_id','=',order.id),('product_id','=',product_id)])
-                        scrap_quantities=0
+                    total_manufacture_cost = total_manufacture_cost + res1['data']['summary']['mo_cost']
+                    nos_of_products_manufactured = nos_of_products_manufactured + res1['data']['summary']['quantity']
+                    if order.scrap_count > 0:
+                        scrap_orders = self.env['stock.scrap'].search([('production_id', '=', order.id), ('product_id', '=', product_id)])
+                        scrap_quantities = 0
                         for scrap_order in scrap_orders:
-                            scrap_quantities+=scrap_order.scrap_qty
-                        nos_of_products_manufactured-=scrap_quantities
+                            scrap_quantities = scrap_quantities + scrap_order.scrap_qty
+                        nos_of_products_manufactured = nos_of_products_manufactured - scrap_quantities
             res[product_id]['total_cost'] += total_manufacture_cost
-            res[product_id]['manufactured_num_products']= nos_of_products_manufactured
-            res[product_id]['total_margin']-=total_manufacture_cost
+            res[product_id]['manufactured_num_products'] = nos_of_products_manufactured
+            res[product_id]['total_margin'] -= total_manufacture_cost
             res[product_id]['total_margin_rate'] -= res[product_id].get('turnover', 0.0) and total_manufacture_cost * 100 / res[product_id].get('turnover', 0.0) or 0.0
-            product.update(res[product.id]) 
+            product.update(res[product.id])
         return res
