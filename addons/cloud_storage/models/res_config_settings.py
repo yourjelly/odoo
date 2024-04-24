d22cc9af664472480c9c22dc4eba8e6d329b93af
Chong Wang (cwg) <cwg@odoo.com>
2024-04-24 14:37:15 +0200

    fix

--- addons/cloud_storage/models/res_config_settings.py
+++ addons/cloud_storage/models/res_config_settings.py
@@ -15,9 +15,9 @@ class CloudStorageSettings(models.TransientModel):
         but existing cloud storage attachments can still be downloaded/deleted.
     cloud_storage_mim_file_size: a soft limit for the file size that can be
         uploaded as the cloud storage attachments for web client.
-    cloud_storage_auto_delete: if enabled, the cloud storage blob will be
+    cloud_storage_manual_delete: if disabled, the cloud storage blob will be
         deleted when the attachment is deleted from the server. And the account
-        provided should have the blob deletion permission. If disabled, the
+        provided should have the blob deletion permission. If enabled, the
         administrator needs to manually delete them with scripts.
     """
     _inherit = 'res.config.settings'
@@ -37,22 +37,21 @@ class CloudStorageSettings(models.TransientModel):
         default=DEFAULT_CLOUD_STORAGE_MIN_FILE_SIZE,
     )
 
-    cloud_storage_auto_delete = fields.Boolean(
-        string="Automatically Delete Cloud Storage Blobs",
-        default=True,
-        help='''Automatically delete the blob from the cloud storage when the
+    cloud_storage_manual_delete = fields.Boolean(
+        string="Manually Delete Cloud Storage Blobs",
+        help='''Manually delete the blob from the cloud storage when the
         attachment is deleted from the server.''',
-        config_parameter='cloud_storage_auto_delete',
+        config_parameter='cloud_storage_manual_delete',
     )
 
     def set_values(self):
         cloud_storage_configuration_before = self.env['cloud.storage.provider']._get_configuration()
-        cloud_storage_auto_delete_before = self.env['ir.config_parameter'].get_param('cloud_storage_auto_delete', default=True)
+        cloud_storage_manual_delete_before = self.env['ir.config_parameter'].get_param('cloud_storage_manual_delete')
         super().set_values()
         cloud_storage_configuration = self.env['cloud.storage.provider']._get_configuration()
         if not cloud_storage_configuration and self.cloud_storage_provider:
             raise UserError(_('Please configure the Cloud Storage before enabling it'))
         if cloud_storage_configuration and cloud_storage_configuration != cloud_storage_configuration_before:
             self.env['cloud.storage.provider']._setup()
-        if cloud_storage_auto_delete_before != self.cloud_storage_auto_delete:
-            self.env.ref('cloud_storage.ir_cron_cloud_storage_blobs_delete_action').active = self.cloud_storage_auto_delete
+        if cloud_storage_manual_delete_before != self.cloud_storage_manual_delete:
+            self.env.ref('cloud_storage.ir_cron_cloud_storage_blobs_delete_action').active = self.cloud_storage_manual_delete
