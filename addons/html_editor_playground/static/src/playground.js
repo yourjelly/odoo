aeb0144cf5be41e3ead0e58ec0ebe1e456f63256
Géry Debongnie <ged@odoo.com>
2024-04-24 10:42:20 +0200

    minor fix in playground

--- addons/html_editor_playground/static/src/playground.js
+++ addons/html_editor_playground/static/src/playground.js
@@ -39,7 +39,7 @@ const testHtml = `Hello Phoenix editor!
 <p>this is a link: <a href="http://test.com">link</a></p>
 <p>this is another link: <a>link2</a></p>
 <div data-embedded="card" data-title="Some Title">
-    <p >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 </div>
 <p>
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
