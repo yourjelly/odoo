e4e2f644e5c0d0dcc316db5c290dc53184a840fa
Nicolas Bayet <nby@odoo.com>
2024-05-07 14:23:21 +0200

    add checklist ids plugin

--- addons/html_editor/static/tests/list/checklist_ids.test.js
+++ addons/html_editor/static/tests/list/checklist_ids.test.js
@@ -1,22 +1,18 @@
 import { test, expect } from "@odoo/hoot";
-import { testEditor } from "../_helpers/editor";
+import { setupEditor } from "../_helpers/editor";
 import { toggleCheckList } from "../_helpers/user_actions";
-import { MAIN_PLUGINS } from "../../src/plugin_sets";
+import { getContent } from "../_helpers/selection";
+import { MAIN_PLUGINS } from "@html_editor/plugin_sets";
+import { ChecklistIdsPlugin } from "@html_editor/main/list/checklist_ids_plugin";
 
-// @phoenix @todo: ChecklistIdsPlugin
-import { Plugin } from "@html_editor/plugin";
-class ChecklistIdsPlugin extends Plugin {}
-
-test.todo("should add a unique id on a new checklist", async () => {
-    await testEditor({
-        contentBefore: "<p>ab[]cd</p>",
-        stepFunction: (editor) => {
-            toggleCheckList(editor);
-            const id = editor.editable.querySelector("li[id^=checkId-]").getAttribute("id");
-            expect(editor.editable.innerHTML).toBe(
-                `<ul class="o_checklist"><li id="${id}">abcd</li></ul>`
-            );
-        },
+test("should add a unique id on a new checklist", async () => {
+    const { el, editor } = await setupEditor("<p>ab[]cd</p>", {
         config: { Plugins: [...MAIN_PLUGINS, ChecklistIdsPlugin] },
     });
+    toggleCheckList(editor);
+    editor.dispatch("ADD_STEP");
+    const id = el.querySelector("li[data-check-id]").getAttribute("data-check-id");
+    expect(getContent(el)).toBe(
+        `<ul class="o_checklist"><li data-check-id="${id}">ab[]cd</li></ul>`
+    );
 });
