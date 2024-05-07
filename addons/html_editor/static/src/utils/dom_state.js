dd054f8f0df876675ecd4e8ee44ae7afc80abdce
Nicolas Bayet <nby@odoo.com>
2024-05-07 11:22:18 +0200

    fix enforceWhitespace

--- addons/html_editor/static/src/utils/dom_state.js
+++ addons/html_editor/static/src/utils/dom_state.js
@@ -553,7 +553,8 @@ export function enforceWhitespace(el, offset, direction, rule) {
         if (
             spaceVisibility &&
             !foundVisibleSpaceTextNode &&
-            getState(...rightPos(spaceNode), DIRECTIONS.RIGHT).cType & CTGROUPS.BLOCK
+            getState(...rightPos(spaceNode), DIRECTIONS.RIGHT).cType & CTGROUPS.BLOCK &&
+            getState(...leftPos(spaceNode), DIRECTIONS.LEFT).cType !== CTYPES.CONTENT
         ) {
             spaceVisibility = false;
         }
--- addons/html_editor/static/tests/insert/text.test.js
+++ addons/html_editor/static/tests/insert/text.test.js
@@ -63,16 +63,13 @@ describe("not collapsed selection", () => {
         });
     });
 
-    test.todo(
-        "should transform the space node preceded by a styled element to &nbsp;",
-        async () => {
-            await testEditor({
-                contentBefore: `<p><strong>ab</strong> [cd]</p>`,
-                stepFunction: async (editor) => {
-                    insertText(editor, "x");
-                },
-                contentAfter: `<p><strong>ab</strong>&nbsp;x[]</p>`,
-            });
-        }
-    );
+    test("should transform the space node preceded by a styled element to &nbsp;", async () => {
+        await testEditor({
+            contentBefore: `<p><strong>ab</strong> [cd]</p>`,
+            stepFunction: async (editor) => {
+                insertText(editor, "x");
+            },
+            contentAfter: `<p><strong>ab</strong>&nbsp;x[]</p>`,
+        });
+    });
 });
