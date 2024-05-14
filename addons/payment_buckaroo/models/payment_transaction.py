christian-rb/odoo-dev

[IMP] payment_*: compare notification data with transaction values
TODO

addons/payment_buckaroo/models/payment_transaction.py
@@ -71,6 +71,11 @@ class PaymentTransaction(models.Model):
 
         return tx
 
+    def _compare_notification_data(self, notification_data):
+        amount = notification_data.get('Brq_amount')
+        currency_code = notification_data.get('Brq_currency')
+        self._validate_amount_and_currency_code(amount, currency_code)
+
     def _process_notification_data(self, notification_data):
         """ Override of payment to process the transaction based on Buckaroo data.
 

yourjelly/odoo

addons/payment_buckaroo/models/payment_transaction.py
@@ -71,6 +71,11 @@ class PaymentTransaction(models.Model):
 
         return tx
 
+    def _compare_notification_data(self, notification_data):
+        amount = notification_data.get('brq_amount')
+        currency_code = notification_data.get('brq_currency')
+        self._validate_amount_and_currency_code(amount, currency_code)
+
     def _process_notification_data(self, notification_data):
         """ Override of payment to process the transaction based on Buckaroo data.
 