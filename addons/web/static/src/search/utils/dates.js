christian-rb/odoo-dev

[IMP] web: add support for domains on date filters
This commit adds the possibility to define custom domains on date filters,
which was previously only possible on non-date filters.

Whenever a date filter is selected, the domain will be used to further
restrict the displayed record on top of the date domain.

Task-3692123

addons/web/static/src/search/utils/dates.js
@@ -156,7 +156,10 @@ export function constructDateDomain(
         selectedOptions = getSelectedOptions(referenceMoment, searchItem, selectedOptionIds);
     }
     if ("domain" in selectedOptions) {
-        return selectedOptions.domain[0];
+        return {
+            description: selectedOptions.domain[0].description,
+            domain: Domain.and([selectedOptions.domain[0].domain, searchItem.domain]),
+        };
     }
     const yearOptions = selectedOptions.year;
     const otherOptions = [...(selectedOptions.quarter || []), ...(selectedOptions.month || [])];
@@ -192,10 +195,11 @@ export function constructDateDomain(
             ranges.push(range);
         }
     }
-    const domain = Domain.combine(
+    let domain = Domain.combine(
         ranges.map((range) => range.domain),
         "OR"
     );
+    domain = Domain.and([domain, searchItem.domain]);
     const description = ranges.map((range) => range.description).join("/");
     return { domain, description };
 }

yourjelly/odoo

addons/web/static/src/search/utils/dates.js
@@ -156,7 +156,10 @@ export function constructDateDomain(
         selectedOptions = getSelectedOptions(referenceMoment, searchItem, selectedOptionIds);
     }
     if ("withDomain" in selectedOptions) {
-        return selectedOptions.withDomain[0];
+        return {
+            description: selectedOptions.withDomain[0].description,
+            domain: Domain.and([selectedOptions.withDomain[0].domain, searchItem.domain]),
+        };
     }
     const yearOptions = selectedOptions.year;
     const otherOptions = [...(selectedOptions.quarter || []), ...(selectedOptions.month || [])];
@@ -192,10 +195,11 @@ export function constructDateDomain(
             ranges.push(range);
         }
     }
-    const domain = Domain.combine(
+    let domain = Domain.combine(
         ranges.map((range) => range.domain),
         "OR"
     );
+    domain = Domain.and([domain, searchItem.domain]);
     const description = ranges.map((range) => range.description).join("/");
     return { domain, description };
 }