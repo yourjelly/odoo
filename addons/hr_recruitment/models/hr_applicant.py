4d85ede2ebf197290f01b3823a3c33f29f43e510
arpr-odoo <arpr@odoo.com>
2024-05-20 17:02:01 +0530

    [IMP] hr_recruitment_(reports): stop stage timer for refused applicant
    This commit introduces functionality to stop the timer on stage when an
    applicant is rejected. This feature is useful for tracking the duration an
    applicant spent in that stage before being rejected.
    
    task-3810883

--- addons/hr_recruitment/models/hr_applicant.py
+++ addons/hr_recruitment/models/hr_applicant.py
@@ -11,6 +11,9 @@ from odoo.exceptions import AccessError, UserError
 from odoo.osv import expression
 from odoo.tools.translate import _
 
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 AVAILABLE_PRIORITIES = [
     ('0', 'Normal'),
     ('1', 'Good'),
@@ -115,6 +118,7 @@ class Applicant(models.Model):
         ('archived', 'Archived'),
     ], compute="_compute_application_status", search="_search_application_status")
     applicant_properties = fields.Properties('Properties', definition='job_id.applicant_properties_definition', copy=True)
+    refuse_date = fields.Datetime('Refuse Date')
 
     def init(self):
         self.env.cr.execute("""
@@ -778,3 +782,10 @@ class Applicant(models.Model):
                 'default_applicant_ids': self.ids,
             }
         }
+
+    def _get_duration_from_tracking(self, trackings):
+        json = super()._get_duration_from_tracking(trackings)
+        for record in self:
+            if record.refuse_reason_id and record.refuse_date:
+                json[record.stage_id.id] -= (datetime.now() - record.refuse_date).total_seconds()
+        return json
