0358ca4897539a42a0bc21a36fe6f98339008b18
temo-odoo <temo@odoo.com>
2024-05-23 12:49:10 +0530

    [ADD] stock_transport: Replace Dock to Location

--- addons/stock_transport/models/stock_picking_batch.py
+++ addons/stock_transport/models/stock_picking_batch.py
@@ -12,8 +12,8 @@ class StockPickingBatch(models.Model):
 
     vehicle_id = fields.Many2one('fleet.vehicle', string="Vehicle")
     vehicle_category_id = fields.Many2one(
-        'fleet.vehicle.model.category', string="Vehicle Category", compute='_compute_vehicle_category_id', inverse='_inverse_vehicle_category_id', store=True)
-    dock_id = fields.Many2one('stock.batch.dock', string="Dock", domain="[('operation_type_ids', 'in', picking_type_id)]")
+        'fleet.vehicle.model.category', string="Vehicle Category", compute='_compute_vehicle_category_id', store=True, readonly=False)
+    dock_id = fields.Many2one('stock.location', string="Dock Location", domain="[('is_a_dock', '=', True)]")
     vehicle_details = fields.Char(
         "Vehicle Details", compute='_compute_vehicle_details')
     max_weight = fields.Float(string="Max Weight (Kg)",
@@ -23,11 +23,11 @@ class StockPickingBatch(models.Model):
     driver_id = fields.Many2one(
         related="vehicle_id.driver_id", string="Driver")
     used_weight_percentage = fields.Float(
-        string="Weight %", compute='_compute_used_weight_percentage', store=True)
+        string="Weight %", compute='_compute_used_weight_percentage')
     used_volume_percentage = fields.Float(
-        string="Volume %", compute='_compute_used_volume_percentage', store=True)
-    max_picking_weight = fields.Float(string="Weight", compute='_compute_used_weight_percentage', store=True)
-    max_picking_volume = fields.Float(string="Volume", compute='_compute_used_volume_percentage', store=True)
+        string="Volume %", compute='_compute_used_volume_percentage')
+    max_picking_weight = fields.Float(string="Weight", compute='_compute_used_weight_percentage')
+    max_picking_volume = fields.Float(string="Volume", compute='_compute_used_volume_percentage')
     end_date = fields.Datetime('End Date', default=_default_end_date)
     picking_info = fields.Char(string="Picking Type Information",
                             compute='_compute_picking_info', store=True)
@@ -37,13 +37,20 @@ class StockPickingBatch(models.Model):
         'Properties',
         definition='picking_type_id.picking_properties_definition')
 
+    def write(self, vals):
+        res = super().write(vals)
+        self._update_sequences()
+        return res
+
+    def _update_sequences(self):
+        sorted_records = self.picking_ids.sorted(key=lambda r: r.zip_code or '0')
+        for idx, record in enumerate(sorted_records):
+            record.sequence = idx
+
     @api.depends('vehicle_id')
     def _compute_vehicle_category_id(self):
         for rec in self:
             rec.vehicle_category_id = rec.vehicle_id.category_id if rec.vehicle_id else False
-
-    def _inverse_vehicle_category_id(self):
-        pass
 
     @api.depends('vehicle_id', 'max_picking_weight', 'max_picking_volume')
     def _compute_vehicle_details(self):
@@ -55,18 +62,18 @@ class StockPickingBatch(models.Model):
             vehicle_details.append(f"{round(record.max_picking_volume, 2)} m³") if record.max_picking_volume else vehicle_details.append("0m³")
             record.vehicle_details = ', '.join(vehicle_details)
 
-    @api.depends('picking_ids.max_weight', 'vehicle_category_id.max_weight')
+    @api.depends('picking_ids.weight', 'vehicle_category_id.max_weight')
     def _compute_used_weight_percentage(self):
         for batch in self:
-            batch.max_picking_weight = sum(picking.max_weight for picking in batch.picking_ids)
+            batch.max_picking_weight = sum(picking.weight for picking in batch.picking_ids)
             batch_vehicle_max_weight = batch.vehicle_category_id.max_weight
             used_weight_percentage = 100 * (batch.max_picking_weight / batch_vehicle_max_weight) if batch_vehicle_max_weight else 0.0
             batch.used_weight_percentage = used_weight_percentage
 
-    @api.depends('picking_ids.max_volume', 'vehicle_category_id.max_volume')
+    @api.depends('picking_ids.volume', 'vehicle_category_id.max_volume')
     def _compute_used_volume_percentage(self):
         for batch in self:
-            batch.max_picking_volume = sum(picking.max_volume for picking in batch.picking_ids)
+            batch.max_picking_volume = sum(picking.volume for picking in batch.picking_ids)
             batch_vehicle_max_volume = batch.vehicle_category_id.max_volume
             used_volume_percentage = 100 * (batch.max_picking_volume / batch_vehicle_max_volume) if batch_vehicle_max_volume else 0.0
             batch.used_volume_percentage = used_volume_percentage
@@ -80,12 +87,11 @@ class StockPickingBatch(models.Model):
             picking_info_str = f"{picking_type}{max_picking_weight}, {max_picking_volume}".rstrip(', ')
             record.picking_info = picking_info_str
 
-    def action_map_delivery_batch(self):
-        action = {
-            'type': 'ir.actions.act_window',
-            'name': 'All Deliveries',
-            'res_model': 'stock.picking',
-            'view_mode': 'map',
-            'domain': [('id', 'in', self.picking_ids.ids)],
-        }
-        return action
+    @api.depends('dock_id')
+    def _compute_move_ids(self):
+        super()._compute_move_ids()
+        if self.dock_id:
+            parent_path_id = [int(parent_id) for parent_id in self.dock_id.parent_path.split('/')[:-1]]
+            for line in self.move_line_ids:
+                if line.location_dest_id.id in parent_path_id:
+                    line.location_dest_id = self.dock_id
