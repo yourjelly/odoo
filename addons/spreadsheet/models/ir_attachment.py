e0611e5511131434fa2ac6b3a8c67b606ec2726e
Lucas Lefèvre (lul) <lul@odoo.com>
2024-05-28 11:27:44 +0200

    [FIX] spreadsheet_* TODO

---
+++ addons/spreadsheet/models/ir_attachment.py
@@ -0,0 +1,52 @@
+import logging
+
+
+from odoo import models, _
+from odoo.exceptions import AccessError
+from odoo.osv import expression
+
+_logger = logging.getLogger(__name__)
+
+
+class IrAttachment(models.Model):
+    _inherit = "ir.attachment"
+
+    def _move_spreadsheet_files_to_db(self):
+        """
+        Moves all spreadsheet files from the filestore to the database.
+
+        This method should be executed before sending the database to the upgrade platform.
+        """
+        if not self.env.is_admin():
+            raise AccessError(_('Only administrators can execute this action.'))
+        domain = self._get_spreadsheet_attachment_domain()
+        attachments = self.search(domain)
+        for attachment in attachments:
+            attachment.db_datas = attachment.raw
+        # attachments.raw = False
+        _logger.info("%s spreadsheet attachments moved to the database", len(attachments))
+
+    def _move_spreadsheet_files_to_current_storage(self):
+        """
+        Moves all spreadsheet data from the database to the filestore.
+
+        This method should be executed after restoring the database upgraded by the upgrade platform.
+        """
+        if not self.env.is_admin():
+            raise AccessError(_('Only administrators can execute this action.'))
+        if self._storage() == "db":
+            return
+        domain = expression.AND([self._get_spreadsheet_attachment_domain(), [('db_datas', '!=', False)]])
+        attachments = self.search(domain)
+        for attachment in attachments:
+            if (attachment.db_datas):
+                attachment.raw = attachment.db_datas
+        attachments.db_datas = False
+        _logger.info("%s spreadsheet attachments moved from the database to the filestore", len(attachments))
+
+    def _get_spreadsheet_attachment_domain(self):
+        """
+        Override this method to include attachments containing spreadsheet data
+        to be be sent to the upgrade platform.
+        """
+        return expression.FALSE_DOMAIN
