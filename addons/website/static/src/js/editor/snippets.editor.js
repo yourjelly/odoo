4ee40961423249f76f84fd264b3a87dcd90ed942
Benoit Socias <bso@odoo.com>
2024-05-29 15:42:07 +0200

    [REF] website: convert GPSPicker to Owl
    task-3850413

--- addons/website/static/src/js/editor/snippets.editor.js
+++ addons/website/static/src/js/editor/snippets.editor.js
@@ -7,7 +7,7 @@ import { user } from "@web/core/user";
 import weSnippetEditor from "@web_editor/js/editor/snippets.editor";
 import wLegacySnippetOptions from "@website/js/editor/snippets.options.legacy";
 import * as OdooEditorLib from "@web_editor/js/editor/odoo-editor/src/utils/utils";
-import { Component, onMounted, onWillStart, useEffect, useRef, useState } from "@odoo/owl";
+import { Component, onMounted, onWillStart, useEffect, useRef, useState, useSubEnv } from "@odoo/owl";
 import { throttleForAnimation } from "@web/core/utils/timing";
 import { switchTextHighlight } from "@website/js/text_processing";
 import { registry } from "@web/core/registry";
@@ -58,6 +58,10 @@ export class WebsiteSnippetsMenu extends weSnippetEditor.SnippetsMenu {
      * @override
      */
     setup() {
+        useSubEnv({
+            gmapApiRequest: this._onGMapAPIRequest.bind(this),
+            gmapApiKeyRequest: this._onGMapAPIKeyRequest.bind(this),
+        });
         super.setup();
         this.notification = useService("notification");
         this.dialog = useService("dialog");
