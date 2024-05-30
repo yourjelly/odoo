1b8136f32873069b95ba020fbef90d426232ccc6
Aurélien Warnon <awa@odoo.com>
2024-05-30 16:56:32 +0200

    [IMP] utm, various*: introduce utm_reference as a 4th UTM dimension various: account, crm, hr_applicant, link_tracker, sale
    RATIONALE
    
    The way we currently use the UTM trackers (aka source/medium/campaign) does not
    comply with the industry standards, leading to confusion and globally unclear
    or not usable reporting on UTM data.
    
    PURPOSE
    
    In addition to the existing source, medium and data UTM trackers, we would also
    like to know the specific reference record from which we come from.
    
    This role was previously assumed by the UTM source, but we aim to change this
    to make the source more "generic", allowing better overall reporting.
    (Source will become "mass_mailing", "facebook", ...).
    
    Which leads to the new "utm_reference" field on the UTM Mixin., mostly
    replacing the role of the UTM source.
    It references for example a mass mailing record or specific social post record.
    
    The reason we introduce this reference logic is to be able to keep stat-buttons
    on those records, to see the amount of generated revenue or leads, which is
    part of the power of the Odoo integrated eco-system.
    It can also be used to compare social.posts together and see which one generate
    the most revenue to deduce what content performs the best.
    
    This commit only introduces the new concept, and following commits will apply
    it to our various functional apps and use cases.
    
    SPECS
    
    Technically, this is implemented by adding a utm_reference field on the
    utm.mixin, which is a fields.Reference.
    
    We choose fields.Reference as it allows referencing a record at a lower
    database cost (1 field vs 2 fields for a Many2oneReference).
    
    Similarly to the existing UTM fields, this reference is added to the computed
    URLs (mostly through link.trackers) and to the cookies of the website visitors.
    
    They are then copied onto the records implementing the utm.mixin
    (crm.lead, sale.order, ...).
    
    IMPACTED MODELS
    
    The following models are globally impacted by this change as they receive an
    extra UTM Reference fields, that is added to their form views.
    
    - account.move
    - crm.lead
    - hr.applicant
    - link.tracker
    - sale.order
    
    The field is invisible when not set to avoid polluting the view and is readonly
    as it makes little sense to allow modifying it (it's also not super user
    friendly as it involves a reference field so choosing model + record).
    
    EXAMPLE
    
    Let's take a real example: a mass.mailing with a button to purchase something.
    The button is clicked and a client buys a product from the store with the UTM
    values of that mass.mailing's link trackers.
    
    The resulting sale order's UTM trackers will look like the following:
    
    Before:
    - UTM Campaign: Black Friday Sales
    - UTM Medium: utm.utm_medium_email
    - UTM Source: "Black Frid... (Mailing Created on 2024-04-14)"
    
    After:
    - UTM Campaign: Black Friday Sales
    - UTM Medium: utm.utm_medium_email
    - UTM Source: utm.utm_source_mailing
    - UTM Reference: mailing.mailing,42
    
    Task-3330036

--- addons/utm/models/utm_mixin.py
+++ addons/utm/models/utm_mixin.py
@@ -14,12 +14,20 @@ class UtmMixin(models.AbstractModel):
     """ Mixin class for objects which can be tracked by marketing. """
     _description = 'UTM Mixin'
 
+    @api.model
+    def _selection_target_model(self):
+        return [(model.model, model.name) for model in self.env['ir.model'].sudo().search([])]
+
     campaign_id = fields.Many2one('utm.campaign', 'Campaign', index='btree_not_null',
                                   help="This is a name that helps you keep track of your different campaign efforts, e.g. Fall_Drive, Christmas_Special")
     source_id = fields.Many2one('utm.source', 'Source', index='btree_not_null',
                                 help="This is the source of the link, e.g. Search Engine, another domain, or name of email list")
     medium_id = fields.Many2one('utm.medium', 'Medium', index='btree_not_null',
                                 help="This is the method of delivery, e.g. Postcard, Email, or Banner Ad")
+    utm_reference = fields.Reference(string='UTM Reference',
+                                     selection='_selection_target_model',
+                                     index=True,  # will be used extensively for statistics (e.g: how many leads did this mailing generate)
+                                     help="This is the (optional) reference to the originating record (e.g: the social post, the mailing)")
 
     @api.model
     def default_get(self, fields):
@@ -36,10 +44,17 @@ class UtmMixin(models.AbstractModel):
                 if request:
                     # ir_http dispatch saves the url params in a cookie
                     value = request.cookies.get(cookie_name)
-                # if we receive a string for a many2one, we search/create the id
                 if field.type == 'many2one' and isinstance(value, str) and value:
+                    # if we receive a string for a many2one, we search/create the id
                     record = self._find_or_create_record(field.comodel_name, value)
                     value = record.id
+                elif field.type == 'reference' and isinstance(value, str) and value:
+                    # sanitize the value first, the ORM will crash if ill-formatted
+                    # we prefer to just void the field if so, as UTMs are optional
+                    # see 'fields_reference#convert_to_cache'
+                    res_model, res_id = value.split(',')
+                    if not res_model in self.env or not self.env[res_model].browse(int(res_id)).exists():
+                        value = False
                 if value:
                     values[field_name] = value
         return values
@@ -56,6 +71,7 @@ class UtmMixin(models.AbstractModel):
             ('utm_campaign', 'campaign_id', 'odoo_utm_campaign'),
             ('utm_source', 'source_id', 'odoo_utm_source'),
             ('utm_medium', 'medium_id', 'odoo_utm_medium'),
+            ('utm_reference', 'utm_reference', 'odoo_utm_reference'),
         ]
 
     def _tracking_models(self):
