cf7ceb43500f8bb32232e75fa705368d82d1dfb0
Rodolpho Lima <rcdl@odoo.com>
2024-05-31 12:00:54 +0200

    [IMP] Pad links with zero-width non-breaking spaces
    Original commit: https://github.com/odoo/odoo/pull/165470/commits/9a95d56db471c7f9b6c279019a275bbc2cc5f138
    
    Missing: changes to insert command (now domInsert shared method).
    
    This implements a new approach to solve selection issues around links
    (allowing the cursor at the inner _and_ outer edges of links).
    
    In the sanitization process, every link now receives 4 zero-width
    non-breaking spaces (unicode FEFF characters, hereafter referred to as
    ZWNBSP):
    - one before the link
    - one as the link's first child
    - one as the link's last child
    - one after the link
    like so: `//ZWNBSP//<a>//ZWNBSP//label//ZWNBSP//</a>//ZWNBSP`.
    
    An advantage of ZWNBSP over regular ZWSP (unicode 200B) is they're less
    likely to be used deliberately by the user, so much so that we can
    safely assume all of them are technical and can be removed
    indiscriminately. ZWSP and ZWNBSP are used to mark a separation between
    words in languages that don't use spaces for that purpose (eg, Lao).
    ZWNBSP are to ZWSP what NBSP (unicode 00A0) are to regular spaces.
    
    Because of that advantage, we don't need to track the ZWNBSP (so there
    is no need to wrap them in `span` elements), simplifying the code
    considerably. We therefore now remove all ZWNBSP when saving.
    
    There is a possibility to introduce "orphaned" ZWNBSP during the editing
    process, if for instance the link has a big enough padding or margin
    that it's possible to click between two of a link's ZWNBSP (one outer,
    the other inner). Inserting a character or a paragraph break in such a
    position will move the ZWNBSP to a place where it's not useful anymore.
    The sanitizer will then reintroduce the useful ZWNBSP in their rightful
    places. We remove the orphaned ZWNBSP from the sanitizer whenever that
    is possible without risking to break the selection.
    
    This is not applied in the following cases:
    
    - in a navbar (since its links are managed via the snippets system, not
      via pure edition) and, similarly, in .nav-link links
    - in links that have content more complex than simple text (as was the
      case with the previous implementation)

--- addons/html_editor/static/src/main/link/link_selection_plugin.js
+++ addons/html_editor/static/src/main/link/link_selection_plugin.js
@@ -1,8 +1,32 @@
 import { Plugin } from "@html_editor/plugin";
-import { closestElement } from "@html_editor/utils/dom_traversal";
+import { closestElement, descendants } from "@html_editor/utils/dom_traversal";
 import { removeClass } from "@html_editor/utils/dom";
 import { isBlock } from "@html_editor/utils/blocks";
+import { prepareUpdate } from "@html_editor/utils/dom_state";
+import { leftPos } from "@html_editor/utils/position";
 
+/*
+    This plugin solves selection issues around links (allowing the cursor at the
+    inner and outer edges of links).
+
+    Every link receives 4 zero-width non-breaking spaces (unicode FEFF
+    characters, hereafter referred to as ZWNBSP):
+    - one before the link
+    - one as the link's first child
+    - one as the link's last child
+    - one after the link
+    like so: `//ZWNBSP//<a>//ZWNBSP//label//ZWNBSP//</a>//ZWNBSP`.
+
+    A visual indication ( `o_link_in_selection` class) is added to a link when
+    the selection is contained within it.
+
+    This is not applied in the following cases:
+
+    - in a navbar (since its links are managed via the snippets system, not
+    via pure edition) and, similarly, in .nav-link links
+    - in links that have content more complex than simple text
+    - on non-editable links or links that are not within the editable area
+ */
 export class LinkSelectionPlugin extends Plugin {
     static name = "link_selection";
     static dependencies = ["selection"];
@@ -15,6 +39,9 @@ export class LinkSelectionPlugin extends Plugin {
         switch (command) {
             case "CLEAN":
                 this.clean(payload.root);
+                break;
+            case "NORMALIZE":
+                this.normalize(payload.root || this.editable);
                 break;
         }
     }
@@ -55,5 +82,115 @@ export class LinkSelectionPlugin extends Plugin {
         for (const link of root.querySelectorAll(".o_link_in_selection")) {
             removeClass(link, "o_link_in_selection");
         }
+
+        // Remove all FEFF within a `prepareUpdate` to make sure to make <br>
+        // nodes visible if needed.
+        for (const node of descendants(root)) {
+            if (node.nodeType === Node.TEXT_NODE && node.textContent.includes("\uFEFF")) {
+                // @todo: isn't rightPos needed as well?
+                const restore = prepareUpdate(...leftPos(node));
+                node.textContent = node.textContent.replaceAll("\uFEFF", "");
+                restore(); // Make sure to make <br>s visible if needed.
+            }
+        }
+    }
+
+    /**
+     * Take a link and pad it with non-break zero-width spaces to ensure that it
+     * is always possible to place the cursor at its inner and outer edges.
+     *
+     * @param {HTMLElement} editable
+     * @param {HTMLAnchorElement} link
+     */
+    padLinkWithZws(link) {
+        if (!this.isLinkEligibleForZwnbsp(link)) {
+            // Only add the ZWNBSP for simple (possibly styled) text links, and
+            // never in a nav.
+            return;
+        }
+        if (!link.textContent.startsWith("\uFEFF")) {
+            link.prepend(this.document.createTextNode("\uFEFF"));
+        }
+        if (!link.textContent.endsWith("\uFEFF")) {
+            link.append(this.document.createTextNode("\uFEFF"));
+        }
+        if (!(link.previousSibling && link.previousSibling.textContent.endsWith("\uFEFF"))) {
+            link.before(this.document.createTextNode("\uFEFF"));
+        }
+        if (!(link.nextSibling && link.nextSibling.textContent.startsWith("\uFEFF"))) {
+            link.after(this.document.createTextNode("\uFEFF"));
+        }
+    }
+
+    normalize(root) {
+        // @todo review the need for "root" parameter
+        depthFirstPreOrderTraversal(root, (node) => this.updateZWNBSPs(node, root));
+    }
+
+    // @todo: ZWNBSPs are not removed in the case it is in the middle of link
+    updateZWNBSPs(node, root) {
+        if (node.nodeName === "A") {
+            // Ensure links have ZWNBSPs so the selection can be set at their edges.
+            this.padLinkWithZws(node);
+        } else if (
+            node.nodeType === Node.TEXT_NODE &&
+            node.textContent.includes("\uFEFF") &&
+            !closestElement(node, "a") &&
+            // @todo review the need for "root" parameter
+            // This apparently depends on the selection state. Should it?
+            // Does it have to do with preserving cursor (avoid messing the selection)?
+            !(
+                closestElement(root, "[contenteditable=true]") &&
+                this.shared.getTraversedNodes().includes(node)
+            )
+        ) {
+            // Remove link ZWNBSP not in selection.
+            const startsWithLegitZws =
+                node.textContent.startsWith("\uFEFF") &&
+                node.previousSibling &&
+                node.previousSibling.nodeName === "A";
+            const endsWithLegitZws =
+                node.textContent.endsWith("\uFEFF") &&
+                node.nextSibling &&
+                node.nextSibling.nodeName === "A";
+            let newText = node.textContent.replace(/\uFEFF/g, "");
+            if (startsWithLegitZws) {
+                newText = "\uFEFF" + newText;
+            }
+            if (endsWithLegitZws) {
+                newText = newText + "\uFEFF";
+            }
+            if (newText !== node.textContent) {
+                // We replace the text node with a new text node with the
+                // update text rather than just changing the text content of
+                // the node because these two methods create different
+                // mutations and at least the tour system breaks if all we
+                // send here is a text content change.
+                const newTextNode = this.document.createTextNode(newText);
+                node.before(newTextNode);
+                node.remove();
+                node = newTextNode;
+            }
+        }
     }
 }
+
+// @todo move this elsewhere, consider unifying with list's applyToTree
+/**
+ * @param {Node} root
+ * @param {Function} callback
+ */
+function depthFirstPreOrderTraversal(root, callback) {
+    callback(root);
+    // Frozen childNodes list
+    const childNodes = [...root.childNodes];
+    for (const child of childNodes) {
+        depthFirstPreOrderTraversal(child, callback);
+    }
+}
+
+/*
+TODO:
+- handle insertion of a link eligible for ZWNBSP (age's commit ajusts selection
+    in the insert command)
+*/
