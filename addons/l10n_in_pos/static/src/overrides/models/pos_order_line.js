9c7f2565a5f6a473c17c8d9b3a53d5179c9aa744
meji-odoo <meji@odoo.com>
2024-06-02 10:38:32 +0530

    [IMP] pos_urban_piper: add urban piper

--- addons/l10n_in_pos/static/src/overrides/models/pos_order_line.js
+++ addons/l10n_in_pos/static/src/overrides/models/pos_order_line.js
@@ -4,13 +4,13 @@ import { patch } from "@web/core/utils/patch";
 
 patch(PosOrderline.prototype, {
     setup(vals) {
-        this.l10n_in_hsn_code = this.product_id.l10n_in_hsn_code;
+        this.l10n_in_hsn_code = this.product_id?.l10n_in_hsn_code;
         return super.setup(...arguments);
     },
     getDisplayData() {
         return {
             ...super.getDisplayData(),
-            l10n_in_hsn_code: this.get_product().l10n_in_hsn_code,
+            l10n_in_hsn_code: this.get_product()?.l10n_in_hsn_code,
         };
     },
 });
