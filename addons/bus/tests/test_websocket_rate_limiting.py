a88fe13b032acdef2c1c11bffc84f0a1128d65a3
tsm-odoo <tsm@odoo.com>
2024-06-03 10:26:02 +0200

    [FIX] bus: correct handling of websocket rate limiting burst
    Websocket rate limiting uses `RL_DELAY` and `RL_BURST` parameters.
    `RL_BURST` specifies how many requests can exceed the rate initially.
    
    This is implemented with a deque having `maxlen` set to `RL_BURST`.
    When the deque is full, we check if the time from the first frame to
    now is within the limit. However the check occurs when there are exactly
    `RL_BURST` frames, not when the number of received frames exceed the
    burst.
    
    This commit fixes this issue.

--- addons/bus/tests/test_websocket_rate_limiting.py
+++ addons/bus/tests/test_websocket_rate_limiting.py
@@ -13,19 +13,23 @@ from odoo.tests import common
 from .common import WebsocketCase
 from ..websocket import CloseCode, Websocket
 
-@common.tagged('post_install', '-at_install')
+
+@common.tagged("post_install", "-at_install")
 class TestWebsocketRateLimiting(WebsocketCase):
     def test_rate_limiting_base_ok(self):
         ws = self.websocket_connect()
 
+        # slepeing after initial ping frame
+        time.sleep(Websocket.RL_DELAY)
+
         for _ in range(Websocket.RL_BURST + 1):
-            ws.send(json.dumps({'event_name': 'test_rate_limiting'}))
+            ws.send(json.dumps({"event_name": "test_rate_limiting"}))
             time.sleep(Websocket.RL_DELAY)
 
     def test_rate_limiting_base_ko(self):
         def check_base_ko():
             for _ in range(Websocket.RL_BURST + 1):
-                ws.send(json.dumps({'event_name': 'test_rate_limiting'}))
+                ws.send(json.dumps({"event_name": "test_rate_limiting"}))
             self.assert_close_with_code(ws, CloseCode.TRY_LATER)
 
         ws = self.websocket_connect()
@@ -37,34 +41,37 @@ class TestWebsocketRateLimiting(WebsocketCase):
             # https://www.iana.org/assignments/websocket/websocket.xhtml
             with self.assertRaises(WebSocketProtocolException) as cm:
                 check_base_ko()
-            self.assertEqual(str(cm.exception), 'Invalid close opcode.')
+            self.assertEqual(str(cm.exception), "Invalid close opcode.")
         else:
             check_base_ko()
 
     def test_rate_limiting_opening_burst(self):
         ws = self.websocket_connect()
 
-        # first RL_BURST requests are accepted.
-        for _ in range(Websocket.RL_BURST):
-            ws.send(json.dumps({'event_name': 'test_rate_limiting'}))
+        # slepeing after initial ping frame
+        time.sleep(Websocket.RL_DELAY)
 
-        # sending at a correct rate after burst should be accepted.
-        for _ in range(2):
-            time.sleep(Websocket.RL_DELAY)
-            ws.send(json.dumps({'event_name': 'test_rate_limiting'}))
+        # burst is allowed
+        for _ in range(Websocket.RL_BURST // 2):
+            ws.send(json.dumps({"event_name": "test_rate_limiting"}))
+
+        # as long as the rate is respected afterwards
+        for _ in range(Websocket.RL_BURST):
+            time.sleep(Websocket.RL_DELAY * 2)
+            ws.send(json.dumps({"event_name": "test_rate_limiting"}))
 
     def test_rate_limiting_start_ok_end_ko(self):
         def check_end_ko():
             # those requests are illicit and should not be accepted.
             for _ in range(Websocket.RL_BURST * 2):
-                ws.send(json.dumps({'event_name': 'test_rate_limiting'}))
+                ws.send(json.dumps({"event_name": "test_rate_limiting"}))
             self.assert_close_with_code(ws, CloseCode.TRY_LATER)
 
         ws = self.websocket_connect()
 
         # first requests are legit and should be accepted
         for _ in range(Websocket.RL_BURST + 1):
-            ws.send(json.dumps({'event_name': 'test_rate_limiting'}))
+            ws.send(json.dumps({"event_name": "test_rate_limiting"}))
             time.sleep(Websocket.RL_DELAY)
 
         if 1013 not in VALID_CLOSE_STATUS:
@@ -74,6 +81,6 @@ class TestWebsocketRateLimiting(WebsocketCase):
             # https://www.iana.org/assignments/websocket/websocket.xhtml
             with self.assertRaises(WebSocketProtocolException) as cm:
                 check_end_ko()
-            self.assertEqual(str(cm.exception), 'Invalid close opcode.')
+            self.assertEqual(str(cm.exception), "Invalid close opcode.")
         else:
             check_end_ko()
