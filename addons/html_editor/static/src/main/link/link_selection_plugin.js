ebdf355baae68791d91c6d3f80cb0c3b23c61db9
Rodolpho Lima <rcdl@odoo.com>
2024-06-11 12:27:32 +0200

    [IMP] apply class to identify if a link is in selection
    Original commit:
    
    https://github.com/odoo/odoo/pull/165470/commits/f5dc6338b0689c1fd55995b11322213c3f5d8311
    
    This adds the `o_link_in_selection` to a link when the selection is
    contained within it. This class existed in the previous implementation
    ("link zws") which was reverted.
    
    This is not applied in the following cases:
    
    - in a navbar (since its links are managed via the snippets system, not
      via pure edition) and, similarly, in .nav-link links
    - in links that have content more complex than simple text (as was the
      case with the previous implementation)
    - on non-editable links or links that are not within the editable area

--- addons/html_editor/static/src/main/link/link_selection_plugin.js
+++ addons/html_editor/static/src/main/link/link_selection_plugin.js
@@ -1,4 +1,59 @@
 import { Plugin } from "@html_editor/plugin";
+import { closestElement } from "@html_editor/utils/dom_traversal";
+import { removeClass } from "@html_editor/utils/dom";
+import { isBlock } from "@html_editor/utils/blocks";
+
 export class LinkSelectionPlugin extends Plugin {
     static name = "link_selection";
+    static dependencies = ["selection"];
+    static resources = (p) => ({
+        history_rendering_classes: ["o_link_in_selection"],
+        onSelectionChange: p.handleSelectionChange.bind(p),
+    });
+
+    handleCommand(command, payload) {
+        switch (command) {
+            case "CLEAN":
+                this.clean(payload.root);
+                break;
+        }
+    }
+
+    // Apply the o_link_in_selection class if the selection is in a single
+    // link, remove it otherwise.
+    handleSelectionChange(selection) {
+        const { anchorNode, focusNode } = selection;
+        const [anchorLink, focusLink] = [anchorNode, focusNode].map((node) =>
+            closestElement(node, "a:not(.btn)")
+        );
+        const singleLinkInSelection = anchorLink === focusLink && anchorLink;
+
+        if (singleLinkInSelection && this.isLinkEligibleForZwnbsp(singleLinkInSelection)) {
+            singleLinkInSelection.classList.add("o_link_in_selection");
+        }
+
+        for (const link of this.editable.querySelectorAll(".o_link_in_selection")) {
+            if (link !== singleLinkInSelection) {
+                removeClass(link, "o_link_in_selection");
+            }
+        }
+    }
+
+    isLinkEligibleForZwnbsp(link) {
+        return (
+            link.isContentEditable &&
+            this.editable.contains(link) &&
+            !(
+                [link, ...link.querySelectorAll("*")].some(
+                    (el) => el.nodeName === "IMG" || isBlock(el)
+                ) || link.matches("nav a, a.nav-link")
+            )
+        );
+    }
+
+    clean(root) {
+        for (const link of root.querySelectorAll(".o_link_in_selection")) {
+            removeClass(link, "o_link_in_selection");
+        }
+    }
 }
