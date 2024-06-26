d51f9c61647cec50dafd04594aea757e2465ce76
Jorge Pinna Puissant <jpp@odoo.com>
2024-06-26 08:22:15 +0200

    [FIX] web: fix scroll to anchor

--- addons/web/static/tests/legacy/core/scroller_tests.js
+++ addons/web/static/tests/legacy/core/scroller_tests.js
@@ -12,6 +12,7 @@ import {
     doAction,
     getActionManagerServerData,
 } from "@web/../tests/legacy/webclient/helpers";
+import { redirect } from "@web/core/utils/urls";
 
 let target;
 
@@ -529,4 +530,63 @@ QUnit.test("Simple scroll to HTML elements", async (assert) => {
     assert.ok(isVisible(div_4));
     assert.ok(border(div_4).top, "the element must be at the top border");
     assert.ok(border(subScrollable).top, "the element must be a the thop border");
+});
+
+QUnit.test("scroll to anchor from load", async (assert) => {
+    const serverData = getActionManagerServerData();
+    // Need to remove the UglyHack menu to load the first menu at webClient mount (or at the ROUTE_CHANGE)
+    serverData.menus = {
+        root: { id: "root", children: [1, 2], name: "root", appID: "root" },
+        1: { id: 1, children: [], name: "App1", appID: 1, actionID: 1001, xmlid: "menu_1" },
+        2: { id: 2, children: [], name: "App2", appID: 2, actionID: 1002, xmlid: "menu_2" },
+    };
+
+    class MyComponent extends Component {
+        static template = xml/* xml */ `
+            <div class="o_content" style="overflow:scroll;height:150px;width:400px">
+                <a href="#scrollToHere"  class="alert-link" role="button">sroll to ...</a>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+                    Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed,
+                    dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper
+                    congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est
+                    eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu
+                    massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut
+                    in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent
+                    egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue
+                    blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                    posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque
+                    fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+                </p>
+                <p>
+                    Ut velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa
+                    suscipit pulvinar. Nulla sollicitudin. Fusce varius, ligula non tempus aliquam, nunc
+                    turpis ullamcorper nibh, in tempus sapien eros vitae ligula. Pellentesque rhoncus
+                    nunc et augue. Integer id felis. Curabitur aliquet pellentesque diam. Integer quis
+                    metus vitae elit lobortis egestas. Lorem ipsum dolor sit amet, consectetuer adipiscing
+                    elit. Morbi vel erat non mauris convallis vehicula. Nulla et sapien. Integer tortor
+                    tellus, aliquam faucibus, convallis id, congue eu, quam. Mauris ullamcorper felis
+                    vitae erat. Proin feugiat, augue non elementum posuere, metus purus iaculis lectus,
+                    et tristique ligula justo vitae magna.
+                </p>
+                <p>
+                    Aliquam convallis sollicitudin purus. Praesent aliquam, enim at fermentum mollis,
+                    ligula massa adipiscing nisl, ac euismod nibh nisl eu lectus. Fusce vulputate sem
+                    at sapien. Vivamus leo. Aliquam euismod libero eu enim. Nulla nec felis sed leo
+                    placerat imperdiet. Aenean suscipit nulla in justo. Suspendisse cursus rutrum
+                    augue. Nulla tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus,
+                    felis magna fermentum augue, et ultricies lacus lorem varius purus. Curabitur eu amet.
+                </p>
+                <div id="scrollToHere">sroll here!</div>
+            </div>
+        `;
+        static props = ["*"];
+        static path = "my_component";
+    }
+    registry.category("actions").add("my_component", MyComponent);
+    redirect("/odoo/my_component#scrollToHere");
+    await createWebClient({ serverData });
+
+    const scrollableParent = document.querySelector(".o_content");
+    assert.ok(scrollableParent.scrollTop !== 0);
 });
