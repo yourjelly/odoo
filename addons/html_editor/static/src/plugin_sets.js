8f36d4a6a29dba7197de7fba5fabca68babf476d
FrancoisGe <fge@odoo.com>
2024-06-28 11:26:19 +0200

    [REF] html_editor: powerbox orders categories and adds commands
    The purpose of this commit is to:
    - restore the order of caterories in the powerbox as in the old editor
    - add the commands missing from the main set of plugin (chatgpt and rating)

--- addons/html_editor/static/src/plugin_sets.js
+++ addons/html_editor/static/src/plugin_sets.js
@@ -13,42 +13,42 @@ import { ShortCutPlugin } from "./core/shortcut_plugin";
 import { SplitPlugin } from "./core/split_plugin";
 import { TransientNodePlugin } from "./core/transient_node_plugin";
 import { ZwsPlugin } from "./core/zws_plugin";
+import { BannerPlugin } from "./main/banner_plugin";
 import { ColumnPlugin } from "./main/column_plugin";
+import { EmojiPlugin } from "./main/emoji_plugin";
 import { ColorPlugin } from "./main/font/color_plugin";
 import { FontPlugin } from "./main/font/font_plugin";
 import { HintPlugin } from "./main/hint_plugin";
-import { ImagePlugin } from "./main/media/image_plugin";
 import { InlineCodePlugin } from "./main/inline_code";
 import { JustifyPlugin } from "./main/justify_plugin";
 import { LinkPastePlugin } from "./main/link/link_paste_plugin";
 import { LinkPlugin } from "./main/link/link_plugin";
+import { OdooLinkSelectionPlugin } from "./main/link/link_selection_odoo_plugin";
+import { LinkSelectionPlugin } from "./main/link/link_selection_plugin";
 import { ListPlugin } from "./main/list/list_plugin";
 import { LocalOverlayPlugin } from "./main/local_overlay_plugin";
+import { ImagePlugin } from "./main/media/image_plugin";
 import { MediaPlugin } from "./main/media/media_plugin";
 import { MoveNodePlugin } from "./main/movenode_plugin";
+import { PositionPlugin } from "./main/position_plugin";
 import { PowerboxPlugin } from "./main/powerbox/powerbox_plugin";
 import { SearchPowerboxPlugin } from "./main/powerbox/search_powerbox_plugin";
-import { StarPlugin } from "./others/star_plugin";
+import { SignaturePlugin } from "./main/signature_plugin";
+import { StarPlugin } from "./main/star_plugin";
 import { TablePlugin } from "./main/table/table_plugin";
+import { TableResizePlugin } from "./main/table/table_resize_plugin";
 import { TableUIPlugin } from "./main/table/table_ui_plugin";
 import { TabulationPlugin } from "./main/tabulation_plugin";
 import { TextDirectionPlugin } from "./main/text_direction_plugin";
 import { ToolbarPlugin } from "./main/toolbar/toolbar_plugin";
 import { YoutubePlugin } from "./main/youtube_plugin";
+import { ChatGPTPlugin } from "./main/chatgpt/chatgpt_plugin";
+import { CollaborationOdooPlugin } from "./others/collaboration/collaboration_odoo_plugin";
+import { CollaborationPlugin } from "./others/collaboration/collaboration_plugin";
+import { CollaborationSelectionAvatarPlugin } from "./others/collaboration/collaboration_selection_avatar_plugin";
+import { CollaborationSelectionPlugin } from "./others/collaboration/collaboration_selection_plugin";
 import { InlineComponentPlugin } from "./others/inline_component_plugin";
 import { QWebPlugin } from "./others/qweb_plugin";
-import { ChatGPTPlugin } from "./others/chatgpt/chatgpt_plugin";
-import { TableResizePlugin } from "./main/table/table_resize_plugin";
-import { CollaborationPlugin } from "./others/collaboration/collaboration_plugin";
-import { CollaborationOdooPlugin } from "./others/collaboration/collaboration_odoo_plugin";
-import { CollaborationSelectionPlugin } from "./others/collaboration/collaboration_selection_plugin";
-import { CollaborationSelectionAvatarPlugin } from "./others/collaboration/collaboration_selection_avatar_plugin";
-import { PositionPlugin } from "./main/position_plugin";
-import { LinkSelectionPlugin } from "./main/link/link_selection_plugin";
-import { OdooLinkSelectionPlugin } from "./main/link/link_selection_odoo_plugin";
-import { EmojiPlugin } from "./main/emoji_plugin";
-import { SignaturePlugin } from "./main/signature_plugin";
-import { BannerPlugin } from "./main/banner_plugin";
 
 export const CORE_PLUGINS = [
     ClipboardPlugin,
@@ -70,6 +70,7 @@ export const CORE_PLUGINS = [
 export const MAIN_PLUGINS = [
     ...CORE_PLUGINS,
     BannerPlugin,
+    ChatGPTPlugin,
     ColorPlugin,
     ColumnPlugin,
     EmojiPlugin,
@@ -81,6 +82,7 @@ export const MAIN_PLUGINS = [
     PowerboxPlugin,
     SearchPowerboxPlugin,
     SignaturePlugin,
+    StarPlugin,
     TablePlugin,
     TableUIPlugin,
     TabulationPlugin,
@@ -112,6 +114,4 @@ export const EXTRA_PLUGINS = [
     ...MAIN_PLUGINS,
     QWebPlugin,
     InlineComponentPlugin,
-    StarPlugin,
-    ChatGPTPlugin,
 ];
--- addons/html_editor/static/tests/powerbox.test.js
+++ addons/html_editor/static/tests/powerbox.test.js
@@ -68,7 +68,7 @@ describe("search", () => {
         const { el, editor } = await setupEditor("<p>ab[]</p>");
         insertText(editor, "/");
         await animationFrame();
-        expect(commandNames(el).length).toBe(26);
+        expect(commandNames(el).length).toBe(29);
         insertText(editor, "head");
         await animationFrame();
         expect(commandNames(el)).toEqual(["Heading 1", "Heading 2", "Heading 3"]);
@@ -78,21 +78,22 @@ describe("search", () => {
         const { el, editor } = await setupEditor("<p>ab[]</p>");
         insertText(editor, "/");
         await animationFrame();
-        expect(commandNames(el).length).toBe(26);
-        expect(".o-we-category").toHaveCount(7);
+        expect(commandNames(el).length).toBe(29);
+        expect(".o-we-category").toHaveCount(8);
         expect(queryAllTexts(".o-we-category")).toEqual([
             "STRUCTURE",
-            "MEDIA",
-            "FORMAT",
-            "NAVIGATION",
             "BANNER",
+            "FORMAT",
+            "MEDIA",
+            "NAVIGATION",
             "WIDGET",
+            "AI TOOLS",
             "BASIC BLOC",
         ]);
 
         insertText(editor, "h");
         await animationFrame();
-        expect(commandNames(el).length).toBe(8);
+        expect(commandNames(el).length).toBe(9);
         expect(".o-we-category").toHaveCount(0);
     });
 
@@ -100,7 +101,7 @@ describe("search", () => {
         const { el, editor } = await setupEditor("<p>ab[]</p>", { props: { iframe: true } });
         insertText(editor, "/");
         await animationFrame();
-        expect(commandNames(el).length).toBe(26);
+        expect(commandNames(el).length).toBe(29);
         insertText(editor, "head");
         await animationFrame();
         expect(commandNames(el)).toEqual(["Heading 1", "Heading 2", "Heading 3"]);
@@ -149,7 +150,7 @@ describe("search", () => {
         insertText(editor, "/");
         await animationFrame();
         expect(".o-we-powerbox").toHaveCount(1);
-        expect(commandNames(el).length).toBe(26);
+        expect(commandNames(el).length).toBe(29);
 
         insertText(editor, "headx");
         await animationFrame();
@@ -598,4 +599,24 @@ test("create a new <p> with press 'Enter' then apply a powerbox command", async 
     await animationFrame();
     press("Enter");
     expect(getContent(el)).toBe("<p>ab</p><h1>[]cd</h1>");
+});
+
+test("add plugins with the same powerboxCategory should crash", async () => {
+    expect.errors(1);
+    class Plugin1 extends Plugin {
+        static resources = () => ({
+            powerboxCategory: { id: "test", name: "Test", sequence: 10 },
+        });
+    }
+    class Plugin2 extends Plugin {
+        static resources = () => ({
+            powerboxCategory: { id: "test", name: "Test", sequence: 10 },
+        });
+    }
+    await expect(
+        setupEditor("<p>ab[]cd</p>", {
+            config: { Plugins: [...MAIN_PLUGINS, Plugin1, Plugin2] },
+        })
+    ).rejects.toThrow();
+    expect(["Duplicate category id: test"]).toVerifyErrors();
 });
