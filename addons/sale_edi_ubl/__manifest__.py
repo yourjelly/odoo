3f8ce041c77ec8c7e1d28d584d14e894efe0f8a7
kcv-odoo <kcv@odoo.com>
2024-07-08 10:15:26 +0530

    [ADD] sale{,edi_ubl}: add sale edi
    This commit add EDI to import data from XML file of purchase
    for BIS 3 format.
    
    task-3791197

---
+++ addons/sale_edi_ubl/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Import electronic orders with UBL",
+    'version': '1.0',
+    'category': 'Sales/Sales',
+    'description': """
+Electronic ordering module
+===========================
+
+Allows to import formats: UBL Bis 3.
+When uploading or pasting Files in order list view with order related data inside XML file or PDF
+File with embedded xml data will allow seller to retrieve Order data from Files.
+    """,
+    'depends': ['sale', 'account_edi_ubl_cii'],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
