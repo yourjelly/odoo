c5ceb2f61194ed0b02dad32f4e4e7b087f010849
Thomas Josse (THJO) <thjo@odoo.com>
2024-07-22 10:09:38 +0200

    [IMP] html_editor: add the /draw command
    This commit is part of the big refactoring project to convert every Knowledge
    commands and behaviors to the new html_editor.
    
    This here adds the /draw command as standard inside the editor, so that it can
    be used anywhere.
    
    task-4061015

--- addons/html_editor/static/src/others/embedded_components/embedding_sets.js
+++ addons/html_editor/static/src/others/embedded_components/embedding_sets.js
@@ -1,7 +1,12 @@
+import { excalidrawEmbedding } from "@html_editor/others/embedded_components/excalidraw/component/excalidraw";
+import { readonlyExcalidrawEmbedding } from "@html_editor/others/embedded_components/excalidraw/component/readonly_excalidraw";
 import {
     readonlyTableOfContentEmbedding,
     tableOfContentEmbedding,
 } from "@html_editor/others/embedded_components/table_of_content/component/table_of_content";
 
-export const MAIN_EMBEDDINGS = [tableOfContentEmbedding];
-export const READONLY_MAIN_EMBEDDINGS = [readonlyTableOfContentEmbedding];
+export const MAIN_EMBEDDINGS = [excalidrawEmbedding, tableOfContentEmbedding];
+export const READONLY_MAIN_EMBEDDINGS = [
+    readonlyExcalidrawEmbedding,
+    readonlyTableOfContentEmbedding,
+];
