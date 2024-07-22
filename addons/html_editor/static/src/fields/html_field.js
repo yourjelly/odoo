87f2ae6d73245c0500da91bfa88f46d18e5510e6
abd-msyukyu-odoo <abd@odoo.com>
2024-07-22 16:03:15 +0200

    [IMP] knowledge: mount embedded component in the HtmlViewer
    Embedded components such as a table of contents should also be mounted to be
    used in readonly. This commit extracts the minimal mount logic from the embedded
    component plugin and put it in the HtmlViewer in order to do so.

--- addons/html_editor/static/src/fields/html_field.js
+++ addons/html_editor/static/src/fields/html_field.js
@@ -5,7 +5,10 @@ import {
     EMBEDDED_COMPONENT_PLUGINS,
     MAIN_PLUGINS,
 } from "@html_editor/plugin_sets";
-import { MAIN_EMBEDDINGS } from "@html_editor/others/embedded_components/embedding_sets";
+import {
+    MAIN_EMBEDDINGS,
+    READONLY_MAIN_EMBEDDINGS,
+} from "@html_editor/others/embedded_components/embedding_sets";
 import { Wysiwyg } from "@html_editor/wysiwyg";
 import { Component, status, useRef, useState } from "@odoo/owl";
 import { localization } from "@web/core/l10n/localization";
@@ -73,9 +76,7 @@ export class HtmlField extends Component {
         this.state = useState({
             key: 0,
             showCodeView: false,
-            containsComplexHTML: computeContainsComplexHTML(
-                this.props.record.data[this.props.name]
-            ),
+            containsComplexHTML: computeContainsComplexHTML(this.value),
         });
 
         useRecordObserver((record) => {
@@ -196,7 +197,7 @@ export class HtmlField extends Component {
 
     getConfig() {
         const config = {
-            content: this.props.record.data[this.props.name],
+            content: this.value,
             Plugins: [
                 ...MAIN_PLUGINS,
                 ...(this.props.isCollaborative ? COLLABORATION_PLUGINS : []),
@@ -256,6 +257,18 @@ export class HtmlField extends Component {
                     },
                 },
             };
+        }
+        return config;
+    }
+
+    getReadonlyConfig() {
+        const config = {
+            value: this.value,
+            cssAssetId: this.props.cssReadonlyAssetId,
+            hasFullHtml: this.sandboxedPreview,
+        };
+        if (this.props.embeddedComponents) {
+            config.embeddedComponents = READONLY_MAIN_EMBEDDINGS;
         }
         return config;
     }
