af8766d982c0a818e7de3c294ecb9c07fd5aa5b3
Thomas Josse (THJO) <thjo@odoo.com>
2024-07-25 16:02:57 +0200

    [IMP] html_editor: add /file command to standard

--- addons/html_editor/static/src/others/embedded_components/embedding_sets.js
+++ addons/html_editor/static/src/others/embedded_components/embedding_sets.js
@@ -1,14 +1,22 @@
 import { excalidrawEmbedding } from "@html_editor/others/embedded_components/excalidraw/component/excalidraw";
 import { readonlyExcalidrawEmbedding } from "@html_editor/others/embedded_components/excalidraw/component/readonly_excalidraw";
+import { fileEmbedding } from "@html_editor/others/embedded_components/file/component/file";
+import { readonlyFileEmbedding } from "@html_editor/others/embedded_components/file/component/readonly_file";
 import {
     readonlyTableOfContentEmbedding,
     tableOfContentEmbedding,
 } from "@html_editor/others/embedded_components/table_of_content/component/table_of_content";
 import { videoEmbedding } from "@html_editor/others/embedded_components/video/component/video";
 
-export const MAIN_EMBEDDINGS = [excalidrawEmbedding, tableOfContentEmbedding, videoEmbedding];
+export const MAIN_EMBEDDINGS = [
+    excalidrawEmbedding,
+    fileEmbedding,
+    tableOfContentEmbedding,
+    videoEmbedding,
+];
 export const READONLY_MAIN_EMBEDDINGS = [
     readonlyExcalidrawEmbedding,
+    readonlyFileEmbedding,
     readonlyTableOfContentEmbedding,
     videoEmbedding,
 ];
