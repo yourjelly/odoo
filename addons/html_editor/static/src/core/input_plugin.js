004b14885096d7b5305060cf13f2fddb19214601
Thomas Josse (THJO) <thjo@odoo.com>
2024-07-25 16:02:57 +0200

    [IMP] html_editor: add /file command to standard

--- addons/html_editor/static/src/core/input_plugin.js
+++ addons/html_editor/static/src/core/input_plugin.js
@@ -1,3 +1,4 @@
+import { isProtected, isProtecting } from "@html_editor/utils/dom_info";
 import { Plugin } from "../plugin";
 
 export class InputPlugin extends Plugin {
@@ -11,14 +12,34 @@ export class InputPlugin extends Plugin {
         this.addDomListener(this.editable, "input", this.onInput);
     }
 
+    ignoreInput() {
+        const selection = this.document.getSelection();
+        const ignoreElementForInput = (el) => {
+            if (!el) {
+                return false;
+            }
+            return isProtected(el) || isProtecting(el);
+        };
+        return (
+            ignoreElementForInput(selection.anchorNode) ||
+            ignoreElementForInput(selection.focusNode)
+        );
+    }
+
     onBeforeInput(ev) {
         this.dispatch("HISTORY_STAGE_SELECTION");
+        if (this.ignoreInput()) {
+            return;
+        }
         for (const { handler } of this.resources.onBeforeInput || []) {
             handler(ev);
         }
     }
 
     onInput(ev) {
+        if (this.ignoreInput()) {
+            return;
+        }
         this.dispatch("ADD_STEP");
         for (const { handler } of this.resources.onInput || []) {
             handler(ev);
