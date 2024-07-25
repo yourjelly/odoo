617bd2cee961160483aeb96db1c432d9f8ff1e64
Alice Gaudon <agau@odoo.com>
2024-07-25 08:47:11 +0200

    [REF] website: convert header-scroll-effect options to owl
    task-3850413

--- addons/website/static/src/js/editor/snippets.options.js
+++ addons/website/static/src/js/editor/snippets.options.js
@@ -2506,6 +2506,15 @@ registerWebsiteOption("HeaderElements", {
     sequence: 60,
 });
 
+registerWebsiteOption("HeaderScrollEffect", {
+    template: "website.HeaderScrollEffect",
+    selector: "#wrapwrap > header",
+    noCheck: true,
+    data: {
+        groups: ["website.group_website_designer"],
+    },
+});
+
 export class HeaderNavbar extends SnippetOption {
     /**
      * Particular case: we want the option to be associated on the header navbar
