26bd73e178c0f3f2760dbdff9baeca3669c65d22
Robin Lejeune (role) <role@odoo.com>
2024-07-31 16:13:47 +0200

    [REF] web_editor: add an updateOption util
    This commit adds a util `updateOption()` function to modify a snippet
    option already registered on the registry.
    
    task-3850413

--- addons/web_editor/static/src/js/editor/snippets.registry.js
+++ addons/web_editor/static/src/js/editor/snippets.registry.js
@@ -8,3 +8,27 @@ export function registerOption(name, def, options) {
     }
     return registry.category("snippet_options").add(name, def, options);
 }
+
+/**
+ * @param {String} optionName
+ * @param {Object} params - each key is a callback that returns the new value.
+ * The callback takes the option as parameter. `params` can contain every
+ * possible key on `registry.category("snippet_options")`.
+ * @param {boolean} force [true]
+ * @returns snippet option
+ *
+ * Example of use:
+ * 
+ * updateOption("container_width", {
+ *      Class: () => NewContainerWidth,
+ *      selector: (option) => option.selector + ", .o_new_selector",
+ *      exclude: (option) => option.exclude + ", #o_post_content *",
+ * });
+ */
+export function updateOption(optionName, params, force = true) {
+    const option = registry.category("snippet_options").get(optionName);
+    Object.entries(params).forEach(([key, param]) => {
+        option[key] = param(option);
+    });
+    return registry.category("snippet_options").add(optionName, option, { force });
+}
