d8637b89c007e53e263e8b4d408c7e9aeafdab1c
prgo-odoo <prgo@odoo.com>
2024-08-01 18:49:17 +0530

    [ADD] estate:  created estate module
    After this commit:
    - go through file structure
    - implemented model and fields
    - added access rights

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'estate',
+    'version': '1.0',
+    'summary': 'An real state management application',
+    'description': 'A detailed description of my module',
+    'category': 'Sales',
+    'author': 'prgo',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
