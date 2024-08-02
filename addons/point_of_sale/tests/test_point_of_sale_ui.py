8f0386aa8ba63c95f792961fce7442d89fb0fbf1
mega <mega@odoo.com>
2024-08-02 14:49:51 +0530

    [FIX] point_of_sale: prevent error while choosing store with deleted POS category
    Currently, an error occurs when trying to load onboarding clothes.
    
    Step to produce:
    
    - Install the 'point_of_sale' module.
    -  Go to Point of Sale / Configuration / Products / Pos Product Categories,
    Delete record name 'Upper body'.
    - Go to 'Dashboard' to archive all sessions, Click on the 'Clothes' scenario
    card.
    
    ValueError:External ID not found in the system: point_of_sale.pos_category_upper
    
    An error occurs when the system tries to retrieve an external ID
    'point_of_sale.pos_category_upper' at [1] when opening a 'Clothes' scenario card
    in POS, but it is not available.
    
    link [1]: https://github.com/odoo/odoo/blob/475459d06dfb81031812fd0d1813d13eb865451f/addons/point_of_sale/models/pos_config.py#L923-L927
    
    To handle this issue, add a method in the pos.config model that filters out
    unavailable external ID.
    
    Sentry-5667341424

--- addons/point_of_sale/tests/test_point_of_sale_ui.py
+++ addons/point_of_sale/tests/test_point_of_sale_ui.py
@@ -14,3 +14,10 @@ class TestUi(HttpCase):
     def test_01_point_of_sale_tour(self):
 
         self.start_tour("/web", 'point_of_sale_tour', login="admin")
+
+    @tools.mute_logger('odoo.http')
+    def test_point_of_sale_furnitures_scenario_tour(self):
+        self.env['pos.session'].sudo().search([('state', '!=', 'closed')]).close_session_from_ui()
+        self.env['pos.config'].search([]).action_archive()
+        self.env['pos.category'].search([('name', 'ilike', 'Misc')], limit=1).unlink()
+        self.start_tour("/web", 'point_of_sale_furnitures_scenario_tour', login="admin")
