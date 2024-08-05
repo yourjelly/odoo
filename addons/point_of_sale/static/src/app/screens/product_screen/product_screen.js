b5d9bc6e662906f8a0356f1af36362c34b568a1c
parp-odoo <parp@odoo.com>
2024-08-05 17:22:04 +0530

    [FIX] point_of_sale: tb on Reload When Product is Not in Restricted Category
    Steps:
    - Install the Restaurant module and Open the POS session (with demo data)
    - Add any product or combo product to the cart
      NOTE: Product's POS Category should not be configured in restrict category.
    - Refresh the pos ui.
    - traceback
    
    Issue:
    - When the restricted category configuration is enabled, adding a product
      that does not belong to a restricted category triggers
      an error upon refreshing the POS interface.
    
    Cause:
    - The issue arises because the order line fails to retrieve
      related product data after a refresh.
    
    FIX:
    - Implemented a fix to ensure related product data is stored in IndexedDB..
    
    task-4077317

--- addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
+++ addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
@@ -376,25 +376,17 @@ export class ProductScreen extends Component {
             return;
         }
 
-        this.pos.setSelectedCategory(0);
-        const domain = [
-            "|",
-            "|",
-            ["name", "ilike", searchProductWord],
-            ["default_code", "ilike", searchProductWord],
-            ["barcode", "ilike", searchProductWord],
-            ["available_in_pos", "=", true],
-            ["sale_ok", "=", true],
-        ];
-
-        const { limit_categories, iface_available_categ_ids } = this.pos.config;
-        if (limit_categories && iface_available_categ_ids.length > 0) {
-            const categIds = iface_available_categ_ids.map((categ) => categ.id);
-            domain.push(["pos_categ_ids", "in", categIds]);
-        }
         const product = await this.pos.data.searchRead(
             "product.product",
-            domain,
+            [
+                "&",
+                ["available_in_pos", "=", true],
+                "|",
+                "|",
+                ["name", "ilike", searchProductWord],
+                ["default_code", "ilike", searchProductWord],
+                ["barcode", "ilike", searchProductWord],
+            ],
             this.pos.data.fields["product.product"],
             {
                 context: { display_default_code: false },
