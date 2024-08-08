ba90affe1a19e8c9439ad36f3d28129bc2208bc2
Dylan Kiss (dyki) <dyki@odoo.com>
2024-08-08 11:54:21 +0200

    [IMP] account{_payment},payment,sale: support effectively paying in installments
    We already support configuring payment terms that allow customers to pay
    invoices in installments. However, nothing is provided for users to
    easily manage installment payments or for customers to easily pay an
    installment of an invoice.
    
    This commit adds several features to facilitate that:
    
    Register Payment Dialog
    -----------------------
    
    On the invoice form view, the dialog opened when clicking the
    `Register Payment` button has been updated to display the default amount
    as
    
    1. the overdue installments amount, if there are any;
    2. the next outstanding installment amount, if there is one;
    3. the total outstanding amount if there are no installments, or the
       last outstanding amount equals the total outstanding amount.
    
    In case 1 and 2, we display a text underneath the amount field
    indicating what users are paying and offer the choice to switch to
    paying the full outstanding amount. When switched to this mode, there is
    a link to switch back to installment mode.
    
    The `Payment Difference` field is not displayed when paying less than
    the proposed overdue amount or next outstanding installment.
    
    Payment Link Dialog
    -------------------
    
    When sharing a payment link with a customer, the dialog has been updated
    to show the same amount by default as the `Register Payment` dialog in
    case there are installments. An extra read-only field is added
    underneath to show the total amount due.
    
    Next to the amounts, we show the outstanding installments, containing
    its number, amount due and date due. This makes it easy for the user to
    customize the amount in the payment link to generate.
    
    Finally the generated payment link does not point to the generic payment
    portal page anymore, but rather to the related invoice portal page with
    the payment dialog open containing the custom amount entered.
    
    Customer Portal: Invoice View
    -----------------------------
    
    Several improvements have been made on the customer invoice portal page.
    
    In the left sidebar, we show the total amount of the invoice at the top
    instead of the amount due. Underneath we show the amount already paid
    and/or the remaining amount due, if applicable.
    
    If there is an installment due or a custom amount specified (through a
    payment link), it is shown right above the `Pay Now` button so the
    customer knows which amount will be set by default when clicking that
    button.
    
    **Payment Dialog**
    
    The payment dialog now shows a switcher that allows paying the next
    installment or the full remaining amount, if there are installments.
    The dialog shows the payment reference and the amount that will be paid
    right above the payment form to make it clear for the customer. The
    reference will be appended with the installment number in case the
    customer is paying an installment.
    
    Finally the page and the dialog support a custom amount to be paid
    through the url parameter `amount`, which is checked against a
    `payment_token` parameter to make sure it hasn't been tampered with.
    An additional url parameter `payment=True` is supported to automatically
    open the dialog on page load.
    
    If the user clicks on a payment link when the invoice has already been
    paid, we show the payment dialog with a simple message saying so.
    
    Customer Portal: My Invoices List
    ---------------------------------
    
    On the portal in the overview list of the customer's invoices, we made
    the payment button also open the payment dialog on the linked invoice
    page, so it's much more convenient to pay the invoice.
    
    We also added an extra column for invoices with installments that
    displays the next amount due and the number of days until its due date.
    If there are overdue installments, it displays the overdue amount.
    
    Clicking the `Pay Now` button will always open the payment dialog on
    the default amount, which equals the amount displayed in this previous
    column.
    
    task-3884561
    
    Co-authored-by: Laurent Smet (las) <las@odoo.com>

--- addons/account_payment/controllers/portal.py
+++ addons/account_payment/controllers/portal.py
@@ -1,19 +1,25 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.http import request
-
+from odoo import http
 from odoo.addons.account.controllers import portal
+from odoo.addons.payment import utils as payment_utils
 from odoo.addons.payment.controllers.portal import PaymentPortal
+from odoo.http import request
 
 
 class PortalAccount(portal.PortalAccount, PaymentPortal):
 
-    def _invoice_get_page_view_values(self, invoice, access_token, **kwargs):
+    def _invoice_get_page_view_values(self, invoice, access_token, payment=False, **kwargs):
+        # EXTENDS account
+
         values = super()._invoice_get_page_view_values(invoice, access_token, **kwargs)
 
         if not invoice._has_to_be_paid():
             # Do not compute payment-related stuff if given invoice doesn't have to be paid.
-            return values
+            return {
+                **values,
+                'payment': payment,  # We want to show the dialog even when everything has been paid (with a custom message)
+            }
 
         logged_in = not request.env.user._is_public()
         # We set partner_id to the partner id of the current user if logged in, otherwise we set it
@@ -49,14 +55,23 @@ class PortalAccount(portal.PortalAccount, PaymentPortal):
         portal_page_values = {
             'company_mismatch': company_mismatch,
             'expected_company': invoice_company,
+            'payment': payment,
         }
         payment_form_values = {
             'show_tokenize_input_mapping': PaymentPortal._compute_show_tokenize_input_mapping(
                 providers_sudo
             ),
         }
+        installment = invoice.get_next_installment_due()
+        next_installment = (
+            installment
+            and installment['type'] != 'early_payment_discount'
+            and installment['amount_residual_currency_unsigned'] != invoice.amount_residual
+        )
         payment_context = {
             'amount': invoice.amount_residual,
+            'amount_custom': float(kwargs['amount']) if kwargs.get('amount') else 0.0,
+            'amount_next_installment': installment['amount_residual_currency_unsigned'] if next_installment else 0.0,
             'currency': invoice.currency_id,
             'partner_id': partner_sudo.id,
             'providers_sudo': providers_sudo,
@@ -64,6 +79,8 @@ class PortalAccount(portal.PortalAccount, PaymentPortal):
             'tokens_sudo': tokens_sudo,
             'availability_report': availability_report,
             'invoice_id': invoice.id,
+            'invoice_name': invoice.name,
+            'invoice_name_installment': f"{invoice.name}-{installment['number']}" if next_installment else "",
             'transaction_route': f'/invoice/transaction/{invoice.id}/',
             'landing_route': invoice.get_portal_url(),
             'access_token': access_token,
@@ -72,3 +89,14 @@ class PortalAccount(portal.PortalAccount, PaymentPortal):
         new_values = portal_page_values | payment_form_values | payment_context | self._get_extra_payment_form_values(**kwargs)
         values |= new_values
         return values
+
+    @http.route()
+    def portal_my_invoice_detail(self, invoice_id, access_token=None, report_type=None, download=False, **kw):
+        # EXTENDS account
+
+        # If we have a custom payment amount, make sure it hasn't been tampered with
+        if kw.get('amount') and not payment_utils.check_access_token(
+            kw.get('payment_token'), invoice_id, kw.get('amount')
+        ):
+            return request.redirect('/my')
+        return super().portal_my_invoice_detail(invoice_id, access_token, report_type, download, **kw)
