e53bf8026c329d8c156cbd596c008dca896ae7af
Jigar Vaghela <jva@odoo.com>
2024-08-21 13:01:22 +0530

    [IMP] l10n_in_purchase_stock: unit price from PO currency to company currency

--- addons/l10n_in_purchase_stock/models/stock_move.py
+++ addons/l10n_in_purchase_stock/models/stock_move.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import models, fields
 
 
 class StockMove(models.Model):
@@ -9,8 +9,12 @@ class StockMove(models.Model):
     def _l10n_in_get_product_price_unit(self):
         self.ensure_one()
         if line_id := self.purchase_line_id:
+            purchase_line = self.purchase_line_id
+            purchase_company = purchase_line.company_id
+            company_currency = purchase_company.currency_id
             if qty := line_id.product_qty:
-                return line_id.price_subtotal / qty
+                price_unit_after_discount = (line_id.price_subtotal / qty)
+                return purchase_line.currency_id._convert(price_unit_after_discount, company_currency, purchase_company, fields.Date.today(), round=False)
             return 0.00
         return super()._l10n_in_get_product_price_unit()
 
