d5d0cc554b4c0afed871cf9940b3f65e1a0158ec
Mohammed Basioni (basm) <basm@odoo.com>
2024-08-28 15:29:53 +0300

    [IMP] test_lint: add a linter check for missing gettext calls.
    Some kinds of contrcuts in Odoo always expect their content to be
    translated: UserError, ValidationErorr, etc. Sometimes, developers
    forget to wrap their strings in a call to gettext. This commit add a
    linter check to catch missing gettext calls.
    
    Task-3955122

--- odoo/addons/test_lint/tests/_odoo_checker_gettext.py
+++ odoo/addons/test_lint/tests/_odoo_checker_gettext.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 import astroid
@@ -34,25 +35,61 @@ class OdooBaseChecker(BaseChecker):
     name = 'odoo'
 
     msgs = {
-        'E8502': (
-            'Bad usage of _, _lt function.',
-            'gettext-variable',
-            'See https://www.odoo.com/documentation/master/developer/misc/i18n/translations.html#variables'
+        "E8502": (
+            "Bad usage of _, _lt function.",
+            "gettext-variable",
+            "See https://www.odoo.com/documentation/master/developer/misc/i18n/translations.html#variables",
         ),
-        'E8505': (
-            'Usage of _, _lt function with multiple unnamed placeholders',
-            'gettext-placeholders',
-            'Use keyword arguments when you have multiple placeholders',
+        "E8505": (
+            "Usage of _, _lt function with multiple unnamed placeholders",
+            "gettext-placeholders",
+            "Use keyword arguments when you have multiple placeholders",
         ),
-        'E8506': (
-            'Usage of %r in _, _lt function',
-            'gettext-repr',
-            'Don\'t use %r to automatically insert quotes in translation strings. Quotes can be different depending on the language: they must be part of the translated string.',
+        "E8506": (
+            "Usage of %r in _, _lt function",
+            "gettext-repr",
+            "Don't use %r to automatically insert quotes in translation strings. Quotes can be different depending on the language: they must be part of the translated string.",
+        ),
+        "E8507": (
+            "Static string passed to %s without gettext call.",
+            "missing-gettext",
+            "Ensure that all static strings passed to certain constructs are wrapped in a gettext call.",
         ),
     }
 
-    @only_required_for_messages('gettext-variable', 'gettext-placeholders', 'gettext-repr')
+    required_inside_functions = ['UserError', 'ValidationError', 'AccessError', 'AccessDenied', 'MissingError']
+
+    @only_required_for_messages('missing-gettext', 'gettext-variable', 'gettext-placeholders', 'gettext-repr')
     def visit_call(self, node):
+        file_path = self.linter.current_file
+        if (
+            "test" in file_path
+            and (module_path := find_module_root(file_path))
+            and (
+                # Check if the module's base name starts with "test_"
+                # This identifies modules dedicated to testing (e.g., test_mail)
+                os.path.basename(os.path.normpath(module_path)).startswith("test_")
+                # OR check if the file is in a "tests" subdirectory within the module
+                # This identifies test files located within the standard "tests" directory/package
+                or file_path[len(module_path) + 1 :].startswith("tests" + os.sep)
+            )
+        ):
+            return
+
+        if (
+            isinstance(node.func, astroid.Name)
+            and node.func.name in self.required_inside_functions
+            and len(node.args) > 0
+        ):
+            first_arg = node.args[0]
+            if (
+                isinstance(first_arg, astroid.Const)
+                and isinstance(first_arg.value, str)
+                and not (isinstance(first_arg.parent, astroid.Call) and first_arg.parent.func.name in ("_", "_lt"))
+            ):
+                self.add_message("missing-gettext", node=node, args=(node.func.name,))
+                return
+
         if not isinstance(node.func, astroid.Name) or node.func.name not in ("_", "_lt"):
             return
         first_arg = node.args[0]
@@ -67,3 +104,14 @@ class OdooBaseChecker(BaseChecker):
 
 def register(linter):
     linter.register_checker(OdooBaseChecker(linter))
+
+def find_module_root(file_path):
+    """Manually determine the module root by searching for manifest files."""
+    current_dir = os.path.dirname(file_path)
+
+    while current_dir and current_dir != os.path.dirname(current_dir):
+        if os.path.isfile(os.path.join(current_dir, "__manifest__.py")) or os.path.isfile(os.path.join(current_dir, "__openerp__.py")):
+            return current_dir
+        current_dir = os.path.dirname(current_dir)
+
+    return None
