96245ca1412ab8416ceb3bbe452877e3c3c1f834
Prakash Prajapati <ppr@odoo.com>
2024-09-05 10:26:54 +0530

    [IMP] project: wip1

--- addons/project/static/src/project_sharing/components/breadcrumbs/project_sharing_breadcrumbs.js
+++
@@ -1,5 +0,0 @@
-import { Breadcrumbs } from "@web/search/breadcrumbs/breadcrumbs";
-
-export class ProjectSharingBreadcrumbs extends Breadcrumbs {
-    static template = "project.ProjectSharingBreadcrumbs";
-}
--- addons/project/static/src/project_sharing/components/project_sharing_control_panel/project_sharing_control_panel.js
+++ addons/project/static/src/project_sharing/components/project_sharing_control_panel/project_sharing_control_panel.js
@@ -1,15 +1,18 @@
 import { kanbanView } from '@web/views/kanban/kanban_view';
 import { ControlPanel } from "@web/search/control_panel/control_panel";
-import { ProjectSharingBreadcrumbs } from '../breadcrumbs/project_sharing_breadcrumbs';
 
 
 export class ProjectSharingControlPanel extends ControlPanel {
-	static components = {
-        ...ControlPanel.components,
-        ProjectSharingBreadcrumbs,
-    };
+    setup() {
+        super.setup();
+        this.breadcrumbs.unshift({
+            name: 'project',
+            url: '/my/projects',
+            onSelected: () => {
+                window.location.href = '/my/projects';
+            },
+        });
+    }
 }
-
-ProjectSharingControlPanel.template = "project.project_sharing_control_panel";
 
 kanbanView.ControlPanel = ProjectSharingControlPanel;
