2fd57abe77d0bc7358452fd2b00cdd35cdf9257e
abd-msyukyu-odoo <abd@odoo.com>
2024-09-19 11:06:53 +0200

    [FIX] html_editor, website: don't always add a website domain

--- addons/website/static/src/components/media_dialog/image_selector.js
+++ addons/website/static/src/components/media_dialog/image_selector.js
@@ -3,6 +3,7 @@
 import { patch } from "@web/core/utils/patch";
 import { ImageSelector } from '@web_editor/components/media_dialog/image_selector';
 import { ImageSelector as HtmlImageSelector } from "@html_editor/main/media/media_dialog/image_selector";
+import { FileDocumentsSelector } from "@html_editor/others/embedded_components/file/file_media_dialog/file_documents_selector";
 
 patch(ImageSelector.prototype, {
     get attachmentsDomain() {
@@ -20,4 +21,14 @@ patch(HtmlImageSelector.prototype, {
         domain.push(['key', '=', false]);
         return domain;
     }
+});
+
+// TODO ABD: is this patch applied if in a module which is not dependent on website ?
+patch(FileDocumentsSelector.prototype, {
+    get attachmentsDomain() {
+        const domain = super.attachmentsDomain;
+        domain.push("|", ["url", "=", false], "!", ["url", "=like", "/web/image/website.%"]);
+        domain.push(["key", "=", false]);
+        return domain;
+    },
 });
