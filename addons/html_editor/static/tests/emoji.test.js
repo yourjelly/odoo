c9ea557d589570fb3cc61feb0755c6971d04a2d3
fdardenne <dafl@odoo.com>
2024-09-26 17:01:04 +0200

    [REF] html_editor: use beforeEach for emoji loadBundle

--- addons/html_editor/static/tests/emoji.test.js
+++ addons/html_editor/static/tests/emoji.test.js
@@ -1,4 +1,4 @@
-import { expect, test } from "@odoo/hoot";
+import { expect, test, beforeEach } from "@odoo/hoot";
 import { click, press, waitFor } from "@odoo/hoot-dom";
 import { animationFrame } from "@odoo/hoot-mock";
 import { loadBundle } from "@web/core/assets";
@@ -6,16 +6,19 @@ import { setupEditor } from "./_helpers/editor";
 import { getContent } from "./_helpers/selection";
 import { insertText, undo } from "./_helpers/user_actions";
 
+beforeEach(async () => {
+    await loadBundle("web.assets_emoji");
+});
+
 test("add an emoji with powerbox", async () => {
     const { el, editor } = await setupEditor("<p>ab[]</p>");
-    await loadBundle("web.assets_emoji");
 
     expect(".o-EmojiPicker").toHaveCount(0);
     expect(getContent(el)).toBe("<p>ab[]</p>");
 
     insertText(editor, "/emoji");
     press("enter");
-    await waitFor(".o-EmojiPicker", { timeout: 1000 });
+    await waitFor(".o-EmojiPicker");
     expect(".o-EmojiPicker").toHaveCount(1);
 
     await click(".o-EmojiPicker .o-Emoji");
@@ -32,7 +35,7 @@ test("click on emoji command to open emoji picker", async () => {
     insertText(editor, "/emoji");
     await animationFrame();
     click(".active .o-we-command-name");
-    await waitFor(".o-EmojiPicker", { timeout: 1000 });
+    await waitFor(".o-EmojiPicker");
     expect(".o-EmojiPicker").toHaveCount(1);
 });
 
@@ -44,7 +47,7 @@ test("undo an emoji", async () => {
     insertText(editor, "test");
     insertText(editor, "/emoji");
     press("enter");
-    await waitFor(".o-EmojiPicker", { timeout: 1000 });
+    await waitFor(".o-EmojiPicker");
     await click(".o-EmojiPicker .o-Emoji");
     expect(getContent(el)).toBe("<p>abtest😀[]</p>");
 
