640a84b8d991069176686ad6867c0da53d46a910
Krzysztof Magusiak (krma) <krma@odoo.com>
2024-10-01 09:08:49 +0000

    [IMP] packaging: use hatchling to make the build
    Replace setuptools and use hatchling to build Odoo.
    The tool supports a `force-include` parameter to build copy addons while
    packaging which allows to build on a read-only file system.

--- odoo/release.py
+++ odoo/release.py
@@ -11,9 +11,30 @@ RELEASE_LEVELS_DISPLAY = {ALPHA: 'a',
 # properly comparable using normal operators, for example:
 #  (6,1,0,'beta',0) < (6,1,0,'candidate',1) < (6,1,0,'candidate',2)
 #  (6,1,0,'candidate',2) < (6,1,0,'final',0) < (6,1,2,'final',0)
-version_info = (18, 1, 0, ALPHA, 1, '')
-series = serie = major_version = '.'.join(str(s) for s in version_info[:2])
-version = serie + RELEASE_LEVELS_DISPLAY[version_info[3]] + str(version_info[4] or '') + version_info[5]
+version = "18.1a1"
+
+
+def _make_version_info(version):
+    split_pos = min((pos for rd in RELEASE_LEVELS_DISPLAY.values() if rd and (pos := version.find(rd)) > 0), default=999)
+    version_info = [int(v) for v in version[:split_pos].split('.')]
+    assert version_info
+    version_info += [0] * (3 - len(version_info))
+    if split_pos == 999:
+        version_info.append(FINAL)
+    else:
+        for level, ld in RELEASE_LEVELS_DISPLAY.items():
+            if ld and version[split_pos:].startswith(ld):
+                version_info.append(level)
+                split_pos += len(ld)
+                break
+        assert len(version_info) == 4
+    version_info += [int(v) for v in version[split_pos:].split('.') if v]
+    version_info += [0] * (5 - len(version_info))
+    return tuple(version_info)
+
+
+version_info = _make_version_info(version)
+series = serie = major_version = '.'.join(str(v) for v in version_info[:2])
 
 # XXX code below duplicates pyproject.toml info and can be fetched using importlib.metadata
 product_name = 'Odoo'
