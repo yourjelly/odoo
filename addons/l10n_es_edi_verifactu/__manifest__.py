4c585d49a4c8f86310c762f3ea616fec07dd8bfd
Yosua Nicolaus <yoni@odoo.com>
2024-10-15 17:41:07 +0200

    [IMP] WIP: work-in-progress

---
+++ addons/l10n_es_edi_verifactu/__manifest__.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'author': 'Odoo',
+    'name': 'Spain - Veri*Factu EDI',
+    'version': '1.0',
+    'category': 'Accounting/Localizations/EDI',
+    'summary': "Module for sending Spanish Veri*Factu XML to the AEAT",
+    'countries': ['es'],
+    'depends': ['l10n_es'],
+    'data': [
+        'wizard/account_move_send_views.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
