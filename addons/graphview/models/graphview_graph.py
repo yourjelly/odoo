e1c0d6ea4aadb6326897263164125506024565ee
Chong Wang (cwg) <cwg@odoo.com>
2024-10-16 16:33:15 +0200

    [IMP] add ondelete
    ondelete cascade graph

--- addons/graphview/models/graphview_graph.py
+++ addons/graphview/models/graphview_graph.py
@@ -190,9 +190,100 @@ class GraphviewGraph(models.Model):
                 'module': 'graphview',
                 'noupdate': True
             })
+        self.create_ondelete_dep()
 
         return {
           "type": "ir.actions.act_window",
           "view_mode": "list,form",
           "res_model": self._name,
         }
+
+    @api.model
+    def create_ondelete_dep(self):
+        nodes = set()
+        edges = []
+        red_nodes = set()
+        for model_name, model in self.env.items():
+            for field_name, field in model._fields.items():
+                if field.type == 'many2one' and field.ondelete == 'cascade':
+                    if self.env.registry[model_name].unlink is not models.Model.unlink:
+                        red_nodes.add(model_name)
+                    nodes.add(model_name)
+                    nodes.add(field.comodel_name)
+                    edges.append({
+                        'data': {'source': field.comodel_name, 'target': model_name, 'label': field_name}
+                    })
+        nodes = [
+            {
+                'data': {
+                    'id': node,
+                    'label': node,
+                    'color': 'red' if node in red_nodes else 'green'
+                },
+            }
+            for node in nodes
+        ]
+        stylesheet = [
+            {
+                'selector': 'node',
+                'style': {
+                    'label': 'data(label)',
+                    'text-wrap': 'wrap',
+                    'text-max-width': '100px',
+                    'text-valign': 'bottom',
+                    'background-color': 'data(color)',  # Use the color from node data
+                    'color': 'black',
+                    'font-size': '5px',
+                    'width': '10px',
+                    'height': '10px'
+                }
+            },
+            {
+                'selector': 'edge',
+                'style': {
+                    'target-arrow-shape': 'triangle',
+                    'target-arrow-color': 'red',
+                    'curve-style': 'bezier',
+                    'line-color': 'black',
+                    'width': '1px',
+                    'arrow-scale': 0.5,
+                }
+            }
+        ]
+        layout = {
+            'name': 'cose',
+            # Optional layout parameters
+            'idealEdgeLength': 100,
+            'nodeOverlap': 20,
+            'refresh': 20,
+            'fit': True,  # whether to fit to viewport
+            'padding': 30,  # padding around layout
+            'randomize': False,  # whether to randomize node positions on first run
+            'componentSpacing': 100,
+            "nodeRepulsion": 4000,
+            "edgeElasticity": 100,
+            'nestingFactor': 5,
+            'gravity': 80,
+            'numIter': 1000,
+            'initialTemp': 200,
+            'coolingFactor': 0.95,
+            'minTemp': 1.0
+        }
+        data = {
+            'name': 'Ondelete Cascade',
+            'layout': layout,
+            'nodes': nodes,
+            'edges': edges,
+            'stylesheet': stylesheet,
+        }
+        if ondelete_graph := self.env.ref('graphview.ondelete', raise_if_not_found=False):
+            ondelete_graph.write(data)
+        else:
+            ondelete_graph = self.create(data)
+            self.env['ir.model.data'].create({
+                'name': 'ondelete',
+                'model': 'graphview.graph',
+                'res_id': ondelete_graph.id,
+                'module': 'graphview',
+                'noupdate': True
+            })
