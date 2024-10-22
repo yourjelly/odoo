keyboard-slayer/odoo-eval

[IMP] web: check edit access rights to enable editing
-This commit ensures that users with 'write' access rights can edit, while
 editing is disabled for users without such permissions.
-test case is added to verify access rights

task-3814575

addons/web/static/tests/views/calendar/calendar_view.test.js
@@ -1874,6 +1874,7 @@ test(`Add filters and specific color`, async () => {
     expect([
         "get_views (event)",
         "check_access_rights (event)",
+        "check_access_rights (event)",
         "search_read (filter.partner) [partner_id]",
         "search_read (event) [display_name, start, stop, is_all_day, color, attendee_ids, type_id]",
     ]).toVerifySteps();
@@ -3860,4 +3861,20 @@ test(`Retaining the 'all' filter value on re-rendering`, async () => {
     await getService("action").switchView("list");
     await getService("action").switchView("calendar");
     expect(`.o_calendar_filter_item[data-value='all'] input`).toBeChecked();
+});
+
+test(`disable editing without write access rights`, async () => {
+    onRpc("check_access_rights", ({ args: [operation] }) => operation !== 'write');
+
+    await mountView({
+        resModel: "event",
+        type: "calendar",
+        arch: `
+            <calendar date_start="start" date_stop="stop">
+                <field name="name"/>
+            </calendar>
+        `,
+    });
+
+    expect(`.fc-event-draggable`).toHaveCount(0);
 });

yourjelly/odoo

addons/web/static/tests/views/calendar/calendar_view.test.js
@@ -2614,6 +2614,7 @@ test(`Add filters and specific color`, async () => {
     expect.verifySteps([
         "get_views (event)",
         "has_access (event)",
+        "has_access (event)",
         "search_read (filter.partner) [partner_id]",
         "search_read (event) [display_name, start, stop, is_all_day, color, attendee_ids, type_id]",
     ]);
@@ -5337,4 +5338,20 @@ test.tags("mobile")("calendar (year): tap on date switch to day scale", async ()
 
     // should open a Quick create modal view in mobile on short tap on date in monthly view
     expect(".modal").toHaveCount(1);
+});
+
+test(`disable editing without write access rights`, async () => {
+    onRpc("has_access", ({ args }) => args[1] != 'write');
+
+    await mountView({
+        resModel: "event",
+        type: "calendar",
+        arch: `
+            <calendar date_start="start" date_stop="stop">
+                <field name="name"/>
+            </calendar>
+        `,
+    });
+
+    expect(`.fc-event-draggable`).toHaveCount(0);
 });