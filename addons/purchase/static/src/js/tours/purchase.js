9e9de31be51b2b5a92c5c07b6f964f6e03befab0
dhha-odoo <dhha@odoo.com>
2024-10-22 18:33:26 +0530

    [FIX] purchase: fix tour by providing appropriate class
    Before this commit:
    ===================
    - The purchase tour was not running successfully because it was unable to
      access the appropriate class.
    
    After this commit:
    ===================
    - Fix the purchase tour by providing the correct class to access the
      actual element, ensuring that the tour runs perfectly without any breaks.
    
    TaskId: 4268662

--- addons/purchase/static/src/js/tours/purchase.js
+++ addons/purchase/static/src/js/tours/purchase.js
@@ -48,20 +48,19 @@ tour.register('purchase_tour' , {
     content: _t("Let's create your first request for quotation."),
     position: "bottom",
 }, {
-    trigger: ".o_form_editable .o_field_many2one[name='partner_id']",
+    trigger: ".o_form_editable .o_field_res_partner_many2one[name='partner_id']",
     extra_trigger: ".o_purchase_order",
     content: _t("Search a vendor name, or create one on the fly."),
     position: "bottom",
     run: function (actions) {
-        actions.text("Agrolait", this.$anchor.find("input"));
+        actions.text("Azure Interior", this.$anchor.find("input"));
     },
 }, {
-    trigger: ".ui-menu-item > a",
+    trigger: ".ui-menu-item > a:contains('Azure Interior')",
     auto: true,
     in_modal: false,
 }, {
     trigger: ".o_field_x2many_list_row_add > a",
-    extra_trigger: ".o_field_many2one[name='partner_id'] .o_external_button",
     content: _t("Add some products or services to your quotation."),
     position: "bottom",
 }, {
@@ -83,14 +82,10 @@ tour.register('purchase_tour' , {
         });
     },
 }, {
-    trigger: '.ui-menu.ui-widget .ui-menu-item a:contains("DESK0001")',
+    trigger: "a:contains('DESK0001')",
     auto: true,
 }, {
-    trigger: '.o_form_editable textarea[name="name"].product_creation_success',
-    auto: true,
-    run: function () {} // wait for product creation
-}, {
-    trigger: ".o_form_editable input[name='product_qty'] ",
+    trigger: "div.o_field_widget[name='product_qty'] input ",
     extra_trigger: ".o_purchase_order",
     content: _t("Indicate the product quantity you want to order."),
     position: "right",
