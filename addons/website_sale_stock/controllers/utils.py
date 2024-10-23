57a25cae0803192828a1a07c3be61729970cb739
jeep-odoo <jeep@odoo.com>
2024-10-23 18:09:27 +0530

    [IMP] sale,webiste_sale,_* : improve ux
    Before this commit:
    - On the /shop page, the price of a product was visible even when it was out
      of stock.
    - In the Product Configurator, there was no information about the available
      quantities.
    
    After this commit:
    - On the /shop page, an `Out of Stock` notification will now be displayed
      alongside the price.
    - In the Product Configurator, remaining quantities will be shown if the option
      `Show available quantity if below X units` is enabled.
    
    task-2746419

--- addons/website_sale_stock/controllers/utils.py
+++ addons/website_sale_stock/controllers/utils.py
@@ -10,15 +10,25 @@ def _get_stock_data(product_or_template, website, **kwargs):
     :rtype: dict
     :return: A dict with the following structure:
         {
+            'available_threshold': float,
             'free_qty': float,
+            'qty': float,
+            'show_availability': boolean,
+            'uom_name': string
         }
     """
+    stock_data = {
+        'available_threshold': product_or_template.available_threshold,
+        'qty': product_or_template.free_qty,
+        'show_availability': product_or_template.show_availability,
+        'uom_name': product_or_template.uom_id.name
+    }
+
     if not product_or_template.allow_out_of_stock_order:
         available_qty = website._get_product_available_qty(
             product_or_template.sudo(), **kwargs
         ) if product_or_template.is_product_variant else 0
         cart_quantity = product_or_template._get_cart_qty(website)
-        return {
-            'free_qty': available_qty - cart_quantity,
-        }
-    return {}
+        stock_data['free_qty'] = available_qty - cart_quantity
+
+    return stock_data
--- addons/website_sale_stock/static/src/js/product/product.js
+++ addons/website_sale_stock/static/src/js/product/product.js
@@ -6,7 +6,11 @@ import { Product } from '@sale/js/product/product';
 patch(Product, {
     props: {
         ...Product.props,
+        available_threshold: { type: Number, optional: true },
         free_qty: { type: Number, optional: true },
+        qty: { type: Number, optional: true },
+        show_availability: { type: Boolean, optional: true },
+        uom_name : { type: String, optional: true },
     },
 });
 
