d72d8d8909a706e9b23418f55a551ab858f4e72e
Romeo Fragomeli <rfr@odoo.com>
2024-10-25 16:01:37 +0200

    [FIX] web: hide useless buttons in PDF.js
    This commit restores the old behaviour hiding not necessary or
    overlapping buttons in PDF.js to avoid confusing UI/UX.

--- addons/web/static/src/core/file_viewer/file_viewer.js
+++ addons/web/static/src/core/file_viewer/file_viewer.js
@@ -1,5 +1,6 @@
-import { Component, useRef, useState } from "@odoo/owl";
+import { Component, useEffect, useRef, useState } from "@odoo/owl";
 import { useAutofocus, useService } from "@web/core/utils/hooks";
+import { hidePDFJSButtons } from "@web/libs/pdfjs";
 
 /**
  * @typedef {Object} File
@@ -32,6 +33,7 @@ export class FileViewer extends Component {
         useAutofocus();
         this.imageRef = useRef("image");
         this.zoomerRef = useRef("zoomer");
+        this.iframeViewerPdfRef = useRef("iframeViewerPdf");
 
         this.isDragging = false;
         this.dragStartX = 0;
@@ -55,6 +57,11 @@ export class FileViewer extends Component {
             angle: 0,
         });
         this.ui = useState(useService("ui"));
+        useEffect(() => {
+            if (this.iframeViewerPdfRef.el) {
+                hidePDFJSButtons(this.iframeViewerPdfRef.el);
+            }
+        });
     }
 
     onImageLoaded() {
