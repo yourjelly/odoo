21f3cf201f1aaaf97c72a13a5f83de80263b60d0
assellalou <asm@odoo.com>
2024-10-31 21:37:50 +0000

    [FIX] account: fix empty string and False/None inconsistencies
    When empty, the name field of account.move.line could inconsistently hold either an empty string or False. This inconsistency led to different results when searching for lines with an empty label, as label->not set and label == "" yielded different outcomes. The latter behavior aligns with expected ORM operations.
    
    This fix ensures the name field is set to False whenever the computed result is an empty string, standardizing search behavior.
    
    opw-4167139
    
    X-original-commit: 437ba987a312fd9d81567e04dc5432a6aa86157c

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -509,11 +509,12 @@ class AccountMoveLine(models.Model):
             elif line.journal_id.type == 'purchase':
                 if product.description_purchase:
                     values.append(product.description_purchase)
-            return '\n'.join(values)
+            return '\n'.join(values) if values else False
 
         term_by_move = (self.move_id.line_ids | self).filtered(lambda l: l.display_type == 'payment_term').sorted(lambda l: l.date_maturity or date.max).grouped('move_id')
         for line in self.filtered(lambda l: l.move_id.inalterable_hash is False):
             if line.display_type == 'payment_term':
+<<<<<<< 17.0
                 term_lines = term_by_move.get(line.move_id, self.env['account.move.line'])
                 n_terms = len(line.move_id.invoice_payment_term_id.line_ids)
                 name = line.move_id.payment_reference or ''
@@ -522,6 +523,15 @@ class AccountMoveLine(models.Model):
                     name = _('%s installment #%s', name, index + 1).lstrip()
                 if n_terms > 1 or not line.name or line._origin.name == line._origin.move_id.payment_reference:
                     line.name = name
+||||||| 1e789d298a5819d6e66218a92e2638d6ce03ae1d
+                if not line.name or line._origin.name == line._origin.move_id.payment_reference:
+                    line.name = line.move_id.payment_reference or ''
+                continue
+=======
+                if not line.name or line._origin.name == line._origin.move_id.payment_reference:
+                    line.name = line.move_id.payment_reference or False
+                continue
+>>>>>>> 703132dc0c4a24b9606f70fd890c9d04b46fd040
             if not line.product_id or line.display_type in ('line_section', 'line_note'):
                 continue
 
