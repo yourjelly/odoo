d93913cb200fe5fc2fe5b8185ffaa211a2c884bb
manv-afk <manv@odoo.com>
2024-11-04 15:13:43 +0100

    [REF] move hardware proxy service to services folder

--- addons/point_of_sale/static/src/@types/services.d.ts
+++ addons/point_of_sale/static/src/@types/services.d.ts
@@ -1,7 +1,7 @@
 declare module "services" {
     import { barcodeReaderService } from "@point_of_sale/app/barcode/barcode_reader_service";
     import { debugService } from "@point_of_sale/app/services/debug_service";
-    import { hardwareProxyService } from "@point_of_sale/app/hardware_proxy/hardware_proxy_service";
+    import { hardwareProxyService } from "@point_of_sale/app/services/hardware_proxy_service";
     import { numberBufferService } from "@point_of_sale/app/services/number_buffer_service";
     import { notificationService } from "@point_of_sale/app/notification/notification_service";
     import { reportService } from "@point_of_sale/app/services/report_service";
