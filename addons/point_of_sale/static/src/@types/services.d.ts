2ae6d1fa441db28916802f308e312ce2d86e1a78
manv-afk <manv@odoo.com>
2024-11-04 15:03:15 +0100

    [REF] move report service to services folder

--- addons/point_of_sale/static/src/@types/services.d.ts
+++ addons/point_of_sale/static/src/@types/services.d.ts
@@ -4,7 +4,7 @@ declare module "services" {
     import { hardwareProxyService } from "@point_of_sale/app/hardware_proxy/hardware_proxy_service";
     import { numberBufferService } from "@point_of_sale/app/services/number_buffer_service";
     import { notificationService } from "@point_of_sale/app/notification/notification_service";
-    import { reportService } from "@point_of_sale/app/utils/report_service";
+    import { reportService } from "@point_of_sale/app/services/report_service";
 
     export interface Services {
         barcode_reader: typeof barcodeReaderService;
