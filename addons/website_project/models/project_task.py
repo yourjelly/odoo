2709671294f3c70104f8b6ae55903185e5524dfc
mjvi-odoo <mjvi@odoo.com>
2024-11-04 11:24:09 +0530

    [REV] website,*: revert set task partner from form email
    * = website_crm, website_project, website_hr_recruitment
    
    task-4295363

--- addons/website_project/models/project_task.py
+++ addons/website_project/models/project_task.py
@@ -6,5 +6,5 @@ from odoo import models, fields
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    # Need this field to check there is no email loops when Odoo reply automatically
+    # Deprecated: To be removed in master.
     email_from = fields.Char('Email From')
