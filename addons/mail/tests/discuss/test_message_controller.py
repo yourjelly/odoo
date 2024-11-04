eed49a95a12382c7772ab62ae2bf0e90ec7b0445
kmdi-odoo <kmdi@odoo.com>
2024-11-04 17:35:58 +0530

    [IMP] mail: add access token for the non internal user
    This commit adds an access_token for non-internal user when
    the attachment data is returned from _to_store of the ir_attachment.
    
    Task-4223292

--- addons/mail/tests/discuss/test_message_controller.py
+++ addons/mail/tests/discuss/test_message_controller.py
@@ -98,6 +98,7 @@ class TestMessageController(HttpCaseWithUserDemo):
             data1["ir.attachment"],
             [
                     {
+                    "access_token": self.attachments[0].access_token,
                     "checksum": False,
                     "create_date": fields.Datetime.to_string(self.attachments[0].create_date),
                     "id": self.attachments[0].id,
@@ -156,6 +157,7 @@ class TestMessageController(HttpCaseWithUserDemo):
             data2["ir.attachment"],
             [
                 {
+                    "access_token": self.attachments[0].access_token,
                     "checksum": False,
                     "create_date": fields.Datetime.to_string(self.attachments[0].create_date),
                     "id": self.attachments[0].id,
@@ -170,6 +172,7 @@ class TestMessageController(HttpCaseWithUserDemo):
                     'url': False,
                 },
                 {
+                    "access_token": self.attachments[1].access_token,
                     "checksum": False,
                     "create_date": fields.Datetime.to_string(self.attachments[1].create_date),
                     "id": self.attachments[1].id,
@@ -206,6 +209,7 @@ class TestMessageController(HttpCaseWithUserDemo):
             data3["ir.attachment"],
             [
                 {
+                    "access_token": self.attachments[0].access_token,
                     "checksum": False,
                     "create_date": fields.Datetime.to_string(self.attachments[0].create_date),
                     "id": self.attachments[0].id,
@@ -220,6 +224,7 @@ class TestMessageController(HttpCaseWithUserDemo):
                     'url': False,
                 },
                 {
+                    "access_token": self.attachments[1].access_token,
                     "checksum": False,
                     "create_date": fields.Datetime.to_string(self.attachments[1].create_date),
                     "id": self.attachments[1].id,
