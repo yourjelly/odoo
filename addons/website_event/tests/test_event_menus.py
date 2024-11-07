keyboard-slayer/odoo-eval

[IMP] website_event,*: event website facelift
*=website,website_event_booth,website_event_track,website_event_exhibitor,website_event_booth_exhibitor,website_event_booth_sale,test_event_full

Before this Commit:
- User can't edit 'Social' Block under Event Registration through website editor
- Ungroup event menus
- Can't create event page that have event's submenus
- User have to manually add event's url path

After this Commit:
- Removed Location page as it's covered in '/register' page
- User can edit Social Share Block using website editor
- Changed 'Introduction' page to 'Home' page
- Changed 'Info' page to 'Practical' page
- On Event Name click user will redirect to '/register' page
- Regrouped event menus inside parent menus for ease of use
- User can create Event pages with submenus
- User don't have to enter Event's url path manually

Note- Some test cases were changed accordingly to work with this PR's flow!

Task-3661323

addons/website_event/tests/test_event_menus.py
@@ -20,14 +20,13 @@ class TestEventMenus(OnlineEventCase, HttpCase):
             'community_menu': False,
         })
         self.assertTrue(event.website_menu)
-        self.assertTrue(event.introduction_menu)
-        self.assertTrue(event.location_menu)
+        self.assertTrue(event.home_menu)
         self.assertTrue(event.register_menu)
         self.assertFalse(event.community_menu)
-        self._assert_website_menus(event, ['Introduction', 'Location', 'Info'], menus_out=['Community'])
+        self._assert_website_menus(event, ['Home', 'Practical'], menus_out=['Community'])
 
         event.community_menu = True
-        self._assert_website_menus(event, ['Introduction', 'Location', 'Info', 'Community'])
+        self._assert_website_menus(event, ['Home', 'Community', 'Practical'])
 
         # test create without any requested menus
         event = self.env['event.event'].create({
@@ -37,15 +36,14 @@ class TestEventMenus(OnlineEventCase, HttpCase):
             'website_menu': False,
         })
         self.assertFalse(event.website_menu)
-        self.assertFalse(event.introduction_menu)
-        self.assertFalse(event.location_menu)
+        self.assertFalse(event.home_menu)
         self.assertFalse(event.register_menu)
         self.assertFalse(event.community_menu)
         self.assertFalse(event.menu_id)
 
         # test update of website_menu triggering 3 sub menus
         event.write({'website_menu': True})
-        self._assert_website_menus(event, ['Introduction', 'Location', 'Info'], menus_out=['Community'])
+        self._assert_website_menus(event, ['Home', 'Practical'], menus_out=['Community'])
 
     @users('user_event_web_manager')
     def test_menu_management_frontend(self):
@@ -56,17 +54,17 @@ class TestEventMenus(OnlineEventCase, HttpCase):
             'website_menu': True,
             'community_menu': False,
         })
-        self._assert_website_menus(event, ['Introduction', 'Location', 'Info'], menus_out=['Community'])
+        self._assert_website_menus(event, ['Home', 'Practical'], menus_out=['Community'])
 
         # simulate menu removal from frontend: aka unlinking a menu
-        event.menu_id.child_id.filtered(lambda menu: menu.name == 'Introduction').unlink()
+        event.menu_id.child_id.filtered(lambda menu: menu.name == 'Home').unlink()
 
         self.assertTrue(event.website_menu)
-        self._assert_website_menus(event, ['Location', 'Info'], menus_out=['Introduction', 'Community'])
+        self._assert_website_menus(event, ['Practical'], menus_out=['Home', 'Community'])
 
         # re-created from backend
-        event.introduction_menu = True
-        self._assert_website_menus(event, ['Introduction', 'Location', 'Info'], menus_out=['Community'])
+        event.home_menu = True
+        self._assert_website_menus(event, ['Home', 'Practical'], menus_out=['Community'])
 
     def test_submenu_url(self):
         """ Test that the different URL of a submenu page of an event are accessible """
@@ -89,17 +87,17 @@ class TestEventMenus(OnlineEventCase, HttpCase):
         )
 
         # Use previous URL for submenu page
-        old_event_1.introduction_menu_ids.menu_id.url = f"/event/test-event-{old_event_1.id}/page/introduction-test-event"
-        old_event_2.introduction_menu_ids.menu_id.url = f"/event/test-event-{old_event_2.id}/page/introduction-test-event"
-        old_event_menus = (old_event_1 + old_event_2).introduction_menu_ids
+        old_event_1.home_menu_ids.menu_id.url = f"/event/test-event-{old_event_1.id}/page/home-test-event"
+        old_event_2.home_menu_ids.menu_id.url = f"/event/test-event-{old_event_2.id}/page/home-test-event"
+        old_event_menus = (old_event_1 + old_event_2).home_menu_ids
         self.assertEqual(len(old_event_menus.view_id), 2, "Each menu should have a view")
 
         # Menu with unique page
-        new_event_menus = (event_1 + event_2).introduction_menu_ids
+        new_event_menus = (event_1 + event_2).home_menu_ids
         self.assertEqual(len(new_event_menus.view_id), 2, "Each menu should have a view")
 
         # Menu without views
-        menu_without_view = event_3._create_menu(1, 'custom', f"/event/test-event-{event_3.id}/page/introduction-test-event", 'website_event.template_intro', 'introduction')
+        menu_without_view = event_3._create_menu(1, 'custom', f"/event/test-event-{event_3.id}/page/home-test-event", 'website_event.template_intro', 'home')
         self.assertEqual(
             len(self.env['website.event.menu'].search([('menu_id', 'in', menu_without_view.ids)]).view_id), 0,
             "The menu should not have a view assigned because an URL has been given manually"
@@ -131,13 +129,14 @@ class TestEventMenus(OnlineEventCase, HttpCase):
         )
 
         # Skip the register and community menus since they already have a unique URL
-        event_1_menus = event_1.menu_id.child_id.filtered(
-            lambda menu: menu.name in ["Introduction", "Location"]
+        event_1_home_menu = event_1.menu_id.child_id.filtered(
+            lambda menu: menu.name == "Home"
         )
-        event_2_menus = event_2.menu_id.child_id.filtered(
-            lambda menu: menu.name in ["Introduction", "Location"]
+        event_2_home_menu = event_2.menu_id.child_id.filtered(
+            lambda menu: menu.name == "Home"
         )
-        for event_1_menu, event_2_menu in zip(event_1_menus, event_2_menus):
+
+        for event_1_menu, event_2_menu in zip(event_1_home_menu, event_2_home_menu):
             end_url_1 = event_1_menu.url.split("/")[-1]
             end_url_2 = event_2_menu.url.split("/")[-1]
             self.assertNotEqual(end_url_1, end_url_2)

yourjelly/odoo

addons/website_event/tests/test_event_menus.py
@@ -20,14 +20,13 @@ class TestEventMenus(OnlineEventCase, HttpCase):
             'community_menu': False,
         })
         self.assertTrue(event.website_menu)
-        self.assertTrue(event.introduction_menu)
-        self.assertTrue(event.location_menu)
+        self.assertTrue(event.home_menu)
         self.assertTrue(event.register_menu)
         self.assertFalse(event.community_menu)
-        self._assert_website_menus(event, ['Introduction', 'Location', 'Info'], menus_out=['Community'])
+        self._assert_website_menus(event, ['Home', 'Practical'], menus_out=['Rooms'])
 
         event.community_menu = True
-        self._assert_website_menus(event, ['Introduction', 'Location', 'Info', 'Community'])
+        self._assert_website_menus(event, ['Home', 'Rooms', 'Practical'])
 
         # test create without any requested menus
         event = self.env['event.event'].create({
@@ -37,15 +36,14 @@ class TestEventMenus(OnlineEventCase, HttpCase):
             'website_menu': False,
         })
         self.assertFalse(event.website_menu)
-        self.assertFalse(event.introduction_menu)
-        self.assertFalse(event.location_menu)
+        self.assertFalse(event.home_menu)
         self.assertFalse(event.register_menu)
         self.assertFalse(event.community_menu)
         self.assertFalse(event.menu_id)
 
         # test update of website_menu triggering 3 sub menus
         event.write({'website_menu': True})
-        self._assert_website_menus(event, ['Introduction', 'Location', 'Info'], menus_out=['Community'])
+        self._assert_website_menus(event, ['Home', 'Practical'], menus_out=['Rooms'])
 
     @users('user_event_web_manager')
     def test_menu_management_frontend(self):
@@ -56,17 +54,17 @@ class TestEventMenus(OnlineEventCase, HttpCase):
             'website_menu': True,
             'community_menu': False,
         })
-        self._assert_website_menus(event, ['Introduction', 'Location', 'Info'], menus_out=['Community'])
+        self._assert_website_menus(event, ['Home', 'Practical'], menus_out=['Rooms'])
 
         # simulate menu removal from frontend: aka unlinking a menu
-        event.menu_id.child_id.filtered(lambda menu: menu.name == 'Introduction').unlink()
+        event.menu_id.child_id.filtered(lambda menu: menu.name == 'Home').unlink()
 
         self.assertTrue(event.website_menu)
-        self._assert_website_menus(event, ['Location', 'Info'], menus_out=['Introduction', 'Community'])
+        self._assert_website_menus(event, ['Practical'], menus_out=['Home', 'Rooms'])
 
         # re-created from backend
-        event.introduction_menu = True
-        self._assert_website_menus(event, ['Introduction', 'Location', 'Info'], menus_out=['Community'])
+        event.home_menu = True
+        self._assert_website_menus(event, ['Home', 'Practical'], menus_out=['Rooms'])
 
     def test_submenu_url(self):
         """ Test that the different URL of a submenu page of an event are accessible """
@@ -89,17 +87,17 @@ class TestEventMenus(OnlineEventCase, HttpCase):
         )
 
         # Use previous URL for submenu page
-        old_event_1.introduction_menu_ids.menu_id.url = f"/event/test-event-{old_event_1.id}/page/introduction-test-event"
-        old_event_2.introduction_menu_ids.menu_id.url = f"/event/test-event-{old_event_2.id}/page/introduction-test-event"
-        old_event_menus = (old_event_1 + old_event_2).introduction_menu_ids
+        old_event_1.home_menu_ids.menu_id.url = f"/event/test-event-{old_event_1.id}/page/home-test-event"
+        old_event_2.home_menu_ids.menu_id.url = f"/event/test-event-{old_event_2.id}/page/home-test-event"
+        old_event_menus = (old_event_1 + old_event_2).home_menu_ids
         self.assertEqual(len(old_event_menus.view_id), 2, "Each menu should have a view")
 
         # Menu with unique page
-        new_event_menus = (event_1 + event_2).introduction_menu_ids
+        new_event_menus = (event_1 + event_2).home_menu_ids
         self.assertEqual(len(new_event_menus.view_id), 2, "Each menu should have a view")
 
         # Menu without views
-        menu_without_view = event_3._create_menu(1, 'custom', f"/event/test-event-{event_3.id}/page/introduction-test-event", 'website_event.template_intro', 'introduction')
+        menu_without_view = event_3._create_menu(1, 'custom', f"/event/test-event-{event_3.id}/page/home-test-event", 'website_event.template_home', 'home')
         self.assertEqual(
             len(self.env['website.event.menu'].search([('menu_id', 'in', menu_without_view.ids)]).view_id), 0,
             "The menu should not have a view assigned because an URL has been given manually"
@@ -131,13 +129,14 @@ class TestEventMenus(OnlineEventCase, HttpCase):
         )
 
         # Skip the register and community menus since they already have a unique URL
-        event_1_menus = event_1.menu_id.child_id.filtered(
-            lambda menu: menu.name in ["Introduction", "Location"]
+        event_1_home_menu = event_1.menu_id.child_id.filtered(
+            lambda menu: menu.name == "Home"
         )
-        event_2_menus = event_2.menu_id.child_id.filtered(
-            lambda menu: menu.name in ["Introduction", "Location"]
+        event_2_home_menu = event_2.menu_id.child_id.filtered(
+            lambda menu: menu.name == "Home"
         )
-        for event_1_menu, event_2_menu in zip(event_1_menus, event_2_menus):
+
+        for event_1_menu, event_2_menu in zip(event_1_home_menu, event_2_home_menu):
             end_url_1 = event_1_menu.url.split("/")[-1]
             end_url_2 = event_2_menu.url.split("/")[-1]
             self.assertNotEqual(end_url_1, end_url_2)