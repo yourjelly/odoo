ce471202bb6fbd7c38f2d8cd6b7951266d17d519
Pedram (pebr) <pebr@odoo.com>
2024-11-08 14:38:44 +0000

    [PERF] pos_loyalty: make product addition to cart faster
    Before this commit, adding products to the cart would become slow when
    there were several loyalty.card records and several pos.order.line
    records.
    
    opw-4317125
    
    X-original-commit: 0b346282cd70d2945d1fb320b3d4761261d9e081

--- addons/pos_loyalty/static/src/overrides/models/data_service_options.js
+++ addons/pos_loyalty/static/src/overrides/models/data_service_options.js
@@ -3,6 +3,7 @@ import { patch } from "@web/core/utils/patch";
 
 patch(DataServiceOptions.prototype, {
     get databaseTable() {
+<<<<<<< 18.0
         return {
             ...super.databaseTable,
             "loyalty.card": {
@@ -12,6 +13,23 @@ patch(DataServiceOptions.prototype, {
                         (l) => l.coupon_id?.id === record.id
                     );
                 },
+||||||| c05de309a8828c4a84c423753ed015d4e8513cec
+        const data = super.databaseTable;
+        data.push({
+            name: "loyalty.card",
+            key: "id",
+            condition: (record) => {
+                return record.models["pos.order.line"].find((l) => l.coupon_id?.id === record.id);
+=======
+        const data = super.databaseTable;
+        data.push({
+            name: "loyalty.card",
+            key: "id",
+            condition: (record) => {
+                return record["<-pos.order.line.coupon_id"].find(
+                    (l) => l.order_id?.finalized && typeof l.order_id.id === "number"
+                );
+>>>>>>> c581cf4a33b5b8dbe7732f88196433f98296473a
             },
         };
     },
