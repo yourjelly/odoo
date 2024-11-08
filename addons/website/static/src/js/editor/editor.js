2fe020ba6f80339d1feb5de4e1aae1702125e568
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-11-08 10:52:19 +0100

    [FIX] website: url autocomplete in linktools
    Issue:
    ======
    url autocomplete doesn't work in mass mailing
    
    Steps to reproduce the issue:
    =============================
    - Create a new mass mailing
    - Choose welcome message
    - Select some text and click on the link icon on the toolbar in the
      snippets sidebar
    - You will see the hint of `type / to search ....`
    - If you type that nothing happens
    
    Origin of the issue:
    ====================
    The behavior of autocomplete was added only in website but the hint was
    added for the linkTools in general.
    
    Solution:
    =========
    Patch linkTools to include the behavior too.
    
    opw-4318224

--- addons/website/static/src/js/editor/editor.js
+++ addons/website/static/src/js/editor/editor.js
@@ -3,7 +3,8 @@
 import { LinkDialog } from "@web_editor/js/wysiwyg/widgets/link_dialog";
 import { patch } from "@web/core/utils/patch";
 import wUtils from "@website/js/utils";
-import { useEffect } from '@odoo/owl';
+import { onMounted, onWillUnmount, useEffect } from '@odoo/owl';
+import { LinkTools } from "@web_editor/js/wysiwyg/widgets/link_tools";
 
 patch(LinkDialog.prototype, {
     /**
@@ -26,4 +27,30 @@ patch(LinkDialog.prototype, {
             return () => unmountAutocompleteWithPages();
         }, () => [this.$link, this.linkComponentWrapperRef.el]);
     }
+});
+
+patch(LinkTools.prototype, {
+    /**
+     * Allows the URL input to propose existing website pages.
+     *
+     * @override
+     */
+    async setup() {
+        super.setup(...arguments);
+        this.unmountAutocompleteWithPages = null;
+        onMounted(() => {
+            const input = this.$el?.get(0)?.querySelector(`input[name="url"]`);
+            if (!input) {
+                return;
+            }
+            const options = {
+                body: this.$link && this.$link[0].ownerDocument.body,
+                urlChosen: () => this.__onURLInput(),
+            };
+            this.unmountAutocompleteWithPages = wUtils.autocompleteWithPages(input, options);
+        });
+        onWillUnmount(() => {
+            this.unmountAutocompleteWithPages?.();
+        });
+    },
 });
