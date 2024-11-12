72fb28b45ada4ba0fa500b390ca58bc110c121f5
alan-odoo <alan@odoo.com>
2024-11-12 08:57:58 +0100

    *creation of the websiteEventModalEmailReminder widget.

--- addons/website_event_track/static/src/js/event_track_email_reminder.js
+++ addons/website_event_track/static/src/js/event_track_email_reminder.js
@@ -2,18 +2,38 @@
 
 import publicWidget from "@web/legacy/js/public/public_widget";
 import { rpc } from "@web/core/network/rpc";
-import { Component } from "@odoo/owl";
 
 publicWidget.registry.websiteEventModalEmailReminder = publicWidget.Widget.extend({
-    selector: '.o_wetrack_js_email_modal_reminder',
-    start: function (){
-        console.log("ici you are in the modal.");
-    }
+    template: 'website_event_track.modal_email_reminder',
+    selector: '.o_wetrack_js_modal_email_reminder',
+    events: {
+        'click .btn-close': '_remove',
+        'click .btn-cancel': '_remove',
+        'submit #email_reminder_form': '_submit',
+    },
+
+    /**
+     * @override
+     */
+    init: function (trackId) {
+        this._super.apply(this, arguments);
+        this.trackId = trackId
+    },
 
     //--------------------------------------------------------------------------
     // Handlers
     //-------------------------------------------------------------------------
 
+    _remove: function (){
+        this.$el.remove();
+    },
+
+    _submit: function (ev){
+        ev.preventDefault();
+        var form = this.$(ev.target);
+        rpc(form.attr('action'), Object.fromEntries(new FormData(form[0]).entries()));
+        this._remove();
+    },
 });
 
 export default publicWidget.registry.websiteEventTrackReminder;
