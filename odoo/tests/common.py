5242a8e41b1e911b20fa9755d5303f5bc6aad989
Pierre Paridans <app@odoo.com>
2024-11-14 13:49:31 +0100

    [FIX] wip: support "new" Chrome headless mode

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -1093,7 +1093,7 @@ class ChromeBrowser:
             debug=False,
     ):
         headless_switches = {
-            '--headless': '',
+            '--headless': 'new',
             '--disable-extensions': '',
             '--disable-background-networking' : '',
             '--disable-background-timer-throttling' : '',
