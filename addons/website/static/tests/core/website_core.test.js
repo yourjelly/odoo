c36764edb685dfe4a0c18b548a08369249836835
Géry Debongnie <ged@odoo.com>
2024-11-15 10:44:04 +0100

    make test pass

--- addons/website/static/tests/core/website_core.test.js
+++ addons/website/static/tests/core/website_core.test.js
@@ -1,33 +1,21 @@
 import { expect, test } from "@odoo/hoot";
-import { animationFrame, Deferred } from "@odoo/hoot-mock";
+import { animationFrame } from "@odoo/hoot-mock";
 
-import { onRpc } from "@web/../tests/web_test_helpers";
 import { Interaction } from "@website/core/interaction";
 import { startInteraction } from "./helpers";
 
 test("wait for translation before starting interactions", async () => {
-    const def = new Deferred();
-    onRpc("/web/webclient/translations", async () => {
-        await def;
-    });
-    let started = false;
+    let flag = false;
 
     class Test extends Interaction {
         static selector = ".test";
 
         setup() {
-            started = true;
+            flag = true;
+            expect("localization" in this.services).toBe(true)
         }
     }
-
-    const p = startInteraction(Test, `<div class="test"></div>`);
-    await animationFrame();
-    expect(started).toBe(false);
-
-    def.resolve();
-    await p;
-    await animationFrame();
-    expect(started).toBe(true);
+    await startInteraction(Test, `<div class="test"></div>`);
 });
 
 
