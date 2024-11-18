642d7028ae2dca7673d1418d423ae14c86bb20f2
Krzysztof Magusiak (krma) <krma@odoo.com>
2024-11-18 06:39:13 +0000

    [IMP] *: migrate additional indexes
    Use `models.Index` for additional indexes.

--- addons/mail/models/discuss/discuss_channel_member.py
+++ addons/mail/models/discuss/discuss_channel_member.py
@@ -51,6 +51,8 @@ class DiscussChannelMember(models.Model):
     rtc_session_ids = fields.One2many(string="RTC Sessions", comodel_name='discuss.channel.rtc.session', inverse_name='channel_member_id')
     rtc_inviting_session_id = fields.Many2one('discuss.channel.rtc.session', string='Ringing session')
 
+    _seen_message_id_idx = models.Index("(channel_id, partner_id, seen_message_id)")
+
     @api.autovacuum
     def _gc_unpin_outdated_sub_channels(self):
         outdated_dt = fields.Datetime.now() - timedelta(days=2)
