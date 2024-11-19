77e9cc557d9a399e13cfec2a9dea0cdc3c92d16c
Jitendra Prajapat <jipr@odoo.com>
2024-11-19 12:35:38 +0530

    [IMP] pos_restaurant: early receipt printing
    Before this commit:
    ====================
    The `Bill`control button was always visible in POS Restaurant
    
    After this commit:
    =======================
    - The visibility of the `Bill` control button is now determined by the
      activation of the `Early Receipt Printing` setting in the Restaurant
      configuration. and the receipt will have a label pro forma receipt if early
      receipt print
    - Fixed a traceback occurred while trying to print a receipt using the control
      button `Bill`​.
    
    Task- 4342124

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -1439,7 +1439,9 @@ export class PosStore extends Reactive {
             { webPrintFallback: true }
         );
         const nbrPrint = order.nb_print;
-        await this.data.write("pos.order", [order.id], { nb_print: nbrPrint + 1 });
+        if (typeof order.id === "number") {
+            await this.data.write("pos.order", [order.id], { nb_print: nbrPrint + 1 });
+        }
         return true;
     }
     getOrderChanges(skipped = false, order = this.get_order()) {
